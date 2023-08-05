373a64223c008ea37abbf3f3d4047d5eb0bcc099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package fac/1.0.0/fac-1.0.0.zip

---
+++ packages/fac/fac-1.0.0.zip/fac-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='fac',
+      author='guzhen',
+      author_email='604608916@qq.com',
+      description='a GUI project for calculating the factorial of the number you input',
+      version='1.0.0'
+      )
