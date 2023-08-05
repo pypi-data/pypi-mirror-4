43932db104f6810558bd36c97569a4cddbfdab72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:42 +0000

    Add package nirnaor/1.0.0/nirnaor-1.0.0.tar.gz

---
+++ packages/nirnaor/nirnaor-1.0.0.tar.gz/nirnaor-1.0.0/nirnaor.py
@@ -0,0 +1,4 @@
+"""this is nir comments about the module"""
+def print_nir(string):
+    print(string + " from nir naor")
+
---
+++ packages/nirnaor/nirnaor-1.0.0.tar.gz/nirnaor-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+        name = 'nirnaor',
+        version = '1.0.0',
+        py_modules = ['nirnaor'],
+        author = 'nirnaor',
+        author_email = 'nirnaori@gmail.com',
+        url = 'www.bla.com',
+        description = 'a function that adds from nir naor just to check something'
+      )
+
+
