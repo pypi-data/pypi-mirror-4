0d3b2c3a4342b48794888cafa8424317c3a6304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:09 +0000

    Add package cloudpy/0.1.3/cloudpy-0.1.3.tar.gz

---
+++ packages/cloudpy/cloudpy-0.1.3.tar.gz/cloudpy-0.1.3/cloudpy/cloudpy_package.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from cloudpy_deps import DepsFinder
+from cloudpy_files import DirTree
+from cloudpy_struct import DirStruct
+import os
+import shutil
+import uuid
+
+
+class Package(object):
+    def __init__(self, mainfile, name=None, guess_mods=True, guess_files=False):
+        self.mainfile = mainfile
+        self.df = DepsFinder(mainfile, guess_mods, guess_files)
+        self.dt = DirTree(self.df.needed_files)
+        self.files = []
+        if name:
+            self.name = name
+        else:
+            self.name = uuid.uuid1().hex
+        self.dir = DirStruct(self.name)
+
+    def build(self):
+        for f in self.df.needed_files:
+            rp = f[self.dt.get_cplen():]
+            target = self.dir.append("SRC_DIR", rp)
+            # self.files.append(rp)
+            path_dir = os.path.split(target)[0]
+            if not os.path.exists(path_dir):
+                os.makedirs(path_dir)
+            shutil.copy2(f, target)
+        target_wd = self.dir.append("SRC_DIR", os.getcwd()[self.dt.get_cplen():])
+        if not os.path.exists(target_wd):
+            os.makedirs(target_wd)
+        if not os.path.exists(self.dir.CONF_DIR):
+            os.makedirs(self.dir.CONF_DIR)
+        self.df.write2file_mods(self.dir.MODS_FILE)
+        # self.write2file_files()
+        self.write_config()
+
+    def write_config(self):
+        with open(self.dir.CONF_FILE, "w") as f:
+            pwd = os.getcwd()[self.dt.get_cplen():]
+            if not pwd:
+                pwd = "./"
+            f.write("PWD=%s\n" % pwd)
+            exec_path = os.path.abspath(self.mainfile)[self.dt.get_cplen():]
+            f.write("EXEC=%s\n" % exec_path)
+
+def pack(script, name, mods, files):
+    p = Package(script, name, mods, files)
+    p.build()
+    return p.name
