357aa67cb3b51cac4b572fbf238c6b04c3230401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package hlsclient/0.2.8/hlsclient-0.2.8.tar.gz

---
+++ packages/hlsclient/hlsclient-0.2.8.tar.gz/hlsclient-0.2.8/hlsclient/__main__.py
@@ -0,0 +1,43 @@
+import logging
+import time
+
+import helpers
+
+from balancer import Balancer
+from consumer import consume_from_balancer
+from discover import discover_playlists, get_servers
+from combine import combine_playlists
+from cleaner import clean
+
+
+def main():
+    config = helpers.load_config()
+    helpers.setup_logging(config)
+
+    logging.debug('HLS CLIENT Started')
+    destination = config.get('hlsclient', 'destination')
+    clean_maxage = int(config.get('hlsclient', 'clean_maxage'))
+    encrypt = config.getboolean('hlsclient', 'encrypt')
+
+    # ignore all comma separated wildcard names for `clean` call
+    ignores = helpers.get_ignore_patterns(config)
+
+    balancer = Balancer()
+
+    while True:
+        try:
+            playlists = discover_playlists(config)
+            combine_playlists(playlists, destination)
+            paths = get_servers(playlists)
+            balancer.update(paths)
+            consume_from_balancer(balancer, playlists, destination, encrypt)
+            clean(destination, clean_maxage, ignores)
+        except Exception as e:
+            logging.exception('An unknown error happened')
+        except KeyboardInterrupt:
+            logging.debug('Quitting...')
+            return
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
