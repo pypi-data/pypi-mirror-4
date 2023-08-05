67568f4dca3a17ff6b43c826b2e33ab5b514080c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:36 +0000

    Add package gametex-django/0.3.1/gametex-django-0.3.1.tar.gz

---
+++ packages/gametex-django/gametex-django-0.3.1.tar.gz/gametex-django-0.3.1/gametex/admin.py
@@ -0,0 +1,28 @@
+"""
+Admin models and interface for GameTeX.
+"""
+
+from django.contrib import admin
+from gametex.models import GameTeXObject, GameTeXFieldValue, GameTeXUser
+
+class CustomFieldInline(admin.TabularInline):
+    """
+    Inline editor for GameTeX custom fields.
+    """
+    model = GameTeXFieldValue
+
+class GTUInline(admin.TabularInline):
+    """
+    Inline editor for GameTeX users.
+    """
+    model = GameTeXUser
+
+class GTOAdmin(admin.ModelAdmin):
+    """
+    GameTeXObject admin.
+    """
+    fields = ('name', 'macro', 'classes')
+    readonly_fields = ('classes',)
+    inlines = [CustomFieldInline, GTUInline]
+
+admin.site.register(GameTeXObject, GTOAdmin)
