a1b4e3ec360b36723aa46138c6bfb3925e499839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package collective.portlet.collectionmultiview/2.1.3/collective.portlet.collectionmultiview-2.1.3.zip

---
+++ packages/collective.portlet.collectionmultiview/collective.portlet.collectionmultiview-2.1.3.zip/collective.portlet.collectionmultiview-2.1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1.3'
+
+setup(name='collective.portlet.collectionmultiview',
+      version=version,
+      description="A collection portlet product which supports multiple views",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet inigo collection multi view',
+      author='Izhar Firdaus',
+      author_email='izhar@inigo-tech.com',
+      url='http://www.inigo-tech.com/',
+      license='GPLv2+',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'':'src'},
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.portlet.collection>=2.0',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/collective.portlet.collectionmultiview/collective.portlet.collectionmultiview-2.1.3.zip/collective.portlet.collectionmultiview-2.1.3/src/collective/portlet/collectionmultiview/interfaces.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+
+class ICollectionMultiViewRenderer(Interface):
+    pass
+
+class ICollectionMultiViewBaseRenderer(Interface):
+    pass
+
