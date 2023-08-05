a21342d2b1f54c2e644ef6868e0088d946f5cd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package autoresponse/0.3.1/autoresponse-0.3.1.tar.gz

---
+++ packages/autoresponse/autoresponse-0.3.1.tar.gz/autoresponse-0.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='autoresponse',
+    version='0.3.1',
+    packages=['autoresponse'],
+    license='Apache License 2.0',
+    long_description=open('README').read(),
+    install_requires=[
+        'scrapy',
+        ],
+)
+
