47d0c0cb095efd31ef73464c1fe24716f4a94d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package rapidsms-httprouter/2.0.1/rapidsms-httprouter-2.0.1.tar.gz

---
+++ packages/rapidsms-httprouter/rapidsms-httprouter-2.0.1.tar.gz/rapidsms-httprouter-2.0.1/rapidsms_httprouter/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.conf.urls.defaults import *
+from .views import receive, outbox, delivered, console, relaylog, alert
+from django.contrib.admin.views.decorators import staff_member_required
+
+urlpatterns = patterns("",
+   ("^router/receive", receive),
+   ("^router/outbox", outbox),
+   ("^router/relaylog", relaylog),
+   ("^router/alert", alert),
+   ("^router/delivered", delivered),
+   ("^router/console", staff_member_required(console), {}, 'httprouter-console')
+)
