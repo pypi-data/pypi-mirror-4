7dc65de6d851c49404eb7a72ee67f9a4650aa7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:51 +0000

    Add package ContrailOnlineCAService/0.1.0/ContrailOnlineCAService-0.1.0-py2.7.egg

---
+++ packages/ContrailOnlineCAService/ContrailOnlineCAService-0.1.0-py2.7.egg/contrail/security/onlineca/server/wsgi/auth_plugins/__init__.py
@@ -0,0 +1,10 @@
+"""Online CA Service - HTTP Basic Auth plugins package
+ 
+Contrail Project
+"""
+__author__ = "P J Kershaw"
+__date__ = "23/03/11"
+__copyright__ = "(C) 2012 Science and Technology Facilities Council"
+__license__ = "BSD - see LICENSE file in top-level directory"
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__revision__ = "$Id: __init__.py 8189 2012-10-14 20:42:24Z pjkersha $"
