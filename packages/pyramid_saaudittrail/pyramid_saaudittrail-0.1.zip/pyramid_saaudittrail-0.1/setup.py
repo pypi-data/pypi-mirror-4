c3dd4dbc2aa7a5135e1c770e06c1290fc8b0fa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:11 +0000

    Add package pyramid_saaudittrail/0.1/pyramid_saaudittrail-0.1.zip

---
+++ packages/pyramid_saaudittrail/pyramid_saaudittrail-0.1.zip/pyramid_saaudittrail-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='pyramid_saaudittrail',
+      version=version,
+      description="Automatically audit db changes for pyramid and sqlalchemy",
+      long_description=open('README.rst').read(),
+      classifiers=[
+            'Framework :: Pyramid'],
+      keywords='',
+      author='Nathan Van Gheem',
+      author_email='vangheem@gmail.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+            'zope.sqlalchemy',
+            'JSON-Datetime'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
