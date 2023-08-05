e951c0f968f68acf6dd6158abbf0024913b3431d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package you-get/0.2.9/you-get-0.2.9.tar.gz

---
+++ packages/you-get/you-get-0.2.9.tar.gz/you-get-0.2.9/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+PROJ_METADATA = 'you-get.json'
+
+import os, json
+
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA)).read())
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.txt')).read()
+
+from setuptools import setup, find_packages
+setup(
+    name = proj_info['name'],
+    version = proj_info['version'],
+    
+    author = proj_info['author'],
+    author_email = proj_info['author_email'],
+    url = proj_info['url'],
+    download_url = proj_info['download_url'],
+    license = proj_info['license'],
+    
+    description = proj_info['description'],
+    keywords = proj_info['keywords'],
+    
+    long_description = README + '\n\n' + CHANGELOG,
+    
+    packages = find_packages(),
+    
+    platforms = 'any',
+    zip_safe = False,
+    include_package_data = True,
+    
+    classifiers = proj_info['classifiers'],
+    
+    entry_points = {'console_scripts': proj_info['console_scripts']}
+)
