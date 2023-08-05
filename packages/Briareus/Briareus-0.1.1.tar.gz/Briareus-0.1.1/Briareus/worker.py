59d7ca71dc198a33736dca924da45119caec14d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:54 +0000

    Add package Briareus/0.1.1/Briareus-0.1.1.tar.gz

---
+++ packages/Briareus/Briareus-0.1.1.tar.gz/Briareus-0.1.1/Briareus/worker.py
@@ -0,0 +1,23 @@
+from utils import CachedData
+
+class Runtime(object):
+    def __init__(self):
+        self.cache = {}
+
+    def getvalue(self, v):
+        if isinstance(v, CachedData):
+            if v.id in self.cache:
+                return self.cache[v.id]
+            else:
+                self.cache[v.id] = v.value
+                return v.value
+        else:
+            return v
+
+    def eval(self, f, args):
+        return f(*map(self.getvalue, args))
+
+def run():
+    from Corellia.RedisQueue import Worker
+    from config import config
+    Worker(config.host, config.port, config.queue_name).run(Runtime)
