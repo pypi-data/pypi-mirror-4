45239216d6ce68ffca31dbabd1e5c5b8b5c0ec9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package ftw.publisher.receiver/1.3.1/ftw.publisher.receiver-1.3.1.tar.gz

---
+++ packages/ftw.publisher.receiver/ftw.publisher.receiver-1.3.1.tar.gz/ftw.publisher.receiver-1.3.1/ftw/publisher/receiver/events.py
@@ -0,0 +1,19 @@
+from interfaces import IAfterCreatedEvent, IAfterUpdatedEvent
+from zope import interface
+from zope.component.interfaces import ObjectEvent
+
+
+class AfterCreatedEvent(ObjectEvent):
+    interface.implements(IAfterCreatedEvent)
+
+    def __init__(self, context):
+        ObjectEvent.__init__(self, context)
+        self.obj = context
+
+
+class AfterUpdatedEvent(ObjectEvent):
+    interface.implements(IAfterUpdatedEvent)
+
+    def __init__(self, context):
+        ObjectEvent.__init__(self, context)
+        self.obj = context
---
+++ packages/ftw.publisher.receiver/ftw.publisher.receiver-1.3.1.tar.gz/ftw.publisher.receiver-1.3.1/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join('ftw', 'publisher', 'receiver',
+                            'version.txt')).read().strip()
+maintainer = 'Jonas Baumann'
+
+tests_require=[
+    'collective.testcaselayer',
+    'simplejson',
+    ]
+
+setup(name='ftw.publisher.receiver',
+      version=version,
+      description="Staging and publishing addon for Plone contents.",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='ftw publisher receiver',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.publisher.core',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.publisher'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'ftw.publisher.core',
+        ],
+
+      extras_require={
+        'tests': tests_require,
+        'PloneFormGen': ['Products.PloneFormGen'],
+        'python2.4': ['simplejson']
+        },
+
+      tests_require=tests_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
