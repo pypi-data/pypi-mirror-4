e6bacfb38886177c9e988540ef2c7def12d740a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:10 +0000

    Add package WebDispatch/1.0b4/WebDispatch-1.0b4.tar.gz

---
+++ packages/WebDispatch/WebDispatch-1.0b4.tar.gz/WebDispatch-1.0b4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+requires = []
+import os
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    requires.append('ordereddict')
+
+here = os.path.dirname(__file__)
+readme = None
+changes = None
+
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+
+readme = _read("README.txt")
+changes = _read("CHANGES.txt")
+
+setup(
+    name="WebDispatch",
+    author="Atsushi Odagiri",
+    author_email="aodagx@gmail.com",
+    description="dispatch request on wsgi application.",
+    long_description=readme + "\n" + changes,
+    version="1.0b4",
+    test_suite="webdispatch",
+    license="MIT",
+    install_requires=requires,
+    url='http://github.com/aodag/WebDispatch',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+    ],
+)
