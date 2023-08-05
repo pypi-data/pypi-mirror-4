0e1c0cc50fb7ee1fc5f5a8e998df77bb0743dcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:16 +0000

    Add package ipythonblocks/1.1.1/ipythonblocks-1.1.1.tar.gz

---
+++ packages/ipythonblocks/ipythonblocks-1.1.1.tar.gz/ipythonblocks-1.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ipythonblocks',
+      version='1.1.1',
+      description='Practice Python with colored grids in the IPython Notebook',
+      author='Matt Davis',
+      author_email='jiffyclub@gmail.com',
+      url='https://github.com/jiffyclub/ipythonblocks/',
+      packages=['ipythonblocks'],
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Intended Audience :: Education',
+                   'Topic :: Education'])
