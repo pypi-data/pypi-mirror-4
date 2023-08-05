e5519059a579d6c371156b1dd36f53191181a371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package pschachtler/1.4/pschachtler-1.4.tar.gz

---
+++ packages/pschachtler/pschachtler-1.4.tar.gz/pschachtler-1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name 		= "pschachtler", 
+	version 	= "1.4", 
+	py_modules 	= ["pschachtler"],
+	author 		= "Andreas Pfeil", 
+	author_email 	= "der.andreas.pfeil@gmail.com", 
+	url 		= "",
+	description 	= 
+		"Outputs elements in lists of lists of list... one element per line!"
+	)
