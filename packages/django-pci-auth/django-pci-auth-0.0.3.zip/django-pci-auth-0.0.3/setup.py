695005521dd01244c4a26012480ac581453f1c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:53 +0000

    Add package django-pci-auth/0.0.3/django-pci-auth-0.0.3.zip

---
+++ packages/django-pci-auth/django-pci-auth-0.0.3.zip/django-pci-auth-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+VERSION = '0.0.3'
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    description='PCI-compliant authentication application for Django 1.4+. '
+        'Uses "best of" existing libraries then fills in the gaps.',
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.4',
+        'docutils',
+        'django-passwords',
+        'py-bcrypt',
+    ],
+    long_description=open("README.rst").read(),
+    name='django-pci-auth',
+    packages=[
+        'django_pci_auth',
+        'axes',
+    ],
+    package_dir={
+        'django_pci_auth': 'django_pci_auth',
+        '': 'django-axes',
+    },
+    url='https://github.com/aclark4life/django-pci-auth',
+    version=VERSION,
+)
