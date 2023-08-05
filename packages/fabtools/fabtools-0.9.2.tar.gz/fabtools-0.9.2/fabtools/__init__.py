fb2864cc82b32c6c61e90868b52fc976fcb7bf35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package fabtools/0.9.2/fabtools-0.9.2.tar.gz

---
+++ packages/fabtools/fabtools-0.9.2.tar.gz/fabtools-0.9.2/fabtools/__init__.py
@@ -0,0 +1,18 @@
+import fabtools.cron
+import fabtools.deb
+import fabtools.files
+import fabtools.postgres
+import fabtools.mysql
+import fabtools.network
+import fabtools.nodejs
+import fabtools.openvz
+import fabtools.python
+import fabtools.python_distribute
+import fabtools.service
+import fabtools.shorewall
+import fabtools.supervisor
+import fabtools.system
+import fabtools.user
+
+import fabtools.require
+icanhaz = require
