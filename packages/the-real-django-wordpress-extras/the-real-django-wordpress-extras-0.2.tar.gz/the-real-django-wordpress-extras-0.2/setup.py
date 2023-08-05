fc29b9aa98be7b39454b463e2ace8c2c09c000da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:17 +0000

    Add package the-real-django-wordpress-extras/0.2/the-real-django-wordpress-extras-0.2.tar.gz

---
+++ packages/the-real-django-wordpress-extras/the-real-django-wordpress-extras-0.2.tar.gz/the-real-django-wordpress-extras-0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='the-real-django-wordpress-extras',
+    version="0.2",
+    description="A collection of non-WordPress extensions for django-wordpress (the real one)",
+    long_description=long_description,
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@sunlightfoundation.com',
+    url='http://github.com/sunlightlabs/django-wordpress-extras/',
+    packages=[
+        'wordpressext',
+        'wordpressext.templatetags',
+    ],
+    package_data={'wordpressext': ['templates/wordpressext/mail/*.txt']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+    license='BSD License',
+    platforms=["any"],
+)
