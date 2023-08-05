d89cbdcebe1f0ffe78c935727646be3639c62210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package utf8tobibtex/1.0.0/utf8tobibtex-1.0.0.tar.gz

---
+++ packages/utf8tobibtex/utf8tobibtex-1.0.0.tar.gz/utf8tobibtex-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+  name = 'utf8tobibtex',
+  version = '1.0.0',
+  py_modules = ['utf8tobibtex'],
+  author = 'Jörg Knappen',
+  author_email = 'jknappen@web.de',
+  description = 'Converts a lot of latin letter in utf8 to BibTeX input.',
+  )
