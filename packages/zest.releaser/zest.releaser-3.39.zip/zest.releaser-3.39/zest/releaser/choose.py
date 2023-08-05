e24fae52849a1e52823ca0a8f33e35307b819e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:24 +0000

    Add package zest.releaser/3.39/zest.releaser-3.39.zip

---
+++ packages/zest.releaser/zest.releaser-3.39.zip/zest.releaser-3.39/zest/releaser/choose.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import sys
+from zest.releaser import git
+from zest.releaser import hg
+from zest.releaser import bzr
+from zest.releaser import svn
+
+logger = logging.getLogger(__name__)
+
+
+def version_control():
+    """Return an object that provides the version control interface based
+    on the detected version control system."""
+    curdir_contents = os.listdir('.')
+    if '.svn' in curdir_contents:
+        return svn.Subversion()
+    elif '.hg' in curdir_contents:
+        return hg.Hg()
+    elif '.bzr' in curdir_contents:
+        return bzr.Bzr()
+    elif '.git' in curdir_contents:
+        return git.Git()
+    else:
+        logger.critical('No version control system detected.')
+        sys.exit(1)
