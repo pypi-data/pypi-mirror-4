607c6429bd5a8f3e495f6e0371973972f1563457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package tf/1.0.0/tf-1.0.0.tar.gz

---
+++ packages/tf/tf-1.0.0.tar.gz/tf-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'tf',
+	version = '1.0.0',
+	py_modules = ['tf'],
+	author = 'Antonis Kalou',
+	author_email = 'kalouantonis@gmail.com',
+	url = 'hostakis.com',
+	description = 'Randomised true or false values',
+	)
