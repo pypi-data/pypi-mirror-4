5848c499d53ff69742e936a89d9a1f3f23ecec53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:12 +0000

    Add package catsup/0.0.5/catsup-0.0.5.tar.gz

---
+++ packages/catsup/catsup-0.0.5.tar.gz/catsup-0.0.5/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='catsup',
+    version='0.0.5',
+    author='whtsky, messense',
+    author_email='whtsky@me.com, wapdevelop@gmail.com',
+    url='https://github.com/whtsky/catsup',
+    packages=find_packages(),
+    description='Catsup: a lightweight static blog generator',
+    entry_points={
+        'console_scripts': ['catsup= catsup.app:main'],
+    },
+    install_requires=[
+        'tornado',
+        'misaka',
+        'pygments',
+    ],
+    include_package_data=True,
+    license='MIT License',
+)
