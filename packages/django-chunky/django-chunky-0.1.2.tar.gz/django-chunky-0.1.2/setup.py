9f97f264eea994cd794a25f7b36a71de150bad3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package django-chunky/0.1.2/django-chunky-0.1.2.tar.gz

---
+++ packages/django-chunky/django-chunky-0.1.2.tar.gz/django-chunky-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup,find_packages
+import os
+def get_source_files():
+    for dirname, _, files in os.walk('chunky'):
+        for filename in files:
+            if not filename.endswith('.pyc'):
+                yield os.path.join('/'.join(dirname.split('/')[1:]), filename)
+
+
+setup(
+    name='django-chunky',
+    version='0.1.2',
+    description='Editable content chunks for Django',
+    author='Chris Pitzer, Jeff Schwaber, LoFi Art',
+    author_email='freyley@gmail.com',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    #test_suite='tests.runtests.runtests',
+    package_data = { 'chunky' : list(get_source_files()) }
+        
+
+)
