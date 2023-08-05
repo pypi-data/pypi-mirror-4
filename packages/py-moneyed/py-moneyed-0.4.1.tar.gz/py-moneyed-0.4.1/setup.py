b3e0ad5806051ac481e220344d11b16995c01c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package py-moneyed/0.4.1/py-moneyed-0.4.1.tar.gz

---
+++ packages/py-moneyed/py-moneyed-0.4.1.tar.gz/py-moneyed-0.4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-moneyed',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    version='0.4.1', 
+    description='Provides Currency and Money classes for use in your Python code.',
+    author='Kai',
+    author_email='k@limist.com',
+    url='http://github.com/limist/py-moneyed',
+    download_url='',
+    keywords="money currency class abstraction",
+    license='BSD',
+    install_requires=[
+        'setuptools',
+        'pytest >= 2'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description=open('README.rst', 'r').read()
+    )
