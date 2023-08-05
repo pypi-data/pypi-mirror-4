f7a1cc69210bddcc6aead04e24e32c6dca444de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package pkgbuilder/2.1.5.4/pkgbuilder-2.1.5.4.tar.gz

---
+++ packages/pkgbuilder/pkgbuilder-2.1.5.4.tar.gz/pkgbuilder-2.1.5.4/docs/pbds.rst
@@ -0,0 +1,127 @@
+========================
+pbds module (PBDS class)
+========================
+
+:Author: Kwpolska
+:Copyright: © 2011-2012, Kwpolska.
+:License: BSD (see /LICENSE or :doc:`Appendix B <LICENSE>`.)
+:Date: 2012-10-16
+:Version: 2.1.5.4
+
+.. module: pbds
+
+PBDS
+====
+
+.. index:: PBDS
+.. versionadded:: 2.1.0.0
+.. class:: PBDS
+
+This is the class used for storing data.  Currently, it stores the following
+information (not including information humans should not touch and care about):
+
++-----------+-----------------------------------------------+---------------+
+| variable  | contents/usage                                | default       |
++===========+===============================================+===============+
+| colors    | colors currently used in the script           | [colors]_     |
++-----------+-----------------------------------------------+---------------+
+| pacman    | using wrapper-friendly behavior? [pacman]_    | False         |
++-----------+-----------------------------------------------+---------------+
+| validate  | validating package installation?              | True          |
++-----------+-----------------------------------------------+---------------+
+| depcheck  | checking if deps are installed?               | True          |
++-----------+-----------------------------------------------+---------------+
+| pkginst   | if makepkg should install packages            | True          |
++-----------+-----------------------------------------------+---------------+
+| protocol  | protocol used to connect to the AUR           | http          |
++-----------+-----------------------------------------------+---------------+
+| categories| AUR categories list                           | [categories]_ |
++-----------+-----------------------------------------------+---------------+
+| inttext   | text shown while interrupting (^C)            | [inttext]_    |
++-----------+-----------------------------------------------+---------------+
+| confhome  | configuration home                            | [conf]_       |
++-----------+-----------------------------------------------+---------------+
+| kwdir     | directory used by all projects by yours truly | [conf]_       |
++-----------+-----------------------------------------------+---------------+
+| confdir   | configuration directory                       | [conf]_       |
++-----------+-----------------------------------------------+---------------+
+| log       | logger object (e.g. PBDS.log.info)            | logger object |
++-----------+-----------------------------------------------+---------------+
+| hassudo   | If ``sudo`` is present (see :meth:`sudo`)     | (bool)        |
++-----------+-----------------------------------------------+---------------+
+| uid       | User UID                                      | (int)         |
++-----------+-----------------------------------------------+---------------+
+
+.. [colors] Code below.
+
+::
+
+
+    colors = {
+        'all_off':    '\x1b[1;0m',
+        'bold':       '\x1b[1;1m',
+        'blue':       '\x1b[1;1m\x1b[1;34m',
+        'green':      '\x1b[1;1m\x1b[1;32m',
+        'red':        '\x1b[1;1m\x1b[1;31m',
+        'yellow':     '\x1b[1;1m\x1b[1;33m'
+    }
+
+.. [pacman] *wrapper-friendly behavior* (-S): building in /tmp;
+    :meth:`Utils.print_package` says aur/name
+
+.. [categories] The categories come from `aurweb <https://aur.archlinux.org>`_,
+    and are as follows:
+
+::
+
+    categories = ['ERROR', 'none', 'daemons', 'devel', 'editors',
+                  'emulators', 'games', 'gnome', 'i18n', 'kde',
+                  'lib', 'modules', 'multimedia', 'network',
+                  'office', 'science', 'system', 'x11',
+                  'xfce', 'kernels']
+
+.. [inttext] Used by /scripts/pkgbuilder, internationalized, looks like this:
+    ``Aborted by user! Quitting...``
+
+.. [conf] In order: ``~/.config/``, ``~/.config/kwpolska``,
+    ``~/.config/kwpolska/pkgbuilder`` (may differ depending on system config)
+
+It also has a few methods:
+
+.. method:: sudo(\*args)
+
+.. versionadded:: 2.1.4.8
+
+Run as root.  ``sudo`` if present, ``su -c`` otherwise, nothing if
+already running as root.
+
+.. note:: Accepts only one command.  `shell=False`, for safety.
+
+``*args`` is catching all the arguments.  However, in order to make sure that
+nothing breaks, it checks if the element is a list or a tuple.  If yes, it is
+appended to the argument list (Python’s ``+`` operator); if not, it is split on
+spaces (``.split(' ')``) and appended to the argument list.  Finally, the list
+is passed to ``subprocess.call``.
+
+.. method:: debugmode(nochange)
+
+.. versionadded:: 2.1.4.0
+
+Print all the logged messages to stderr.
+
+.. method:: colorson()
+
+Turns colors on.
+
+.. method:: colorsoff()
+
+Turns colors off.
+
+.. method:: fancy_msg(msg)
+.. method:: fancy_msg2(msg)
+.. method:: fancy_warning(msg)
+.. method:: fancy_warning2(msg)
+.. method:: fancy_error(msg)
+.. method:: fancy_error2(msg)
+
+``makepkg``’s message functions with PKGBUILDer’s own additions.  Use for displaying messages.
---
+++ packages/pkgbuilder/pkgbuilder-2.1.5.4.tar.gz/pkgbuilder-2.1.5.4/pkgbuilder/utils.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+# PKGBUILDer v2.1.5.4
+# An AUR helper (and library) in Python 3.
+# Copyright © 2011-2012, Kwpolska.
+# See /LICENSE for licensing information.
+
+# Names convention: pkg = a package object, pkgname = a package name.
+
+"""
+    pkgbuilder.Utils
+    ~~~~~~~~~~~~~~~~
+    Common global utilities.  Provides useful data.
+
+    :Copyright: © 2011-2012, Kwpolska.
+    :License: BSD (see /LICENSE).
+"""
+
+from . import DS, _, PBError
+from .aur import AUR
+import pyalpm
+import pycman
+import os
+import subprocess
+import textwrap
+import datetime
+
+
+### Utils           common global utilities ###
+class Utils:
+    """Common global utilities.  Provides useful data."""
+
+    aur = AUR()
+
+    def info(self, pkgnames):
+        """
+        .. versionchanged:: 2.1.4.8
+
+        Returns info about packages.
+        """
+        aur_pkgs = self.aur.multiinfo(pkgnames, DS.protocol)
+        if aur_pkgs == []:
+            return []
+        elif aur_pkgs['results'] == 'No results found':
+            return []
+        elif aur_pkgs['type'] == 'error':
+            # There are other cases where the "results" element is a string;
+            # type = error seems to cover at least one case
+            raise PBError(aur_pkgs['results'])
+        else:
+            return aur_pkgs['results']
+
+    def search(self, pkgname):
+        """Searches for AUR packages."""
+        aur_pkgs = self.aur.request('search', pkgname, DS.protocol)
+        if aur_pkgs == []:
+            return []
+        elif aur_pkgs['results'] == 'No results found':
+            return []
+        else:
+            return aur_pkgs['results']
+
+    def print_package_search(self, pkg, use_categories=True,
+                             cachemode=False, prefix='', prefixp=''):
+        """
+        Outputs/returns a package representation, which is close to the output
+        of ``pacman -Ss``.
+        """
+        size = subprocess.Popen(['stty', 'size'], stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        try:
+            termwidth = int(size.stdout.read().split()[1])
+        except IndexError:
+            termwidth = 9001  # Auto-wrap by terminal.  A reference to an old
+                              # meme and a cheat, too. Sorry.
+        size.stdout.close()
+        H = pycman.config.init_with_config('/etc/pacman.conf')
+        localdb = H.get_localdb()
+        lpkg = localdb.get_pkg(pkg['Name'])
+        category = ''
+        installed = ''
+        prefix2 = prefix + '    '
+        prefixp2 = prefixp + '    '
+        if lpkg is not None:
+            if pyalpm.vercmp(pkg['Version'], lpkg.version) != 0:
+                installed = _(' [installed: {}]').format(lpkg.version)
+            else:
+                installed = _(' [installed]')
+        if pkg['OutOfDate'] == '1':
+            installed = (installed + ' ' + DS.colors['red'] + _(
+                         '[out of date]') + DS.colors['all_off'])
+        if use_categories:
+            category = DS.categories[int(pkg['CategoryID'])]
+        else:
+            category = 'aur'
+
+        descl = textwrap.wrap(pkg['Description'], termwidth - len(prefixp2))
+
+        desc = []
+        for i in descl:
+            desc.append(prefix2 + i)
+        desc = '\n'.join(desc)
+        base = (prefix + '{0}/{1} {2} ({4} ' + _('votes') +
+                '){5}\n' + '{3}')
+        entry = (base.format(category, pkg['Name'], pkg['Version'],
+                             desc, pkg['NumVotes'], installed))
+
+        if cachemode:
+            return entry
+        else:
+            print(entry)
+
+    def print_package_info(self, pkgs, cachemode=False, force_utc=False):
+        """
+        .. versionchanged:: 2.1.4.8
+
+        Outputs/returns a package representation, which is close to the output
+        of ``pacman -Si``.
+        """
+        if pkgs == []:
+            raise PBError(_('Package not found.'))
+        else:
+            loct = os.getenv('LC_TIME')
+            loc = os.getenv('LC_ALL')
+
+            if loc == '':
+                loc = os.getenv('LANG')
+
+            if loc == '':
+                loc = 'en_US.UTF-8'
+
+            if loct == '':
+                loct = loc
+
+            fmt = '%Y-%m-%dT%H:%M:%S%Z'
+
+            class UTC(datetime.tzinfo):
+                """Universal Time, Coordinated."""
+
+                def utcoffset(self, dt):
+                    return datetime.timedelta(0)
+
+                def tzname(self, dt):
+                    return "Z"
+
+                def dst(self, dt):
+                    return datetime.timedelta(0)
+
+            # TRANSLATORS: space it properly.  `yes/no' below are
+            # for `out of date'.
+            t = _("""Repository     : aur
+Category       : {cat}
+Name           : {nme}
+Version        : {ver}
+URL            : {url}
+Licenses       : {lic}
+Votes          : {cmv}
+Out of Date    : {ood}
+Maintainer     : {mnt}
+First Submitted: {fsb}
+Last Updated   : {upd}
+Description    : {dsc}
+""")
+
+            to = []
+            for pkg in pkgs:
+                if force_utc:
+                    upd = datetime.datetime.fromtimestamp(
+                        float(pkg['LastModified']),
+                        tz=UTC()).strftime(fmt)
+                    fsb = datetime.datetime.fromtimestamp(
+                        float(pkg['FirstSubmitted']),
+                        tz=UTC()).strftime(fmt)
+                else:
+                    upd = datetime.datetime.fromtimestamp(
+                        float(pkg['LastModified'])).strftime(fmt)
+                    fsb = datetime.datetime.fromtimestamp(
+                        float(pkg['FirstSubmitted'])).strftime(fmt)
+
+                if pkg['OutOfDate'] == '1':
+                    ood = DS.colors['red'] + _('yes') + DS.colors['all_off']
+                else:
+                    ood = _('no')
+
+                to.append(t.format(cat=DS.categories[int(pkg['CategoryID'])],
+                                   nme=pkg['Name'],
+                                   url=pkg['URL'],
+                                   ver=pkg['Version'],
+                                   lic=pkg['License'],
+                                   cmv=pkg['NumVotes'],
+                                   ood=ood,
+                                   mnt=pkg['Maintainer'],
+                                   upd=upd,
+                                   fsb=fsb,
+                                   dsc=pkg['Description']))
+
+        if cachemode:
+            return '\n'.join(to)
+        else:
+            print('\n'.join(to))
