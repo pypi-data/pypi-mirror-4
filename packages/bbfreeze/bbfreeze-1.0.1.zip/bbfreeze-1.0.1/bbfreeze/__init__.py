6733ac6751dc298bb857c65cec9bc04271aab5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:11 +0000

    Add package bbfreeze/1.0.1/bbfreeze-1.0.1.zip

---
+++ packages/bbfreeze/bbfreeze-1.0.1.zip/bbfreeze-1.0.1/bbfreeze/__init__.py
@@ -0,0 +1,24 @@
+__version_info__ = (1, 0, 1)
+version = __version__ = "1.0.1"
+
+import sys
+from bbfreeze import modulegraph
+sys.modules['modulegraph'] = modulegraph
+
+from bbfreeze.freezer import Freezer
+
+
+def main():
+    scripts = sys.argv[1:]
+    if not scripts:
+        print "Version: %s (Python %s)" % (version, ".".join([str(x) for x in sys.version_info]))
+        print "Usage: bb-freeze SCRIPT1 [SCRIPT2...]"
+        print "   creates standalone executables from python scripts SCRIPT1,..."
+        print
+
+        sys.exit(0)
+
+    f = Freezer()
+    for x in scripts:
+        f.addScript(x)
+    f()
