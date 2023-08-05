3401e722b54e521d04c266cfb6b5917df8b46bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package whatidid/1.1/whatidid-1.1.tar.gz

---
+++ packages/whatidid/whatidid-1.1.tar.gz/whatidid-1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='whatidid',
+      version='1.1',
+      description='Keep track of your life',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+      ],
+      keywords='lifelogging whatidid',
+      url='https://github.com/zachseifts/whatidid',
+      author='Zach Seifts',
+      author_email='zach.seifts@gmail.com',
+      license='MIT',
+      packages=['whatidid'],
+      zip_safe=False,
+      scripts=['bin/wid', 'bin/wid-update-mail'])
+
