9ac0962cd22c4971b13b34de1cbf3d8c1db5e9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package whack/0.2.2/whack-0.2.2.tar.gz

---
+++ packages/whack/whack-0.2.2.tar.gz/whack-0.2.2/whack/builder.py
@@ -0,0 +1,60 @@
+import os
+import contextlib
+import tempfile
+import shutil
+
+import blah
+
+from whack.installer import PackageInstaller
+from whack import downloads
+
+class Builders(object):
+    def __init__(self, cacher, builder_repo_uris):
+        self._cacher = cacher
+        self._builder_repo_urls = builder_repo_uris
+
+    def install(self, package_name, install_dir, params=None):
+        with self._fetch_package(package_name) as package_dir:
+            builder = PackageInstaller(package_dir, self._cacher)
+            return builder.install(install_dir, params=params)
+    
+    def _fetch_package(self, package):
+        if blah.is_source_control_uri(package):
+            package_dir = self._fetch_package_from_source_control(package)
+        else:
+            package_dir = self._fetch_package_from_repo(package)
+        
+        if package_dir is None:
+            raise RuntimeError("No builders found for package: {0}".format(package))
+        else:
+            return package_dir
+    
+    @contextlib.contextmanager
+    def _fetch_package_from_source_control(self, package):
+        with _create_temporary_dir() as temporary_dir:
+            archive_dir = os.path.join(temporary_dir, "archive")
+            blah.fetch(package, archive_dir, archive=True)
+            yield archive_dir
+    
+    @contextlib.contextmanager
+    def _fetch_package_from_repo(self, package):
+        for uri in self._builder_repo_urls:
+            if self._is_local_uri(uri):
+                repo_dir = uri
+            else:
+                repo_dir = downloads.fetch_source_control_uri(uri)
+            # FIXME: race condition between this and when we acquire the lock
+            package_dir = os.path.join(repo_dir, package)
+            if os.path.exists(package_dir):
+                yield package_dir
+            
+    def _is_local_uri(self, uri):
+        return "://" not in uri
+
+@contextlib.contextmanager
+def _create_temporary_dir():
+    try:
+        build_dir = tempfile.mkdtemp()
+        yield build_dir
+    finally:
+        shutil.rmtree(build_dir)
---
+++ packages/whack/whack-0.2.2.tar.gz/whack-0.2.2/whack/installer.py
@@ -0,0 +1,85 @@
+import os
+import subprocess
+import shutil
+import json
+
+from whack import downloads
+from whack.hashes import Hasher
+from whack.tempdir import create_temporary_dir
+
+__all__ = ["PackageInstaller"]
+
+class PackageInstaller(object):
+    def __init__(self, package_dir, cacher):
+        self._package_dir = package_dir
+        self._cacher = cacher
+    
+    def install(self, install_dir, params={}):
+        install_id = self._generate_install_id(params)
+        
+        with create_temporary_dir() as temp_dir:
+            build_dir = os.path.join(temp_dir, "build")
+            result = self._cacher.fetch(install_id, build_dir)
+            if not result.cache_hit:
+                self._build(build_dir, params)
+                self._cacher.put(install_id, build_dir)
+                
+            self._run_install_script(build_dir, install_dir)
+
+    def _build(self, build_dir, params):
+        ignore = shutil.ignore_patterns(".svn", ".hg", ".hgignore", ".git", ".gitignore")
+        shutil.copytree(self._package_dir, build_dir, ignore=ignore)
+        build_env = params_to_build_env(params)
+        
+        self._fetch_downloads(build_dir, build_env)
+        subprocess.check_call(
+            [os.path.join(self._package_dir, "whack/build")],
+            cwd=build_dir,
+            env=build_env
+        )
+
+    def _fetch_downloads(self, build_dir, build_env):
+        downloads_file_path = os.path.join(build_dir, "whack/downloads")
+        download_urls = self._read_downloads_file(downloads_file_path, build_env)
+        for url in download_urls:
+            downloads.download_to_dir(url, build_dir)
+
+    def _run_install_script(self, build_dir, install_dir):
+        subprocess.check_call(
+            [os.path.join(build_dir, "whack/install"), install_dir],
+            cwd=build_dir
+        )
+
+    def _generate_install_id(self, params):
+        hasher = Hasher()
+        hasher.update(self._uname("--kernel-name"))
+        hasher.update(self._uname("--machine"))
+        hasher.update_with_dir(self._package_dir)
+        hasher.update(json.dumps(params, sort_keys=True))
+        return hasher.hexdigest()
+
+    def _uname(self, arg):
+        return subprocess.check_output(["uname", arg])
+
+    def _read_downloads_file(self, path, build_env):
+        if os.path.exists(path):
+            first_line = open(path).readline()
+            if first_line.startswith("#!"):
+                downloads_output = subprocess.check_output(
+                    [path],
+                    env=build_env
+                )
+                lines = downloads_output.split("\n")
+            else:
+                lines = open(path)
+                
+            return [line.strip() for line in lines if line]
+        else:
+            return []
+
+def params_to_build_env(params):
+    build_env = os.environ.copy()
+    for name, value in (params or {}).iteritems():
+        build_env[name.upper()] = str(value)
+    return build_env
+
