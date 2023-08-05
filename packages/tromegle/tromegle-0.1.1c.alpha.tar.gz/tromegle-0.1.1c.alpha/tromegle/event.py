3842bbe32322b5c8a88177a01e20079c3f48f11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package tromegle/0.1.1c.alpha/tromegle-0.1.1c.alpha.tar.gz

---
+++ packages/tromegle/tromegle-0.1.1c.alpha.tar.gz/tromegle-0.1.1c.alpha/tromegle/event.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+from collections import namedtuple, deque
+
+# Omegle events
+OmegleEvent = namedtuple('OmegleEvent', ['id', 'type', 'data'])
+ID_SET = "idSet"
+WAITING = "waiting"
+CONNECTED = "connected"
+TYPING = "typing"
+STOPPED_TYPING = "stoppedTyping"
+GOT_MESSAGE = "gotMessage"
+DISCONNECTED = "strangerDisconnected"
+ERROR = "error"
+NULL_EVENT = OmegleEvent(None, None, None)
+
+# Reactor events
+ReactorEvent = namedtuple('ReactorEvent', ['type', 'data'])
+IDLE_TIMEOUT = 'timeout'
+MESSAGE_MODIFIED = 'messageModified'
+
+
+def isEvent(obj):
+    """Return True if object is a Tromegle event of any variety.
+    """
+    return hasattr(obj, '_fields')
+
+
+def mkIterableSequence(obj):
+    """Place obj in a tuple if obj is not a sequence container.
+
+    Useful for handing cases where a single event can be passed to
+        a function that habitually handles sequences of events.
+    """
+    if not hasattr(obj, '__iter__'):
+        obj = (obj,)
+    return obj
+
+
+def spell(fn):
+    """Spell decorator
+    """
+    if fn is not None:
+        return fn
+    else:
+        def throwNone(out, ev):
+            return None
+        return throwNone
+
+
+class Transmogrifier(object):
+    def __init__(self, spells=()):
+        self._spells = ()
+
+        self._evQueue = None
+        self.purge(spells)
+
+    def __call__(self, events):
+        """Cast all spells for each event in an iterable of events.
+        """
+        for ev in events:
+            for spell in self._spells:
+                ev = spell(self, ev)
+            if ev:  # Functions may return None in order to "blackhole" an event
+                self.output(ev)
+
+    def connect(self, eventQueue):
+        """Connect Transmogrifier to an eventQueue.
+        """
+        if not isinstance(eventQueue, deque):
+            raise TypeError('Event queue must be a deque.')
+        self._evQueue = eventQueue
+
+    def push(self, spell):
+        self._spells.append(spell)
+
+    def purge(self, spells=()):
+        """Remove all spells from the Transmogrifier and optionally assigns
+        new ones.
+
+        spells : tuple or list
+            Spells to add (in FIFO order).
+        """
+        self._spells = list(mkIterableSequence(spells))
+
+    def modifyMessage(self, ev, new_msg):
+        """Take in a gotMessage event and modify its message contents.
+
+        ev : OmegleEvent
+            Must be a gotMessage event.
+
+        new_msg : str
+            String with which to replace the original message.
+
+        return : ReactorEvent
+            ReactorEvent containing old and new message.
+        """
+        assert ev.type == GOT_MESSAGE, "Event is not a gotMessage event."
+        return ReactorEvent(MESSAGE_MODIFIED, (ev, new_msg))
+
+    def output(self, ev):
+        """Output event to the registered event queue.
+        """
+        assert self._evQueue is not None, 'Transmogrifier is not connected.'
+        self._evQueue.append(ev)
+
+    def levenshtein_dist(self, s1, s2):
+        """
+        Use:
+        ====
+        levenshtein_dist offers an intuitive metric for the "closeness"
+        of two strings.  It is useful for catching permutation of target strings
+        that differ in such aspects as capitalization and misspellings.
+
+        Note:  for single words, a distance of 2 or 3 will catch most permutations.
+
+
+        Details:
+        ========
+        Calculate the Levenshtein edit-distance between two strings.
+        The edit distance is the number of characters that need to be
+        substituted, inserted, or deleted, to transform s1 into s2.  For
+        example, transforming "rain" to "shine" requires three steps,
+        consisting of two substitutions and one insertion:
+        "rain" -> "sain" -> "shin" -> "shine".  These operations could have
+        been done in other orders, but at least three steps are needed.
+
+        :param s1, s2: The strings to be analysed
+        :type s1: str
+        :type s2: str
+        :rtype int
+        """
+        # set up a 2-D array
+        len1 = len(s1)
+        len2 = len(s2)
+        lev = self._edit_dist_init(len1 + 1, len2 + 1)
+
+        # iterate over the array
+        for i in range(len1):
+            for j in range(len2):
+                self._edit_dist_step(lev, i + 1, j + 1, s1[i], s2[j])
+
+        return lev[len1][len2]
+
+    def _edit_dist_init(self, len1, len2):
+        """from NLTK 2.0
+        """
+        lev = []
+        for i in range(len1):
+            lev.append([0] * len2)  # initialize 2-D array to zero
+        for i in range(len1):
+            lev[i][0] = i  # column 0: 0,1,2,3,4,...
+        for j in range(len2):
+            lev[0][j] = j  # row 0: 0,1,2,3,4,...
+        return lev
+
+    def _edit_dist_step(self, lev, i, j, c1, c2):
+        """from NLTK 2.0
+        """
+        a = lev[i - 1][j] + 1  # skipping s1[i]
+        b = lev[i - 1][j - 1] + (c1 != c2)  # matching s1[i] with s2[j]
+        c = lev[i][j - 1] + 1  # skipping s2[j]
+        lev[i][j] = min(a, b, c)  # pick the cheapest
---
+++ packages/tromegle/tromegle-0.1.1c.alpha.tar.gz/tromegle-0.1.1c.alpha/tromegle/omegle.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+from urllib import urlencode
+import json
+from random import choice
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+
+from twisted.internet.defer import Deferred
+from twisted.internet.protocol import Protocol
+from twisted.web.client import Agent, FileBodyProducer
+from twisted.web.http_headers import Headers
+
+from event import OmegleEvent, ID_SET
+
+
+class HTTP(Protocol):
+    def __init__(self, response):
+        self.response = response
+        self.data = ''
+
+    def dataReceived(self, bytes):
+        self.data += bytes
+
+    def connectionLost(self, reason):
+        self.response.callback(self.data)
+
+
+class Stranger(object):
+    """Class to encapsulate I/O to an Omegle user.
+    """
+    _RESPONSE_OK = 200
+    _ACTIONS = ('start', 'events', 'send', 'typing', 'disconnect')
+    _api = dict(((a, 'http://omegle.com/' + a) for a in _ACTIONS))
+    uagents = ["Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1",
+              "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)",
+              "Mozilla/5.0 (Windows; U; Windows NT 6.1; es-AR; rv:1.9) Gecko/2008051206 Firefox/3.0"]
+
+    def __init__(self, reactor, troll, protocol):
+        """reactor : twisted reactor instance
+        protocol : class object
+            Class of protocol being used
+        """
+        self.typing = False
+        self.connected = False
+        self.id = None
+
+        self.reactor = reactor
+        self.troll = troll  # exposes troll.notify
+        self.protocol = protocol
+        self.agent = choice(self.uagents)
+
+        self._getStrangerID()
+
+    def request(self, api_call, data):
+        agent = Agent(self.reactor)
+        header = {'User-Agent': [self.agent],
+                  'content-type': ['application/x-www-form-urlencoded']}
+        data = urlencode(data)
+        return agent.request(
+                'POST', self._api[api_call], Headers(header),
+                FileBodyProducer(StringIO(data)))
+
+    def getBody(self, response):
+        body = Deferred()
+        response.deliverBody(self.protocol(body))
+        return body
+
+    def _getStrangerID(self):
+        d = self.request('start', '')
+        # lvl 1
+        d.addCallback(self.checkForOkStatus)
+        # lvl 2
+        d.addCallback(self.getBody)
+        # lvl 3
+        d.addCallback(self._assignID)
+
+    def checkForOkStatus(self, response):
+        assert response.code == self._RESPONSE_OK, "Bad response to HTTP request."
+        return response
+
+    def _assignID(self, body):
+        """Download the body on successful header fetch
+        from _getStrangerID
+        """
+        self.id = body.replace('"', '')
+        ev = OmegleEvent(self.id, ID_SET, '')
+        self.troll.feed(ev)  # ready to go!
+
+    def parse_raw_events(self, events):
+        """Produce OmegleEvents from a list of raw events.
+        events : string
+            String of raw events from a POST request to
+            an omegle subpage.
+
+        return : generator or NoneType
+            Return generator with events or None if there are no events.
+        """
+        events = json.loads(events) or None
+        if events:
+            events = (OmegleEvent(self.id,
+                            ev[0],
+                            None if len(ev) == 1 else ev[1])
+                      for ev in events)
+
+        return events
+
+    def getEventsPage(self):
+        d = self.request('events', {'id': self.id})
+        d.addCallback(self.getBody)
+        d.addCallback(self.parse_raw_events)
+        d.addCallback(self.troll.feed)
+
+    def toggle_typing(self):
+        def flip(resp):
+            self.typing = not self.typing
+
+        d = self.request('typing', {'id': self.id})
+        d.addCallback(self.checkForOkStatus)
+        d.addCallback(flip)
+
+    def announceDisconnect(self):
+        self.request('disconnect', {'id': self.id})
+
+    def sendMessage(self, msg):
+        self.request('send', {'msg': msg, 'id': self.id})
