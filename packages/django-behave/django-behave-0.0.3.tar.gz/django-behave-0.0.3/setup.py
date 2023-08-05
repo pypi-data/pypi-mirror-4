ca20f9dad38cf91319ea36828159fe59968e9f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:12 +0000

    Add package django-behave/0.0.3/django-behave-0.0.3.tar.gz

---
+++ packages/django-behave/django-behave-0.0.3.tar.gz/django-behave-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+	name = 'django-behave',
+	packages = ['django-behave'],
+	version = '0.0.3',
+	description = 'Django Test Runner for the Behave BDD module',
+	author = 'Rachel Willmer',
+	author_email = 'rachel@willmer.org',
+        url = 'https://github.com/rwillmer/django-behave',
+	classifiers = [
+                'Programming Language :: Python :: 2.7',
+		'Framework :: Django',
+                'Development Status :: 3 - Alpha',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: Apache Software License',
+                'Topic :: Software Development :: Testing',
+	],
+)
