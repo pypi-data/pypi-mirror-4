0fdf588cccc7c027466eadada2401e4b905c31f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:33 +0000

    Add package infi.projector/0.5.4/infi.projector-0.5.4-py2.7.egg

---
+++ packages/infi.projector/infi.projector-0.5.4-py2.7.egg/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ab9145fc45c1b0a067c7bfaf964ca3a965760df6'
+__git_head_subject__ = 'Finished release v0.5.4.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
