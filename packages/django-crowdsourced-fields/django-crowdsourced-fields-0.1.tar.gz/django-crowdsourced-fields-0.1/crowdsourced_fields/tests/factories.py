e84d52d12d1dae2f776173dc88897e73715be89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:41 +0000

    Add package django-crowdsourced-fields/0.1/django-crowdsourced-fields-0.1.tar.gz

---
+++ packages/django-crowdsourced-fields/django-crowdsourced-fields-0.1.tar.gz/django-crowdsourced-fields-0.1/crowdsourced_fields/tests/factories.py
@@ -0,0 +1,16 @@
+"""Factories for the models of the ``django-crowdsourced-fields`` app."""
+import factory
+
+from crowdsourced_fields.models import (
+    CrowdsourcedItem,
+    CrowdsourcedItemGenericForeignKey,
+)
+
+
+
+class CrowdsourcedItemFactory(factory.Factory):
+    """Factory for the ``CrowdsourcedItem`` model."""
+    FACTORY_FOR = CrowdsourcedItem
+
+    item_type = 'countries'
+    value = 'Germany'
