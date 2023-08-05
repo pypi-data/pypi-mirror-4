16ae987ad0c420c55a192b2af17c04af8dd820e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:55 +0000

    Add package jmbo-banner/0.1.3/jmbo_banner-0.1.3-py2.7.egg

---
+++ packages/jmbo-banner/jmbo_banner-0.1.3-py2.7.egg/banner/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from jmbo.models import ModelBase
+from preferences.models import Preferences
+
+
+class Banner(ModelBase):
+    pass
+
+
+class CodeBanner(Banner):
+    code = models.TextField(
+        help_text='The full HTML/Javascript code snippet to be \
+embedded for this banner.'
+    )
+
+
+class ImageBanner(Banner):
+    url = models.CharField(
+        max_length='256',
+        verbose_name='URL',
+        help_text='URL (internal or external) to which this banner will link.'
+    )
+
+    def get_absolute_url(self):
+        return self.url
+
+
+class BannerPreferences(Preferences):
+    __module__ = 'preferences.models'
+
+    class Meta():
+        verbose_name = 'Banner preferences'
+        verbose_name_plural = 'Banner preferences'
+
+
+class BannerOption(models.Model):
+    banner_preferences = models.ForeignKey('preferences.BannerPreferences')
+    is_default = models.BooleanField(
+        verbose_name="Default",
+        default=False,
+    )
+    url_name = models.CharField(
+        max_length=256,
+        verbose_name="URL Name",
+        blank=True,
+        null=True,
+    )
+    url = models.CharField(
+        max_length=256,
+        verbose_name="URL (takes precedence)",
+        blank=True,
+        null=True,
+    )
+    banner = models.ForeignKey('banner.Banner')
+    position = models.CharField(max_length=256)
