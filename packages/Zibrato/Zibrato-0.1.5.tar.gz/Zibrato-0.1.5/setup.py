858fd36bb1a773182b5eb3151521abe7044c457f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:23 +0000

    Add package Zibrato/0.1.5/Zibrato-0.1.5.tar.gz

---
+++ packages/Zibrato/Zibrato-0.1.5.tar.gz/Zibrato-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='Zibrato',
+    version='0.1.5',
+    author='Rob Martin @version2beta',
+    author_email='rob@version2beta.com',
+    packages=['zibrato', 'zibrato.tests'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/zibrato/',
+    license='LICENSE.txt',
+    description='Send metrics to Librato via ZeroMQ.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'pyzmq',
+        'fuzzywuzzy',
+        'requests',
+        'simplejson',
+    ],
+)
---
+++ packages/Zibrato/Zibrato-0.1.5.tar.gz/Zibrato-0.1.5/zibrato/__init__.py
@@ -0,0 +1,4 @@
+from zibrato import Zibrato
+from backend import Backend, Broker
+from librato import Librato
+
