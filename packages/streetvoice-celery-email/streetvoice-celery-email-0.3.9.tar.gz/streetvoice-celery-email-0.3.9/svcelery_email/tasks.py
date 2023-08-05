2b1ea5d40f9c7b7ab1e8a4500c38796cb6aa47f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package streetvoice-celery-email/0.3.9/streetvoice-celery-email-0.3.9.tar.gz

---
+++ packages/streetvoice-celery-email/streetvoice-celery-email-0.3.9.tar.gz/streetvoice-celery-email-0.3.9/svcelery_email/tasks.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.core.mail import get_connection
+from svcelery_email.models import Blacklist, MessageLog
+from smtplib import SMTPDataError
+from celery.task import task
+
+BACKEND = getattr(settings, 'CELERY_EMAIL_BACKEND',
+                  'django.core.mail.backends.smtp.EmailBackend')
+
+@task(ignore_result=True)
+def send_email(message, **kwargs):
+    logger = send_email.get_logger()
+    conn = get_connection(backend=BACKEND)
+
+    # check blacklist
+    try:
+        Blacklist.objects.get(email=message.to[0])
+        logger.debug("Email already in blacklist.")
+    except Blacklist.DoesNotExist:
+        pass
+
+    # send
+    try:
+        result = conn.send_messages([message])
+        logger.debug("Successfully sent email message to %r.", message.to)
+        MessageLog.objects.log(message, 1)
+        return result
+    except SMTPDataError, e:
+        logger.warning("Message to %r, blacklisted.", message.to)
+        if e.smtp_code == 554:
+            MessageLog.objects.log(message, 3)
+            Blacklist(email=message.to[0]).save()
+    except Exception, e:
+        MessageLog.objects.log(message, 2)
+        logger.warning("Failed to send email message to %r, retrying.", message.to)
+        send_email.retry(exc=e)
+
+    conn.close()
