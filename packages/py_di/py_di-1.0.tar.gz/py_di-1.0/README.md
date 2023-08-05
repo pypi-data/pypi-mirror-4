2b4ae81b5144c792d8a8daa98c9badbdeceabf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package py_di/1.0/py_di-1.0.tar.gz

---
+++ packages/py_di/py_di-1.0.tar.gz/py_di-1.0/README.md
@@ -0,0 +1,36 @@
+# Py_DI - Python Container of Dependency Injection (IOC) implementation
+
+## Authors
+- [Moises P. Sena](http://moisespsena.com)
+
+## Issues
+
+[Issues on GitHub](https://github.com/moisespsena/py_di/issues).
+
+## Installation
+
+### By pip:
+
+```bash
+sudo pip install py_di
+```
+
+### Manual:
+
+For default python executable:
+
+```bash
+git clone https://github.com/moisespsena/py_di py_di
+cd py_di
+sudo python setup.py install
+```
+
+For another python executable:
+
+```bash
+sudo python3 setup.py install
+```
+
+## Example:
+
+See [tests](https://github.com/moisespsena/py_di/blob/master/tests/p/test_all.py) for more details.
---
+++ packages/py_di/py_di-1.0.tar.gz/py_di-1.0/setup.py
@@ -0,0 +1,74 @@
+from distutils.cmd import Command
+from setuptools import setup
+import os
+import re
+import sys
+import shutil
+
+THIS_PATH = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
+
+def pkg_by_version(name=""):
+    return name + ("" if sys.version_info[0] < 3 else "3")
+
+def read(fname):
+    path = os.path.join(THIS_PATH, fname)
+    if os.path.exists(path):
+        with open(path) as f:
+            return f.read()
+    return None
+          
+class CleanCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        self.paths = []
+        
+        for f in os.listdir(THIS_PATH):
+            if re.search("(^(build|dist|__pycache__)$|\.egg-info)", f):
+                self.paths.append(f)
+            
+        for root, dirs, files in os.walk(THIS_PATH):
+            for f in files:
+                if f.endswith(".pyc") or f.endswith(".pyc") or f.endswith(".pyo"):
+                    self.paths.append(os.path.join(root, f))
+                    
+            for f in dirs:
+                if re.search("^(__pycache__)$", f):
+                    self.paths.append(os.path.join(root, f))
+
+    def finalize_options(self):
+        pass
+    
+    def run(self):
+        for p in self.paths:
+            if os.path.exists(p):
+                if os.path.isdir(p):
+                    print("Remove directory: " + p)
+                    shutil.rmtree(p)
+                else:
+                    print("Remove file     : " + p)
+                    os.unlink(p)
+
+setup(
+    name="py_di",
+    version="1.0",
+    author="Moises P. Sena",
+    author_email="moisespsena@gmail.com",
+    description="Container of Dependency Injection (IOC) implementation.",
+    license="BSD",
+    keywords="utils utilities di dependecy injection ioc",
+    url="https://github.com/moisespsena/py_di",
+    packages=["di"],
+    test_suite='tests.' + pkg_by_version("p") + ".test_all",
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+    cmdclass={'clean': CleanCommand},
+)
