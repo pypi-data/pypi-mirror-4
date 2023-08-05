866f02f0db6097872284d4ba7b12c028f232d7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package py-Asterisk/0.1a1/py-Asterisk-0.1a1.tar.gz

---
+++ packages/py-Asterisk/py-Asterisk-0.1a1.tar.gz/py-Asterisk-0.1a1/Asterisk/Logging.py
@@ -0,0 +1,72 @@
+'''
+Asterisk/Logging.py: extensions to the Python 2.3 logging module.
+'''
+
+__author__ = 'David M. Wilson <dw-py-Asterisk-Util.py@botanicus.net>'
+__Id__ = '$Id: Logging.py 74 2004-09-23 22:52:40Z dw $'
+
+import logging
+
+
+
+
+# Add two new levels.
+
+logging.PACKET = logging.DEBUG  - 1
+logging.IO     = logging.PACKET - 1
+
+logging.addLevelName(logging.PACKET, 'PACKET')
+logging.addLevelName(logging.IO,     'IO')
+
+
+
+
+# Attempt to find the parent logger class using the Python 2.4 API.
+
+if hasattr(logging, 'getLoggerClass'):
+    loggerClass = logging.getLoggerClass()
+else:
+    loggerClass = logging.Logger
+
+
+
+# Provide a new logger class that supports our new levels.
+
+class AsteriskLogger(loggerClass):
+    def packet(self, msg, *args, **kwargs):
+        "Log a message with severity 'PACKET' on this logger."
+        return self.log(logging.PACKET, msg, *args, **kwargs)
+
+    def io(self, msg, *args, **kwargs):
+        "Log a message with severity 'IO' on this logger."
+        return self.log(logging.IO, msg, *args, **kwargs)
+
+
+
+
+# Install the new system-wide logger class.
+
+logging.setLoggerClass(AsteriskLogger)
+
+
+
+# Per-instance logging mix-in.
+
+class InstanceLogger(object):
+    def getLoggerClass(self):
+        '''
+        Return the namespace where debug messages for all instances of this
+        class are sent.
+        '''
+
+        return '%s.%s' % (self.__module__, self.__class__.__name__)
+
+
+    def getLogger(self):
+        '''
+        Return the Logger instance which receives debug messages for this class
+        instance.
+        '''
+
+        log_name = self.getLoggerClass() + '.' + str(id(self))
+        return logging.getLogger(log_name)
---
+++ packages/py-Asterisk/py-Asterisk-0.1a1.tar.gz/py-Asterisk-0.1a1/Asterisk/Util.py
@@ -0,0 +1,198 @@
+'''
+Asterisk/Util.py: utility classes.
+'''
+
+__author__ = 'David M. Wilson <dw-py-Asterisk-Util.py@botanicus.net>'
+__Id__ = '$Id: Util.py 74 2004-09-23 22:52:40Z dw $'
+
+import sys, copy
+import Asterisk
+
+
+
+
+
+class SubscriptionError(Asterisk.BaseException):
+    '''
+    This exception is raised when an attempt to register the same (event,
+    handler) tuple twice is detected.
+    '''
+
+
+
+# This special unique object is used to indicate that an argument has not been
+# specified. It is used where None may be a valid argument value.
+
+class Unspecified(object):
+    'A class to represent an unspecified value that cannot be None.'
+
+    def __repr__(self):
+        return '<Asterisk.Util.Unspecified>'
+
+Unspecified = Unspecified()
+
+
+
+
+class AttributeDict(dict):
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+
+
+class EventCollection(object):
+    '''
+    Utility class to allow grouping and automatic registration of event.
+    '''
+
+    def __init__(self, initial = None):
+        '''
+        If <initial> is not None, register functions from the list <initial>
+        waiting for events with the same name as the function.
+        '''
+
+        self.subscriptions = {}
+
+        if initial is not None:
+            for func in initial:
+                self.subscribe(func.__name__, func)
+
+
+    def subscribe(self, name, handler):
+        '''
+        Subscribe callable <handler> to event named <name>.
+        '''
+
+        if name not in self.subscriptions:
+            subscriptions = self.subscriptions[name] = []
+        else:
+            subscriptions = self.subscriptions[name]
+
+        if handler in subscriptions:
+            raise SubscriptionError
+
+        subscriptions.append(handler)
+
+
+    def unsubscribe(self, name, handler):
+        'Unsubscribe callable <handler> to event named <name>.'
+        self.subscriptions[name].remove(handler)
+
+
+    def fire(self, name, *args, **kwargs):
+        'Fire event <name> passing *<args> and **<kwargs> to subscribers.'
+
+        if name not in self.subscriptions:
+            return
+
+        for subscription in self.subscriptions[name]:
+            subscription(*args, **kwargs)
+
+
+    def copy(self):
+        new = self.__class__()
+
+        for name, subscriptions in self.subscriptions.iteritems():
+            new.subscriptions[name] = []
+            for subscription in subscriptions:
+                new.subscriptions[name].append(subscription)
+
+
+    def __iadd__(self, collection):
+        'Add all the events in <collection> to our collection.'
+
+        if not isinstance(collection, EventCollection):
+            raise TypeError
+
+        new = self.copy()
+
+        try:
+            for name, handlers in collection.subscriptions.iteritems():
+                for handler in handlers:
+                    self.subscribe(name, handler)
+        except Exception, e:
+            self.subscriptions = new.subscriptions
+            raise
+
+        return self
+
+
+    def __isub__(self, collection):
+        'Remove all the events in <collection> from our collection.'
+
+        if not isinstance(collection, EventCollection):
+            raise TypeError
+
+        new = self.copy()
+
+        try:
+            for name, handlers in collection.subscriptions.iteritems():
+                for handler in handlers:
+                    self.unsubscribe(name, handler)
+        except Exception, e:
+            self.subscriptions = new.subscriptions
+            raise
+
+        return self
+    
+
+
+
+def dump_packet(packet, file = sys.stdout):
+    '''
+    Dump a packet in human readable form to file-like object <file>.
+    '''
+
+    packet = dict(packet)
+
+    if 'Event' in packet:
+        file.write('-- %s\n' % packet.pop('Event'))
+    else:
+        file.write('-- Response: %s\n' % packet.pop('Response'))
+
+
+    packet = packet.items()
+    packet.sort()
+
+    for tuple in packet:
+        file.write('   %s: %s\n' % tuple)
+
+    file.write('\n')
+
+
+
+
+
+
+def dump_human(data, file = sys.stdout, _indent = 0):
+    scalars = (str, int, float)
+    recursive = (dict, list, tuple, AttributeDict)
+    indent = lambda a = 0, i = _indent: ('   ' * (a + i))
+    Type = type(data)
+
+
+    if Type in (dict, AttributeDict):
+        items = data.items()
+        items.sort()
+
+        for key, val in items:
+            file.write(indent() + str(key) + ': ')
+            if type(val) in recursive:
+                file.write('\n')
+                dump_human(val, file, _indent + 1)
+            else:
+                dump_human(val, file, 0)
+
+    elif Type in (list, tuple):
+        for val in data:
+            dump_human(val, file, _indent + 1)
+
+    elif Type in (int, float):
+        file.write(indent() + '%r\n' % data)
+
+    elif Type is str:
+        file.write(indent() + data + '\n')
