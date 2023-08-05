2cd31908b2dad5b13110e67d39285c1cc33b5b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package edition1.menu/0.1a3/edition1.menu-0.1a3.zip

---
+++ packages/edition1.menu/edition1.menu-0.1a3.zip/edition1.menu-0.1a3/src/edition1/menu/browser/resources/responsive-menu.js
@@ -0,0 +1,52 @@
+function resetSmallMenu() {
+    // Reset the small menu so all items are 'collapsed'.
+    jq('.small-menu .has-children').removeClass('children-visible')
+                                   .addClass('children-hidden');
+    jq('.big-menu .children-hidden').removeClass('children-hidden');
+    jq('.small-menu .has-children').unbind();
+    jq('.small-menu .has-children').toggle(function(event) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        jq(this).removeClass('children-hidden').addClass('children-visible');
+        jq(this).unbind();
+    }, function () {
+        // Don't do anything, let the browser handle the click on the link.
+        return true;
+    });
+
+    // Open/close the small menu
+    jq('.small-menu .tabtitle').addClass('children-hidden').toggle(function() {
+        jq(this).removeClass('children-hidden').addClass('children-visible');
+    }, function () {
+        jq(this).removeClass('children-visible').addClass('children-hidden');
+        resetSmallMenu();
+    });
+}
+
+function setMenuSize() {
+    // Set a class on the menu which determines if the small or big menu
+    // should be shown.
+    var menu = jq('#responsive-menu');
+    var forced_small_flag = menu.attr('data-force-small');
+    var min_width = menu.attr('data-min-width');
+    if (typeof min_width == 'undefined') {
+        min_width = 0;
+    }
+
+    var newMenuType = 'big-menu';
+    var oldMenuType = 'small-menu';
+    if (forced_small_flag == 'True' || jq(window).width() < min_width) {
+        newMenuType = 'small-menu';
+        oldMenuType = 'big-menu';
+    }
+    menu.addClass(newMenuType).removeClass(oldMenuType);
+    resetSmallMenu();
+}
+
+jq(window).resize(function() {
+    setMenuSize();
+});
+
+jq(document).ready(function() {
+    setMenuSize();
+});
