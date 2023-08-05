c405d5aa0ecc5011439fc282594a980655a70382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package mongo/0.0.2/mongo-0.0.2.tar.gz

---
+++ packages/mongo/mongo-0.0.2.tar.gz/mongo-0.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from setuptools import setup
+import mongo
+
+
+if sys.argv[-1] in ['test', 'publish']:
+    import doctest
+
+    if doctest.testfile('README.md', verbose=True).failed:
+        sys.exit()
+
+    if sys.argv[-1] == 'publish':
+        os.system('python setup.py sdist upload')
+        sys.exit()
+
+setup(
+    name         = 'mongo',
+    version      = mongo.__version__,
+    description  = 'Minimalistic pymongo object wrapper',
+    url          = 'https://github.com/imbolc/mongo',
+
+    packages     = ['mongo'],
+    install_requires = ['pymongo'],
+
+    author       = 'Imbolc',
+    author_email = 'imbolc@imbolc.name',
+    license      = open('LICENSE').read(),
+    long_description = open('README.md').read(),
+
+    keywords     = ['mongodb', 'pymongo', 'orm'],
+    classifiers  = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+    ],
+)
