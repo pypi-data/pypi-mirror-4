dc2cedeb6ed311bc24083457c9f52901b1de5926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:41 +0000

    Add package Pyped/0.3/Pyped-0.3.tar.gz

---
+++ packages/Pyped/Pyped-0.3.tar.gz/Pyped-0.3/setup.py
@@ -0,0 +1,30 @@
+
+import os
+from setuptools import setup, find_packages
+
+
+open('MANIFEST.in', 'w').write('\n'.join((
+    "include *.txt *.md",
+)))
+
+setup(
+    name='Pyped',
+    version='0.3',
+    author='Kevin Samuel',
+    author_email='kevin@yeleman.com',
+    py_modules=['pyped'],
+    license='GPL2',
+    long_description=open('README.md').read(),
+    description='Command that pipes data from bash to Python, and vice-versa',
+    url='http://github.com/ksamuel/Pyped',
+    keywords="python, pipe",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['py = pyped:main'],
+     },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+)
