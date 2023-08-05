9f89a918df86102b55635c0e8fb363a5ea53708e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package wildcard.cleanprint/1.0a2/wildcard.cleanprint-1.0a2.tar.gz

---
+++ packages/wildcard.cleanprint/wildcard.cleanprint-1.0a2.tar.gz/wildcard.cleanprint-1.0a2/wildcard/cleanprint/__init__.py
@@ -0,0 +1,9 @@
+# -*- extra stuff goes here -*-
+
+from zope.i18nmessageid import MessageFactory
+WildcardCleanPrintMessageFactory = \
+    MessageFactory('wildcard.cleanprint')
+
+
+def initialize(context):
+    pass
