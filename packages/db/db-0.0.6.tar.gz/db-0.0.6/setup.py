a849ae51b7f678361e4d3d36bae3b2cd5f1f2d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package db/0.0.6/db-0.0.6.tar.gz

---
+++ packages/db/db-0.0.6.tar.gz/db-0.0.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+if __name__ == "__main__":
+    setup(name="db",
+          version="0.0.6",
+          description="Databases for Humans",
+          author="John Evans",
+          author_email="lgastako@gmail.com",
+          install_requires=["antiorm"],
+          packages=find_packages(),
+          provides=["db"])
