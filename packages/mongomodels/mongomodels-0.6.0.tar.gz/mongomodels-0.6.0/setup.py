517e34afcd4e4cfaccb698493b344f0e891fdecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package mongomodels/0.6.0/mongomodels-0.6.0.tar.gz

---
+++ packages/mongomodels/mongomodels-0.6.0.tar.gz/mongomodels-0.6.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="mongomodels",
+    version="0.6.0",
+    description="A simple ODM for MongoDB",
+    author="David Litvak",
+    author_email = "david.litvakb@gmail.com",
+    license = "GPL v3",
+    keywords = "MongoDB ODM Database",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'pymongo',
+      'event_handler'
+    ],
+    url='http://github.com/dlitvakb',
+)
