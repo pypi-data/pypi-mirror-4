194e2eac5db63bf302c20fc08e6c3206a1ddb7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:07 +0000

    Add package django-socialregistration/0.5.8/django-socialregistration-0.5.8.tar.gz

---
+++ packages/django-socialregistration/django-socialregistration-0.5.8.tar.gz/django-socialregistration-0.5.8/example/app/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from socialregistration.contrib.facebook.models import FacebookProfile
+from socialregistration.contrib.foursquare.models import FoursquareProfile
+from socialregistration.contrib.github.models import GithubProfile
+from socialregistration.contrib.instagram.models import InstagramProfile
+from socialregistration.contrib.linkedin.models import LinkedInProfile
+from socialregistration.contrib.openid.models import OpenIDProfile
+from socialregistration.contrib.tumblr.models import TumblrProfile
+from socialregistration.contrib.twitter.models import TwitterProfile
+
+def index(request):
+    return render_to_response(
+        'index.html', dict(
+            facebook=FacebookProfile.objects.all(),
+            twitter=TwitterProfile.objects.all(),
+            openid=OpenIDProfile.objects.all(),
+            linkedin=LinkedInProfile.objects.all(),
+            github=GithubProfile.objects.all(),
+            foursquare=FoursquareProfile.objects.all(),
+            tumblr=TumblrProfile.objects.all(),
+            instagram=InstagramProfile.objects.all(),
+    ), context_instance=RequestContext(request))
+    
---
+++ packages/django-socialregistration/django-socialregistration-0.5.8.tar.gz/django-socialregistration-0.5.8/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import socialregistration
+
+METADATA = dict(
+    name='django-socialregistration',
+    version=socialregistration.__version__,
+    author='Alen Mujezinovic',
+    author_email='alen@caffeinehit.com',
+    description='Django app providing registration through a variety of APIs',
+    long_description=open('README.rst').read(),
+    url='http://github.com/flashingpumpkin/django-socialregistration',
+    keywords='django facebook twitter oauth openid registration foursquare '\
+        'linkedin github oauth2',
+    install_requires=['oauth2', 'python-openid', 'mock'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+        'Topic :: Internet',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+)
+
+if __name__ == '__main__':
+    setup(**METADATA)
