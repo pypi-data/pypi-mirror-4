63854a3bc530e44074cc014cbcb717adea67aedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:05 +0000

    Add package infi.storagemodel/0.1.39/infi.storagemodel-0.1.39.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.39.tar.gz/infi.storagemodel-0.1.39/src/infi/storagemodel/vendor/infinidat/infinibox/mixin/volume/__init__.py
@@ -0,0 +1,51 @@
+from infi.pyutils.lazy import cached_method
+from ..inquiry import JSONInquiryException
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+class InfiniBoxVolumeMixin(object):
+    @cached_method
+    def _is_volume_mapped(self):
+        """In race condition between a rescan and volume unmap operation, the device may stil exists while there volume
+        is already unampped.
+        :returns: this method returns True if a volume is mapped to the device."""
+        standard_inquiry = self.device.get_scsi_standard_inquiry()
+        # spc4r30 section 6.4.2 tables 140 + 141, peripheral device type 0 is disk, 31 is unknown or no device
+        return standard_inquiry.peripheral_device.type == 0
+
+    @cached_method
+    def get_volume_id(self):
+        """:returns: the volume name within the InfiniBox
+        :rtype: int"""
+        return self.get_naa().get_volume_serial()
+
+    @cached_method
+    def get_volume_name(self):
+        """:returns: A string if the volume name inside the Infinibox, or None if not a volume"""
+        try:
+            return self._get_volume_name_from_json_page()
+        except JSONInquiryException:
+            return self._get_volume_name_from_management()
+
+    @cached_method
+    def get_volume_type(self):
+        """:returns: A string of the volume type, or None if it is not a volume"""
+        return self._get_volume_type_from_management()
+
+    def _get_volume_name_from_json_page(self):
+        return self._get_key_from_json_page('vol')
+
+    def _get_volume_name_from_management(self):
+        volume = self._get_volume_from_management()
+        return None if volume is None else volume['name']
+
+    def _get_volume_type_from_management(self):
+        volume = self._get_volume_from_management()
+        return None if volume is None else volume['type']
+
+    def _get_volume_from_management(self):
+        volume_id = self.get_volume_id()
+        sender = self._get_management_json_sender()
+        return None if volume_id == -1 or volume_id == 0 else sender.get('volumes/{}'.format(volume_id))
+
