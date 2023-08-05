091154b33c692943ad1321cd054a723c9fd17198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package argcomplete/0.2.0/argcomplete-0.2.0.tar.gz

---
+++ packages/argcomplete/argcomplete-0.2.0.tar.gz/argcomplete-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='argcomplete',
+    version='0.2.0',
+    url='https://github.com/kislyuk/argcomplete',
+    license='Apache Software License',
+    author='Andrey Kislyuk',
+    author_email='kislyuk@gmail.com',
+    description='Bash tab completion for argparse',
+    long_description=open('README.rst').read(),
+    packages = find_packages(),
+    scripts = ['scripts/register-python-argcomplete',
+               'scripts/activate-global-python-argcomplete'],
+    data_files=[('argcomplete/resources', ['etc/bash_completion.d/python-argcomplete.sh'])],
+    zip_safe=False,
+    include_package_data=True,
+    platforms=['MacOS X', 'Posix'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Unix Shell',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
