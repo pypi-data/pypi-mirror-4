00e2cfafcf4e48de97ce3a65a7cc7eea76074207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package aliUtil/1.0.0/aliUtil-1.0.0.zip

---
+++ packages/aliUtil/aliUtil-1.0.0.zip/aliUtil-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'aliUtil',
+    version         = '1.0.0',
+    py_moudles      = ['aliUtil'],
+    author          = 'ilehao',
+    author_email    = 'milehao@gmail.com',
+    url             = 'http://www.ilehao.com',
+    description     = 'milehao util'
+    )
