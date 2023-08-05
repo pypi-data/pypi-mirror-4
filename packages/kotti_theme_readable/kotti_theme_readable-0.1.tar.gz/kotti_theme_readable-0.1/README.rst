c710ef592310257a9795b4797fe1542580f55cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package kotti_theme_readable/0.1/kotti_theme_readable-0.1.tar.gz

---
+++ packages/kotti_theme_readable/kotti_theme_readable-0.1.tar.gz/kotti_theme_readable-0.1/README.rst
@@ -0,0 +1,24 @@
+====================
+kotti_theme_readable
+====================
+
+Readable theme from http://bootswatch.com/readable/ for Kotti.
+
+`Find out more about Kotti`_
+
+Setup
+=====
+
+To activate the ``kotti_theme_readable`` theme in your Kotti site, you need to
+add an entry to the ``kotti.configurators`` setting in your Paste
+Deploy config.  If you don't have a ``kotti.configurators`` option,
+add one.  The line in your ``[app:kotti]`` section could then look like
+this::
+
+  kotti.configurators = kotti_theme_readable.kotti_configure
+
+With this, you'll be able to use kotti_theme_readable in your Kotti site.
+
+
+
+.. _Find out more about Kotti: http://pypi.python.org/pypi/Kotti
---
+++ packages/kotti_theme_readable/kotti_theme_readable-0.1.tar.gz/kotti_theme_readable-0.1/setup.py
@@ -0,0 +1,51 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.1'
+project = 'kotti_theme_readable'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(
+    name=project,
+    version=version,
+    description="Readable theme from http://bootswatch.com/readable/ for Kotti",
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        'License :: Repoze Public License',
+    ],
+    keywords='kotti theme',
+    author='Jeff Pittman',
+    author_email='geojeff@me.com',
+    url='https://github.com/geojeff/kotti_theme_readable',
+    license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Kotti',
+    ],
+    entry_points={
+        'fanstatic.libraries': [
+            'kotti_theme_readable = kotti_theme_readable.static:library',
+        ],
+    },
+    extras_require={
+        'development': [
+            'minify',
+        ]
+    },
+    message_extractors={
+        'kotti_theme_readable': [
+            ('**.py', 'lingua_python', None),
+            ('**.zcml', 'lingua_xml', None),
+            ('**.pt', 'lingua_xml', None),
+        ]
+    },
+)
