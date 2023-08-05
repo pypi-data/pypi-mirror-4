2ed6aa1bc2bda2fe144750cc4b682cfa90eadf4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package infi.sysv_service/0.1.5/infi.sysv_service-0.1.5.tar.gz

---
+++ packages/infi.sysv_service/infi.sysv_service-0.1.5.tar.gz/infi.sysv_service-0.1.5/src/infi/sysv_service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b5f12af288e25849846c591200766c1aa31cfcd2'
+__git_head_subject__ = 'Finished release v0.1.5.'
+__git_head_message__ = '# By Arnon Yaari (1) and Guy Rozendorn (1)\n* release/v0.1.5:\n  HIP-372 increasing sleep from 1 to 3\n  empty commit after version v0.1.4'
+__git_dirty_diff__ = ''
