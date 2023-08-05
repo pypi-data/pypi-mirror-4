e15a7c4d544abb73f6275bff782c68a160bb4b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package pyhammer/0.2.26/pyhammer-0.2.26.zip

---
+++ packages/pyhammer/pyhammer-0.2.26.zip/pyhammer-0.2.26/pyhammer/tasks/io/copytask.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+from pyhammer.filters.filefilter import FileFilter
+from pyhammer.tasks.taskbase import TaskBase
+
+class CopyTask(TaskBase):
+    def __init__(self, srcDir, destDir, fileFilter=None):
+        super().__init__()
+        self.destDir = destDir
+        self.srcDir = srcDir
+
+        if fileFilter is None:
+            self.fileFilter = FileFilter()
+        else:
+            self.fileFilter = fileFilter
+
+    def do( self ):
+        self.reporter.message( "src: '%s', dest: '%s'" % (self.srcDir, self.destDir) )
+        return self.__copyFileList(self.fileFilter, self.srcDir, self.destDir, self.reporter)
+
+    def __copyFileList(self, fileFilter, srcDir, destDir, reporter):
+        files = fileFilter.Filter( srcDir )
+        reporter.message( "Copying files: %s => %s" % ( srcDir, destDir ) )
+
+        for fp in files:
+            relPath = fp.lower().replace( os.path.realpath( srcDir ).lower(), "" )
+            reporter.message("os.path.realpath( srcDir ).lower(): %s" % os.path.realpath( srcDir ).lower())
+            reporter.message("fp: %s" % fp)
+            reporter.message("os.path.realpath( srcDir ): %s" % os.path.realpath( srcDir ))
+            reporter.message("relPath: %s" % relPath)
+
+            destPath = os.path.join( os.path.realpath( destDir ), relPath )
+            reporter.message("destPath: %s" % destPath)
+
+            reporter.message(fp)
+
+            if not os.path.exists(os.path.dirname(destPath)):
+                os.makedirs(os.path.dirname(destPath))
+
+            reporter.message("copying %s to %s" % (fp, destPath))
+
+            if not shutil.copyfile(fp, destPath):
+                reporter.failure("copying %s to %s" % (fp, destPath))
+                return False
+        return True
