b408a07d2521d68f51e4efe9c867329833f4e1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package bdajax/1.4.1/bdajax-1.4.1.tar.gz

---
+++ packages/bdajax/bdajax-1.4.1.tar.gz/bdajax-1.4.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.4.1'
+shortdesc = 'Ajax convenience.'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+
+setup(name='bdajax',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Operating System :: OS Independent',
+            'Programming Language :: JavaScript', 
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',        
+      ],
+      keywords='',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      url=u'https://github.com/bluedynamics/bdajax',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require = dict(
+      ),
+      tests_require=[
+      ],
+      test_suite = "",
+      entry_points = """
+      """
+      )
