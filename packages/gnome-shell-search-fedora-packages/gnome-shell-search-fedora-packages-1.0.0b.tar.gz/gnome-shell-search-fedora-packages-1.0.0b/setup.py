e6c04180d798a467428ef4b2d5a403c88186b802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package gnome-shell-search-fedora-packages/1.0.0b/gnome-shell-search-fedora-packages-1.0.0b.tar.gz

---
+++ packages/gnome-shell-search-fedora-packages/gnome-shell-search-fedora-packages-1.0.0b.tar.gz/gnome-shell-search-fedora-packages-1.0.0b/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requires = [
+    'pkgwat.api',
+]
+
+setup(
+    name="gnome-shell-search-fedora-packages",
+    version='1.0.0b',
+    description="A gnome shell search provider for apps.fp.o/packages",
+    url="http://github.com/ralphbean/gnome-shell-search-fedora-packages",
+    author="Ralph Bean",
+    author_email="rbean@redhat.com",
+    license='GPLv3',
+    install_requires=requires,
+    packages=['gs_search_fedora_packages'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'gnome-shell-search-fedora-packages-daemon = gs_search_fedora_packages.daemon:main',
+        ],
+    }
+)
