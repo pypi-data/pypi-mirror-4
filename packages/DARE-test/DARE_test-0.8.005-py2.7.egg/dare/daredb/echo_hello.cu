7ed3e0c7515a79752296a6654a4cb12def49f20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:30 +0000

    Add package DARE-test/0.8.005/DARE_test-0.8.005-py2.7.egg

---
+++ packages/DARE-test/DARE_test-0.8.005-py2.7.egg/dare/daredb/echo_hello.cu
@@ -0,0 +1,11 @@
+#__author__    = "Sharath Maddineni"
+#__copyright__ = "Copyright 2011-2012, Sharath Maddineni"
+#__license__   = "MIT"
+
+
+
+[echo_hello]
+executable = /bin/echo
+spmd_variation = single
+number_of_processes = 1
+arguments = 
