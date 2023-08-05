9d46b23bfba1e1c11a4a536f2e948ef2e714a2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package summary/0.1.1/summary-0.1.1.tar.gz

---
+++ packages/summary/summary-0.1.1.tar.gz/summary-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+    name = "summary",
+    packages = ["summary"],
+    version = "0.1.1",
+    install_requires = ["chardet", "lxml==2.3.5"],
+    description = "Extractor to get main content from the web page.",
+    author = "Satoshi Okami",
+    author_email = "me.after.12am@gmail.com",
+    url = "https://github.com/after12am/summary",
+    download_url = "https://github.com/after12am/summary/archive/master.zip",
+    keywords = ["summary", "extract", "scraping", "web"],
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: MacOS",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet"
+    ],
+    long_description = """\
+
+Extractor to get main content from the web page.
+
+ - title
+
+ - body
+
+ - candidate of main images
+
+Usages
+
+visit at https://github.com/after12am/summary
+"""
+)
---
+++ packages/summary/summary-0.1.1.tar.gz/summary-0.1.1/summary/__init__.py
@@ -0,0 +1,3 @@
+from extractor import is_collection_of_links
+from extractor import not_body_rate
+from extractor import extract
