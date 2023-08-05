f5b94e4996832fac6825ebb6cbba4c05849b161c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:17 +0000

    Add package collective.js.timeago/1.1/collective.js.timeago-1.1.zip

---
+++ packages/collective.js.timeago/collective.js.timeago-1.1.zip/collective.js.timeago-1.1/collective/js/timeago/viewlet.py
@@ -0,0 +1,18 @@
+from plone.app.layout.viewlets.common import ViewletBase
+
+SCRIPT_TAG = """<script type="text/javascript"
+  src="%s/++resource++collective.js.timeago/locales/jquery.timeago.%s.js"></script>"""
+
+class L10nTimeAgo(ViewletBase):
+    """Load L10N for this javascript"""
+
+    def update(self):
+        super(L10nTimeAgo, self).update()
+        self.lang = self.portal_state.language()
+
+    def index(self):
+        return SCRIPT_TAG % (self.site_url, self.lang)
+
+
+class TimeAgo(ViewletBase):
+    """Load timeago on css class .timeago"""
