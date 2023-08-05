ea1c4e582070ada08966c2c1df3576042484b459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package plone.portlets/2.2/plone.portlets-2.2.zip

---
+++ packages/plone.portlets/plone.portlets-2.2.zip/plone.portlets-2.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+version = '2.2'
+
+setup(name='plone.portlets',
+      version=version,
+      description="An extension of zope.viewlet to support dynamic portlets",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='portlet viewlet',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.portlets',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=[
+              'zope.browserpage',
+              'zope.configuration',
+              'zope.location',
+              'zope.security',
+          ],
+      ),
+      install_requires=[
+        'setuptools',
+        'ZODB3',
+        'plone.memoize',
+        'zope.annotation',
+        'zope.component',
+        'zope.container',
+        'zope.contentprovider',
+        'zope.interface',
+        'zope.publisher',
+        'zope.schema',
+        'zope.site',
+      ],
+      )
