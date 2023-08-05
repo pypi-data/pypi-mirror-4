467d6fe0fa01af548366e4e044c7f5b847a0b443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package Products.SimpleCalendar/1.0.1b2/Products.SimpleCalendar-1.0.1b2.tar.gz

---
+++ packages/Products.SimpleCalendar/Products.SimpleCalendar-1.0.1b2.tar.gz/Products.SimpleCalendar-1.0.1b2/Products/SimpleCalendar/interfaces.py
@@ -0,0 +1,10 @@
+from zope.interface import Interface
+
+class ISimpleViewClass(Interface):
+    pass
+
+class ISimpleViewClassWeek(Interface):
+    pass
+
+class ISimpleViewClassDay(Interface):
+    pass
