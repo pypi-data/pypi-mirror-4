d9831fc5fb259ead360e94eb51942a5fc8c329df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:56 +0000

    Add package infi.wmpio/0.1.13/infi.wmpio-0.1.13.tar.gz

---
+++ packages/infi.wmpio/infi.wmpio-0.1.13.tar.gz/infi.wmpio-0.1.13/src/infi/wmpio/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.13"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8f9daf34a2bbe30e38bd00eb03cf98059bd05753'
+__git_head_subject__ = 'Finished release v0.1.13.'
+__git_head_message__ = '# By Arnon Yaari (1) and Guy Rozendorn (1)\n* release/v0.1.13:\n  TRIVIAL skeleton update\n  empty commit after version v0.1.12'
+__git_dirty_diff__ = ''
