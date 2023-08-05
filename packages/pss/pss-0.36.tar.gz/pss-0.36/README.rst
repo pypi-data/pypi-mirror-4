67fdd3b1a52e1f7252f74bd70bea355c4860ffa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:52 +0000

    Add package pss/0.36/pss-0.36.tar.gz

---
+++ packages/pss/pss-0.36.tar.gz/pss-0.36/README.rst
@@ -0,0 +1,60 @@
+Introduction: what is pss?
+--------------------------
+
+**pss** is a power-tool for searching inside source code files. **pss**
+searches recursively within a directory tree, knows which extensions and
+file names to search and which to ignore, automatically skips directories
+you wouldn't want to search in (for example ``.svn`` or ``.git``), colors
+its output in a helpful way, and does much more.
+
+If you're familiar with the **ack** tool, then you will find **pss** very
+similar (see https://bitbucket.org/eliben/pss/wiki/PssAndAck).
+
+Pre-requisites
+--------------
+
+**pss** needs only Python to run. It works with with Python versions 2.6, 2.7
+and 3.2 on Linux and Windows. Some testing was done on on Mac OS X and FreeBSD
+as well.
+
+Installing
+----------
+
+**pss** can be installed from PyPi (Python package index)::
+
+    > pip install pss
+
+Alternatively, you can download the source distribution either from PyPi or go to the *Downloads* tab on the `pss project page <https://bitbucket.org/eliben/pss>`_ and pick the version you're interested in from *Tags*. When you unzip the source distribution, run::
+
+    > python setup.py install
+
+Running without installing
+--------------------------
+
+**pss** supports direct invocation even without installing it. This may
+be useful if you're on a machine without administrator rights, or want to
+experiment with a source distribution of **pss**.
+
+Just unzip the **pss** distribution into some directory. Let's assume its full
+path is ``/path/to/pss``. You can now run::
+
+    > /path/to/python /path/to/pss
+
+And this will invoke **pss** as expected. This command can also be tied to an
+alias or placed in a shell (or batch) script for convenience.
+
+How to use it?
+--------------
+
+**pss** is meant to be executed from the command line. Running it with no
+arguments or with ``-h`` will print a detailed usage message. 
+
+For some detailed usage examples, check out the 
+Usage wiki page - https://bitbucket.org/eliben/pss/wiki/Usage.
+
+License
+-------
+
+**pss** is open-source software. Its code is in the public domain. See the
+``LICENSE`` file for more details.
+
---
+++ packages/pss/pss-0.36.tar.gz/pss-0.36/psslib/filefinder.py
@@ -0,0 +1,162 @@
+#-------------------------------------------------------------------------------
+# pss: filefinder.py
+#
+# FileFinder class that finds files recursively according to various rules.
+#
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+import os
+import re
+
+from .utils import istextfile
+
+
+class FileFinder(object):
+    def __init__(self,
+            roots,
+            recurse=True,
+            ignore_dirs=[],
+            find_only_text_files=False,
+            search_extensions=[],
+            ignore_extensions=[],
+            search_file_patterns=[],
+            ignore_file_patterns=[]):
+        """ Create a new FileFinder. The parameters are the "search rules"
+            that dictate which files are found.
+
+            roots:
+                Root files/directories from which the search will start
+
+            recurse:
+                Should I recurse into sub-directories?
+
+            ignore_dirs:
+                Iterable of directory names that will be ignored during the
+                search
+
+            find_only_text_files:
+                If True, uses a heuristic to determine which files are text
+                and which are binary, and ignores the binary files.
+                Warning: this option makes FileFinder actually open the files
+                and read a portion from them, so it is quite slow.
+
+            search_extensions:
+                If non-empty, only files with extensions listed here will be
+                found. If empty, files with all extensions will be found
+
+            ignore_extensions:
+                Files with extensions listed here will never be found.
+                Overrides "search_*" rules
+
+            search_file_patterns:
+                If non-empty, only files with names matching these pattens will
+                be found. If empty, no pattern restriction is applied
+
+            ignore_file_patterns:
+                Files with names matching these patterns will never be found.
+                Overrides "search_*" rules
+        """
+        # Prepare internal data structures from the parameters
+        self.roots = roots
+        self.recurse = recurse
+        self.search_extensions = set(search_extensions)
+        self.ignore_extensions = set(ignore_extensions)
+
+        # Distinguish between dirs (like "foo") and paths (like "foo/bar")
+        # to ignore.
+        self.ignore_dirs = set()
+        self.ignore_paths = set()
+        for d in ignore_dirs:
+            if os.sep in d:
+                self.ignore_paths.add(d)
+            else:
+                self.ignore_dirs.add(d)
+
+        self.find_only_text_files = find_only_text_files
+        self.search_file_patterns = [re.compile(p) for p in search_file_patterns]
+        self.ignore_file_patterns = [re.compile(p) for p in ignore_file_patterns]
+
+    def files(self):
+        """ Generate files according to the search rules. Yield
+            paths to files one by one.
+        """
+        for root in self.roots:
+            if os.path.isfile(root):
+                if self._file_is_found(root):
+                    yield root
+            else: # dir
+                for dirpath, subdirs, files in os.walk(root):
+                    if self._should_ignore_dir(dirpath):
+                        # This dir should be ignored, so remove all its subdirs
+                        # from the walk and go to next dir.
+                        del subdirs[:]
+                        continue
+                    for filename in files:
+                        fullpath = os.path.join(dirpath, filename)
+                        if (    self._file_is_found(fullpath) and
+                                os.path.exists(fullpath)):
+                            yield fullpath
+                    if not self.recurse:
+                        break
+
+    def _should_ignore_dir(self, dirpath):
+        """ Should the given directory be ignored?
+        """
+        if os.path.split(dirpath)[1] in self.ignore_dirs:
+            return True
+        elif len(self.ignore_paths) > 0:
+            # If we have paths to ignore, things are more difficult...
+            for ignored_path in self.ignore_paths:
+                found_i = dirpath.rfind(ignored_path)
+                if (    found_i == 0 or (
+                        found_i > 0 and dirpath[found_i - 1] == os.sep)):
+                    print('ignoring')
+                    return True
+        return False
+
+    def _file_is_found(self, filename):
+        """ Should this file be "found" according to the search rules?
+        """
+        # Tries to eliminate the file by all the given search rules. If the
+        # file survives until the end, it's found
+        root, ext = os.path.splitext(filename)
+
+        if ext in self.ignore_extensions:
+            return False
+
+        if self.search_extensions and ext not in self.search_extensions:
+            # If search_extensions is non-empty, only files with extensions
+            # listed there can be found
+            return False
+
+        if any(ignored_pattern.search(filename) for ignored_pattern in self.ignore_file_patterns):
+            return False
+
+        # If search_file_patterns is non-empty, the file has to match at least
+        # one of the patterns.
+        if (self.search_file_patterns and
+            not any(p.search(filename) for p in self.search_file_patterns)
+            ):
+            return False
+
+        # If find_only_text_files, open the file and try to determine whether
+        # it's text or binary.
+        if self.find_only_text_files:
+            try:
+                with open(filename, 'rb') as f:
+                    if not istextfile(f):
+                        return False
+            except OSError:
+                # If there's a problem opening or reading the file, we
+                # don't need it.
+                return False
+
+        return True
+
+
+if __name__ == '__main__':
+    import sys
+    ff = FileFinder(sys.argv[1:], ignore_dirs=[], recurse=True)
+    print(list(ff.files()))
+
