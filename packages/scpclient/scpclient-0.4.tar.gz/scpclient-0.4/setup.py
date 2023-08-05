756f25d7bab3728f0c857f2f10fca2db0d8aef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package scpclient/0.4/scpclient-0.4.tar.gz

---
+++ packages/scpclient/scpclient-0.4.tar.gz/scpclient-0.4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Command
+
+setup(name='scpclient',
+      version='0.4',
+      url='https://bitbucket.org/ericvsmith/scpclient',
+      author='Eric V. Smith',
+      author_email='eric@trueblade.com',
+      description='scp client, for use with paramiko.',
+      long_description=open('README.txt').read() + '\n' + open('CHANGES.txt').read(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Security :: Cryptography',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   ],
+      license='Apache License Version 2.0',
+      py_modules=['scpclient'],
+      )
