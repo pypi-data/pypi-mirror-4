bb847c5b7249123651da09e5c9adc152c5f817ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package alignment/1.0.2/alignment-1.0.2.tar.gz

---
+++ packages/alignment/alignment-1.0.2.tar.gz/alignment-1.0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='alignment',
+	version='1.0.2',
+	description='Python Alignment Library',
+	author='Eser Aygun',
+	author_email='eser.aygun@gmail.com',
+	url='https://github.com/eseraygun/python-alignment',
+	requires=['numpy'],
+)
