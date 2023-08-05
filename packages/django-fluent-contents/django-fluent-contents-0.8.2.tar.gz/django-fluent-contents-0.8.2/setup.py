f2d70e0c72435df878f79ac68797dea2efbb0736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package django-fluent-contents/0.8.2/django-fluent-contents-0.8.2.tar.gz

---
+++ packages/django-fluent-contents/django-fluent-contents-0.8.2.tar.gz/django-fluent-contents-0.8.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os.path import dirname, join
+import sys, os
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv:
+    try:
+        os.chdir('fluent_contents')
+        from django.core.management.commands.compilemessages import compile_messages
+        compile_messages(sys.stderr)
+    finally:
+        os.chdir('..')
+
+
+setup(
+    name='django-fluent-contents',
+    version='0.8.2',
+    license='Apache License, Version 2.0',
+
+    install_requires=[
+        'Django>=1.3.0',   # Using staticfiles
+        'django-polymorphic>=0.2',
+        'django-template-analyzer>=1.1.0',
+    ],
+    extras_require = {
+        'code': ['Pygments'],
+        'disquswidgets': ['django-disqus'],
+        'formdesignerlink': ['django-form-designer'],
+        'markup': ['docutils', 'textile', 'Markdown>=1.7'],
+        'text': ['django-wysiwyg>=0.3.0'],
+        'twitter': ['twitter-text-py>=1.0.3'],
+    },
+    dependency_links = [
+        'git+https://github.com/philomat/django-form-designer.git#egg=django-form-designer',
+    ],
+
+    description='A widget engine to display various content on Django pages',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-fluent-contents',
+    download_url='https://github.com/edoburu/django-fluent-contents/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ]
+)
