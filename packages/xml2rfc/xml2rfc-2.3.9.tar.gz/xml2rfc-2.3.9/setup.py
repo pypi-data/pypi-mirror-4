1ce4263b063809e02ac54421a4f712042999fe53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:37 +0000

    Add package xml2rfc/2.3.9/xml2rfc-2.3.9.tar.gz

---
+++ packages/xml2rfc/xml2rfc-2.3.9.tar.gz/xml2rfc-2.3.9/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# --------------------------------------------------
+# Copyright The IETF Trust 2011, All Rights Reserved
+# --------------------------------------------------
+
+import os
+from setuptools import setup
+
+description = 'Validate and convert XML RFC documents to various output ' \
+              'formats'
+
+try:
+    long_description = open(os.path.join(os.path.dirname(__file__), 
+                                         'README.rst')).read()
+except Exception:
+    long_description = description
+
+setup(
+    # Package metadata
+    name='xml2rfc',
+    version='2.3.9',
+    description=description,
+    long_description=long_description,
+    author='Concentric Sky',
+    author_email='tools-discuss@ietf.org',
+    url='http://tools.ietf.org/',
+    classifiers= [
+        'Environment :: Console',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
+
+    # Program data
+    scripts=['scripts/xml2rfc'],
+    packages=['xml2rfc', 'xml2rfc/writers'],
+    package_data={'xml2rfc': ['templates/*',
+                              ]},
+                 
+    install_requires=['lxml>=2.2.8'],
+)
