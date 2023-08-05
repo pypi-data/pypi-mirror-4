de6badcf6427709d06c5d7cfa4982fa9e48bebdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package kippt/1.0.1/kippt-1.0.1.tar.gz

---
+++ packages/kippt/kippt-1.0.1.tar.gz/kippt-1.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import requests
+from requests.compat import is_py2
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+setup(
+    name='kippt',
+    version="1.0.1",
+    description='Kippt.com API wrapper for Python',
+    long_description=open('README').read(),
+    author='TJ (Thomas) Biddle',
+    author_email='me@ThomasBiddle.com',
+    url='https://github.com/thomasbiddle/Kippt-Python-Wrapper',
+    packages=['kippt',],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    license='wtfpl',
+)
+
+del os.environ['PYTHONDONTWRITEBYTECODE']
