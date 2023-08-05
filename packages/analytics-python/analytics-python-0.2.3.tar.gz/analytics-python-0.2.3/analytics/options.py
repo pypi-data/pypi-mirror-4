ef9a94e9bd490fee1a884da0d3561fc00885a1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:01 +0000

    Add package analytics-python/0.2.3/analytics-python-0.2.3.tar.gz

---
+++ packages/analytics-python/analytics-python-0.2.3.tar.gz/analytics-python-0.2.3/analytics/options.py
@@ -0,0 +1,9 @@
+
+#host = 'https://api2.segment.io'
+host = 'http://localhost:7001'
+
+endpoints = {
+    'track': '/v1/track',
+    'identify': '/v1/identify',
+    'batch': '/v1/import'
+}
---
+++ packages/analytics-python/analytics-python-0.2.3.tar.gz/analytics-python-0.2.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+long_description = '''
+Segment.io is the simplest way to integrate analytics into your application.
+One API allows you to turn on any other analytics service. No more learning
+new APIs, repeated code, and wasted development time.
+
+This is the official python client that wraps the Segment.io REST API (https://segment.io).
+
+Documentation and more details at https://github.com/segmentio/analytics-python
+'''
+
+
+setup(
+    name='analytics-python',
+    version='0.2.3',
+    url='https://github.com/segmentio/analytics-python',
+    author='Ilya Volodarsky',
+    author_email='ilya@segment.io',
+    maintainer='Segment.io',
+    maintainer_email='friends@segment.io',
+    packages=['analytics'],
+    license='MIT License',
+    install_requires=[
+        'requests',
+        'python-dateutil'
+    ],
+    description='The hassle-free way to integrate analytics into any python application.',
+    long_description=long_description
+)
