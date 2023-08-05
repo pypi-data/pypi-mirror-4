2cbb055f018bb578f064d2f6cb745be50a0538b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package django-photologue/2.5/django-photologue-2.5.zip

---
+++ packages/django-photologue/django-photologue-2.5.zip/django-photologue-2.5/setup.py
@@ -0,0 +1,39 @@
+#/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+version = '2.5'
+
+setup(
+    name="django-photologue",
+    version=version,
+    description="Powerful image management for the Django web framework.",
+    author="Justin Driscoll, Marcos Daniel Petry, Richard Barran",
+    author_email="justin@driscolldev.com, marcospetry@gmail.com",
+    url="https://github.com/jdriscoll/django-photologue",
+    packages=find_packages(),
+    package_data={
+        'photologue': [
+            'res/*.jpg',
+            'locale/*/LC_MESSAGES/*',
+            'templates/photologue/*.html',
+            'templates/photologue/tags/*.html',
+        ]
+    },
+    zip_safe=False,
+    classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+    install_requires=['Django>=1.3', # Change to class-based views means 1.3 minimum. 
+                      'South>=0.7.5', # Might work with earlier versions, but not tested.
+                      'PIL>=1.1.7', # Might work with earlier versions, but not tested.
+                      ],
+)
