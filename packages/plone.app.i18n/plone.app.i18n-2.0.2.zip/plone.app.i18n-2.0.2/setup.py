a57fca83406d3572347588a3f3331656343c9e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package plone.app.i18n/2.0.2/plone.app.i18n-2.0.2.zip

---
+++ packages/plone.app.i18n/plone.app.i18n-2.0.2.zip/plone.app.i18n-2.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '2.0.2'
+
+setup(name='plone.app.i18n',
+      version=version,
+      description="Plone specific i18n extensions.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='plone i18n',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.i18n',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
