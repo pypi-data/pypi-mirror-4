70c0d30095527aa24fea9fc150e6cf8e98705565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package cerberos/0.1.3/cerberos-0.1.3.tar.gz

---
+++ packages/cerberos/cerberos-0.1.3.tar.gz/cerberos-0.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+from setuptools import setup, find_packages
+
+
+version = '0.1.3'
+
+
+if sys.argv[-1] == 'publish':
+    subprocess.call(['python', 'setup.py', 'sdist', 'upload'])
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %s -m 'Tag version %s'" % (version, version)
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name='cerberos',
+    version=version,
+    description='Cerberos is a django app that watches failed logins and block the user after N attempts.',
+    author='Adrián Ribao',
+    url='https://github.com/AdrianRibao/cerberos',
+    packages = find_packages(exclude=['tests', 'tests.*']),
+    include_package_data = True,
+    license='BSD',
+)
