4c2946058ecb7198b03d4531934410aa5abf8758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package trafaret/0.4.8/trafaret-0.4.8.tar.gz

---
+++ packages/trafaret/trafaret-0.4.8.tar.gz/trafaret-0.4.8/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os.path
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setupconf = dict(
+    name = 'trafaret',
+    version = '0.4.8',
+    license = 'BSD',
+    url = 'https://github.com/Deepwalker/trafaret/',
+    author = 'Barbuza, Deepwalker, nimnull',
+    author_email = 'krivushinme@gmail.com',
+    description = ('Validation and parsing library'),
+    long_description = read('README.rst'),
+    keywords = 'validatation form forms data schema',
+
+    packages = ['trafaret'],
+
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        ],
+    )
+
+if __name__ == '__main__':
+    setup(**setupconf)
