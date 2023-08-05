3963bb9bf8d0da644c1fa407d874e231048edcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package rfclient/0.2.2/rfclient-0.2.2.tar.gz

---
+++ packages/rfclient/rfclient-0.2.2.tar.gz/rfclient-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rfclient",
+    version="0.2.2",
+    author="ReThought Ltd",
+    author_email="code@rethought-solutions.com",
+    license="BSD",
+    keywords="api",
+    description="API to the RedFlash notification management system.",
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+)
