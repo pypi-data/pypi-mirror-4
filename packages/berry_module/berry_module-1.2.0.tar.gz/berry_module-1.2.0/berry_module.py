e7dc64a1a8a9a3dcaca7b2d0d70b159f8ccf63c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:35 +0000

    Add package berry_module/1.2.0/berry_module-1.2.0.tar.gz

---
+++ packages/berry_module/berry_module-1.2.0.tar.gz/berry_module-1.2.0/berry_module.py
@@ -0,0 +1,12 @@
+"""This is a simple module to print the data items within a nested list"""
+def print_lol(items,level=0):
+    for each_item in items:
+        if isinstance(each_item, list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+
+
+        
---
+++ packages/berry_module/berry_module-1.2.0.tar.gz/berry_module-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup 
+
+setup(
+	name = 'berry_module',
+	version = '1.2.0',
+	py_modules = ['berry_module'],
+	author = 'berry',
+	author_email = 'mansa.121@gmail.com',
+	url = ' ',
+	description = 'Module to print data items within a nested list',
+     )
