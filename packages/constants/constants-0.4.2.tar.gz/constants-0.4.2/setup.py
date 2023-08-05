1475c6efa3a2f5ec45e8d4932b5251abd3e2eb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package constants/0.4.2/constants-0.4.2.tar.gz

---
+++ packages/constants/constants-0.4.2.tar.gz/constants-0.4.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+
+setup(
+    name='constants',
+    version='0.4.2',
+    description='The simple way to deal with environment constants.',
+    long_description=open('README.rst').read(),
+    author='Eugene Van den Bulke',
+    author_email='eugene.vandenbulke@gmail.com',
+    url='http://github.com/3kwa/constants',
+    py_modules=['constants'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries'
+        ],
+)
