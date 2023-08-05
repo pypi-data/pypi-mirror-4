da4549d9643e231325240f8734a91cbf566251d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:05 +0000

    Add package collective.socialbar/1.1/collective.socialbar-1.1.zip

---
+++ packages/collective.socialbar/collective.socialbar-1.1.zip/collective.socialbar-1.1/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+A very simple social bar which uses GPlus/Facebook/Twitter sharer url dialogs.
---
+++ packages/collective.socialbar/collective.socialbar-1.1.zip/collective.socialbar-1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.socialbar',
+      version=version,
+      description="socialbar viewlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone social viewlet zope',
+      author='Izhar Firdaus',
+      author_email='izhar@inigo-tech.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
