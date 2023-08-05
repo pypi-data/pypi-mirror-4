8d7cec191fc66d3a26ef649be1260ff0b70cc27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package streetvoice-celery-email/0.3.4/streetvoice-celery-email-0.3.4.tar.gz

---
+++ packages/streetvoice-celery-email/streetvoice-celery-email-0.3.4.tar.gz/streetvoice-celery-email-0.3.4/svcelery_email/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Blacklist(models.Model):
+    email = models.EmailField(db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+RESULT_CODES = (
+    ("1", "success"),
+    ("2", "retry"),
+    ("3", "blacklisted"),
+)
+
+class MessageLogManager(models.Manager):
+    def log(self, message, result_code):
+        self.create(email=message.to, body=str(message.message), result=result_code)
+        
+
+class MessageLog(models.Model):
+    email = models.EmailField()
+    body = models.TextField()
+    result = models.CharField(max_length=1, choices=RESULT_CODES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = MessageLogManager()
