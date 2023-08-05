09b73c2e4667e231d849cb9c1d0c774a0b866749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package collective.portlet.socialnetworks/1.0/collective.portlet.socialnetworks-1.0.zip

---
+++ packages/collective.portlet.socialnetworks/collective.portlet.socialnetworks-1.0.zip/collective.portlet.socialnetworks-1.0/collective/portlet/socialnetworks/socialnetworks.py
@@ -0,0 +1,90 @@
+from zope.interface import implements
+from zope import component
+from zope import schema
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.registry.interfaces import IRegistry
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+
+from collective.portlet.socialnetworks import SocialNetworksMessageFactory as _
+
+
+class ISocialNetworks(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+
+    header = schema.TextLine(title=_(u"Header"),
+                             required=True)
+
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(ISocialNetworks)
+
+    def __init__(self, header=u""):
+        self.header = header
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        if self.header:
+            return self.header
+        return _(u"Social Networks")
+
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+    """
+
+    render = ViewPageTemplateFile('socialnetworks.pt')
+
+    def site_url(self):
+        if not hasattr(self, '_site_url'):
+            self._site_url = self.context.portal_url()
+        return self._site_url
+
+    def get_socialnetworks(self):
+        networks = []
+        registry = component.getUtility(IRegistry)
+        record = registry.get('collective.portlet.socialnetworks', [])
+        unknown = 0
+
+        for row in record:
+
+            if row.startswith('http'):
+                #unknow network
+                networks.append({'id': 'unknown%s'%unknown})
+                unknonw += 1
+            else:
+                splited = row.split('|')
+                if len(splited) == 2:
+                    networks.append({'id': splited[0],
+                                     'url': splited[1]})
+
+        return networks
+
+
+class AddForm(base.AddForm):
+    """Portlet add form.
+    """
+    form_fields = form.Fields(ISocialNetworks)
+
+    def create(self, data):
+        return Assignment(**data)
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+    """
+    form_fields = form.Fields(ISocialNetworks)
---
+++ packages/collective.portlet.socialnetworks/collective.portlet.socialnetworks-1.0.zip/collective.portlet.socialnetworks-1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.portlet.socialnetworks',
+      version=version,
+      description="Social networks to follow",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        ],
+      keywords='plone socialnetworks portlet',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/toutpt/collective.portlet.socialnetworks',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
