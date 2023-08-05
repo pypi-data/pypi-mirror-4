5c4aa6108e7b26f9c2e4aa93aadedda95691e175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package seed/0.2.12/seed-0.2.12.tar.gz

---
+++ packages/seed/seed-0.2.12.tar.gz/seed-0.2.12/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from os.path import exists
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from seed import __version__
+
+setup(
+    name='seed',
+    version=__version__,
+    author='Adam Charnock',
+    author_email='adam@playnice.ly',
+    packages=['seed', 'seed.commands', 'seed.vcs'],
+    scripts=[],
+    url='https://github.com/adamcharnock/seed',
+    license='MIT',
+    description='A utility for easily creating and releasing Python packages',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    entry_points=dict(console_scripts=['seed=seed.run:main']),
+    install_requires=[
+        "path.py>=2.2.2",
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Software Distribution',
+    ],
+)
