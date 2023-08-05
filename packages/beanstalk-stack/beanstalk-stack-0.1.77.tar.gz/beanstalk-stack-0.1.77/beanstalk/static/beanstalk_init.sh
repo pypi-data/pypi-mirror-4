33bdb82bff38ba87f74423df5ecb2955f59c5130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:49 +0000

    Add package beanstalk-stack/0.1.77/beanstalk-stack-0.1.77.tar.gz

---
+++ packages/beanstalk-stack/beanstalk-stack-0.1.77.tar.gz/beanstalk-stack-0.1.77/beanstalk/static/beanstalk_init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          beanstalk
+# Required-Start:    $local_fs $network $httpd
+# Required-Stop:     $local_fs $network $httpd
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the beanstalk app server
+# Description:       starts beanstalk app server using start-stop-daemon
+### END INIT INFO
+
+case "$1" in
+    start|stop|reload|restart)
+        bsjack daemon.$1
+        OUT=$?
+        exit ${OUT}
+        ;;
+    *)
+        bsjack daemon.usage
+        exit 1
+    ;;
+esac
