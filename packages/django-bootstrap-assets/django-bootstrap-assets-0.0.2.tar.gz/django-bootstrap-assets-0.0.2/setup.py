af5ece8d115ceb402a4938dfc5378d5b14df072a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package django-bootstrap-assets/0.0.2/django-bootstrap-assets-0.0.2.tar.gz

---
+++ packages/django-bootstrap-assets/django-bootstrap-assets-0.0.2.tar.gz/django-bootstrap-assets-0.0.2/setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+
+setup(
+    name='django-bootstrap-assets',
+    description='jQuery plugin that auto-hides form field help blocks as '
+    'pluggable Django app',
+    long_description=open('README.rst').read(),
+    version='0.0.2',
+    packages=[
+        'bootstrap',
+        'bootstrap_all',
+        'bootstrap_datepicker',
+        'bootstrap_select2',
+    ],
+    package_data={
+        'bootstrap': [
+            'static/js/*.js',
+            'static/css/*.css',
+            'static/img/*.png',
+        ],
+        'bootstrap_all': [
+            'static/js/*.js',
+            'static/css/*.css',
+            'static/img/*.png',
+            'static/img/*.gif',
+        ],
+        'bootstrap_datepicker': [
+            'static/js/*.js',
+            'static/css/*.css'
+        ],
+        'bootstrap_select2': [
+            'static/js/*.js',
+            'static/css/*.css',
+            'static/img/*.png',
+            'static/img/*.gif',
+        ],
+    },
+    include_package_data=True,
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-bootstrap-assets',
+    download_url='https://bitbucket.org/monwara/django-bootstrap-assets/downloads',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
