fe33f47ded6ee8ea97adc9244112eb87c3b28fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:22 +0000

    Add package kotti_media/0.3/kotti_media-0.3.tar.gz

---
+++ packages/kotti_media/kotti_media-0.3.tar.gz/kotti_media-0.3/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.3'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+try:
+    TODO = open(os.path.join(here, 'TODO.txt')).read()
+except IOError:
+    TODO = ''
+try:
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    CHANGES = ''
+
+development_requires = ['minify', ]
+install_requires = ['Kotti>=0.7',
+                    'js.mediaelement', ]
+tests_require = ['pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'pytest-xdist',
+                'WebTest',
+                'wsgi_intercept',
+                'zope.testbrowser', ]
+
+setup(name='kotti_media',
+      version=version,
+      description='Add media content to your Kotti site',
+      long_description=README + '\n\n' + TODO + '\n\n' + CHANGES,
+      classifiers=['Programming Language :: Python',
+                   'Framework :: Pylons',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+                   'License :: Repoze Public License', ],
+      keywords='video audio medialementjs kotti cms pylons pyramid',
+      author='Andreas Kaiser',
+      author_email='disko@binary-punks.com',
+      url='https://github.com/disko/kotti_media',
+      license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'testing': tests_require,
+                      'development': development_requires, },
+      message_extractors={'kotti_media': [('**.py', 'lingua_python', None),
+                                          ('**.zcml', 'lingua_xml', None),
+                                          ('**.pt', 'lingua_xml', None), ]}, )
