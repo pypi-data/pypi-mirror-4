7840be3f71ad2012b78c8d402449dae35a19c356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package infi.recipe.application-packager/0.3.1/infi.recipe.application_packager-0.3.1.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.1.tar.gz/infi.recipe.application_packager-0.3.1/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '1b48b9f27cd3cb2f87b26e3a147597a21632b428'
+__git_head_subject__ = 'Finished release v0.3.1.'
+__git_head_message__ = '* release/v0.3.1:\n  TRIVIAL updated README.md\n  HOSTDEV-444\n  empty commit after version v0.3'
+__git_dirty_diff__ = ''
