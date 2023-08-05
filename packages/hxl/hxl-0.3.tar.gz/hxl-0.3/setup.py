3d6ee27ff863ed84345eb0a7d57df4674782a6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:21 +0000

    Add package hxl/0.3/hxl-0.3.tar.gz

---
+++ packages/hxl/hxl-0.3.tar.gz/hxl-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='hxl',
+    version='0.3',
+    description='Model to dictionary mapping for Python',
+    author='K.K.BeProud',
+    author_email='xinlei@yahoo.com.cn',
+    url='http://www.baidu.com/',
+    packages=find_packages(),
+    )
