64e046b3ee96821e287a25078e3c54ba7b9023f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package opkg_repoutils/1.0/opkg_repoutils-1.0.tar.gz

---
+++ packages/opkg_repoutils/opkg_repoutils-1.0.tar.gz/opkg_repoutils-1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Setup and install the opkg repo utils package.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='opkg_repoutils',
+    version='1.0',
+    description='Tools for maintaining an opkg repository.',
+    author='Kyle Isom',
+    author_email='coder@kyleisom.net',
+    license='ISC',
+    url='http://bitbucket.org/kisom/opkg_repoutils',
+    scripts=['scripts/opkg_repo_manager.py',],
+    packages=['opkg_repo', ],
+    requires=['yaml', 'pycrypto']
+)
