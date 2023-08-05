e5a497172e6ef2f0ba9339ee944830004a93759d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package xkcd/2.0/xkcd-2.0.tar.gz

---
+++ packages/xkcd/xkcd-2.0.tar.gz/xkcd-2.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='xkcd',
+	version='2.0',
+	description='Library to access xkcd.com',
+	long_description="Routines for retrieving comic information from Randall Munroe's xkcd webcomic",
+	author='Ben Rosser',
+	license="MIT",
+	author_email='rosser.bjr@gmail.com',
+	url="http://venus.arosser.com/projects/xkcd",
+	py_modules=['xkcd'])
+
