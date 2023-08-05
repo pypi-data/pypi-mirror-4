473e353b04d872224a4cafb0001eb26df684efcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package wheezy.validation/0.1.81/wheezy.validation-0.1.81-py2.5.egg

---
+++ packages/wheezy.validation/wheezy.validation-0.1.81-py2.5.egg/wheezy/validation/__init__.py
@@ -0,0 +1,7 @@
+
+""" ``validation`` package.
+"""
+
+from wheezy.validation.mixin import ValidationMixin
+from wheezy.validation.model import try_update_model
+from wheezy.validation.validator import Validator
