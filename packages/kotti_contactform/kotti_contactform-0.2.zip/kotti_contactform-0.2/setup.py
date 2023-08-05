924246e2583ffe612d953b12b62473f05b6cd66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package kotti_contactform/0.2/kotti_contactform-0.2.zip

---
+++ packages/kotti_contactform/kotti_contactform-0.2.zip/kotti_contactform-0.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+install_requires = [
+    'Kotti >= 0.8a1',
+    'Babel',
+    'Pillow',
+    'colander >= 0.9.8',
+    'alembic',
+]
+
+
+setup(name='kotti_contactform',
+      version='0.2',
+      description="Simple contact form for Kotti sites",
+      long_description=README + '\n\n' + CHANGES,
+      keywords='kotti contact form',
+      maintainer='Christian Neumann',
+      maintainer_email='cneumann@datenkarussell.de',
+      url='https://github.com/Kotti/kotti_contactform',
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: OSI Approved :: BSD License",
+        ],
+      author='Christian Neumann and contributors',
+      author_email='cneumann@datenkarussell.de',
+      license='BSD-2-Clause',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={},
+      message_extractors={"kotti_contactform": [
+        ("**.py", "lingua_python", None),
+        ("**.pt", "lingua_xml", None),
+        ]},
+      )
