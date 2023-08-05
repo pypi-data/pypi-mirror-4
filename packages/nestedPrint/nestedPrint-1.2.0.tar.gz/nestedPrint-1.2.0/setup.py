710df56ac761c0499d7c8b16a6a3e84e81528175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package nestedPrint/1.2.0/nestedPrint-1.2.0.tar.gz

---
+++ packages/nestedPrint/nestedPrint-1.2.0.tar.gz/nestedPrint-1.2.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from distutils.core import setup
+
+setup(
+
+    name            = 'nestedPrint',
+    version         = '1.2.0',
+    py_modules      = ['nestedPrintList'],
+    author          = 'vamshi vijaya krishna. J',
+    author_email    = 'vamshivjkrsna@gmail.com',
+    description     = 'A print utility compatable with 1.1.0'
+    
+    )
