a546159e20e83b93cd46635ddb29968bd5913acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package python-tds/0.3.0/python-tds-0.3.0.tar.gz

---
+++ packages/python-tds/python-tds-0.3.0.tar.gz/python-tds-0.3.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import pytds
+
+setup(name='python-tds',
+        version=pytds.__version__,
+        description='Python DBAPI driver for MSSQL using pure Python TDS (Tabular Data Stream) protocol implementation',
+        author='Mikhail Denisenko',
+        author_email='denisenkom@gmail.com',
+        url='https://github.com/denisenkom/pytds',
+        packages=['pytds'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+        ],
+        zip_safe=True,
+        install_requires=['BitVector'],
+        )
