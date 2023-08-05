b2b66935fe39d8c356d1e37537f9cdf28dcaddcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package DARE-test/0.8.0157/DARE-test-0.8.0157.tar.gz

---
+++ packages/DARE-test/DARE-test-0.8.0157.tar.gz/DARE-test-0.8.0157/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import distribute_setup
+distribute_setup.use_setuptools()
+
+import dare
+
+
+setup(name='DARE-test',
+    version=dare.__version__,
+    description='Dynamic Application Runtime Environment',
+    author='Sharath Maddineni',
+    author_email='smaddineni@cct.lsu.edu',
+    maintainer="Sharath Maddineni",
+    maintainer_email="smaddineni@cct.lsu.edu",
+    url='https://github.com/saga-project/DARE',
+    license="MIT",
+    packages=['dare', 'dare/bin', 'dare/core', 'dare/helpers', 'dare/daredb'],
+    package_data={'dare': ['daredb/*']},
+    install_requires=['bigjob'],
+    entry_points={'console_scripts': ['dare-run = dare.bin.darerun:main']})
