9ae6e814fd3d7c063de16eaed7991725a0e818cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:46 +0000

    Add package snackwich/1.3.9/snackwich-1.3.9.tar.gz

---
+++ packages/snackwich/snackwich-1.3.9.tar.gz/snackwich-1.3.9/snackwich/exceptions.py
@@ -0,0 +1,20 @@
+
+class GotoPanelException(Exception):
+    _key = None
+
+    def __init__(self, key):
+        self._key = key
+
+    @property
+    def key(self):
+        return self._key
+
+class BreakSuccessionException(Exception):
+    pass
+
+class QuitException(Exception):
+    pass
+
+class RedrawException(Exception):
+    pass
+
