f0c9cd340f15de80fe5ce65eb4e139f31be669bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:49 +0000

    Add package zero-common/0.1.9/zero-common-0.1.9.tar.gz

---
+++ packages/zero-common/zero-common-0.1.9.tar.gz/zero-common-0.1.9/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+package_data = {
+    'common': [
+        'templates/*.html',
+        'templates/base/*.html',
+        'templates/email/*.html',
+        'templates/forms/*.html',
+        'templates/pagination/*.html',
+        'static/bootstrap/css/*.css',
+        'static/bootstrap/img/*.png',
+        'static/bootstrap/js/*.js',
+        'static/css/style.css',
+        'static/images/*.png',
+        'static/js/*.js',
+    ]
+}
+packages = ['common', 'common/templatetags']
+
+setup(
+    # Basic package information:
+    name = 'zero-common',
+    version = '0.1.9',
+    author = 'Jose Maria Zambrana Arze',
+    author_email = 'contact@josezambrana.com',
+    license = 'apache license v2.0',
+    url = 'http://github.com/mandlaweb/Zero-Common',
+    keywords = 'zero common app',
+    description = 'App with common views, libs and templates that can be used by other apps',
+
+    # Packaging options:
+    packages = find_packages(),
+    package_data = package_data,
+    #zip_safe = False,
+    #include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['Django>=1.3.1', 'South>=0.7.3', 'django-less>=0.3'],
+)
