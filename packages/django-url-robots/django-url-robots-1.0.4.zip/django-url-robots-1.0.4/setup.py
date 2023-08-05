120bb0174031461271442f1cab9076ee317e3426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:56 +0000

    Add package django-url-robots/1.0.4/django-url-robots-1.0.4.zip

---
+++ packages/django-url-robots/django-url-robots-1.0.4.zip/django-url-robots-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-url-robots',
+    version='1.0.4',
+    description='Django robots.txt generator',
+    long_description=long_description,
+    url='http://github.com/dimka665/django-url-robots',
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
+    license='Python Software Foundation License',
+    packages=['url_robots', 'url_robots.tests'],
+    package_data={'url_robots': ['templates/*.*']},
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        ],
+)
