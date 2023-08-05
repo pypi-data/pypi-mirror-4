70457fe460339c22fe9cf41e6d1e65324548be3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:02 +0000

    Add package multireadline/0.1.0/multireadline-0.1.0.tar.gz

---
+++ packages/multireadline/multireadline-0.1.0.tar.gz/multireadline-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = "multireadline",
+    version = "0.1.0",
+    description = "A wrapper around Python's readline module which makes it easy to have an arbitrary number of 'inputs', each with its own history",
+    author = "Shawn Boyette",
+    author_email = "shawn@firepear.net",
+    url = "https://github.com/firepear/multireadline",
+    keywords = ["console", "readline"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces",
+        ],
+    py_modules=["multireadline"],
+    long_description = """"""
+)
