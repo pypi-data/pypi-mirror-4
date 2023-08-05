a57ff0a8f41a3ea23fb5c34c8b9355d053dd6103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:03 +0000

    Add package infi.pkgmgr/0.1.2/infi.pkgmgr-0.1.2.tar.gz

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.2.tar.gz/infi.pkgmgr-0.1.2/src/infi/pkgmgr/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5c7f05ad248cf2cd18e3aa53035c44d8b8ae2d58'
+__git_head_subject__ = 'empty commit after version v0.1.1'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
