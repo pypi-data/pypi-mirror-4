27a8b7691a11424a555c84df6e367105a0a0eaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:05 +0000

    Add package zope.app.i18n/3.6.4/zope.app.i18n-3.6.4.tar.gz

---
+++ packages/zope.app.i18n/zope.app.i18n-3.6.4.tar.gz/zope.app.i18n-3.6.4/setup.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.i18n package
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.i18n',
+    version='3.6.4',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Persistent translation domains and message catalogs',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    keywords = "zope3 i18n message factory",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.app.i18n',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require = dict(test=['zope.app.testing']),
+    install_requires=['setuptools',
+                      'zope.publisher>=3.9',
+                      'zope.component>=3.6',
+                      'zope.container',
+                      'zope.configuration',
+                      'zope.i18n',
+                      'zope.i18nmessageid',
+                      'zope.interface',
+                      'zope.security',
+                      'ZODB3',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/zope.app.i18n/zope.app.i18n-3.6.4.tar.gz/zope.app.i18n-3.6.4/src/zope/app/i18n/xmlrpc/methods.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Translation Domain XML-RPC Methods 
+
+$Id: methods.py 126935 2012-06-18 15:48:16Z tseaver $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.publisher.xmlrpc import XMLRPCView
+
+class Methods(XMLRPCView):
+
+    def getAllLanguages(self):
+        return self.context.getAllLanguages()
+
+    def getMessagesFor(self, languages):
+        messages = []
+        for msg in self.context.getMessages():
+            if msg['language'] in languages:
+                messages.append(msg)
+
+        return messages
