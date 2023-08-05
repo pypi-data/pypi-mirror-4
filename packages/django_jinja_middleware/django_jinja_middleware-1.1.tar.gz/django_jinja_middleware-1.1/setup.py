a5f521cc1d7247e8a789788a01f5dd99c90f651b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:29 +0000

    Add package django_jinja_middleware/1.1/django_jinja_middleware-1.1.tar.gz

---
+++ packages/django_jinja_middleware/django_jinja_middleware-1.1.tar.gz/django_jinja_middleware-1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='django_jinja_middleware',
+    version='1.1',
+    description='Django middleware class that takes TemplateResponses and renders them with Jinja, instead of the Django stock templating language.',
+    author='Luke Sneeringer',
+    author_email='lukesneeringer@gmail.com',
+    url='http://github.com/lukesneeringer/django-jinja/',
+    
+    # what to install
+    packages=['django_jinja'],
+    
+    # searches and classifications
+    keywords='django,jinja,middleware',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    
+    # dependencies
+    install_requires=[
+        'django >= 1.4',
+        'jinja2 >= 2.6',
+    ],
+)
