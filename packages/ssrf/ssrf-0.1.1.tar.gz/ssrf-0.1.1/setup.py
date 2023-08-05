7c154ca27cedc8d478d659fb3b47f4c83becbeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package ssrf/0.1.1/ssrf-0.1.1.tar.gz

---
+++ packages/ssrf/ssrf-0.1.1.tar.gz/ssrf-0.1.1/setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup, find_packages
+except ImporterError:
+    from ez_setup import use_setuptools #@UnresolvedImport
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='ssrf',
+    version='0.1.1',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    license='Creative Commons Attribution-Share Alike license',
+    description="Simple Spaced Repetition Formula",
+    long_description="""
+    Simple yet powerful spaced repetition algorithm used by RapidStudy.com.
+    See: http://www.rapidstudy.com
+    """,
+    install_requires=[],
+    tests_require=['nose', 'mox'],
+    author='Adam Dziendziel',
+    author_email='adam.dziendziel@gmail.com',
+    url='https://github.com/AdamDz/ssrf-python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education',
+    ],
+)
+
