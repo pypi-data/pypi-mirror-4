834aa1b7e3f82072cedd1d5d0e9acda741b36f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:18 +0000

    Add package collective.folderishtypes/1.4/collective.folderishtypes-1.4.tar.gz

---
+++ packages/collective.folderishtypes/collective.folderishtypes-1.4.tar.gz/collective.folderishtypes-1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.4'
+
+setup(name='collective.folderishtypes',
+      version=version,
+      description="Provides folderish types as a replacement for some ATContentTypes",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='ATContentTypes Archetypes Plone Folderish',
+      author='Johannes Raggam',
+      author_email='johannes@raggam.co.at',
+      url='http://github.com/collective/collective.folderishtypes',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      )
