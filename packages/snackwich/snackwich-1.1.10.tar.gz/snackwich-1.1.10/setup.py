be74bd0cc47d272076e2f8b824a9181a2405789d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package snackwich/1.1.10/snackwich-1.1.10.tar.gz

---
+++ packages/snackwich/snackwich-1.1.10.tar.gz/snackwich-1.1.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.1.10'
+
+setup(name='snackwich',
+      version=version,
+      description="Configuration-based console UI forms.",
+      long_description="Configuration-based Snack/Newt adaptation for easy and attractive console UI forms.",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Environment :: Console :: Newt'
+                  ],
+      keywords='console ui newt snack forms',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/Snackwich',
+      license='New BSD',
+      packages=['snackwich'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
