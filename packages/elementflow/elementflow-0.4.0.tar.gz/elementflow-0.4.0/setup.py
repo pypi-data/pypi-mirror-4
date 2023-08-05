e23a92c245a128e99567befe9c4962faee815c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package elementflow/0.4.0/elementflow-0.4.0.tar.gz

---
+++ packages/elementflow/elementflow-0.4.0.tar.gz/elementflow-0.4.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'elementflow',
+    version = '0.4.0',
+    author = 'Ivan Sagalaev',
+    author_email = 'Maniac@SoftwareManiacs.Org',
+    package_dir = {'': 'src'},
+    py_modules = ['elementflow'],
+    url = 'https://github.com/isagalaev/elementflow',
+    license = 'LICENSE.txt',
+    description = 'Python library for generating XML as a stream without first building a tree in memory.',
+    long_description = open('README.md').read(),
+)
