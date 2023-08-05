d48be809c92f80ea14e693c27c56bea39a0b1453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package chaussette/0.7/chaussette-0.7.tar.gz

---
+++ packages/chaussette/chaussette-0.7.tar.gz/chaussette-0.7/chaussette/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+logger = logging.getLogger('chaussette')
+
+__version__ = '0.7'
---
+++ packages/chaussette/chaussette-0.7.tar.gz/chaussette-0.7/chaussette/backend/__init__.py
@@ -0,0 +1,56 @@
+from chaussette.backend import _wsgiref
+
+_backends = {'wsgiref': _wsgiref.ChaussetteServer}
+
+try:
+    from chaussette.backend import _gevent
+    _backends['gevent'] = _gevent.Server
+
+    from chaussette.backend import _fastgevent
+    _backends['fastgevent'] = _fastgevent.Server
+except ImportError:
+    pass
+
+try:
+    from chaussette.backend import _geventwebsocket
+    _backends['geventwebsocket'] = _geventwebsocket.Server
+except ImportError:
+    pass
+
+try:
+    from chaussette.backend import _geventws4py
+    _backends['geventws4py'] = _geventws4py.Server
+except ImportError:
+    pass
+
+try:
+    from chaussette.backend import _meinheld
+    _backends['meinheld'] = _meinheld.Server
+except ImportError:
+    pass
+
+
+try:
+    from chaussette.backend import _waitress
+    _backends['waitress'] = _waitress.Server
+except ImportError:
+    pass
+
+
+try:
+    from chaussette.backend import _eventlet
+    _backends['eventlet'] = _eventlet.Server
+except ImportError:
+    pass
+
+
+def register(name, server):
+    _backends[name] = server
+
+
+def get(name):
+    return _backends[name]
+
+
+def backends():
+    return _backends.keys()
