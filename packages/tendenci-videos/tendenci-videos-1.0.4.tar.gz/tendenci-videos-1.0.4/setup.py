d46f96cf613f5954043644eb6ff4abc5f4bf6953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:02 +0000

    Add package tendenci-videos/1.0.4/tendenci-videos-1.0.4.tar.gz

---
+++ packages/tendenci-videos/tendenci-videos-1.0.4.tar.gz/tendenci-videos-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+longdesc = \
+'''
+An addon for Tendenci for displaying embedded videos.
+
+Supports Vimeo and YouTube embeds. Includes drag-n-drop reordering
+in the Django admin area.
+'''
+
+setup(
+    name='tendenci-videos',
+    author='Schipul',
+    author_email='programmers@schipul.com',
+    version='1.0.4',
+    license='GPL3',
+    description='Videos addon for Tendenci',
+    long_description=longdesc,
+    url='https://github.com/tendenci/tendenci-videos',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['tendenci>=5.1'],
+)
