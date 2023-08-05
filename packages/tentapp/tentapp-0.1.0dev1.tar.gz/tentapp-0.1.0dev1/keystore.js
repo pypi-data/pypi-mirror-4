a8b10bfaaf8538ae23ccb87440a77654ac6c65ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package tentapp/0.1.0dev1/tentapp-0.1.0dev1.tar.gz

---
+++ packages/tentapp/tentapp-0.1.0dev1.tar.gz/tentapp-0.1.0dev1/keystore.js
@@ -0,0 +1,10 @@
+{
+    "https://pythonclienttest.tent.is": {
+        "appID": "9houn1", 
+        "permanent_mac_key": "8941ae679b4f64878336684ebfd5c5c1", 
+        "permanent_mac_key_id": "u:0478dea8", 
+        "registration_mac_key": "355f0ae6d92f513043778cb9265de838", 
+        "registration_mac_key_id": "a:40982c45", 
+        "state": "Gp2riNit8Tbek4p8iHaa"
+    }
+}
