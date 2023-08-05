c47f5cde104324997c7e06613fd1a00747e8c26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package django-plugshop/0.4.0/django-plugshop-0.4.0.tar.gz

---
+++ packages/django-plugshop/django-plugshop-0.4.0.tar.gz/django-plugshop-0.4.0/plugshop/templatetags/plugshop.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+from django import template
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+class HasProduct(template.Node):
+    def __init__(self, parser, token):
+        try:
+            tag_name, product = token.split_contents()
+        except ValueError:
+            raise template.TemplateSyntaxError, "%r tag requires exactly one \
+argument" % token.contents.split()[0]
+
+        nodelist = parser.parse(('endplusghop_has_product',))
+        parser.delete_first_token()
+        
+        self.product = parser.compile_filter(product)
+        self.nodelist = nodelist
+
+    def render(self, context, *args, **kwargs):
+        product = self.product.resolve(context, True)
+        return self.nodelist.render(context)
+
+
+@register.tag
+def plusghop_has_product(parser, token, *args, **kwargs):
+    return HasProduct(parser, token)
+
+def plugshop_currency(value):
+    if value is None: 
+        return ""
+    v = str(value)[::-1]
+    return " ".join([v[i:i+3][::-1] for i in xrange(0, len(v), 3) ][::-1])
+register.filter('plugshop_currency', plugshop_currency)
+
+@register.inclusion_tag('plugshop/tags/action.html')
+def plugshop_action(product, action, quantity=1):
+    return {
+        'product': product,
+        'action': action,
+        'quantity': quantity,
+    }
---
+++ packages/django-plugshop/django-plugshop-0.4.0.tar.gz/django-plugshop-0.4.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+# python setup.py bdist_egg sdist upload
+
+setup(
+    author = "Eugene Cheltsov",
+    author_email = "chill.icp@gmail.com",
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    description = ("A small shop for Django Framework"),
+    download_url='',
+    include_package_data = True,
+    install_requires = (
+        'Django>=1.4',
+        'django-mptt>=0.5.2',
+    ),
+    keywords = "plugshop",
+    long_description = open(os.path.join(os.path.dirname(__file__), 
+                            'README.md')).read(),
+    license = "BSD",
+    name = "django-plugshop",
+    packages = find_packages(exclude=['testshop']),
+    platforms = ['OS Independent'],
+    url = "https://github.com/ChillyBwoy/django-plugshop",
+    version = '0.4.0',
+    zip_safe = False
+)
