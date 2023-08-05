b005a330aeb1583bb70742793410867a8f86742b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:58 +0000

    Add package MacPacbot/1.0.9/MacPacbot-1.0.9.tar.gz

---
+++ packages/MacPacbot/MacPacbot-1.0.9.tar.gz/MacPacbot-1.0.9/README.txt
@@ -0,0 +1,36 @@
+# MacPacbot
+
+MacPacbot is a Python toolkit of PAC(auto proxy configuration) for Mac OS X, which makes it much easier to create, edit and enable PAC script.
+It could transform a simple YAML config into a valid PAC script. 
+
+## Install
+
+    pip install MacPacbot
+
+## Usage
+
+Transform YAML info PAC, automatically detect network-service and update auto proxy configuration:
+
+    sudo pacbot <yaml_file> [-o <pac_file>]
+
+Update auto proxy configuration with an existing PAC file:
+
+    sudo pacbot -u <pac_file>
+
+Disable auto proxy in current network-service:
+
+    sudo pacbot -s off
+
+Vim plugin:
+    function! EnablePAC()
+        if &filetype == 'javascript'
+            !sudo pacbot -u %
+        elseif &filetype == 'yaml'
+            !sudo pacbot %
+        endif
+    endfunction
+
+    command! PACenable call EnablePAC() 
+    autocmd! bufwritepost *.pac :PACenable
+    autocmd! bufwritepost *.ypac :PACenable
+
---
+++ packages/MacPacbot/MacPacbot-1.0.9.tar.gz/MacPacbot-1.0.9/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'MacPacbot',
+    version = '1.0.9',
+    author = 'dexteryy',
+    author_email = 'dexter.yy@gmail.com',
+    url = 'http://dexteryy.github.com/MacPacbot',
+    description = 'MacPacbot is a Python toolkit of PAC(auto proxy configuration) for Mac OS X',
+    entry_points = {
+        "console_scripts": ['pacbot = MacPacbot.pacbot:main']
+    },
+    packages = ['MacPacbot'],
+    install_requires=[
+        "PyYAML",
+        "Mako"
+    ],
+    keywords = ["PAC", "proxy", "mac", "GFW"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: System :: Systems Administration"
+    ],
+    long_description = """\
+MacPacbot is a Python toolkit of PAC(auto proxy configuration) for Mac OS X, which makes it much easier to create, edit and enable PAC script.
+It could transform a simple YAML config into a valid PAC script. 
+
+## Usage
+
+Transform YAML info PAC, automatically detect network-service and update auto proxy configuration:
+
+    sudo pacbot <yaml_file> [-o <pac_file>]
+
+Update auto proxy configuration with an existing PAC file:
+
+    sudo pacbot -u <pac_file>
+
+Disable auto proxy in current network-service:
+
+    sudo pacbot -s off
+
+Vim plugin:
+
+    function! EnablePAC()
+        if &filetype == 'javascript'
+            !sudo pacbot -u %
+        elseif &filetype == 'yaml'
+            !sudo pacbot %
+        endif
+    endfunction
+
+    command! PACenable call EnablePAC() 
+    autocmd! bufwritepost *.pac :PACenable
+    autocmd! bufwritepost *.ypac :PACenable
+
+"""
+)
