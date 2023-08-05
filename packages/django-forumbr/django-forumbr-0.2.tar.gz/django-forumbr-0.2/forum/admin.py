7cb1267a529b6e674dd0d9636df4acd7d2fd2acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package django-forumbr/0.2/django-forumbr-0.2.tar.gz

---
+++ packages/django-forumbr/django-forumbr-0.2.tar.gz/django-forumbr-0.2/forum/admin.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+from django.contrib import admin
+
+from .forms import ProfileAdminForm
+from .models import Category, Forum, Thread, Reply, Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileAdminForm
+
+
+class ForumInlineModel(admin.StackedInline):
+    model = Forum
+    exclude = ('parent',)
+    max_num = 2
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+    inlines = [ForumInlineModel]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Forum)
+admin.site.register(Thread)
+admin.site.register(Reply)
+admin.site.register(Profile, ProfileAdmin)
