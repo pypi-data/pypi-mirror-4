db636b1a2fa2118aaa4f34d0a3d1094be8374b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:17 +0000

    Add package horus/0.9.13/horus-0.9.13.tar.gz

---
+++ packages/horus/horus-0.9.13.tar.gz/horus-0.9.13/horus/schemas.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+    unicode_literals)
+import colander
+import deform
+
+from hem.schemas import CSRFSchema
+
+
+class LoginSchema(CSRFSchema):
+    username = colander.SchemaNode(colander.String())
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=2),
+        widget=deform.widget.PasswordWidget()
+    )
+
+
+class RegisterSchema(CSRFSchema):
+    username = colander.SchemaNode(colander.String())
+    email = colander.SchemaNode(colander.String(), validator=colander.Email())
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=2),
+        widget=deform.widget.CheckedPasswordWidget()
+    )
+
+
+class ForgotPasswordSchema(CSRFSchema):
+    email = colander.SchemaNode(colander.String(), validator=colander.Email())
+
+
+class ResetPasswordSchema(CSRFSchema):
+    username = colander.SchemaNode(
+        colander.String(),
+        widget=deform.widget.TextInputWidget(template='readonly/textinput'),
+        missing=colander.null,
+    )
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=2),
+        widget=deform.widget.CheckedPasswordWidget()
+    )
+
+
+class ProfileSchema(CSRFSchema):
+    username = colander.SchemaNode(
+        colander.String(),
+        widget=deform.widget.TextInputWidget(template='readonly/textinput'),
+        missing=colander.null,
+    )
+    email = colander.SchemaNode(colander.String(), validator=colander.Email())
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=2),
+        widget=deform.widget.CheckedPasswordWidget(),
+        missing=colander.null
+    )
+
+
+class AdminUserSchema(CSRFSchema):
+    username = colander.SchemaNode(colander.String())
+    email = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Email()
+    )
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=2),
+        widget=deform.widget.CheckedPasswordWidget(),
+        missing=colander.null
+    )
