2c7c28a8d8a2e5d904270e5ef53434304dfbd6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:56 +0000

    Add package collective.cookiecuttr/0.6/collective.cookiecuttr-0.6.zip

---
+++ packages/collective.cookiecuttr/collective.cookiecuttr-0.6.zip/collective.cookiecuttr-0.6/src/collective/cookiecuttr/browser/viewlet.py
@@ -0,0 +1,73 @@
+from zope.interface import implements
+from zope.viewlet.interfaces import IViewlet
+
+from Products.Five.browser import BrowserView
+from Products.CMFPlone.utils import safe_unicode
+
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+from collective.cookiecuttr.interfaces import ICookieCuttrSettings
+from plone.app.layout.analytics.view import AnalyticsViewlet
+
+
+class CookieCuttrViewlet(BrowserView):
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        super(CookieCuttrViewlet, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+        registry = getUtility(IRegistry)
+        try:
+            self.settings = registry.forInterface(ICookieCuttrSettings)
+        except KeyError:
+            self.settings = None
+
+    def update(self):
+        pass
+
+    def available(self):
+        return self.settings and self.settings.cookiecuttr_enabled
+
+    def render(self):
+        if self.available():
+            snippet = safe_unicode(js_template % (self.settings.link,
+                                                  self.settings.text,
+                                                  self.settings.accept_button))
+            return snippet
+        return ""
+
+
+class CookieCuttrAwareAnalyticsViewlet(AnalyticsViewlet):
+
+    def render(self):
+        settings = getUtility(IRegistry).forInterface(ICookieCuttrSettings)
+        available = settings and settings.cookiecuttr_enabled
+
+        # Render if CookieCuttr is 'Not available' or Cookies were accepted
+        if not available or self.request.cookies.get('cc_cookie_accept', None):
+            return super(CookieCuttrAwareAnalyticsViewlet, self).render()
+
+        return ""
+
+js_template = """
+<script type="text/javascript">
+
+    (function($) {
+        $(document).ready(function () {
+            if($.cookieCuttr) {
+                $.cookieCuttr({cookieAnalytics: false,
+                               cookiePolicyLink: "%s",
+                               cookieMessage: "%s",
+                               cookieAcceptButtonText: "%s"
+                               });
+                }
+        })
+    })(jQuery);
+</script>
+
+"""
