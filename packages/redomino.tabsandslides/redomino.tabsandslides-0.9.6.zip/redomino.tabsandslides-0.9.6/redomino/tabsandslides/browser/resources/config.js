982ce053875b53410ea4d6c6bc0124e8ed869a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package redomino.tabsandslides/0.9.6/redomino.tabsandslides-0.9.6.zip

---
+++ packages/redomino.tabsandslides/redomino.tabsandslides-0.9.6.zip/redomino.tabsandslides-0.9.6/redomino/tabsandslides/browser/resources/config.js
@@ -0,0 +1,69 @@
+window.tabsandslides = {};
+
+(function (tabsandslides){
+
+
+
+//
+// GALLERY
+//
+tabsandslides.gallery = {};
+tabsandslides.gallery.config_boxscrollable = {scroll:1};
+//http://sorgalla.com/projects/jcarousel/
+
+//
+// IMAGE TABS
+//
+tabsandslides.image_tabs = {};
+tabsandslides.image_tabs.config = {};
+tabsandslides.image_tabs.config_boxscrollable = {scroll:1};
+//http://sorgalla.com/projects/jcarousel/
+
+//
+// ACCORDION
+//
+
+tabsandslides.accordion = {};
+tabsandslides.accordion.config = { collapsible: true, autoHeight: false };
+//http://api.jqueryui.com/1.8/accordion/
+
+//
+// TABS
+//
+tabsandslides.tabs = {};
+
+tabsandslides.tabs.config = {};
+
+//
+// SLIDESHOW
+//
+
+tabsandslides.slideshow = {};
+
+tabsandslides.slideshow.config = {
+    // enable "cross-fading" effect
+    effect: 'fade',
+    fadeOutSpeed: "slow",
+    rotate: true
+};
+
+tabsandslides.slideshow.slideshow_config = {clickable:false};
+
+
+//
+// SLIDESHOW
+//
+
+tabsandslides.slideshowpreview = {};
+
+tabsandslides.slideshowpreview.config = {
+    // enable "cross-fading" effect
+    effect: 'fade',
+    fadeOutSpeed: "slow",
+    rotate: true
+};
+
+tabsandslides.slideshowpreview.slideshow_config = {clickable:false};
+
+}(window.tabsandslides));
+
