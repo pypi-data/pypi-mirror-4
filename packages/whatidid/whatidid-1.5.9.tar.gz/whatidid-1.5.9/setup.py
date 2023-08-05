af90ff9a6bcbd644cfc68842971b096af1e87e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package whatidid/1.5.9/whatidid-1.5.9.tar.gz

---
+++ packages/whatidid/whatidid-1.5.9.tar.gz/whatidid-1.5.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='whatidid',
+      version='1.5.9',
+      description='A minimalist command line app for life logging.',
+      long_description='An application that helps you log your life, see the github page for more info.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+      ],
+      keywords='lifelogging whatidid',
+      url='https://github.com/zachseifts/whatidid',
+      author='Zach Seifts',
+      author_email='zach.seifts@gmail.com',
+      license='MIT',
+      packages=['whatidid'],
+      zip_safe=False,
+      scripts=['bin/wid', 'bin/wid-update-mail'])
+
