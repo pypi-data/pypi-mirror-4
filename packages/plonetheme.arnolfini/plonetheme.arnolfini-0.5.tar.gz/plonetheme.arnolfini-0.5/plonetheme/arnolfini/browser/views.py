b85d62363e1518d57835805c7343a96bfe22e0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:32 +0000

    Add package plonetheme.arnolfini/0.5/plonetheme.arnolfini-0.5.tar.gz

---
+++ packages/plonetheme.arnolfini/plonetheme.arnolfini-0.5.tar.gz/plonetheme.arnolfini-0.5/plonetheme/arnolfini/browser/views.py
@@ -0,0 +1,52 @@
+from plone.app.search.browser import Search
+from Products.Five import BrowserView
+from plone.app.layout.viewlets.common import FooterViewlet
+from AccessControl import getSecurityManager
+
+class CommonBrowserView(BrowserView):
+    """
+    Common utilities for all the other views
+    """
+    def slideshow(self, parent):
+        """
+        Creates a slideshow with the media from parent
+        """
+        parentURL = parent.absolute_url()
+        structure = """
+        <div class="embededMediaShow">
+            <a  href="%s?recursive=true">slideshow</a>
+        </div>
+        """%parentURL
+        
+        return structure
+
+class DocumentView(CommonBrowserView):
+    """'
+    Override of document view class.
+    """
+    
+class EventView(CommonBrowserView):
+    """'
+     Override of media event view class.
+    """
+    
+class FolderListing(CommonBrowserView):
+    """'
+    Override of folder_listing view
+    """
+    
+class SearchView(Search):
+    """
+    Adding to Search view
+    """
+
+class Footer(FooterViewlet):
+    """
+    helper classes for footer
+    """
+    def showManageButton(self):
+        secman = getSecurityManager()
+        if not secman.checkPermission('Portlets: Manage portlets', self.context):
+            return False
+        else:
+            return True
