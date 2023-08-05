624fe5274c092bfe360fbe4b574840b0a236aecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package django-live-profiler/0.0.1/django-live-profiler-0.0.1.tar.gz

---
+++ packages/django-live-profiler/django-live-profiler-0.0.1.tar.gz/django-live-profiler-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-live-profiler",
+    version = "0.0.1",
+    author = "Alexander Tereshkin",
+    author_email = "atereshkin@invitebox.com",
+    description = ("A database access profiler for Django-based applications that can be ran in production "
+                                   "with minimal performace overhead."),
+    license = "BSD",
+    keywords = "django profiler",
+    url = "https://github.com/InviteBox/django-live-profiler",
+    packages=['profiler',],
+    long_description=read('README.md'),
+    install_requires=('pymongo',),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",        
+    ],
+)
