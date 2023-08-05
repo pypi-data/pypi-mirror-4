ea8a98dcede8681e7c44ae47a8d21c302ccf54b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package tl/0.4.1/tl-0.4.1.tar.gz

---
+++ packages/tl/tl-0.4.1.tar.gz/tl-0.4.1/tl/version.py
@@ -0,0 +1,26 @@
+# tl/version.py
+#
+#
+
+""" version related stuff. """
+
+## tl imports
+
+from tl.lib.config import getmainconfig
+
+## basic imports
+
+import os
+import binascii
+
+## defines
+
+version = "0.4.1"
+__version__ = version
+
+## getversion function
+
+def getversion(txt=""):
+    """ return a version string. """
+    if txt: return "VERSION %s %s" % (version, txt)
+    else: return "VERSION %s" % version
