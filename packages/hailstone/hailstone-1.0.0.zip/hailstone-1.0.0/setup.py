3dfb57380a578c94c9562901fae344ee3355a1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package hailstone/1.0.0/hailstone-1.0.0.zip

---
+++ packages/hailstone/hailstone-1.0.0.zip/hailstone-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name        ='hailstone',
+    version     ='1.0.0',
+    py_modules  =['hailstone'],
+    author      ='try',
+    author_email='try@gmail.com',
+    url         ='try.com',
+    description ='just a try',
+    )
+    
