4df0036e01182d6feeba861d01d9801a1830ef8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:52 +0000

    Add package tryfer/0.2.2/tryfer-0.2.2.tar.gz

---
+++ packages/tryfer/tryfer-0.2.2.tar.gz/tryfer-0.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tryfer',
+    version='0.2.2',
+    description='Twisted Zipkin Tracing Library',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Twisted'
+    ],
+    maintainer='David Reid',
+    maintainer_email='david.reid@rackspace.com',
+    license='APL2',
+    url='https://github.com/racker/tryfer',
+    long_description=open('README.rst').read(),
+    packages=find_packages('.'),
+    install_requires=[
+        'Twisted >= 12.0.0',
+        'thrift == 0.8.0',
+        'scrivener == 0.2'
+    ],
+)
