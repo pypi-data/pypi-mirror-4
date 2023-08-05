b0d45b4bef537eb79d6acff2e972c08381766433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:34 +0000

    Add package django-db-readonly/0.3.0/django-db-readonly-0.3.0.tar.gz

---
+++ packages/django-db-readonly/django-db-readonly-0.3.0.tar.gz/django-db-readonly-0.3.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+version = '0.3.0'
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+        # Upgrade().run(dist=True)
+        # test.run(self, *args, **kwargs)
+
+setup(
+    name='django-db-readonly',
+    version=version,
+    author='Chris Streeter',
+    author_email='pypi@chrisstreeter.com',
+    url='http://github.com/streeter/django-db-readonly',
+    description='Add a global database read-only setting.',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+    ],
+    test_suite='readonly.tests',
+    include_package_data=True,
+    cmdclass={"test": mytest},
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
