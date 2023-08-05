6f2f326bcbc07f960b15fa6f5e744205d61e5e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:08 +0000

    Add package ccnmtldjango/0.17.3/ccnmtldjango-0.17.3.tar.gz

---
+++ packages/ccnmtldjango/ccnmtldjango-0.17.3.tar.gz/ccnmtldjango-0.17.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='ccnmtldjango',
+      version="0.17.3",
+      author="anders pearson",
+      description='Paste template for Django development at CCNMTL',
+      long_description=open('README.md').read(),
+      url='http://github.com/ccnmtl/ccnmtldjango/',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      install_requires = ["PasteScript"],
+      zip_safe=False,
+      package_data = {'' : ['*.*']},
+      include_package_data=True,
+      entry_points="""
+      [paste.paster_create_template]
+      ccnmtldjango = ccnmtldjango:CcnmtlDjangoTemplate
+      """
+      )
+	
