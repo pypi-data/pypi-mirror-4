81751d14b4dc920a8f8e21ec95efea24185fd280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:29 +0000

    Add package ll-xist/4.5/ll_xist-4.5-py3.3-macosx-10.7-x86_64.egg

---
+++ packages/ll-xist/ll_xist-4.5-py3.3-macosx-10.7-x86_64.egg/EGG-INFO/scripts/db2ul4.py
@@ -0,0 +1,9 @@
+#!/Users/walter/pyvenvs/default/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.scripts import db2ul4
+	sys.exit(db2ul4.main())
---
+++ packages/ll-xist/ll_xist-4.5-py3.3-macosx-10.7-x86_64.egg/EGG-INFO/scripts/uhpp.py
@@ -0,0 +1,9 @@
+#!/Users/walter/pyvenvs/default/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.xist.scripts import uhpp
+	sys.exit(uhpp.main())
