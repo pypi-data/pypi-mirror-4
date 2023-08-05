c670c9c79dddc74da62c56fc9b886d422570d30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:06 +0000

    Add package txmetrics/0.1/txmetrics-0.1.tar.gz

---
+++ packages/txmetrics/txmetrics-0.1.tar.gz/txmetrics-0.1/setup.py
@@ -0,0 +1,23 @@
+import sys, os, os.path, subprocess
+from setuptools.command import easy_install
+import pkg_resources as pkgrsrc
+
+from setuptools import setup
+from distutils import log
+log.set_threshold(log.INFO)
+
+setup(
+        name            = "txmetrics",
+        version         = "0.1",
+
+        packages        =   ['txmetrics'],
+        install_requires = ['txredisapi'],
+
+        # metadata for upload to PyPI
+        author          = "Gleicon Moraes",
+        author_email    = "gleicon@gmail.com",
+        keywords        = "twisted metrics redis",
+        description     = "simple python metrics for twisted",
+        url             = "https://github.com/gleicon/txmetrics",
+    )
+
