7f2a6746677dfa3e662fa4e1b958c681aee1b5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package django-quicky/0.4/django-quicky-0.4.tar.gz

---
+++ packages/django-quicky/django-quicky-0.4.tar.gz/django-quicky-0.4/django_quicky/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+
+__VERSION__ = "0.4"
+
+from decorators import view, routing
+from utils import get_object_or_None, setting
