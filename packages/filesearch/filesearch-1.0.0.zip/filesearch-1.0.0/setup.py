d08fd44d885faa300356a404d9e9ae735eda65b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package filesearch/1.0.0/filesearch-1.0.0.zip

---
+++ packages/filesearch/filesearch-1.0.0.zip/filesearch-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='filesearch',
+      description='a little project for searching filenames from a given path',
+      version='1.0.0',
+      author='guzhen',
+      author_email='604608916@qq.com'
+      )
