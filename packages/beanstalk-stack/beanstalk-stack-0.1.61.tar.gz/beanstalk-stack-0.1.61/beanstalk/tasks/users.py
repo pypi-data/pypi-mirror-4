2adddc1ff479698fecb1800cf66d36f7a4f470ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:17 +0000

    Add package beanstalk-stack/0.1.61/beanstalk-stack-0.1.61.tar.gz

---
+++ packages/beanstalk-stack/beanstalk-stack-0.1.61.tar.gz/beanstalk-stack-0.1.61/beanstalk/tasks/users.py
@@ -0,0 +1,39 @@
+import hashlib
+from fabric.api import *
+from ground_soil.filesystem import rsync
+from beanstalk.decorators import beanstalk_role
+from beanstalk.tasks.utils import load_role_settings as load_beanstalk_settings, load_web_servers
+from beanstalk.console.titles import tool_title
+from beanstalk.validator import validate_file_existence
+
+
+@task
+@beanstalk_role('app')
+def add(**settings_patches):
+    """Add current user to beanstalk-stack server
+    """
+    # Load settings
+    load_beanstalk_settings(**settings_patches)
+    load_web_servers()
+
+    print tool_title('Beanstalk-Stack users tool')
+    print ''
+
+    # Get ssh public key
+    ssh_pub_key_path = prompt('Where\'s your ssh public key?', default='~/.ssh/id_rsa.pub',
+                              validate=validate_file_existence)
+    with open(ssh_pub_key_path, 'r') as f:
+        key_content = f.read().strip()
+    hash_key = hashlib.md5(key_content).hexdigest()[:16]
+    tmp_path = '/tmp/beanstalk-stack.{hash}.pub.key'.format(hash=hash_key)
+
+    @roles('web_servers')
+    def add_user_core():
+        host_name = env.host_string.split('@')[-1]
+
+        local(rsync(ssh_pub_key_path, 'beanstalk@{host}:{key_path}'.format(host=host_name, key_path=tmp_path)))
+        run('bsjack server.add_user:{key_path},clean_tmp_key=True'.format(key_path=tmp_path))
+
+    with settings(hide('running')):
+        # TODO: Should not prompt beanstalk's SSH password
+        execute(add_user_core)
