98ba88d43c6a88c6b84cc4c508a52c5cb949c9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package github3.py/0.1/github3.py-0.1.tar.gz

---
+++ packages/github3.py/github3.py-0.1.tar.gz/github3.py-0.1/AUTHORS.rst
@@ -0,0 +1,31 @@
+Development Lead
+----------------
+
+- Ian Cordasco <graffatcolmingov@gmail.com>
+
+Requests
+````````
+
+- Kenneth Reitz <me@kennethreitz.com>
+
+Design Advice
+-------------
+
+- Kenneth Reitz
+
+Contributors
+------------
+
+- Dominik Dabrowski (@doda)
+
+- Dave Lacey (@davelxmos)
+
+- James Sullivan (@jsullivanlive)
+
+- Dennis Kaarsemaker (@seveas)
+
+- Hong Minhee (@dahlia)
+
+- Paul Swartz (@paulswartz)
+
+- Kristian Glass (@doismellburning)
---
+++ packages/github3.py/github3.py-0.1.tar.gz/github3.py-0.1/github3/decorators.py
@@ -0,0 +1,65 @@
+"""
+github3.decorators
+==================
+
+This module provides decorators to the rest of the library
+
+"""
+
+from functools import wraps
+from requests.models import Response
+import os
+
+try:  # (No coverage)
+    # python2
+    from StringIO import StringIO  # (No coverage)
+except ImportError:  # (No coverage)
+    # python3
+    from io import BytesIO as StringIO  # NOQA
+
+
+def requires_auth(func):
+    """Decorator to note which object methods require authorization."""
+    @wraps(func)
+    def auth_wrapper(self, *args, **kwargs):
+        auth = False
+        if hasattr(self, '_session'):
+            auth = (self._session.auth or
+                    self._session.headers.get('Authorization'))
+
+        if auth:
+            return func(self, *args, **kwargs)
+        else:
+            from github3.models import GitHubError
+            # Mock a 401 response
+            r = Response()
+            r.status_code = 401
+            r.encoding = 'utf-8'
+            r.raw = StringIO('{"message": "Requires authentication"}'.encode())
+            raise GitHubError(r)
+    return auth_wrapper
+
+
+def requires_basic_auth(func):
+    """Decorator to note which object methods require username/password
+    authorization and won't work with token based authorization."""
+    @wraps(func)
+    def auth_wrapper(self, *args, **kwargs):
+        if hasattr(self, '_session') and self._session.auth:
+            return func(self, *args, **kwargs)
+        else:
+            from github3.models import GitHubError
+            # Mock a 401 response
+            r = Response()
+            r.status_code = 401
+            r.encoding = 'utf-8'
+            msg = ('{"message": "Requires username/password '
+                   'authentication"}').encode()
+            r.raw = StringIO(msg)
+            raise GitHubError(r)
+    return auth_wrapper
+
+# Use mock decorators when generating documentation, so all functino signatures
+# are displayed correctly
+if os.environ.get('GENERATING_DOCUMENTATION', None) == 'github3':
+    requires_auth = requires_basic_auth = lambda x: x
