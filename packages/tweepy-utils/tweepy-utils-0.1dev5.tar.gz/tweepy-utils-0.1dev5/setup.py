3acca34176a6e9075d042df23d16571d1b3d852d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package tweepy-utils/0.1dev5/tweepy-utils-0.1dev5.tar.gz

---
+++ packages/tweepy-utils/tweepy-utils-0.1dev5.tar.gz/tweepy-utils-0.1dev5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tweepy-utils',
+    version='0.1dev5',
+    description='A set of Python utility scripts for Twitter account management, using Tweepy.',
+    author='Audrey Roy',
+    author_email='audreyr@cartwheelweb.com',
+    url='https://github.com/audreyr/tweepy-utils/',
+    # packages=['tweepy_utils',],
+    license='MIT license',
+    scripts=['scripts/list-nonfollowers.py', 'scripts/unfollow-nonfollowers.py'],
+    # long_description=open('README.md').read(),
+)
