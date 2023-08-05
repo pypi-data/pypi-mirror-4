65b15b8d0c1722b922ea821c7fdb85ba48472bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:51 +0000

    Add package zopyx.convert2/2.4.4/zopyx.convert2-2.4.4.zip

---
+++ packages/zopyx.convert2/zopyx.convert2-2.4.4.zip/zopyx.convert2-2.4.4/src/zopyx/convert2/logger.py
@@ -0,0 +1,10 @@
+##########################################################################
+# zopyx.convert2 - SmartPrintNG low-level functionality
+#
+# (C) 2007, 2008, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+import logging
+
+LOG = logging.getLogger('zopyx.convert2')
