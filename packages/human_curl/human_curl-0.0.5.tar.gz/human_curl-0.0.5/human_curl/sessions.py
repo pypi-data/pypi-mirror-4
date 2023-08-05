5d12d07700e999d6974b74275c496d5c78787484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package human_curl/0.0.5/human_curl-0.0.5.tar.gz

---
+++ packages/human_curl/human_curl-0.0.5.tar.gz/human_curl-0.0.5/human_curl/sessions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+human_curl.sessions
+~~~~~~~~~~~~~~~~~~~
+
+Human curl module to store and manage sessions
+
+:copyright: (c) 2012 by Alexandr Lispython (alex@obout.ru).
+:license: BSD, see LICENSE for more details.
+"""
+
+class Session(object):
+    """Human curl sessions store
+    """
+    def __init__(self,*args,**kwargs):
+        pass
+
+
+def session(**kwargs):
+    return Session(**kwargs)
