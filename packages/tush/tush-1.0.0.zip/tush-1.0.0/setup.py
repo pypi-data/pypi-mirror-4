dd8adc6f6b7f28842c8cbac2777ed062ffe0a3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package tush/1.0.0/tush-1.0.0.zip

---
+++ packages/tush/tush-1.0.0.zip/tush-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        ='tush',
+        version     ='1.0.0',
+        py_modules  =['nester'],
+        author      ='tushar',
+        author_email='patil.tushar04@gmail.com',
+        url         ='patiltushar04.blogspot.in',
+        description ='A simple printer',
+     )
