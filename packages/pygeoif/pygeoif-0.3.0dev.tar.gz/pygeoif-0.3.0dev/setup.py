d4ef35c1826ade40373ed248577a93f6d57db9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package pygeoif/0.3.0dev/pygeoif-0.3.0dev.tar.gz

---
+++ packages/pygeoif/pygeoif-0.3.0dev.tar.gz/pygeoif-0.3.0dev/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3.0'
+
+setup(name='pygeoif',
+      version=version,
+      description="A basic implementation of the __geo_interface__",
+            long_description=open(
+              "README.rst").read() + "\n" +
+              open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='GIS Spatial WKT',
+      author='Christian Ledermann',
+      author_email='christian.ledermann@gmail.com',
+      url='https://github.com/cleder/pygeoif/',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
