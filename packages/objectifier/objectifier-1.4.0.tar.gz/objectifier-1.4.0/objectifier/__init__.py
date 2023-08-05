5e3df74bfae1dd0d62c7a28d5dd75a0064971e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package objectifier/1.4.0/objectifier-1.4.0.tar.gz

---
+++ packages/objectifier/objectifier-1.4.0.tar.gz/objectifier-1.4.0/objectifier/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+objectifier
+~~~~~~~~~~~
+
+:copyright: (c) 2012, Elm City Labs LLC
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+from .objectifier import Objectifier
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+__all__ = [
+    '__author__', '__copyright__', '__email__', '__license__',
+    '__maintainer__', '__version__', 'Objectifier',
+]
+
