f4f234155b3c7188dd5aa1599822eb104de2a663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package django-celery-transactions/0.1.3/django-celery-transactions-0.1.3.tar.gz

---
+++ packages/django-celery-transactions/django-celery-transactions-0.1.3.tar.gz/django-celery-transactions-0.1.3/setup.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-celery-transactions",
+    version="0.1.3",
+    description="Django transaction support for Celery tasks.",
+    long_description="See https://github.com/chrisdoble/django-celery-transactions",
+    author="Chris Doble",
+    author_email="chris@chrisdoble.com",
+    url="https://github.com/chrisdoble/django-celery-transactions",
+    license="Simplified BSD",
+    packages=["djcelery_transactions"],
+    install_requires=[
+        "celery>=2.2.7",
+        "Django>=1.2.4",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Database",
+    ],
+)
