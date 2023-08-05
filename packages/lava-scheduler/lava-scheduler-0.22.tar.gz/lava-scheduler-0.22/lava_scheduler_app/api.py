741f9808c23429ad119386eed41c43460e869b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:37 +0000

    Add package lava-scheduler/0.22/lava-scheduler-0.22.tar.gz

---
+++ packages/lava-scheduler/lava-scheduler-0.22.tar.gz/lava-scheduler-0.22/lava_scheduler_app/api.py
@@ -0,0 +1,53 @@
+import xmlrpclib
+
+from simplejson import JSONDecodeError
+
+from linaro_django_xmlrpc.models import ExposedAPI
+
+from lava_scheduler_app.models import (
+    Device,
+    DeviceType,
+    JSONDataError,
+    TestJob,
+    )
+
+
+class SchedulerAPI(ExposedAPI):
+
+    def submit_job(self, job_data):
+        if not self.user:
+            raise xmlrpclib.Fault(
+                401, "Authentication with user and token required for this "
+                "API.")
+        if not self.user.has_perm('lava_scheduler_app.add_testjob'):
+            raise xmlrpclib.Fault(
+                403, "Permission denied.  User %r does not have the "
+                "'lava_scheduler_app.add_testjob' permission.  Contact "
+                "the administrators." % self.user.username)
+        try:
+            job = TestJob.from_json_and_user(job_data, self.user)
+        except JSONDecodeError as e:
+            raise xmlrpclib.Fault(400, "Decoding JSON failed: %s." % e)
+        except (JSONDataError, ValueError) as e:
+            raise xmlrpclib.Fault(400, str(e))
+        except Device.DoesNotExist:
+            raise xmlrpclib.Fault(404, "Specified device not found.")
+        except DeviceType.DoesNotExist:
+            raise xmlrpclib.Fault(404, "Specified device type not found.")
+        return job.id
+
+    def resubmit_job(self, job_id):
+        try:
+            job = TestJob.objects.accessible_by_principal(self.user).get(pk=job_id)
+        except TestJob.DoesNotExist:
+            raise xmlrpclib.Fault(404, "Specified job not found.")
+        return self.submit_job(job.definition)
+
+    def cancel_job(self, job_id):
+        if not self.user:
+            raise xmlrpclib.Fault(401, "Authentication required.")
+        job = TestJob.objects.get(pk=job_id)
+        if not job.can_cancel(self.user):
+            raise xmlrpclib.Fault(403, "Permission denied.")
+        job.cancel()
+        return True
