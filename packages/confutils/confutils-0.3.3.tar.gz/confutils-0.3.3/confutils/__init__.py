8fa0eae2a3d500040d29c20f4f3c504ba7e588e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:15 +0000

    Add package confutils/0.3.3/confutils-0.3.3.tar.gz

---
+++ packages/confutils/confutils-0.3.3.tar.gz/confutils-0.3.3/confutils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# This code is distributed under the LGPLv3+ license.
+# Copyright (c) 2012 Raphaël Barrois
+
+from __future__ import unicode_literals
+
+__author__ = "Raphaël Barrois <raphael.barrois+confutils@polytechnique.org>"
+__version__ = '0.3.3'
+
+from .configfile import ConfigFile, ConfigLine, Parser
+from .configfile import ConfigError, ConfigReadingError, ConfigWritingError
+from .merged_config import Default, NoDefault
+from .merged_config import NormalizedDict, DictNamespace, MergedConfig
