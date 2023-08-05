9e13ee2175ce2d082138c8df64c3efb70b72ad28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package wheezy.web/0.1.325/wheezy.web-0.1.325.tar.gz

---
+++ packages/wheezy.web/wheezy.web-0.1.325.tar.gz/wheezy.web-0.1.325/src/wheezy/web/__init__.py
@@ -0,0 +1,9 @@
+
+""" ``web`` package.
+"""
+
+# flake8: noqa
+
+from wheezy.web.authorization import authorize
+from wheezy.web.authorization import secure
+from wheezy.web.caching import handler_cache
