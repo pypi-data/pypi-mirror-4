bcea11fdb753b81436ff053b8387fac66e991184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package nsnitro/1.0.10/nsnitro-1.0.10.tar.gz

---
+++ packages/nsnitro/nsnitro-1.0.10.tar.gz/nsnitro-1.0.10/nsnitro/__init__.py
@@ -0,0 +1,5 @@
+from nsnitro import NSNitro
+from nsutil import NSNitroError, NSNitroResponse
+from nsresources import *
+__all__ = ['NSNitro', 'NSNitroError', 'NSNitroResponse' ] + nsresources.__all__
+__import__("pkg_resources").declare_namespace(__name__)
