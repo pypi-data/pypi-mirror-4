3e85b00823545cde4069543fcf4d6b75d16ba8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package ModelValidations/0.4dev/ModelValidations-0.4dev.tar.gz

---
+++ packages/ModelValidations/ModelValidations-0.4dev.tar.gz/ModelValidations-0.4dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ModelValidations',
+    version='0.4dev',
+    packages=['modelvalidations'],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
