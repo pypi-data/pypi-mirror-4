51a01b303a775db96f7f86f612715b8809217121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:30 +0000

    Add package django-babylon/0.1.12/django-babylon-0.1.12.tar.gz

---
+++ packages/django-babylon/django-babylon-0.1.12.tar.gz/django-babylon-0.1.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.12'
+APPLICATION = 'django-babylon'
+
+setup(
+    name=APPLICATION,
+    version=VERSION,
+    description='Django infinite caching for mandems.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/radiosilence/django-babylon',
+    author='James Cleveland',
+    author_email='jamescleveland@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license="LICENSE.txt",
+    install_requires=open('requirements.txt').read().split("\n")
+)
