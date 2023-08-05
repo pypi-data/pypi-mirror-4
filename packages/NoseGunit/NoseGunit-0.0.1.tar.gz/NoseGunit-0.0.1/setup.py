755e221618666d3648c40e83d0327c506ef0caa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:33 +0000

    Add package NoseGunit/0.0.1/NoseGunit-0.0.1.tar.gz

---
+++ packages/NoseGunit/NoseGunit-0.0.1.tar.gz/NoseGunit-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup(
+    name="NoseGunit",
+    version=VERSION,
+    author='Beau Lyddon',
+    author_email='lyddonb@gmail.com',
+    maintainer='Beau Lyddon',
+    maintainer_email='lyddonb@gmail.com',
+    descrption='nose plugin for bootstrapping the GAE SDK and GAE Testbed',
+    url='https://github.com/beaulyddon-wf/nose-gunit',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        ],
+    entry_points= {
+        'nose.plugins.0.10':  [
+            'nosegunit = nosegunit:NoseGunit'
+            ]
+    },
+    py_modules=['nosegunit'],
+    install_requires=['nose>=0.10.1'],
+)
