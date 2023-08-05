d1ada550067b5872a9aea33fa6a4c294a62c0651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:26 +0000

    Add package trac-MultiRepoSearchPlugin/0.3/trac-MultiRepoSearchPlugin-0.3.tar.gz

---
+++ packages/trac-MultiRepoSearchPlugin/trac-MultiRepoSearchPlugin-0.3.tar.gz/trac-MultiRepoSearchPlugin-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+try:
+    long_description = open("README.txt").read()
+except:
+    long_description = ''
+try:
+    long_description += open("CHANGES.txt").read()
+except:
+    pass
+
+setup(name='trac-MultiRepoSearchPlugin',
+      version='0.3',
+      description="Search the text of source code in your Trac repositories (0.12 and up)",
+      long_description=long_description,
+      packages=['multireposearch'],
+      author='Ethan Jucovy',
+      author_email='ejucovy@gmail.com',
+      url="http://trac-hacks.org/wiki/MultiRepoSearchPlugin",
+      install_requires=["tracsqlhelper"],
+      license='BSD',
+      entry_points = {'trac.plugins': ['multireposearch = multireposearch']})
