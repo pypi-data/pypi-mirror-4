4750b3146a28e82c8abab708446d346df55876a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package m01.gmap/0.5.0/m01.gmap-0.5.0.zip

---
+++ packages/m01.gmap/m01.gmap-0.5.0.zip/m01.gmap-0.5.0/src/m01/gmap/tests.py
@@ -0,0 +1,32 @@
+###############################################################################
+#
+# Copyright (c) 2011 Projekt01 GmbH and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+###############################################################################
+"""
+$Id:$
+"""
+__docformat__ = "reStructuredText"
+
+import doctest
+import unittest
+from zope.testing.doctestunit import DocFileSuite
+
+from z3c.form import testing
+
+
+def test_suite():
+    return unittest.TestSuite((
+        DocFileSuite('README.txt',
+            setUp=testing.setUp, tearDown=testing.tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        ))
