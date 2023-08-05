31b739bc605f6a4b25ad4f5c8ed612e233e9fbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package infi.jira_cli/0.0.2/infi.jira_cli-0.0.2.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.0.2.tar.gz/infi.jira_cli-0.0.2/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2c8f160ceeaacf13fec294e5430adbe4a7ea8421'
+__git_head_subject__ = 'Finished release v0.0.2.'
+__git_head_message__ = '* release/v0.0.2: (23 commits)\n  HOSTDEV-588\n  HOSTDEV-584 unicode conversion\n  HOSTDEV-576 simple print\n  HOSTDEV-575 added a space to make the split work\n  HOSTDEV-573 trivial indentation fixes\n  HOSTDEV-563 jissue show unicode fix\n  HOSTDEV-558 trivial fix\n  HOSTDEV-556 fix\n  trivial fix\n  trivial fix\n  trivial fix\n  HOSTDEV-556 jissue create --short implementation\n  HOSTDEV-553 updated documentation\n  HOSTDEV-553 jissue resolve --commit=<commit>\n  TRIVIAL minor refactoring around issue_mappings\n  TRIVIAL more case-insensitivity support in component, issue-type, resolve-as, link-type\n  TRIVIAL <target-issue> is now case-insensitive\n  TRIVIAL message in resolve and link is now optional\n  TRIVIAL updated usage section in README\n  TRIVIAL show now prints sub-tasks and links\n  ...'
+__git_dirty_diff__ = ''
