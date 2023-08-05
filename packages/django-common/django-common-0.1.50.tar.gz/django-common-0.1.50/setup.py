fb893c61cf0285a2fcb48a457698e4cd0769c9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:52 +0000

    Add package django-common/0.1.50/django-common-0.1.50.tar.gz

---
+++ packages/django-common/django-common-0.1.50.tar.gz/django-common-0.1.50/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-common',
+    description = 'A number of useful django shortcuts and helpers',
+    version = '0.1.50',
+    author = 'Grigoriy Petukhov',
+    author_email = 'lorien@lorien.name',
+    url = 'http://bitbucket.org/lorien/django-common/',
+
+    packages = find_packages(),
+    include_package_data = True,
+
+    license = "BSD",
+    keywords = "django application development shortcuts helpers",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
