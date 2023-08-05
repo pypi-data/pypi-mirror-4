2d62b6c865697bc1bbcbfdba85bd0d227ab581c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:31 +0000

    Add package ground-soil/0.1.2/ground-soil-0.1.2.tar.gz

---
+++ packages/ground-soil/ground-soil-0.1.2.tar.gz/ground-soil-0.1.2/ground_soil/datastucture/__init__.py
@@ -0,0 +1,8 @@
+from ground_soil.datastucture.settings_dict import SettingsDict
+from ground_soil.datastucture.modules import load_module_as_dict, dict_from_module_attr
+
+__all__ = [
+    SettingsDict.__name__,
+    load_module_as_dict.__name__,
+    dict_from_module_attr.__name__,
+]
---
+++ packages/ground-soil/ground-soil-0.1.2.tar.gz/ground-soil-0.1.2/ground_soil/fabric/__init__.py
@@ -0,0 +1,32 @@
+import ast
+import json
+from fabric.api import local, prefix
+
+
+def lsudo(command, capture=False):
+    return local('sudo %s' % command, capture=capture)
+
+
+def virtual_env(venv_path):
+    return prefix('source %s/bin/activate' % venv_path)
+
+
+def eval_arg(raw_value, is_json=False):
+    if is_json:
+        try:
+            return json.loads(raw_value)
+        except ValueError:
+            return raw_value
+    else:
+        try:
+            return ast.literal_eval(raw_value)
+        except (SyntaxError, ValueError):
+            return raw_value
+
+
+def eval_args(args):
+    return map(eval_arg, args)
+
+
+def eval_kwargs(kwargs):
+    return dict(map(lambda x: (x[0], eval_arg(x[1])), kwargs.items()))
