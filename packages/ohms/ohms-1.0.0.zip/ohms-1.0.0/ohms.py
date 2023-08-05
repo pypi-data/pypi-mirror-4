f00461fd4039bd0c13fc128bf8058fafae76a36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package ohms/1.0.0/ohms-1.0.0.zip

---
+++ packages/ohms/ohms-1.0.0.zip/ohms-1.0.0/ohms.py
@@ -0,0 +1,7 @@
+"""Program to calculate resistance in ohms given a voltage and current"""
+
+import math
+
+def ohmslaw(v, i):
+    return v * i
+
---
+++ packages/ohms/ohms-1.0.0.zip/ohms-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name            = 'ohms',
+        version         = '1.0.0',
+        py_modules      =['ohms'],
+        author          ='bbailey',
+        description     ='simple resistance calculator',
+        )
