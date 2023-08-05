5e366dc2de1490c3306e7e589e87838f1e2ae9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:57 +0000

    Add package infi.hbaapi/0.1.19/infi.hbaapi-0.1.19-py2.7.egg

---
+++ packages/infi.hbaapi/infi.hbaapi-0.1.19-py2.7.egg/infi/hbaapi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.19"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '399f72e9b217c1d0859c1ef2886d303fa7987739'
+__git_head_subject__ = 'Finished release v0.1.19.'
+__git_head_message__ = '# By Arnon Yaari (1) and Guy Rozendorn (1)\n* release/v0.1.19:\n  TRIVIAL skeleton update\n  empty commit after version v0.1.18'
+__git_dirty_diff__ = ''
