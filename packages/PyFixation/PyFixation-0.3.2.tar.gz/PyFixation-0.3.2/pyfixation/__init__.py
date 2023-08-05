2f7ebdcc8898053ccfc5cf30875851f4a82c2ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package PyFixation/0.3.2/PyFixation-0.3.2.tar.gz

---
+++ packages/PyFixation/PyFixation-0.3.2.tar.gz/PyFixation-0.3.2/pyfixation/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding:    utf-8 -*-
+#===============================================================================
+# This file is part of PyFixation.
+# Copyright (C) 2012 Ryan Hope <rmh3093@gmail.com>
+#
+# PyViewX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyViewX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyViewX.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+"""A library for classifying raw eye gaze data into discrete events like saccades and fixations.
+
+.. moduleauthor:: Ryan Hope <rmh3093@gmail.com>
+"""
+
+from velocity import VelocityFP
+from dispersion import DispersionFP
+
+__version__ = '0.3.2'
