37b7bb7cb2ca6afec8e57f1b38d645b34de7fbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package croniter/0.3.1/croniter-0.3.1.tar.gz

---
+++ packages/croniter/croniter-0.3.1.tar.gz/croniter-0.3.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+__version__, __doc__, __license__, __author__ = None, None, None, None
+# get __version__, __doc__, __license__, __author__
+execfile("croniter/_release.py")
+setup(
+    packages         = ('croniter',),
+    name             = 'croniter',
+    version          = __version__,
+    py_modules       = ['croniter', 'croniter_test'],
+    description      = 'croniter provides iteration for datetime object with cron like format',
+    long_description = __doc__,
+    author           = __author__,
+    author_email     = 'taichino@gmail.com',
+    url              = 'http://github.com/taichino/croniter',
+    keywords         = 'datetime, iterator, cron',
+    install_requires = ["python-dateutil", "setuptools",],
+    license          = __license__,
+    classifiers      = ["Development Status :: 4 - Beta",
+                        "Intended Audience :: Developers",
+                        "License :: OSI Approved :: MIT License",
+                        "Operating System :: POSIX",
+                        "Programming Language :: Python",
+                        "Topic :: Software Development :: Libraries :: Python Modules"]
+)
