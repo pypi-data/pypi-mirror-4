e0b0f5c3e004fcf3fb7b985cb8f033c54cb6d9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package sse/1.1/sse-1.1.tar.gz

---
+++ packages/sse/sse-1.1.tar.gz/sse-1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "sse",
+    url = "https://github.com/niwibe/sse",
+    author = "Andrei Antoukh",
+    author_email = "niwi@niwi.be",
+    version="1.1",
+    description = "Server-Sent Events protocol implemetation.",
+    py_modules = ['sse'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
