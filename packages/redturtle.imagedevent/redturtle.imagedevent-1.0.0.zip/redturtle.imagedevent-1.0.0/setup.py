eff108610437611170458cd2b2c171d40dbff72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:47 +0000

    Add package redturtle.imagedevent/1.0.0/redturtle.imagedevent-1.0.0.zip

---
+++ packages/redturtle.imagedevent/redturtle.imagedevent-1.0.0.zip/redturtle.imagedevent-1.0.0/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+install_requires = ['setuptools',
+                    'archetypes.schemaextender']
+
+if sys.version_info < (2, 6):
+    install_requires.append('experimental.backportGS')
+
+tests_require=['zope.testing']
+
+setup(name='redturtle.imagedevent',
+      version=version,
+      description="A replacement of the Event content type for Plone, with image field and "
+                  "separate keyword and event type fields.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 3.3',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='plone event image content plonegov',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.org/products/redturtle.imagedevent',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'redturtle.imagedevent.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
