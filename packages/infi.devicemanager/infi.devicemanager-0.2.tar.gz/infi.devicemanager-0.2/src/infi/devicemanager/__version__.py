4be06e56654037fed4dec3e11fc00409cf9a98d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:55 +0000

    Add package infi.devicemanager/0.2/infi.devicemanager-0.2.tar.gz

---
+++ packages/infi.devicemanager/infi.devicemanager-0.2.tar.gz/infi.devicemanager-0.2/src/infi/devicemanager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '9aa9d04cb52df7c4e28b6efc3b24c52d8368da3d'
+__git_head_subject__ = 'Finished release v0.2.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
