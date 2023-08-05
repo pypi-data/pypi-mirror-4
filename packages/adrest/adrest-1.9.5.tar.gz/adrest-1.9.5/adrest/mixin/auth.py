b6436954671e7856a6559320353393cda008b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package adrest/1.9.5/adrest-1.9.5.tar.gz

---
+++ packages/adrest/adrest-1.9.5.tar.gz/adrest-1.9.5/adrest/mixin/auth.py
@@ -0,0 +1,61 @@
+from ..settings import ALLOW_OPTIONS
+from ..utils import status
+from ..utils.auth import AnonimousAuthenticator, AbstractAuthenticator
+from ..utils.exceptions import HttpError
+from ..utils.tools import as_tuple
+
+
+def check_authenticators(authenticators):
+    authenticators = as_tuple(authenticators)
+    for a in authenticators:
+        assert issubclass(a, AbstractAuthenticator), "Authenticators must be subclasses of AbstractAuthenticator"
+    return authenticators
+
+
+class AuthMeta(type):
+
+    def __new__(mcs, name, bases, params):
+        cls = super(AuthMeta, mcs).__new__(mcs, name, bases, params)
+        cls.authenticators = check_authenticators(cls.authenticators)
+        return cls
+
+
+class AuthMixin(object):
+    " Adds pluggable authentication behaviour "
+
+    __metaclass__ = AuthMeta
+
+    authenticators = AnonimousAuthenticator
+    auth = None
+    identifier = ''
+
+    def authenticate(self, request):
+        """ Attempt to authenticate the request, returning an authentication context or None.
+            An authentication context may be any object, although in many cases it will simply be a :class:`User` instance.
+        """
+        authenticators = self.authenticators
+
+        if request.method == 'OPTIONS' and ALLOW_OPTIONS:
+            authenticators = AnonimousAuthenticator,
+
+        error_message = "Authorization required."
+        for authenticator in authenticators:
+            auth = authenticator(self)
+            try:
+                assert auth.authenticate(request), error_message
+                auth.configure(request)
+                return True
+            except AssertionError, e:
+                error_message = str(e)
+
+        raise HttpError(error_message, status=status.HTTP_401_UNAUTHORIZED)
+
+    def check_rights(self, resources, request=None):
+        " Check rights of client for queried resources "
+        if not self.auth:
+            return True
+
+        try:
+            assert self.auth.test_rights(resources, request=request)
+        except AssertionError, e:
+            raise HttpError("Access forbidden. %s" % str(e), status=status.HTTP_403_FORBIDDEN)
---
+++ packages/adrest/adrest-1.9.5.tar.gz/adrest-1.9.5/adrest/settings.py
@@ -0,0 +1,33 @@
+" Adrest API settings. "
+try:
+    from django.conf import settings
+
+    getattr(settings, 'DEBUG')
+
+except ImportError:
+
+    settings.configure()
+
+from .utils.tools import as_tuple
+
+
+# Enable Adrest API logs
+ACCESS_LOG = getattr(settings, 'ADREST_ACCESS_LOG', False)
+
+# Auto create adrest access-key for created user
+ACCESSKEY = getattr(settings, 'ADREST_ACCESSKEY', 'django.contrib.auth' in settings.INSTALLED_APPS)
+AUTO_CREATE_ACCESSKEY = getattr(settings, 'ADREST_AUTO_CREATE_ACCESSKEY', False)
+
+# Max resources per page in list views
+LIMIT_PER_PAGE = int(getattr(settings, 'ADREST_LIMIT_PER_PAGE', 50))
+
+# Display django standart technical 500 page
+DEBUG = getattr(settings, 'ADREST_DEBUG', False)
+MAIL_ERRORS = as_tuple(getattr(settings, 'ADREST_MAIL_ERRORS', 500))
+
+# Limit request number per second from same identifier, null is not limited
+THROTTLE_AT = getattr(settings, 'ADREST_THROTTLE_AT', 120)
+THROTTLE_TIMEFRAME = getattr(settings, 'ADREST_THROTTLE_TIMEFRAME', 60)
+
+# We do not restrict access for OPTIONS request.
+ALLOW_OPTIONS = getattr(settings, 'ADREST_ALLOW_OPTIONS', False)
