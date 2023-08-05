2b73caab46eb37b5b97651171485b88e7e2ee0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package django-multi-sessions/0.0.4/django-multi-sessions-0.0.4.tar.gz

---
+++ packages/django-multi-sessions/django-multi-sessions-0.0.4.tar.gz/django-multi-sessions-0.0.4/multi_sessions/tests.py
@@ -0,0 +1,106 @@
+import time
+import unittest
+from nose.tools import eq_
+
+from django.utils.importlib import import_module
+from django.core.management.color import no_style
+
+from session import SessionStore as MultiSession
+
+
+class TestMultiSessions(unittest.TestCase):
+
+    def setUp(self):
+        self.multi_session = MultiSession()
+
+        # Create the django_session table
+        from django.db import connection
+        from django.contrib.sessions.models import Session
+
+        self.django_session_model = Session
+        self.database_cursor = connection.cursor()
+
+        tables = connection.introspection.table_names()
+        if Session._meta.db_table not in tables:
+            sql, references = connection.creation.sql_create_model(self.django_session_model, no_style())
+            for statement in sql:
+                self.database_cursor.execute(statement)
+
+    def tearDown(self):
+        # Destroy the django_session table
+        from django.db import connection
+
+        sql = connection.creation.sql_destroy_model(self.django_session_model, {}, no_style())
+        for statement in sql:
+            self.database_cursor.execute(statement)
+
+    def test_modify_and_keys(self):
+        eq_(self.multi_session.modified, False)
+        self.multi_session.create()
+        self.multi_session['test'] = 'test_me'
+        eq_(self.multi_session.modified, True)
+        eq_(self.multi_session['test'], 'test_me')
+
+    def test_save_and_delete(self):
+        self.multi_session['key'] = 'value'
+        self.multi_session.save()
+        self.multi_session.exists(self.multi_session.session_key)
+        eq_(self.multi_session.exists(self.multi_session.session_key), True)
+        self.multi_session.delete(self.multi_session.session_key)
+        eq_(self.multi_session.exists(self.multi_session.session_key), False)
+
+    def test_flush(self):
+        self.multi_session['key'] = 'another_value'
+        self.multi_session.save()
+        key = self.multi_session.session_key
+        self.multi_session.flush()
+        eq_(self.multi_session.exists(key), False)
+
+    def test_items(self):
+        self.multi_session['item1'], self.multi_session['item2'] = 1, 2
+        self.multi_session.save()
+        eq_(self.multi_session.items(), [('item2', 2), ('item1', 1)])
+
+    def test_expiry(self):
+        self.multi_session.set_expiry(1)
+        eq_(self.multi_session.get_expiry_age(), 1)
+        self.multi_session['key'] = 'expiring_value'
+        self.multi_session.save()
+        key = self.multi_session.session_key
+        eq_(self.multi_session.exists(key), True)
+        time.sleep(2)
+        eq_(self.multi_session.exists(key), False)
+
+    def test_save_and_load(self):
+        self.multi_session.set_expiry(60)
+        self.multi_session.setdefault('item_test', 777)
+        self.multi_session.save()
+        session_data = self.multi_session.load()
+        eq_(session_data.get('item_test'), 777)
+            
+    def test_multiple_backends_separately(self):
+        # Check the backend if more than two
+        if len(self.multi_session.pool_backends) < 2:
+            return None
+        self.multi_session.create()
+        session_key = self.multi_session.session_key
+        for backend in self.multi_session.pool_backends:
+            engine = import_module(backend['backend'])
+            session = engine.SessionStore(session_key)
+            if "write" not in backend["modes"]:
+                eq_(session.exists(session_key), False)
+            if "write" in backend["modes"]:
+                eq_(session.exists(session_key), True)
+            if "delete" in backend["modes"] and session.exists(session_key) is True:
+                session.delete(session_key)
+                eq_(session.exists(session_key), False)
+
+
+if __name__ == '__main__':
+    import os
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    unittest.main()
+
+# To run the test suite
+# pip install nose
+# python tests.py -v
---
+++ packages/django-multi-sessions/django-multi-sessions-0.0.4.tar.gz/django-multi-sessions-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from multi_sessions import __version__
+
+long_description = ""
+try:
+    readme = open("README.rst")
+    long_description = str(readme.read())
+finally:
+    readme.close()
+
+
+setup(
+    name='django-multi-sessions',
+    version=__version__,
+    description="Multi-sessions backend for Django",
+    long_description=long_description,
+    keywords='django, sessions',
+    author='Mikhail Andreev',
+    author_email='x11org@gmail.com',
+    url='http://github.com/adw0rd/django-multi-sessions',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['setuptools', ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
