e090eb554e24e9a29920321997d79fbfd25fa0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:16 +0000

    Add package emit/0.1.1/emit-0.1.1.tar.gz

---
+++ packages/emit/emit-0.1.1.tar.gz/emit-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import emit
+
+setup(
+    name='emit',
+    version=emit.__version__,
+    description='Emitter for stream processing',
+    long_description=open('README.md').read(),
+    url='http://github.com/brianhicks/emit',
+    author='Brian Hicks',
+    author_email='brian@brianthicks.com',
+    license='MIT',
+    packages=['emit'],
+    scripts=['emit/bin/emit_digraph'],
+    zip_safe=False
+)
