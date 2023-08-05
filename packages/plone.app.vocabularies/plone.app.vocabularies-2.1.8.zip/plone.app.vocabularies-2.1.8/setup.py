140b2dc9e3827f689ece890869e117e2c8f51ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:54 +0000

    Add package plone.app.vocabularies/2.1.8/plone.app.vocabularies-2.1.8.zip

---
+++ packages/plone.app.vocabularies/plone.app.vocabularies-2.1.8.zip/plone.app.vocabularies-2.1.8/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+version = '2.1.8'
+
+setup(name='plone.app.vocabularies',
+      version=version,
+      description="A collection of generally useful vocabularies.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='Plone Zope formlib vocabularies',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.vocabularies',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'zope.configuration',
+            'zope.testing',
+            'Products.PloneTestCase',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'zope.browser',
+        'zope.component',
+        'zope.formlib',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.schema',
+        'zope.site',
+        'Acquisition',
+        'Products.CMFCore',
+        'Zope2',
+      ],
+      )
