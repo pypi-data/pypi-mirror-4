f06378264efef7ff3991597886926b39bd0e9621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package slurpy/0.1.4/slurpy-0.1.4.tar.gz

---
+++ packages/slurpy/slurpy-0.1.4.tar.gz/slurpy-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+dependencies = [ "tornado" ]
+
+setup(
+    name="slurpy",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=dependencies,
+    author="Jorge Niedbalski R.",
+    author_email="jnr@pyrosome.org",
+    description="Call python methods from javascript and viceversa",
+    keywords="rpc javascript js-rpc javascript-rpc call python from javascript slurpy slurpe slurp",
+    include_package_data=True,
+    license="BSD",
+    classifiers=['Development Status :: 3 - Alpha',
+                'Intended Audience :: Developers',
+                'Operating System :: Unix ']
+)
