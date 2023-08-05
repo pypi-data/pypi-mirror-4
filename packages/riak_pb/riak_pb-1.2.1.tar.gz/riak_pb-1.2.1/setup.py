2b90a1f89af26f64cdbef1af15e88b12e1c69c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package riak_pb/1.2.1/riak_pb-1.2.1.tar.gz

---
+++ packages/riak_pb/riak_pb-1.2.1.tar.gz/riak_pb-1.2.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from proto_cmd import build_proto, clean_proto
+
+setup(name='riak_pb',
+      version='1.2.1',
+      description='Riak Protocol Buffers Messages',
+      packages=['riak_pb'],
+      requires=['protobuf(==2.4.1)'],
+      install_requires=['protobuf==2.4.1'],
+      options={'easy_install': {'allow_hosts': 'pypi.python.org'}},
+      license='Apache 2',
+      platforms='Platform Independent',
+      author='Basho Technologies',
+      author_email='clients@basho.com',
+      url='https://github.com/basho/riak_pb',
+      zip_safe=True,
+      classifiers=['License :: OSI Approved :: Apache Software License',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Topic :: Database'],
+      cmdclass={'build_proto': build_proto,
+                'clean': clean_proto}
+      )
