27b9bf58a2c67812cbbb7f80ea01cdaaa3ad55e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:22 +0000

    Add package HL7py/1.01/HL7py-1.01.zip

---
+++ packages/HL7py/HL7py-1.01.zip/HL7py-1.01/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.txt') as f:
+        return f.read()
+
+setup(name='HL7py',
+      version='1.01',
+      description='HL7 message parser',
+      long_description=readme(),
+      keywords='hl7 parsing ',
+      url='https://github.com/norlowski/HL7py',
+      author='Nicholas Orlowski',
+      author_email='nick.orlowski@gmail.com',
+      license='MIT',
+      packages=['HL7py'],
+      zip_safe=False)
