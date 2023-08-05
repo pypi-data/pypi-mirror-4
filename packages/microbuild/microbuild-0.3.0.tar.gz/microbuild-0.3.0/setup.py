d3cd7a89b24c1034a362a43f322420f3cea3c423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:43 +0000

    Add package microbuild/0.3.0/microbuild-0.3.0.tar.gz

---
+++ packages/microbuild/microbuild-0.3.0.tar.gz/microbuild-0.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+   name="microbuild",
+   version="0.3.0",
+   author="Calum J. Eadie",
+   author_email="calum@calumjeadie.com",
+   url="https://github.com/CalumJEadie/microbuild",
+   packages=["microbuild"],
+   license="MIT License",
+   description="Lightweight Python Build Tool.",
+   long_description=open("README.rst").read()+"\n"+open("CHANGES.rst").read()
+)
