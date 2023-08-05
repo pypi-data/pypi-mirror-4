18bc9938767eddbf10afa2418ee232edc0901b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:34 +0000

    Add package CrudAlchemy/0.2dev/CrudAlchemy-0.2dev.tar.gz

---
+++ packages/CrudAlchemy/CrudAlchemy-0.2dev.tar.gz/CrudAlchemy-0.2dev/crudalchemy/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2012 the CrudAlchemy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of CrudAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+from .utils import setup_sqlalchemy
+from .declarative import CRUDBase
