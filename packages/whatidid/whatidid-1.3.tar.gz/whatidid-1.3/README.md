12c3e5d28534134f073f524001b9903a22b8e5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package whatidid/1.3/whatidid-1.3.tar.gz

---
+++ packages/whatidid/whatidid-1.3.tar.gz/whatidid-1.3/README.md
@@ -0,0 +1,32 @@
+# whatidid
+
+Keeping track of things you've done on your Dropbox by command line.
+
+## Requirements
+
+ - Python 2.7
+ - OSX or Linux
+ - Dropbox (optional, you can change the path of the data in the script)
+
+## Installing
+
+Clone, download, or whatever and add the bin folder to your $PATH.
+
+    pip install whatidid
+
+## Usage
+
+### Updating your status
+
+    $ wid update -m "This is my status"
+
+### Viewing your statuses
+
+    $ wid update-show
+    Monday: This is my status
+    Tuesday: Did something
+
+### Emailing your weekly log to someone
+
+    $ wid-update-mail user@example.com
+
---
+++ packages/whatidid/whatidid-1.3.tar.gz/whatidid-1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='whatidid',
+      version='1.3',
+      description='Keep track of your life',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+      ],
+      keywords='lifelogging whatidid',
+      url='https://github.com/zachseifts/whatidid',
+      author='Zach Seifts',
+      author_email='zach.seifts@gmail.com',
+      license='MIT',
+      packages=['whatidid'],
+      zip_safe=False,
+      scripts=['bin/wid', 'bin/wid-update-mail'])
+
