39903888da9306770ae88807c60412e2111ebc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:42 +0000

    Add package FilenameSort/0.1.3/FilenameSort-0.1.3.tar.gz

---
+++ packages/FilenameSort/FilenameSort-0.1.3.tar.gz/FilenameSort-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='FilenameSort',
+    version='0.1.3',
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['FilenameSort'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/FilenameSort/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.rst').read(),
+    install_requires=[
+                      'cmpGen>=0.1'
+                     ],
+)
