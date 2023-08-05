b8a49a703f4d36e2a67d18a72a26a470361b57b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package assert-select/0.10/assert-select-0.10.tar.gz

---
+++ packages/assert-select/assert-select-0.10.tar.gz/assert-select-0.10/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+assert-select
+=============
+
+A convenience library for making assertions about the CSS selectors present
+in a rendered template.  Concept lifted directly from Rails.  Code paraphrased
+from Rails.
+
+.. _github: http://github.com/aroberts/assert-select
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='assert-select',
+    version='0.10',
+    url='http://github.com/aroberts/assert-select',
+    license='BSD',
+    author='Andrew Roberts',
+    author_email='adroberts@gmail.com',
+    description='Make assertions about CSS selectors',
+    long_description=__doc__,
+    py_modules=['assert_select'],
+    include_package_data=True,
+    install_requires=['lxml>=2.3,<3.0',],
+    zip_safe=False,
+    platforms='any'
+)
