a28a181c327885b7f30e4d5519e6820bc3a8ef1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package configure/0.5/configure-0.5.tar.gz

---
+++ packages/configure/configure-0.5.tar.gz/configure-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+version = "0.5"
+
+with open('README') as f:
+    long_description = f.read()
+
+setup(
+    name="configure",
+    version=version,
+    description="configuration toolkit based on YAML",
+    long_description=long_description,
+    author="Andrey Popp",
+    author_email="8mayday@gmail.com",
+    url='https://github.com/andreypopp/configure',
+    py_modules=["configure"],
+    test_suite="tests",
+    install_requires=["pyyaml"],
+    zip_safe=False)
