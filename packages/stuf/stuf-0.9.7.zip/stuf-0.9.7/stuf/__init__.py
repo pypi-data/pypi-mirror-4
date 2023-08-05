13c1f6ca1f9fadcb49690f8294deb1165ae4a5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package stuf/0.9.7/stuf-0.9.7.zip

---
+++ packages/stuf/stuf-0.9.7.zip/stuf-0.9.7/stuf/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''dictionaries with attribute-style access.'''
+
+from stuf.iterable import (
+    exhaustmap as exhaustitems, exhaustcall as exhaustmap, exhauststar)
+from stuf.core import defaultstuf, fixedstuf, frozenstuf, orderedstuf, stuf
+
+__version__ = (0, 9, 7)
+__all__ = 'defaultstuf fixedstuf frozenstuf orderedstuf stuf'.split()
