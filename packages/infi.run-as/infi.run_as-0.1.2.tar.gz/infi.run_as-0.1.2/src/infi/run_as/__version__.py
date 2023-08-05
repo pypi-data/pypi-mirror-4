0c90a98c6a462c0bf6b37c20add7ef4e877af849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:04 +0000

    Add package infi.run-as/0.1.2/infi.run_as-0.1.2.tar.gz

---
+++ packages/infi.run-as/infi.run_as-0.1.2.tar.gz/infi.run_as-0.1.2/src/infi/run_as/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5a02eb13c6e67ede8e840f6aa533a0b272f741ed'
+__git_head_subject__ = 'Finished release v0.1.2.'
+__git_head_message__ = '* release/v0.1.2:\n  TRIVIAL skeleton update\n  empty commit after version v0.1.1'
+__git_dirty_diff__ = ''
