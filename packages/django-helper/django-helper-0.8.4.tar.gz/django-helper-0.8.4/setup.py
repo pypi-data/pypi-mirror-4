9df8b72464b9758c542ad90402c26f6b0239ce41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package django-helper/0.8.4/django-helper-0.8.4.tar.gz

---
+++ packages/django-helper/django-helper-0.8.4.tar.gz/django-helper-0.8.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import djangohelper
+
+version = djangohelper.__version__
+
+LONG_DESCRIPTION = """
+"""
+
+setup(
+    name='django-helper',
+    version=version,
+    description="A set of useful function/tags/filter for Django.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='helper,django',
+    author='vicalloy',
+    author_email='zbirder@gmail.com',
+    url='https://github.com/vicalloy/django-helper/',
+    license='BSD',
+    packages=find_packages(),
+    package_data = {
+        'djangohelper': [
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
