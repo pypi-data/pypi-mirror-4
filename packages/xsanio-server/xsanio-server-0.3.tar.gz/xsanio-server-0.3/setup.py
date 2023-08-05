bf20579b9aab87e748d53ab41ea7cdc795329929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package xsanio-server/0.3/xsanio-server-0.3.tar.gz

---
+++ packages/xsanio-server/xsanio-server-0.3.tar.gz/xsanio-server-0.3/setup.py
@@ -0,0 +1,12 @@
+#   One day, this file will setup the xsanio_server package
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'xsanio-server',
+    version = '0.3',
+    packages = find_packages(),
+    author = 'Vasily Kolosov',
+    author_email = 'vasily.kolosov@shortcut.ru',
+    include_package_data = True,
+)
