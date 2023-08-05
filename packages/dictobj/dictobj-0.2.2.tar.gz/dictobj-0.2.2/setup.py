f28b4adedcecacbca3934da7778497a4f1ada65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:49 +0000

    Add package dictobj/0.2.2/dictobj-0.2.2.tar.gz

---
+++ packages/dictobj/dictobj-0.2.2.tar.gz/dictobj-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name='dictobj',
+  version='0.2.2',
+  author='William Grim',
+  author_email='william@grimapps.com',
+  url='https://github.com/grimwm/py-dictobj',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+  description='A set of Python dictionary objects where keys can be accessed as instnace attributes.',
+  py_modules=['dictobj'],
+  test_suite='dictobj_test'
+  )
