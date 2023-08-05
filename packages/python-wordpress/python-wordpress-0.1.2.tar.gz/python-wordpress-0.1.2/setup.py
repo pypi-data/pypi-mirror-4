5daef9fdba69ae5aa9b35a04b2e04263d58c5de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package python-wordpress/0.1.2/python-wordpress-0.1.2.tar.gz

---
+++ packages/python-wordpress/python-wordpress-0.1.2.tar.gz/python-wordpress-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+readme = open('README.md').read()
+
+setup(
+    name = "python-wordpress",
+    description = "A Python client for the WordPress JSON API plugin",
+    long_description = readme,
+    version = "0.1.2",
+    author = "Chris Amico",
+    author_email = "eyeseast@gmail.com",
+    py_modules = ['wordpress'],
+    url = "https://github.com/eyeseast/python-wordpress",
+    license = "MIT",
+    install_requires = ['httplib2'],
+    classifiers = [
+        'Environment :: Web Environment',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
