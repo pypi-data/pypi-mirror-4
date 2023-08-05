79e85365dd1ac259bba1180f06bbb46eea8a863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:14 +0000

    Add package pyramid_amdjs/0.3.2/pyramid_amdjs-0.3.2.tar.gz

---
+++ packages/pyramid_amdjs/pyramid_amdjs-0.3.2.tar.gz/pyramid_amdjs-0.3.2/pyramid_amdjs/tests/base.py
@@ -0,0 +1,45 @@
+import sys
+from pyramid import testing
+from pyramid.interfaces import IRequest
+
+if sys.version_info[:2] == (2, 6):
+    from unittest2 import TestCase
+    TestCase
+else:
+    from unittest import TestCase
+
+
+class BaseTestCase(TestCase):
+
+    _include = True
+    _auto_include = True
+    _settings = {'amd.debug': 'f'}
+    _environ = {
+        'wsgi.url_scheme':'http',
+        'wsgi.version':(1,0),
+        'HTTP_HOST': 'example.com',
+        'SCRIPT_NAME': '',
+        'PATH_INFO': '/'}
+
+    def setUp(self):
+        self.init_pyramid()
+
+    def make_request(self, environ=None, request_iface=IRequest, **kwargs):
+        if environ is None:
+            environ=self._environ
+        request = testing.DummyRequest(environ=dict(environ), **kwargs)
+        request.request_iface = IRequest
+        return request
+
+    def init_pyramid(self):
+        self.request = request = self.make_request()
+        self.config = testing.setUp(
+            request=request,
+            settings=self._settings,
+            autocommit=self._auto_include)
+        self.config.get_routes_mapper()
+        self.registry = self.config.registry
+        self.request.registry = self.registry
+
+        if self._include:
+            self.config.include('pyramid_amdjs')
