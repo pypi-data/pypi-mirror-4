88873638ad8c5e5402dcf2ca7c1510d32d51cda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package waltz/0.1.2/waltz-0.1.2.tar.gz

---
+++ packages/waltz/waltz-0.1.2.tar.gz/waltz-0.1.2/setup.py
@@ -0,0 +1,39 @@
+#-*- coding: utf-8 -*-
+
+"""
+    waltz
+    ~~~~~
+
+    Setup
+    `````
+
+    $ pip install waltz    
+"""
+
+from distutils.core import setup
+
+setup(
+    name='waltz',
+    version='0.1.2',
+    url='http://github.com/mekarpeles/waltz',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=[
+        'waltz',
+        'waltz.ballroom',
+        'waltz.configs',
+        'waltz.static',
+        'waltz.templates',
+        'waltz.test'
+        ],
+    platforms='any',
+    scripts=[],
+    license='LICENSE',
+    install_requires=[
+        'lazydb >= 0.1.4',
+        'web.py >= 0.36',
+        'nose >= 1.1.2'
+    ],
+    description="Waltz is a web.py framework for designing web apps in 3/4 time.",
+    long_description=open('README.md').read(),
+)
---
+++ packages/waltz/waltz-0.1.2.tar.gz/waltz-0.1.2/waltz/configs/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    config.py
+    ~~~~~~~~~
+
+    This module is the middle man for handling/consolidating
+    configurations for the Dungeons project.
+
+    :copyright: (c) 2012 by Mek
+    :license: BSD, see LICENSE for more details.
+"""
+
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+
+try:
+    with open('configs/server.cfg') as f: pass
+    config.read('configs/server.cfg')
+except IOError as e:
+    config.read('configs/server_defaults.cfg')
+
+server = {"debug_mode": bool(config.get("server", "debug")),
+          "secret": config.get("server", "secret")
+          }
