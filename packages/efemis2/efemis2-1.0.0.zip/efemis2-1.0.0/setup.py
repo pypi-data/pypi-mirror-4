b2f355296566b888e72a683d0c8e6131bcaa17dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:08 +0000

    Add package efemis2/1.0.0/efemis2-1.0.0.zip

---
+++ packages/efemis2/efemis2-1.0.0.zip/efemis2-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name= 'efemis2',
+    version = '1.0.0',
+    py_modules = ['efemis2'],
+    author = 'emisirli',
+    author_email = 'emisirli@sisma.com.tr',
+    url = 'efemis.com',
+    description = 'super mod'
+    )
