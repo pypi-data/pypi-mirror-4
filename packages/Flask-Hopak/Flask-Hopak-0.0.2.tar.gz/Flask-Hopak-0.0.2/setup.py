4316cc4dd1dcb4fc0c9340969894e4817abe8618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package Flask-Hopak/0.0.2/Flask-Hopak-0.0.2.tar.gz

---
+++ packages/Flask-Hopak/Flask-Hopak-0.0.2.tar.gz/Flask-Hopak-0.0.2/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def desc():
+    info = read('README.md')
+    try:
+        return info + '\n\n' + read('doc/changelog.rst')
+    except IOError:
+        return info
+
+setup(
+    name='Flask-Hopak',
+    version='0.0.2',
+    url='https://github.com/xen/flask-hopak',
+    license='BSD',
+    author='Mikhail Kashkin (@xen), Ilya Petrov (@muromec)',
+    description='Admin interface for Flask that uses Hopak models',
+    long_description=desc(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.9',
+        'hopak',
+        'Flask-PyMongo',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
