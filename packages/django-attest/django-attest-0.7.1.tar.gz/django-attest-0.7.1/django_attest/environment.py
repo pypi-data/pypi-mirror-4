90df484e080b9182191d6a07e204d5115f797513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:47 +0000

    Add package django-attest/0.7.1/django-attest-0.7.1.tar.gz

---
+++ packages/django-attest/django-attest-0.7.1.tar.gz/django-attest-0.7.1/django_attest/environment.py
@@ -0,0 +1,35 @@
+from attest import AssertImportHook, COMPILES_AST
+from contextlib import contextmanager
+import django
+from django.test.simple import DjangoTestSuiteRunner
+import logging
+from pkg_resources import parse_version
+
+
+logger = logging.getLogger("django_attest")
+
+
+if parse_version(django.get_version()) < parse_version('1.4'):
+    logger.info("Django <1.4 has broken import infrastructure, Attest's"
+                " assert hook will be disabled.")
+    AssertImportHook.disable()
+elif COMPILES_AST:
+    AssertImportHook.enable()
+
+
+@contextmanager
+def testing_environment():
+    """
+    Context manager to put Django into a state suitable for testing.
+    """
+    # setup test environment
+    runner = DjangoTestSuiteRunner()
+    runner.setup_test_environment()
+    old_config = runner.setup_databases()
+
+    # do stuff
+    yield
+
+    # tear down environment
+    runner.teardown_databases(old_config)
+    runner.teardown_test_environment()
