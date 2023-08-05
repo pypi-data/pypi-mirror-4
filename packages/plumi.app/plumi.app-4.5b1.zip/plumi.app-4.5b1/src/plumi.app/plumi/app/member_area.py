043defe7f9e308b456641a3b8027301942cf2e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package plumi.app/4.5b1/plumi.app-4.5b1.zip

---
+++ packages/plumi.app/plumi.app-4.5b1.zip/plumi.app-4.5b1/src/plumi.app/plumi/app/member_area.py
@@ -0,0 +1,76 @@
+import logging
+from Products.CMFCore.utils import getToolByName
+
+#i18n
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory("plumi")
+
+exclude_filter = [ 'syndication_information' ]
+def updateCreatorAndOwnership(container, member_id,object):
+    plone_utils = getToolByName(container,'plone_utils') 
+    ids_to_update = [ o for o in object.objectIds() if o not in exclude_filter ]
+    for o in ids_to_update:
+        obj = getattr(object, o)
+        obj.setCreators([member_id])
+
+        logging.info('updateCreatorAndOwnership : object is %s object type is %s, creators %s ' %  (o,obj.getTypeInfo().getId(),obj.Creators()))
+        try:
+            plone_utils.changeOwnershipOf (obj, member_id, 1)
+        except KeyError:
+            #special case, we must be using a user outside of this plone site, 
+            #eg an admin user defined in a higher level
+            pass
+        #call recursively on sub-objects
+        updateCreatorAndOwnership(container,member_id,obj)
+
+
+def notifyMemberAreaCreated(container,context):
+    ''' Called when a user logins in for the first time, and enable user folders is enabled'''
+    logger=logging.getLogger('plumi.app.member_area')
+    logger.info('starting notifyMemberAreaCreated , container is %s , context is %s' % (container,context))
+
+    # this is the plone folder that holds the default member content
+    default_member_content = getattr(container, 'default_member_content', None)
+    portal_membership = getToolByName(container,'portal_membership') 
+    workflow = getToolByName(container,'portal_workflow')
+
+    #XXX this assumption is broken on importing members from CSV file.
+    memb = portal_membership.getAuthenticatedMember()
+    member_id = memb.getId()
+    #hack around above bug. It manifests itself as everyone owned by 'admin' usually.
+    if member_id == 'admin':
+        #get name from folder context name
+            member_id = context.getId()
+
+    logger.info('found member, is %s ' % member_id) 
+    # here we do the copying
+    copy_ids = default_member_content.objectIds()
+    #exclude
+    id_filter = ['.personal']
+    copy_ids = [id for id in copy_ids if id not in id_filter]
+    objects = default_member_content.manage_copyObjects(copy_ids)
+    context.manage_pasteObjects(objects)
+    logger.info('copied objects from default member content')
+ 
+    context.manage_fixupOwnershipAfterAdd()
+
+    #publish the members folder, and whatever got copied into it.
+    workflow.doActionFor(context,'publish')
+    for pubobj in context.objectIds():
+        logging.info('publishing %s ' % pubobj)
+        obj2 = getattr(context, pubobj)
+        workflow.doActionFor(obj2, 'publish')
+
+    #finally, acquire ownership for the new member
+    #run the recursive update creator and ownership
+    updateCreatorAndOwnership(container,member_id,context)
+
+    #register users to mailchimp, for engagemedia.org
+    try:
+        logger.info('calling inviteToList, that registers engagemedia.org users to the relevant mailing list')
+        from em.skin.mailchimp import inviteToList
+        inviteToList(container,context)
+    except:
+        pass
+ 
+    logger.info('ending notifyMemberAreaCreated')
---
+++ packages/plumi.app/plumi.app-4.5b1.zip/plumi.app-4.5b1/src/plumi.app/plumi/app/setuphandlers.py
@@ -0,0 +1,105 @@
+import logging
+from StringIO import StringIO
+from Products.CMFCore.utils import getToolByName
+from plone.app.controlpanel.security import ISecuritySchema
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from plumi.app.vocabs  import vocab_set as vocabs
+from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
+from Products.CMFPlone.utils import _createObjectByType
+from plumi.app.install import app_installation_tasks
+from plone.registry.record import Record
+
+
+def setupHome(portal, out):
+    """
+        set default homepage 
+    """
+    portal.setLayout('featured_videos_homepage')
+
+def setupSiteSecurity(portal, out):
+    """
+        site security setup!
+    """
+    secSchema = ISecuritySchema(portal)
+    secSchema.set_enable_self_reg(True)
+    secSchema.set_enable_user_pwd_choice(True)
+    secSchema.set_enable_user_folders(True)
+
+def setupTranscoding(portal, logger):
+    """
+        configure PlumiVideo as transcodable
+    """
+    registry = getUtility(IRegistry)
+    registry['collective.transcode.star.interfaces.ITranscodeSettings.portal_types'] = (u'PlumiVideo:video_file',)
+
+def setupVocabs(portal, logger):
+    #
+    #ATVocabManager setup
+    #
+    logger.info('Starting ATVocabManager configuration ')
+    atvm = getToolByName(portal, ATVOCABULARYTOOL)
+    wftool = getToolByName(portal,'portal_workflow')
+
+    for vkey in vocabs.keys():
+        # create vocabulary if it doesnt exist:
+        vocabname = vkey
+        if atvm.getVocabularyByName(vocabname):
+            atvm._delObject(vocabname)
+        logger.debug("adding vocabulary %s" % vocabname)
+        _createObjectByType('SimpleVocabulary', atvm, vocabname)
+
+        vocab = atvm[vocabname]
+
+        #delete the 'default' item
+        if hasattr(vocab, 'default'):
+            vocab.manage_delObjects(['default'])
+
+        for (ikey, value) in vocabs [vkey]:
+            if not hasattr(vocab, ikey):
+                _createObjectByType('SimpleVocabularyTerm', vocab, ikey)
+                logger.debug("adding vocabulary item %s %s" % (ikey,value))
+                vocab[ikey].setTitle(value)
+
+        #reindex
+        vocab.reindexObject()
+
+def setupVarious(context):
+
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+
+    if context.readDataFile('plumi.app_various.txt') is None:
+        return
+
+    portal = context.getSite()
+    logger = logging.getLogger('plumi.app')
+    #setupRSS(portal, logger)
+    setupHome(portal, logger)
+    setupSiteSecurity(portal, logger)
+    setupTranscoding(portal, logger)
+    setupVocabs(portal, logger)
+    app_installation_tasks(portal)
+
+def uninstallVocabs(portal, logger):
+    #
+    #ATVocabManager setup
+    #
+    logger.info('Reverting ATVocabManager configuration ')
+    atvm = getToolByName(portal, ATVOCABULARYTOOL)
+
+    for vkey in vocabs.keys():
+        vocabname = vkey
+        if atvm.getVocabularyByName(vocabname):
+            atvm._delObject(vocabname)
+            logger.debug("removing vocabulary %s" % vocabname)
+
+def uninstall(context):
+    if context.readDataFile('plumi.app_uninstall.txt') is None:
+        return
+    print "uninstalling plumi.app"
+    portal = context.getSite()    
+    logger = logging.getLogger('plumi.app')
+    uninstallVocabs(portal, logger)   
