63129c7d774dabcaf0a2be2595d98b54cf31d4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package slapprepare/1.0a5/slapprepare-1.0a5.tar.gz

---
+++ packages/slapprepare/slapprepare-1.0a5.tar.gz/slapprepare-1.0a5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import glob
+import os
+
+version = '1.0a5'
+name = 'slapprepare'
+long_description = open("README.txt").read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+setup(name=name,
+      version=version,
+      description="SlapOS Setup kit for dedicated SuSE machines.",
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      keywords='slapos Setup Kit',
+      license='GPLv3',
+      url='http://www.slapos.org',
+      author='VIFIB',
+      packages=['slapprepare'],
+      include_package_data=True,
+      install_requires=['slapos.libnetworkcache',],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'slapprepare = slapprepare.autoupdate:main',
+              'slapprepare-raw = slapprepare.slapprepare:main',
+              'slapupdate = slapprepare.slapupdate:main',
+          ]
+      },
+)
