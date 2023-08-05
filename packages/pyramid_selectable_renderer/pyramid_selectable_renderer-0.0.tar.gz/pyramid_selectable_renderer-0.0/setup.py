9a927fd13bc81dcb0c69bb0aabf6894e47dbf0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package pyramid_selectable_renderer/0.0/pyramid_selectable_renderer-0.0.tar.gz

---
+++ packages/pyramid_selectable_renderer/pyramid_selectable_renderer-0.0.tar.gz/pyramid_selectable_renderer-0.0/setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    "pyramid"
+    ]
+
+
+setup(name='pyramid_selectable_renderer',
+      version='0.0',
+      description='using multi template on one view_config',
+      long_description=open("README.markdown"), 
+      author='podhmo',
+      author_email='ababjam61@gmail.com',
+      url='https://github.com/podhmo/pyramid_selectable_renderer',
+      package_dir={'': '.'},
+      packages=find_packages('.'),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_selectable_renderer',
+      install_requires = requires,
+      entry_points = """
+      """,
+      )
+
