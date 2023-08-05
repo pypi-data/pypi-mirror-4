229a39468326b7707be25b4e56c61d1a3f8d7c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:00 +0000

    Add package django-tinycontent/0.1.4/django-tinycontent-0.1.4.tar.gz

---
+++ packages/django-tinycontent/django-tinycontent-0.1.4.tar.gz/django-tinycontent-0.1.4/tinycontent/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class TinyContent(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Content block'
