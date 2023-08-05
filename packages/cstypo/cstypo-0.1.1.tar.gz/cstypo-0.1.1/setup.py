c480f09a8100a0a76de789778783fb3bf824d2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package cstypo/0.1.1/cstypo-0.1.1.tar.gz

---
+++ packages/cstypo/cstypo-0.1.1.tar.gz/cstypo-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cstypo',
+    version='0.1.1',
+    author='Juda Kaleta',
+    author_email='juda.kaleta@gmail.com',
+    packages=['cstypo', 'cstypo.templatetags', 'cstypo.tests'],
+    scripts=['bin/cstypo'],
+    url='https://github.com/yetty/cstypo',
+    license=open('LICENSE').read(),
+    description='Package to apply Czech typography easily',
+    long_description=open('README').read(),
+)
