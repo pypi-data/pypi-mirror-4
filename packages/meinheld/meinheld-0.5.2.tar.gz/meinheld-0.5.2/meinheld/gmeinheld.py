9470bf64bbb23747869f82e1cd6f314a9c8c6df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package meinheld/0.5.2/meinheld-0.5.2.tar.gz

---
+++ packages/meinheld/meinheld-0.5.2.tar.gz/meinheld-0.5.2/meinheld/gmeinheld.py
@@ -0,0 +1,49 @@
+import sys
+import types
+from gunicorn.workers.base import Worker
+from gunicorn.glogging import Logger
+from meinheld import server
+from meinheld.mlogging import _access, _error
+import os
+
+def is_py3():
+    return sys.hexversion >=  0x3000000
+
+if not is_py3():
+
+
+    class MeinheldWorker(Worker):
+
+        def __init__(self, *args, **kwargs):
+            Worker.__init__(self, *args, **kwargs)
+            Logger.access = _access
+            Logger.error = _error
+
+            server.set_access_logger(self.log)
+            server.set_error_logger(self.log)
+        
+        def watchdog(self):
+            self.notify()
+
+            if self.ppid != os.getppid():
+                self.log.info("Parent changed, shutting down: %s" % self)
+                server.stop(int(self.timeout))
+
+        def run(self):
+            fd = self.socket.fileno()
+            server.set_keepalive(self.cfg.keepalive)
+            server.set_picoev_max_fd(self.cfg.worker_connections)
+            
+            server.set_fastwatchdog(self.tmp.fileno(), self.ppid, int(self.timeout))
+            #server.set_watchdog(self.watchdog)
+            
+            server.set_listen_socket(fd)
+            server.run(self.wsgi)
+
+        def handle_quit(self, sig, frame):
+            server.stop(int(self.timeout))
+
+        def handle_exit(self, sig, frame):
+            server.stop()
+            sys.exit(0)
+
