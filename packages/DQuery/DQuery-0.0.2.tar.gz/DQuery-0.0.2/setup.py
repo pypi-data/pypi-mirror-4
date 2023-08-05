6e420fc92443365a9e7b6432d051ac7a2289c013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package DQuery/0.0.2/DQuery-0.0.2.tar.gz

---
+++ packages/DQuery/DQuery-0.0.2.tar.gz/DQuery-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name='DQuery',
+    version='0.0.2',
+    author='David Gustafsson',
+    author_email='david.gustafsson@xelera.se',
+    packages=['dquery', 'dquery.commands', 'dquery.formatters', 'dquery.test'],
+    data_files={'dquery' : ['json_settings.php']},
+    scripts=['bin/php_var_json', 'bin/dquery'],
+    url='http://pypi.python.org/pypi/DQuery/',
+    license='GPL',
+    description='Drupal multisite query-tool.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "PyYAML => 3.10",
+        "SQLAlchemy => 0.7.8",
+        "colorama => 0.2.4",
+        "httplib2 => 0.7.6",
+        "lxml => 3.0alpha2",
+        "phpserialize => 1.3",
+        "pyCLI => 2.0.3",
+        "termcolor => 1.1.0",
+    ],
+)
+
