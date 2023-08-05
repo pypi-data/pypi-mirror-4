a8b7bb2efdbeb7543d21bfadb2532abc9969e9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package wtdojo/0.1/wtdojo-0.1-py2.7.egg

---
+++ packages/wtdojo/wtdojo-0.1-py2.7.egg/wtdojo/fields/__init__.py
@@ -0,0 +1,5 @@
+from core import (DojoStringField, DojoDateField, DojoTimeField, DojoIntegerField,
+                  DojoSelectField, DojoSelectMultipleField, DojoBooleanField)
+
+__all__ = ['DojoStringField', 'DojoDateField', 'DojoTimeField', 'DojoIntegerField',
+           'DojoSelectField', 'DojoSelectMultipleField', 'DojoBooleanField']
