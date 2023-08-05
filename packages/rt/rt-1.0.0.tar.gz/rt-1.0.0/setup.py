5064cfb8b0e88270ce30a3b36cb8450dc0c6bf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:49 +0000

    Add package rt/1.0.0/rt-1.0.0.tar.gz

---
+++ packages/rt/rt-1.0.0.tar.gz/rt-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+
+setup(name='rt',
+    version='1.0.0',
+    description='Python interface to Request Tracker API',
+    long_description=README,
+    license='GNU General Public License (GPL)',
+    author='Jiri Machalek',
+    author_email='jiri.machalek@nic.cz',
+    url='https://git.nic.cz/redmine/projects/python-rt',
+    install_requires=['requests'],
+    py_modules=['rt'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
