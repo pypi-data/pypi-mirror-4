633c93af2cf953cc4a6509c459f20485197805ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package django-pipeline-eco/0.1.2/django-pipeline-eco-0.1.2.tar.gz

---
+++ packages/django-pipeline-eco/django-pipeline-eco-0.1.2.tar.gz/django-pipeline-eco-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    description = file.read()
+
+setup(
+    name='django-pipeline-eco',
+    version='0.1.2',
+    description='django-pipeline compiler for eco templates',
+    long_description=description,
+    author='Luca Del Bianco',
+    author_email='vshjxyz@gmail.com',
+    url='https://github.com/vshjxyz/django-pipeline-eco',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'django-pipeline',
+        'eco',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
