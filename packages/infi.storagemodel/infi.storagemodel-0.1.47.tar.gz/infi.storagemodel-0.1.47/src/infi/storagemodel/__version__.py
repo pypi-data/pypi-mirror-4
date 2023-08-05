081b38e0bc45d9a073f6fd3bcd561b73642bc1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:19 +0000

    Add package infi.storagemodel/0.1.47/infi.storagemodel-0.1.47.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.47.tar.gz/infi.storagemodel-0.1.47/src/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.47"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f7a4bd4fd64e82b839a092f18714ac1c9d6f8e4c'
+__git_head_subject__ = 'Finished release v0.1.47.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
