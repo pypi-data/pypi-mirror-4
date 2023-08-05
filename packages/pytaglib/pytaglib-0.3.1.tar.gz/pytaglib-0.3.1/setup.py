059259ef99d56bd413994ebb176b09676c30ebd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package pytaglib/0.3.1/pytaglib-0.3.1.tar.gz

---
+++ packages/pytaglib/pytaglib-0.3.1.tar.gz/pytaglib-0.3.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2011-2012 Michael Helmling, michaelhelmling@posteo.de
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+#
+"""Setup file for pytaglib. Type <python setup.py install> to install this package."""
+ 
+import os.path
+from setuptools import setup
+from Cython.Build import cythonize
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Cython',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+def readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as rm:
+        return rm.read()
+
+
+setup(
+    name='pytaglib',
+    description='TagLib bindings for python 2.x/3.x, written using cython',
+    long_description=readme(),
+    classifiers=CLASSIFIERS,
+    version='0.3.1',
+    license='GPLv3+',
+    author='Michael Helmling',
+    author_email='michaelhelmling@posteo.de',
+    url='http://github.com/supermihi/pytaglib',
+    install_requires=['cython>=0.16'],
+    ext_modules=cythonize("src/taglib.pyx"),
+    entry_points={ 'console_scripts': ['pyprinttags = pyprinttags:script'] },
+    package_dir={'': 'src'},
+    test_suite='tests'
+)
---
+++ packages/pytaglib/pytaglib-0.3.1.tar.gz/pytaglib-0.3.1/src/pyprinttags.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2011-2012 Michael Helmling, michaelhelmling@posteo.de
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+#
+"""A sample script printing the tags of a given audio file.
+
+The main purpose is to show how pytaglib is used, but it also serves as a tool
+showing *all* metadata of a given while, while most taggers only display a set
+of certain tags they know.
+"""
+from __future__ import unicode_literals, print_function
+
+import sys
+import taglib
+
+def script():
+    if len(sys.argv) != 2:
+        raise ValueError('need exactly one argument, the path of the file')
+    audioFile = taglib.File(sys.argv[1])
+    tags = audioFile.tags
+    if len(tags) > 0:
+        maxKeyLen = max(len(key) for key in tags.keys())
+        for key, values in tags.items():
+            for value in values:
+                print(('{0:' + str(maxKeyLen) + '} = {1}').format(key, value))
+    if len(audioFile.unsupported) > 0:
+        print('Unsupported tag elements: ' + "; ".join(audioFile.unsupported))
+        if sys.version_info.major == 2:
+            inputFunction = raw_input
+        else:
+            inputFunction = input
+        if inputFunction("remove unsupported properties? [yN] ") in "yY":
+            audioFile.removeUnsupportedProperties(audioFile.unsupported)
+            audioFile.save()
+
+if __name__ == '__main__':
+    script()
