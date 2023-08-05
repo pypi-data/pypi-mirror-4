4966eb849a5b12625c62866319e583183519ccf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package sc.contentrules.group/1.0b1/sc.contentrules.group-1.0b1.zip

---
+++ packages/sc.contentrules.group/sc.contentrules.group-1.0b1.zip/sc.contentrules.group-1.0b1/setup.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("src", "sc", "contentrules",
+                            "group", "version.txt")).read().strip()
+
+long_description = (open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read())
+
+setup(name='sc.contentrules.group',
+      version=version,
+      description="Plone content rule to create a group based on a content",
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='contentrules action plone group',
+      author='Simples Consultoria',
+      author_email='products@simplesconsultoria.com.br',
+      url='http://www.simplesconsultoria.com.br',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['sc', 'sc.contentrules'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.stringinterp',
+      ],
+      extras_require={
+          'develop': [
+              'Sphinx',
+              'manuel',
+              'pep8',
+              'setuptools-flakes',
+          ],
+          'test': [
+              'interlude',
+              'plone.app.testing'
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ packages/sc.contentrules.group/sc.contentrules.group-1.0b1.zip/sc.contentrules.group-1.0b1/src/sc/contentrules/group/tests/test_create_group.py
@@ -0,0 +1,168 @@
+# -*- coding:utf-8 -*-
+
+import unittest2 as unittest
+
+from OFS.interfaces import IObjectManager
+
+from zope.component import getUtility, getMultiAdapter
+from zope.component.interfaces import IObjectEvent
+from zope.interface import implements
+
+from plone.app.contentrules.rule import Rule
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+from plone.contentrules.engine.interfaces import IRuleStorage
+from plone.contentrules.rule.interfaces import IRuleAction
+from plone.contentrules.rule.interfaces import IExecutable
+
+from Products.CMFCore.utils import getToolByName
+
+from sc.contentrules.group.actions.create import GroupAction
+from sc.contentrules.group.actions.create import GroupEditForm
+from sc.contentrules.group.testing import INTEGRATION_TESTING
+
+
+class DummyEvent(object):
+    implements(IObjectEvent)
+
+    def __init__(self, object):
+        self.object = object
+
+
+class TestGroupAction(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        #setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+        self.gt = self.portal.portal_groups
+        self.gt.addGroup('Fav Customer', title='Our Fav Customer', roles=())
+
+    def testRegistered(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.group.CreateGroup')
+        self.assertEquals('sc.contentrules.group.CreateGroup',
+                          element.addview)
+        self.assertEquals('edit', element.editview)
+        self.assertEquals(IObjectManager, element.for_)
+        self.assertEquals(IObjectEvent, element.event)
+
+    def testInvokeAddView(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.group.CreateGroup')
+        storage = getUtility(IRuleStorage)
+        storage[u'foo'] = Rule()
+        rule = self.portal.restrictedTraverse('++rule++foo')
+
+        adding = getMultiAdapter((rule, self.portal.REQUEST), name='+action')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+
+        addview.createAndAdd(data={'groupid': 'Users',
+                                   'grouptitle': 'Users of this portal',
+                                   'roles': set(['Reader', ])})
+
+        e = rule.actions[0]
+        self.failUnless(isinstance(e, GroupAction))
+        self.assertEquals('Users', e.groupid)
+        self.assertEquals('Users of this portal', e.grouptitle)
+        self.assertEquals(set(['Reader', ]), e.roles)
+
+    def testInvokeEditView(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.group.CreateGroup')
+        e = GroupAction()
+        editview = getMultiAdapter((e, self.folder.REQUEST),
+                                   name=element.editview)
+        self.failUnless(isinstance(editview, GroupEditForm))
+
+    def testExecute(self):
+        e = GroupAction()
+        e.groupid = 'New Group'
+        e.grouptitle = 'Newly Created Group'
+        e.roles = set(['Member', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                             IExecutable)
+        self.assertEquals(True, ex())
+        group = self.gt.getGroupById(e.groupid)
+        self.failUnless(group)
+
+    def testExecuteInterp(self):
+        # Setup scenario
+        self.portal.invokeFactory('Folder', 'customer', title='Customer')
+        folder = self.portal['customer']
+        e = GroupAction()
+        e.groupid = '${title}'
+        e.grouptitle = 'Group of Contributors for folder ${title}'
+        e.roles = set(['Contributor', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(folder)),
+                             IExecutable)
+        self.assertEquals(True, ex())
+        group = self.gt.getGroupById(folder.Title())
+        self.failUnless(group)
+        self.failUnless(group.getGroupId() == folder.Title())
+        grouptitle = 'Group of Contributors for folder %s' % folder.Title()
+        self.failUnless(group.getGroupTitleOrName() == grouptitle)
+
+    def testActionSummary(self):
+        e = GroupAction()
+        e.groupid = '${title}'
+        e.grouptitle = 'Group of Contributors for folder ${title}'
+        e.roles = set(['Contributor', ])
+        summary = u"Create an user group ${groupid} with roles ${roles}"
+        self.assertEquals(summary, e.summary)
+
+    def testExecuteWithoutGroupManagementPlugin(self):
+        ''' No group management plugins enabled '''
+        from Products.PlonePAS.interfaces.group import IGroupManagement
+        # Disable group management plugins
+        acl_users = getToolByName(self.portal, 'acl_users')
+        acl_users.plugins.deactivatePlugin(IGroupManagement,
+                                           'source_groups')
+        # Execute action
+        e = GroupAction()
+        e.groupid = 'New Group'
+        e.grouptitle = 'Newly Created Group'
+        e.roles = set(['Member', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                             IExecutable)
+        self.assertEquals(False, ex())
+
+    def testExecuteWithoutGroupTool(self):
+        ''' Test what happens if portal_groups is not available '''
+        # Remove portal_tool
+        self.portal._delOb('portal_groups')
+
+        # Execute action
+        e = GroupAction()
+        e.groupid = 'New Group'
+        e.grouptitle = 'Newly Created Group'
+        e.roles = set(['Member', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                             IExecutable)
+        self.assertEquals(False, ex())
+
+    def testExecuteWithError(self):
+        ''' Already a group with the same id '''
+        e = GroupAction()
+        e.groupid = 'Fav Customer'
+        e.grouptitle = 'Our Fav Customer'
+        e.roles = set(['Member', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                             IExecutable)
+
+        self.assertEquals(False, ex())
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
