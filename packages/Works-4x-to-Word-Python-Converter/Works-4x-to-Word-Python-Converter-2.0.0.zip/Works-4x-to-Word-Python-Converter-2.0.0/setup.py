f114b6574a969887161e34404b17f1e46456cebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:19 +0000

    Add package Works-4x-to-Word-Python-Converter/2.0.0/Works-4x-to-Word-Python-Converter-2.0.0.zip

---
+++ packages/Works-4x-to-Word-Python-Converter/Works-4x-to-Word-Python-Converter-2.0.0.zip/Works-4x-to-Word-Python-Converter-2.0.0/setup.py
@@ -0,0 +1,26 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "Works-4x-to-Word-Python-Converter",
+    version = "2.0.0",
+    packages = find_packages(),
+    scripts = ['works_to_word.py'],
+
+    # Project has some significant requirements so grab em ALL!
+    install_requires = ['lxml',
+                        'PIL',
+                        'docx',
+                        'Send2Trash'
+                        ''],
+
+    # metadata for upload to PyPI
+    author = "Nick Wilde",
+    author_email = "nick.wilde.90@gmail.com",
+    description = "Works 4.x to Word Python Converter bulk converts those ancient archaic Microsoft Works Word Processor 4.x (.wps) files to Microsoft Word 2007 format (.docx).",
+    license = "New BSD",
+    keywords = "Microsoft Works 4x Word Converter",
+    url = "https://code.google.com/p/works-4x-to-word-python-converter/",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
