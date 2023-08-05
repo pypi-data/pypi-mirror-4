a85be4e269823316a94a8e2deccc5eedba3e3e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:53 +0000

    Add package zodbbrowser/0.10.3/zodbbrowser-0.10.3.tar.gz

---
+++ packages/zodbbrowser/zodbbrowser-0.10.3.tar.gz/zodbbrowser-0.10.3/src/zodbbrowser/__init__.py
@@ -0,0 +1,28 @@
+"""
+ZODB Browser has the following submodules:
+
+  diff         -- compute differences between two dictionaries
+  testing      -- doodads to make writing tests easier
+
+  history      -- extracts historical state information from the ZODB
+  state        -- IStateInterpreter adapters for making sense of unpickled data
+  value        -- IValueRenderer adapters for pretty-printing objects to HTML
+
+  btreesupport -- special handling of OOBTree objects
+
+  interfaces   -- interface definitions
+  browser      -- browser views
+
+  standalone   -- standalone application that starts a web server
+
+"""
+
+
+__version__ = '0.10.3'
+__homepage__ = 'http://launchpad.net/zodbbrowser'
+
+
+if __name__ == '__main__':
+    # support python -m zodbbrowser on Python 2.5
+    from zodbbrowser.standalone import main
+    main()
