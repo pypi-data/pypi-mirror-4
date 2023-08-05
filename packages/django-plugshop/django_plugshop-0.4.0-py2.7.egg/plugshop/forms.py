b740913aeb89a20a0c11921d689dd6b448941f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package django-plugshop/0.4.0/django_plugshop-0.4.0-py2.7.egg

---
+++ packages/django-plugshop/django_plugshop-0.4.0-py2.7.egg/plugshop/forms.py
@@ -0,0 +1,76 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+from plugshop import settings
+from plugshop.utils import get_model
+
+PRODUCT_CLASS = get_model(settings.PRODUCT_MODEL)
+ORDER_CLASS = get_model(settings.ORDER_MODEL)
+ORDER_PRODUCTS_CLASS = get_model(settings.ORDER_PRODUCTS_MODEL)
+NAME_ERROR = _(u'Name is required')
+EMAIL_ERROR = _(u'Invalid email address')
+EMAIL_ERROR_EXISTS = _(u"Email address '%s' already exits, must be unique")
+
+class ProductForm(forms.Form):
+    product = forms.ModelChoiceField(queryset=PRODUCT_CLASS.objects)
+    quantity = forms.IntegerField(required=False)
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = ORDER_CLASS
+        exclude = (
+            'number', 
+            'status',
+            'created_at',
+            'updated_at',
+            'user',
+            'products',
+        )
+
+    name = forms.CharField(required=True, error_messages={
+                           'required': NAME_ERROR})
+    email = forms.EmailField(required=True, error_messages={
+                             'required': EMAIL_ERROR})
+
+    def save(self, commit=True, **kwargs):
+        cart = kwargs.get('cart')
+
+        user, created = User.objects.get_or_create(
+                                        email=self.cleaned_data.get('email'))
+        if created:
+            user.username = self.cleaned_data.get('email')
+            user.email = self.cleaned_data.get('email')
+            user.first_name = self.cleaned_data.get('first_name', '')
+            user.last_name = self.cleaned_data.get('last_name', '')
+            user.is_active = False
+            user.save()
+
+        model = super(OrderForm, self).save(commit=False)
+        model.user = user
+
+        if commit:
+            model.save()
+            for c in cart:
+                ORDER_PRODUCTS_CLASS.objects.create(
+                    product=c.product,
+                    quantity=c.quantity,
+                    order=model)
+                
+        return model
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name').strip()
+        if len(name):
+            n = name.split()
+            if len(n) > 1:
+                self.cleaned_data['first_name'] = " ".join(n[1:])
+                self.cleaned_data['last_name']  = n[0]
+            else:
+                self.cleaned_data['first_name'] = name
+                self.cleaned_data['last_name'] = ''
+        else:
+            raise forms.ValidationError(NAME_ERROR)
+        return name
---
+++ packages/django-plugshop/django_plugshop-0.4.0-py2.7.egg/plugshop/urls.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+from django.conf.urls import patterns, url
+from django.utils.translation import ugettext_lazy as _
+
+from plugshop import settings
+from plugshop.views import *
+from plugshop.exceptions import NoUrlFound
+
+PREFIX = settings.URL_PREFIX
+
+urlpatterns = patterns('plugshop.views',
+    url(r'^$', ProductListView.as_view(), name='plugshop'),
+    url(r'^products/$', ProductListView.as_view(), name='plugshop-product-list'),
+    
+    #categories
+    url(r'^categories/$', CategoryListView.as_view(), 
+        name='plugshop-caterory-list'),
+    url(r'^products/(?P<category_path>[\-\/\w]+)/$', CategoryView.as_view(), 
+        name='plugshop-category'),
+        
+    #products
+    url(r'^products/(?P<category_path>[\-\/\w]+)/(?P<slug>[\-\/\w]+)$', 
+        ProductView.as_view(), 
+        name='plugshop-product'),
+        
+    url(r'^cart/$', CartView.as_view(), name='plugshop-cart'),
+    url(r'^order/(?P<number>[\-\/\w]+)$', OrderView.as_view(), 
+        name='plugshop-order'),
+    url(r'^order/$', OrderCreateView.as_view(), name='plugshop-order-new'),
+)
+
+def get_url(name):
+    patterns = filter(lambda x: x.name == name, urlpatterns)
+    try:
+        pattern = patterns[0]
+    except IndexError:
+        raise NoUrlFound(_("No url with name '%s'" % name))
+
+    return pattern._regex
