fbed7b3ae59f8d006f1a365d91096a78ac9a7728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package nwdiag/0.9.4/nwdiag-0.9.4.tar.gz

---
+++ packages/nwdiag/nwdiag-0.9.4.tar.gz/nwdiag-0.9.4/src/packetdiag_sphinxhelper.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2011 Takeshi KOMIYA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import packetdiag.parser
+import packetdiag.builder
+import packetdiag.drawer
+core = packetdiag
+
+import blockdiag.utils.bootstrap
+import blockdiag.utils.collections
+import blockdiag.utils.fontmap
+utils = blockdiag.utils
+
+from packetdiag.utils.rst import nodes
+from packetdiag.utils.rst import directives
+
+# FIXME: obsoleted interface (keep for compatibility)
+from blockdiag.utils import collections
+from blockdiag.utils.fontmap import FontMap
+from packetdiag import command, parser, builder, drawer
+from packetdiag.utils.rst.directives import packetdiag, PacketdiagDirective
+
+(collections, FontMap, command, parser, builder, drawer,
+ packetdiag, PacketdiagDirective)
