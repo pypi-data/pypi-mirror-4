ad7d5145f9b862a4d1baf24de77e6ee5a468a604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:47 +0000

    Add package ccp_sde_parser/1.0.1/ccp_sde_parser-1.0.1-py2.7.egg

---
+++ packages/ccp_sde_parser/ccp_sde_parser-1.0.1-py2.7.egg/ccp_sde_parser/csvize.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import csv
+from progressbar import ProgressBar, Bar, ETA, Percentage
+import os
+import sys
+import json
+
+def main():
+  if len(sys.argv) != 3:
+    print 'Incorrect number of arguments'
+    print 'USAGE: %s JSON_FILE OUTPUT_FILE' % sys.argv[0]
+    sys.exit(1)
+  json_file = sys.argv[1]
+  out_file = sys.argv[2]
+  if not os.path.isfile(json_file):
+    print 'Unable to open specified JSON file.'
+    sys.exit(1)
+  contents = None
+  with open(json_file, 'rb') as f:
+    contents = json.load(f)
+  if not contents:
+    print 'Unable to parse json'
+    sys.exit(1)
+  with open(out_file, 'wb') as f:
+    dw = csv.DictWriter(f, contents['columns'])
+    dw.writeheader()
+    pbar = ProgressBar(widgets=['CSVizing... ', Bar(), ' ', Percentage(), ' ', ETA()],
+                       maxval=len(contents['data'])).start()
+    for idx, d in enumerate(contents['data']):
+      dw.writerow({k:unicode(v).encode('utf-8') for k,v in d.items()})
+      pbar.update(idx)
+    pbar.finish()
+
+if __name__ == '__main__':
+  main()
