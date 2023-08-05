155ad711461c99469020b32d5545aafe5ff884ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:07 +0000

    Add package jargon/0.1/jargon-0.1.tar.gz

---
+++ packages/jargon/jargon-0.1.tar.gz/jargon-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='jargon',
+    namespace_packages=['jargon'],
+    version='0.1',
+    description='jargon',
+    long_description='jargon',
+    classifiers=[],
+    author='Jargon',
+    author_email='info@jargon.io',
+    url='',
+    keywords='',
+    packages=find_packages(),
+    zip_safe=False,
+)
