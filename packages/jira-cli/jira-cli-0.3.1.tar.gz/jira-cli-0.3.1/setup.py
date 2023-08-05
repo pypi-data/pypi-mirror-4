1853f560f7cd3f65ef1f2fdedb231f7fe98b4bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package jira-cli/0.3.1/jira-cli-0.3.1.tar.gz

---
+++ packages/jira-cli/jira-cli-0.3.1.tar.gz/jira-cli-0.3.1/setup.py
@@ -0,0 +1,32 @@
+"""
+setup.py for jira-cli
+"""
+__author__ = "Ali-Akber Saifee"
+__email__ = "ali@indydevs.org"
+
+import os
+import sys
+from setuptools import setup, find_packages, Command
+import jiracli
+
+setup(name='jira-cli',
+     author="Ali-Akber Saifee",
+     author_email="ali@indydevs.org",
+     url="http://github.com/alisaifee/jira-cli",
+     version = jiracli.__version__,
+     description = "command line utility for interacting with jira",
+     long_description = open("README.rst").read(),
+     packages = find_packages(exclude=['ez_setup']),
+     include_package_data = True,
+     zip_safe = False,
+     install_requires =[
+         'setuptools',
+         'termcolor'
+         ],
+     entry_points = {
+         'console_scripts' : [
+             'jira-cli = jiracli.cli:main',
+             ]
+        },
+     )
+
