a5353f81a8c771e9d411a6ca3487484bc815784a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package adi.simplesite/0.4/adi.simplesite-0.4.tar.gz

---
+++ packages/adi.simplesite/adi.simplesite-0.4.tar.gz/adi.simplesite-0.4/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4'
+
+setup(name='adi.simplesite',
+      version=version,
+      description="Configs for small sites",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ida Ebkes',
+      author_email='contact@ida-ebkes.eu',
+      url='http://svn.plone.org/svn/collective/adi.simplesite',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['adi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'adi.dropdownmenu',
+          'adi.init',
+          'adi.samplecontent',
+          'adi.simplestructure',
+          'adi.slickstyle',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
