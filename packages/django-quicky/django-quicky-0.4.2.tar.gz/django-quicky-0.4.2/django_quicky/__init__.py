19d75b3be81d3c3b172ec68d3d1cd157486ea31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package django-quicky/0.4.2/django-quicky-0.4.2.tar.gz

---
+++ packages/django-quicky/django-quicky-0.4.2.tar.gz/django-quicky-0.4.2/django_quicky/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+
+__VERSION__ = "0.4.2"
+
+try:
+    # we may want to read the __init__ file to get the version outiside
+    # of the scope of Django and the next imports will fails because
+    # without any settings.py file provided
+    import Django
+except ImportError:
+    pass
+else:
+    from decorators import view, routing
+    from utils import get_object_or_None, setting
