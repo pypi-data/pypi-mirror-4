7298e5c909a047b516e7d735ccaefc2c003b827f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:20 +0000

    Add package listindent/1.0.0/listindent-1.0.0.tar.gz

---
+++ packages/listindent/listindent-1.0.0.tar.gz/listindent-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'listindent', 
+        version      = '1.0.0', 
+        py_modules   = ['listindent'],
+        author       = 'josh',
+        author_email = 'hfpython@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'This indents nexted lists if you desire',
+     )
