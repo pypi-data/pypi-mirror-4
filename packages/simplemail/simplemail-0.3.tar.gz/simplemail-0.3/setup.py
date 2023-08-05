2ca988b1b6cb50963c3ffc3cf2e58e4a829bad37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:28 +0000

    Add package simplemail/0.3/simplemail-0.3.tar.gz

---
+++ packages/simplemail/simplemail-0.3.tar.gz/simplemail-0.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="simplemail",
+    version="0.3",
+    packages=["simplemail"],
+    # install_requires=[""],
+    author="Ilya A. Otyutskiy",
+    author_email="sharp@thesharp.ru",
+    maintainer="Ilya A. Otyutskiy",
+    url="https://github.com/thesharp/simplemail",
+    description="An easy way to send emails in Python",
+    license="MIT"
+)
