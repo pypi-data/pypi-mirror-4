ec392ba8b74b15d0b7bdbefa9bcac65813702d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package django-helper/0.8.4/django_helper-0.8.4-py2.7.egg

---
+++ packages/django-helper/django_helper-0.8.4-py2.7.egg/djangohelper/templatetags/djangohelper_tags.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def getvars(request, excludes):
+    getvars = request.GET.copy()
+    excludes = excludes.split(',')
+    for p in excludes:
+        if p in getvars:
+            del getvars[p]
+        if len(getvars.keys()) > 0:
+            return "&%s" % getvars.urlencode()
+        else:
+            return ''
+
+@register.simple_tag(takes_context=True)
+def get_setting(context, key, default_val="", as_key=None):
+    """ 
+    get val form settings and set to context
+      {% load djangohelper_tags %}
+      {% get_setting "key" default_val "as_key" %}
+      {{ as_key }}
+      if as_key is None, this tag will return val
+    """
+    #as_key = as_key if as_key else key
+    if ("%s" % default_val).startswith('$.'):
+        default_val = getattr(settings, default_val[2:])
+    val = getattr(settings, key, default_val)
+    if not as_key:
+        return val
+    context[as_key] = val
+    return ''
