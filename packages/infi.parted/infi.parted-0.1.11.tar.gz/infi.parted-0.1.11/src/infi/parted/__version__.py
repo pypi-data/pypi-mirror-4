851ddfc794c129b197452dd53f7863862be49764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package infi.parted/0.1.11/infi.parted-0.1.11.tar.gz

---
+++ packages/infi.parted/infi.parted-0.1.11.tar.gz/infi.parted-0.1.11/src/infi/parted/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.11"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2e415a5a9f160bd5c6fd575c7beffe963ce004f7'
+__git_head_subject__ = 'Finished release v0.1.11.'
+__git_head_message__ = '# By Guy Rozendorn (3) and Arnon Yaari (1)\n* release/v0.1.11:\n  HIP-330\n  HIP-362\n  HIP-362\n  empty commit after version v0.1.10'
+__git_dirty_diff__ = ''
