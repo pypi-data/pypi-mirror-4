2682be6245eb558487555601f33350ac534fc7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package snaptastic/0.0.1/snaptastic-0.0.1.zip

---
+++ packages/snaptastic/snaptastic-0.0.1.zip/snaptastic-0.0.1/snaptastic/snap.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+path = os.path.abspath(__file__)
+parent = os.path.join(path, '../', '../')
+sys.path.append(parent)
+
+from argh import command, ArghParser
+from snaptastic import get_snapshotter
+
+
+@command
+def make_snapshots(snapshotter_name, verbosity=2):
+    snapshotter_class = get_snapshotter(snapshotter_name)
+    snap = snapshotter_class()
+    snap.make_snapshots()
+
+
+@command
+def mount_snapshots(snapshotter_name, verbosity=2):
+    snapshotter_class = get_snapshotter(snapshotter_name)
+    snap = snapshotter_class()
+    snap.mount_snapshots()
+
+
+p = ArghParser()
+p.add_commands([make_snapshots, mount_snapshots])
+p.dispatch()
