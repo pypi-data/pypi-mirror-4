be89a9050b030e3b17e73d68c6c2069b9157b2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:31 +0000

    Add package busmon/0.3.3/busmon-0.3.3.tar.gz

---
+++ packages/busmon/busmon-0.3.3.tar.gz/busmon-0.3.3/busmon/consumers.py
@@ -0,0 +1,32 @@
+import pygments.lexers
+import pygments.formatters
+from fedmsg.consumers import FedmsgConsumer
+
+import fedmsg
+import fedmsg.encoding
+
+
+class MessageColorizer(FedmsgConsumer):
+    topic = "*"
+    config_key = 'busmon.consumers.enabled'
+    jsonify = False
+
+    def consume(self, message):
+        destination_topic = "colorized-messages"
+
+        if destination_topic in message.topic:
+            return
+
+        html_args = {'full': False}
+        code = pygments.highlight(
+            fedmsg.encoding.pretty_dumps(fedmsg.encoding.loads(message.body)),
+            pygments.lexers.JavascriptLexer(),
+            pygments.formatters.HtmlFormatter(**html_args)
+        )
+
+        code = code.strip()
+
+        fedmsg.send_message(
+            topic=destination_topic,
+            msg=code,
+        )
