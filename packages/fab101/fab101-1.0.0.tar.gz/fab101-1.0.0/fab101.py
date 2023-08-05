d0946b0b2285b99a4a65b7c75ab470ce82147b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:32 +0000

    Add package fab101/1.0.0/fab101-1.0.0.tar.gz

---
+++ packages/fab101/fab101-1.0.0.tar.gz/fab101-1.0.0/fab101.py
@@ -0,0 +1,11 @@
+"""This is a fibonati modelule
+	ofd sfdsfdsfsd"""
+
+
+def fab(x):
+	""" function comments"""
+	# also comments, but on one line
+	if x==1:
+		return 1
+	else:
+		return x*fab(x-1)
---
+++ packages/fab101/fab101-1.0.0.tar.gz/fab101-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'fab101',
+	version	= '1.0.0',
+	py_modules	= ['fab101'],
+	author		= 'bla bla',
+	author_email	= 'blabla@bal.com',
+	url				= 'http://www.fdfdfdf.com',
+	description		= 'a simple gab def',
+)
