95b77454eebeb07e3a7e5200a6771da8e53e2f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:26 +0000

    Add package quicklog/1/quicklog-1.tar.gz

---
+++ packages/quicklog/quicklog-1.tar.gz/quicklog-1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='quicklog',
+    version='1',
+    description='Small logging utility',
+    author='Chris Anoikesis',
+    author_email='tutow@hotmail.com',
+    url='http://packages.python.org/QuickLog',
+    packages=["quicklog"],
+      long_description="""\
+      QuickLog is a small logging utility.
+      """,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2 :: Only",
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Topic :: Utilities",
+      ],
+      keywords='logging',
+      license='MIT',
+      install_requires=[],
+    )
+
