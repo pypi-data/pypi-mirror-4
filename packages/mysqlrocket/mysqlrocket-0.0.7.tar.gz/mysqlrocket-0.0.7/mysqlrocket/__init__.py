e1ced95233ec6c93a0920cdee05c192d89743178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package mysqlrocket/0.0.7/mysqlrocket-0.0.7.tar.gz

---
+++ packages/mysqlrocket/mysqlrocket-0.0.7.tar.gz/mysqlrocket-0.0.7/mysqlrocket/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+__author__ = "Cyprien Devillez"
+__license__ = "GPL"
+__version__ = "0.0.7"
+__description__ = " Simple CLI tool to create and delete easily MySQL databases. "
+
+import mysqlrocket
