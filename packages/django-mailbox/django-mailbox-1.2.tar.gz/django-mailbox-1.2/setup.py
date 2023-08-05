5818fe3e1bcc258b716c02a1d44ce0ee2f6404c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package django-mailbox/1.2/django-mailbox-1.2.tar.gz

---
+++ packages/django-mailbox/django-mailbox-1.2.tar.gz/django-mailbox-1.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='django-mailbox',
+    version='1.2',
+    url='http://bitbucket.org/latestrevision/django-mailbox/',
+    description='Automatically import mail from POP3, IMAP, or a local mailbox into Django',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Communications :: Email :: Post-Office',
+        'Topic :: Communications :: Email :: Post-Office :: IMAP',
+        'Topic :: Communications :: Email :: Post-Office :: POP3',
+        'Topic :: Communications :: Email :: Email Clients (MUA)',
+    ],
+    packages=[
+        'django_mailbox',
+        'django_mailbox.management',
+        'django_mailbox.management.commands',
+        'django_mailbox.migrations',
+        'django_mailbox.transports',
+        ],
+)
