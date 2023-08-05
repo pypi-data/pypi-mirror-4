6432cfa32fe419dd47e0e06082e021537f9e32b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:22 +0000

    Add package python-mwuppet/0.0.4/python-mwuppet-0.0.4.tar.gz

---
+++ packages/python-mwuppet/python-mwuppet-0.0.4.tar.gz/python-mwuppet-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    
+    entry_points = {
+        'console_scripts': [
+            'mwuppet = mwuppet.mwuppet:main',
+        ]
+    },
+        name="python-mwuppet",
+        version="0.0.4",
+        author="Yuvi Panda",
+        author_email="yuvipanda@gmail.com",
+        url="https://github.com/yuvipanda/mwuppet",
+        packages=['mwuppet',],
+        license="MIT License",
+        description = " Mediawiki UserScript Deploy Script",
+        long_description = open("README").read(),
+        install_requires = ["python-mwapi"],
+)
+
