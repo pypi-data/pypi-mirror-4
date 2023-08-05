d7b12d0451f5067acafc2e7584273e855343bdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:09 +0000

    Add package bagit/0.9.9/bagit-0.9.9.tar.gz

---
+++ packages/bagit/bagit-0.9.9.tar.gz/bagit-0.9.9/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+description = \
+"""
+This package can be used to create BagIt style packages of
+digital content for safe transmission and digital preservation.
+See: http://en.wikipedia.org/wiki/BagIt for more details.
+"""
+
+# for older pythons ...
+requirements = []
+try:
+    import multiprocessing
+except:
+    requirements.append("multiprocessing")
+try:
+    import hashlib
+except:
+    requirements.append("hashlib")
+
+
+setup(
+    name = 'bagit',
+    version = '0.9.9',
+    url = 'http://github.com/edsu/bagit',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['bagit',],
+    scripts = ['bagit.py'],
+    description = description,
+    platforms = ['POSIX'],
+    test_suite = 'test',
+    install_requires = requirements,
+    classifiers = [
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+    ],
+)
