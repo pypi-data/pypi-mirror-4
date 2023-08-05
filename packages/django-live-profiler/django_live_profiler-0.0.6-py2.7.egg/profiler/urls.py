db370fc64fc7c697e2d4e9b9f1371e99352c8c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package django-live-profiler/0.0.6/django_live_profiler-0.0.6-py2.7.egg

---
+++ packages/django-live-profiler/django_live_profiler-0.0.6-py2.7.egg/profiler/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns(
+    'profiler.views',
+    url(r'^$', 'global_stats', name='profiler_global_stats'),
+    url(r'^by_view/$', 'stats_by_view', name='profiler_stats_by_view'),
+    url(r'^reset/$', 'reset', name='profiler_reset'),
+    )
+
