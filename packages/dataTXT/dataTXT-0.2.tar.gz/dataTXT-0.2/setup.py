6ff4e96746e000c52c93f4614fcb9e2b63d66559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package dataTXT/0.2/dataTXT-0.2.tar.gz

---
+++ packages/dataTXT/dataTXT-0.2.tar.gz/dataTXT-0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+__version__ = '0.2'
+
+setup(name='dataTXT',
+      version=__version__,
+      description="python interface for SpazioDati's dataTXT and DBpedia",
+      long_description=open('README.txt', 'r').read(),
+      author="Marco Amadori",
+      author_email="amadori@spaziodati.eu",
+      url="https://github.com/SpazioDati/python-datatxt",
+      py_modules=['datatxt'],
+      install_requires=['sparql-client >= 0.13', 'requests >= 0.11'],
+      license='LICENSE.TXT',
+      classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Natural Language :: Italian',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Indexing'
+      ],
+      )
+
