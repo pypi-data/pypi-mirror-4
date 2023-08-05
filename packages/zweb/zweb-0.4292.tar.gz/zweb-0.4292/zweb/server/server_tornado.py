dad5ec12af6285eef15c020450d4165229456596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package zweb/0.4292/zweb-0.4292.tar.gz

---
+++ packages/zweb/zweb-0.4292.tar.gz/zweb-0.4292/zweb/server/server_tornado.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+import _tornado
+import tornado.ioloop
+
+class Run(object):
+    def __init__(self, port, application):
+        self.port = port
+        self.application = application
+
+    def __call__(self):
+        import sys
+        if len(sys.argv) > 1 and sys.argv[1].isdigit():
+            port = int(sys.argv[1])
+        else:
+            port = self.port
+            if type(port) in (list, tuple):
+                port = port[0]
+
+        print 'server on port %s'%port
+
+        self.application.listen(port)
+        tornado.ioloop.IOLoop.instance().start()
+
