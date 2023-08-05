88bcc981adeae6b42a853c31123afc400835fff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package django-discoverage/0.5.0/django-discoverage-0.5.0.tar.gz

---
+++ packages/django-discoverage/django-discoverage-0.5.0.tar.gz/django-discoverage-0.5.0/discoverage/settings.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+# The name of the iterable ``django-discoverage`` looks for in the discovery
+# process
+TESTED_APPS_VAR_NAME = getattr(settings, 'TESTED_APPS_VAR_NAME', 'TESTS_APPS')
+
+# Modules not to trace
+COVERAGE_OMIT_MODULES = getattr(settings, 'COVERAGE_OMIT_MODULES', ['*test*'])
+COVERAGE_EXCLUDE_PATTERNS = getattr(settings, 'COVERAGE_EXCLUDE_PATTERNS', [
+    r'def __unicode__\(self\):',
+    r'def __str__\(self\):',
+    r'def get_absolute_url\(self\):',
+    r'from .* import .*', 'import .*',
+])
+
+# Determines whether the apps to be included in the coverage report
+# should be inferred from the test's subpackage name
+PKG_NAME_APP_DISCOVERY = getattr(settings, 'PKG_NAME_APP_DISCOVERY', True)
+
+# Determines whether tested apps are guessed from module names
+MODULE_NAME_APP_DISCOVERY = getattr(settings, 'MODULE_NAME_APP_DISCOVERY', False)
+MODULE_NAME_DISCOVERY_PATTERN = getattr(settings, 'MODULE_NAME_DISCOVERY_PATTERN',
+                                        r'test_?(\w+)')
