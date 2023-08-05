079efb4498ca580df949dcc0d32e9f275c64ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:08 +0000

    Add package altf2/1.0.3/altf2-1.0.3.tar.bz2

---
+++ packages/altf2/altf2-1.0.3.tar.bz2/altf2-1.0.3/altf2/ui/mycombobox.py
@@ -0,0 +1,22 @@
+
+from PyQt4 import QtCore, QtGui
+
+class MyComboBox(QtGui.QComboBox):
+    
+    def keyPressEvent(self, event):
+        
+        key = event.key()
+        event.setAccepted(False)
+        
+        if event.modifiers() == QtCore.Qt.NoModifier and key == QtCore.Qt.Key_Escape:
+            self.parent().parent().close()
+            event.accept()
+        
+        elif (event.modifiers() == QtCore.Qt.KeypadModifier and key == QtCore.Qt.Key_Enter) or \
+             (event.modifiers() == QtCore.Qt.NoModifier and key == QtCore.Qt.Key_Return):
+            self.parent().parent().run()
+            event.accept()
+
+        if not event.isAccepted():
+            super(MyComboBox, self).keyPressEvent(event)
+
