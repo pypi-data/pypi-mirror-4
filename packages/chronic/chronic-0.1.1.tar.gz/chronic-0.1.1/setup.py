e1431d06c7914581ee2cee7abf09e9605b78cf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package chronic/0.1.1/chronic-0.1.1.tar.gz

---
+++ packages/chronic/chronic-0.1.1.tar.gz/chronic-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='chronic',
+      version='0.1.1',
+      py_modules=['chronic', 'signals'],
+      description='Half profiler, half timer, 100% gangsta',
+      url='http://github.com/davidcrawford/chronic',
+      author='David Crawford',
+      author_email='david.crawford@gmail.com',
+      license='MIT'
+      )
