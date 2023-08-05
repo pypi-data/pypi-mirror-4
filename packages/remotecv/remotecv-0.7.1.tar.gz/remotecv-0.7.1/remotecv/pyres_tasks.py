d0def7a155fb11556c9dfa54be30fbde3501257b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package remotecv/0.7.1/remotecv-0.7.1.tar.gz

---
+++ packages/remotecv/remotecv-0.7.1.tar.gz/remotecv-0.7.1/remotecv/pyres_tasks.py
@@ -0,0 +1,14 @@
+from remotecv.image_processor import ImageProcessor
+from remotecv.result_store import ResultStore
+from remotecv.utils import config
+
+class DetectTask:
+    queue = "Detect"
+    processor = ImageProcessor()
+
+    @classmethod
+    def perform(clz, detection_type, image_path, key):
+        image_data = config.loader.load_sync(image_path)
+        points = clz.processor.detect(detection_type, image_data)
+        result_store = ResultStore(clz.resq.redis)
+        result_store.store(key, points)
