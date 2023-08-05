4ca70438ff9c88f26a1e57b9a2784d320d3eb61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package bluetrain/0.9.9/bluetrain-0.9.9.zip

---
+++ packages/bluetrain/bluetrain-0.9.9.zip/bluetrain-0.9.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bluetrain",
+    packages=[
+            "bluetrain",
+            "bluetrain.templatetags",
+    ],
+    package_data={
+        "bluetrain": ["templates/*.html", "fixtures/*.json"],
+    },
+    include_package_data=True,
+    version="0.9.9",
+    description="Django CMS",
+    long_description=open("README.txt").read(),
+    author="Tom Clancy",
+    author_email="tclancy@gmail.com",
+    url="https://github.com/tclancy/bluetrain",
+    license="LGPL",
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Framework :: Django"
+    ],
+)
