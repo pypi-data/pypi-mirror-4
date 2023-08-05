809e88518691da35d84a67c55716a29098f412a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package phileo/0.6/phileo-0.6.tar.gz

---
+++ packages/phileo/phileo-0.6.tar.gz/phileo-0.6/phileo/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+
+class Like(models.Model):
+    
+    sender = models.ForeignKey(User, related_name="liking")
+    
+    receiver_content_type = models.ForeignKey(ContentType)
+    receiver_object_id = models.PositiveIntegerField()
+    receiver = generic.GenericForeignKey(
+        ct_field="receiver_content_type",
+        fk_field="receiver_object_id"
+    )
+    
+    timestamp = models.DateTimeField(default=timezone.now)
+    
+    class Meta:
+        unique_together = (
+            ("sender", "receiver_content_type", "receiver_object_id"),
+        )
+    
+    def __unicode__(self):
+        return "%s likes %s" % (self.sender, self.receiver)
