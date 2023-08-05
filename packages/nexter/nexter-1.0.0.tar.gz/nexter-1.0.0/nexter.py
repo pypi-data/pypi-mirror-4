7df5f66d6969d63a748121f7b4e8d40b45ecef1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package nexter/1.0.0/nexter-1.0.0.tar.gz

---
+++ packages/nexter/nexter-1.0.0.tar.gz/nexter-1.0.0/nexter.py
@@ -0,0 +1,13 @@
+"""This is the sample module for didiatic purpose only
+    It is not advised to use this in production """
+
+
+def print_lol(the_list):
+    
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+
---
+++ packages/nexter/nexter-1.0.0.tar.gz/nexter-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'nexter',
+        version = '1.0.0',
+        py_modules = ['nexter'],
+        author = 'basileis',
+        author_email = 'basileis@gmail.com',
+        url = 'http://basileis.com',
+        description = 'New module for didiatic purpose',
+    )
+
