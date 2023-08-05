a83d918144938a6dd2a0550b5d7a2a0010920275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package js_project/0.1/js_project-0.1.tar.gz

---
+++ packages/js_project/js_project-0.1.tar.gz/js_project-0.1/README.txt
@@ -0,0 +1,13 @@
+JS Project
+============================================================================
+
+This package provides a pcreate scaffold for creating a `Fanstatic <http://fanstatic.org>`_ js.* library project.
+
+JS Project uses the Pyramid Scaffold pcreate command to create a barebones python project which provices a Fanstatic js.* resource.
+
+Usage
+_____
+
+    $ pcreate -s js_project <project_name>
+
+The new project will contain dummy css and js files linked as Fanstatic resources. These resources are ready to be 'need()'ed (though of course they are empty. 
---
+++ packages/js_project/js_project-0.1.tar.gz/js_project-0.1/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'fanstatic',
+]
+
+setup(
+    name='js_project',
+    version='0.1',
+    author='Parker Pinette',
+    author_email='parker@parkerpinette.com',
+    url='https://github.com/ppinette/pyramid_foundation',
+    license='LICENSE.txt',
+    description='pcreate scaffold for fanstatic resource library projects',
+    long_description=README + '\n\n' + CHANGES,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    entry_points="""\
+        [pyramid.scaffold]
+        js_project=js_project.scaffolds:JSProjectTemplate
+    """,
+)
