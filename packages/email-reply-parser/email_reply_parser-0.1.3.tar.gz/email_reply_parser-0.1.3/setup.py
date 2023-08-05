3453fe4bc325dd3bc44f59b642e859dc051c69a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package email-reply-parser/0.1.3/email_reply_parser-0.1.3.tar.gz

---
+++ packages/email-reply-parser/email_reply_parser-0.1.3.tar.gz/email_reply_parser-0.1.3/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='email_reply_parser',
+    version='0.1.3',
+    description='A email parser library, making it easy to extract a signature, reply, or quote block from a text-formatted email.',
+    author='Royce Haynes',
+    author_email='royce.haynes@gmail.com',
+    url='https://github.com/zapier/email-reply-parser',
+    license='MIT',
+    packages=find_packages(exclude=('tests')),
+    test_suite='tests.test_email_message'
+)
