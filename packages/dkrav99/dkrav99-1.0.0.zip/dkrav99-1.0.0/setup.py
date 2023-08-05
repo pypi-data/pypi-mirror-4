def39e89b2a8b09770b65615864277bbd2809633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:45 +0000

    Add package dkrav99/1.0.0/dkrav99-1.0.0.zip

---
+++ packages/dkrav99/dkrav99-1.0.0.zip/dkrav99-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='dkrav99',
+	version		='1.0.0',
+	py_modules	=['distirbution_test'],
+	author		='MingShiou',
+	author_email	='assa4415@yahoo.com.tw',
+	url		='http://www.plurk.com/assa4415',
+	description	='A first test in python to distribution module on 101.11.30',
+       )
