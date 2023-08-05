4b8f760e4af6cd0321d344d8e7e9096417c671ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:50 +0000

    Add package daemonize/2.1.1/daemonize-2.1.1.tar.gz

---
+++ packages/daemonize/daemonize-2.1.1.tar.gz/daemonize-2.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="daemonize",
+    version="2.1.1",
+    py_modules=["daemonize"],
+    author="Ilya A. Otyutskiy",
+    author_email="sharp@thesharp.ru",
+    maintainer="Ilya A. Otyutskiy",
+    url="https://github.com/thesharp/daemonize",
+    description="Library to enable your code run as a daemon process "
+                "on Unix-like systems.",
+    license="MIT"
+)
