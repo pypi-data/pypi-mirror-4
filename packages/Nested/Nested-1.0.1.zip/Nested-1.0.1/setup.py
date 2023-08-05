8dbda527630d9f05593991ea96698372bf3e093f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package Nested/1.0.1/Nested-1.0.1.zip

---
+++ packages/Nested/Nested-1.0.1.zip/Nested-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name         = 'Nested',
+      version      = '1.0.1',
+      py_modules   = ['nester'],
+      author       = 'Vishal',
+      author_email = 'vishal3346@gmail.com',
+      description  = 'A simple printer of nested lines',
+     )
