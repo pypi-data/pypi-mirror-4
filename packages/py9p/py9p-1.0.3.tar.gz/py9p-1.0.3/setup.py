a49d928d5b672e856465ea41be04d0a219587b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package py9p/1.0.3/py9p-1.0.3.tar.gz

---
+++ packages/py9p/py9p-1.0.3.tar.gz/py9p-1.0.3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py9p',
+    version='1.0.3',
+    description='9P Protocol Implementation',
+    author='Andrey Mirtchovski',
+    author_email='aamirtch@ucalgary.ca',
+    maintainer='Peter V. Saveliev',
+    maintainer_email='peet@redhat.com',
+    url='https://github.com/svinota/py9p',
+    license="MIT",
+    packages=[
+        'py9p'
+        ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        ],
+    long_description='''
+9P protocol implementation
+--------------------------
+
+The library allows you to use 9P protocol in your
+applications.
+
+Please note, that the library is not fully compatible
+with the initial version by Andrey Mirtchovski.
+'''
+)
