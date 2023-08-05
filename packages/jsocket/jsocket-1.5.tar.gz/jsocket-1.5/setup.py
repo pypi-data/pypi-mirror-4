2443123b346aa253c744f018d328bcb29585ca76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package jsocket/1.5/jsocket-1.5.tar.gz

---
+++ packages/jsocket/jsocket-1.5.tar.gz/jsocket-1.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jsocket',
+      version='1.5',
+      description='Python JSON Server & Client',
+      author='Christopher Piekarski',
+      author_email='chris@cpiekarski.com',
+      maintainer='Christopher Piekarski',
+      maintainer_email='chris@cpiekarski.com',
+      license='OSI Approved Apache Software License',
+      keywords=['json','socket','server','client'],
+      packages=['jsocket'],
+      provides=['jsocket'],
+      classifiers=[
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7',
+                   'Operating System :: OS Independent',
+                   ],
+      url='http://cpiekarski.com/2011/05/09/super-easy-python-json-client-server/'
+     )
