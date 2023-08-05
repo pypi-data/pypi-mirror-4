034bb500004517842d41b0f27d717d393e1fe365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:36 +0000

    Add package Products.mediaOrganization/0.1/Products.mediaOrganization-0.1.tar.gz

---
+++ packages/Products.mediaOrganization/Products.mediaOrganization-0.1.tar.gz/Products.mediaOrganization-0.1/Products/mediaOrganization/interfaces/mediaorganization.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class IMediaOrganization(Interface):
+    """Organization type with media such as  images and video"""
+
+    # -*- schema definition goes here -*-
