cf52dde8fc8bca03bfddd4620f454d005fc1a705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package doc2dash/1.1.0/doc2dash-1.1.0.tar.gz

---
+++ packages/doc2dash/doc2dash-1.1.0.tar.gz/doc2dash-1.1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+import doc2dash
+
+
+setup(
+    name='doc2dash',
+    version=doc2dash.__version__,
+    description=doc2dash.__doc__.strip(),
+    long_description=open('README.rst').read(),
+    url='http://github.com/hynek/doc2dash/',
+    license=doc2dash.__license__,
+    author=doc2dash.__author__,
+    author_email='hs@ox.cx',
+    packages=[
+        'doc2dash',
+        'doc2dash.parsers',
+    ],
+    entry_points={
+        'console_scripts': [
+            'doc2dash = doc2dash.__main__:main',
+        ],
+    },
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Documentation',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing',
+    ],
+)
