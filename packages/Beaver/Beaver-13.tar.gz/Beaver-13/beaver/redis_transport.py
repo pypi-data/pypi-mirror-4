ab2db6a13c3073d2bb5258f91139041b76995b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:50 +0000

    Add package Beaver/13/Beaver-13.tar.gz

---
+++ packages/Beaver/Beaver-13.tar.gz/Beaver-13/beaver/redis_transport.py
@@ -0,0 +1,40 @@
+import datetime
+import redis
+import time
+import urlparse
+
+import beaver.transport
+
+
+class RedisTransport(beaver.transport.Transport):
+
+    def __init__(self, file_config, beaver_config):
+        super(RedisTransport, self).__init__(file_config, beaver_config)
+
+        redis_url = beaver_config.get('redis_url')
+        _url = urlparse.urlparse(redis_url, scheme="redis")
+        _, _, _db = _url.path.rpartition("/")
+
+        self.redis = redis.StrictRedis(host=_url.hostname, port=_url.port, db=int(_db), socket_timeout=10)
+        self.redis_namespace = beaver_config.get('redis_namespace')
+
+        wait = 0
+        while 1:
+            if wait == 20:
+                break
+
+            time.sleep(0.1)
+            wait += 1
+            try:
+                self.redis.ping()
+                break
+            except redis.exceptions.ConnectionError:
+                pass
+
+    def callback(self, filename, lines):
+        timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        for line in lines:
+            self.redis.rpush(
+                self.redis_namespace,
+                self.format(filename, timestamp, line)
+            )
---
+++ packages/Beaver/Beaver-13.tar.gz/Beaver-13/beaver/transport.py
@@ -0,0 +1,63 @@
+import os
+
+
+class Transport(object):
+
+    def __init__(self, file_config, beaver_config):
+        self._file_config = file_config
+        self._format = beaver_config.get('format')
+
+        self._current_host = beaver_config.get('hostname')
+
+        if self._format == 'msgpack':
+            import msgpack
+            self.packer = msgpack.Packer()
+        elif self._format == 'json':
+            # priority: ujson > simplejson > jsonlib2 > json
+            priority = ['ujson', 'simplejson', 'jsonlib2', 'json']
+            for mod in priority:
+                try:
+                    self.json = __import__(mod)
+                except ImportError:
+                    pass
+                else:
+                    break
+
+    def callback(self, filename, lines):
+        return True
+
+    def interrupt(self):
+        return True
+
+    def unhandled(self):
+        return True
+
+    def format(self, filename, timestamp, line):
+        if self._format == 'json':
+            return self.json.dumps({
+                '@source': "file://{0}{1}".format(self._current_host, filename),
+                '@type': self._file_config.gettype(filename),
+                '@tags': self._file_config.gettags(filename),
+                '@fields': self._file_config.getaddfield(filename),
+                '@timestamp': timestamp,
+                '@source_host': self._current_host,
+                '@source_path': filename,
+                '@message': line.strip(os.linesep),
+            })
+        elif self._format == 'msgpack':
+            return self.packer.pack({
+                '@source': "file://{0}{1}".format(self._current_host, filename),
+                '@type': self._file_config.gettype(filename),
+                '@tags': self._file_config.gettags(filename),
+                '@fields': self._file_config.getaddfield(filename),
+                '@timestamp': timestamp,
+                '@source_host': self._current_host,
+                '@source_path': filename,
+                '@message': line.strip(os.linesep),
+            })
+
+        return "[{0}] [{1}] {2}".format(self._current_host, timestamp, line)
+
+
+class TransportException(Exception):
+    pass
