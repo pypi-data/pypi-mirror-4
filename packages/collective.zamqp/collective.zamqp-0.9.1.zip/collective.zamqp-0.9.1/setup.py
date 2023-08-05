67bb81b3af5ed55d6ccaa49ddae08be7c9d8dfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:25 +0000

    Add package collective.zamqp/0.9.1/collective.zamqp-0.9.1.zip

---
+++ packages/collective.zamqp/collective.zamqp-0.9.1.zip/collective.zamqp-0.9.1/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='collective.zamqp',
+    version='0.9.1',
+    description="AMQP consumer and producer integration for Zope2",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Communications",
+        "Topic :: System :: Distributed Computing",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Asko Soukka',
+    author_email='asko.soukka@iki.fi',
+    url='http://github.com/datakurre/collective.zamqp',
+    license='ZPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['publishmsg = collective.zamqp.cli:main']
+    },
+    extras_require={
+        'test': [
+            'rabbitfixture',
+            'plone.testing',
+            'zope.configuration',
+            'msgpack-python',
+        ],
+        'docs': [
+            'sphinx',
+            'collective.sphinx.includedoc',
+            'repoze.sphinx.autointerface',
+            'sphinxcontrib-plantuml',
+            'sphinxtogithub',
+        ]
+    },
+    install_requires=[
+        'setuptools',
+        'Zope2',
+        'ZODB3',
+        'transaction',
+        'zope.interface',
+        'zope.component',
+        'zope.publisher',
+        'zope.event',
+        'zope.processlifetime',
+        'grokcore.component',
+        'pika == 0.9.5',  # pika > 0.9.5 will require review and fixes
+        'zope.deprecation',
+        'z3c.unconfigure',  # only for sauna.reload-support
+    ]
+)
---
+++ packages/collective.zamqp/collective.zamqp-0.9.1.zip/collective.zamqp-0.9.1/src/collective/zamqp/testing.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+###
+# collective.zamqp
+#
+# Licensed under the ZPL license, see LICENCE.txt for more details.
+#
+# Copyrighted by University of Jyväskylä and Contributors.
+###
+"""Test fixtures"""
+
+from zope.configuration import xmlconfig
+
+from plone.testing import Layer, z2
+
+from rabbitfixture.server import (
+    RabbitServer,
+    RabbitServerResources
+    )
+
+
+class FixedHostname(RabbitServerResources):
+    """Allocate the resources a RabbitMQ server needs with the explicitly
+    defined hostname.
+    (Does not query the hostname from a socket as the default implementation
+    does.)
+    """
+
+    @property
+    def fq_nodename(self):
+        """The node of the RabbitMQ that is being exported."""
+        return '%s@%s' % (self.nodename, self.hostname)
+
+
+class Rabbit(Layer):
+
+    def setUp(self):
+        # setup a RabbitMQ
+        config = FixedHostname()
+        self['rabbit'] = RabbitServer(config=config)
+        self['rabbit'].setUp()
+        # define a shortcut to rabbitmqctl
+        self['rabbitctl'] = self['rabbit'].runner.environment.rabbitctl
+
+    def tearDown(self):
+        self['rabbit'].cleanUp()
+
+RABBIT_FIXTURE = Rabbit()
+
+RABBIT_APP_INTEGRATION_TESTING = z2.IntegrationTesting(
+    bases=(RABBIT_FIXTURE, z2.STARTUP), name='RabbitAppFixture:Integration')
+RABBIT_APP_FUNCTIONAL_TESTING = z2.FunctionalTesting(
+    bases=(RABBIT_FIXTURE, z2.STARTUP), name='RabbitAppFixture:Functional')
+
+
+class ZAMQP(Layer):
+    defaultBases = (RABBIT_FIXTURE, z2.STARTUP)
+
+    def setUp(self):
+        import collective.zamqp
+        xmlconfig.file('testing.zcml', collective.zamqp,
+                       context=self['configurationContext'])
+
+        from zope.component import getUtility
+        from collective.zamqp.interfaces import IBrokerConnection
+        connection = getUtility(IBrokerConnection, name="test.connection")
+        connection.port = self['rabbit'].config.port
+
+        # from collective.zamqp import connection
+        # connection.connect_all()
+
+    def tearDown(self):
+        pass
+
+
+ZAMQP_FIXTURE = ZAMQP()
+
+ZAMQP_INTEGRATION_TESTING = z2.IntegrationTesting(
+    bases=(ZAMQP_FIXTURE,), name='ZAMQPFixture:Integration')
+ZAMQP_FUNCTIONAL_TESTING = z2.FunctionalTesting(
+    bases=(ZAMQP_FIXTURE,), name='ZAMQPFixture:Functional')
