7a9c0414da3888e29d299a9350f82904e6160bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package simpleconfig/1.0/simpleconfig-1.0.tar.gz

---
+++ packages/simpleconfig/simpleconfig-1.0.tar.gz/simpleconfig-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simpleconfig',
+      version='1.0',
+      py_modules=['simpleconfig'],
+      description=('Dead simple configuration file parser.'),
+      author='Kyle Isom',
+      author_email='coder@kyleisom.net',
+      license='ISC',
+      url='http://kisom.github.com/pysimpleconfig',
+)
+
