451626a7537890994615cc552d4e7a923a085d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package plone.app.collection/2.0b4/plone.app.collection-2.0b4.zip

---
+++ packages/plone.app.collection/plone.app.collection-2.0b4.zip/plone.app.collection-2.0b4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0b4'
+
+setup(name='plone.app.collection',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("CHANGES.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Timo Stollenwerk - Plone Foundation',
+      author_email='contact@timostollenwerk.net',
+      url='http://github.com/plone/plone.app.collection',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.app.contentmenu',
+          'plone.formwidget.querystring',
+      ],
+      extras_require={
+          'test': [
+              'lxml',
+              'plone.app.testing',
+              'plone.act',
+              'robotsuite',
+              'robotframework-selenium2library',
+              'decorator',
+              'selenium',
+           ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
