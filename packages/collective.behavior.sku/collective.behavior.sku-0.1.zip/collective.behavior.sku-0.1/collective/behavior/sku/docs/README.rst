782b413368ccf3dc5132172458e1c3303b199f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package collective.behavior.sku/0.1/collective.behavior.sku-0.1.zip

---
+++ packages/collective.behavior.sku/collective.behavior.sku-0.1.zip/collective.behavior.sku-0.1/collective/behavior/sku/docs/README.rst
@@ -0,0 +1,34 @@
+=======================
+collective.behavior.sku
+=======================
+
+collective.behavior.sku provides behavior add SKU field to dexterity content types.
+
+Currently tested with
+---------------------
+
+* Plone-4.2.1 [taito]
+
+Behavior
+--------
+
+The behavior can be added through the web or directly through the file system to the dexterity content type xml file like::
+
+  <property name="behaviors">
+    ...
+    <element value="collective.behavior.sku.interfaces.ISKU" />
+    ...
+  </property>
+
+
+Farther Documentation URL
+-------------------------
+
+`http://packages.python.org/collective.behavior.sku/
+<http://packages.python.org/collective.behavior.sku/>`_
+
+Repository URL
+--------------
+
+`https://github.com/collective/collective.behavior.sku/
+<https://github.com/collective/collective.behavior.sku/>`_
---
+++ packages/collective.behavior.sku/collective.behavior.sku-0.1.zip/collective.behavior.sku-0.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+long_description = (
+    open(os.path.join("collective", "behavior", "sku", "docs", "README.rst")).read() + "\n" +
+    open(os.path.join("collective", "behavior", "sku", "docs", "HISTORY.rst")).read() + "\n" +
+    open(os.path.join("collective", "behavior", "sku", "docs", "CONTRIBUTORS.rst")).read())
+
+
+setup(
+    name='collective.behavior.sku',
+    version='0.1',
+    description="Behavior to add sku field.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@gmail.com',
+    url='https://github.com/collective/collective.behavior.sku/',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.behavior'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Plone>=4.2',
+        'five.grok',
+        'hexagonit.testing',
+        'plone.behavior',
+        'plone.directives.form',
+        'setuptools',
+        'zope.i18nmessageid'],
+    entry_points="""
+    # -*- Entry points: -*-
+
+    [z3c.autoinclude.plugin]
+    target = plone
+    """)
