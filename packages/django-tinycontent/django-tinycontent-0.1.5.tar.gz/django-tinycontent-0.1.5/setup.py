7d2f91c8fa3dace4abab29c4bd240e9943e38c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:00 +0000

    Add package django-tinycontent/0.1.5/django-tinycontent-0.1.5.tar.gz

---
+++ packages/django-tinycontent/django-tinycontent-0.1.5.tar.gz/django-tinycontent-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import tinycontent
+
+setup(
+    name='django-tinycontent',
+    version=tinycontent.__version__,
+    description="A Django app for managing re-usable blocks of tiny content.",
+    long_description=open('README.rst').read(),
+    author='Dominic Rodger',
+    author_email='internet@dominicrodger.com',
+    url='http://github.com/dominicrodger/django-tinycontent',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django==1.4.3",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+    tests_require=(
+        'django-setuptest==0.1.2',
+    ),
+    test_suite='setuptest.setuptest.SetupTestSuite',
+)
