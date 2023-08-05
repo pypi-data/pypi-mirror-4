f15628cd46984c6f7f886dc3ec3f911925d9b9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package jeetpatel/1.0.1/jeetpatel-1.0.1.zip

---
+++ packages/jeetpatel/jeetpatel-1.0.1.zip/jeetpatel-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'jeetpatel',
+version = '1.0.1',
+py_modules = ['jeetpatel'],
+author = 'jeet',
+author_email = 'pateljeet@tapandtype.com',
+url = 'http://www.tapandtype.com',
+description = 'A simple program for addition and subtraction',
+)
