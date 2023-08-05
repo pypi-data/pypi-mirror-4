7d7c3f8048a958f31289f80ca372fbc947f50cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:37 +0000

    Add package collective.richdescription/1.0/collective.richdescription-1.0.tar.gz

---
+++ packages/collective.richdescription/collective.richdescription-1.0.tar.gz/collective.richdescription-1.0/docs/HISTORY.rst
@@ -0,0 +1,20 @@
+Changelog
+=========
+
+1.0 (2012-11-07)
+----------------
+
+- Initial release
+
+1.0pre
+------
+
+- When no description is set, return a BaseUnit as fallback. Fixes
+  AttributeError, where original_encoding was tried to accessed on a string
+  value.
+  [thet]
+
+- Register the skins folder for any theme.
+  [thet]
+
+- Initial version on 2010-10-05 for sfd.at.
---
+++ packages/collective.richdescription/collective.richdescription-1.0.tar.gz/collective.richdescription-1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.richdescription',
+      version=version,
+      description="Richtext/HTML Description field for Archetypes",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'zope.component',
+          'zope.interface',
+          'archetypes.schemaextender',
+          'Products.Archetypes',
+      ],
+      extras_require={'test': ['interlude',]},
+      )
