35ab2dc1650d9cf6714da433807a0aec9dbc7ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package tgext.pluggable/0.1.1/tgext.pluggable-0.1.1.tar.gz

---
+++ packages/tgext.pluggable/tgext.pluggable-0.1.1.tar.gz/tgext.pluggable-0.1.1/tgext/pluggable/__init__.py
@@ -0,0 +1,5 @@
+from plug import plug, plugged
+from session_wrapper import PluggableSession
+from utils import call_partial, plug_url, plug_redirect
+from template_replacements import replace_template
+from adapt_models import app_model, primary_key
