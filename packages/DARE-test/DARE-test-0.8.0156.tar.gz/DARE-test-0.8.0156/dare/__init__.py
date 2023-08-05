214ead22584559eb2ff5f9477f5aab137ef59137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package DARE-test/0.8.0156/DARE-test-0.8.0156.tar.gz

---
+++ packages/DARE-test/DARE-test-0.8.0156.tar.gz/DARE-test-0.8.0156/dare/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+__author__ = "Sharath Maddineni"
+__copyright_ = "Copyright 2011-2012, Sharath Maddineni"
+__license__ = "MIT"
+
+__version_info__ = ('0', '8', '0156')
+__version__ = '.'.join(__version_info__)
+
+import os
+import ConfigParser
+
+from .helpers.misc import darelogger
+
+darelogger.info("Loading DARE version: " + __version__)
+
+try:
+    _conf_file = os.path.expanduser('~/.darerc')
+except:
+    darelogger.error("Cannot find home directory")
+
+if not os.path.exists(_conf_file):
+    _conf_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'dare.conf')
+
+darelogger.info("Loading settings in %s" % _conf_file)
+
+cfgparser = ConfigParser.ConfigParser()
+cfgparser.read(_conf_file)
+cfgdict = cfgparser.defaults()
+COORDINATION_URL = str(cfgdict.get('coordination_url'))
+
+darelogger.info("Bigjob COORDINATION_URL: %s " % COORDINATION_URL)
