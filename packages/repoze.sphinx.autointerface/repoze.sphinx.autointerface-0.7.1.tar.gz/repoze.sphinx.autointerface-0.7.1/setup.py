316c003f4d4046ba2f9308030f7d54c2de57d00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:46 +0000

    Add package repoze.sphinx.autointerface/0.7.1/repoze.sphinx.autointerface-0.7.1.tar.gz

---
+++ packages/repoze.sphinx.autointerface/repoze.sphinx.autointerface-0.7.1.tar.gz/repoze.sphinx.autointerface-0.7.1/setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2008 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(name='repoze.sphinx.autointerface',
+      version='0.7.1',
+      description='Sphinx extension: auto-generates API docs '
+                  'from Zope interfaces',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        ],
+      keywords='web wsgi zope',
+      author="Agendaless Consulting",
+      author_email="repoze-dev@lists.repoze.org",
+      url="http://www.repoze.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['repoze', 'repoze.sphinx'],
+      zip_safe=False,
+      tests_require = [],
+      install_requires=['zope.interface',
+                        'Sphinx>=0.6.1',
+                        'setuptools',
+                       ],
+      #test_suite="repoze.",
+      )
+
