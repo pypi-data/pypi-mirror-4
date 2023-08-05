401079116a29019d3e6cfe1ab72beb77c60a3ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package pydi/0.3.0/pydi-0.3.0.tar.gz

---
+++ packages/pydi/pydi-0.3.0.tar.gz/pydi-0.3.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+packages = [
+    'pydi'
+]
+
+requires = []
+
+setup(
+    name='pydi',
+    version='0.3.0',
+    description='Little Dependency Injection Container',
+    long_description=open('README.md').read(),
+    author='Adam Venturella',
+    author_email='aventurella@gmail.com',
+    url='http://github.com/aventurella/pydi',
+    license=open('LICENSE').read(),
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    package_dir={'pydi': 'pydi'},
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+    ),
+
+)
+
+del os.environ['PYTHONDONTWRITEBYTECODE']
