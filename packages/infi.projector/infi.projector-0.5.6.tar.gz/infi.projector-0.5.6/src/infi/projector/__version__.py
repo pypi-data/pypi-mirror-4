5509a6c3c456b38a146cb5c2e9d9380b86b4d184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package infi.projector/0.5.6/infi.projector-0.5.6.tar.gz

---
+++ packages/infi.projector/infi.projector-0.5.6.tar.gz/infi.projector-0.5.6/src/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3062efe23c077fcd624d884209254b9450404a38'
+__git_head_subject__ = 'Finished release v0.5.6.'
+__git_head_message__ = '* release/v0.5.6:\n  HOSTDEV-571 requirements of == will stay as is in freeze requirements\n  Unfreezing dependencies\n  empty commit after version v0.5.5'
+__git_dirty_diff__ = ''
