49df2ea56998b47a6e265bd1f61b16b8ace3dd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package culinary/0.1.0/culinary-0.1.0.zip

---
+++ packages/culinary/culinary-0.1.0.zip/culinary-0.1.0/culinary/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+from . import (command, core, dir, file, group, misc, mode, option, package,
+               process, python, repository, ssh, text, user)
+
+
+# Sets up the default options so that @dispatch'ed functions work
+def _init():
+    for key, value in option.DEFAULT_OPTIONS.items():
+        getattr(option, 'select_' + key)(value)
+
+_init()
---
+++ packages/culinary/culinary-0.1.0.zip/culinary-0.1.0/culinary/decorator.py
@@ -0,0 +1,67 @@
+import functools
+import types
+
+import fabric.api
+
+import culinary
+
+
+def dispatch(prefix=None):
+    """
+    Dispatches the current function to specific implementation. The `prefix`
+    parameter indicates the common option prefix, and the `option_select()`
+    function will determine the function suffix.
+
+    For instance the package functions are defined like that:
+
+    {{{
+    @dispatch("package")
+    def ensure(...):
+        ...
+    def ensure_apt(...):
+        ...
+    def ensure_yum(...):
+        ...
+    }}}
+
+    and then when a user does
+
+    {{{
+    option.select("package", "yum")
+    package.ensure(...)
+    }}}
+
+    then the `dispatch` function will dispatch `package_ensure` to
+    `ensure_yum`.
+
+    If your prefix is the first word of the function name before the
+    first `_` then you can simply use `@dispatch` without parameters.
+    """
+    def dispatch_wrapper(function, prefix=prefix):
+        def wrapper(*args, **kwargs):
+            function_name = function.__name__
+            _prefix = prefix or function_name.split("_")[0].replace(".", "_")
+            select = fabric.api.env.get("OPTION_" + _prefix.upper())
+            assert select, \
+                ("No option defined for: %s, call select_%s(<YOUR OPTION>) "
+                 "to set it") % (_prefix.upper(),
+                                 _prefix.lower().replace(".", "_"))
+            function_name = function.__name__ + "_" + select
+            function_name = function.__module__ + "." + function_name
+            specific = eval(function_name)
+            if specific:
+                if type(specific) == types.FunctionType:
+                    return specific(*args, **kwargs)
+                else:
+                    raise Exception("Function expected for: " + function_name)
+            else:
+                raise Exception("Function variant not defined: " \
+                        + function_name)
+        # We copy name and docstring
+        functools.update_wrapper(wrapper, function)
+        return wrapper
+
+    if type(prefix) == types.FunctionType:
+        return dispatch_wrapper(prefix, None)
+    else:
+        return dispatch_wrapper
