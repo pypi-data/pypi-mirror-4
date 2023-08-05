6e13bb5fb16ebc46e3cd7a25a0bc90c349b5bf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package ziggurat-foundations/0.3/ziggurat_foundations-0.3.tar.gz

---
+++ packages/ziggurat-foundations/ziggurat_foundations-0.3.tar.gz/ziggurat_foundations-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import sys
+setup(name='ziggurat_foundations',
+      version='0.3',
+      description=""" Set of classes that are reusable across various types of
+      web apps, base user object, auth relationships + structured resource tree
+      """,
+      author='Marcin Lulek',
+      author_email='info@webreactor.eu',
+      license='BSD',
+      packages=find_packages(),
+      zip_safe=True,
+      include_package_data=True,
+      package_data={
+        '': ['*.txt', '*.rst', '*.ini'],
+        'ziggurat_foundations': ['migrations/versions/*.py'],
+        },
+      test_suite='ziggurat_foundations.tests',
+      install_requires=["sqlalchemy", "cryptacular", 'webhelpers']
+      )
