460829a8eccc7d215da1ed888f3487e674d51fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:39 +0000

    Add package bisheng/0.9.0/bisheng-0.9.0.tar.gz

---
+++ packages/bisheng/bisheng-0.9.0.tar.gz/bisheng-0.9.0/setup.py
@@ -0,0 +1,16 @@
+import bisheng
+
+from distutils.core import setup
+
+
+setup(
+    name='bisheng',
+    version=bisheng.__version__,
+    url = 'http://github.com/eliangcs/bisheng/',
+    description='Adds spaces between Chinese/Japanese/Korean and halfwidth characters.',
+    long_description=open('README.rst').read(),
+    author='Chang-Hung Liang',
+    author_email='eliang.cs@gmail.com',
+    license='BSD',
+    packages=['bisheng'],
+)
