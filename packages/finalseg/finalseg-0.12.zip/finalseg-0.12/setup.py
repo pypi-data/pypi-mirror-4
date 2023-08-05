8751385e948f1ca3750710d8a1f391b83e872af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:39 +0000

    Add package finalseg/0.12/finalseg-0.12.zip

---
+++ packages/finalseg/finalseg-0.12.zip/finalseg-0.12/setup.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+ 
+from setuptools import setup, find_packages
+ 
+setup(
+    name='finalseg', #项目名
+    version="0.12", #版本号
+    description="A Chinese Words Segmentation Library", #介绍
+    author="Sun, Junyi",
+    author_email="ccnusjy@gmail.com",
+    packages = ['finalseg'],   #要打包的文件夹名字
+    zip_safe=False,          #不压缩
+    include_package_data=True,
+    install_requires = [
+        
+    ],
+    entry_points = {
+        
+    },
+)
