3e2c68447192e9cfc7e8bfe849f6d4a97556731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package cs.donedukia/2.1.1/cs.donedukia-2.1.1.zip

---
+++ packages/cs.donedukia/cs.donedukia-2.1.1.zip/cs.donedukia-2.1.1/cs/donedukia/interfaces.py
@@ -0,0 +1,6 @@
+from Products.ATContentTypes.interface import IATDocument, IATFolder
+# -*- extra stuff goes here -*-
+
+class IDonEdukia(IATDocument, IATFolder):
+    """A Folderish page"""
+
---
+++ packages/cs.donedukia/cs.donedukia-2.1.1.zip/cs.donedukia-2.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1.1'
+
+setup(name='cs.donedukia',
+      version=version,
+      description="Folderish pages to simplify plone page-folder story",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone page folderish',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://github.com/codesyntax/cs.donedukia',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone >= 4.0',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
