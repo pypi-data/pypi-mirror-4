7054bc9a56abe021d815b36fa7d97ce2c83e5b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package wheel/0.9.5/wheel-0.9.5.tar.gz

---
+++ packages/wheel/wheel-0.9.5.tar.gz/wheel-0.9.5/setup.py
@@ -0,0 +1,41 @@
+import os, codecs
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = codecs.open(os.path.join(here, 'README.txt'), encoding='utf8').read()
+CHANGES = codecs.open(os.path.join(here, 'CHANGES.txt'), encoding='utf8').read()
+
+setup(name='wheel',
+      version='0.9.5',
+      description='A built-package format for Python.',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        ],
+      author='Daniel Holth',
+      author_email='dholth@fastmail.fm',
+      url='http://bitbucket.org/dholth/wheel/',
+      keywords='wheel packaging',
+      license='MIT',
+      packages=['wheel', 'wheel.test', 'wheel.tool', 'wheel.signatures'], 
+      install_requires=['distribute>=0.6.28', 'markerlib'],
+      extras_require={
+          'signatures': [],
+          'faster-signatures': ['ed25519ll'], 
+          'tool': ['keyring', 'dirspec']
+          },
+      include_package_data=True,
+      zip_safe=False,
+      test_suite = 'nose.collector',
+      entry_points = open(os.path.join(here, 'entry_points.txt')).read()
+      )
+
---
+++ packages/wheel/wheel-0.9.5.tar.gz/wheel-0.9.5/wheel/util.py
@@ -0,0 +1,171 @@
+"""Utility functions."""
+
+import sys
+import base64
+import json
+import hashlib
+try:
+    import sysconfig
+except ImportError:  # pragma nocover
+    # Python < 2.7
+    import distutils.sysconfig as sysconfig
+from distutils.util import get_platform
+
+__all__ = ['urlsafe_b64encode', 'urlsafe_b64decode', 'utf8', 'to_json',
+           'from_json', 'generate_supported', 'get_abbr_impl', 'get_impl_ver',
+           'compatibility_match']
+
+
+def urlsafe_b64encode(data):
+    """urlsafe_b64encode without padding"""
+    return base64.urlsafe_b64encode(data).rstrip(binary('='))
+
+
+def urlsafe_b64decode(data):
+    """urlsafe_b64decode without padding"""
+    pad = b'=' * (4 - (len(data) & 3))
+    return base64.urlsafe_b64decode(data + pad)
+
+
+def to_json(o):
+    '''Convert given data to JSON.'''
+    return json.dumps(o, sort_keys=True)
+
+
+def from_json(j):
+    '''Decode a JSON payload.'''
+    return json.loads(j)
+
+def open_for_csv(name, mode):
+    if sys.version_info[0] < 3:
+        nl = {}
+        bin = 'b'
+    else:
+        nl = { 'newline': '' }
+        bin = ''
+    return open(name, mode + bin, **nl)
+
+try:
+    unicode
+
+    def utf8(data):
+        '''Utf-8 encode data.'''
+        if isinstance(data, unicode):
+            return data.encode('utf-8')
+        return data
+except NameError:
+    def utf8(data):
+        '''Utf-8 encode data.'''
+        if isinstance(data, str):
+            return data.encode('utf-8')
+        return data
+
+
+try:
+    # For encoding ascii back and forth between bytestrings, as is repeatedly
+    # necessary in JSON-based crypto under Python 3
+    unicode
+    def native(s):
+        return s
+    def binary(s):
+        if isinstance(s, unicode):
+            return s.encode('ascii')
+        return s
+except NameError:
+    def native(s):
+        if isinstance(s, bytes):
+            return s.decode('ascii')
+        return s
+    def binary(s):
+        if isinstance(s, str):
+            return s.encode('ascii')
+
+
+def get_abbr_impl():
+    """Return abbreviated implementation name."""
+    if hasattr(sys, 'pypy_version_info'):
+        pyimpl = 'pp'
+    elif sys.platform.startswith('java'):
+        pyimpl = 'jy'
+    elif sys.platform == 'cli':
+        pyimpl = 'ip'
+    else:
+        pyimpl = 'cp'
+    return pyimpl
+
+
+def get_impl_ver():
+    '''Return implementation version.'''
+    impl_ver = sysconfig.get_config_var("py_version_nodot")
+    if not impl_ver:
+        impl_ver = ''.join(map(str, sys.version_info[:2]))
+    return impl_ver
+
+
+def generate_supported(versions=None):
+    '''Generate supported tags for each version specified in `versions`.
+
+    Versions must be given with respect to preference from best to worst.
+    If `versions` is None, then the current version is assumed.
+    Returned tags are sorted from best-matching tags to worst. All tags
+    returned should be compatible with the machine.
+    '''
+    supported = []
+    
+    # Versions must be given with respect to the preference
+    if versions is None:
+        versions = []
+        major = sys.version_info[0]
+        # Support all previous minor Python versions.
+        for minor in range(sys.version_info[1], -1, -1):
+            versions.append(''.join(map(str, (major, minor))))
+            
+    impl = get_abbr_impl()
+    
+    abis = ['none']
+    
+    soabi = sysconfig.get_config_var('SOABI')
+    if soabi and soabi.startswith('cpython-'):
+        abis[0:0] = ['cp' + soabi.split('-', 1)[-1]]
+    
+    arch = get_platform().replace('.', '_').replace('-', '_')
+    
+    # Current version, current API (built specifically for our Python):
+    for abi in abis:
+        supported.append(('%s%s' % (impl, versions[0]), abi, arch))
+            
+    # No abi / arch, but requires our implementation:
+    for i, version in enumerate(versions):
+        supported.append(('%s%s' % (impl, version), 'none', 'any'))
+        if i == 0:
+            # Tagged specifically as being cross-version compatible 
+            # (with just the major version specified)
+            supported.append(('%s%s' % (impl, versions[0][0]), 'none', 'any')) 
+            
+    # No abi / arch, generic Python
+    for i, version in enumerate(versions):
+        supported.append(('py%s' % (version,), 'none', 'any'))
+        if i == 0:
+            supported.append(('py%s' % (version[0]), 'none', 'any'))
+        
+    return supported
+
+
+class HashingFile(object):
+    def __init__(self, fd, hashtype='sha256'):
+        self.fd = fd
+        self.hashtype = hashtype
+        self.hash = hashlib.new(hashtype)
+        self.length = 0
+    def read(self, n):
+        data = self.fd.read(n)
+        self.hash.update(data)
+        self.length += len(data)
+        return data
+    def close(self):
+        self.fd.close()
+    def digest(self):
+        if self.hashtype == 'md5':
+            return self.hash.hexdigest()
+        digest = self.hash.digest()
+        return self.hashtype + '=' + native(urlsafe_b64encode(digest))
