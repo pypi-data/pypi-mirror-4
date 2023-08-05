c38c62b9a2ca46145d8e38fc6ebd67502f68c3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package chompy/0.0.1/chompy-0.0.1.tar.gz

---
+++ packages/chompy/chompy-0.0.1.tar.gz/chompy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'chompy',
+    version = '0.0.1',
+    description = 'A chomp function for Python',
+    author = 'Brian Lauber',
+    author_email = 'constructible.truth@gmail.com',
+    packages = ['chompy'],
+    test_suite = 'tests',
+)
+
