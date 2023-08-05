6ff89b267ec415a9ff16f429f70f94f8be2ad66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:10 +0000

    Add package clever-alexis/0.1.1.dev3/clever-alexis-0.1.1.dev3.tar.gz

---
+++ packages/clever-alexis/clever-alexis-0.1.1.dev3.tar.gz/clever-alexis-0.1.1.dev3/README.rst
@@ -0,0 +1,44 @@
+Alexis
+============
+
+Clever redhead girl that builds and packs Python project with Virtualenv into rpm, deb, etc.
+
+Quick start
+------------
+
+Install using pip
+
+``$ pip install clever-alexis``
+
+or `download the latest release`_ add run setup.py script
+
+``$ python setup.py install``
+
+Look at `example/fabfile.py`
+
+
+Local dependencies
+------------------
+
+* fabric_ (>=1.4.0)
+* python_ (2.x only)
+
+Server dependencies
+-------------------
+
+* fpm_ (>=0.4.20)
+* python_ (>=2.5)
+* hg_ (>=2.0)
+
+Code includes
+-------------
+* virtualenv.py_ (1.8.2)
+* clonevirtualenv.py_ (0.2.4)
+
+.. _download the latest release: https://bitbucket.org/KulaPard/clever-alexis/downloads
+.. _fabric: https://github.com/fabric/fabric
+.. _fpm: https://github.com/jordansissel/fpm
+.. _python: http://python.org/download/
+.. _hg: http://mercurial.selenic.com/
+.. _virtualenv.py: https://github.com/pypa/virtualenv
+.. _clonevirtualenv.py: https://github.com/edwardgeorge/virtualenv-clone
---
+++ packages/clever-alexis/clever-alexis-0.1.1.dev3.tar.gz/clever-alexis-0.1.1.dev3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup
+
+from alexis import __version__
+
+if sys.version_info >= (3,):
+    sys.stderr.write('Sorry! Not ready for Python 3.x')
+    sys.exit(1)
+
+readme = open('README.rst', 'r').read()
+
+setup(
+    name='clever-alexis',
+    version=__version__,
+    description='Clever redhead girl that builds and packs Python project with Virtualenv into rpm, deb, etc.',
+    author='Taras Drapalyuk (KulaPard)',
+    author_email='kulapard@gmail.com',
+    url='https://bitbucket.org/KulaPard/alexis',
+#    packages=['alexis', 'alexis.libs'],
+#    include_dirs=['alexis/libs',],
+#    requires=['fabric(>=1.4.0)'],
+    install_requires=['fabric>=1.4.0', ],
+    py_modules=['alexis'],
+#    scripts=['ypload'],
+    long_description=readme,
+    license='BSD License',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
