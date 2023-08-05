e82b978618820af1638fd7905622d53485d6f9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package unittest-utils/0.1.1/unittest-utils-0.1.1.tar.gz

---
+++ packages/unittest-utils/unittest-utils-0.1.1.tar.gz/unittest-utils-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='unittest-utils',
+    version='0.1.1',
+    description='Some custom utils for unittest2',
+    long_description=open('README.rst').read(-1),
+    author='',
+    author_email='',
+    url='',
+    install_requires=[
+        "unittest2",
+        "termcolor",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts':
+            ['unittest-utils=unittest_utils.main:_main']
+    }
+)
