93e402d357adfd77cd95e4f5fd369134720e1c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:49 +0000

    Add package zdfm/0.4.4/zdfm-0.4.4.tar.gz

---
+++ packages/zdfm/zdfm-0.4.4.tar.gz/zdfm-0.4.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name="zdfm",
+      version="0.4.4",
+      description="Downloader for the ZDF Mediathek",
+      author="Marian Beermann",
+      author_email="public@enkore.de",
+      license="MIT",
+
+      py_modules=["zdfm"],
+      entry_points={
+          "console_scripts": ["zdfm = zdfm:main"],
+      },
+      install_requires=[
+        "lxml",
+      ]
+     )
