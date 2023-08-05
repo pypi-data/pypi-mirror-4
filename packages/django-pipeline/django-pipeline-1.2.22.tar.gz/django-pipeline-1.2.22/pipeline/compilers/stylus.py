2ac4e87ca75e0ff3ccd585a0e382573128d53e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package django-pipeline/1.2.22/django-pipeline-1.2.22.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.2.22.tar.gz/django-pipeline-1.2.22/pipeline/compilers/stylus.py
@@ -0,0 +1,20 @@
+from os.path import dirname
+
+from pipeline.conf import settings
+from pipeline.compilers import SubProcessCompiler
+
+
+class StylusCompiler(SubProcessCompiler):
+    output_extension = 'css'
+
+    def match_file(self, filename):
+        return filename.endswith('.styl')
+
+    def compile_file(self, infile, outfile, outdated=False, force=False):
+        command = "%s %s < %s > %s" % (
+            settings.PIPELINE_STYLUS_BINARY,
+            settings.PIPELINE_STYLUS_ARGUMENTS,
+            infile,
+            outfile
+        )
+        return self.execute_command(command, cwd=dirname(infile))
