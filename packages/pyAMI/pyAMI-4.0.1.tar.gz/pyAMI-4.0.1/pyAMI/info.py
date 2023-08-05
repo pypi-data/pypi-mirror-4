5beb211d3df6d6b64d1a85cae867376c7e94e5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:10 +0000

    Add package pyAMI/4.0.1/pyAMI-4.0.1.tar.gz

---
+++ packages/pyAMI/pyAMI-4.0.1.tar.gz/pyAMI-4.0.1/pyAMI/info.py
@@ -0,0 +1,23 @@
+"""
+                _    __  __ ___
+ _ __  _   _   / \  |  \/  |_ _|
+| '_ \| | | | / _ \ | |\/| || |
+| |_) | |_| |/ ___ \| |  | || |
+| .__/ \__, /_/   \_\_|  |_|___|
+|_|    |___/
+                   %s
+Authors:
+
+Solveig Albrand
+Noel Dawe
+Jerome Fulachier
+Fabian Lambert
+
+"""
+
+VERSION = '4.0.1'
+AUTHOR_EMAIL = 'atlas-bookkeeping@cern.ch'
+URL = 'https://end.web.cern.ch/end/projects/pyAMI/'
+DOWNLOAD_URL = 'http://pypi.python.org/packages/source/p/pyAMI/pyAMI-%s.tar.gz' % VERSION
+
+__doc__ %= VERSION
