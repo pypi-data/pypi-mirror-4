f4f906056e0de48f843e42534b9af72c2aae91b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:51 +0000

    Add package pyramid/1.4a1/pyramid-1.4a1.tar.gz

---
+++ packages/pyramid/pyramid-1.4a1.tar.gz/pyramid-1.4a1/pyramid/chameleon_text.py
@@ -0,0 +1,43 @@
+import sys
+
+from zope.interface import implementer
+
+from chameleon.zpt.template import PageTextTemplateFile
+
+from pyramid.interfaces import ITemplateRenderer
+
+from pyramid.decorator import reify
+from pyramid import renderers
+
+def renderer_factory(info):
+    return renderers.template_renderer_factory(info, TextTemplateRenderer)
+
+@implementer(ITemplateRenderer)
+class TextTemplateRenderer(object):
+    def __init__(self, path, lookup, macro=None):
+        self.path = path
+        self.lookup = lookup
+        # text template renderers have no macros, so we ignore the
+        # macro arg
+
+    @reify # avoid looking up reload_templates before manager pushed
+    def template(self):
+        if sys.platform.startswith('java'): # pragma: no cover
+            raise RuntimeError(
+                'Chameleon templates are not compatible with Jython')
+        return PageTextTemplateFile(self.path,
+                                    auto_reload=self.lookup.auto_reload,
+                                    debug=self.lookup.debug,
+                                    translate=self.lookup.translate)
+
+    def implementation(self):
+        return self.template
+    
+    def __call__(self, value, system):
+        try:
+            system.update(value)
+        except (TypeError, ValueError):
+            raise ValueError('renderer was passed non-dictionary as value')
+        result = self.template(**system)
+        return result
+
