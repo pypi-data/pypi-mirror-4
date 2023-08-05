33202c5fbf3ca6972f96999341612d950efaa3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package wheezy.security/0.1.40/wheezy.security-0.1.40.tar.gz

---
+++ packages/wheezy.security/wheezy.security-0.1.40.tar.gz/wheezy.security-0.1.40/src/wheezy/security/__init__.py
@@ -0,0 +1,7 @@
+
+""" ``security`` package.
+"""
+
+from wheezy.security.authorization import authorized
+from wheezy.security.errors import SecurityError
+from wheezy.security.principal import Principal
