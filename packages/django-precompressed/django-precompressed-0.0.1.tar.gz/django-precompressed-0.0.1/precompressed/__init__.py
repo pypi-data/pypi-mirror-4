1236812c270fb0265eb1e169d975fedfac48d776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package django-precompressed/0.0.1/django-precompressed-0.0.1.tar.gz

---
+++ packages/django-precompressed/django-precompressed-0.0.1.tar.gz/django-precompressed-0.0.1/precompressed/__init__.py
@@ -0,0 +1,11 @@
+# *****************************************************************************
+# precompressed/__init__.py
+# *****************************************************************************
+
+"""
+precompressed is a pluggable application for Django developers who want
+to serve precompressed (gzipped) static files.
+
+"""
+
+__version__ = (0, 0, 1)
