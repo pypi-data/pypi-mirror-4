04acfb31109f791429f56233432280c3ee9fe77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package CodeSkel/1.8.8/CodeSkel-1.8.8.zip

---
+++ packages/CodeSkel/CodeSkel-1.8.8.zip/CodeSkel-1.8.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '1.8.8'
+
+setup(name='CodeSkel',
+      version=version,
+      description="A collection of skeletons for quickstarting CS projects.",
+      long_description=open("README.rst").read() + "\n" +
+                       open('HISTORY.TXT', 'r').read(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope command-line skeleton project',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://github.com/codesyntax/CodeSkel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+         'ZopeSkel <= 2.99',
+         'zopeskel.dexterity',
+      ],
+      entry_points="""
+      [paste.paster_create_template]
+      cs_plone3_theme = codeskel:Plone3Theme
+      cs_plone4_buildout = codeskel:Plone4Buildout
+      csdexterity = codeskel:CSDexterity
+
+      [zopeskel.zopeskel_sub_template]
+      cs_dexterity_content = codeskel:DexterityContentCS
+      """,
+      )
