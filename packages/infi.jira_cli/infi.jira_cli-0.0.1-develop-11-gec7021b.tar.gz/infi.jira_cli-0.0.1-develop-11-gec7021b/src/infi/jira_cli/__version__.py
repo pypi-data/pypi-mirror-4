ab005013688ebeb55a22866aa2e391123f7bc50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:10 +0000

    Add package infi.jira_cli/0.0.1-develop-11-gec7021b/infi.jira_cli-0.0.1-develop-11-gec7021b.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.0.1-develop-11-gec7021b.tar.gz/infi.jira_cli-0.0.1-develop-11-gec7021b/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1-develop-11-gec7021b"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@gitserver:host/infi-jira-cli.git'
+__git_head_hash__ = 'ec7021b6b148fb286166a5f0a94d5ab0983fa6d8'
+__git_head_subject__ = 'HOSTDEV-556 jissue create --short implementation'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
