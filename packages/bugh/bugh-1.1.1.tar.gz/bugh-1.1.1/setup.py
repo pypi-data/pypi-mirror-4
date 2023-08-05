a38d47d547e91df8c0a06ac19d67bacae3600ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package bugh/1.1.1/bugh-1.1.1.tar.gz

---
+++ packages/bugh/bugh-1.1.1.tar.gz/bugh-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+		name='bugh',
+		version='1.1.1',
+		description='Create read-only backups of your Github repositories.',
+		author='Will Haldean Brown',
+		author_email='will.h.brown@gmail.com',
+		url='https://github.com/haldean/bugh',
+		scripts=['bugh'],
+		install_requires=['sh', 'requests'],
+		)
