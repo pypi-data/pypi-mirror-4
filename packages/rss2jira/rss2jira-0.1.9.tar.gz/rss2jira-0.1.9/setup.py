dbaef0543bb32791306e03010172222eb3b248fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:00 +0000

    Add package rss2jira/0.1.9/rss2jira-0.1.9.tar.gz

---
+++ packages/rss2jira/rss2jira-0.1.9.tar.gz/rss2jira-0.1.9/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='rss2jira',
+    version='0.1.9',
+    description='Create JIRA issues from RSS entries matching key terms.',
+    author='nnutter',
+    author_email='iam@nnutter.com',
+    url='https://github.com/nnutter/rss2jira',
+    packages=['rss2jira'],
+    install_requires=['PyYAML>=3.10', 'feedparser>=5.1.2', 'jira-python>=0.12'],
+    scripts=['bin/rss2jira'],
+    include_package_data=True,
+    license='BSD',
+    keywords=['rss', 'jira', 'rss2jira'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+)
