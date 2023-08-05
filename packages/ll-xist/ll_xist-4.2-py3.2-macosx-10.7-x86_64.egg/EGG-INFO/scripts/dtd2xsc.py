86feb231ef2bd2cb56d7a2a1014ec36bccf72e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package ll-xist/4.2/ll_xist-4.2-py3.2-macosx-10.7-x86_64.egg

---
+++ packages/ll-xist/ll_xist-4.2-py3.2-macosx-10.7-x86_64.egg/EGG-INFO/scripts/dtd2xsc.py
@@ -0,0 +1,10 @@
+#!/Users/walter/.local/bin/python3.2
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.xist.scripts import dtd2xsc
+	sys.exit(dtd2xsc.main())
---
+++ packages/ll-xist/ll_xist-4.2-py3.2-macosx-10.7-x86_64.egg/EGG-INFO/scripts/oramerge.py
@@ -0,0 +1,10 @@
+#!/Users/walter/.local/bin/python3.2
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.orasql.scripts import oramerge
+	sys.exit(oramerge.main())
