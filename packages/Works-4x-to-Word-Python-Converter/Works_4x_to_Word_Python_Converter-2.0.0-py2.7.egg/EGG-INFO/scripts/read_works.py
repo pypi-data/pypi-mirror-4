61a350a514999c4f091dc20b05cd8d5e0cac93dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:19 +0000

    Add package Works-4x-to-Word-Python-Converter/2.0.0/Works_4x_to_Word_Python_Converter-2.0.0-py2.7.egg

---
+++ packages/Works-4x-to-Word-Python-Converter/Works_4x_to_Word_Python_Converter-2.0.0-py2.7.egg/EGG-INFO/scripts/read_works.py
@@ -0,0 +1,20 @@
+def read_ms_works_file(file):
+    text = []
+    started = False
+    with open(file, 'rb') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.endswith('\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\r\n'):
+                started = True
+                continue
+            #elif line.strip().startswith('\x00')  and started: break
+            #elif line.
+            if started:
+                if line.strip().startswith('\x00') or line.strip().endswith('\x00'): break
+                text.append(line)
+    return text[2:]        
+        
+
+if __name__ == '__main__':
+    text = read(r'C:\users\wilde\desktop\a sweet and bitter stew.wps')
+    print text
