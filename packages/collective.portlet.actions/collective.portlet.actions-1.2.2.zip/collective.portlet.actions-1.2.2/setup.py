d91a830a600d63331e37ca3031a1797d5d7b8219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package collective.portlet.actions/1.2.2/collective.portlet.actions-1.2.2.zip

---
+++ packages/collective.portlet.actions/collective.portlet.actions-1.2.2.zip/collective.portlet.actions-1.2.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='collective.portlet.actions',
+    version='1.2.2',
+    description="A portlet that provides the links of an action category",
+    long_description=(open("README.rst").read().strip() + "\n\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read().strip()),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone :: 3.2",
+    "Framework :: Plone :: 3.3",
+    "Framework :: Plone :: 4.0",
+    "Framework :: Plone :: 4.1",
+    "Framework :: Plone :: 4.2",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='plone portlet',
+    author='Gilles Lenfant',
+    author_email='gilles.lenfant@alterway.fr',
+    url='http://plone.org/products/collective-portlet-actions',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.portlet'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    'setuptools',
+    # -*- Extra requirements: -*-
+    'Acquisition',
+    'Products.CMFCore',
+    'Zope2',
+    'plone.app.portlets',
+    'plone.memoize',
+    'plone.portlets',
+    'zope.formlib',
+    'zope.schema',
+    ],
+    extras_require={'test': ['Products.PloneTestCase']},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """
+    )
