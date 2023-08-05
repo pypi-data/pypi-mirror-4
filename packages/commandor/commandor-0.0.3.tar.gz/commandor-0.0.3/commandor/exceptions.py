421a93b932bb107f258ef26fa6fe9d9c0536dde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package commandor/0.0.3/commandor-0.0.3.tar.gz

---
+++ packages/commandor/commandor-0.0.3.tar.gz/commandor-0.0.3/commandor/exceptions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:  utf-8 -*-
+"""
+commandor.exceptions
+~~~~~~~~~~~~~~~~~~~
+
+Commandor library exceptions
+
+:copyright: (c) 2012 by Alexandr Lispython (alex@obout.ru).
+:license: BSD, see LICENSE for more details.
+:github: http://github.com/Lispython/commandor
+"""
+
+class InvalidCommand(Exception):
+    """Invalid console command
+    """
