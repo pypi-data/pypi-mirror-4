21ce5e68a031304cca8c136f721d509aea1b9b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:10 +0000

    Add package infi.recipe.application-packager/0.2.3/infi.recipe.application_packager-0.2.3-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.2.3-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '35a47d5426c0cf4e37f4d1281dcda338823e5860'
+__git_head_subject__ = 'Finished release v0.2.3.'
+__git_head_message__ = '* release/v0.2.3:\n  TRIVIAL fixing pre-compilation of some packages such as python-cjson\n  empty commit after version v0.2.2'
+__git_dirty_diff__ = ''
