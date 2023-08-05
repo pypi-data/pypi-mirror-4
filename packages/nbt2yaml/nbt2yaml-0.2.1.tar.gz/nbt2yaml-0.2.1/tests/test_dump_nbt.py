bba2fd90bd3c60c819ccfc5c48c7e9c511df6319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package nbt2yaml/0.2.1/nbt2yaml-0.2.1.tar.gz

---
+++ packages/nbt2yaml/nbt2yaml-0.2.1.tar.gz/nbt2yaml-0.2.1/tests/test_dump_nbt.py
@@ -0,0 +1,23 @@
+import unittest
+from nbt2yaml import parse_nbt, dump_nbt
+from tests import datafile, eq_
+import gzip
+import StringIO
+
+class DumpNBTTest(unittest.TestCase):
+    def _assert_data(self, fname):
+        unzipped_data = gzip.GzipFile(fileobj=datafile(fname)).read()
+        parsed = parse_nbt(datafile(fname))
+        out = StringIO.StringIO()
+        dump_nbt(parsed, out)
+        out = gzip.GzipFile(fileobj=StringIO.StringIO(out.getvalue())).read()
+        eq_(unzipped_data, out)
+
+    def test_basic(self):
+        self._assert_data("test.nbt")
+
+    def test_large(self):
+        self._assert_data("bigtest.nbt")
+
+    def test_spawner(self):
+        self._assert_data("spawner.nbt")
