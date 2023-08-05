29c771eebfab9afaf01936cc0d70f8248ccdceaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:22 +0000

    Add package Products.EasyAsPiIE/3.0/Products.EasyAsPiIE-3.0.tar.gz

---
+++ packages/Products.EasyAsPiIE/Products.EasyAsPiIE-3.0.tar.gz/Products.EasyAsPiIE-3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.0'
+
+setup(name='Products.EasyAsPiIE',
+      version=version,
+      description="This enables you to do CSS 3 and psuedo classes for Internet Explorer 6-8.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='WebLion',
+      author_email='support@weblion.psu.edu',
+      url='https://weblion.psu.edu/svn/weblion/weblion/Products.EasyAsPiIE',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
