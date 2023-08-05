e89b38865b7f791dd8a4b90ebacda4d0a2b10f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:24 +0000

    Add package adrest/1.9.0/adrest-1.9.0.tar.gz

---
+++ packages/adrest/adrest-1.9.0.tar.gz/adrest-1.9.0/adrest/utils/emitter.py
@@ -0,0 +1,183 @@
+from datetime import datetime
+from os import path as op
+from time import mktime
+
+from django.db.models.base import ModelBase, Model
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from ..utils import UpdatedList
+from .paginator import Paginator
+from .response import SerializedHttpResponse
+from .serializer import json_dumps, xml_dumps
+from .status import HTTP_200_OK
+
+
+class EmitterMeta(type):
+    " Preload format attribute. "
+    def __new__(mcs, name, bases, params):
+        cls = super(EmitterMeta, mcs).__new__(mcs, name, bases, params)
+        if not cls.format and cls.media_type:
+            cls.format = str(cls.media_type).split('/')[-1]
+        return cls
+
+
+class BaseEmitter(object):
+    """ All emitters must extend this class, set the media_type attribute, and
+        override the serialize() function.
+    """
+    __metaclass__ = EmitterMeta
+
+    media_type = None
+    format = None
+
+    def __init__(self, resource, request=None, response=None):
+        self.resource = resource
+        self.request = request
+        self.response = response
+        if not isinstance(response, HttpResponse):
+            self.response = SerializedHttpResponse(response, mimetype=self.media_type, status=HTTP_200_OK)
+
+    def emit(self):
+        if not isinstance(self.response, SerializedHttpResponse):
+            return self.response
+
+        self.response.content = self.serialize(self.response.response)
+        self.response['Content-type'] = self.media_type
+        return self.response
+
+    @staticmethod
+    def serialize(content):
+        " Get content and return string. "
+        return content
+
+
+class TextEmitter(BaseEmitter):
+    media_type = 'text/plain'
+
+    @staticmethod
+    def serialize(content):
+        " Get content and return string. "
+        return unicode(content)
+
+
+class JSONEmitter(BaseEmitter):
+    media_type = 'application/json'
+
+    @staticmethod
+    def serialize(content):
+        return json_dumps(content)
+
+
+class JSONPEmitter(BaseEmitter):
+    media_type = 'text/javascript'
+
+    def serialize(self, content):
+        callback = self.request.GET.get('callback', 'callback')
+        return '%s(%s)' % (callback, json_dumps(content))
+
+
+class XMLEmitter(BaseEmitter):
+
+    media_type = 'application/xml'
+    xmldoc_tpl = '<?xml version="1.0" encoding="utf-8"?>\n<response success="%s" version="%s" timestamp="%s">%s</response>'
+
+    def serialize(self, content):
+        return self.xmldoc_tpl % (
+            'true' if self.response.status_code == HTTP_200_OK else 'false',
+            self.resource.version,
+            int(mktime(datetime.now().timetuple())),
+            xml_dumps(content)
+        )
+
+
+class TemplateEmitter(BaseEmitter):
+    " Serialize by django templates. "
+
+    def serialize(self, content):
+        if self.response.status_code != HTTP_200_OK:
+            template_name = op.join('api', 'error.%s' % self.format)
+        else:
+            template_name = self.resource.template or self.get_template_path(content)
+
+        template = loader.get_template(template_name)
+        return template.render(RequestContext(self.request, dict(
+            content=content,
+            emitter=self,
+            resource=self.resource)))
+
+    def get_template_path(self, content=None):
+
+        if isinstance(content, Paginator):
+            return op.join('api', 'paginator.%s' % self.format)
+
+        if isinstance(content, UpdatedList):
+            return op.join('api', 'updated.%s' % self.format)
+
+        app = ''
+        name = self.resource.get_name()
+
+        if not content:
+            content = self.resource.model
+
+        if isinstance(content, (Model, ModelBase)):
+            app = content._meta.app_label
+            name = content._meta.module_name
+
+        basedir = self.resource.api.prefix if getattr(self.resource, 'api', None) else 'api'
+        return op.join(
+            basedir,
+            self.resource.version,
+            app,
+            "%s.%s" % (name, self.format)
+        )
+
+
+class JSONTemplateEmitter(TemplateEmitter):
+    " Template emitter with JSON media type. "
+    media_type = 'application/json'
+
+
+class JSONPTemplateEmitter(TemplateEmitter):
+    " Template emitter with javascript media type. "
+    media_type = 'text/javascript'
+    format = 'json'
+
+    def serialize(self, content):
+        content = super(JSONPTemplateEmitter, self).serialize(content)
+        callback = self.request.GET.get('callback', 'callback')
+        return '%s(%s)' % (callback, content)
+
+
+class HTMLTemplateEmitter(TemplateEmitter):
+    " Template emitter with HTML media type. "
+    media_type = 'text/html'
+
+
+class XMLTemplateEmitter(TemplateEmitter):
+    " Template emitter with XML media type. "
+
+    media_type = 'application/xml'
+    xmldoc_tpl = '<?xml version="1.0" encoding="utf-8"?>\n<response success="%s" version="%s" timestamp="%s">%s</response>'
+
+    def serialize(self, content):
+        return self.xmldoc_tpl % (
+            'true' if self.response.status_code == HTTP_200_OK else 'false',
+            self.resource.version,
+            int(mktime(datetime.now().timetuple())),
+            super(XMLTemplateEmitter, self).serialize(content)
+        )
+
+
+try:
+    from bson import BSON
+
+    class BSONEmitter(BaseEmitter):
+        media_type = 'application/bson'
+
+        @staticmethod
+        def serialize(content):
+            return BSON.encode(content)
+
+except ImportError:
+    pass
---
+++ packages/adrest/adrest-1.9.0.tar.gz/adrest-1.9.0/adrest/views.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import traceback
+from logging import getLogger
+
+from django.conf.urls.defaults import url
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
+from django.core.mail import mail_admins
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+from .mixin import auth, emitter, handler, parser, throttle
+from .settings import ALLOW_OPTIONS, DEBUG, MAIL_ERRORS
+from .signals import api_request_started, api_request_finished
+from .utils import status, MetaOptions
+from .utils.exceptions import HttpError
+from .utils.response import SerializedHttpResponse
+from .utils.tools import as_tuple, gen_url_name, gen_url_regex, fix_request
+
+
+logger = getLogger('django.request')
+
+
+class ResourceMetaClass(
+    handler.HandlerMeta, throttle.ThrottleMeta, emitter.EmitterMeta,
+        parser.ParserMeta, auth.AuthMeta):
+    """ MetaClass for ResourceView.
+        Create meta options.
+    """
+
+    def __new__(mcs, name, bases, params):
+
+        # Create meta if not exists
+        params['meta'] = params.get('meta', MetaOptions())
+
+        # Run other meta classes
+        cls = super(ResourceMetaClass, mcs).__new__(mcs, name, bases, params)
+
+        # Prepare allowed methods
+        cls.allowed_methods = mcs.prepare_methods(cls.allowed_methods)
+
+        # Check parent
+        if cls.parent:
+            try:
+                cls.meta.parents += cls.parent.meta.parents + [cls.parent]
+            except AttributeError:
+                raise TypeError("%s.parent must be instance of %s" %
+                                (name, "ResourceView"))
+
+        # Meta name (maybe precalculate in handler)
+        cls.meta.name = cls.meta.name or cls.name or ''.join(
+            bit for bit in name.split('Resource') if bit).lower()
+
+        # Prepare urls
+        cls.url_params = list(as_tuple(cls.url_params))
+        cls.meta.url_name = cls.url_name or '-'.join(gen_url_name(cls))
+        cls.meta.url_regex = cls.url_regex or '/'.join(gen_url_regex(cls))
+
+        return cls
+
+    @staticmethod
+    def prepare_methods(methods):
+        " Prepare allowed methods. "
+
+        methods = tuple([str(m).upper() for m in as_tuple(methods)])
+
+        if not 'OPTIONS' in methods and ALLOW_OPTIONS:
+            methods += 'OPTIONS',
+
+        if not 'HEAD' in methods and 'GET' in methods:
+            methods += 'HEAD',
+
+        return methods
+
+
+class ResourceView(handler.HandlerMixin,
+                   throttle.ThrottleMixin,
+                   emitter.EmitterMixin,
+                   parser.ParserMixin,
+                   auth.AuthMixin,
+                   View):
+
+    # Create meta options
+    __metaclass__ = ResourceMetaClass
+
+    # Allowed methods
+    allowed_methods = 'GET',
+
+    # Name (Defaul by model name or class name)
+    name = None
+
+    # Link to api if connected
+    api = None
+
+    # Saves access log if enabled
+    log = True
+
+    # Link to parent resource
+    parent = None
+
+    # Custom prefix for url name and regex
+    prefix = ''
+
+    # Some custom URI params here
+    url_params = None
+    url_regex = None
+    url_name = None
+
+    # If children object in hierarchy has FK=Null to parent, allow to get this
+    # object (default: True)
+    allow_public_access = False
+
+    @csrf_exempt
+    def dispatch(self, request, _emit_=True, **resources):
+
+        # Fix PUT and PATH methods in Django request
+        request = fix_request(request)
+
+        # Send ADREST started signal
+        api_request_started.send(self, request=request)
+
+        # Send current api started signal
+        if self.api:
+            self.api.request_started.send(self, request=request)
+
+        # Current HTTP method
+        method = request.method.upper()
+
+        try:
+
+            # Check request method
+            self.check_method_allowed(method)
+
+            # Authentificate
+            self.authenticate(request)
+
+            # Throttle check
+            self.throttle_check()
+
+            if request.method != 'OPTIONS' or not ALLOW_OPTIONS:
+
+                # Get required resources
+                resources = self.get_resources(
+                    request, resource=self, **resources)
+
+                # Check owners
+                self.check_owners(**resources)
+
+                # Check rights for resources with this method
+                self.check_rights(resources, request=request)
+
+                # Parse content
+                request.data = self.parse(request)
+
+            # Get the appropriate create/read/update/delete function
+            view = getattr(self, self.callmap.get(method, None))
+
+            # Get function data
+            response = view(request, **resources)
+
+            # Serialize response
+            response = self.emit(response, request=request) if _emit_ else SerializedHttpResponse(response)
+            response["Allow"] = ', '.join(self.allowed_methods)
+            response["Vary"] = 'Authenticate, Accept'
+
+        except HttpError, e:
+            response = SerializedHttpResponse(e.content, status=e.status)
+            if _emit_:
+                response = self.emit(
+                    response, request=request, emitter=e.emitter)
+
+        except (AssertionError, ValidationError), e:
+            response = SerializedHttpResponse(
+                unicode(e), status=status.HTTP_400_BAD_REQUEST)
+            if _emit_:
+                response = self.emit(response, request=request)
+
+        except Exception, e:
+            response = self.handle_exception(e, request=request)
+
+        # Send errors on mail
+        errors_mail(response, request)
+
+        # Send finished signal
+        api_request_finished.send(
+            self, request=request, response=response, **resources)
+
+        # Send finished signal in API context
+        if self.api:
+            self.api.request_finished.send(self, request=request, response=response, **resources)
+
+        return response
+
+    @classmethod
+    def check_method_allowed(cls, method):
+        """ Ensure the request HTTP method is permitted for this resource, raising a ResourceException if it is not.
+        """
+        if not method in cls.callmap.keys():
+            raise HttpError('Unknown or unsupported method \'%s\'' % method,
+                            status=status.HTTP_501_NOT_IMPLEMENTED)
+
+        if not method in cls.allowed_methods:
+            raise HttpError(
+                'Method \'%s\' not allowed on this resource.' % method,
+                status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @classmethod
+    def get_resources(cls, request, resource=None, **resources):
+        " Parse resource objects from URL and GET. "
+
+        if cls.parent:
+            resources = cls.parent.get_resources(request, resource=resource, **resources)
+
+        pks = resources.get(
+            cls.meta.name) or request.REQUEST.getlist(cls.meta.name)
+
+        if not pks or cls.queryset is None:
+            return resources
+
+        pks = as_tuple(pks)
+
+        try:
+            if len(pks) == 1:
+                resources[cls.meta.name] = cls.queryset.get(pk=pks[0])
+
+            else:
+                resources[cls.meta.name] = cls.queryset.filter(pk__in=pks)
+
+        except (ObjectDoesNotExist, ValueError, AssertionError):
+            raise HttpError("Resource not found.",
+                            status=status.HTTP_404_NOT_FOUND)
+
+        except MultipleObjectsReturned:
+            raise HttpError("Resources conflict.",
+                            status=status.HTTP_409_CONFLICT)
+
+        return resources
+
+    @classmethod
+    def check_owners(cls, **resources):
+        """ Recursive scanning of the fact that the child has FK
+            to the parent and in resources we have right objects.
+
+            We check that in request like /author/1/book/2/page/3
+
+            Page object with pk=3 has ForeignKey field linked to Book object with pk=2
+            and Book with pk=2 has ForeignKey field linked to Author object with pk=1.
+        """
+
+        if cls.allow_public_access or not cls.parent:
+            return True
+
+        cls.parent.check_owners(**resources)
+
+        objects = resources.get(cls.meta.name)
+        if cls.model and cls.parent.model and objects:
+            try:
+                pr = resources.get(cls.parent.meta.name)
+                assert pr and all(pr.pk == getattr(
+                    o, "%s_id" % cls.parent.meta.name, None) for o in as_tuple(objects))
+            except AssertionError:
+                # 403 Error if there is error in parent-children relationship
+                raise HttpError(
+                    "Access forbidden.", status=status.HTTP_403_FORBIDDEN)
+
+        return True
+
+    @staticmethod
+    def handle_exception(e, request=None):
+        """ Handle code exception.
+        """
+        if DEBUG:
+            raise
+
+        logger.exception('\nADREST API Error: %s' % request.path)
+
+        return HttpResponse(str(e), status=500)
+
+    @property
+    def version(self):
+        return str(self.api or '')
+
+    @classmethod
+    def as_url(cls, api=None, name_prefix='', url_prefix=''):
+        " Generate url for resource. "
+        url_prefix = url_prefix and "%s/" % url_prefix
+        name_prefix = name_prefix and "%s-" % name_prefix
+
+        url_regex = '^%s%s/?$' % (
+            url_prefix, cls.meta.url_regex.lstrip('^').rstrip('/$'))
+        url_regex = url_regex.replace('//', '/')
+        url_name = '%s%s' % (name_prefix, cls.meta.url_name)
+
+        return url(url_regex, cls.as_view(api=api), name=url_name)
+
+    def get_name(self):
+        return self.meta.name
+
+
+def errors_mail(response, request):
+
+    if not response.status_code in MAIL_ERRORS:
+        return False
+
+    subject = 'ADREST API Error (%s): %s' % (
+        response.status_code, request.path)
+    stack_trace = '\n'.join(traceback.format_exception(*sys.exc_info()))
+    message = """
+Stacktrace:
+===========
+%s
+
+Handler data:
+=============
+%s
+
+Request information:
+====================
+%s
+
+""" % (stack_trace, repr(getattr(request, 'data', None)), repr(request))
+    return mail_admins(subject, message, fail_silently=True)
+
+# pymode:lint_ignore=E1120
