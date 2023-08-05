d417b218dc33aaf14d28e0c0e7d8ad2c1a0b3bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package rdflib-django/0.2.dev-201210011245/rdflib_django-0.2.dev_201210011245-py2.7.egg

---
+++ packages/rdflib-django/rdflib_django-0.2.dev_201210011245-py2.7.egg/rdflib_django/tests.py
@@ -0,0 +1,19 @@
+"""
+Unittests and doctests for the rdflib_django app.
+"""
+import doctest
+from django.utils import unittest
+import rdflib_django
+from rdflib_django import store, tests_store, tests_rdflib
+
+
+def suite():
+    """
+    Generate the test suite.
+    """
+    s = unittest.TestSuite()
+    s.addTest(doctest.DocTestSuite(rdflib_django))
+    s.addTest(doctest.DocTestSuite(store))
+    s.addTest(unittest.findTestCases(tests_store))
+    s.addTest(unittest.findTestCases(tests_rdflib))
+    return s
