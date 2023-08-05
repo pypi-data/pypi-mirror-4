8157c3069cca5729bd45a0336c92bc5d863cd57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package Muxpy/v0.0.2/Muxpy-v0.0.2.tar.gz

---
+++ packages/Muxpy/Muxpy-v0.0.2.tar.gz/Muxpy-v0.0.2/muxpy/commands.py
@@ -0,0 +1,97 @@
+import os
+import sys
+
+from collections import OrderedDict
+from itertools import count
+
+from . import profile, parser, formats, backend
+
+
+def edit(p):
+    prof = profile.get_profile_path(p.profile, p.format)
+    os.system('/etc/alternatives/editor "%s"' % prof)
+
+
+def start(p):
+    prof = profile.get_profile_path(p.profile, p.format)
+    if not os.path.isfile(prof):
+        print >> sys.stderr, 'Cannot find profile %s' % p.profile
+        sys.exit(1)
+
+    tmux = backend.Backend(socket=p.socket)
+
+    with open(prof) as f:
+        data = formats.parse(p.format, f.read())
+
+    for session in data:
+        tmux.create_session(session=session['name'], window=session['windows'][0]['name'])
+
+        for window in session['windows'][1:]:
+            tmux.new_window(window['name'])
+
+        for window in session['windows']:
+            tmux.select_window(window['name'])
+
+            # make the required number of panes
+            for index, pane in enumerate(window['panes']):
+                if index > 0:
+                    tmux.new_pane()
+
+                if pane and pane['cwd']:
+                    tmux.send_keys('cd %s\n' % pane['cwd'])
+
+                if pane and pane['cmd']:
+                    tmux.send_keys('%s\n' % pane['cmd'])
+
+            tmux.select_layout(window['layout'])
+
+    os.system('tmux -u2 -S %s attach' % p.socket)
+
+
+def create(p):
+    prof = profile.get_profile_path(p.profile, p.format)
+    pname, fname = os.path.split(prof)
+    if not os.path.isdir(pname):
+        os.makedirs(pname)
+
+    data = []
+
+    tmux = parser.TmuxParser(p.socket)
+    sessions = tmux.list_sessions()
+    panes = tmux.get_panes()
+
+    for session in sessions:
+        session_data = OrderedDict(name=session.name)
+        windows = tmux.list_windows(session.name)
+
+        unique_windows = []
+        for window in windows:
+            # make sure the window name is unique, or else tmux will weep loudly
+            if not window['name'] in unique_windows:
+                unique_windows.append(window['name'])
+            else:
+                for x in count():
+                    new_window = "%s_%d" % (window['name'], x)
+                    if new_window not in unique_windows:
+                        break
+                unique_windows.append(new_window)
+                window['name'] = new_window
+
+            window['panes'] = panes[session['name']][window['number']]
+
+        session_data['windows'] = windows
+        data.append(session_data)
+
+    formatted = formats.format(p.format, data)
+
+    with open(prof, 'w') as f:
+        f.write(formatted)
+
+    print 'made profile, %d sessions with total %d windows' % (len(data), sum(len(x['windows']) for x in data))
+
+
+def kill(p):
+    tmux = backend.Backend(socket=p.socket)
+    tmux.kill_server()
+    os.unlink(p.socket)
+    print 'killed tmux at %s' % p.socket
---
+++ packages/Muxpy/Muxpy-v0.0.2.tar.gz/Muxpy-v0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import re
+
+
+def get_version():
+    init_py = open('muxpy/__init__.py').read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+    return metadata['version']
+
+
+setup(
+    name='Muxpy',
+    version=get_version(),
+    author='Bjørnar Snoksrud',
+    author_email='bjornar@snoksrud.no',
+    packages=['muxpy'],
+    scripts=['bin/muxpy'],
+    license='ICS',
+    description='A tmux session handler',
+    long_description=open('README').read(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    url='https://github.com/bjornars/muxpy',
+)
