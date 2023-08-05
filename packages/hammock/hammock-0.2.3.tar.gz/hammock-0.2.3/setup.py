bedaded3ba8228d5fddfd050b836308f27a90346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package hammock/0.2.3/hammock-0.2.3.tar.gz

---
+++ packages/hammock/hammock-0.2.3.tar.gz/hammock-0.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hammock',
+    py_modules=['hammock'],
+    version='0.2.3',
+    description='rest like a boss',
+    author='Kadir Pekel',
+    author_email='kadirpekel@gmail.com',
+    url='https://github.com/kadirpekel/hammock',
+    install_requires=[
+        'requests>=0.13.6'
+    ],
+    tests_require=['httpretty==0.5.4'],
+)
