6d39a39fe598dec63bd59d3f93ceafd9ca9b1cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:10 +0000

    Add package infi.jira_cli/0.0.1-develop-9-g687e309/infi.jira_cli-0.0.1-develop-9-g687e309.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.0.1-develop-9-g687e309.tar.gz/infi.jira_cli-0.0.1-develop-9-g687e309/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1-develop-9-g687e309"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@gitserver:host/infi-jira-cli.git'
+__git_head_hash__ = '687e3098620c41ad023c6e18612ca0fdd2aec24a'
+__git_head_subject__ = 'HOSTDEV-553 jissue resolve --commit=<commit>'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
