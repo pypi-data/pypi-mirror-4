e2a5664304cc4912d37ef7116a5f23c9311636a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package radon/0.2/radon-0.2-py2.py3-none-any.whl

---
+++ packages/radon/radon-0.2-py2.py3-none-any.whl/radon/tests/run.py
@@ -0,0 +1,8 @@
+import unittest
+from test_complexity_visitor import *
+from test_complexity_utils import *
+from test_raw import *
+from test_halstead import *
+from test_other_metrics import *
+
+unittest.main()
