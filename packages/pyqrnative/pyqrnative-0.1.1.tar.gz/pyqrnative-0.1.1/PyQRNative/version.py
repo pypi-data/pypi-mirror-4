066c6a56245e33134090fbfe6aadb03c5c856651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package pyqrnative/0.1.1/pyqrnative-0.1.1.tar.gz

---
+++ packages/pyqrnative/pyqrnative-0.1.1.tar.gz/pyqrnative-0.1.1/PyQRNative/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.1.1'
+version = '0.1.1'
+version_major = 0
+version_minor = 1
+version_micro = 1
+release = False
+
+if not release:
+    version += '.dev68cb4ce6021dc03c4d9593c404fb0e43cc771e91'
