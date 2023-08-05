e266693b492d8ab162001a21a8eec84d94bf3394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package pycompsense/0.1.1/pycompsense-0.1.1.tar.gz

---
+++ packages/pycompsense/pycompsense-0.1.1.tar.gz/pycompsense-0.1.1/compsense/__init__.py
@@ -0,0 +1,9 @@
+"""
+A toolbox for compressed sensing and sparse represenation.
+
+.. codeauthor:: Amit Aides <amitibo@tx.technion.ac.il>
+"""
+
+from . import operators
+from . import problems
+from . import algorithms
