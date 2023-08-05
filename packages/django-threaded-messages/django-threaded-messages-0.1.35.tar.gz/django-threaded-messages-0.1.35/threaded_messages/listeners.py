6d3d2e61007a65d2b0acb33243510e401fac8524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:20 +0000

    Add package django-threaded-messages/0.1.35/django-threaded-messages-0.1.35.tar.gz

---
+++ packages/django-threaded-messages/django-threaded-messages-0.1.35.tar.gz/django-threaded-messages-0.1.35/threaded_messages/listeners.py
@@ -0,0 +1,40 @@
+import logging
+
+from django.utils.html import strip_tags
+
+from . import settings as sendgrid_settings
+
+logger = logging.getLogger('threaded_messages')
+
+if sendgrid_settings.THREADED_MESSAGES_USE_SENDGRID:
+    from sendgrid_parse_api.signals import email_received
+else:
+    email_received = None
+
+
+def signal_received_email(sender, sma, app_id, html, text, from_field, **kwargs):
+    from .utils import reply_to_thread, strip_mail
+    logger.debug("Sendgrid signal receive: %s, %s, %s, %s, %s, %s" % (sender, sma, app_id,
+                                                                    html, repr(text), from_field))
+    if app_id == sendgrid_settings.THREADED_MESSAGES_ID:
+        body = ''
+
+        if text:
+            body = text
+
+        if not body:
+            body = html
+
+        if body:
+            body = strip_tags(body)
+            body = strip_mail(body)
+            thread = sma.content_object
+            reply_to_thread(thread, sma.user, body)
+
+
+def start_listening():
+    if email_received:
+        logger.debug("Sendgrid start listening")
+        email_received.connect(signal_received_email, dispatch_uid="thm_reply")
+
+    
---
+++ packages/django-threaded-messages/django-threaded-messages-0.1.35.tar.gz/django-threaded-messages-0.1.35/threaded_messages/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
+
+from threaded_messages.views import *
+
+urlpatterns = patterns('',
+    url(r'^$', redirect_to, {'url': 'inbox/'}),
+    url(r'^search/$', search, name='messages_search'),
+    url(r'^inbox/$', inbox, name='messages_inbox'),
+    url(r'^outbox/$', outbox, name='messages_outbox'),
+    url(r'^compose/$', compose, name='messages_compose'),
+    url(r'^compose/(?P<recipient>[\+\w]+)/$', compose, name='messages_compose_to'),
+    url(r'^view/(?P<thread_id>[\d]+)/$', view, name='messages_detail'),
+    url(r'^delete/(?P<thread_id>[\d]+)/$', delete, name='messages_delete'),
+    url(r'^undelete/(?P<thread_id>[\d]+)/$', undelete, name='messages_undelete'),
+    url(r'^batch-update/$', batch_update, name='messages_batch_update'),
+    url(r'^trash/$', trash, name='messages_trash'),
+
+    url(r"^recipient-search/$", recipient_search, name="recipient_search"),
+    url(r'^message-reply/(?P<thread_id>[\d]+)/$', message_ajax_reply, name="message_reply"),
+
+    # modal composing
+    url(r'^modal-compose/(?P<recipient>[\w.+-_]+)/$', compose, {
+                            "template_name":"django_messages/modal_compose.html",
+                            "form_class": ComposeForm
+                        }, name='modal_messages_compose_to'),
+
+    url(r'^modal-compose/$', compose, {
+                            "template_name":"django_messages/modal_compose.html",
+                            "form_class": ComposeForm
+                        }, name='modal_messages_compose'),
+)
