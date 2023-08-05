576c653d37ec208c435b120bf9109847fef0a3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package heroku-django-helpers/0.1dev/heroku-django-helpers-0.1dev.tar.gz

---
+++ packages/heroku-django-helpers/heroku-django-helpers-0.1dev.tar.gz/heroku-django-helpers-0.1dev/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='heroku-django-helpers',
+	version='0.1dev',
+	author='Jack Shedd, Mess',
+	author_email='jshedd@thisismess.com',
+	packages=['heroku_django_helpers',],
+	license='BSD',
+	url='http://github.com/thisismess/heroku-django-helpers',
+	long_description=open('README.md').read(),
+	install_requires=["Django >= 1.4"]
+)
