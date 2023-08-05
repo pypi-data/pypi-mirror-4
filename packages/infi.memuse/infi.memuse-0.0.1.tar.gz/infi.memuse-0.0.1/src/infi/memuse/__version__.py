19ee201506879e717ce3d61bbc20cec24dd40d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:03 +0000

    Add package infi.memuse/0.0.1/infi.memuse-0.0.1.tar.gz

---
+++ packages/infi.memuse/infi.memuse-0.0.1.tar.gz/infi.memuse-0.0.1/src/infi/memuse/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4a602cb9ee7997b16c5f30e0a9b7f6875bc312c6'
+__git_head_subject__ = 'Finished release v0.0.1.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
