38117ed11b101c8eb0b2820c70c5cc2df29095a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:47 +0000

    Add package summary/0.1.0/summary-0.1.0.tar.gz

---
+++ packages/summary/summary-0.1.0.tar.gz/summary-0.1.0/summary/test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import urllib, sys
+from extractor import extract
+
+urls = [
+    # japanese site
+    'http://www.gizmodo.jp/2012/09/post_10869.html',
+    'http://labs.cybozu.co.jp/blog/nakatani/2007/09/web_1.html',
+    # english site
+    'http://capturevision.wordpress.com/2009/08/05/music-visualizer-progress/',
+    'http://www.dasprinzip.com/prinzipiell/2012/04/03/shaders-in-their-natural-habitat-episode-1/',
+    'http://www.generatorx.no/20101217/abstrakt-abstrakt-jorinde-voigt/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+generatorx+%28Generator.x%3A+Generative+strategies+in+art+%26+design%29'
+]
+
+def extract_test(url):
+    try:
+        print url, '\n'
+        res = extract(urllib.urlopen(url).read())
+        print 'Title: ', res['title'], '\n'
+        print 'Content: ', res['body'], '\n'
+        print 'Images: ', res['img'], '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
+
+def main():
+    for url in urls:
+        extract_test(url)
+
+if __name__ == '__main__': main()
