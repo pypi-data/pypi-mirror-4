c05057f37be09e09a009d3add6de31a653f96df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:54 +0000

    Add package bedup/0.0.6/bedup-0.0.6.tar.gz

---
+++ packages/bedup/bedup-0.0.6.tar.gz/bedup-0.0.6/bedup/__pycache__/_cffi__x8d12ba84xa776430d.c
@@ -0,0 +1,287 @@
+
+#include <Python.h>
+#include <stddef.h>
+
+#ifdef MS_WIN32
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#endif
+
+#if PY_MAJOR_VERSION < 3
+# undef PyCapsule_CheckExact
+# undef PyCapsule_GetPointer
+# define PyCapsule_CheckExact(capsule) (PyCObject_Check(capsule))
+# define PyCapsule_GetPointer(capsule, name) \
+    (PyCObject_AsVoidPtr(capsule))
+#endif
+
+#if PY_MAJOR_VERSION >= 3
+# define PyInt_FromLong PyLong_FromLong
+#endif
+
+#define _cffi_from_c_double PyFloat_FromDouble
+#define _cffi_from_c_float PyFloat_FromDouble
+#define _cffi_from_c_signed_char PyInt_FromLong
+#define _cffi_from_c_short PyInt_FromLong
+#define _cffi_from_c_int PyInt_FromLong
+#define _cffi_from_c_long PyInt_FromLong
+#define _cffi_from_c_unsigned_char PyInt_FromLong
+#define _cffi_from_c_unsigned_short PyInt_FromLong
+#define _cffi_from_c_unsigned_long PyLong_FromUnsignedLong
+#define _cffi_from_c_unsigned_long_long PyLong_FromUnsignedLongLong
+#define _cffi_from_c__Bool PyInt_FromLong
+
+#if SIZEOF_INT < SIZEOF_LONG
+#  define _cffi_from_c_unsigned_int PyInt_FromLong
+#else
+#  define _cffi_from_c_unsigned_int PyLong_FromUnsignedLong
+#endif
+
+#if SIZEOF_LONG < SIZEOF_LONG_LONG
+#  define _cffi_from_c_long_long PyLong_FromLongLong
+#else
+#  define _cffi_from_c_long_long PyInt_FromLong
+#endif
+
+#define _cffi_to_c_double PyFloat_AsDouble
+#define _cffi_to_c_float PyFloat_AsDouble
+
+#define _cffi_to_c_char_p                                                \
+                 ((char *(*)(PyObject *))_cffi_exports[0])
+#define _cffi_to_c_signed_char                                           \
+                 ((signed char(*)(PyObject *))_cffi_exports[1])
+#define _cffi_to_c_unsigned_char                                         \
+                 ((unsigned char(*)(PyObject *))_cffi_exports[2])
+#define _cffi_to_c_short                                                 \
+                 ((short(*)(PyObject *))_cffi_exports[3])
+#define _cffi_to_c_unsigned_short                                        \
+                 ((unsigned short(*)(PyObject *))_cffi_exports[4])
+#define _cffi_to_c_int                                                   \
+                 ((int(*)(PyObject *))_cffi_exports[5])
+#define _cffi_to_c_unsigned_int                                          \
+                 ((unsigned int(*)(PyObject *))_cffi_exports[6])
+#define _cffi_to_c_long                                                  \
+                 ((long(*)(PyObject *))_cffi_exports[7])
+#define _cffi_to_c_unsigned_long                                         \
+                 ((unsigned long(*)(PyObject *))_cffi_exports[8])
+#define _cffi_to_c_char                                                  \
+                 ((char(*)(PyObject *))_cffi_exports[9])
+#define _cffi_from_c_pointer                                             \
+    ((PyObject *(*)(char *, CTypeDescrObject *))_cffi_exports[10])
+#define _cffi_to_c_pointer                                               \
+    ((char *(*)(PyObject *, CTypeDescrObject *))_cffi_exports[11])
+#define _cffi_get_struct_layout                                          \
+    ((PyObject *(*)(Py_ssize_t[]))_cffi_exports[12])
+#define _cffi_restore_errno                                              \
+    ((void(*)(void))_cffi_exports[13])
+#define _cffi_save_errno                                                 \
+    ((void(*)(void))_cffi_exports[14])
+#define _cffi_from_c_char                                                \
+    ((PyObject *(*)(char))_cffi_exports[15])
+#define _cffi_from_c_deref                                               \
+    ((PyObject *(*)(char *, CTypeDescrObject *))_cffi_exports[16])
+#define _cffi_to_c                                                       \
+    ((int(*)(char *, CTypeDescrObject *, PyObject *))_cffi_exports[17])
+#define _cffi_from_c_struct                                              \
+    ((PyObject *(*)(char *, CTypeDescrObject *))_cffi_exports[18])
+#define _cffi_to_c_wchar_t                                               \
+    ((wchar_t(*)(PyObject *))_cffi_exports[19])
+#define _cffi_from_c_wchar_t                                             \
+    ((PyObject *(*)(wchar_t))_cffi_exports[20])
+#define _cffi_to_c_long_double                                           \
+    ((long double(*)(PyObject *))_cffi_exports[21])
+#define _cffi_to_c__Bool                                                 \
+    ((_Bool(*)(PyObject *))_cffi_exports[22])
+#define _cffi_to_c_long_long                                             \
+                 ((long long(*)(PyObject *))_cffi_exports[23])
+#define _cffi_to_c_unsigned_long_long                                    \
+                 ((unsigned long long(*)(PyObject *))_cffi_exports[24])
+#define _CFFI_NUM_EXPORTS 25
+
+typedef struct _ctypedescr CTypeDescrObject;
+
+static void *_cffi_exports[_CFFI_NUM_EXPORTS];
+static PyObject *_cffi_types, *_cffi_VerificationError;
+
+static PyObject *_cffi_setup_custom(PyObject *lib);   /* forward */
+
+static PyObject *_cffi_setup(PyObject *self, PyObject *args)
+{
+    PyObject *library;
+    if (!PyArg_ParseTuple(args, "OOO", &_cffi_types, &_cffi_VerificationError,
+                                       &library))
+        return NULL;
+    Py_INCREF(_cffi_types);
+    Py_INCREF(_cffi_VerificationError);
+    return _cffi_setup_custom(library);
+}
+
+static void _cffi_init(void)
+{
+    PyObject *module = PyImport_ImportModule("_cffi_backend");
+    PyObject *c_api_object;
+
+    if (module == NULL)
+        return;
+
+    c_api_object = PyObject_GetAttrString(module, "_C_API");
+    if (c_api_object == NULL)
+        return;
+    if (!PyCapsule_CheckExact(c_api_object)) {
+        PyErr_SetNone(PyExc_ImportError);
+        return;
+    }
+    memcpy(_cffi_exports, PyCapsule_GetPointer(c_api_object, "cffi"),
+           _CFFI_NUM_EXPORTS * sizeof(void *));
+}
+
+#define _cffi_type(num) ((CTypeDescrObject *)PyList_GET_ITEM(_cffi_types, num))
+
+/**********/
+
+
+
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    
+
+static PyObject *
+_cffi_f_fstat(PyObject *self, PyObject *args)
+{
+  int x0;
+  struct stat*  x1;
+  int result;
+  PyObject *arg0;
+  PyObject *arg1;
+
+  if (!PyArg_ParseTuple(args, "OO:fstat", &arg0, &arg1))
+    return NULL;
+
+  x0 = _cffi_to_c_int(arg0);
+  if (x0 == (int)-1 && PyErr_Occurred())
+    return NULL;
+
+  x1 = (struct stat* )_cffi_to_c_pointer(arg1, _cffi_type(0));
+  if (x1 == (struct stat* )NULL && PyErr_Occurred())
+    return NULL;
+
+  Py_BEGIN_ALLOW_THREADS
+  _cffi_restore_errno();
+  { result = fstat(x0, x1); }
+  _cffi_save_errno();
+  Py_END_ALLOW_THREADS
+
+  return _cffi_from_c_int(result);
+}
+
+static void _cffi_check_struct_stat(struct stat *p)
+{
+  /* only to generate compile-time warnings or errors */
+  { struct timespec(*tmp) = &p->st_atim; (void)tmp; }
+  { struct timespec(*tmp) = &p->st_mtim; (void)tmp; }
+}
+static PyObject *
+_cffi_layout_struct_stat(PyObject *self, PyObject *noarg)
+{
+  struct _cffi_aligncheck { char x; struct stat y; };
+  static Py_ssize_t nums[] = {
+    sizeof(struct stat),
+    offsetof(struct _cffi_aligncheck, y),
+    offsetof(struct stat, st_atim),
+    sizeof(((struct stat *)0)->st_atim),
+    offsetof(struct stat, st_mtim),
+    sizeof(((struct stat *)0)->st_mtim),
+    -1
+  };
+  return _cffi_get_struct_layout(nums);
+  /* the next line is not executed, but compiled */
+  _cffi_check_struct_stat(0);
+}
+
+static void _cffi_check_struct_timespec(struct timespec *p)
+{
+  /* only to generate compile-time warnings or errors */
+  (void)((p->tv_sec) << 1);
+  (void)((p->tv_nsec) << 1);
+}
+static PyObject *
+_cffi_layout_struct_timespec(PyObject *self, PyObject *noarg)
+{
+  struct _cffi_aligncheck { char x; struct timespec y; };
+  static Py_ssize_t nums[] = {
+    sizeof(struct timespec),
+    offsetof(struct _cffi_aligncheck, y),
+    offsetof(struct timespec, tv_sec),
+    sizeof(((struct timespec *)0)->tv_sec),
+    offsetof(struct timespec, tv_nsec),
+    sizeof(((struct timespec *)0)->tv_nsec),
+    -1
+  };
+  return _cffi_get_struct_layout(nums);
+  /* the next line is not executed, but compiled */
+  _cffi_check_struct_timespec(0);
+}
+
+static PyObject *
+_cffi_f_futimens(PyObject *self, PyObject *args)
+{
+  int x0;
+  struct timespec*  x1;
+  int result;
+  PyObject *arg0;
+  PyObject *arg1;
+
+  if (!PyArg_ParseTuple(args, "OO:futimens", &arg0, &arg1))
+    return NULL;
+
+  x0 = _cffi_to_c_int(arg0);
+  if (x0 == (int)-1 && PyErr_Occurred())
+    return NULL;
+
+  x1 = (struct timespec* )_cffi_to_c_pointer(arg1, _cffi_type(1));
+  if (x1 == (struct timespec* )NULL && PyErr_Occurred())
+    return NULL;
+
+  Py_BEGIN_ALLOW_THREADS
+  _cffi_restore_errno();
+  { result = futimens(x0, x1); }
+  _cffi_save_errno();
+  Py_END_ALLOW_THREADS
+
+  return _cffi_from_c_int(result);
+}
+
+static PyObject *_cffi_setup_custom(PyObject *lib)
+{
+  if (0 < 0)
+    return NULL;
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+static PyMethodDef _cffi_methods[] = {
+  {"fstat", _cffi_f_fstat, METH_VARARGS},
+  {"_cffi_layout_struct_stat", _cffi_layout_struct_stat, METH_NOARGS},
+  {"_cffi_layout_struct_timespec", _cffi_layout_struct_timespec, METH_NOARGS},
+  {"futimens", _cffi_f_futimens, METH_VARARGS},
+  {"_cffi_setup", _cffi_setup, METH_VARARGS},
+  {NULL, NULL}    /* Sentinel */
+};
+
+PyMODINIT_FUNC
+init_cffi__x8d12ba84xa776430d(void)
+{
+  PyObject *lib;
+  lib = Py_InitModule("_cffi__x8d12ba84xa776430d", _cffi_methods);
+  if (lib == NULL || 0 < 0)
+    return;
+  _cffi_init();
+  return;
+}
