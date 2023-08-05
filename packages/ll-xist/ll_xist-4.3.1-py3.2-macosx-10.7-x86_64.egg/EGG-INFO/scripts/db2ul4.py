555c086db3715b99ef5490acf6d3fa2f5ff232be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package ll-xist/4.3.1/ll_xist-4.3.1-py3.2-macosx-10.7-x86_64.egg

---
+++ packages/ll-xist/ll_xist-4.3.1-py3.2-macosx-10.7-x86_64.egg/EGG-INFO/scripts/db2ul4.py
@@ -0,0 +1,9 @@
+#!/Users/walter/.local/bin/python3.2
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.scripts import db2ul4
+	sys.exit(db2ul4.main())
---
+++ packages/ll-xist/ll_xist-4.3.1-py3.2-macosx-10.7-x86_64.egg/EGG-INFO/scripts/xml2xsc.py
@@ -0,0 +1,10 @@
+#!/Users/walter/.local/bin/python3.2
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.xist.scripts import xml2xsc
+	sys.exit(xml2xsc.main())
