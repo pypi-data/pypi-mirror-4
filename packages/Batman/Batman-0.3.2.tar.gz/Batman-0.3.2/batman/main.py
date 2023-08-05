8300e50d188b7bf58dc242ce2d775c4c3b48f604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package Batman/0.3.2/Batman-0.3.2.tar.gz

---
+++ packages/Batman/Batman-0.3.2.tar.gz/Batman-0.3.2/batman/main.py
@@ -0,0 +1,35 @@
+import os
+import virtualenvs
+import parser
+import update
+import symlinks
+import sys
+import argparse
+
+BATMAN_FILE = '.batman.yml'
+
+
+def parse_basedir():
+    p = argparse.ArgumentParser(description='manage deployment')
+    p.add_argument('project_directory', help='path to the project to process')
+    args = p.parse_args()
+    return os.path.abspath(args.project_directory)
+
+
+def main():
+    basedir = parse_basedir()
+    file = os.path.join(basedir, BATMAN_FILE)
+    cfg = parser.load(file)
+    virtualenvs.create_if_not_exists(cfg['virtualenv'])
+    if cfg.get('add2virtualenv'):
+        virtualenvs.sync_add2virtualenv(cfg['add2virtualenv'], cfg['virtualenv'])
+    if cfg.get('ensure_symlinks'):
+        symlinks.ensure(cfg['ensure_symlinks'], basedir)
+    if cfg.get('update_on_change'):
+        update.check_and_update(
+            cfg['update_on_change'],
+            cfg['hash_dir'],
+            basedir,
+            cfg['hash'],
+            cfg['virtualenv']
+        )
---
+++ packages/Batman/Batman-0.3.2.tar.gz/Batman-0.3.2/batman/symlinks.py
@@ -0,0 +1,22 @@
+import os
+from utils import normalize_path
+
+def add_basepath(path, basedir):
+    if not os.path.isabs(path):
+        path = os.path.join(basedir, path)
+    return  path
+
+def ensure(links, basedir):
+    for target, link_name in links.iteritems():
+        target, link_name = add_basepath(normalize_path(target), basedir), add_basepath(normalize_path(link_name), basedir)
+
+        if not os.path.islink(link_name):
+            # Non-link file exists.  Delete but warn
+            print "WARNING: Deleting non-link file {0}".format(link_name)
+            os.remove(link_name)
+        if os.path.islink(link_name) and os.readlink(link_name) != target:
+            # Link exists but points to the wrong place
+            os.unlink(link_name)
+        if not os.path.islink(link_name):
+            print "creating link at {link_name} pointing to {target}".format(link_name=link_name, target=target)
+            os.symlink(target, link_name)
