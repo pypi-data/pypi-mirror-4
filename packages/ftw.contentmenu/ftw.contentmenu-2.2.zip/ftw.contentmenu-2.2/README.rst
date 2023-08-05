2439a86392f96b9f32c7bdeaa5c9e529e8c8aaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:11 +0000

    Add package ftw.contentmenu/2.2/ftw.contentmenu-2.2.zip

---
+++ packages/ftw.contentmenu/ftw.contentmenu-2.2.zip/ftw.contentmenu-2.2/README.rst
@@ -0,0 +1,47 @@
+.. contents::
+
+Introduction
+============
+
+``ftw.contentmenu`` customizes Plone's content menu in the following way:
+
+* The workflow menu is removed. Workflow actions are displayed in 
+  the actions menu instead.
+
+* The Add... menu (aka folder factories) can additionally display actions
+  having the category ``folder_factories``.
+
+* The Add... menu can be customized further by providing a specific adapter
+  which can modify the list of factory items.
+
+
+Installation
+============
+
+Install ``ftw.contentmenu`` by adding it to the list of eggs in your buildout or by adding it as a dependency of your policy package. Then run buildout and
+restart your instance.
+
+::
+
+  [instance]
+  eggs =
+      ftw.contenmenu
+
+Go to Site Setup of your Plone site and activate the ``ftw.contentmenu``
+add-on.
+
+Links
+=====
+
+- Main github project repository: https://github.com/4teamwork/ftw.contentmenu
+- Issue tracker: https://github.com/4teamwork/ftw.contentmenu/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.contentmenu
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.contentmenu
+
+
+Copyright
+=========
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.contentmenu`` is licensed under GNU General Public License, version 2.
---
+++ packages/ftw.contentmenu/ftw.contentmenu-2.2.zip/ftw.contentmenu-2.2/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.2'
+maintainer = 'Thomas Buchberger'
+
+tests_require = [
+    'plone.app.testing',
+    ]
+
+setup(name='ftw.contentmenu',
+      version=version,
+      description="Customize Plone's content menu",
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw contentmenu plone',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.contentmenu',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
