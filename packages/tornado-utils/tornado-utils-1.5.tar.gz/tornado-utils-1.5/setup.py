874ad5f241a2bf5b14e558f41e4b006b4ace866f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:20 +0000

    Add package tornado-utils/1.5/tornado-utils-1.5.tar.gz

---
+++ packages/tornado-utils/tornado-utils-1.5.tar.gz/tornado-utils-1.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='tornado-utils',
+      version='1.5',
+      description='Utility scripts for a Tornado site',
+      long_description=read('README.md'),
+      author='Peter Bengtsson',
+      author_email='mail@peterbe.com',
+      url='http://github.com/peterbe/tornado-utils',
+      packages = [
+        'tornado_utils',
+        'tornado_utils.send_mail',
+        'tornado_utils.send_mail.backends',
+        'tornado_utils.tests',
+      ],
+      classifiers=[
+           'Programming Language :: Python :: 2',
+           'Intended Audience :: Developers',
+           'Operating System :: POSIX :: Linux',
+      ],
+)
