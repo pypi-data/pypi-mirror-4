be5f0c0c236dbe516de77c34753c540d88b5c0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package flightrecorder/0.9.1/flightrecorder-0.9.1.tar.gz

---
+++ packages/flightrecorder/flightrecorder-0.9.1.tar.gz/flightrecorder-0.9.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+version = '0.9.1'
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+        author='Tom Payne',
+        author_email='twpayne@gmail.com',
+        description='Utilities for flight recorders',
+        long_description=long_description,
+        name='flightrecorder',
+        packages=['flightrecorder'],
+        scripts=['scripts/flightrecorder'],
+        setup_requires=['nose'],
+        #test_suite='tests',
+        url='https://github.com/twpayne/flightrecorder',
+        version=version,
+        zip_safe=True)
