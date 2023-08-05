922b6755120d4398f24955307e531d8bef9c2b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:54 +0000

    Add package tweepy-utils/0.1dev2/tweepy-utils-0.1dev2.tar.gz

---
+++ packages/tweepy-utils/tweepy-utils-0.1dev2.tar.gz/tweepy-utils-0.1dev2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tweepy-utils',
+    version='0.1dev2',
+    description='A set of Python utility scripts for Twitter account management, using Tweepy.',
+    author='Audrey Roy',
+    author_email='audreyr@cartwheelweb.com',
+    url='https://github.com/audreyr/tweepy-utils/',
+    packages=['tweepy_utils',],
+    license='MIT license',
+    # long_description=open('README.md').read(),
+)
