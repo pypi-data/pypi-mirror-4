27121cd227087aee3571af9e709d9be1b549407c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package raven/2.0.8/raven-2.0.8.tar.gz

---
+++ packages/raven/raven-2.0.8.tar.gz/raven-2.0.8/raven/contrib/django/__init__.py
@@ -0,0 +1,10 @@
+"""
+raven.contrib.django
+~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from raven.contrib.django.client import *
+from raven.contrib.django.models import *
