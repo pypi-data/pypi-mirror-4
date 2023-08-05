fe1dbdaa33e4669aef2d98fbf0287edd5360aeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:36 +0000

    Add package speedrack/0.3.0/speedrack-0.3.0.tar.gz

---
+++ packages/speedrack/speedrack-0.3.0.tar.gz/speedrack-0.3.0/speedrack/constants/task_params.py
@@ -0,0 +1,18 @@
+NAME = "name"
+COMMAND = "command"
+DESCRIPTION = "description"
+SPAM = "spam"
+MAX_KEEP = "max_keep"
+FAIL_BY_STDERR = "fail_by_stderr"
+FAIL_BY_RETCODE = "fail_by_retcode"
+CONFIG = "config"
+
+# yaml-friendly parameters
+TASK_EMAIL = "email"
+TASK_INTERVAL = "interval"
+TASK_CRON = "cron"
+
+# parsed versions of the friendly parameters
+PARSED_CRON = "parsed_cron"
+PARSED_INTERVAL = "parsed_interval"
+EMAIL_RECIPIENTS = "email_recipients"
