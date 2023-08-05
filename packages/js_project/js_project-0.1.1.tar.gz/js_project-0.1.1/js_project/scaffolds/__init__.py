9e82d2f0763dc7b1d86525f12f055555bf4cb7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package js_project/0.1.1/js_project-0.1.1.tar.gz

---
+++ packages/js_project/js_project-0.1.1.tar.gz/js_project-0.1.1/js_project/scaffolds/__init__.py
@@ -0,0 +1,6 @@
+from pyramid.scaffolds import PyramidTemplate
+
+
+class JSProjectTemplate(PyramidTemplate):
+    _template_dir = 'js_project_scaffold'
+    summary = 'pcreate scaffold for fanstatic resource library projects'
