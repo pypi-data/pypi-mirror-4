ab96f12aaaf60107f14fb5ad871743298f1d0603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:41 +0000

    Add package RimuDNS/0.0.2/RimuDNS-0.0.2.tar.gz

---
+++ packages/RimuDNS/RimuDNS-0.0.2.tar.gz/RimuDNS-0.0.2/rimudns/rimudns.py
@@ -0,0 +1,336 @@
+# See https://rimuhosting.com/dns/dyndns.jsp for more information
+#  or https://zonomi.com/app/dns/dyndns.jsp
+# Tests
+#
+# from rimudns import RimuDNS
+#
+# api_key = 'GETYOURAPIKEYFROMTHECONTROLPANEL'
+# dns = DNS(api_key)
+#
+# dns.change_ip('192.168.59.133', '192.168.59.132')
+# if dns.set_record('aa1.example.com', '94.162.59.133'): 
+#     print 'updated: %s records' % dns.record_count
+#
+# dns.list_zones()
+#
+# dns.create_zone('test.example.com') # should work the first time
+# dns.create_zone('test.example.com') # should fail with 500 error
+#
+# dns.delete_zone('test.example.com') # should work the first time
+# dns.delete_zone('test.example.com') # should fail with 500 error
+#
+# dns.list_records('example.com')
+# dns.list_records('example.com', all_records=True)
+#
+# dns.create_zone('test.example.com') # should work the first time
+# dns.set_record('test.example.com', '127.0.0.1')
+# dns.set_record('mail.test.example.com', 'test.example.com', 'CNAME')
+#
+# updates_list = [
+#            {
+#                'action': 'SET', # or 'DELETE',
+#                'host': 'foo.test.example.com,bar.test.deeb.com', # or ['foo.com', 'bar.com],
+#                'value': '192.168.1.1',
+#                'type': 'A' # or 'MX' or 'CNAME' or 'TXT', optional default A, 
+#                #'prio': for MX records, number default 0,
+#                #'ttl': number, optional default 3600,
+#            },
+#            { 
+#                'action': 'DELETE',
+#                'host': ['foo.test.example.com', 'bar.test.example.com'],
+#                'value': '192.168.1.1',
+#                'type': 'CNAME', # or 'TXT', optional default A, 
+#                #'prio': for MX records, number default 0,
+#                #'ttl': number, optional default 3600,
+#            },      
+#        ]
+# dns.multiple_actions(updates_list)
+# dns.delete_zone('test.example.com') # should work the first time
+
+import urllib2
+from lxml import objectify
+from zonehandle import ZoneHandle
+import consts
+
+class RimuDNS:
+    
+    def __init__(self, apikey):
+        self.apikey = apikey
+        self.debug = False
+        self.base_url = 'https://rimuhosting.com' 
+        
+    def use_rimuhosting(self):
+        self.base_url = consts.rimu_base_url
+        
+    def use_zonomi(self):
+        self.base_url = consts.zonomi_base_url
+        
+    def change_ip(self, old_ip, new_ip):
+        '''Change an IP across all your zones.
+        https://rimuhosting.com/dns/ipchange.jsp?old_ip=0.0.0.0&new_ip=0.0.0.0&api_key=apikeyvaluehere
+        
+        '''
+        url = '%s/dns/ipchange.jsp?old_ip=%s&new_ip=%s&api_key=%s' % (self.base_url, old_ip, new_ip, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                self.record_count = root.record_count
+                return self.record_count
+        except Exception, e:
+            if self.debug: print e.read()
+            raise e
+        
+    
+    def list_zones(self):
+        '''Return a list of zones on your account
+        https://rimuhosting.com/dns/dyndns.jsp?action=QUERYZONES&api_key=apikeyvaluehere 
+        
+        '''
+        zones = []
+        url = '%s/dns/dyndns.jsp?action=QUERYZONES&api_key=%s' % (self.base_url, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                for zone in root.actions.action.iterchildren():
+                    zoneinfo = {}
+                    for item in zone.items():
+                        zoneinfo[item[0]]= item[1]
+                    zones.append(zoneinfo) 
+        except Exception, e:
+            if self.debug: print e.read()
+            raise e
+        return zones
+        
+    def create_zone(self, zone_name):
+        '''Setup a new DNS zone
+        https://rimuhosting.com/dns/addzone.jsp?name=example.com&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/addzone.jsp?name=%s&api_key=%s' % (self.base_url, zone_name, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                return zone_name
+        except urllib2.HTTPError, e:
+            raise e
+            
+    def delete_zone(self, zone_name):
+        '''Delete a DNS zone.
+        https://rimuhosting.com/dns/dyndns.jsp?action=DELETEZONE&name=example.com&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/dyndns.jsp?action=DELETEZONE&name=%s&api_key=%s' % (self.base_url, zone_name, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                return zone_name
+        except Exception, e:
+            raise e
+            
+    def convert_to_slave(self, zone_name, master_ip): 
+        '''Convert a zone to a slave zone with the specified master name server IP address.
+         https://rimuhosting.com/dns/converttosecondary.jsp?name=example.com&master=0.0.0.0&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/converttosecondary.jsp?name=%s&master=%s&api_key=%s' % (self.base_url, zone_name, master_ip, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                return zone_name
+        except urllib2.HTTPError, e:
+            raise e
+            
+    def convert_to_regular(self, zone_name): 
+        '''Convert a zone from a slave zone back to a 'regular' zone.
+         https://rimuhosting.com/dns/converttomaster.jsp?name=example.com&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/converttosecondary.jsp?name=%s&master=%s&api_key=%s' % (self.base_url, zone_name, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                return zone_name
+        except urllib2.HTTPError, e:
+            raise e
+            
+    def list_records(self, zone_name, all_records=False):
+        '''Retrieve a list of records with the specified name.
+        https://rimuhosting.com/dns/dyndns.jsp?action=QUERY&name=example.com&api_key=apikeyvaluehere
+
+        '''
+        if all_records: zone_name = '**.%s' % zone_name
+        url = '%s/dns/dyndns.jsp?action=QUERY&name=%s&api_key=%s' % (self.base_url, zone_name, self.apikey)
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            records = {}
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                for record in root.actions.action.iterchildren():
+                    record_type = record.attrib['type']
+                    if not records.has_key(record_type): records[record_type] = []
+                    records[record_type].append(record.attrib)
+                return records
+        except Exception, e:
+            raise e
+            
+
+            
+    def set_record(self, host, value, record_type='A', prio=None, ttl=None):
+        '''Set an IP Address (A) record
+        https://rimuhosting.com/dns/dyndns.jsp?action=SET&name=example.com&value=10.0.0.1&type=A&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/dyndns.jsp?api_key=%s&action=SET' % (self.base_url, self.apikey)
+        url += '&name=%s' % host
+        url += '&value=%s' % value
+        url += '&type=%s' % record_type
+        if prio and record_type=='MX': 
+            url += '&prio=%s' % prio
+        if ttl: 
+            url += '&ttl=%s' % ttl
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                self.result_counts = root.result_counts.attrib
+                return self.result_counts
+        except Exception, e:
+            if self.debug: print e.read()
+            raise e
+
+    def delete_record(self, host, value, record_type='A'):
+        '''Delete an IP Address (A) record
+        https://rimuhosting.com/dns/dyndns.jsp?action=DELETE&name=example.com&type=A&api_key=apikeyvaluehere
+
+        '''
+        url = '%s/dns/dyndns.jsp?api_key=%s&action=DELETE' % (self.base_url, self.apikey) 
+        url += '&name=%s' % host
+        url += '&value=%s' % value
+        url += '&type=%s' % record_type
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                self.result_counts = root.result_counts.attrib
+                return self.result_counts
+        except Exception, e:
+            if self.debug: print e.read()
+            raise e
+            
+            
+           
+    def multiple_actions(self, updates_list):
+        '''Multiple actions: set two records to the same IP and delete the bar.example.com IP Address (A) record. 
+        https://rimuhosting.com/dns/dyndns.jsp?action[1]=SET&name[1]=example.com,bar.example.com&value[1]=10.0.0.1&action[2]=DELETE&host[2]=bar.example.com&api_key=apikeyvaluehere
+        
+        updates_list = [
+            {
+                'action': 'SET' or 'DELETE',
+                'host': 'foo.com,bar.com' or ['foo.com', 'bar.com],
+                'value': '192.168.1.1',
+                'type': 'A' or 'MX' or 'CNAME' or 'TXT', optional default A, 
+                'prio': for MX records, number default 0,
+                'ttl': number, optional default 3600,
+            }        
+        ]
+        
+        '''
+        url = '%s/dns/dyndns.jsp?api_key=%s' % (self.base_url, self.apikey)
+        for index, update in enumerate(updates_list):
+            for key,val in update.iteritems():
+                if key=='host' and isinstance(val, list):
+                    val = ','.join([str(i) for i in val])
+                url_part = '&%s[%s]=%s' % (key, index, val)
+                if self.debug: print url_part
+                url += url_part
+            if self.debug: print url 
+        req = urllib2.Request(url, headers={'User-Agent': consts.user_agent})
+        try:
+            response = urllib2.urlopen(req).read()
+            if self.debug: print response
+            root = objectify.fromstring(response)
+            if str(root.is_ok).startswith('OK'):
+                self.result_counts = root.result_counts.attrib
+                return self.result_counts
+        except urllib2.HTTPError, e:
+            if self.debug: "Error: %s" % e.read()
+            raise e
+        
+    def to_file(self, zone_name, file, records_dict=None):
+        '''Export zone to file
+        
+        '''
+        try:
+            if not records_dict:
+                records_dict = self.list_records(zone_name, True)
+            if records_dict:
+                zh = ZoneHandle(zone_name)
+                zh.from_records_dict(records_dict)
+                zh.to_file(file)
+        except Exception, e:
+            if self.debug: print e
+            raise e
+            
+    def import_zone(self, zone_name, method, param=None, dryrun=False):
+        '''Import zone 
+        methods are AXFR, FILE, TEXT, DICT and GUESS
+        
+        '''
+        zh = ZoneHandle(zone_name)
+        try:
+            if method==ZoneHandle.IMPORT_AXFR:
+                zh.from_axfr(param)
+            elif method==ZoneHandle.IMPORT_FILE: 
+                zh.from_file(param)
+            elif method==ZoneHandle.IMPORT_TEXT:
+                zh.from_text(param)
+            elif method==ZoneHandle.IMPORT_DICT:
+                zh.from_records_dict(param)
+            elif method==ZoneHandle.IMPORT_GUESS:
+                zh.from_guessing(param)
+                
+        except Exception, e:
+                if self.debug: print e
+                raise e
+            
+        records_dict = zh.to_records_dict()
+        if not dryrun:
+            created = self.create_zone(zone_name)
+            for record_type, records in records:
+                if record_type=='SOA': continue
+                for record in records:
+                    try:
+                        self.set_record(record['name'], record['content'], record_type, record['prio'], record['ttl'])
+                    except Exception, e:
+                        if self.debug: print e
+                        raise e
+                    
+        return records_dict
+            
+            
+            
+            
---
+++ packages/RimuDNS/RimuDNS-0.0.2.tar.gz/RimuDNS-0.0.2/rimudns/zonehandle.py
@@ -0,0 +1,284 @@
+import dns.zone
+import dns.resolver 
+import dns.name
+from dns.rdataclass import *
+from dns.rdatatype import *
+import dns.rdtypes.ANY
+import dns.rdtypes.ANY.CNAME
+import dns.rdtypes.ANY.SOA
+import dns.rdtypes.ANY.TXT
+import dns.rdtypes.ANY.NS
+import dns.rdtypes.ANY.MX
+import dns.rdtypes.IN.A
+import dns.rdtypes.IN
+import re
+
+class ZoneHandle:
+    IMPORT_AXFR = 1
+    IMPORT_FILE = 2
+    IMPORT_TEXT = 3
+    IMPORT_DICT = 4
+    IMPORT_GUESS = 5
+    
+    def __init__(self, domain_name):
+        self.dns_name = dns.name.Name((domain_name,))
+        self.domain_name = domain_name
+        self.zone = dns.zone.Zone(self.dns_name)
+        self.possible_subdomains = ['www', 'www1', 'www2', 
+                                    'ftp', 
+                                    'webmail', 'mail', 'mail1', 'mail2' 'smtp', 'imap', 'pop', 
+                                    'ns1', 'ns2', 'ns3', 'ns4']
+        self.dns_server = 'ns1.rimuhosting.com'
+        self.debug = False
+        
+    def from_axfr(self, dns_server):
+        try:
+            self.zone = dns.zone.from_xfr(dns.query.xfr(dns_server, self.domain_name))
+            return self.zone
+        except Exception, e:
+            if self.debug: print e
+            raise e
+
+    def from_file(self, file):
+        try:
+            self.zone = dns.zone.from_file(file, self.domain_name)
+            return self.zone
+        except Exception, e:
+            if self.debug: print e
+            raise e
+        
+    def from_text(self, text):
+        try:
+            self.zone = dns.zone.from_text(text, self.domain_name)
+            return self.zone
+        except Exception, e:
+            if self.debug: print e
+            raise e
+    
+    def from_records_dict(self, records_dict):
+        try:
+            zone_text = ''
+            for rtype in records_dict.keys():
+                records = records_dict[rtype]
+                if rtype=='SOA' and len(records)>0: 
+                    soa_rec = records[0]
+                    mname, rname, serial, refresh, retry, expire, minimum = soa_rec['content'].split(' ')
+                    if self.domain_name!=soa_rec['name']:
+                        if self.debug: print 'Domain name does not match'
+                        return False
+                    zone_text += '\n; SOA Record\n'
+                    zone_text += '$ORIGIN %s.\n' % self.domain_name
+                    zone_text += '$TTL 1h\n'
+                    zone_text += '%s. IN SOA %s %s (\n' % (self.domain_name, mname, rname)
+                    zone_text += '  %s ; serial number of this zone file\n' % serial
+                    zone_text += '  %s ; slave refresh\n' % refresh
+                    zone_text += '  %s ; slave retry time in case of a problem\n' % retry
+                    zone_text += '  %s ; slave expiration time\n' % expire
+                    zone_text += '  %s ; maximum caching time in case of failed lookups\n' % minimum
+                    zone_text += ')\n'
+                if rtype=='A' and len(records)>0:
+                    for record in records:
+                        ttl = re.findall('\d+', record['ttl'])[0]
+                        zone_text += '%s  %s  IN  A %s\n' % (record['name'], ttl, record['content'])
+                if rtype=='MX' and len(records)>0:
+                    for record in records:
+                        ttl = re.findall('\d+', record['ttl'])[0]
+                        if record['name']==self.domain_name:
+                            record['name'] = '@'
+                        zone_text += '%s  %s  IN  MX  %s  %s\n' % (record['name'], ttl, record['prio'], record['content'])
+                if rtype=='NS' and len(records)>0:
+                    for record in records:
+                        ttl = re.findall('\d+', record['ttl'])[0]
+                        zone_text += '@  %s  IN  NS %s\n' % (ttl, record['content'])
+                if rtype=='TXT' and len(records)>0:
+                    for record in records:
+                        ttl = re.findall('\d+', record['ttl'])[0]
+                        zone_text += '%s  %s  IN  TXT "%s"\n' % (record['name'], ttl, record['content'])
+                if rtype=='CNAME' and len(records)>0:
+                    for record in records:
+                        ttl = re.findall('\d+', record['ttl'])[0]
+                        if record['content']==self.domain_name:
+                            record['content'] = '@'
+                        zone_text += '%s  %s  IN  CNAME  %s\n' % (record['name'], ttl, record['content'])
+                
+            print zone_text
+            self.zone = dns.zone.from_text(zone_text)
+            return self.zone
+
+        except Exception, e:
+            print e
+            if self.debug: print e
+            raise e
+        
+    def to_records_dict(self):
+        records_dict = { 'SOA': [], 'A': [], 'MX': [], 'NS': [], 'TXT': [], 'CNAME': [] }
+        try:
+            zone = self.zone
+            for name, node in zone.nodes.items():
+                prio = 0
+                if self.debug: print 'name: ', name
+                rdatasets = node.rdatasets
+                for rdataset in rdatasets:
+                    if self.debug:
+                        print "--- BEGIN RDATASET ---"
+                        print "rdataset string representation:", rdataset
+                        print "rdataset rdclass:", rdataset.rdclass
+                        print "rdataset rdtype:", rdataset.rdtype
+                        print "rdataset ttl:", rdataset.ttl
+                        print "rdataset has following rdata:"
+                    for rdata in rdataset:
+                        if rdataset.rdtype == SOA:
+                            if self.debug: print 'SOA, getting info ...'
+                            rdtype = 'SOA'
+                            content = ' '.join((
+                                                str(rdata.mname), str(rdata.rname), 
+                                                str(rdata.serial), 
+                                                str(rdata.refresh), 
+                                                str(rdata.retry), 
+                                                str(rdata.expire), 
+                                                str(rdata.minimum)
+                                                ))
+                            if self.debug: print 'SOA, contents: ', content
+                        if rdataset.rdtype == A:
+                            rdtype = 'A'
+                            content = rdata.address
+                        if rdataset.rdtype == MX:
+                            rdtype = 'MX'
+                            content = str(rdata.exchange)
+                            prio = rdata.preference
+                        if rdataset.rdtype == NS:
+                            rdtype = 'NS'
+                            content = str(rdata.target)
+                        if rdataset.rdtype == TXT:
+                            rdtype = 'TXT'
+                            content = rdata.strings
+                        if rdataset.rdtype == CNAME:
+                            rdtype = 'CNAME'
+                            content = str(rdata.target)
+                            
+                        record = {'content': content,  'type': rdtype, 'name': str(name), 'prio': prio, 'ttl': rdataset.ttl}
+                        records_dict[rdtype].append(record)
+                
+            return records_dict
+        except Exception, e:
+            if self.debug: print e
+            raise e
+
+    def from_guessing(self, extra_names=None):
+        if extra_names: 
+            self.possible_subdomains.extend(x for x in extra_names if x not in self.possible_subdomains) 
+        if not self._guess_soa(): 
+            raise Exception('Unable to guess SOA!')
+        if not self._guess_ns():
+            if self.debug: raise Exception( 'Unable to guess NS records')
+        if not self._guess_mx():
+            if self.debug: raise Exception('Unable to guess MX records')
+        if not self._guess_a():
+            if self.debug: raise Exception('Unable to guess A records')
+        if not self._guess_cname():
+            if self.debug: raise Exception('Unable to guess CNAME records')
+        if not self._guess_txt():
+            if self.debug: raise Exception('Unable to guess TXT records')
+        
+        if self.debug: print self.zone.items()
+        return self.zone
+            
+    def _guess_soa(self):
+        try:
+            soa_answer = dns.resolver.query(self.domain_name, 'SOA')
+            soa_rr = soa_answer.rrset[0]
+            rd_set = self.zone.find_rdataset(self.domain_name, rdtype=SOA , create=True)
+            rdata = dns.rdtypes.ANY.SOA.SOA(soa_rr.rdclass, 
+                                            soa_rr.rdtype,
+                                            soa_rr.mname, 
+                                            soa_rr.rname, 
+                                            soa_rr.serial, 
+                                            soa_rr.refresh, 
+                                            soa_rr.retry,
+                                            soa_rr.expire, 
+                                            soa_rr.minimum)
+            rd_set.add(rdata)
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+
+    def _guess_ns(self):
+        try: 
+            ns_answer = dns.resolver.query(self.domain_name, 'NS')
+            for ns_record in ns_answer.rrset:
+                rd_set = self.zone.find_rdataset(self.domain_name, rdtype=NS, create=True)
+                rdata = dns.rdtypes.ANY.NS.NS(IN, NS, ns_record.target)
+                rd_set.add(rdata)
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+
+    def _guess_mx(self):
+        try: 
+            mx_answer = dns.resolver.query(self.domain_name, 'MX')
+            for mx_record in mx_answer.rrset:
+                rd_set = self.zone.find_rdataset(self.domain_name, rdtype=MX, create=True)
+                rdata = dns.rdtypes.ANY.MX.MX(IN, MX, mx_record.preference, mx_record.exchange)
+                rd_set.add(rdata)
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+
+    def _guess_a(self):
+        try: 
+            a_answer = dns.resolver.query(self.domain_name, 'A')
+            for a_record in a_answer.rrset:
+                rd_set = self.zone.find_rdataset(self.domain_name, rdtype=A, create=True)
+                rdata = dns.rdtypes.IN.A.A(IN, A, address=a_record.address)
+                rd_set.add(rdata)
+            for subdomain in self.possible_subdomains:
+                try: 
+                    a_answer = dns.resolver.query('%s.%s.' % (subdomain, self.domain_name), 'A')
+                    for a_record in a_answer.rrset:
+                        rd_set = self.zone.find_rdataset(self.domain_name, rdtype=A, create=True)
+                        rdata = dns.rdtypes.IN.A.A(IN, A, address=a_record.address)
+                        rd_set.add(rdata)
+                except:
+                    pass
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+        
+    def _guess_cname(self):
+        try: 
+            for subdomain in self.possible_subdomains:
+                try: 
+                    cname_answer = dns.resolver.query('%s.%s.' % (subdomain, self.domain_name), 'CNAME')
+                    for cname_record in cname_answer.rrset:
+                        rd_set = self.zone.find_rdataset(self.domain_name, rdtype=CNAME, create=True)
+                        rdata = dns.rdtypes.ANY.CNAME.CNAME(IN, CNAME, target=cname_record.target)
+                        rd_set.add(rdata)
+                except:
+                    pass
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+        
+    def _guess_txt(self):
+        try: 
+            txt_answer = dns.resolver.query(self.domain_name, 'TXT')
+            for txt_record in txt_answer.rrset:
+                rd_set = self.zone.find_rdataset(self.domain_name, rdtype=TXT, create=True)
+                rdata = dns.rdtypes.ANY.TXT.TXT(IN, TXT, txt_record.strings)
+                rd_set.add(rdata)
+            return True
+        except Exception, e:
+            if self.debug: print e
+            return False
+
+    def to_file(self, file, sorted=True, relativize=True):
+        try:
+            self.zone.to_file(file, sorted, relativize)
+            return True 
+        except Exception, e:
+            raise e
