1258e8bb2e05ae34c31c6148321e3fbae7fa5a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package django-viewmodels/0.1.1/django-viewmodels-0.1.1.tar.gz

---
+++ packages/django-viewmodels/django-viewmodels-0.1.1.tar.gz/django-viewmodels-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import subprocess
+import os.path
+try: 
+    long_description = (open('README.rst').read() + 
+                    open('CHANGES.rst').read() +
+                    open('TODO.rst').read())
+except IOError:
+    long_description = "Class based views for Django with automatic viewmodel rendering"
+
+setup(
+    name='django-viewmodels',
+    version='0.1.1',
+    description='Class based views for Django with automatic viewmodel rendering',
+    author='Jeff Schwaber, Chris Pitzer, LoFi Art',
+    author_email='freyley@gmail.com',
+    long_description=long_description,
+    url='',
+    packages=['djviewmodels'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    #test_suite='tests.runtests.runtests',
+    package_data={
+        'djviewmodels': [
+            '*.py',
+        ]
+    },
+)
