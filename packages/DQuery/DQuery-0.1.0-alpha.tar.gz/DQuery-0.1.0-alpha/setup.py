dd2541e1f09dcc2ef1a39d4b22440a781e85baa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package DQuery/0.1.0-alpha/DQuery-0.1.0-alpha.tar.gz

---
+++ packages/DQuery/DQuery-0.1.0-alpha.tar.gz/DQuery-0.1.0-alpha/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DQuery',
+    version='0.1.0-alpha',
+    author='David Gustafsson',
+    author_email='david.gustafsson@xelera.se',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['bin/dquery', 'bin/dquery_php_var_json'],
+    url='http://pypi.python.org/pypi/DQuery/',
+    download_url='https://github.com/david-xelera/dquery/tarball/master',
+    license='GPL',
+    description='Drupal multisite query-tool.',
+    long_description=open('README.txt').read(),
+    keywords=["drupal"],
+    install_requires=[
+        "PyYAML",
+        "SQLAlchemy",
+        "MySQL-python",
+        "colorama",
+        "httplib2",
+        "lxml",
+        "phpserialize",
+        "pyCLI",
+        "termcolor",
+    ],
+)
+
