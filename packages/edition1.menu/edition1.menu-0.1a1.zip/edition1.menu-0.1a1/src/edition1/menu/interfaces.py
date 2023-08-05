ee4dc693cce9617695ab89a93ecbb76c3df95322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:05 +0000

    Add package edition1.menu/0.1a1/edition1.menu-0.1a1.zip

---
+++ packages/edition1.menu/edition1.menu-0.1a1.zip/edition1.menu-0.1a1/src/edition1/menu/interfaces.py
@@ -0,0 +1,31 @@
+from plone.app.portlets import PloneMessageFactory as PMF
+from zope import schema
+from zope.interface import Interface
+
+from edition1.menu import Edition1MenuMessageFactory as _
+
+
+class IMenuSettings(Interface):
+    """Global menu settings."""
+
+    edition1_menu_min_width = schema.Int(
+        title=_(u'label_min_width',
+                default=u'Minimal width (in px) for the wide menu'),
+        description=_(u'help_min_width',
+                      default=u'If the width of the browser window size is '
+                              u'below this value, the small version of the '
+                              u'menu is shown. The default value (0) makes '
+                              u'sure the wide menu is always shown.'),
+        default=0,
+        required=True,)
+
+    edition1_menu_navigation_depth = schema.Int(
+        title=PMF(u"label_navigation_tree_depth",
+                default=u"Navigation tree depth"),
+        description=PMF(u"help_navigation_tree_depth",
+                      default=u"How many folders should be included "
+                      "before the navigation tree stops. 0 "
+                      "means no limit. 1 only includes the "
+                      "root folder."),
+        default=0,
+        required=False)
