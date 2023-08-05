b303b9a5e6d08cff5dbee0fbe1f956b13fec9794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:50 +0000

    Add package turses/0.2.8/turses-0.2.8.tar.gz

---
+++ packages/turses/turses-0.2.8.tar.gz/turses-0.2.8/turses/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+turses
+~~~~~~
+
+A Twitter client with a sexy curses interface.
+
+:copyright: © 2012 turses contributors. See ``AUTHORS``.
+:license: GPL3
+
+"""
+
+__title__ = "turses"
+__author__ = "Alejandro Gómez"
+__copyright__ = "Copyright 2012 turses contributors"
+__license__ = "GPL3"
+__version__ = (0, 2, 8)
+
+version = "%s.%s.%s" % __version__
