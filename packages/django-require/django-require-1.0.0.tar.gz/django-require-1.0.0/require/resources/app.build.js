072164b4652be56db15c4444c74db3f5ccb0db9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:25 +0000

    Add package django-require/1.0.0/django-require-1.0.0.tar.gz

---
+++ packages/django-require/django-require-1.0.0.tar.gz/django-require-1.0.0/require/resources/app.build.js
@@ -0,0 +1,14 @@
+/**
+ * Build profile for django-require.
+ * 
+ * This supports all the normal configuration available to a r.js build profile. The only gotchas are:
+ *
+ *   - 'baseUrl' will be overidden by django-require during the build process.
+ *   - 'appDir' will be overidden by django-require during the build process.
+ *   - 'dir' will be overidden by django-require during the build process. 
+ */
+({
+    optimize: "closure",
+    optimizeCss: "standard",
+    modules: []
+})
