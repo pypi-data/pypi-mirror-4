908f3276b2512526562237fedf4d92c22cc26260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package django-attest/0.9.0/django-attest-0.9.0.tar.gz

---
+++ packages/django-attest/django-attest-0.9.0.tar.gz/django-attest-0.9.0/setup.py
@@ -0,0 +1,40 @@
+# coding: utf8
+from setuptools import setup
+
+
+setup(
+    name='django-attest',
+    version='0.9.0',
+    description='Provides Django specific testing helpers to Attest',
+
+    author='Bradley Ayers',
+    author_email='bradley.ayers@gmail.com',
+    license='Simplified BSD',
+    url='https://github.com/bradleyayers/django-attest/',
+
+    packages=['django_attest'],
+    install_requires=['Django >=1.2', 'Attest >=0.6', 'distribute', 'six'],
+    use_2to3=True,
+
+    entry_points={
+        'attest.reporters': [
+            'django-xml = django_attest:XmlReporter',
+            'django-xunit = django_attest:XUnitReporter',
+            'django-quickfix = django_attest:QuickFixReporter',
+            'django-plain = django_attest:PlainReporter',
+            'django-fancy = django_attest:FancyReporter',
+            'django = django_attest:auto_reporter',
+        ],
+    },
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
