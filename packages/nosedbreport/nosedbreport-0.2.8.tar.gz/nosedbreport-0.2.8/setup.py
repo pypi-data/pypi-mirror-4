48d437a476cb1c54674d87a80412286b570fb4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package nosedbreport/0.2.8/nosedbreport-0.2.8.tar.gz

---
+++ packages/nosedbreport/nosedbreport-0.2.8.tar.gz/nosedbreport-0.2.8/setup.py
@@ -0,0 +1,31 @@
+"""
+setup.py for nosedbreport
+
+
+"""
+__author__ = "Ali-Akber Saifee"
+__email__ = "ali@mig33global.com"
+__copyright__ = "Copyright 2011, ProjectGoth"
+
+from setuptools import setup, find_packages
+import nosedbreport
+setup(
+    name='nosedbreport',
+    author = __author__,
+    author_email = __email__,
+    license = "MIT",
+    version=nosedbreport.__version__,
+    url='https://nosedbreport.readthedocs.org/en/latest/',
+    include_package_data = True,
+    package_data = {
+            '':[ 'README.rst' ],
+         },
+    description='Nose plugin for recording test results to a database',
+    long_description=open('README.rst').read(),
+    entry_points = {
+        'nose.plugins.0.10': [
+            'nosedbreport = nosedbreport:NoseDBReporter']
+        },
+    packages = ['nosedbreport'],
+)
+
