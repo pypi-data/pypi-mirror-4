9ea3842cab9114da6879839dbda526a8b5649c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package tiddlywebplugins.tiddlyspace/1.1.29/tiddlywebplugins.tiddlyspace-1.1.29.tar.gz

---
+++ packages/tiddlywebplugins.tiddlyspace/tiddlywebplugins.tiddlyspace-1.1.29.tar.gz/tiddlywebplugins.tiddlyspace-1.1.29/tiddlywebplugins/tiddlyspace/config.py
@@ -0,0 +1,49 @@
+"""
+Base configuration for TiddlySpace.
+
+This provides the basics which may be changed in tidlywebconfig.py.
+"""
+
+from tiddlywebplugins.instancer.util import get_tiddler_locations
+
+from tiddlywebplugins.tiddlyspace.instance import store_contents
+
+try:
+    from pkg_resources import resource_filename
+except ImportError:
+    from tiddlywebplugins.utils import resource_filename
+
+
+PACKAGE_NAME = 'tiddlywebplugins.tiddlyspace'
+TIDDLYWIKI_ALPHA = resource_filename(PACKAGE_NAME, 'resources/alpha.html')
+TIDDLYWIKI_BETA = resource_filename(PACKAGE_NAME, 'resources/beta.html')
+TIDDLYWIKI_EXTERNAL_ALPHA = resource_filename(PACKAGE_NAME,
+        'resources/external_alpha.html')
+
+config = {
+    'instance_tiddlers': get_tiddler_locations(store_contents, PACKAGE_NAME),
+    'atom.default_filter': 'select=tag:!excludeLists;sort=-modified;limit=20',
+    'atom.author_uri_map': '/profiles/%s',
+    'atom.hub': 'http://pubsubhubbub.appspot.com/',
+    'auth_systems': ['tiddlywebplugins.tiddlyspace.cookie_form',
+        'tiddlywebplugins.tiddlyspace.openid'],
+    'beanstalk.listeners': ['tiddlywebplugins.dispatcher.listener',
+        'tiddlywebplugins.tiddlyspace.profiles'],
+    'bag_create_policy': 'ANY',
+    'recipe_create_policy': 'ANY',
+    'css_uri': '/bags/common/tiddlers/tiddlyweb.css',
+    'socialusers.reserved_names': ['www', 'about', 'announcements',
+        'dev', 'info', 'api', 'status', 'login', 'frontpage'],
+    'cookie_age': '2592000',  # 1 month
+    'server_store': ['tiddlywebplugins.tiddlyspace.store', {
+        'db_config': 'mysql:///tiddlyspace?charset=utf8&use_unicode=0'}],
+    'indexer': 'tiddlywebplugins.mysql3',
+    'tiddlywebwiki.binary_limit': 1048576,  # 1 MB
+    # TiddlyWiki external, alpha, beta serialization
+    'base_tiddlywiki_beta': TIDDLYWIKI_BETA,
+    'base_tiddlywiki_alpha': TIDDLYWIKI_ALPHA,
+    'base_tiddlywiki_external_alpha': TIDDLYWIKI_EXTERNAL_ALPHA,
+    'wsearch.handler': 'hsearch',
+    'serializers': {
+        'text/x-tiddlywiki': ['tiddlywebplugins.tiddlyspace.betaserialization',
+            'text/html; charset=UTF-8']}}
---
+++ packages/tiddlywebplugins.tiddlyspace/tiddlywebplugins.tiddlyspace-1.1.29.tar.gz/tiddlywebplugins.tiddlyspace-1.1.29/tiddlywebplugins/tiddlyspace/openid.py
@@ -0,0 +1,106 @@
+"""
+Subclass of tiddlywebplugins.openid2 to support
+tiddlyweb_secondary_user cookie.
+"""
+
+
+import urlparse
+
+from tiddlyweb.web.util import server_host_url, make_cookie
+
+from tiddlywebplugins.openid2 import Challenger as OpenID
+
+
+FRAGMENT_PREFIX = 'auth:OpenID:'
+
+
+class Challenger(OpenID):
+
+    def __init__(self):
+        self.name = __name__
+
+    def _domain_path(self, environ):
+        return "." + environ['tiddlyweb.config']['server_host']['host']
+
+    def _success(self, environ, start_response, info):
+        """
+        After successful validation of an openid generate
+        and send a cookie with the value of that openid.
+        If this is a normal auth scenario make the name
+        of the cookie the normal 'tiddlyweb_user'. If this
+        is auth addition, where a fragment of 'auth:OpenID' is
+        set, then name the cookie 'tiddlyweb_secondary_user'.
+        """
+        usersign = info.getDisplayIdentifier()
+        if info.endpoint.canonicalID:
+            usersign = info.endpoint.canonicalID
+        # canonicolize usersign to tiddlyweb form
+        if usersign.startswith('http'):
+            usersign = usersign.split('://', 1)[1]
+        usersign = usersign.rstrip('/')
+        redirect = environ['tiddlyweb.query'].get(
+            'tiddlyweb_redirect', ['/'])[0]
+        uri = urlparse.urljoin(server_host_url(environ), redirect)
+
+        cookie_name = 'tiddlyweb_user'
+        cookie_age = environ['tiddlyweb.config'].get('cookie_age', None)
+        try:
+            fragment = uri.rsplit('#', 1)[1]
+        except (ValueError, IndexError):
+            fragment = None
+        secondary_cookie_name = 'tiddlyweb_secondary_user'
+        secondary_cookie_only = False
+        if fragment:
+            openid = fragment[len(FRAGMENT_PREFIX):]
+            uri = uri.replace(FRAGMENT_PREFIX + openid,
+                    FRAGMENT_PREFIX + usersign)
+            secondary_cookie_only = True
+
+        secret = environ['tiddlyweb.config']['secret']
+        cookie_header_string = make_cookie(cookie_name, usersign,
+                mac_key=secret, path=self._cookie_path(environ),
+                expires=cookie_age)
+        secondary_cookie_header_string = make_cookie(
+                secondary_cookie_name, usersign,
+                mac_key=secret, path=self._cookie_path(environ),
+                expires=cookie_age, domain=self._domain_path(environ))
+        headers = [('Location', uri.encode('utf-8')),
+                    ('Content-Type', 'text/plain'),
+                    ('Set-Cookie', secondary_cookie_header_string)]
+        if not secondary_cookie_only:
+            headers.append(('Set-Cookie', cookie_header_string))
+
+        start_response('303 See Other', headers)
+        return [uri]
+
+    def _render_form(self, environ, start_response, openid='',
+            message='', form=''):
+        redirect = environ['tiddlyweb.query'].get(
+            'tiddlyweb_redirect', ['/'])[0]
+        start_response('200 OK', [(
+            'Content-Type', 'text/html')])
+        environ['tiddlyweb.title'] = 'OpenID Login'
+        return ["""
+<div id='content'>
+    <div class='message'>%s</div>
+    <pre>
+    <form action="" method="POST">
+    OpenID: <input name="openid" size="60" value="%s"/>
+    <input type="hidden" name="tiddlyweb_redirect" value="%s" />
+    <input type="hidden" id="csrf_token" name="csrf_token" />
+    <input type="submit" value="submit" />
+    </form>
+    <script type="text/javascript"
+            src="%s/bags/tiddlyspace/tiddlers/TiddlySpaceCSRF"></script>
+    <script type="text/javascript">
+        var csrfToken = window.getCSRFToken(),
+            el = null;
+
+        if (csrfToken) {
+            el = document.getElementById('csrf_token');
+            el.value = csrfToken;
+        }
+    </script>
+    </pre>
+</div>""" % (message, openid, redirect,
+    environ['tiddlyweb.config']['server_prefix'])]
