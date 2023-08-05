0039279d0e2879670a81824e6f9629d4410dcb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:24 +0000

    Add package infi.asi/0.3/infi.asi-0.3-py2.7.egg

---
+++ packages/infi.asi/infi.asi-0.3-py2.7.egg/infi/asi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c55dba9c7a56151fca8fbb8219aef2272cd44372'
+__git_head_subject__ = 'Finished release v0.3.'
+__git_head_message__ = '* release/v0.3:\n  STORAGEMODEL-176 ReportLuns implementation and test\n  TRIVIAL updated README.md\n  empty commit after version v0.2.6'
+__git_dirty_diff__ = ''
