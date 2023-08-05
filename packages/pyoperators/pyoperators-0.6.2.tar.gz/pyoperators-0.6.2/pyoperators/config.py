55598054b406aba1301f4d6a9695fc3d471b0aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package pyoperators/0.6.2/pyoperators-0.6.2.tar.gz

---
+++ packages/pyoperators/pyoperators-0.6.2.tar.gz/pyoperators-0.6.2/pyoperators/config.py
@@ -0,0 +1,9 @@
+import os
+
+VERSION = '0.6.2'
+
+LOCAL_PATH = os.path.join(os.path.expanduser('~'), '.local/share/pyoperators')
+if not os.path.exists(LOCAL_PATH):
+    os.makedirs(LOCAL_PATH)
+
+del os
