10d8d79ee98354b707cd6dae6e445089eff5d0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:07 +0000

    Add package httpexceptor/1.1.0/httpexceptor-1.1.0.tar.gz

---
+++ packages/httpexceptor/httpexceptor-1.1.0.tar.gz/httpexceptor-1.1.0/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup, find_packages
+
+from httpexceptor import __version__ as VERSION, __author__ as AUTHOR
+
+DESC = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+
+META = {
+    'name': 'httpexceptor',
+    'url': 'http://pypi.python.org/pypi/httpexceptor',
+    'version': VERSION,
+    'description': 'WSGI middleware to handle HTTP responses using exceptions',
+    'long_description': DESC,
+    'license': 'LICENSE',
+    'author': AUTHOR,
+    'author_email': 'cdent@peermore.com',
+    'maintainer': 'FND',
+    'packages': find_packages(exclude=['test']),
+    'platforms': 'Posix; MacOS X; Windows',
+    'include_package_data': False,
+    'zip_safe': False,
+    'install_requires': [],
+    'extras_require': {
+        'testing': ['pytest'],
+        'coverage': ['figleaf', 'coverage']
+    }
+}
+
+
+setup(**META)
