99fcf91b8f3f9f98858eaa7b307a3d13fbd66bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package fib/1.0.2/fib-1.0.2.zip

---
+++ packages/fib/fib-1.0.2.zip/fib-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='fib',
+      description='calculate the giving number of fabnacci sequence',
+      author='guzhen',
+      author_email='604608916@qq.com',
+      version='1.0.2'
+      )
