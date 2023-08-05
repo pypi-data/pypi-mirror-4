1880fdd112f67b668a4857015e9737c96eb7c856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package django-cached-s3-storage/0.2/django-cached-s3-storage-0.2.tar.gz

---
+++ packages/django-cached-s3-storage/django-cached-s3-storage-0.2.tar.gz/django-cached-s3-storage-0.2/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='django-cached-s3-storage',
+    version='0.2',
+    py_modules=['cached_s3_storage'],
+    license='MIT',
+    author='Ulysses V',
+    author_email='uvilela [at] inoa.com.br',
+    url='https://github.com/ulyssesv/django-cached-s3-storage',
+    description='Django storage backend to be used with S3.',
+    long_description=open('README.rst').read()
+)
