f7b00f0951d8358728a1df5abc11df37d4489582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package blackhole/1.3.6/blackhole-1.3.6.tar.gz

---
+++ packages/blackhole/blackhole-1.3.6.tar.gz/blackhole-1.3.6/blackhole/__init__.py
@@ -0,0 +1,13 @@
+__author__ = "Kura"
+__copyright__ = "None"
+__credits__ = ["Kura", ]
+__license__ = "BSD 3-Clause"
+__version__ = "1.3.6"
+__maintainer__ = "Kura"
+__email__ = "kura@kura.io"
+__status__ = "Beta"
+
+__pname__ = "blackhole"
+__desc__ = "blackhole.io MTA"
+
+__fullname__ = "%s %s (%s)" % (__pname__, __version__, __status__)
