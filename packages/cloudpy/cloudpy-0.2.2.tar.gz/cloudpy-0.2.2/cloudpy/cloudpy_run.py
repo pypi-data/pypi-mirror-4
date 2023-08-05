bdf9de167481544caad71dd16045109e465ec170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:18 +0000

    Add package cloudpy/0.2.2/cloudpy-0.2.2.tar.gz

---
+++ packages/cloudpy/cloudpy-0.2.2.tar.gz/cloudpy-0.2.2/cloudpy/cloudpy_run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sh
+import sys
+import argparse
+from sys import argv
+
+def run(package, noisy, clean, config):
+    ENDLINE = "Connection to %s closed." % config.host.split("@")[1]
+    target = config.host_sep.join([config.depository, package])
+    eval_prog = "cloudpy-eval"
+
+    if not noisy:
+        eval_prog += " -q"
+    if clean:
+        eval_prog += " -c"
+
+    def print_line(line):
+        if line.strip() == ENDLINE:
+            return
+        sys.stdout.write(line)
+
+    try:
+        sh.ssh("-t", config.host, eval_prog, target, _out=print_line, err=print_line, _tty_in=True).wait()
+    except Exception:
+        pass
