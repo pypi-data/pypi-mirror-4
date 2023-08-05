95af7fe10effeb11c7c84186076b7f068b85bbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:40 +0000

    Add package n123ester/3.1.3/n123ester-3.1.3.tar.gz

---
+++ packages/n123ester/n123ester-3.1.3.tar.gz/n123ester-3.1.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'n123ester',
+	version		= '3.1.3',
+	py_modules	= ['n123ester'],
+	author  	= 'fzhudoudou',
+	author_email	= 'fzhudoudou@gmail.com',
+	url		= 'http://www.gmail.com',
+	description	= 'a printer by following tutorial',
+     )
+
