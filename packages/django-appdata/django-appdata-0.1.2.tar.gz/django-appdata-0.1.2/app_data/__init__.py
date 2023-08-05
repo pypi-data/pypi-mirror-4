2f55bcb9070b1f2bfdfae7598168d202a2ba95e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:03 +0000

    Add package django-appdata/0.1.2/django-appdata-0.1.2.tar.gz

---
+++ packages/django-appdata/django-appdata-0.1.2.tar.gz/django-appdata-0.1.2/app_data/__init__.py
@@ -0,0 +1,4 @@
+from .fields import AppDataField, ListModelMultipleChoiceField
+from .registry import app_registry
+from .containers import AppDataContainer
+from .forms import AppDataForm, multiform_factory, MultiForm
