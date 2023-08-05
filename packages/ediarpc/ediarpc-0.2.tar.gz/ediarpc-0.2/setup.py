8fd63081dce868ab009b8729c01f90e6f07099ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:10 +0000

    Add package ediarpc/0.2/ediarpc-0.2.tar.gz

---
+++ packages/ediarpc/ediarpc-0.2.tar.gz/ediarpc-0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2012, 2013 TortoiseLabs LLC
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+This software is provided 'as is' and without any warranty, express or
+implied. In no event shall the authors be liable for any damages arising
+from the use of this software.
+"""
+
+from distutils.core import setup
+
+setup(name='ediarpc',
+      version='0.2',
+      description='A JSON-based RPC calling framework which supports inline signatures derived from passphrases',
+      long_description="""
+A library which provides JSON-based RPC calls over TCP sockets with inline signatures to
+prove authenticity between two trusted parties.
+
+Provides a ``ServerProxy`` like object for abstracting the details of the RPC system behind
+python.""",
+      author='William Pitcock',
+      author_email='nenolod@tortois.es',
+      url='http://bitbucket.org/tortoiselabs/ediarpc',
+      packages=['ediarpc'],
+      classifiers=['License :: OSI Approved :: ISC License (ISCL)', 'Operating System :: POSIX',
+                   'Topic :: System :: Systems Administration'])
+
