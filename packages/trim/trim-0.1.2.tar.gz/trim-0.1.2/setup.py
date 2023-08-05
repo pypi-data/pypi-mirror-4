43834e05e02a1236a6bc7d70b85eb57cbdc1bf8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:17 +0000

    Add package trim/0.1.2/trim-0.1.2.tar.gz

---
+++ packages/trim/trim-0.1.2.tar.gz/trim-0.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""Setup for trim."""
+
+from distutils import core
+
+
+def version():
+    """Return version string."""
+    with open('trim') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                import ast
+                return ast.literal_eval(line.split('=')[1].strip())
+
+
+with open('README.rst') as readme:
+    core.setup(name='trim',
+               version=version(),
+               description='Trims trailing whitespace from files.',
+               long_description=readme.read(),
+               license='Expat License',
+               author='myint',
+               url='https://github.com/myint/trim',
+               classifiers=[
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Environment :: Console',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License'],
+               keywords='trim, strip, format, trailing, whitespace',
+               scripts=['trim'])
