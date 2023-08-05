50fe9495dcebc401609c1afb1f60cb090774b045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:33 +0000

    Add package django-fluent-contents/0.8.1/django-fluent-contents-0.8.1.tar.gz

---
+++ packages/django-fluent-contents/django-fluent-contents-0.8.1.tar.gz/django-fluent-contents-0.8.1/fluent_contents/cache.py
@@ -0,0 +1,18 @@
+"""
+Functions for caching.
+"""
+
+def get_rendering_cache_key(placeholder_name, contentitem):
+    """
+    Return a cache key for the content item output.
+
+    .. seealso::
+
+        The :func:`ContentItem.clear_cache() <fluent_contents.models.ContentItem.clear_cache>` function
+        can be used to remove the cache keys of a retrieved object.
+    """
+    return "contentitem-@{0}-{1}-{2}".format(
+        placeholder_name,
+        contentitem.plugin.type_name,  # always returns the upcasted name.
+        contentitem.id
+    )
