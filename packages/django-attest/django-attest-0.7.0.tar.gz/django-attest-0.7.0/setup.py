a00fc52d8a2207212715da5ffadf7f6c449c52e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:47 +0000

    Add package django-attest/0.7.0/django-attest-0.7.0.tar.gz

---
+++ packages/django-attest/django-attest-0.7.0.tar.gz/django-attest-0.7.0/setup.py
@@ -0,0 +1,31 @@
+# coding: utf8
+from setuptools import setup
+
+
+setup(
+    name='django-attest',
+    version='0.7.0',
+    description='Provides Django specific testing helpers to Attest',
+
+    author='Bradley Ayers',
+    author_email='bradley.ayers@gmail.com',
+    license='Simplified BSD',
+    url='https://github.com/bradleyayers/django-attest/',
+
+    packages=['django_attest'],
+    install_requires=['Django >=1.1', 'Attest >=0.5', 'distribute'],
+
+    test_loader='tests:loader',
+    test_suite='tests.suite',
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
