3d59ee1ab205332b43452b49e7f434805d3e758d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package csvquerytool/0.0.2/csvquerytool-0.0.2.tar.gz

---
+++ packages/csvquerytool/csvquerytool-0.0.2.tar.gz/csvquerytool-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='csvquerytool',
+    version='0.0.2',
+    author='Alister Cordiner',
+    author_email='alister@cordiner.net',
+    packages=['csvquerytool', 'csvquerytool.test'],
+    scripts=['bin/csvquery'],
+    url='http://bitbucket.org/acordiner/csvquerytool/',
+    license='http://www.gnu.org/licenses/gpl.html',
+    description='Execute SQL queries on CSV files.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
