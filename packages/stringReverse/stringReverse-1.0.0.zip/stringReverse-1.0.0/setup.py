02b068053e684e00a09b138ade02a4f08cfbffca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package stringReverse/1.0.0/stringReverse-1.0.0.zip

---
+++ packages/stringReverse/stringReverse-1.0.0.zip/stringReverse-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='stringReverse',
+      description='a project for outputting the reverse of the input string',
+      author='guzhen',
+      author_email='604608916@qq.com',
+      version='1.0.0'
+      )
