638a95fd26d416cc22c665337fcbb35c9dad5db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:17 +0000

    Add package easyimap/0.3.0/easyimap-0.3.0.tar.gz

---
+++ packages/easyimap/easyimap-0.3.0.tar.gz/easyimap-0.3.0/setup.py
@@ -0,0 +1,42 @@
+#This file is part easyimap.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+from setuptools import setup
+
+from sys import version
+if version < '2.6.0':
+    raise Exception("This module doesn't support any version less than 2.6")
+
+import sys
+sys.path.append("./test")
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+
+setup(
+    author='Keita Oouchi',
+    author_email='keita.oouchi@gmail.com',
+    url='https://github.com/keitaoouchi/easyimap',
+    name='easyimap',
+    version='0.3.0',
+    package_dir={"": "src"},
+    packages=['easyimap'],
+    test_suite="test_seleniumwrapper.suite",
+    license='BSD License',
+    classifiers=classifiers,
+    description='Simple imap wrapper.',
+    long_description=long_description,
+)
