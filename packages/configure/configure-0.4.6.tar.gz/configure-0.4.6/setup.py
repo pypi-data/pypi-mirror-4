0123d9d96486e1c22a52f6bb059eea9060fb885b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package configure/0.4.6/configure-0.4.6.tar.gz

---
+++ packages/configure/configure-0.4.6.tar.gz/configure-0.4.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = "0.4.6"
+
+setup(
+    name="configure",
+    version=version,
+    description="configuration toolkit based on YAML",
+    author="Andrey Popp",
+    author_email="8mayday@gmail.com",
+    py_modules=["configure"],
+    test_suite="tests",
+    install_requires=["pyyaml"],
+    zip_safe=False)
