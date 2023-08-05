1504b30d45801498c845e442e29cd881907f8d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package espresso/1.0.2/espresso-1.0.2.tar.gz

---
+++ packages/espresso/espresso-1.0.2.tar.gz/espresso-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='espresso',
+    description='Manage the configuration of your servers easily.',
+    version='1.0.2',
+    author='Jorge E. Cardona',
+    author_email='jorge@cardona.co',
+    packages=find_packages(),
+    license="BSD",
+    test_suite='tests',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        ],
+    entry_points={
+        'console_scripts': [
+            'setup-espresso = espresso.cli:setup',
+            'espresso = espresso.cli:main'
+            ],
+        },
+    install_requires=[
+        'distribute',
+        'sh',
+        'gevent'],
+    setup_requires=[
+        'distribute',
+        'mock']
+    )
