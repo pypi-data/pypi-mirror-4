da2897ce9e6abe597a139db85a6a28ed7ed21c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:05 +0000

    Add package infi.recipe.template.version/0.4.6/infi.recipe.template.version-0.4.6-py2.7.egg

---
+++ packages/infi.recipe.template.version/infi.recipe.template.version-0.4.6-py2.7.egg/infi/recipe/template/version/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a3789b808f5db7262aeb56e021f8af6705c7569b'
+__git_head_subject__ = 'Finished release v0.4.6.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
---
+++ packages/infi.recipe.template.version/infi.recipe.template.version-0.4.6-py2.7.egg/infi/recipe/template/version/tests/__init__.py
@@ -0,0 +1,111 @@
+__import__("pkg_resources").declare_namespace(__name__)
+
+import unittest
+from mock import patch
+from ..recipe import Recipe
+
+def run_cmd(cmd, return_stderr=False):
+    from subprocess import Popen, PIPE
+    import shlex
+    p = Popen(shlex.split(cmd, posix=True), stdout=PIPE, stderr=PIPE if return_stderr else None)
+    p.wait()
+    stdout = p.stdout.read()
+    if return_stderr:
+        stderr = p.stderr.read()
+        return (p.returncode, stdout, stderr)
+    return (p.returncode, stdout)
+
+def strip(string):
+    output = string.strip('\n').strip('"')
+    return output
+
+class VersionInfoTestCase(unittest.TestCase):
+    def setUp(self):
+        from os.path import abspath
+        from os import chdir, curdir
+        from tempfile import mkdtemp
+        self._curdir = abspath(curdir)
+        self._tempdir = mkdtemp()
+        chdir(self._tempdir)
+
+    def tearDown(self):
+        from os import chdir, curdir
+        chdir(self._curdir)
+
+    def test_vesion_tag_simple(self):
+        from os import system
+        system('git init .')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.1 -m tag')
+        version = Recipe.extract_version_tag()
+        self.assertEquals(version, 'v0.0.1')
+
+    def test_vesion_tag_longer(self):
+        from os import system
+        from gitpy import LocalRepository
+        system('git init .')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.1 -m tag')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        version = Recipe.extract_version_tag()
+        self.assertTrue('v0.0.1-3' in version)
+
+    def test_vesion_tag_in_feature_branch(self):
+        from os import system
+        system('git init .')
+        system('git flow init -fd')
+        system('git flow release start v0.0')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.0.alpha -m tag')
+        system('git flow release finish v0.0.0')
+        system('git flow feature start feature1')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0-feature1 -m tag')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        version = Recipe.extract_version_tag()
+        self.assertTrue('v0.0-feature1-4' in version)
+
+    def test_vesion_tag_in_release_branch(self):
+        from os import system
+        system('git init .')
+        system('git flow init -fd')
+        system('git flow release start v0.0')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.alpha -m tag')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        version = Recipe.extract_version_tag()
+        self.assertTrue('v0.0.alpha-2' in version)
+
+    def test_vesion_tag_in_no_branch(self):
+        from os import system
+        system('git init .')
+        system('git flow init -fd')
+        system('git flow release start v0.0')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.alpha -m tag')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        system('git checkout v0.0.alpha')
+        version = Recipe.extract_version_tag()
+        self.assertEquals(version, 'v0.0.alpha')
+
+    def test_verison_tag_with_non_version_tag_outside_of_branch(self):
+        from os import system
+        system('git init .')
+        system('git flow init -fd')
+        system('git flow release start v0.0')
+        system('git commit --allow-empty -m empty')
+        system('git tag -a v0.0.alpha -m tag')
+        system('git commit --allow-empty -m empty')
+        system('git commit --allow-empty -m empty')
+        system('git checkout HEAD^')
+        system('git tag -a foo -m foo')
+        version = Recipe.extract_version_tag()
+        self.assertTrue('v0.0.alpha' in version)
+
