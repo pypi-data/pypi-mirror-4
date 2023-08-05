a1af771d71383361d761015fc768521fdb5491e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:22 +0000

    Add package pyad/0.4.19/pyad-0.4.19.tar.gz

---
+++ packages/pyad/pyad-0.4.19.tar.gz/pyad-0.4.19/setup.py
@@ -0,0 +1,40 @@
+import os
+import os.path
+from setuptools import setup
+
+
+def read(fname):
+    if os.path.exists(fname):
+    	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    else:
+        return ''
+
+setup(
+    name = "pyad",
+    version = "0.4.19",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+    download_url = " https://github.com/zakird/pyad/tarball/master",
+    url = "https://github.com/zakird/pyad",
+    description = "An Object-Oriented Active Directory management framework built on ADSI",
+    license = "GNUv3",
+    keywords = "python microsoft windows active directory AD adsi",
+    packages=[
+        'pyad'
+    ],
+    long_description = read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: System Administrators",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP"
+    ],
+    install_requires=[
+        'setuptools',
+        'pywin32'
+    ]
+)
