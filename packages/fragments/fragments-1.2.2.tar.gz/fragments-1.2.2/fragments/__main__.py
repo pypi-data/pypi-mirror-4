2ab39c6f295433b61107d792f9d046613323ac41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package fragments/1.2.2/fragments-1.2.2.tar.gz

---
+++ packages/fragments/fragments-1.2.2.tar.gz/fragments-1.2.2/fragments/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals
+
+from fragments.commands import _main
+
+
+if __name__ == '__main__': # pragma: no cover
+    _main()
---
+++ packages/fragments/fragments-1.2.2.tar.gz/fragments-1.2.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install_requires = []
+if sys.version_info < (2, 7):
+    install_requires.append('argparse')
+
+import fragments
+
+setup(
+    name='fragments',
+    version='.'.join(map(str, fragments.__version__)),
+    description='tool for diff-based templating / fragmentation control',
+    long_description=open('README.md').read(),
+    author='Matt Chisholm',
+    author_email='matt-fragments@theory.org',
+    url='https://github.com/glyphobet/fragments',
+    packages= ['fragments',],
+    install_requires=install_requires,
+    entry_points = {
+        'console_scripts': [
+            'fragments = fragments.commands:_main',
+        ],
+    },
+    data_files=[('share/fragments', ['README.md', 'bash_completion', 'LICENSE.txt'])],
+    license='BSD License',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
