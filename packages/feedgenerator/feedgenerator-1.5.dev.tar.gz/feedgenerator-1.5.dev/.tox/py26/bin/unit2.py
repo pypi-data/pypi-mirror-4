b5b736e85d8e5ddf12c6ec1ac2f84f4e3298ca77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package feedgenerator/1.5.dev/feedgenerator-1.5.dev.tar.gz

---
+++ packages/feedgenerator/feedgenerator-1.5.dev.tar.gz/feedgenerator-1.5.dev/.tox/py26/bin/unit2.py
@@ -0,0 +1,7 @@
+#!/home/alexis/dev/github.com/feedgenerator-py3k/.tox/py26/bin/python2.6
+
+__unittest = True
+
+from unittest2.main import main_
+
+main_()
---
+++ packages/feedgenerator/feedgenerator-1.5.dev.tar.gz/feedgenerator-1.5.dev/.tox/py32/bin/unit2.py
@@ -0,0 +1,7 @@
+#!/home/alexis/dev/github.com/feedgenerator-py3k/.tox/py32/bin/python3.2mu
+
+__unittest = True
+
+from unittest2 import main_
+
+main_()
