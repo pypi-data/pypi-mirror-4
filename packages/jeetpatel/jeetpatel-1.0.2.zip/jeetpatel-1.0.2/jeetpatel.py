e8ecbf90f838d784526b227b04d309244c62ef52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package jeetpatel/1.0.2/jeetpatel-1.0.2.zip

---
+++ packages/jeetpatel/jeetpatel-1.0.2.zip/jeetpatel-1.0.2/jeetpatel.py
@@ -0,0 +1,16 @@
+'''
+Created on 10-Dec-2012
+
+@author: jeet
+'''
+def print_jeet_patel(a,b,c):
+    if(c==1):
+        print(a+b)
+    else:
+        if(a>b):
+            print(a-b)
+        else:
+            print(b-a)
+
+    
+    
