a83e091cfac29c48a2fbb203bf473a4cf48166ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:33 +0000

    Add package tdparser/1.1.1/tdparser-1.1.1.tar.gz

---
+++ packages/tdparser/tdparser-1.1.1.tar.gz/tdparser-1.1.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Python3
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+import os
+import re
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    path_components = package_components + ['__init__.py']
+    with open(os.path.join(root_dir, *path_components)) as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.1.0'
+
+
+PACKAGE = 'tdparser'
+
+
+setup(
+    name="tdparser",
+    version=get_version(PACKAGE),
+    author="Raphaël Barrois",
+    author_email="raphael.barrois+tdparser@polytechnique.org",
+    description=("A very simple parsing library, based on the Top-Down "
+        "algorithm."),
+    license="MIT",
+    keywords=['parser', 'lexer', 'token', 'topdown'],
+    url="http://github.com/rbarrois/tdparser",
+    download_url="http://pypi.python.org/pypi/tdparser/",
+    packages=find_packages(),
+    install_requires=[
+        'distribute',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='tests',
+)
+
