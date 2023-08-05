5b804036e7d9002bba6a9f5462f67ce65e5095b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package mapnik2/2.1.0.2/mapnik2-2.1.0.2.zip

---
+++ packages/mapnik2/mapnik2-2.1.0.2.zip/mapnik2-2.1.0.2/CHANGES.rst
@@ -0,0 +1,45 @@
+Changelog for mapnik2
+========================
+
+2.1.0.2 (2012-10-31)
+--------------------
+
+- fix for py26
+
+
+2.1.0.1 (2012-09-02)
+--------------------
+
+- fix release
+
+
+2.1.0 (2012-09-02)
+------------------
+
+- refresh 2.1.0
+
+
+2.0.1.3 (2012-08-05)
+--------------------
+
+- Nothing changed yet.
+
+
+2.0.1.1 (2012-08-05)
+--------------------
+
+- renaming release
+
+
+2.0.2 (2012-08-04)
+------------------
+
+- Multi Arch Support, thx to  noirbizarre. [kiorky]
+  See https://github.com/mapnik/pymapnik2/pull/4
+
+
+2.0.1 (2012-05-06)
+------------------
+First public release of mapnik2 eggified python bindings
+
+
---
+++ packages/mapnik2/mapnik2-2.1.0.2.zip/mapnik2-2.1.0.2/setup.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from setuptools import setup, find_packages, extension
+
+# we do not have the mapnik.utils module installed yet
+# just load it with exec
+mapnik_utils = {}
+exec open('src/mapnik/utils.py').read() in mapnik_utils
+read = mapnik_utils['read']
+get_compilation_flags = mapnik_utils['get_compilation_flags']
+summary = mapnik_utils['summary']
+
+README = read(('README.rst',))
+CHANGELOG = read(('CHANGES.rst',))
+long_description = '\n'.join([README, CHANGELOG])+'\n'
+
+sources_dir = os.path.abspath('cpp')
+agg_sources_dir = os.path.join(
+    os.path.abspath('agg'), 'include')
+files = [os.path.join('cpp', f)
+         for f in os.listdir(sources_dir)
+         if f.endswith('.cpp')]
+compilation_flags = get_compilation_flags()
+# BBB: needed for non standard pyairo.h to be found
+cf = ' '.join(compilation_flags['includes'])
+ldf = ' '.join(compilation_flags['extra_link_args'])
+os.environ['CFLAGS'] = cf
+os.environ['LDFLAGS'] = ldf
+
+# pycairo does not play well with setuptools
+# the user may install it himself
+#for lib in compilation_flags['extra_link_args']:
+#    if 'cairo' in lib:
+#        dep = 'pycairo'
+#        if sys.version_info[0] < 3:
+#            dep = 'py2cairo'
+#        install_requires.append(dep)
+#        break
+
+version = '2.1.0.2'
+if 'MAPNIK_DEBUG' in os.environ:
+    summary(compilation_flags)
+install_requires=['setuptools',]
+test_requires = ['nose']
+setup(
+    name='mapnik2',
+    version = version,
+    description="Python bindings for mapnik",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Mathieu Le Marec - Pasquet & the mapnik community',
+    author_email='kiorky@cryptelium.net',
+    url='http://pypi.python.org/pypi/mapnik',
+    license='LGPL',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    extras_require={'tests': test_requires},
+    ext_modules = [
+        extension.Extension(
+            "_mapnik", files,
+            include_dirs=[sources_dir, agg_sources_dir],
+            extra_compile_args = compilation_flags.get('includes', []),
+            libraries = compilation_flags.get('libraries', []),
+            extra_link_args = compilation_flags.get('extra_link_args', []),
+        ),
+    ],
+    install_requires=['setuptools'],
+    test_suite = 'nose.collector',
+    entry_points={},
+)
