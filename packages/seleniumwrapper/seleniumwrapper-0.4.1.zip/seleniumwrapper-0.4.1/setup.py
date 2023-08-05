51bc5de2c4d589b040d2104332efaf42c05547ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package seleniumwrapper/0.4.1/seleniumwrapper-0.4.1.zip

---
+++ packages/seleniumwrapper/seleniumwrapper-0.4.1.zip/seleniumwrapper-0.4.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+from sys import version
+if version < '2.6.0':
+    raise Exception("This module doesn't support any version less than 2.6")
+
+import sys
+sys.path.append("./test")
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+requires = ['selenium>=2.25.0']
+
+setup(
+    author='Keita Oouchi',
+    author_email='keita.oouchi@gmail.com',
+    url = 'https://github.com/keitaoouchi/seleniumwrapper',
+    name = 'seleniumwrapper',
+    version = '0.4.1',
+    package_dir={"":"src"},
+    packages = ['seleniumwrapper'],
+    test_suite = "test_seleniumwrapper.suite",
+    license='BSD License',
+    classifiers=classifiers,
+    description = 'selenium webdriver wrapper to make manipulation easier.',
+    long_description=long_description,
+    install_requires=requires,
+)
