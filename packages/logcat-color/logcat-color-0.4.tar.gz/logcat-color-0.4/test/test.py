ec01922cac8c9da20ffe210d62c1574a0f1bf36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package logcat-color/0.4/logcat-color-0.4.tar.gz

---
+++ packages/logcat-color/logcat-color-0.4.tar.gz/logcat-color-0.4/test/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+logcat_color_dir = os.path.dirname(this_dir)
+sys.path.append(logcat_color_dir)
+
+from format_test import *
+from config_test import *
+from logcat_color_test import *
+
+if __name__ == "__main__":
+    unittest.main()
