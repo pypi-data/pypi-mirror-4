c9b5e3a8138d269873c87ae068a0e50cdeee270b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:41 +0000

    Add package ifstat/1.0/ifstat-1.0.tar.gz

---
+++ packages/ifstat/ifstat-1.0.tar.gz/ifstat-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+version = '1.0'
+
+setup(name='ifstat',
+      version=version,
+      description="Display simple throughput statistics for network interface controllers. Requires Linux kernel >= 2.6",
+      long_description=open(os.sep.join([DIRNAME, 'README.md'])).read(),
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Michael Jenny',
+      author_email='michael.jenny@projekt-und-partner.de',
+      url='',
+      license='MIT license',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+        'console_scripts': ['ifstat = ifstat.main:main']  
+      },
+      )
