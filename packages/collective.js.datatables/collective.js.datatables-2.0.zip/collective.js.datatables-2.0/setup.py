1b98b3face9c375d90bcf8a528b59ef7ef2418cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:58 +0000

    Add package collective.js.datatables/2.0/collective.js.datatables-2.0.zip

---
+++ packages/collective.js.datatables/collective.js.datatables-2.0.zip/collective.js.datatables-2.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '2.0'
+
+setup(name='collective.js.datatables',
+      version=version,
+      description="Plone Integration of jquery.dataTables plugin",
+      long_description=read('README.rst') + read('docs', 'HISTORY.txt'),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='https://github.com/collective/collective.js.datatables',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require = dict(
+          test=['plone.app.testing'],
+      ),
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
