a98f77c545074e77c1b1bc7f758d515e48ded5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package Django-transactional/0.2/Django-transactional-0.2.tar.gz

---
+++ packages/Django-transactional/Django-transactional-0.2.tar.gz/Django-transactional-0.2/dtrans/urls.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+Url definitions.
+
+Copyright (C) 2012 Corp B2C S.A.C.
+
+Authors:
+    Nicolas Valcarcel Scerpella <nvalcarcel@corpb2c.com>
+
+"""
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('dtrans',
+    url(
+        r'^(?P<app_name>\w+)/(?P<model_name>\w+)/add/$',
+        'add_or_modify',
+        name='add'
+    ),
+    url(
+        r'^(?P<app_name>\w+)/(?P<model_name>\w+)/add/(?P<obj_id>\d+)/$',
+        'add_or_modify',
+        name='modify'
+    ),
+)
