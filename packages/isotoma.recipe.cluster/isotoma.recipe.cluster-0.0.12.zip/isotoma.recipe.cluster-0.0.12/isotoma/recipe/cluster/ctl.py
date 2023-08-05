1a4ae2c8c099f2f6bb9aa1143cd8c6357212d51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package isotoma.recipe.cluster/0.0.12/isotoma.recipe.cluster-0.0.12.zip

---
+++ packages/isotoma.recipe.cluster/isotoma.recipe.cluster-0.0.12.zip/isotoma.recipe.cluster-0.0.12/isotoma/recipe/cluster/ctl.py
@@ -0,0 +1,270 @@
+# Copyright 2010 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, sys, subprocess, shlex, time, pwd, grp, ConfigParser
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+
+class NothingToDo(Exception):
+    pass
+
+
+class ActionFailed(Exception):
+    pass
+
+
+class BaseService(object):
+
+    """ I am a service that has a pidfile and can report whether i am alive or not """
+
+    @property
+    def pid(self):
+        """ I read the pidfile and return it """
+        pidfile = self.pidfile
+        if not os.path.exists(pidfile):
+            return None
+
+        pid = open(pidfile).read().strip()
+        try:
+            pid = int(pid)
+        except:
+            return None
+
+        return pid
+
+    def alive(self):
+        """ I check if my pid is alive """
+        pid = self.pid
+
+        if not pid:
+            return False
+
+        # Try kill() with signal 0
+        # No exceptions means the pid is alive
+        #  Special case: if we dont have permissions, give up
+        try:
+            os.kill(pid, 0)
+            return True
+        except OSError, e:
+            if e.errno == 3:
+                if "No such process" in e.strerror:
+                    return False
+                raise SystemExit("We don't have permission to check the status of that pid")
+        return False
+
+
+class Service(BaseService):
+
+    """ I am a service that can be stopped and started and can report my status """
+
+    def __init__(self, bindir, varrundir, service, settings):
+        self.bindir = bindir
+        self.varrundir = varrundir
+        self.service = service
+        self.settings = settings
+
+    @property
+    def start_command(self):
+        if "start-command" in self.settings:
+            return self.settings["start-command"]
+        return "%s start" % os.path.join(self.bindir, self.service)
+
+    @property
+    def stop_command(self):
+        if "stop-command" in self.settings:
+            return self.settings["stop-command"]
+        return "%s stop" % os.path.join(self.bindir, self.service)
+
+    @property
+    def pidfile(self):
+        if "pidfile" in self.settings:
+            return self.settings["pidfile"]
+        return os.path.join(self.varrundir, "%s.pid" % self.service)
+
+    @property
+    def env(self):
+        if "env" in self.settings:
+            return self.settings["env"]
+        return None
+
+    @property
+    def user(self):
+        return self.settings.get("user", None)
+
+    def get_command(self, command, user):
+        cmd = []
+
+        if user and pwd.getpwnam(user).pw_uid != os.getuid():
+            cmd = ["sudo", "-u", user]
+
+        cmd.extend(shlex.split(command.encode("UTF-8")))
+        return cmd
+
+    def start(self):
+        """ I attempt to to start a service """
+
+        print "Attempting to start %s" % self.service
+
+        if self.alive():
+            raise NothingToDo("Service already running")
+            return 1
+
+        p = subprocess.Popen(self.get_command(self.start_command, self.user), env=self.env)
+        p.wait()
+
+        if p.returncode != 0:
+            raise ActionFailed("Start script reported error")
+
+        for i in range(600):
+            time.sleep(0.1)
+            if self.alive():
+                return
+
+        if not self.alive():
+            raise ActionFailed("Could not start service")
+
+
+    def stop(self):
+        """ I attempt to to stop a service """
+
+        print "Attempting to stop %s (pid=%s)" % (self.service, self.pid)
+
+        if not self.alive():
+            raise NothingToDo("Service is already stopped")
+
+        p = subprocess.Popen(self.get_command(self.stop_command, self.user), env=self.env)
+        p.wait()
+
+        if p.returncode != 0:
+            raise ActionFailed("Stop script reported error")
+
+        for i in range(600):
+            time.sleep(0.1)
+            if not self.alive():
+                return
+
+        if self.alive():
+            raise ActionFailed("Service wouldn't shut down")
+
+    def status(self):
+        pid = self.pid or "no pid"
+        if self.alive():
+            print "'%s' is alive (%s)." % (self.service, pid)
+            return True
+        else:
+            print "'%s' is not running." % self.service
+            return False
+
+
+class Services(object):
+
+    """ I am a collection of services that can be start, stopped, restarted and query for their status as a group """
+
+    def __init__(self, bindir, varrundir, services):
+        self.services = []
+        for service in services:
+            self.services.append(Service(bindir, varrundir, service["name"], service))
+
+    def start(self):
+        """ I start everything in the list of daemons """
+        for service in self.services:
+            try:
+                service.start()
+            except NothingToDo:
+                print "Skipped as already running"
+
+    def stop(self):
+        """ I reverse the list of daemons, then stop everything """
+        errorflag = False
+
+        services = self.services[:]
+        services.reverse()
+        for service in services:
+            try:
+                service.stop()
+            except NothingToDo:
+                # if service is already stopped, we just carry on
+                print "Skipped as already stopped"
+            except ActionFailed, e:
+                print e.args[0]
+                errorflag = True
+
+        if errorflag:
+            raise ActionFailed("Not all services shut down")
+
+    def restart(self):
+        """ I stop all of the daemons, then start them again """
+        self.stop()
+        self.start()
+
+    def status(self):
+        """ I iterate a dictionary of daemon information and get status info """
+        not_running = 0
+        for service in self.services:
+            if not service.status():
+                not_running += 1
+        return not_running
+
+
+def main(path):
+    if len(sys.argv) != 2:
+        return 1
+
+    config = ConfigParser.RawConfigParser()
+    config.read(path)
+
+    user = config.get('cluster', 'user')
+    owner = config.get('cluster', 'owner')
+    bindir = config.get('cluster', 'bindir')
+    varrundir = config.get('cluster', 'varrundir')
+
+    svcinf = [dict(config.items(s)) for s in config.get('cluster', 'services').strip().split(" ")]
+
+    if len(user.strip()) > 0 and user != pwd.getpwuid(os.getuid()).pw_name:
+        print >>sys.stderr, "Only '%s' is allowed to run this script" % user
+        return
+
+    if not os.path.exists(varrundir):
+        os.makedirs(varrundir)
+        os.chown(varrundir, pwd.getpwnam(owner).pw_uid, grp.getgrnam(owner).gr_gid)
+        os.chmod(varrundir, 0755)
+
+    services = Services(bindir, varrundir, svcinf)
+
+    try:
+        if sys.argv[1] == "start":
+            return services.start()
+        elif sys.argv[1] == "stop":
+            return services.stop()
+        elif sys.argv[1] == "restart":
+            return services.restart()
+        elif sys.argv[1] == "status":
+            return services.status()
+        elif sys.argv[1] == "running":
+            sys.exit(services.status())
+    except NothingToDo, e:
+        print >>sys.stderr, "Nothing To Do:", e.args[0]
+        sys.exit(0)
+    except ActionFailed, e:
+        print >>sys.stderr, "Action Failed:", e.args[0]
+        sys.exit(1)
+
+    print >>sys.stderr, "%s (start|stop|restart|status|running)" % name
+    sys.exit(0)
+
+
---
+++ packages/isotoma.recipe.cluster/isotoma.recipe.cluster-0.0.12.zip/isotoma.recipe.cluster-0.0.12/isotoma/recipe/cluster/tests.py
@@ -0,0 +1,159 @@
+"""Test setup for isotoma.recipe.apache.
+"""
+
+import os, sys, subprocess, re, time
+import pkg_resources
+
+import zc.buildout.testing
+
+import unittest
+import zope.testing
+from zope.testing import doctest, renormalizing
+
+from isotoma.recipe.cluster.ctl import BaseService, Service, Services, NothingToDo
+
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('isotoma.recipe.cluster', test)
+    zc.buildout.testing.install('simplejson', test)
+    zc.buildout.testing.install('zope.testing', test)
+
+
+checker = renormalizing.RENormalizing([
+    zc.buildout.testing.normalize_path,
+    (re.compile('#![^\n]+\n'), ''),
+    (re.compile('-\S+-py\d[.]\d(-\S+)?.egg'),
+     '-pyN.N.egg',
+    ),
+    ])
+
+def sibpath(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+class TestCtl(unittest.TestCase):
+
+    def raw_test_service(self, pid, command):
+        env = os.environ.copy()
+        env["PYTHONPATH"] = ":".join(sys.path)
+        p = subprocess.Popen([sys.executable, sibpath("testservice.py"), os.path.realpath(pid), command], env=env)
+        p.wait()
+        time.sleep(1) # evil
+
+    def service(self, pid):
+        s = Service("", "", "cluster", {
+            "name": pid,
+            "pidfile": os.path.realpath(pid),
+            "start-command": " ".join((sys.executable, sibpath("testservice.py"), os.path.realpath(pid), "start")),
+            "stop-command": " ".join((sys.executable, sibpath("testservice.py"), os.path.realpath(pid), "stop")),
+            "env": {"PYTHONPATH": ":".join(sys.path)},
+            })
+        return s
+
+    def services(self, *pids):
+        services = []
+        for pid in pids:
+            service = {
+                "name": pid,
+                "pidfile": os.path.realpath(pid),
+                "start-command": " ".join((sys.executable, sibpath("testservice.py"), os.path.realpath(pid), "start")),
+                "stop-command": " ".join((sys.executable, sibpath("testservice.py"), os.path.realpath(pid), "stop")),
+                "env": {"PYTHONPATH": ":".join(sys.path)},
+                }
+            services.append(service)
+
+        return Services("", "", services)
+
+    def raw_start_service(self, pid):
+        self.raw_test_service(pid, "start")
+
+    def raw_stop_service(self, pid):
+        self.raw_test_service(pid, "stop")
+
+    def status_service(self, pid):
+        c = BaseService()
+        c.pidfile = os.path.realpath(pid)
+        return c.alive()
+
+    def test_testservice(self):
+        self.failUnless(not self.status_service("a.pid"), "Test environment wasnt clean")
+        self.raw_start_service("a.pid")
+        self.failUnless(self.status_service("a.pid"), "Couldnt start testservice")
+        self.raw_stop_service("a.pid")
+        self.failUnless(not self.status_service("a.pid"), "Couldnt stop testservice")
+
+    def test_service_start(self):
+        s = self.service("a.pid")
+        s.start()
+        self.failUnless(self.status_service("a.pid"))
+        self.raw_stop_service("a.pid")
+
+    def test_service_start_twice(self):
+        s = self.service("a.pid")
+        s.start()
+        self.failUnlessRaises(NothingToDo, s.start)
+        self.raw_stop_service("a.pid")
+
+    def test_service_alive(self):
+        s = self.service("a.pid")
+        self.failUnless(not s.alive())
+        s.start()
+        self.failUnless(s.alive())
+        self.raw_stop_service("a.pid")
+
+    def test_service_stop(self):
+        s = self.service("a.pid")
+        self.failUnless(not s.alive())
+        s.start()
+        self.failUnless(s.alive())
+        s.stop()
+        self.failUnless(not s.alive())
+
+    def test_service_stop_when_not_running(self):
+        s = self.service("a.pid")
+        self.failUnlessRaises(NothingToDo, s.stop)
+
+    def test_services_start(self):
+        s = self.services("a.pid", "b.pid")
+        self.failUnless(not self.status_service("a.pid") and not self.status_service("b.pid"))
+        s.start()
+        self.failUnless(self.status_service("a.pid") and self.status_service("b.pid"))
+        self.raw_stop_service("a.pid")
+        self.raw_stop_service("b.pid")
+        self.failUnless(not self.status_service("a.pid") and not self.status_service("b.pid"))
+
+    def test_services_stop(self):
+        s = self.services("a.pid", "b.pid")
+        self.failUnless(not self.status_service("a.pid") and not self.status_service("b.pid"))
+        s.start()
+        self.failUnless(self.status_service("a.pid") and self.status_service("b.pid"))
+        s.stop()
+        self.failUnless(not self.status_service("a.pid") and not self.status_service("b.pid"))
+
+    def test_services_status(self):
+        s = self.services("a.pid", "b.pid")
+        self.assertEqual(s.status(), 2)
+        s.start()
+        self.assertEqual(s.status(), 0)
+        s.stop()
+        self.assertEqual(s.status(), 2)
+
+
+def test_suite():
+    tests = [
+        "doctests/simple-usage.txt",
+        ]
+
+    suites = []
+    for test in tests:
+        suites.append(doctest.DocFileSuite(test,
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.ELLIPSIS, checker=checker))
+
+    suites.append(unittest.TestLoader().loadTestsFromTestCase(TestCtl))
+
+    return unittest.TestSuite(suites)
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
