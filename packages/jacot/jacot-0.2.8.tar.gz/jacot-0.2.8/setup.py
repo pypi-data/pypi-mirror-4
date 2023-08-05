7421612ac99ec3532e19529f048ec40b4ff9be7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package jacot/0.2.8/jacot-0.2.8.tar.gz

---
+++ packages/jacot/jacot-0.2.8.tar.gz/jacot-0.2.8/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from jacot import __version__, __license__, __author__, __doc__
+
+setup(name                  = 'jacot',
+      version               = __version__,
+      description           = "Japanese Auto character set Conversion On Tty",
+      long_description      = open("README.rst").read(),
+      py_modules            = ['jacot'],
+      eager_resources       = [],
+      classifiers           = ['Development Status :: 4 - Beta',
+                               'Topic :: Terminals',
+                               'Environment :: Console',
+                               'Intended Audience :: End Users/Desktop',
+                               'License :: OSI Approved :: GNU General Public License (GPL)',
+                               'Programming Language :: Python'
+                               ],
+      keywords              = 'japanese terminal',
+      author                = __author__,
+      author_email          = 'user@zuse.jp',
+      url                   = 'https://github.com/saitoha/jacot',
+      license               = __license__,
+      packages              = find_packages(exclude=[]),
+      zip_safe              = True,
+      include_package_data  = False,
+      install_requires      = ['tff ==0.0.1', 'sentimental-skk ==0.0.2'],
+      entry_points          = """
+                              [console_scripts]
+                              jacot = jacot:main
+                              """
+      )
+
