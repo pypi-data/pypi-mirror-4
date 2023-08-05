7272f5aba3500fc0545f11f8747bcc3b060ed6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package SeedWaterSegmenter/0.5.4.6/SeedWaterSegmenter-0.5.4.6.tar.gz

---
+++ packages/SeedWaterSegmenter/SeedWaterSegmenter-0.5.4.6.tar.gz/SeedWaterSegmenter-0.5.4.6/SeedWaterSegmenter/__init__.py
@@ -0,0 +1,5 @@
+# This should import everything relevant:
+from SeedWaterSegmenter import *
+import SWHelpers
+
+__all__ = ['SeedWaterSegmenter','SWHelpers']
---
+++ packages/SeedWaterSegmenter/SeedWaterSegmenter-0.5.4.6.tar.gz/SeedWaterSegmenter-0.5.4.6/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    name='SeedWaterSegmenter',
+    version='0.5.4.6',
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['SeedWaterSegmenter'],
+    package_dir={'SeedWaterSegmenter': 'SeedWaterSegmenter'},
+    package_data={'SeedWaterSegmenter': ['icons/*']},   
+    scripts = ['postinstall.py'],
+    url='http://pypi.python.org/pypi/SeedWaterSegmenter/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.rst').read(),
+    install_requires=[
+# Adding install requirements may make this quirk out on a new system
+# If it does, I'll have to patch it!
+                      #'wxPython>=2.8', # wxPython isn't being found correctly by setuptools -- please install it manually!
+                      'numpy>=1.0',
+                      'scipy>=0.8',
+                      'matplotlib>=1.0',
+                      'PIL>=1.1.5',
+                      'xlrd>=0.7',
+                      'xlwt>=0.7',
+                      'mahotas>=0.5',
+#                      # Projects that used to be internal
+                      'cmpGen>=0.1',
+                      'EllipseFitter>=0.1',
+                      'FilenameSort>=0.1.3',
+                      'GifTiffLoader>=0.1.4',
+                      'ImageContour>=0.1',
+                      'np_utils>=0.2.1',
+                      ],
+)
