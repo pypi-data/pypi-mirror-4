e057a7eb87df7b8c61765fafc992714379225510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:41 +0000

    Add package django-shop/0.1.0/django-shop-0.1.0.tar.gz

---
+++ packages/django-shop/django-shop-0.1.0.tar.gz/django-shop-0.1.0/AUTHORS.rst
@@ -0,0 +1,39 @@
+CORE DEVELOPERS
+===============
+
+* Chris Glass (chrisglass)
+* Martin Brochhaus
+
+CONTRIBUTORS
+============
+
+* Adrien Lemaire
+* airtonix
+* Aleš Kocjančič
+* Audrey Roy
+* Ben Lopatin
+* Benjamin Wohlwend
+* Bojan Mihelac
+* Chris Morgan
+* fivethreeo
+* Issac Kelly
+* Jacob Rief
+* Jonas Obrist
+* Justin Steward 
+* Kristian Øllegaard
+* Martin Ogden
+* Mike Yumatov
+* Pavel Zhukov
+* Per Rosengren
+* Raúl Cumplido
+* Rolo Mawlabaux
+* Simon Luijk
+* Stephen Muss
+* Thomas Woolford
+
+TRANSLATORS
+===========
+
+* Mikhail Kolesnik (Russian)
+* Pavel Zhukov (Russian)
+* Pedro Gracia (Spanish)
---
+++ packages/django-shop/django-shop-0.1.0.tar.gz/django-shop-0.1.0/shop/urls/checkout.py
@@ -0,0 +1,32 @@
+from django.conf.urls.defaults import url, patterns
+from shop.util.decorators import cart_required
+
+from shop.views.checkout import (
+    # SelectPaymentView,
+    # SelectShippingView,
+    CheckoutSelectionView,
+    PaymentBackendRedirectView,
+    ShippingBackendRedirectView,
+    ThankYouView,
+)
+
+urlpatterns = patterns('',
+    url(r'^$', cart_required(CheckoutSelectionView.as_view()),
+        name='checkout_selection'  # First step of the checkout process
+        ),
+    #url(r'^checkout/ship/$', SelectShippingView.as_view(),
+    #    name='checkout_shipping'  # First step of the checkout process
+    #    ),
+    url(r'^ship/$', ShippingBackendRedirectView.as_view(),
+        name='checkout_shipping'  # First step of the checkout process
+        ),
+    #url(r'^checkout/pay/$', SelectPaymentView.as_view(),
+    #    name='checkout_payment'  # Second step of the checkout process
+    #    ),
+    url(r'^pay/$', PaymentBackendRedirectView.as_view(),
+        name='checkout_payment'  # First step of the checkout process
+        ),
+    url(r'^thank_you/$', ThankYouView.as_view(),
+        name='thank_you_for_your_order'  # Second step of the checkout process
+        ),
+    )
