f0e5b8738b2bda2e49801cc01ebf7a801db560c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:12 +0000

    Add package foorep/0.1.4-1/foorep-0.1.4-1.tar.gz

---
+++ packages/foorep/foorep-0.1.4-1.tar.gz/foorep-0.1.4-1/foorep/plugins/exif.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2012 Johan Berggren.
+# This file is part of foorep
+# See the file 'LICENSE.txt' for copying permission.
+
+from foorep import Plugin
+
+class Exif(Plugin):
+    def analyze(self, path):
+        try:
+            import pyexiv2
+        except ImportError:
+            return None
+        try:
+            exif = pyexiv2.ImageMetadata(path)
+            exif.read()
+        except IOError:
+            return None
+        except AttributeError:
+            return None
+        self.tags = exif.exif_keys
+        result = {
+            "type": "exif",
+            "data": {},
+        }
+        if self.tags:
+            for tag in exif.exif_keys:
+                result['data'][tag.replace(".", "_")] = exif[tag].human_value
+        return result 
