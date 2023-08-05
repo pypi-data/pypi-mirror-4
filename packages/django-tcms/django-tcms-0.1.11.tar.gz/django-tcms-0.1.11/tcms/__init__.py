da840f39c4a8ee9d9ff8d486ca7eb452503e7b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package django-tcms/0.1.11/django-tcms-0.1.11.tar.gz

---
+++ packages/django-tcms/django-tcms-0.1.11.tar.gz/django-tcms-0.1.11/tcms/__init__.py
@@ -0,0 +1,2 @@
+version = (0, 1, 11)
+__version__ = '.'.join(map(str, version))
---
+++ packages/django-tcms/django-tcms-0.1.11.tar.gz/django-tcms-0.1.11/tcms/static/js/ckeditor_config.js
@@ -0,0 +1,11 @@
+﻿CKEDITOR.editorConfig = function(config) {
+	config.toolbar_tCMS = [
+		['Source'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord'],
+		['Undo','Redo','-','Find','Replace','-','Link','Unlink','Anchor'],
+		['Outdent','Indent','Blockquote','CreateDiv'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Bold','Italic','Underline','Strike'],
+		['Styles','Format','TextColor','FontSize','-','RemoveFormat']
+	];
+};
