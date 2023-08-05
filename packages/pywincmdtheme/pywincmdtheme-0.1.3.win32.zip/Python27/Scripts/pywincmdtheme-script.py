473111b441bc4420e0aecabb35ac6b493106d30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:16 +0000

    Add package pywincmdtheme/0.1.3/pywincmdtheme-0.1.3.win32.zip

---
+++ packages/pywincmdtheme/pywincmdtheme-0.1.3.win32.zip/Python27/Scripts/pywincmdtheme-script.py
@@ -0,0 +1,10 @@
+#!C:\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pywincmdtheme==0.1.3','console_scripts','pywincmdtheme'
+__requires__ = 'pywincmdtheme==0.1.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pywincmdtheme==0.1.3', 'console_scripts', 'pywincmdtheme')()
+    )
