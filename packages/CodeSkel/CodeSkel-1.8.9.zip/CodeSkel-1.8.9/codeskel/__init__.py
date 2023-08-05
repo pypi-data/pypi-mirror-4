45361fdbf0619ff928125dddaf64ded62b4ab2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package CodeSkel/1.8.9/CodeSkel-1.8.9.zip

---
+++ packages/CodeSkel/CodeSkel-1.8.9.zip/CodeSkel-1.8.9/codeskel/__init__.py
@@ -0,0 +1,6 @@
+#
+from cs_plone4_buildout import Plone4Buildout
+from cs_plone3_theme import Plone3Theme
+from dexterity_cs import CSDexterity
+from localcommands.dexterity import DexterityContentCS
+
