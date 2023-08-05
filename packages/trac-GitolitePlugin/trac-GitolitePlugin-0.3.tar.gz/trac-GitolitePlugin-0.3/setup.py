a9172ae0fa25f91a0712020177af9b7b3bb25981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:05 +0000

    Add package trac-GitolitePlugin/0.3/trac-GitolitePlugin-0.3.tar.gz

---
+++ packages/trac-GitolitePlugin/trac-GitolitePlugin-0.3.tar.gz/trac-GitolitePlugin-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+try:
+    long_description = open("README.rst").read()
+except:
+    long_description = ''
+try:
+    long_description += open("CHANGES.txt").read()
+except:
+    pass
+
+setup(name='trac-GitolitePlugin',
+      version='0.3',
+      description="",
+      long_description=long_description,
+      packages=['trac_gitolite'],
+      package_data={'trac_gitolite': ['templates/*.html']},
+      author='Ethan Jucovy',
+      author_email='ejucovy@gmail.com',
+      url="http://trac-hacks.org/wiki/GitolitePlugin",
+      license='BSD',
+      entry_points = {'trac.plugins': ['trac_gitolite = trac_gitolite']})
---
+++ packages/trac-GitolitePlugin/trac-GitolitePlugin-0.3.tar.gz/trac-GitolitePlugin-0.3/trac_gitolite/__init__.py
@@ -0,0 +1,4 @@
+from .perm_manager import GitolitePermissionManager
+from .perm_policy import GitolitePermissionPolicy
+from .repo_manager import GitoliteRepositoryManager
+from .user_manager import GitoliteUserManager
