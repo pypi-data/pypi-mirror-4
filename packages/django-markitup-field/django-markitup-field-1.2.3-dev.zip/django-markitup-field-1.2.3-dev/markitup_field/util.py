79fe32e9e2654df720e117d7f4bf3a5ecc539a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package django-markitup-field/1.2.3-dev/django-markitup-field-1.2.3-dev.zip

---
+++ packages/django-markitup-field/django-markitup-field-1.2.3-dev.zip/django-markitup-field-1.2.3-dev/markitup_field/util.py
@@ -0,0 +1,9 @@
+import posixpath
+from django.conf import settings
+
+def absolute_url(path, prefix=None):
+    if prefix is None:
+        prefix = settings.STATIC_URL
+    if path.startswith(u'http://') or path.startswith(u'https://') or path.startswith(u'/'):
+        return path
+    return posixpath.join(prefix, path)
