1ed3514ed768a9a4386f67d1270a5b01c33cb79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package dynconf/0.1/dynconf-0.1.tar.gz

---
+++ packages/dynconf/dynconf-0.1.tar.gz/dynconf-0.1/dynconf/cli.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import sys
+
+def main():
+    " Main command to dynconf cli"
+
+    # Read args.
+    parser = argparse.ArgumentParser(description="Dynamic configuration.")
+
+    # Filename to replace.
+    parser.add_argument('--filename', help="Filename to replace")
+    parser.add_argument('--keep-file', help="Don't delete the original file.",
+                        default=False, action='store_true')
+
+    # Parse the arguments.
+    args = parser.parse_args()
+
+    if not args.keep_file:
+
+        # Remove file.
+        os.unlink(args.filename)
+
+        # Create fifo.
+        os.mkfifo(args.filename)
+
+    # Read content.
+    content = sys.stdin.read()
+
+    # Open to read.
+    fd = os.open(args.filename, os.O_WRONLY)
+
+    # Pass stdin to filename.
+    os.write(fd, content)
+
+    # Close the fifo.
+    os.close(fd)
