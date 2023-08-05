1129c8eeef090477954ec5a960bda850cec8bc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package objpool/0.1.1/objpool-0.1.1.tar.gz

---
+++ packages/objpool/objpool-0.1.1.tar.gz/objpool-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='objpool',
+    version='0.1.1',
+    author='GRNet Development Team',
+    author_email='synnefo@lists.grnet.gr',
+    packages=['objpool'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/objpool',
+    license='LICENSE.txt',
+    description='An object pooling library',
+    long_description=open('README.txt').read(),
+)
