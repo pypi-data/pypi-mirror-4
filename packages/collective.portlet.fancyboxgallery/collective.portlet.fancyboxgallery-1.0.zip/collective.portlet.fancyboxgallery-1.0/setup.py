abe59f01a729eec3a1507481314a6b8ff1d63a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:04 +0000

    Add package collective.portlet.fancyboxgallery/1.0/collective.portlet.fancyboxgallery-1.0.zip

---
+++ packages/collective.portlet.fancyboxgallery/collective.portlet.fancyboxgallery-1.0.zip/collective.portlet.fancyboxgallery-1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.portlet.fancyboxgallery',
+      version=version,
+      description="Gallery portlet for Plone",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        ],
+      keywords='plone gallery fancybox',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/toutpt/collective.portlet.fancyboxgallery',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.gallery',
+          'collective.js.fancybox',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
