781b741932954f5f810261f5917e1ddb96fc6f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package HiveFDW/0.0.2/HiveFDW-0.0.2.tar.gz

---
+++ packages/HiveFDW/HiveFDW-0.0.2.tar.gz/HiveFDW-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='HiveFDW',
+      version='0.0.2',
+      description='Hive FDW for PostgreSQL',
+      author='Youngwoo Kim',
+      author_email='warwithin@gmail.com',
+      url='https://github.com/youngwookim/hive-fdw-for-postgresql',
+      py_modules=['hivefdw'])
