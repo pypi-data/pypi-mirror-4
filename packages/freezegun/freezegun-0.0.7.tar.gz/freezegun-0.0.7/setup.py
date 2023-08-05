269a08d929e69c8d93afba79e121f5e9e3250804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:45 +0000

    Add package freezegun/0.0.7/freezegun-0.0.7.tar.gz

---
+++ packages/freezegun/freezegun-0.0.7.tar.gz/freezegun-0.0.7/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='freezegun',
+    version='0.0.7',
+    description='Let your Python tests travel through time',
+    author='Steve Pulec',
+    author_email='spulec@gmail',
+    url='https://github.com/spulec/freezegun',
+    packages=find_packages(),
+    install_requires=['python-dateutil>=1.0, <2.0'],
+    include_package_data=True,
+)
