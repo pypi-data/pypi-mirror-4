d264b9ca20571acfab66cbf30346b12e50227e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:33 +0000

    Add package srcdspy/1.1/srcdspy-1.1-py2.7.egg

---
+++ packages/srcdspy/srcdspy-1.1-py2.7.egg/srcdspy/__init__.py
@@ -0,0 +1,11 @@
+# srcdspy
+# Half-Life 2 and Half-Life Dedicated Server Interface for Python
+#
+# Mark McGuire
+# Based off (most code copied from) SRCDS.py Christopher Munn
+#
+
+from query import SourceQuery
+from rcon import SourceRcon
+
+__all__ = ['SourceQuery', 'SourceRcon']
