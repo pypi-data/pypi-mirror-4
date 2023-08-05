4bf30f8b3195c88b121f31ceb82b5dd091fb5218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:26 +0000

    Add package pytest-django/2.1.0/pytest-django-2.1.0.tar.gz

---
+++ packages/pytest-django/pytest-django-2.1.0.tar.gz/pytest-django-2.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pytest-django',
+    version=__import__('pytest_django').__version__,
+    description='A Django plugin for py.test.',
+    author='Andreas Pelme',
+    author_email='andreas@pelme.se',
+    maintainer="Andreas Pelme",
+    maintainer_email="andreas@pelme.se",
+    url='http://pytest-django.readthedocs.org/',
+    packages=['pytest_django'],
+    long_description=read('README.rst'),
+    install_requires=['pytest>=2.3.4', 'django>=1.3'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Testing'],
+    # the following makes a plugin available to py.test
+    entry_points={'pytest11': ['django = pytest_django.plugin']})
