cf719df52560620c699a560dcce14d5872ca0d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:32 +0000

    Add package nl_gist/1.0.0/nl_gist-1.0.0.tar.gz

---
+++ packages/nl_gist/nl_gist-1.0.0.tar.gz/nl_gist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup	(
+	name		= 'nl_gist',
+	version		= '1.0.0',
+	py_modules	= ['nl_gist'],
+	author		= 'nikunjlahoti',
+	author_email	= 'me@nikunjlahoti.com',
+	url		= 'http://nikunjlahoti.com',
+	description	= 'random beginners Python stuff',
+	)
