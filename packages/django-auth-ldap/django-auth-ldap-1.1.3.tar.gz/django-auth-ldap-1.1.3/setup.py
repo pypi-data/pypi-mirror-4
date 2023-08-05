0ef6ff392894d60103c0cf6d308d04d19c690fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package django-auth-ldap/1.1.3/django-auth-ldap-1.1.3.tar.gz

---
+++ packages/django-auth-ldap/django-auth-ldap-1.1.3.tar.gz/django-auth-ldap-1.1.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="django-auth-ldap",
+    version="1.1.3",
+    description="Django LDAP authentication backend",
+    long_description=open('README').read(),
+    url="http://bitbucket.org/psagers/django-auth-ldap/",
+    author="Peter Sagerson",
+    author_email="psagers.pypi@ignorare.net",
+    license="BSD",
+    packages=["django_auth_ldap"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["django", "ldap", "authentication", "auth"],
+    install_requires=[
+        "django >= 1.3",
+        "python-ldap >= 2.0",
+    ]
+)
