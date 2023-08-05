eaa1912cb45beae37c61b6687648ebf98fb3ca31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package infi.nose_html_output/0.1.9/infi.nose_html_output-0.1.9-py2.7.egg

---
+++ packages/infi.nose_html_output/infi.nose_html_output-0.1.9-py2.7.egg/infi/nose_html_output/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '38f978e48826f968c3c95838cb75c0e9204ebb59'
+__git_head_subject__ = 'Finished release v0.1.9.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
