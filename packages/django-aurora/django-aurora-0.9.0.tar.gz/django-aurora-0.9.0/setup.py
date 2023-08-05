4dbddc4759b935c830e255f5839511a19100feb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package django-aurora/0.9.0/django-aurora-0.9.0.tar.gz

---
+++ packages/django-aurora/django-aurora-0.9.0.tar.gz/django-aurora-0.9.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python
+
+from setuptools import setup
+
+setup(
+    name = 'django-aurora',
+    version = '0.9.0',
+    url = 'http://aurora.io',
+    license = 'BSD',
+    description = 'Django decorators and some other utilities.',
+    author = 'Dan Loewenherz',
+    author_email = 'dan@aurora.io',
+    packages=['aurora'],
+)
+
