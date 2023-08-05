cb993ed663680093c6570f5d3787f87c700540f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package django-filepicker/0.1.2/django-filepicker-0.1.2.tar.gz

---
+++ packages/django-filepicker/django-filepicker-0.1.2.tar.gz/django-filepicker-0.1.2/django_filepicker/widgets.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.forms import widgets
+
+#JS_URL is the url to the filepicker.io javascript library
+JS_VERSION = 0
+JS_URL = "//api.filepicker.io/v%d/filepicker.js" % (JS_VERSION)
+
+if hasattr(settings, 'FILEPICKER_INPUT_TYPE'):
+    INPUT_TYPE = settings.FILEPICKER_INPUT_TYPE
+else:
+    INPUT_TYPE = 'filepicker-dragdrop'
+
+
+class FPFileWidget(widgets.Input):
+    input_type = INPUT_TYPE
+    needs_multipart_form = False
+
+    def value_from_datadict_old(self, data, files, name):
+        #If we are using the middleware, then the data will already be
+        #in FILES, if not it will be in POST
+        if name not in data:
+            return super(FPFileWidget, self).value_from_datadict(
+                    data, files, name)
+
+        return data
+
+    class Media:
+        js = (JS_URL,)
