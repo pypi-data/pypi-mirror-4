0b226fa3bf810176b8d65b10a0ac30f0f5e533a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package Vanda/2.1.9/Vanda-2.1.9.tar.gz

---
+++ packages/Vanda/Vanda-2.1.9.tar.gz/Vanda-2.1.9/vanda/apps/contactus/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render_to_response as rr
+from django.utils.translation import ugettext as _
+from django.core.mail import send_mail
+from django.template import RequestContext
+from django.conf import settings
+
+from forms import ContactUs
+
+
+def index(request):
+    if request.method == 'POST':
+        form = ContactUs(request.POST)
+        if form.is_valid():
+            send_mail('message from %s' % form.cleaned_data['email'],
+                      form.cleaned_data['message'],
+                      settings.EMAIL_HOST_USER,
+                      [i[1] for i in settings.ADMINS], fail_silently=False)
+            SentMessage = _('Your message has been sent successfully!')
+            return rr('msg.html', {'msg': SentMessage})
+        else:
+            return rr('contact_us.html', {'ContactUs': form},
+                      context_instance=RequestContext(request))
+    else:
+        form = ContactUs()
+        return rr('contact_us.html', {'ContactUs': form},
+                  context_instance=RequestContext(request))
