842c2953adad89c4b1bc3940515ee518adbfbc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package libais/0.12/libais-0.12.tar.bz2

---
+++ packages/libais/libais-0.12.tar.bz2/libais-0.12/ais10.cpp
@@ -0,0 +1,30 @@
+// : UTC and date query
+
+#include "ais.h"
+
+Ais10::Ais10(const char *nmea_payload, const size_t pad) {
+    assert(nmea_payload);
+    assert(pad < 6);
+    init();
+
+    if (pad != 0 || strlen(nmea_payload) != 12) {
+      status = AIS_ERR_BAD_BIT_COUNT;
+      return;
+    }
+
+    bitset<72> bs;
+    status = aivdm_to_bits(bs, nmea_payload);
+    if (had_error()) return;
+
+    message_id = ubits(bs, 0, 6);
+    if (message_id != 10) {
+      status = AIS_ERR_WRONG_MSG_TYPE;
+      return;
+    }
+    repeat_indicator = ubits(bs, 6, 2);
+    mmsi = ubits(bs, 8, 30);
+
+    spare = ubits(bs, 38, 2);
+    dest_mmsi = ubits(bs, 40, 30);
+    spare2 = ubits(bs, 70, 2);
+}
