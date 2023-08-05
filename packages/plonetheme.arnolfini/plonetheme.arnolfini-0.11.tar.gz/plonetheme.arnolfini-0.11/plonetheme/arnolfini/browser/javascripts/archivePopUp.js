0d923aed5bb38fe0c41920ed136b0b19a3ec592d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package plonetheme.arnolfini/0.11/plonetheme.arnolfini-0.11.tar.gz

---
+++ packages/plonetheme.arnolfini/plonetheme.arnolfini-0.11.tar.gz/plonetheme.arnolfini-0.11/plonetheme/arnolfini/browser/javascripts/archivePopUp.js
@@ -0,0 +1,40 @@
+Arnolfini.archivePopUp = {}
+
+Arnolfini.archivePopUp.activateLinks = function () {
+    $('.portletItem .yearItem.undocYear a').click(function(event)
+                                                                                    {
+                                                                                        href = $(event.target).attr('href');
+                                                                                        Arnolfini.archivePopUp.showMessage(href);
+                                                                                        return false;
+                                                                                    });
+}
+
+Arnolfini.archivePopUp.showMessage = function (href)
+{
+    var title = $('<h2 class="archivePopupTitle">'+href.substr(-4)+'</h2>')
+    var message = "This year only contains undocumented events. It might be that we have no documentation about the events described or we are still in the process of digitalizing the content. Are you sure you want to continue to this year?";
+    var popupDiv = $('<div class="archivepopup"></div>');
+    var messageDiv = $('<div class="message"></div>');
+    var cancelButton = $('<div class="cancelButton">Cancel</div>')
+    var continueButton = $('<div class="continueButton">Continue</div>');
+    
+    $(cancelButton).click(Arnolfini.archivePopUp.closeMessage);
+    $(continueButton).click(function () {window.location = href})
+    
+    $(messageDiv).text(message);
+    $(popupDiv).append(title);
+    $(popupDiv).append(messageDiv);
+    $(popupDiv).append(cancelButton);
+    $(popupDiv).append(continueButton);
+    $('body').append(popupDiv);
+}
+
+Arnolfini.archivePopUp.closeMessage = function ()
+{
+    $('.archivepopup').remove();
+}
+
+$(function ()
+{
+    Arnolfini.archivePopUp.activateLinks(); 
+});
