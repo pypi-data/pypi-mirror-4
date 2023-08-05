b5f71f7238eb8264386cf5837ceabc7ee8ffdcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package nsnitro/1.0.9/nsnitro-1.0.9.tar.gz

---
+++ packages/nsnitro/nsnitro-1.0.9.tar.gz/nsnitro-1.0.9/nsnitro/nsresources/__init__.py
@@ -0,0 +1,40 @@
+from nsbaseresource import NSBaseResource
+from nsconfig import NSConfig
+from nscspolicy import NSCSPolicy
+from nscsvserver import NSCSVServer
+from nscsvservercspolicybinding import NSCSVServerCSPolicyBinding
+from nscsvserverresponderpolicybinding import NSCSVServerResponderPolicyBinding
+from nscsvserverrewritepolicybinding import NSCSVServerRewritePolicyBinding
+from nslbvserver import NSLBVServer
+from nslbvserverservicebinding import NSLBVServerServiceBinding
+from nsresponderaction import NSResponderAction
+from nsresponderpolicy import NSResponderPolicy
+from nsresponderpolicylabel import NSResponderPolicyLabel
+from nsresponderpolicylabelbinding import NSResponderPolicyLabelBinding
+from nsresponderpolicycsvserverbinding import NSResponderPolicyCSVServerBinding
+from nsrewritepolicy import NSRewritePolicy
+from nsrewritepolicycsvserverbinding import NSRewritePolicyCSVServerBinding
+from nsservice import NSService
+from nsserver import NSServer
+from nsservicegroup import NSServiceGroup
+
+__all__ = [ 'NSBaseResource',
+    'NSConfig',
+    'NSCSPolicy',
+    'NSCSVServer',
+    'NSCSVServerCSPolicyBinding',
+    'NSCSVServerResponderPolicyBinding',
+    'NSCSVServerRewritePolicyBinding',
+    'NSLBVServer',
+    'NSLBVServerServiceBinding',
+    'NSResponderAction',
+    'NSResponderPolicy',
+    'NSResponderPolicyLabel',
+    'NSResponderPolicyLabelBinding',
+    'NSResponderPolicyCSVServerBinding',
+    'NSRewritePolicy',
+    'NSRewritePolicyCSVServerBinding',
+    'NSServer',
+    'NSService',
+    'NSServiceGroup'
+ ]
