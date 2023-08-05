1e987383f4ba28c6e3c86c62fb9cd87a28066823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package django-pipeline/1.2.21/django-pipeline-1.2.21.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.2.21.tar.gz/django-pipeline-1.2.21/pipeline/compressors/cssmin.py
@@ -0,0 +1,8 @@
+from pipeline.conf import settings
+from pipeline.compressors import SubProcessCompressor
+
+
+class CSSMinCompressor(SubProcessCompressor):
+    def compress_css(self, css):
+        command = "%s %s" % (settings.PIPELINE_CSSMIN_BINARY, settings.PIPELINE_CSSMIN_ARGUMENTS)
+        return self.execute_command(command, css)
