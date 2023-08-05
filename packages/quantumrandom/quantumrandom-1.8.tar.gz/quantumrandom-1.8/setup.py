e929e64f611f48b70daf629049a815c64f442f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package quantumrandom/1.8/quantumrandom-1.8.tar.gz

---
+++ packages/quantumrandom/quantumrandom-1.8.tar.gz/quantumrandom-1.8/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys
+
+version = '1.8'
+
+f = open('README.rst')
+long_description = f.read()
+f.close()
+
+requires = []
+if sys.version_info[:2] == (2, 4):
+    requires.append('simplejson < 2.0.10')
+
+setup(name='quantumrandom',
+      version=version,
+      description="A Python interface to the ANU Quantum Random Numbers Server",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          ],
+      keywords='quantum random',
+      author='Luke Macken',
+      author_email='lmacken@redhat.com',
+      url='http://github.com/lmacken/quantumrandom',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=requires,
+      entry_points="""
+        [console_scripts]
+        qrandom = quantumrandom.cmd:main
+        qrandom-dev = quantumrandom.dev:main
+      """,
+      )
---
+++ packages/quantumrandom/quantumrandom-1.8.tar.gz/quantumrandom-1.8/test.py
@@ -0,0 +1,74 @@
+import unittest
+import quantumrandom
+
+
+class TestQuantumRandom(unittest.TestCase):
+
+    def test_uint16_json_api(self):
+        data = quantumrandom.get_data('uint16', 1, 1)
+        assert len(data) == 1
+        for i in str(data[0]):
+            assert i in '0123456789', i
+
+    def test_uint16_json_api_max_array(self):
+        data = quantumrandom.get_data('uint16', 100, 100)
+        assert len(data) == 100
+
+    def test_hex16_json_api(self):
+        data = quantumrandom.get_data('hex16', 1, 1)
+        assert len(data) == 1
+        for h in data[0]:
+            assert h in '0123456789abcdef', h
+
+    def test_uint16_json_api_long_array(self):
+        try:
+            data = quantumrandom.get_data('uint16', 101, 1)
+            assert False, "Invalid array length didn't cause error: %s" % data
+        except:
+            pass
+
+    def test_uint16_json_api_large_blocksize(self):
+        try:
+            data = quantumrandom.get_data('uint16', 1, 101)
+            assert False, "Invalid block size didn't cause error: %s" % data
+        except:
+            pass
+
+    def test_uint16_json_api_invalid_type(self):
+        try:
+            data = quantumrandom.get_data('binary', 1, 1)
+            assert False, "Invalid type didn't throw exception: %s" % data
+        except:
+            pass
+
+    def test_ensure_bytestrings(self):
+        data = quantumrandom.get_data('hex16', 1, 1)
+        assert type(data[0]) is str, data
+
+    def test_binary(self):
+        binary = quantumrandom.binary()
+        assert binary
+        assert len(binary) == 10000, len(binary)
+
+    def test_hex(self):
+        hex = quantumrandom.hex()
+        assert hex
+        assert len(hex) == 20000, len(hex)
+        for h in hex:
+            assert h in '1234567890abcdef', h
+
+    def test_uint16(self):
+        ints = quantumrandom.uint16()
+        assert len(ints) == 100
+        assert len(ints.data) == 200
+
+    def test_randint(self):
+        for i in range(5):
+            for j in range(i + 1, 5):
+                for k in range(3):
+                    val = quantumrandom.randint(i, j)
+                    assert(val >= i and val < j)
+
+
+if __name__ == '__main__':
+    unittest.main()
