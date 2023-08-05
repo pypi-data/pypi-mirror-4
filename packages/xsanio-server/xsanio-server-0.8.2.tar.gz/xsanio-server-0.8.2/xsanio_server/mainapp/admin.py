4504812bc3807a48b5b0708fd8d2c05382509725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package xsanio-server/0.8.2/xsanio-server-0.8.2.tar.gz

---
+++ packages/xsanio-server/xsanio-server-0.8.2.tar.gz/xsanio-server-0.8.2/xsanio_server/mainapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from mainapp.models import *
+
+admin.site.register(stat_type)
+admin.site.register(xsanio_client)
+admin.site.register(stat_entry)
+admin.site.register(cvlabel_entry)
+admin.site.register(xsan_volume)
+admin.site.register(xsan_volume_lun)
