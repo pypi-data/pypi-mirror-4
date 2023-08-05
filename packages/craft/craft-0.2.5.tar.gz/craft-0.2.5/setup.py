10ea02a6a88d475b01100f86dd43b074a46a60c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package craft/0.2.5/craft-0.2.5.tar.gz

---
+++ packages/craft/craft-0.2.5.tar.gz/craft-0.2.5/setup.py
@@ -0,0 +1,21 @@
+import os
+from distutils.core import setup
+
+this_path = os.path.dirname(os.path.realpath(__file__))
+
+setup(
+    name='craft',
+    version='0.2.5',
+    author='Jamie Paton',
+    author_email='jamie.paton@googlemail.com',
+    packages=['craft'],
+    scripts=['bin/craft'],
+    data_files=('./', 'README.pypi'),
+    url='http://pypi.python.org/pypi/craft/',
+    license='LICENSE.txt',
+    description='Craft - Like Makefile, but in Python.',
+    long_description=open(os.path.join(this_path,'README.pypi')).read(),
+    install_requires=[
+        "termcolor == 1.1.0",
+    ],
+)
