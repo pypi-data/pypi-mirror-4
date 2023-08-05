23dffccb9382d7ae7a34f69357d5d63d72a10980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package chef-solo-cup/0.0.6/chef-solo-cup-0.0.6.tar.gz

---
+++ packages/chef-solo-cup/chef-solo-cup-0.0.6.tar.gz/chef-solo-cup-0.0.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='chef-solo-cup',
+    version='0.0.6',
+    author='Jose Diaz-Gonzalez',
+    author_email='email@josediazgonzalez.com',
+    packages=['chef_solo_cup', 'chef_solo_cup.commands'],
+    scripts=['bin/chef-solo-cup'],
+    url='https://github.com/josegonzalez/chef-solo-cup',
+    license=open('LICENSE.txt').read(),
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    description='Chef-Solo wrapper',
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').readlines(),
+)
