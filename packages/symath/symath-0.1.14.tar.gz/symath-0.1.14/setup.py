5ea215e52a32f24418b679bfdb00773b241f89dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:07 +0000

    Add package symath/0.1.14/symath-0.1.14.tar.gz

---
+++ packages/symath/symath-0.1.14.tar.gz/symath-0.1.14/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup( \
+  name='symath', \
+  version='0.1.14', \
+  description='symbolic mathematics for python', \
+  author='Brandon Niemczyk', \
+  author_email='brandon.niemczyk@gmail.com', \
+  url='http://github.com/bniemczyk/symbolic', \
+  packages=['symath', 'symath.solvers'], \
+  test_suite='tests', \
+  license='BSD', \
+  classifiers = [ \
+    'Development Status :: 3 - Alpha', \
+    'Intended Audience :: Developers', \
+    'Intended Audience :: Science/Research', \
+    'License :: OSI Approved :: BSD License', \
+    'Topic :: Scientific/Engineering :: Mathematics' \
+    ]
+  )
