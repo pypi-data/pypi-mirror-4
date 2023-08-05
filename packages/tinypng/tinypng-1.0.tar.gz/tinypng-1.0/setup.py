d7cc28eb0c7bb42962d4c4e0b7d6d8618c206fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package tinypng/1.0/tinypng-1.0.tar.gz

---
+++ packages/tinypng/tinypng-1.0.tar.gz/tinypng-1.0/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from os.path import join, dirname
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+
+setup(
+    name='tinypng',
+    version='1.0',
+    description='Access api.tinypng.org from the shell and python scripts',
+    long_description=read('README.markdown'),
+    author='Manuel Barkhau',
+    author_email='mbarkhau@gmail.com',
+    url='http://bitbucket.org/mbarkhau/tinypng/',
+    license="MIT",
+    packages=['tinypng'],
+    requires=['docopt (>=0.5)'],
+    scripts=['scripts/tinypng'],
+    keywords="png image compression tinypng shrink",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+
+)
