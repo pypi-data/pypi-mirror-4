157a593ecb7f1186c719f2db0adf6e8f6503c476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package infi.parted/0.1.13/infi.parted-0.1.13-py2.7.egg

---
+++ packages/infi.parted/infi.parted-0.1.13-py2.7.egg/infi/parted/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.13"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '08bfea4fdc397baa8678686052976ac3d9ec946d'
+__git_head_subject__ = 'empty commit after version v0.1.12'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
