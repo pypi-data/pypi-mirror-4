99c3cb16ea1911c291963de3c4a9ee4ee357c82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:22 +0000

    Add package rdlm/0.1.b1/rdlm-0.1.b1.tar.gz

---
+++ packages/rdlm/rdlm-0.1.b1.tar.gz/rdlm-0.1.b1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of restful-distributed-lock-manager released under the MIT license.
+# See the LICENSE file for more information.
+
+from setuptools import setup, find_packages
+import rdlm
+
+DESCRIPTION = "RDLM (Restful Distributed Lock Manager) is a lock manager over HTTP built on Tornado"
+try:
+    with open('README.rst') as f:
+        LONG_DESCRIPTION = f.read()
+except IOError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+setup(
+    name='rdlm',
+    version=rdlm.__version__,
+    author="Fabien MARTY",
+    author_email="fabien.marty@gmail.com",
+    url="https://github.com/thefab/restful-distributed-lock-manager",
+    packages=find_packages(),
+    license='MIT',
+    download_url='https://github.com/thefab/restful-distributed-lock-manager',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    scripts=["rdlm-daemon.py"],
+    install_requires=[
+        'tornado >= 2.3.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',    
+        'Topic :: Utilities',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: Software Development',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+      ]
+)
