7c9c94306ed241c1ccaac4546a930ce3fc14c989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package rediscluster/0.2.4/rediscluster-0.2.4.tar.gz

---
+++ packages/rediscluster/rediscluster-0.2.4.tar.gz/rediscluster-0.2.4/rediscluster/__init__.py
@@ -0,0 +1,19 @@
+from rediscluster.client import StrictRedis
+from redis.exceptions import (
+    AuthenticationError,
+    ConnectionError,
+    DataError,
+    InvalidResponse,
+    PubSubError,
+    RedisError,
+    ResponseError,
+    WatchError,
+)
+
+__version__ = '0.2.4'
+VERSION = tuple(map(int, __version__.split('.')))
+
+__all__ = [
+    'StrictRedis', 'RedisError', 'ConnectionError', 'ResponseError', 'AuthenticationError',
+    'InvalidResponse', 'DataError', 'PubSubError', 'WatchError'
+]
