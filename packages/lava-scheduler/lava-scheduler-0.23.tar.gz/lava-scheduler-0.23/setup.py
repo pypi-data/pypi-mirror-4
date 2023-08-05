e5b015c24a0ebcdc195f2c90495be1a02b7b83c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:49 +0000

    Add package lava-scheduler/0.23/lava-scheduler-0.23.tar.gz

---
+++ packages/lava-scheduler/lava-scheduler-0.23.tar.gz/lava-scheduler-0.23/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 Linaro Limited
+#
+# Author: Michael Hudson-Doyle <michael.hudson@linaro.org>
+#
+# This file is part of LAVA Scheduler.
+#
+# LAVA Scheduler is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License version 3 as
+# published by the Free Software Foundation
+#
+# LAVA Scheduler is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with LAVA Scheduler.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lava-scheduler',
+    version=":versiontools:lava_scheduler_app:",
+    author="Michael Hudson-Doyle",
+    author_email="michael.hudson@linaro.org",
+    packages=find_packages(),
+    license="AGPL",
+    description="LAVA Scheduler Application",
+    entry_points="""
+    [lava_server.extensions]
+    scheduler = lava_scheduler_app.extension:SchedulerExtension
+    """,
+    install_requires=[
+        "django-restricted-resource >= 0.2.7",
+        "django-tables2 >= 0.9.4",
+        "lava-dashboard",
+        "lava-dispatcher >= 0.5.9",
+        "lava-server >= 0.11",
+        "simplejson",
+        "south >= 0.7.3",
+        "twisted",
+        "versiontools >= 1.8",
+    ],
+    setup_requires=[
+        "versiontools >= 1.8",
+    ],
+    tests_require=[
+        "django-testscenarios",
+    ],
+    zip_safe=False,
+    include_package_data=True)
