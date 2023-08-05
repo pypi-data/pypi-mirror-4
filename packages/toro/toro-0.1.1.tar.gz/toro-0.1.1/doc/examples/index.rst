756a8dd1c5207be8a4088192848c9c823ae41769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package toro/0.1.1/toro-0.1.1.tar.gz

---
+++ packages/toro/toro-0.1.1.tar.gz/toro-0.1.1/doc/examples/index.rst
@@ -0,0 +1,10 @@
+Examples
+========
+
+.. toctree::
+    producer_consumer_example
+    producer_consumer_example_callbacks
+    event_example
+    web_spider_example
+
+.. todo: Lock example
---
+++ packages/toro/toro-0.1.1.tar.gz/toro-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: Apache Software License
+Development Status :: 4 - Beta
+Natural Language :: English
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.5
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Operating System :: MacOS :: MacOS X
+Operating System :: Unix
+Programming Language :: Python
+"""
+
+description = 'Synchronization primitives for Tornado coroutines.'
+
+long_description = open("README.rst").read()
+
+
+setup(name='toro',
+      version='0.1.1',
+      packages=['toro'],
+      description=description,
+      long_description=long_description,
+      author='A. Jesse Jiryu Davis',
+      author_email='ajdavis@cs.oberlin.edu',
+      url='http://github.com/ajdavis/toro/',
+      install_requires=['tornado >= 2.4.0'],
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      classifiers=filter(None, classifiers.split('\n')),
+      keywords='tornado coroutines semaphore mutex queue asynchronous',
+)
