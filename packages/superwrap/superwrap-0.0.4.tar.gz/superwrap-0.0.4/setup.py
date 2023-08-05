8a77aefaf14bcf9bae54646943047527d3016d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:36 +0000

    Add package superwrap/0.0.4/superwrap-0.0.4.tar.gz

---
+++ packages/superwrap/superwrap-0.0.4.tar.gz/superwrap-0.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='superwrap',
+    license='Apache License 2.0',
+    version='0.0.4',
+    description="The easy way to contribute to python packages",
+    author="Steve Pulec",
+    author_email="spulec@gmail.com",
+    url="https://github.com/spulec/superwrap",
+    requires=(
+        'virtualenv',
+        'virtualenvwrapper (>=2.9)',
+        'requests (>=0.12.1)',
+    ),
+    namespace_packages=['virtualenvwrapper'],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'virtualenvwrapper.pre_activate': [
+            'user_scripts = virtualenvwrapper.the_scripts:pre_activate',
+        ],
+    }
+)
