2bd7d0bd2ab7e63ba125dd2268bec3ded036f71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package scraper/0.1.0/scraper-0.1.0.tar.gz

---
+++ packages/scraper/scraper-0.1.0.tar.gz/scraper-0.1.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(  name = 'scraper',
+        version = '0.1.0',
+        author='F. Javier Alba',
+        author_email='me@fjavieralba.com',
+        packages = ['scraper'],
+        url='http://pypi.python.org/pypi/scraper/',
+        license='LICENSE.txt',
+        description = 'Configurable Python Web Scraper',
+        long_description=open('README.txt').read(),
+        install_requires=[
+            "lxml >= 3.0.1",
+        ],
+    )
