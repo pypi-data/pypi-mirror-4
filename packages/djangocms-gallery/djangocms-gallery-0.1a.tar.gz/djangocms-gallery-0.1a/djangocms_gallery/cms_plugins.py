451f06040b5b70f31bafe09eb0d4fe8afa1f068f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package djangocms-gallery/0.1a/djangocms-gallery-0.1a.tar.gz

---
+++ packages/djangocms-gallery/djangocms-gallery-0.1a.tar.gz/djangocms-gallery-0.1a/djangocms_gallery/cms_plugins.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+from djangocms_gallery.models import Gallery
+from djangocms_gallery.admin import SlideAdmin
+from djangocms_gallery import settings
+
+
+class GalleryPlugin(CMSPluginBase):
+    model = Gallery
+    name = _('Gallery')
+    render_template = 'djangocms_gallery/gallery_base.html'
+
+    inlines = [SlideAdmin,]
+    fields = ('title', 'template', 'autoplay')
+    advanced_fields = (('width', 'height'),)
+
+    @property
+    def declared_fieldsets(self):
+        self.fieldsets = ((None, {'fields': self.fields}))
+        if settings.ADVANCED_OPTIONS:
+            self.fieldsets = (self.fieldsets, (_('Advanced'), {'fields': self.advanced_fields, 'classes': ['collapse',]}))
+        return super(GalleryPlugin, self)._declared_fieldsets()
+
+plugin_pool.register_plugin(GalleryPlugin)
