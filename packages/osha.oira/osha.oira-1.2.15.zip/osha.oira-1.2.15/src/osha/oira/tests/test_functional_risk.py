d931b98cec815116298b409483e947d013c49277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:09 +0000

    Add package osha.oira/1.2.15/osha.oira-1.2.15.zip

---
+++ packages/osha.oira/osha.oira-1.2.15.zip/osha.oira-1.2.15/src/osha/oira/tests/test_functional_risk.py
@@ -0,0 +1,136 @@
+# coding=utf-8
+
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.browser.select import SelectWidget
+import zope.component
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+from zope.interface import alsoProvides
+
+from Products.Five.testbrowser import Browser
+
+from plonetheme.nuplone.z3cform.widget import SingleRadioWidget
+
+from euphorie.client.tests.utils import addSurvey
+from euphorie.client.tests.utils import registerUserInClient
+
+from osha.oira.tests.base import OiRATestCase
+from osha.oira.tests.base import OiRAFunctionalTestCase
+from osha.oira import interfaces
+
+class OiRARiskTests(OiRATestCase):
+
+    def _create(self, container, *args, **kwargs):
+        newid = container.invokeFactory(*args, **kwargs)
+        return getattr(container, newid)
+
+    def createModule(self, algorithm='kinney'):
+        country = self.portal.sectors.nl
+        sector = self._create(country, "euphorie.sector", "sector")
+        surveygroup = self._create(
+                                sector, 
+                                "euphorie.surveygroup", 
+                                "group",
+                                evaluation_algorithm=algorithm)
+        survey = self._create(surveygroup, "euphorie.survey", "survey")
+        return self._create(survey, "euphorie.module", "module")
+
+    def createRisk(self, algorithm='kinney'):
+        module = self.createModule(algorithm)
+        return self._create(module, "euphorie.risk", "risk")
+
+    def testDynamicDescription(self):
+        """ #3343: Customize infoBubble description according to calculation
+            method.
+        """
+        self.loginAsPortalOwner()
+        for risk_type in ['kinney', 'french']:
+            module = self.createModule(risk_type)
+            # Merely installing the OiRA skin doesn't set it's layer on the
+            # request. This happens during IBeforeTraverseEvent, so we have to do 
+            # here manually
+            alsoProvides(self.portal.REQUEST, interfaces.IOSHAContentSkinLayer)
+
+            # Test AddForm
+            form = module.unrestrictedTraverse('++add++euphorie.risk').form_instance
+            form.updateFields()
+            self.assertEqual(
+                    form.schema.get('evaluation_method').description, 
+                    'help_evaluation_method_%s' % risk_type
+                    )
+
+            # Test EditForm
+            risk = self._create(module, "euphorie.risk", "risk")
+            form = risk.unrestrictedTraverse('@@edit').form_instance
+            form.updateFields()
+            self.assertEqual(
+                    form.schema.get('evaluation_method').description, 
+                    'help_evaluation_method_%s' % risk_type
+                    )
+            self.portal.sectors.nl.manage_delObjects(['sector'])
+
+
+    def testChoiceWidget(self):
+        """ #1537 The Choice fields must be uniform and all radio buttons.
+        """
+        self.loginAsPortalOwner()
+        module = self.createModule()
+        alsoProvides(self.portal.REQUEST, interfaces.IOSHAContentSkinLayer)
+        form = module.unrestrictedTraverse('++add++euphorie.risk').form_instance
+        form.updateFields()
+
+        # Test with vocabs of different lengths
+        for i in [3, 5, 10]:
+            v = SimpleVocabulary([SimpleTerm("%d" % k) for k in range(0, i)])
+            field = form.schema.get('evaluation_method')
+            field.vocabulary = v
+            form.updateWidgets()
+
+            widget = zope.component.getMultiAdapter(
+                (field, form.request), IFieldWidget)
+
+            if i in [3, 5]:
+                self.assertEqual(type(widget), SingleRadioWidget)
+            else:
+                self.assertEqual(type(widget), SelectWidget)
+
+
+class EuphorieRiskTests(OiRAFunctionalTestCase):
+
+    def testShowFrenchEvaluation(self):
+        from euphorie.content.tests.utils import BASIC_SURVEY
+        # Test for http://code.simplon.biz/tracker/tno-euphorie/ticket/150
+        self.loginAsPortalOwner()
+        addSurvey(self.portal, BASIC_SURVEY)
+        browser=Browser()
+        survey=self.portal.client.nl["ict"]["software-development"]
+        survey.evaluation_algorithm=u"french"
+        survey["1"]["2"].type="risk"
+        browser.open(survey.absolute_url())
+        registerUserInClient(browser)
+        # Create a new survey session
+        browser.getControl(name="title:utf8:ustring").value=u"Sessiøn".encode("utf-8")
+        browser.getControl(name="next", index=1).click()
+        # Start the survey
+        browser.getForm().submit()
+        browser.getLink("Start Risk Identification").click()
+        # Identify the risk
+        browser.getControl("next").click()
+        browser.getControl(name="answer").value=["no"]
+        browser.getControl("next").click()
+        # Move on to the risk's action plan form
+        browser.getLink("Run evaluation").click()
+        browser.handleErrors=False
+        browser.getLink("Next").click()
+        # Verify number of options
+        self.assertEqual(len(browser.getControl(name="frequency:int").controls), 4)
+        self.assertEqual(len(browser.getControl(name="severity:int").controls), 4)
+        # Enter some digits
+        browser.getControl(name="frequency:int").value=["7"]
+        browser.getControl(name="severity:int").value=["10"]
+        browser.getControl("next").click()
+        # Verify the result
+        browser.open(
+                "http://nohost/plone/client/nl/ict/software-development/actionplan/1/1")
+        self.assertEqual(browser.getControl(name="priority").value, ["high"])
+
---
+++ packages/osha.oira/osha.oira-1.2.15.zip/osha.oira-1.2.15/src/osha/oira/utils.py
@@ -0,0 +1,166 @@
+import htmllib
+from mobile.sniffer.detect import  detect_mobile_browser
+from mobile.sniffer.utilities import get_user_agent
+
+from five import grok
+
+from Acquisition import aq_inner
+
+from zope.app.component.hooks import getSite
+
+from Products.CMFCore.utils import getToolByName
+
+from euphorie.client.sector import IClientSector
+from euphorie.client.utils import WebHelpers
+from euphorie.content.survey import ISurvey
+
+from osha.oira import interfaces
+
+grok.templatedir('templates')
+
+def html_unescape(s):
+    p = htmllib.HTMLParser(None)
+    p.save_bgn()
+    p.feed(s)
+    return p.save_end()
+
+def remove_empty_modules(ls):
+    """ Takes a list of modules and risks.
+
+        Removes modules that don't have any risks in them.
+        Modules with submodules (with risks) must however be kept.
+    """
+    while ls and ls[-1].type == 'module':
+        ls = ls[:-1]
+
+    for i in range(len(ls)-1, 0, -1):
+        if ls[i] is None:
+            if ls[i-1].type == 'module':
+                ls[i-1] = None
+
+        elif ls[i].type == 'module':
+            if ls[i-1].type == 'module' and \
+                    ls[i-1].depth >= ls[i].depth:
+                ls[i-1] = None
+
+    return [m for m in ls if m is not None]
+
+
+def get_unactioned_nodes(ls):
+    """ Takes a list of modules and risks and removes all risks that have *not* 
+        actioned (i.e does not have at least one valid action plan)
+        Also remove all modules that have lost all their risks in the process.
+
+        See https://syslab.com/proj/issues/2885
+    """
+    unactioned = []
+    for n in ls:
+        if n.type == 'module':
+            unactioned.append(n)
+
+        elif n.type == 'risk':
+            if not n.action_plans:
+                unactioned.append(n)
+            else:
+                # It's possible that there is an action plan object, but
+                # that it's not yet fully populated
+                if n.action_plans[0] == None or \
+                        n.action_plans[0].action_plan == None:
+                    unactioned.append(n)
+
+    return remove_empty_modules(unactioned)
+
+
+def get_actioned_nodes(ls):
+    """ Takes a list of modules and risks and removes all risks that have been
+        actioned (i.e has at least one valid action plan).
+        Also remove all modules that have lost all their risks in the process
+
+        See https://syslab.com/proj/issues/2885
+    """
+    actioned = []
+    for n in ls:
+        if n.type == 'module':
+            actioned.append(n)
+
+        if n.type == 'risk' and len(n.action_plans):
+                # It's possible that there is an action plan object, but
+                # it's not yet fully populated
+                plans = [p.action_plan for p in n.action_plans]
+                if plans[0] != None:
+                    actioned.append(n)
+
+    return remove_empty_modules(actioned)
+
+
+class OSHAWebHelpers(WebHelpers):
+    """ Override Euphorie's webhelpers to add some more utility methods.
+    """
+    grok.layer(interfaces.IOSHAClientSkinLayer)
+    grok.template('webhelpers')
+    grok.name('webhelpers')
+
+    def language_dict(self):
+        site = getSite()
+        ltool = getToolByName(site, 'portal_languages')
+        return ltool.getAvailableLanguages()
+
+    def on_mobile(self):
+        """ Return True if the site is being browsed on a mobile phone.
+        """
+        ua = get_user_agent(self.request)
+        if ua:
+            if detect_mobile_browser(ua):
+                return True
+            else:
+                return False
+        return False
+        
+    def get_sectors_dict(self):
+        """ Returns a dictionary with keys being countries (and int. orgs) that
+            have sectors inside them and the values being the available survey
+            langauges.
+
+            We use ZCatalog directly to bypass permission checks, otherwise we
+            get zero surveys returned for anon users.
+
+            See #2556.
+        """ 
+        context = aq_inner(self.context)
+        sectorsfolder = getattr(context, 'sectors')
+        if not sectorsfolder:
+            return []
+
+        client = getattr(context, 'client')
+        if not client:
+            return []
+
+        resp = {}
+        ltool = getToolByName(self.context, 'portal_languages')
+        # Only the countries in the client obj should be considered, as the
+        # others are not accessible
+        for country in client.values():
+            ldict = {}
+            for sector in country.values():
+                if not IClientSector.providedBy(sector):
+                    continue
+                for survey in sector.objectValues():
+                    lang = survey.language
+                    if not lang:
+                        continue 
+
+                    if not ISurvey.providedBy(survey):
+                        continue
+                    if getattr(survey, "preview", False):
+                        continue
+                    supported_langs = ltool.getSupportedLanguages()
+                    if lang not in supported_langs:
+                        base_lang = lang.split('-')[0].strip()
+                        if base_lang in supported_langs:
+                            ldict[base_lang] = 'dummy'
+                        continue
+                    ldict[lang] = 'dummy'
+                    
+            if ldict:
+                resp[country.id] = ldict.keys()
+        return resp
