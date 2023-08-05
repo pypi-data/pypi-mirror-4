aba03835975d29d17c1adc122f331f37debe1ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:07 +0000

    Add package zc.lockfile/1.0.2/zc.lockfile-1.0.2.tar.gz

---
+++ packages/zc.lockfile/zc.lockfile-1.0.2.tar.gz/zc.lockfile-1.0.2/setup.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name, version = "zc.lockfile", '1.0.2'
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zc', 'lockfile', 'README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        )
+
+open('doc.txt', 'w').write(long_description)
+
+setup(
+    name = name, version=version,
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "Basic inter-process locks",
+    long_description=long_description,
+    license = "ZPL 2.1",
+    keywords = "lock",
+    url='http://www.python.org/pypi/'+name,
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['zc'],
+    install_requires = 'setuptools',
+    extras_require=dict(
+        test=[
+            'zope.testing',
+            ]),
+    include_package_data = True,
+    zip_safe=False,
+    classifiers = [
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+       ],
+    )
