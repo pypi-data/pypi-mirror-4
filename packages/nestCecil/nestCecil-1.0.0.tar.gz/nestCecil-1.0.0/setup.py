51c77f13e01e5236cd2012798ce40122dba32e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package nestCecil/1.0.0/nestCecil-1.0.0.tar.gz

---
+++ packages/nestCecil/nestCecil-1.0.0.tar.gz/nestCecil-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name='nestCecil',
+		version='1.0.0',
+		py_modules=['nester'],
+		author='Ceclinux',
+		author_mail='src655@gmail.com',
+		url='ceclinux.org',
+		description='From Head First Python',
+		)
