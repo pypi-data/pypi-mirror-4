83225029f494697b0f9f0e523450d906a2f25bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:23 +0000

    Add package Tinkerer/1.0/Tinkerer-1.0.tar.gz

---
+++ packages/Tinkerer/Tinkerer-1.0.tar.gz/Tinkerer-1.0/tinkerer/ext/patch.py
@@ -0,0 +1,169 @@
+'''
+    patch
+    ~~~~~
+
+    Handles HTML link patching for images and cross-references. Sphinx
+    generates these links as relative paths - aggregated pages and RSS
+    feed require these to be patched.
+
+    :copyright: Copyright 2011-2012 by Vlad Riscutia and contributors (see
+    CONTRIBUTORS file)
+    :license: FreeBSD, see LICENSE file
+'''
+import re
+import xml.dom.minidom
+from tinkerer.ext.uistr import UIStr
+
+try:
+    from html.entities import name2codepoint
+except:
+    from htmlentitydefs import name2codepoint    
+
+try:
+    import builtins as __builtin__
+except:
+    import __builtin__
+    
+
+# check whether unichr builtin exists, otherwise use chr
+if "unichr" not in __builtin__.__dict__:
+    unichr = chr
+    
+            
+
+def build_html_only_codepoints():
+    """
+    Convert well formed HTML to XML by removing HTML only entity definitions.
+    """
+    html_name2codepoints = {}
+    html_name2codepoints.update(name2codepoint)
+    for name in ['amp', 'gt', 'lt', 'apos', 'quot']:
+        if name in html_name2codepoints:
+            del html_name2codepoints[name]
+    return html_name2codepoints
+
+
+
+HTML_ONLY_NAME2CODEPOINTS = build_html_only_codepoints()
+
+
+
+def convert(s):
+    """
+    Take an input string s, find all things that look like SGML character 
+    entities, and replace them with the Unicode equivalent.
+
+    Source:
+    http://stackoverflow.com/questions/1197981/convert-html-entities-to-ascii-in-python/1582036#1582036
+    """
+    matches = re.findall("&#\d+;", s)
+    if len(matches) > 0:
+        hits = set(matches)
+        for hit in hits:
+            name = hit[2:-1]
+            try:
+                entnum = int(name)
+                s = s.replace(hit, unichr(entnum))
+            except ValueError:
+                pass
+    matches = re.findall("&\w+;", s)
+    hits = set(matches)
+    amp = "&"
+    if amp in hits:
+        hits.remove(amp)
+    for hit in hits:
+        name = hit[1:-1]
+        if name in HTML_ONLY_NAME2CODEPOINTS:
+            s = s.replace(hit,
+                          unichr(HTML_ONLY_NAME2CODEPOINTS[name]))
+    s = s.replace(amp, "&")
+    return s
+
+
+
+def patch_links(body, docpath, docname=None, link_title=False):
+    '''
+    Parses the document body and calls patch_node from the document root
+    to fix hyperlinks. Also hyperlinks document title. Returns resulting 
+    XML as string.
+    '''
+    in_str = convert(body).encode("utf-8")
+    doc = xml.dom.minidom.parseString(in_str)
+    patch_node(doc, docpath, docname)
+
+    body = doc.toxml()
+    if docname:
+        body = make_read_more_link(body, docpath, docname)
+    
+    if link_title:
+        return hyperlink_title(body, docpath, docname)
+    else:
+        return body
+
+
+
+def hyperlink_title(body, docpath, docname):
+    """
+    Hyperlink titles by embedding appropriate a tag inside
+    h1 tags (which should only be post titles). 
+    """
+    body = body.replace("<h1>", '<h1><a href="%s.html">' % 
+            (docpath + docname), 1)
+    body = body.replace("</h1>", "</a></h1>", 1)
+    return body
+
+
+
+def make_read_more_link(body, docpath, docname):            
+    """
+    Create "read more" link if marker exists.
+    """
+    marker_more = '<a name="more"> </a>'
+    pos = body.find(marker_more)
+
+    if pos == -1:
+        return body
+
+    body = body[:pos]
+    return body + ('<a class="readmore" href="%s.html#more">%s</a></div>' %
+                (docpath + docname, UIStr.READ_MORE))
+
+
+
+def patch_node(node, docpath, docname=None):
+    '''
+    Recursively patches links in nodes.
+    '''
+    node_name = node.localName
+
+    # if node is <img>
+    if node_name == "img":
+        src = node.getAttributeNode("src")
+        # if this is relative path (internal link)
+        if src.value.startswith(".."):
+            src.value = docpath + src.value 
+    # if node is hyperlink            
+    elif node_name == "a":
+        ref = node.getAttributeNode("href")
+        # skip anchor links <a name="anchor1"></a>, <a name="more"/>
+        if ref != None:
+            # patch links only - either starting with "../" or having
+            # "internal" class
+            is_relative = ref.value.startswith("../") 
+            if is_relative or "internal" in node.getAttribute("class"):
+                ref.value = docpath + ref.value
+            # html anchor with missing post.html
+            # e.g. href="2012/08/23/#the-cross-compiler"
+            # now href="2012/08/23/a_post.html#the-cross-compiler"
+            ref.value = ref.value.replace("/#", "/%s.html#" % docname)
+
+    # recurse            
+    for node in node.childNodes:
+        patch_node(node, docpath, docname)
+
+
+def strip_xml_declaration(body):
+    """
+    Remove XML declaration from document body.
+    """
+    return body.replace('<?xml version="1.0" ?>', '')
---
+++ packages/Tinkerer/Tinkerer-1.0.tar.gz/Tinkerer-1.0/tinkerer/writer.py
@@ -0,0 +1,74 @@
+'''
+    writer
+    ~~~~~~
+
+    Internal template writer - handles template rendering and blog setup.
+
+    :copyright: Copyright 2011-2012 by Vlad Riscutia and contributors (see
+    CONTRIBUTORS file)
+    :license: FreeBSD, see LICENSE file
+'''
+
+from jinja2 import Environment, PackageLoader
+from tinkerer import paths, utils
+
+
+# jinja environment
+env = Environment(loader=PackageLoader("tinkerer", "__templates"))
+
+
+def render(template, destination, context={}):
+    '''
+    Renders the given template at the given destination with the given context.
+    '''
+    with open(destination, "wb") as dest:
+        dest.write(env.get_template(template).render(context).encode("utf8"))
+
+
+
+def write_master_file():
+    '''
+    Writes the blog master document.
+    '''
+    render("master.rst", paths.master_file)
+
+
+
+def write_index_file():
+    '''
+    Writes the root index.html file.
+    '''
+    render("index.html", paths.index_file)
+
+
+
+'''
+Default Tinkerer extensions.
+'''
+DEFAULT_EXTENSIONS = [
+    "tinkerer.ext.blog",
+    "tinkerer.ext.disqus"
+]
+
+
+
+def write_conf_file(extensions=DEFAULT_EXTENSIONS, theme="modern5"):
+    '''
+    Writes the Sphinx configuration file.
+    '''
+    render("conf.py", paths.conf_file,
+           {"extensions": ", ".join(["'%s'" % ext for ext in extensions]),
+            "theme": theme })
+
+
+
+def setup_blog():
+    '''
+    Sets up a new blog.
+    '''
+    utils.get_path(paths.root, "_static")
+    utils.get_path(paths.root, "drafts")
+    write_master_file()
+    write_index_file()
+    write_conf_file()
+    
