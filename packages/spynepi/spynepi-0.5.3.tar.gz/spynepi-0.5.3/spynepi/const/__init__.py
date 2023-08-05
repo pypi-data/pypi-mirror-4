2f47eadfb93140b62caf948f64dd6a43c9df8744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package spynepi/0.5.3/spynepi-0.5.3.tar.gz

---
+++ packages/spynepi/spynepi-0.5.3.tar.gz/spynepi-0.5.3/spynepi/const/__init__.py
@@ -0,0 +1,18 @@
+
+# The database connection string must be sqlalchemy compatible
+DB_CONNECTION_STRING = "sqlite:///spynepi.db"
+
+# The path(absolute recommended) where package files will be saved
+FILES_PATH = 'files'
+
+# The server address
+HOST = "0.0.0.0"
+
+# The server port
+PORT = 7789
+
+# The pypi repository name which defined in pypirc
+REPO_NAME = "spynepi"
+
+# The prefix for database tables
+TABLE_PREFIX = "spynepi"
