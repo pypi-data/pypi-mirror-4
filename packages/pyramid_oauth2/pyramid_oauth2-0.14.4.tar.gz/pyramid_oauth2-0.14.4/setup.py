d396df626696bf626d9e6d15e129e9fd9a0c459c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:29 +0000

    Add package pyramid_oauth2/0.14.4/pyramid_oauth2-0.14.4.tar.gz

---
+++ packages/pyramid_oauth2/pyramid_oauth2-0.14.4.tar.gz/pyramid_oauth2-0.14.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+version = '0.14.4'
+
+testing_extras = ['nose', 
+                  'coverage']
+docs_extras = ['Sphinx']
+
+setup(name='pyramid_oauth2',
+      version=version,
+      description="OAuth2 support for pyramid",
+      long_description="""\
+        Pyramid addons intended to support completely the oauth2 flows.
+
+        It currently only supports one of them. It should be possible
+        to create a oauth2 provider in the future using this addons.
+      """,
+      classifiers=[],
+      keywords='oauth2 pyramid',
+      author='Lo\xc3\xafc Faure-Lacroix',
+      author_email='lamerstar@gmail.com',
+      url='delicieuxgateau.ca',
+      license='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite="pyramid_oauth2",
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'pyramid',
+      ],
+      extras_require = {
+          'dev':testing_extras,
+          'docs':docs_extras,
+          },
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
