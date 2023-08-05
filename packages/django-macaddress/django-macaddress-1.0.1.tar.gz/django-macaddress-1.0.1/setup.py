67fe568a9cd948b3ee30a3d960e224afb360236a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:13 +0000

    Add package django-macaddress/1.0.1/django-macaddress-1.0.1.tar.gz

---
+++ packages/django-macaddress/django-macaddress-1.0.1.tar.gz/django-macaddress-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-macaddress",
+    version = "1.0.1",
+    url = 'http://github.com/tubaman/django-macaddress',
+    license = 'BSD',
+    description = "MAC address model and form fields for Django apps.",
+    long_description = read('README.rst'),
+    author = 'Ryan Nowakowski',
+    author_email = 'tubaman@fattuba.com',
+    packages = ['macaddress'],
+    install_requires = ['netaddr'],
+    tests_require = ['django'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
