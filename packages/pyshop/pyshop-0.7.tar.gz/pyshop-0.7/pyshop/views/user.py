3a09a7dcec370a90f8116c87ee86cab0cf5243c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:44 +0000

    Add package pyshop/0.7/pyshop-0.7.tar.gz

---
+++ packages/pyshop/pyshop-0.7.tar.gz/pyshop-0.7/pyshop/views/user.py
@@ -0,0 +1,42 @@
+from pyshop.models import User
+from pyshop.helpers.i18n import trans as _
+
+from .account import AccountMixin
+from .base import EditView
+
+
+
+class UserMixin(AccountMixin):
+    redirect_route = 'list_package'
+
+    def get_model(self):
+        return self.user
+
+    def update_view(self, model, view):
+        pass
+
+
+class Edit(UserMixin, EditView):
+    """
+    Edit connected user
+    """
+
+
+class ChangePassword(UserMixin, EditView):
+    """
+    Change current user password
+    """
+
+    def validate(self, model, errors):
+        r = self.request
+
+        if not User.by_credentials(self.session, model.login,
+                                   r.params['current_password']):
+            errors.append(_(u'current password is not correct'))
+        elif r.params['user.password'] == r.params['current_password']:
+            errors.append(_(u'password is inchanged'))
+
+        if r.params['user.password'] != r.params['confirm_password']:
+            errors.append(_(u'passwords do not match'))
+
+        return len(errors) == 0
---
+++ packages/pyshop/pyshop-0.7.tar.gz/pyshop-0.7/setup.py
@@ -0,0 +1,79 @@
+import os
+import re
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+with open(os.path.join(here, 'pyshop', '__init__.py')) as v_file:
+    version = re.compile(r".*__version__ = '(.*?)'",
+                         re.S).match(v_file.read()).group(1)
+
+requires = [
+    'pyramid',
+    'waitress',
+    'SQLAlchemy',
+#    'pyramid_debugtoolbar',
+    'pyramid_scss',
+    'pyramid_jinja2',
+    'pyramid_xmlrpc',
+
+    'pyramid_tm',
+    'zope.sqlalchemy',
+
+    'cryptacular',
+    'requests < 1.0.0',
+    'docutils',
+
+    'IPython',
+]
+
+
+if 'VIRTUAL_ENV' in os.environ:
+    venv = os.environ['VIRTUAL_ENV']
+    data_files = [(venv, ['pyshop.sample.ini',
+                          ])]
+else:
+    data_files = []
+
+
+setup(name='pyshop',
+      version=version,
+      description='A cheeseshop clone (PyPI server) written in pyramid',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+      'Development Status :: 4 - Beta',
+      'Environment :: Web Environment',
+      'Framework :: Pyramid',
+      'Topic :: Internet :: WWW/HTTP',
+      'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+      'Topic :: Software Development',
+      'Topic :: System :: Archiving :: Mirroring',
+      'Topic :: System :: Archiving :: Packaging',
+      'Intended Audience :: Developers',
+      'Intended Audience :: System Administrators',
+      'License :: OSI Approved :: BSD License',
+      ],
+      author='Guillaume Gauvrit',
+      author_email='ggauvrit@laposte.net',
+      url='https://github.com/mardiros/pyshop',
+      keywords='web wsgi pyramid cheeseshop pypi packaging',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyshop',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyshop:main
+      [console_scripts]
+      pyshop_install = pyshop.bin.install:main
+      pyshop_shell = pyshop.bin.shell:main
+      """,
+      paster_plugins=['pyramid'],
+      data_files=data_files,
+      )
