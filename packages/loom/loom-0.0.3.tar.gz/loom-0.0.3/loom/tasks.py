1f28987c1407c631dc256bacd5824ca5d1f96c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package loom/0.0.3/loom-0.0.3.tar.gz

---
+++ packages/loom/loom-0.0.3.tar.gz/loom-0.0.3/loom/tasks.py
@@ -0,0 +1,53 @@
+from fabric.api import *
+import subprocess
+
+from .config import host_role, current_role
+
+__all__ = ['ssh', 'all', 'uptime', 'upgrade']
+
+@task
+def all():
+    """
+    Select all hosts
+    """
+    env.hosts = []
+    for hosts in env.roledefs.values():
+        env.hosts.extend(hosts)
+    # remove dupes
+    env.hosts = list(set(env.hosts))
+
+@task
+def uptime():
+    run('uptime')
+
+@task
+def upgrade():
+    """
+    Upgrade apt packages
+    """
+    with settings(hide('stdout'), show('running')):
+        sudo('apt-get update')
+    sudo("apt-get upgrade -y")
+
+@task
+def ssh(*cmd):
+    """
+    Open an interactive ssh session
+    """
+    run = ['ssh', '-A', '-t']
+    if env.key_filename:
+        run.extend(["-i", env.key_filename])
+    run.append('%s@%s' % (env.user, env.host_string))
+    run += cmd
+    subprocess.call(run)
+
+@task
+def restart(service):
+    """
+    Restart or start an upstart service
+    """
+    with settings(warn_only=True):
+        result = sudo('restart %s' % service)
+    if result.failed:
+        sudo('start %s' % service)
+
---
+++ packages/loom/loom-0.0.3.tar.gz/loom-0.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='loom',
+    version='0.0.3',
+    description='Elegant deployment with Fabric and Puppet.',
+    author='Ben Firshman',
+    author_email='ben@firshman.co.uk',
+    url='http://github.com/bfirsh/loom',
+    packages = ['loom'],
+    package_data = {'loom': ['files/init/*', 'files/puppet/*']},
+    install_requires = open('requirements.txt').readlines(),
+    #test_suite = 'nose.collector',
+)
+
