57ad37800eb137effb6d74b5a23c57faec06c47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package gnome-shell-search-github-repositories/1.0.0rc2/gnome-shell-search-github-repositories-1.0.0rc2.tar.gz

---
+++ packages/gnome-shell-search-github-repositories/gnome-shell-search-github-repositories-1.0.0rc2.tar.gz/gnome-shell-search-github-repositories-1.0.0rc2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requires = [
+    'requests',
+]
+
+setup(
+    name="gnome-shell-search-github-repositories",
+    version='1.0.0rc2',
+    description="A gnome shell search provider for your github repos",
+    url="http://github.com/ralphbean/gnome-shell-search-github-repositories",
+    author="Ralph Bean",
+    author_email="rbean@redhat.com",
+    license='GPLv3',
+    install_requires=requires,
+    packages=['gs_search_github_repositories'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'gnome-shell-search-github-repositories-daemon = gs_search_github_repositories.daemon:main',
+        ],
+    }
+)
