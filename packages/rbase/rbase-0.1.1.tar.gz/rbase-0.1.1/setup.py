fbe1656c3a148b2f971486f6df976f2477d0dca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:16 +0000

    Add package rbase/0.1.1/rbase-0.1.1.tar.gz

---
+++ packages/rbase/rbase-0.1.1.tar.gz/rbase-0.1.1/setup.py
@@ -0,0 +1,30 @@
+"""
+rbase
+-----
+
+A simple redis base class.
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rbase',
+    version='0.1.1',
+    url='http://github.com/jarodl/rbase',
+    license='MIT',
+    author='Jarod Luebbert',
+    author_email='jarodluebbert@gmail.com',
+    description='A simple redis base class',
+    packages=find_packages(),
+    long_description=__doc__,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['redis'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
