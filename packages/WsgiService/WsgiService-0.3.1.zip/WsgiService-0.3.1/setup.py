41b030ee9e27283c22b89b2804719dd258172f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package WsgiService/0.3.1/WsgiService-0.3.1.zip

---
+++ packages/WsgiService/WsgiService-0.3.1.zip/WsgiService-0.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+setup(
+    name='WsgiService',
+    # Version number also needs to be updated in wsgiservice/__init__.py
+    version='0.3.1',
+    description="A lean WSGI framework for easy creation of REST services",
+    long_description=open('README').read(),
+    author=", ".join([
+        "Patrice Neff <software@patrice.ch>",
+    ]),
+    url='http://github.com/pneff/wsgiservice/tree/master',
+    download_url='http://pypi.python.org/pypi/WsgiService',
+    packages=find_packages(),
+    install_requires=[
+        'decorator',
+        'webob >= 1.2b2',
+    ],
+    tests_require=[
+        'nose',
+        'mox',
+    ],
+    test_suite='nose.collector',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ]
+)
