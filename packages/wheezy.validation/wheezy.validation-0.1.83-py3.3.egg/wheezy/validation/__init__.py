2fbc99ecced728193554c30ca2e0f4a15ead7a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package wheezy.validation/0.1.83/wheezy.validation-0.1.83-py3.3.egg

---
+++ packages/wheezy.validation/wheezy.validation-0.1.83-py3.3.egg/wheezy/validation/__init__.py
@@ -0,0 +1,9 @@
+
+""" ``validation`` package.
+"""
+
+# flake8: noqa
+
+from wheezy.validation.mixin import ValidationMixin
+from wheezy.validation.model import try_update_model
+from wheezy.validation.validator import Validator
