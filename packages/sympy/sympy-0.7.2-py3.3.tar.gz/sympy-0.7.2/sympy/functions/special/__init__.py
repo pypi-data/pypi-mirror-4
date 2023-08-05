aa5532e7a76e92e8453788d808014fa42b7e362a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package sympy/0.7.2/sympy-0.7.2-py3.3.tar.gz

---
+++ packages/sympy/sympy-0.7.2-py3.3.tar.gz/sympy-0.7.2/sympy/functions/special/__init__.py
@@ -0,0 +1,7 @@
+from . import gamma_functions
+from . import error_functions
+from . import zeta_functions
+from . import tensor_functions
+from . import delta_functions
+
+from . import polynomials
