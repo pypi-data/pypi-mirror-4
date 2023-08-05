ddf663d0ff6ef665ce79dfa5205ea487d1039d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:49 +0000

    Add package django-pipeline/1.2.24/django-pipeline-1.2.24.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.2.24.tar.gz/django-pipeline-1.2.24/tests/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 flake8: noqa -*-
+from compiler import *
+from compressor import *
+from glob import *
+from jinja2 import *
+from packager import *
+from storage import *
+from utils import *
