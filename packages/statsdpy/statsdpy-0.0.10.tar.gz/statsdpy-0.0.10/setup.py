11c396d17a3c304a2dd4c59cd25e347e24615d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:36 +0000

    Add package statsdpy/0.0.10/statsdpy-0.0.10.tar.gz

---
+++ packages/statsdpy/statsdpy-0.0.10.tar.gz/statsdpy-0.0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from statsdpy import __version__ as version
+
+install_requires = []
+try:
+    import eventlet
+except ImportError:
+    install_requires.append("eventlet")
+
+name = "statsdpy"
+
+setup(
+    name = name,
+    version = version,
+    author = "Florian Hines",
+    author_email = "syn@ronin.io",
+    description = "Eventlet based statsd clone with pickle protocol support",
+    license = "Apache License, (2.0)",
+    keywords = "statsd",
+    url = "http://github.com/pandemicsyn/statsdpy",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Environment :: No Input/Output (Daemon)',
+        ],
+    install_requires=install_requires,
+    scripts=['bin/statsdpy-server']
+    )
+
