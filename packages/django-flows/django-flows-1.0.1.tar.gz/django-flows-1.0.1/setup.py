276a17d2962bf7736d735284d7be1c5f14fc2ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package django-flows/1.0.1/django-flows-1.0.1.tar.gz

---
+++ packages/django-flows/django-flows-1.0.1.tar.gz/django-flows-1.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+import time
+
+
+#_version = "1.0.dev%s" % int(time.time())
+_version = "1.0.1"
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    
+# common dependencies
+_install_requires = [
+            'django>=1.4',
+       ]
+
+setup( name='django-flows',
+       url='https://github.com/laterpay/django-flows',
+       version=_version,
+       packages=_packages,
+       install_requires=_install_requires,
+       scripts=[
+           # 'scripts/manage',
+       ],
+
+)
