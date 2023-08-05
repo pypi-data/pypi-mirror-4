bc4e0047e59d7dc995f4f704737fc265de368b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:26 +0000

    Add package Zope2/2.12.25/Zope2-2.12.25-py2.6-win-amd64.egg

---
+++ packages/Zope2/Zope2-2.12.25-py2.6-win-amd64.egg/Testing/ZopeTestCase/zopedoctest/__init__.py
@@ -0,0 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""ZopeTestCase doctest support
+
+$Id: __init__.py 111032 2010-04-18 23:34:27Z hannosch $
+"""
+
+from doctest import *
+from functional import *
