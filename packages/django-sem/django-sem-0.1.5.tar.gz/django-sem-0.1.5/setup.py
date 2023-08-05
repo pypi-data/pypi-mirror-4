63560c1d854d504592e19a9cf111684eea7ba379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package django-sem/0.1.5/django-sem-0.1.5.tar.gz

---
+++ packages/django-sem/django-sem-0.1.5.tar.gz/django-sem-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-sem',
+    version='0.1.5',
+    author='Jakub Wasielak',
+    author_email='kuba.wasielak@gmail.com',
+    packages=find_packages(),
+    scripts=[],
+    url='http://pypi.python.org/pypi/SignedEmailMessage/',
+    license='LICENSE.txt',
+    description='Useful tool for sending signed emails.',
+    long_description=open('README.txt').read(),
+    include_package_data=True,
+    install_requires=[
+        "Django >= 1.1.1",
+        "M2Crypto>=0.21.1",
+    ],
+)
