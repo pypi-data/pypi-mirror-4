405cc2e4f857724f4902166090b309473b67c4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package pkgme-devportal/0.4.9/pkgme-devportal-0.4.9.tar.gz

---
+++ packages/pkgme-devportal/pkgme-devportal-0.4.9.tar.gz/pkgme-devportal-0.4.9/devportalbinary/acceptance/data/python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='test',
+    version='1.0',
+    packages='somepackage',
+    maintainer='example',
+    maintainer_email='someone@example.com',
+    description="Some description",
+    license='GPLv3',
+    url='http://example.com',
+    )
---
+++ packages/pkgme-devportal/pkgme-devportal-0.4.9.tar.gz/pkgme-devportal-0.4.9/devportalbinary/db/patch-00001.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE libdep
+  ADD COLUMN architecture TEXT;
