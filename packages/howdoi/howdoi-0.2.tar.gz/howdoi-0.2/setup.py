78ee4ac0453bb834fc54b96731548affeefc8225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package howdoi/0.2/howdoi-0.2.tar.gz

---
+++ packages/howdoi/howdoi-0.2.tar.gz/howdoi-0.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import howdoi
+import os
+
+def read(*names):
+    values = dict()
+    extensions = ['.txt', '.rst']
+    for name in names:
+        value = ''
+        for extension in extensions:
+            filename = name + extension
+            if os.path.isfile(filename):
+                value = open(name + extension).read()
+                break
+        values[name] = value
+    return values
+
+long_description = """
+%(README)s
+
+News
+====
+
+%(CHANGES)s
+
+""" % read('README', 'CHANGES')
+
+setup(name='howdoi',
+      version=howdoi.__version__,
+      description='A code search tool',
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Documentation",
+        ],
+      keywords='howdoi help console',
+      author='Benjamin Gleitzman',
+      author_email='gleitz@mit.edu',
+      maintainer='Benjamin Gleitzman',
+      maintainer_email='gleitz@mit.edu',
+      url='https://github.com/gleitz/howdoi',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+        'console_scripts': [
+            'howdoi = howdoi.howdoi:command_line_runner',
+            ]
+        },
+      install_requires=[
+        'pyquery',
+        ],
+      )
