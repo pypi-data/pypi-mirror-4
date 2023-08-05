b8b4aea69eacefb9a2f45579b72d10997197dd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:30 +0000

    Add package django-thumbnail/11.12/django-thumbnail-11.12.tar.gz

---
+++ packages/django-thumbnail/django-thumbnail-11.12.tar.gz/django-thumbnail-11.12/setup.py
@@ -0,0 +1,37 @@
+import sorl
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+
+class TestCommand(test):
+    def run(self):
+        from tests.runtests import runtests
+        runtests()
+
+
+setup(
+    name='django-thumbnail',
+    version=sorl.__version__,
+    description='Thumbnailing in Django',
+    long_description=open('README.rst').read(),
+    author='Clinton Christian',
+    author_email='pygeek@me.com',
+    license='BSD',
+    url='https://github.com/pygeek/django-thumbnail',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics',
+        'Framework :: Django',
+    ],
+    cmdclass={"test": TestCommand},
+)
+
