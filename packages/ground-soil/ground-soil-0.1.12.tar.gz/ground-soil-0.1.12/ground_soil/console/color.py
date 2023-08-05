255e94f5564d8f862bfa5e055a4ce993a1d17361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:25 +0000

    Add package ground-soil/0.1.12/ground-soil-0.1.12.tar.gz

---
+++ packages/ground-soil/ground-soil-0.1.12.tar.gz/ground-soil-0.1.12/ground_soil/console/color.py
@@ -0,0 +1,34 @@
+class Colors(object):
+    BOLD = 1
+    DARK = 2
+    UNDERLINE = 4
+    BLINK = 5
+    REVERSE = 7
+    CONCEALED = 8
+    ###
+    GREY = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    WHITE = 37
+    ###
+    BG_GREY = 40
+    BG_RED = 41
+    BG_GREEN = 42
+    BG_YELLOW = 43
+    BG_BLUE = 44
+    BG_MAGENTA = 45
+    BG_CYAN = 46
+    BG_WHITE = 47
+
+    @staticmethod
+    def c(text, *attributes):
+        for attribute in attributes:
+            text = '\033[{attr:d}m'.format(attr=attribute) + text
+        return text + '\033[0m'
+
+
+color = Colors.c
