0753a8e4962a1fc556902c12402066e2ca715226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:36 +0000

    Add package Products.mediaPerson/0.1/Products.mediaPerson-0.1.tar.gz

---
+++ packages/Products.mediaPerson/Products.mediaPerson-0.1.tar.gz/Products.mediaPerson-0.1/Products/mediaPerson/interfaces/mediaperson.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class IMediaPerson(Interface):
+    """sPerson with media such as video and images"""
+
+    # -*- schema definition goes here -*-
