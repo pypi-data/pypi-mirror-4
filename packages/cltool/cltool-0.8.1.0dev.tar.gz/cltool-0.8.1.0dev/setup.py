620dc7ea190d1c26c2f42f53cd23cfb81f791e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package cltool/0.8.1.0dev/cltool-0.8.1.0dev.tar.gz

---
+++ packages/cltool/cltool-0.8.1.0dev.tar.gz/cltool-0.8.1.0dev/setup.py
@@ -0,0 +1,31 @@
+from __future__ import ( print_function, with_statement, absolute_import )
+import sys
+# When possible, use distribute_setup's version of tools
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+except ImportError:
+    from setuptools import setup, find_packages
+if sys.version >= '2.3':
+  import re, os, sys
+  version = re.search(
+        "__version__.*'(.+)'",
+        open(os.path.join('cltool','__init__.py')).read()).group(1)
+else:
+    raise Exception("only python 2.3 or newer supported")
+
+setup(
+    name='cltool',
+    version= version,
+    description='Example of installing a comand line tool via pip/easy_install',
+    author='Far McKon',
+    author_email='FarMcKon@gmail.com',
+    packages=find_packages(where='.'),
+    include_package_data=True,
+    zip_safe=False,
+    #scripts=['scripts/cltool','scripts/cltool2',],
+    entry_points={
+        'console_scripts': [ 'cltool4= cltool:command_line_runner',]  },
+)
+   
