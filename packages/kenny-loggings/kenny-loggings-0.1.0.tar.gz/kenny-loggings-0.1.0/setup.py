40637f6439cb8ea951c3689f5c8207ad1385c004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package kenny-loggings/0.1.0/kenny-loggings-0.1.0.tar.gz

---
+++ packages/kenny-loggings/kenny-loggings-0.1.0.tar.gz/kenny-loggings-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="kenny-loggings",
+    version="0.1.0",
+    description='',
+    long_description='',
+    keywords="python, django, logging",
+    author="Chris Jones <chris@brack3t.com>, Kenneth Love <kenneth@brack3t.com>",
+    author_email="chris@brack3t.com",
+    url="https://github.com/brack3t/kenny-loggings",
+    license="BSD",
+    packages=["loggings"],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: Web Environment",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers"
+    ],
+)
