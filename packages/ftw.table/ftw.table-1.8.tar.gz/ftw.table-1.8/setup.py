3f955829183fdfbdca634ad23952184eade51031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package ftw.table/1.8/ftw.table-1.8.tar.gz

---
+++ packages/ftw.table/ftw.table-1.8.tar.gz/ftw.table-1.8/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.8'
+maintainer = 'Jonas Baumann'
+
+tests_require = [
+    'plone.app.testing',
+    'plone.mocktestcase',
+    ]
+
+setup(name='ftw.table',
+      version=version,
+      description='Table generator utility for use within zope.',
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='ftw table generator',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.table',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'five.globalrequest',
+        # -*- Extra requirements: -*-
+        ],
+
+      tests_require=tests_require,
+      extras_require={
+        'extjs': ['collective.js.extjs'],
+        'tests': tests_require},
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
