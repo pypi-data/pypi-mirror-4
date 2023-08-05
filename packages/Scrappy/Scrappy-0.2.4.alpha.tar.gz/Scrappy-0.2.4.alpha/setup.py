aaa30bd549bdfb7bddc89db5e4bc9f081b4ed69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package Scrappy/0.2.4.alpha/Scrappy-0.2.4.alpha.tar.gz

---
+++ packages/Scrappy/Scrappy-0.2.4.alpha.tar.gz/Scrappy-0.2.4.alpha/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+from scrappy.core import __version__
+
+setup(
+    name='Scrappy',
+    version=__version__,
+    author='Louis Thibault',
+    author_email='louist87@gmail.com',
+    packages=['scrappy'],
+    include_package_data=True,
+    setup_requires='ez_setup',
+    install_requires=['guessit', 'tvdb_api', 'ez_setup', 'titlecase', 'hachoir-metadata', 'hachoir-core', 'hachoir-parser'],
+    url='https://github.com/louist87/scrappy',
+    license='GPL 3.0',
+    description='Rename video files based on information scraped from thetvdb.com',
+    keywords=["TVDB", "thetvdb", "rename", "broadcatching", "media"],
+    long_description=open('README.rst').read()
+)
