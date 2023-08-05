ec5cfd3b2610e5f64c83e54eb758a30560e60621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package riak-docs/0.1.2/riak-docs-0.1.2.zip

---
+++ packages/riak-docs/riak-docs-0.1.2.zip/riak-docs-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riak-docs',
+    version='0.1.2',
+    description='Models to facilitate some common interactions with Riak documents.',
+    author='Dan Ostrowski',
+    author_email='dan.ostrowski@gmail.com',
+    license='BSD',
+    keywords='riak models object',
+    install_requires=['riak', 'blinker'],
+    packages=find_packages(),
+    url='https://bitbucket.org/danostrowski/riakdocs',
+    classifiers=[
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
