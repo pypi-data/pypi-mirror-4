4efa93d0627994064dc1987648c167609c09411d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:25 +0000

    Add package django-federated-login/0.3.1/django-federated-login-0.3.1.tar.gz

---
+++ packages/django-federated-login/django-federated-login-0.3.1.tar.gz/django-federated-login-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-federated-login',
+    version='0.3.1',
+    author='Bouke Haarsma',
+    author_email='bouke@webatoom.nl',
+    packages=find_packages(),
+    url='http://github.com/Bouke/django-federated-login',
+    description='Provides federated logins to django projects',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'python-openid == 2.2.5',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+)
