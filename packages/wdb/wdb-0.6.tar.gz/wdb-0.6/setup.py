ddaa7716bf1877603dae547ad8691d1321613f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package wdb/0.6/wdb-0.6.tar.gz

---
+++ packages/wdb/wdb-0.6.tar.gz/wdb-0.6/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wdb
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = "0.6"
+
+
+options = dict(
+    name="wdb",
+    version=VERSION,
+    description="An improbable web debugger through WebSockets",
+    long_description="See http://github.com/Kozea/wdb",
+    author="Florian Mounier @ kozea",
+    author_email="florian.mounier@kozea.fr",
+    url="http://github.com/Kozea/wdb",
+    license="GPLv3",
+    platforms="Any",
+    packages=find_packages(),
+    install_requires=["log_colorizer", ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Debuggers"])
+
+setup(**options)
