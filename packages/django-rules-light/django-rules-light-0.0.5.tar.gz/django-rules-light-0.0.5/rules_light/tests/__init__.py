e68afb932f986e7a832a4d22aa775e0e210fca7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package django-rules-light/0.0.5/django-rules-light-0.0.5.tar.gz

---
+++ packages/django-rules-light/django-rules-light-0.0.5.tar.gz/django-rules-light-0.0.5/rules_light/tests/__init__.py
@@ -0,0 +1,5 @@
+from registry import RegistryTestCase
+from class_decorator import ClassDecoratorTestCase
+from autodiscover import AutodiscoverTestCase
+from views import ViewsTestCase
+from decorators import DecoratorsTestCase
