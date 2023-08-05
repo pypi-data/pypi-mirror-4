bfe9dc4921757c74031eddc2115f0f40177c766d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package GifTiffLoader/0.1.4/GifTiffLoader-0.1.4.tar.gz

---
+++ packages/GifTiffLoader/GifTiffLoader-0.1.4.tar.gz/GifTiffLoader-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='GifTiffLoader',
+    version='0.1.4',
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['GifTiffLoader'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/GifTiffLoader/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.rst').read(),
+    install_requires=[
+         #'wxPython>=2.6', # wxPython isn't being found correctly by setuptools -- please install it manually!
+         "numpy >= 0.9",
+         "PIL >= 1.1.5",
+    ],
+)
