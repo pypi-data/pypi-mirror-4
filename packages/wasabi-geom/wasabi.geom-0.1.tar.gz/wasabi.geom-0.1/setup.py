c1a72d78206d120c3ebb1da2ec3ef30eeabfa855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package wasabi-geom/0.1/wasabi.geom-0.1.tar.gz

---
+++ packages/wasabi-geom/wasabi.geom-0.1.tar.gz/wasabi.geom-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wasabi.geom',
+    version='0.1',
+    author='Daniel Pope',
+    author_email='mauve@mauveweb.co.uk',
+    url='https://bitbucket.org/lordmauve/wasabi-geom',
+    packages=find_packages(),
+    namespace_packages=['wasabi']
+)
