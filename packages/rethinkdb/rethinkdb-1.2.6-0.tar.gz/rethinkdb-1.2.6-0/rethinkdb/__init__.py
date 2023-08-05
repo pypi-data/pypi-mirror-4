131c0c88b88bfba9c12156c672f6a1e936a0030f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:30 +0000

    Add package rethinkdb/1.2.6-0/rethinkdb-1.2.6-0.tar.gz

---
+++ packages/rethinkdb/rethinkdb-1.2.6-0.tar.gz/rethinkdb-1.2.6-0/rethinkdb/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2010-2012 RethinkDB, all rights reserved.
+"""This package implements ReQL - a domain specific language for
+generation of RethinkDB queries embedded into Python. ReQL can be used
+to query and update the database. It is designed to be very expressive
+(i.e. to support advanced operations such as subqueries and table
+joins), as well as to be user-friendly and easy to use.
+
+ReQL is inspired by jQuery and SQLAlchemy. The jQuery style of
+programming allows queries to be chained to form more complex queries
+in an intuitive way. Following SQLAlchemy conventions, as many of
+Python's facilities as possible have been overloaded in order to make
+expressing queries friendly and convenient."""
+
+__all__ = [
+    'connect', 'last_connection',
+    'db_create', 'db_drop', 'db_list', 'db', 'table', 'error',
+    'expr', 'r', 'union', 'js', 'let', 'letvar', 'branch',
+    'ExecutionError', 'BadQueryError']
+
+from query import *
+from net import connect, last_connection, ExecutionError, BadQueryError
+
+# Package initialization
