1b82fcbb4b5bbbc005cd6e345335f12a70e2d1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:00 +0000

    Add package pyproxmox/0.9.0/pyproxmox-0.9.0.tar.gz

---
+++ packages/pyproxmox/pyproxmox-0.9.0.tar.gz/pyproxmox-0.9.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+	name = 'pyproxmox',
+	packages = ['pyproxmox'],
+	version = '0.9.0',
+	description = 'Python Wrapper for the Proxmox 2.x API',
+	author = 'Toby Sears',
+	author_email = 'admin@tobysears.co.uk',
+	url = 'enter_project_url',
+	download_url = 'enter_download_url',
+	keywords = ['proxmox','api'],
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.7",
+		"Intended Audience :: Developers",
+		"Intended Audience :: System Administrators",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+	        "Operating System :: OS Independent",
+       		"Topic :: Software Development :: Libraries :: Python Modules",
+		],
+	long_description = """enter_description"""
+)
