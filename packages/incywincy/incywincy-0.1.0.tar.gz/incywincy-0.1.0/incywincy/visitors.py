f48a9fc84a1162e45abed4d1a9dee026d7e99c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package incywincy/0.1.0/incywincy-0.1.0.tar.gz

---
+++ packages/incywincy/incywincy-0.1.0.tar.gz/incywincy-0.1.0/incywincy/visitors.py
@@ -0,0 +1,18 @@
+from incywincy.config import settings
+
+
+broken = []
+
+
+def http_errors(page):
+    status = page.status_code
+    if status >= 400:
+        print('Status [{0}] for URL: {1}'.format(status, page))
+        broken.append(page)
+
+
+def absolute_links(page):
+    for link in page.links:
+        if link.startswith(settings.root):
+            print('Found absolute internal URL [{0}] in: {1}'
+                  .format(link, page.url))
