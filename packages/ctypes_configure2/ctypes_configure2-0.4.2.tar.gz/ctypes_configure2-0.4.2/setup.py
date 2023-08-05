bc1f827e7e76d887019e1523123b0cfa7fee3623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package ctypes_configure2/0.4.2/ctypes_configure2-0.4.2.tar.gz

---
+++ packages/ctypes_configure2/ctypes_configure2-0.4.2.tar.gz/ctypes_configure2-0.4.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ctypes_configure2',
+      version='0.4.2',
+      description='Ctypes Configuration',
+      author='Maciej Fijalkowski',
+      author_email='fijall@gmail.com',
+      maintainer='Alexey Palazhchenko',
+      maintainer_email='alexey.palazhchenko@gmail.com',
+      url='http://codespeak.net/~fijal/configure.html',
+      packages=['ctypes_configure', 'ctypes_configure/test'],
+      license='MIT',
+     )
