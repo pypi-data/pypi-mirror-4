63ea354939f8bea66bd247bd0cd62762f3890bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package ypassbook/0.1.0/ypassbook-0.1.0.tar.gz

---
+++ packages/ypassbook/ypassbook-0.1.0.tar.gz/ypassbook-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name = "ypassbook",
+    packages = ["ypassbook"],
+    version = "0.1.0",
+    description = "Create an Apple passbook",
+    author = "Yang Hongbo",
+    author_email = "hongbo@yang.me",
+    url = "http://pypi.python.org/pypi/ypassbook",
+    requires = ["M2Crypto"], 
+    keywords = ["apple", "passbook", "ios"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta", 
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities"
+        ],
+long_description = """\
+ypassbook - An Apple Passbook pass generator
+--------------------------------------------
+
+http://github.com/sprhawk/Passbook
+"""
+    )
