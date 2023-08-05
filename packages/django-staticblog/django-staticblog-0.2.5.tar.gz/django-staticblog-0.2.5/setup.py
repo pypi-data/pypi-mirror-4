2409459f0e2acec1d2da4def335dd25f6d30868e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package django-staticblog/0.2.5/django-staticblog-0.2.5.tar.gz

---
+++ packages/django-staticblog/django-staticblog-0.2.5.tar.gz/django-staticblog-0.2.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import staticblog
+
+setup(
+    name = 'django-staticblog',
+    version = staticblog.__version__,
+    packages = find_packages(),
+    long_description=open('README.rst', 'rt').read(),
+    install_requires = ['Markdown >= 2.0', 'html2text >= 3.0'],
+    author = 'Chris Grice',
+    author_email = 'chris@chrisgrice.com',
+    license = 'MIT',
+    description = 'Markdown-based blog engine that compiles to static html pages',
+    url='http://github.com/cgrice/django-staticblog/',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe = False,
+    package_data = { 'staticblog' : ['templates/staticblog/*.html']},
+    include_package_data = True,
+)
