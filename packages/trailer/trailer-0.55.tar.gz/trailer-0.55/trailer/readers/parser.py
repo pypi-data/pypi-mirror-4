021d6f3ad0155511dbfd56ad72745960d3aedf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:26 +0000

    Add package trailer/0.55/trailer-0.55.tar.gz

---
+++ packages/trailer/trailer-0.55.tar.gz/trailer-0.55/trailer/readers/parser.py
@@ -0,0 +1,50 @@
+from lxml import etree
+
+from trailer.readers.gpx_1_0.parser import parse_gpx as parse_gpx_1_0
+from trailer.readers.gpx_1_1.parser import parse_gpx as parse_gpx_1_1
+
+def parse_gpx(xml):
+    """Parse a GPX file into a GpxModel.
+
+    Args:
+        xml: A file-like-object opened in binary mode - that is containing
+             bytes rather than characters. The root element of the XML should
+             be a <gpx> element containing a version attribute. GPX versions
+             1.0 and 1.1 are supported.
+
+    Returns:
+        A GpxModel representing the data from the supplies xml.
+
+    Raises:
+        ValueError: The supplied XML could not be parsed as GPX.
+    """
+    tree = etree.parse(xml)
+    gpx_element = tree.getroot()
+    gpxns = '{' + gpx_element.nsmap.get(None, '') + '}'
+    if not gpxns.startswith('{http://www.topografix.com/GPX'):
+        raise ValueError("Unrecognised GPX namespace '{0}'".format(gpxns))
+
+    if gpx_element.tag != gpxns+'gpx':
+        raise ValueError("No gpx root element")
+
+    version = gpx_element.attrib['version']
+
+    xml.seek(0)
+
+    if version == '1.0':
+        return parse_gpx_1_0(xml)
+    elif version == '1.1':
+        return parse_gpx_1_1(xml)
+    else:
+        raise ValueError("Cannot parse GPX version {0}".format(version))
+
+
+def main():
+    with open('/Users/rjs/dev/trailer/data/culra.gpx', 'rb') as xml:
+        gpx_model = parse_gpx(xml)
+        return gpx_model
+
+
+if __name__ == '__main__':
+    gpx_model = main()
+    pass
