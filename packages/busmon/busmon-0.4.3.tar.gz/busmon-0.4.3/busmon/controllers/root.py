3f80163eb00e8bb7ee9801898a4b269f7cd75b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package busmon/0.4.3/busmon-0.4.3.tar.gz

---
+++ packages/busmon/busmon-0.4.3.tar.gz/busmon-0.4.3/busmon/controllers/root.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Main Controller"""
+
+from tg import expose
+
+from busmon.lib.base import BaseController
+from busmon.controllers.error import ErrorController
+
+from busmon.widgets import (
+    TopicsBarChart,
+    MessagesTimeSeries,
+    ColorizedMessagesWidget,
+)
+
+__all__ = ['RootController']
+
+
+class RootController(BaseController):
+    error = ErrorController()
+
+    @expose('busmon.templates.index')
+    def index(self):
+        """Handle the front-page."""
+        return dict(
+            barchart=TopicsBarChart,
+            timeseries=MessagesTimeSeries(id='messages-time-series'),
+            colorized_messages=ColorizedMessagesWidget,
+        )
