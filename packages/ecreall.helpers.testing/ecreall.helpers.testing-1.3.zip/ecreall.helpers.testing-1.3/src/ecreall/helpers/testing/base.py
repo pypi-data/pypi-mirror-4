6bc717393c54e027720da897b3c51d712a8d7146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:23 +0000

    Add package ecreall.helpers.testing/1.3/ecreall.helpers.testing-1.3.zip

---
+++ packages/ecreall.helpers.testing/ecreall.helpers.testing-1.3.zip/ecreall.helpers.testing-1.3/src/ecreall/helpers/testing/base.py
@@ -0,0 +1,16 @@
+from Products.CMFCore.utils import getToolByName
+
+from plone.app.testing import login
+
+class BaseTest(object):
+    """A class with many useful helpers
+    """
+
+    def login(self, user):
+        login(self.layer['portal'], user)
+
+    def assertLengthEqual(self, list1, length, msg=None):
+        self.assertEqual(len(list1), length, msg)
+
+    def assertContainsSame(self, list1, list2, msg=None, key=None):
+        return self.assertListEqual(sorted(list1, key=key), sorted(list2, key=key), msg)
