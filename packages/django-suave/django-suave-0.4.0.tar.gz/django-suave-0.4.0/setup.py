560dd1b5087bf2351f751bf2d13223673eb67f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:26 +0000

    Add package django-suave/0.4.0/django-suave-0.4.0.tar.gz

---
+++ packages/django-suave/django-suave-0.4.0.tar.gz/django-suave-0.4.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-suave',
+    version="0.4.0",
+    description='Rather nice pages.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/radiosilence/django-suave',
+    author='James Cleveland',
+    author_email='jamescleveland@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license="LICENSE.txt",
+    install_requires=open('requirements.txt').read().split("\n")
+)
