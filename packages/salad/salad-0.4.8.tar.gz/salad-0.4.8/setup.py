5b6abd4296b8c3377573683592afcd53e09aac38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:12 +0000

    Add package salad/0.4.8/salad-0.4.8.tar.gz

---
+++ packages/salad/salad-0.4.8.tar.gz/salad-0.4.8/setup.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+setup(
+    name="salad",
+    description="A nice mix of great BDD ingredients",
+    author="Steven Skoczen",
+    author_email="steven.skoczen@wk.com",
+    url="https://github.com/wieden-kennedy/salad",
+    version="0.4.8",
+    install_requires=["lettuce", "nose", "splinter", "zope.testbrowser"],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
