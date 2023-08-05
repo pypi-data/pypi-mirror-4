8e1315f86b039265e2fdf5d46ef6426682ff82a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package pyzolib/0.2.1/pyzolib-0.2.1.tar.gz

---
+++ packages/pyzolib/pyzolib-0.2.1.tar.gz/pyzolib-0.2.1/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012, The Pyzo team
+#
+# This file is distributed under the terms of the (new) BSD License.
+
+""" Package pyzolib
+
+The pyzolib package provides basic functionality for the Pyzo environment.
+It contains a collection of modules and small packages that should be
+imported as "from pyzolib import xxx"
+
+The packages currently are:
+  * path - object oriented path processing (no more os.path.x)
+  * paths - Get paths to useful directories in a cross platform manner.
+  * ssdf - the Simple Structured Data Format (for config files and 
+    scientific databases)
+  * insertdocs - a sphynx pre-processor to include docstrings in the text,
+    allowing readthedocs.org to host the docs without requiring importing code.
+  * pyximport - for easy on the fly compilation of Cython, using the Pyzo 
+    environment to establish the location of a gcc compiler.
+  * gccutils - used by the above to manage the gcc compiler.
+  * interprerers - list the Python interpreters available on this system.
+
+"""
+
+__version__ = '0.2.1'
