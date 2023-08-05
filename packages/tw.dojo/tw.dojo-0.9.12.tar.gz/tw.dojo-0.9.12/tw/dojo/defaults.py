b38b88a063b5035b766516beb028adac2db98fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package tw.dojo/0.9.12/tw.dojo-0.9.12.tar.gz

---
+++ packages/tw.dojo/tw.dojo-0.9.12.tar.gz/tw.dojo-0.9.12/tw/dojo/defaults.py
@@ -0,0 +1,6 @@
+__DOJO_VERSION__ = '1.8.1'
+__DEFAULT_LINK_IS_EXTERNAL__ = False
+__DOJO_URL_BASE__ = 'http://ajax.googleapis.com/ajax/libs/dojo/1.8.1'
+__DEFAULT_PREFIX__ = 'min'
+#uncomment this if you want to debug dojo itself (sometimes useful)
+#__DEFAULT_PREFIX__ = 'uncompressed.js'
