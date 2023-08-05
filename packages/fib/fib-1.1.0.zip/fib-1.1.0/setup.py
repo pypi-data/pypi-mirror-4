6f530b738d81bf1801f3ea404d4c68669c10a315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package fib/1.1.0/fib-1.1.0.zip

---
+++ packages/fib/fib-1.1.0.zip/fib-1.1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='fib',
+      version='1.1.0',
+      description='a GUI for fibnacci sequence ',
+      author='guzhen',
+      author_email='604608916@qq.com'
+      )
