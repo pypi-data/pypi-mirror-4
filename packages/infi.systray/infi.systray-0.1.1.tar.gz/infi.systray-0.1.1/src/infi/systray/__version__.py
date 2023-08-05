579aae34b8fe3a60f64662d902735d7a807ff491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package infi.systray/0.1.1/infi.systray-0.1.1.tar.gz

---
+++ packages/infi.systray/infi.systray-0.1.1.tar.gz/infi.systray-0.1.1/src/infi/systray/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '521f3e7af607b2962f162d54eb416bb68429247b'
+__git_head_subject__ = 'Finished release v0.1.1.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
