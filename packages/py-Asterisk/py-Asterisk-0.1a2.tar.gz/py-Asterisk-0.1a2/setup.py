17b90e28d021ed53eeb37001af532dd9bce2fff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package py-Asterisk/0.1a2/py-Asterisk-0.1a2.tar.gz

---
+++ packages/py-Asterisk/py-Asterisk-0.1a2.tar.gz/py-Asterisk-0.1a2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.3
+
+'''
+py-Asterisk distutils script.
+'''
+
+__author__ = 'David M. Wilson <dw-py-Asterisk-setup.py@botanicus.net>'
+__id__ = '$Id: setup.py 79 2004-09-24 21:44:39Z dw $'
+
+from distutils.core import setup
+
+
+
+
+setup(
+    name =          'py-Asterisk',
+	version =       '0.1a2',
+	description =   'Asterisk Manager API Python interface.',
+	author =        'David M. Wilson',
+	author_email =  'dw-py-Asterisk-setup.py@botanicus.net',
+	license =       'LGPL',
+	url =           'http://botanicus.net/dw/',
+	download_url =  'http://botanicus.net/dw/py-asterisk.php',
+	packages =      [ 'Asterisk' ],
+	scripts =       [ 'asterisk-dump', 'py-asterisk' ],
+    data_files =    [ ('/etc/asterisk', ['conf/py-asterisk.conf.sample']) ]
+)
