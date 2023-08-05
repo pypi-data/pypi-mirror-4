e1de6d5abb96e078cf8ec3da106cc6497158fd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package ffs/0.0.7/ffs-0.0.7.tar.gz

---
+++ packages/ffs/ffs-0.0.7.tar.gz/ffs-0.0.7/ffs/exceptions.py
@@ -0,0 +1,25 @@
+"""
+ffs.exceptions
+
+Base and definitions for all exceptions raised by FFS
+"""
+class Error(Exception):
+    "Base Error class for FFS"
+
+class DoesNotExistError(Error):
+    "Something should have been here"
+
+class ExistsError(Error):
+    "Something already exisis"
+
+class InappropriateError(Error):
+    "Someone did something inappropriate"
+
+class NonsenseError(Error):
+    "We have encountered a nonsense scenario. Bail."
+
+class NotRaisedError(Error):
+    "There is never a good time to raise this"
+
+class BadParentingError(Error):
+    "We expected the parents to be there, but no, just a dangling child."
