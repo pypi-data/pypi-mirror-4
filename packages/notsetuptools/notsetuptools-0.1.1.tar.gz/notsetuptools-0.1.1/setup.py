472a9cc4b2b015fba9b1d208543131ba0e36565f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package notsetuptools/0.1.1/notsetuptools-0.1.1.tar.gz

---
+++ packages/notsetuptools/notsetuptools-0.1.1.tar.gz/notsetuptools-0.1.1/setup.py
@@ -0,0 +1,27 @@
+"""
+notsetuptools
+=============
+
+This package attempts to correct the namespace hacks provided
+by setuptools so that they're actually useable.
+
+>>> from setuptools import find_packages
+>>> from notsetuptools import setup
+>>> setup(
+>>>     namespace_packages=['package'],
+>>>     # ...
+>>> )
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='notsetuptools',
+    version='0.1.1',
+    description='A hack to fix setuptools namespace packages hack.',
+    long_description=__doc__,
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+)
