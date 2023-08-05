1d1e16e64322516c9569ef677f4ea3d27a7d3a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package corset/0.0.3/corset-0.0.3.tar.gz

---
+++ packages/corset/corset-0.0.3.tar.gz/corset-0.0.3/corset/management/commands/corset.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.template import Context, Template
+from django.conf import settings
+import requests, os, glob
+
+CLOSURE_URL = 'http://closure-compiler.appspot.com/compile'
+payload = {'compilation_level': 'SIMPLE_OPTIMIZATIONS', 'output_format': 'json', 'output_info': 'compiled_code'}
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        JS_DIRS = []
+        for root, dirs, files in os.walk(settings.PROJECT_ROOT):
+            for d in dirs:
+                if d == "js":
+                    path = os.path.join(root, d)
+                    JS_DIRS.append(path)
+                    for d in path:
+                        src = os.path.join(d, "src")
+                        build = os.path.join(d, "build")
+                        for f in glob.glob(os.path.join(src, '*.js')):
+                            payload['js_code'] = open(os.path.join(src, f), 'r').read()
+                            r = requests.post(CLOSURE_URL, data = payload)
+                            compiled_code = r.json['compiledCode']
+                            build_file = open("%s.min.js" % os.path.join(build, os.path.splitext(os.path.basename(f))[0]), 'w')
+                            build_file.write(compiled_code)
+                            build_file.close()
