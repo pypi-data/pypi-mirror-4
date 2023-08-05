a43d63629dc4084e3382693c95f0224ed9f95528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package mysqlrocket/0.0.1/mysqlrocket-0.0.1.tar.gz

---
+++ packages/mysqlrocket/mysqlrocket-0.0.1.tar.gz/mysqlrocket-0.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import mysqlrocket
+ 
+setup(
+
+    name='mysqlrocket',
+    version='0.0.1',
+    packages=find_packages(),
+    author="Cyprien Devillez",
+    author_email="cyp@bidouille.info",
+    description="Simple CLI tool to create, delete and dump easily MySQL databases",
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    url='https://github.com/cypx/mysqlrocket',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database",
+        "Topic :: Utilities",
+        "Development Status :: 3 - Alpha",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'mysqlrocket = mysqlrocket.launcher:launcher',
+        ],
+    },
+ 
+    # A fournir uniquement si votre licence n'est pas listée dans "classifiers"
+    # ce qui est notre cas
+    license="GPL",
+ 
+    # Il y a encore une chiée de paramètres possibles, mais avec ça vous
+    # couvrez 90% des besoins
+ 
+) 
