fc2d0af0bba41a52a3e53d43d82bd364b9663c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:07 +0000

    Add package medialog.newsitemview/0.5.2/medialog.newsitemview-0.5.2.zip

---
+++ packages/medialog.newsitemview/medialog.newsitemview-0.5.2.zip/medialog.newsitemview-0.5.2/medialog/newsitemview/vocabulary.py
@@ -0,0 +1,21 @@
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility 
+from zope.schema.interfaces import IVocabularyFactory 
+from zope.app.component.hooks import getSite
+#from zope.site.hooks import setSite
+
+
+def ImageSizeVocabulary(object):
+    return ['thumb', 'mini', 'preview', 'large', 'none']  
+       
+
+        
+
+
+
+
+        
+
+
+
