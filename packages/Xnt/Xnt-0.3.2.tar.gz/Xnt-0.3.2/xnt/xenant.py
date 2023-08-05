c22b51cfb2c874410c273171be15b3ba2f9bb4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package Xnt/0.3.2/Xnt-0.3.2.tar.gz

---
+++ packages/Xnt/Xnt-0.3.2.tar.gz/Xnt-0.3.2/xnt/xenant.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+#   Xnt -- A Wrapper Build Tool
+#   Copyright (C) 2012  Kenny Ballou
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import time
+import logging
+
+logging.basicConfig(format="%(asctime)s:%(levelname)s:%(message)s")
+logger = logging.Logger(name=__name__)
+logger.addHandler(logging.StreamHandler())
+
+def versionAction():
+    printVersion()
+    sys.exit(0)
+
+def verboseAction():
+    logging.getLogger("xnt.tasks").setLevel(logging.INFO)
+
+actions = {
+    "--version": versionAction,
+    "-v"     : verboseAction,
+}
+
+def main():
+    start_time = time.time()
+    opts = list(o for o in sys.argv[1:] if o.startswith('-'))
+    arg = list(a for a in sys.argv[1:] if a not in opts)
+    for opt in opts:
+        if opt in actions:
+            actions[opt]()
+        else:
+            logger.debug("%s is not a valid option", opt)
+    invokeBuild(__loadBuild(), arg[0] if len(arg) == 1 else "default")
+    from xnt.tasks import rm
+    rm("build.pyc",
+       "__pycache__")
+    elapsed_time = time.time() - start_time
+    logger.info("Execution time: %.3f", elapsed_time)
+
+def invokeBuild(build, targetName):
+    if targetName == "help":
+        return printTargets(build)
+    try:
+        target = getattr(build, targetName)
+        target()
+    except AttributeError:
+        logger.warning("There was no target: %s", targetName)
+    except:
+        logger.error(sys.exc_info()[1].message)
+
+def printVersion():
+    import xnt
+    print(xnt.__version__)
+
+def printTargets(build):
+    printVersion()
+    print("\n")
+    try:
+        for f in dir(build):
+            try:
+                fa = getattr(build, f)
+                if fa.decorator == "target":
+                    print(f + ":")
+                    if fa.__doc__:
+                        print(fa.__doc__)
+                    print("\n")
+            except AttributeError:
+                pass
+    except:
+        logger.error(sys.exc_info()[1].message)
+
+def __loadBuild(path=""):
+    if not path:
+        path = os.getcwd()
+    else:
+        path = os.path.abspath(path)
+    sys.path.append(path)
+    cwd = os.getcwd()
+    os.chdir(path)
+    if not os.path.exists("build.py"):
+        logger.error("There was no build file")
+        sys.exit(1)
+    try:
+        return __import__("build", fromlist=[])
+    except ImportError:
+        logger.error("HOW?!")
+        return None
+    finally:
+        sys.path.remove(path)
+        del sys.modules["build"]
+        os.chdir(cwd)
+
+if __name__ == "__main__":
+    main()
