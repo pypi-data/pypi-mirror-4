2ebc68809ebe773641f1888b6f546026d26cb088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package Spiff/0.1/Spiff-0.1.tar.gz

---
+++ packages/Spiff/Spiff-0.1.tar.gz/Spiff-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='Spiff',
+    version='0.1',
+    description="API to Spaceman Spiff",
+    author='Trever Fischer',
+    author_email='wm161@wm161.net',
+    url='http://github.com/synhak/spiff',
+    py_modules=['spiff']
+)
