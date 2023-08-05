71cbaec947494d8104a9cef8bab1ad3d3d072f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:07 +0000

    Add package httpexceptor/1.1.1/httpexceptor-1.1.1.tar.gz

---
+++ packages/httpexceptor/httpexceptor-1.1.1.tar.gz/httpexceptor-1.1.1/httpexceptor/__init__.py
@@ -0,0 +1,148 @@
+"""
+lightweight WSGI middleware to handle common HTTP responses using exceptions
+
+provides a group of exception classes representing non-2xx HTTP statuses, along
+with a WSGI middleware to turn the exceptions into proper HTTP headers
+
+originally extracted from [TiddlyWeb](http://tiddlyweb.com)
+"""
+
+import sys
+import traceback
+import logging
+
+
+__version__ = '1.1.1'
+__author__ = 'Chris Dent'
+__copyright__ = 'Copyright UnaMesa Association 2012'
+__contributors__ = ['FND']
+__license__ = 'BSD'
+
+
+class HTTPExceptor(object):
+    """
+    WSGI middleware to trap exceptions, turning them into the corresponding
+    HTTP response
+    """
+
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response, exc_info=None):
+        try:
+            return self.application(environ, start_response)
+        except HTTPException, exc:
+            # read status code from exception class's docstring
+            start_response(exc.status, exc.headers(), exc_info)
+            return exc.body()
+        except:
+            exc_info = sys.exc_info()
+            exception_text = ''.join(traceback.format_exception(*exc_info))
+
+            # use the web server's and the application's logging mechanisms
+            print >> environ['wsgi.errors'], exception_text
+            logging.warn(exception_text)
+
+            start_response('500 Internal Server Error',
+                    [('Content-Type', 'text/plain; charset=UTF-8')], exc_info)
+            return [exception_text]
+
+
+class HTTPException(Exception):
+    """
+    base class of an HTTP exception
+    """
+
+    status = None
+
+    def headers(self):
+        return [('Content-Type', 'text/plain; charset=UTF-8')]
+
+    def body(self):
+        if not hasattr(self, 'args'):
+            self.args = ('%s' % self,)
+        output = []
+        for arg in self.args:
+            if isinstance(arg, unicode):
+                arg = arg.encode('utf-8')
+            output.append('%s' % arg)
+        status_code = self.status.split(" ")[0]
+        return ['%s: %s' % (status_code, ' '.join(output))]
+
+
+class HTTP302(HTTPException):
+    """302 Found"""
+
+    status = __doc__
+
+    def headers(self):
+        return [('Location', '%s' % self)]
+
+    def body(self):
+        return ['']
+
+
+class HTTP303(HTTP302):
+    """303 See Other"""
+
+    status = __doc__
+
+
+class HTTP304(HTTPException):
+    """304 Not Modified"""
+
+    status = __doc__
+
+    def headers(self):
+        return [('ETag', '%s' % self)]
+
+    def body(self):
+        return ['']
+
+
+class HTTP400(HTTPException):
+    """400 Bad Request"""
+
+    status = __doc__
+
+
+class HTTP401(HTTPException):
+    """401 Unauthorized"""
+
+    status = __doc__
+
+    def headers(self):
+        return [('WWW-Authenticate', '%s' % self)]
+
+    def body(self):
+        return ['']
+
+
+class HTTP403(HTTPException):
+    """403 Forbidden"""
+
+    status = __doc__
+
+
+class HTTP404(HTTPException):
+    """404 Not Found"""
+
+    status = __doc__
+
+
+class HTTP409(HTTPException):
+    """409 Conflict"""
+
+    status = __doc__
+
+
+class HTTP412(HTTPException):
+    """412 Precondition Failed"""
+
+    status = __doc__
+
+
+class HTTP415(HTTPException):
+    """415 Unsupported Media Type"""
+
+    status = __doc__
---
+++ packages/httpexceptor/httpexceptor-1.1.1.tar.gz/httpexceptor-1.1.1/test/test_main.py
@@ -0,0 +1,140 @@
+import httplib
+
+import httpexceptor
+
+from StringIO import StringIO
+
+
+def mock_response(error=None, message=None):
+    responses = []
+    environ = {'wsgi.errors': StringIO()}
+
+    def start_response_mock(status, headers, exc_info=None):
+        responses.append((status, dict(headers)))
+
+    def app(environ, start_response):
+        if error:
+            exception_class = getattr(httpexceptor, 'HTTP%s' % error)
+            raise exception_class(message or 'error message')
+        else:
+            start_response('200 OK', [('Content-Type', 'text/plain')])
+            return ['no errors']
+
+    # register middleware
+    app = httpexceptor.HTTPExceptor(app)
+
+    body = app(environ, start_response_mock)
+    return responses[0][0], responses[0][1], body
+
+
+def test_no_errors():
+    status, headers, body = mock_response()
+
+    assert status == _status(200)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain'
+    assert body == ['no errors']
+
+
+def test_302():
+    status, headers, body = mock_response(302, 'http://example.org')
+
+    assert status == _status(302)
+    assert len(headers) == 1
+    assert headers['Location'] == 'http://example.org'
+    assert body == ['']
+
+
+def test_303():
+    status, headers, body = mock_response(303, 'http://example.org')
+
+    assert status == _status(303)
+    assert len(headers) == 1
+    assert headers['Location'] == 'http://example.org'
+    assert body == ['']
+
+
+def test_304():
+    status, headers, body = mock_response(304, '123abc')
+
+    assert status == _status(304)
+    assert len(headers) == 1
+    assert headers['ETag'] == '123abc'
+    assert body == ['']
+
+
+def test_400():
+    status, headers, body = mock_response(400)
+
+    assert status == _status(400)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['400: error message']
+
+
+def test_401():
+    status, headers, body = mock_response(401, 'login')
+
+    assert status == _status(401)
+    assert len(headers) == 1
+    assert headers['WWW-Authenticate'] == 'login'
+    assert body == ['']
+
+
+def test_403():
+    status, headers, body = mock_response(403)
+
+    assert status == _status(403)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['403: error message']
+
+
+def test_404():
+    status, headers, body = mock_response(404)
+
+    assert status == _status(404)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['404: error message']
+
+
+def test_409():
+    status, headers, body = mock_response(409)
+
+    assert status == _status(409)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['409: error message']
+
+
+def test_412():
+    status, headers, body = mock_response(412)
+
+    assert status == _status(412)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['412: error message']
+
+
+def test_415():
+    status, headers, body = mock_response(415)
+
+    assert status == _status(415)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert body == ['415: error message']
+
+
+def test_500():
+    status, headers, body = mock_response(999, "fail")
+
+    assert status == _status(500)
+    assert len(headers) == 1
+    assert headers['Content-Type'] == 'text/plain; charset=UTF-8'
+    assert len(body) == 1
+    assert body[0].startswith("Traceback ")
+
+
+def _status(code):
+    return '%s %s' % (code, httplib.responses[code])
