186c786145b087f405beda753ce720fa230298cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:50 +0000

    Add package corset/0.0.4/corset-0.0.4.tar.gz

---
+++ packages/corset/corset-0.0.4.tar.gz/corset-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'corset',
+    packages = ['corset', 'corset.management', 'corset.management.commands'],
+    package_dir = {'management' : 'management/', 'commands' : 'management/commands/'},
+    version = '0.0.4',
+    description = 'Django JS/CSS compiler',
+    author = 'Chad Masso',
+    author_email = 'chad.m.masso@gmail.com',
+    maintainer = 'Chad Masso',
+    maintainer_email = 'chad.m.masso@gmail.com',
+)
