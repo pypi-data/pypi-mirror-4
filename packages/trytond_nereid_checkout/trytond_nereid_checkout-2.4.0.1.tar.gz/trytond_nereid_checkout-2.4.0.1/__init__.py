b0825db2563227caa72c8f5a3a9dfa2abd40fa69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package trytond_nereid_checkout/2.4.0.1/trytond_nereid_checkout-2.4.0.1.tar.gz

---
+++ packages/trytond_nereid_checkout/trytond_nereid_checkout-2.4.0.1.tar.gz/trytond_nereid_checkout-2.4.0.1/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+    
+    nereid_checkout
+    
+    :copyright: (c) 2010-2012 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+    
+'''
+
+from .sale import *
+from .checkout import *
