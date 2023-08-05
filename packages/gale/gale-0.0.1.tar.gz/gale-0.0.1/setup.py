2a0a6775603448108ea1cc79c698bb241ab19de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:30 +0000

    Add package gale/0.0.1/gale-0.0.1.tar.gz

---
+++ packages/gale/gale-0.0.1.tar.gz/gale-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+PACKAGE = "gale"
+NAME = "gale"
+DESCRIPTION = "Sugar for Tornado Asynchrous HTTP client"
+AUTHOR = "simpx"
+AUTHOR_EMAIL = "simpxx@gmail.com"
+URL = 'https://github.com/simpx/gale'
+
+setup(
+    name=NAME,
+    version=__import__(PACKAGE).__version__,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=['gale'],
+    url=URL,
+    license="BSD",
+    description=DESCRIPTION,
+    long_description=open('README').read(),
+)
+
