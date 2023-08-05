be6be03d763dfadde16b95adc4c57a08a193e855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:26 +0000

    Add package sanitize_my/1.0.0/sanitize_my-1.0.0.zip

---
+++ packages/sanitize_my/sanitize_my-1.0.0.zip/sanitize_my-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='sanitize_my',
+    version='1.0.0',
+    py_modules=['sanitize_my'],
+    author='kriscoenen',
+    author_email='kris@analys4it.be',
+    url='www.analys4it.be',
+    description='simple module for cleaning time notations',
+    )
