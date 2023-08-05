12c023efefed3db31d8d1ff72b03a66dcb184426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package infi.storagemodel/0.1.48/infi.storagemodel-0.1.48-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.48-py2.7.egg/infi/storagemodel/vendor/infinidat/infinibox/mixin/__init__.py
@@ -0,0 +1,28 @@
+
+from infi.pyutils.lazy import cached_method
+from infi.storagemodel.vendor import VendorSCSIBlockDevice, VendorSCSIStorageController
+from infi.storagemodel.vendor import VendorMultipathBlockDevice, VendorMultipathStorageController
+
+from logging import getLogger
+log = getLogger(__name__)
+
+from .inquiry import InfiniBoxInquiryMixin
+from .volume import InfiniBoxVolumeMixin
+from .sophisticated import SophisticatedMixin
+
+class scsi_block_class(InfiniBoxInquiryMixin, SophisticatedMixin, InfiniBoxVolumeMixin, VendorSCSIBlockDevice):
+    def __repr__(self):
+        return "<Infinibox Mixin for {!r}>".format(self.device)
+
+class scsi_controller_class(InfiniBoxInquiryMixin, SophisticatedMixin, VendorSCSIStorageController):
+    def __repr__(self):
+        return "<Infinibox Mixin for {!r}>".format(self.device)
+
+class multipath_block_class(InfiniBoxInquiryMixin, SophisticatedMixin, InfiniBoxVolumeMixin, VendorMultipathBlockDevice):
+    def __repr__(self):
+        return "<Infinibox Mixin for {!r}>".format(self.device)
+
+class multipath_controller_class(InfiniBoxInquiryMixin, SophisticatedMixin, VendorMultipathStorageController):
+    def __repr__(self):
+        return "<Infinibox Mixin for {!r}>".format(self.device)
+
