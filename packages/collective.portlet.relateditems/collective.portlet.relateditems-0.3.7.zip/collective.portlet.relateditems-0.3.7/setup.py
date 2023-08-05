506fd5b3550a8e2a2c875d407e077aff02ce2a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package collective.portlet.relateditems/0.3.7/collective.portlet.relateditems-0.3.7.zip

---
+++ packages/collective.portlet.relateditems/collective.portlet.relateditems-0.3.7.zip/collective.portlet.relateditems-0.3.7/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '0.3.7'
+
+long_description = (
+    open('README.txt').read().strip()
+    + '\n\n' +
+    open('CHANGES.rst').read().strip()
+)
+
+setup(name='collective.portlet.relateditems',
+      version=version,
+      description="A Plone portlet to display similar items to context",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      keywords='related, categories, keywords',
+      author='Rui Guerra, kiorky',
+      author_email='rui AT v2 DOT nl, kiorky <kiorky@cryptelium.net>, David Jonas <david@intk.com>',
+      url='https://github.com/collective/collective.portlet.relateditems',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
