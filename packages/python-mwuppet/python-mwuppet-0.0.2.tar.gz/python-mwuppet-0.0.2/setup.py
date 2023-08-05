c03890f5ed5b11b311f6432fa169cbda3baa9ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:18 +0000

    Add package python-mwuppet/0.0.2/python-mwuppet-0.0.2.tar.gz

---
+++ packages/python-mwuppet/python-mwuppet-0.0.2.tar.gz/python-mwuppet-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+        name="python-mwuppet",
+        version="0.0.2",
+        author="Yuvi Panda",
+        author_email="yuvipanda@gmail.com",
+        url="https://github.com/yuvipanda/mwuppet",
+        packages=['mwuppet',],
+        license="MIT License",
+        description = " Mediawiki UserScript Deploy Script",
+        long_description = open("README").read(),
+)
