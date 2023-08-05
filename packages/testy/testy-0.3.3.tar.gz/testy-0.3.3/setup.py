d620fb614065ffaba6431dcc0a493fb1c85dfb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:02 +0000

    Add package testy/0.3.3/testy-0.3.3.tar.gz

---
+++ packages/testy/testy-0.3.3.tar.gz/testy-0.3.3/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+PACKAGES = ['testy']
+
+
+def get_init_val(val, packages=PACKAGES):
+    pkg_init = "%s/__init__.py" % PACKAGES[0]
+    value = '__%s__' % val
+    fn = open(pkg_init)
+    for line in fn.readlines():
+        if line.startswith(value):
+            return line.split('=')[1].strip().strip("'")
+
+
+setup(
+    name=get_init_val('title'),
+    version=get_init_val('version'),
+    provides=['testy'],
+    description=get_init_val('description'),
+    long_description=open('README.rst').read(),
+    author=get_init_val('author'),
+    author_email='jim@jimr.org',
+    url=get_init_val('url'),
+    keywords = ['testing', 'unittest'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing',
+        ],
+    license=get_init_val('license'),
+    install_requires=['six'],
+    packages=PACKAGES,
+    test_suite='tests',
+)
---
+++ packages/testy/testy-0.3.3.tar.gz/testy-0.3.3/testy/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+__title__ = 'testy'
+__version__ = '0.3.3'
+__description__ = 'Python unittest helpers adapted from Testify'
+__url__ = 'https://github.com/jimr/testy'
+__author__ = 'James Rutherford'
+__licence__ = 'MIT'
+__copyright__ = 'Copyright 2012 James Rutherford'
+
+from .assertions import (
+    assert_raises, assert_raises_and_contains, assert_equal,
+    assert_almost_equal, assert_within_tolerance, assert_not_equal, assert_lt,
+    assert_lte, assert_gt, assert_gte, assert_in_range, assert_between,
+    assert_in, assert_not_in, assert_all_in, assert_starts_with,
+    assert_not_reached, assert_rows_equal, assert_length,
+    assert_is, assert_is_not, assert_all_match_regex, assert_match_regex,
+    assert_any_match_regex, assert_all_not_match_regex, assert_sets_equal,
+    assert_dicts_equal, assert_dict_subset, assert_subset, assert_list_prefix,
+    assert_sorted_equal, assert_isinstance, assert_datetimes_equal,
+    assert_exactly_one
+)
+
+from .aliases import (
+    raises, eq, equals, equal, ne, not_equal, lt, lte, gt, gte, in_range,
+    between, in_seq, not_in_seq, not_in, all_in, regex, 
+)
