8a45f1b665275ffd799d1d2826a15182c3f5e2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package tiddlywebplugins.diststore/0.7/tiddlywebplugins.diststore-0.7.tar.gz

---
+++ packages/tiddlywebplugins.diststore/tiddlywebplugins.diststore-0.7.tar.gz/tiddlywebplugins.diststore-0.7/setup.py
@@ -0,0 +1,23 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.diststore'
+DESCRIPTION = 'Use multiple stores in the same TiddlyWeb instance.'
+VERSION = '0.7' # also see the module
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = NAME,
+        version = VERSION,
+        description = DESCRIPTION,
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = AUTHOR,
+        url = 'http://pypi.python.org/pypi/%s' % NAME,
+        packages = find_packages(exclude='test'),
+        author_email = AUTHOR_EMAIL,
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools', 'tiddlyweb>=0.9.95'],
+        zip_safe = False,
+        )
