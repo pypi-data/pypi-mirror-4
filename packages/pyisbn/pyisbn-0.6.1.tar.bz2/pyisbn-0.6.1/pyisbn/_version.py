3bc0e9f1cdd1caa6a61d0d9aafc0301c65a5110e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package pyisbn/0.6.1/pyisbn-0.6.1.tar.bz2

---
+++ packages/pyisbn/pyisbn-0.6.1.tar.bz2/pyisbn-0.6.1/pyisbn/_version.py
@@ -0,0 +1,9 @@
+# This is pyisbn version 0.6.1 (2012-12-19)
+# pylint: disable=C0103, C0111, C0121, W0622
+
+dotted = '0.6.1'
+libtool = '6:21'
+hex = 0x000601
+date = '2012-12-19'
+tuple = (0, 6, 1)
+web = 'pyisbn/0.6.1'
