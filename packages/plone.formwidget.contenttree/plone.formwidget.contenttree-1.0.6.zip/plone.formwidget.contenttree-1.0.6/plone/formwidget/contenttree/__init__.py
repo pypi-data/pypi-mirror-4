77eb7eae2c4a8ffa3b2ff4bb4909dcba41af30b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:23 +0000

    Add package plone.formwidget.contenttree/1.0.6/plone.formwidget.contenttree-1.0.6.zip

---
+++ packages/plone.formwidget.contenttree/plone.formwidget.contenttree-1.0.6.zip/plone.formwidget.contenttree-1.0.6/plone/formwidget/contenttree/__init__.py
@@ -0,0 +1,8 @@
+from zope.i18nmessageid import MessageFactory
+MessageFactory = MessageFactory('plone.formwidget.contenttree')
+
+from plone.formwidget.contenttree.widget import ContentTreeFieldWidget
+from plone.formwidget.contenttree.widget import MultiContentTreeFieldWidget
+
+from plone.formwidget.contenttree.source import PathSourceBinder,\
+    ObjPathSourceBinder, UUIDSourceBinder
