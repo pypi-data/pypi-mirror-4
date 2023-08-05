709e6fec1c9d1427e37412a64602e5b57a4398cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:04 +0000

    Add package dynconf/0.2/dynconf-0.2.tar.gz

---
+++ packages/dynconf/dynconf-0.2.tar.gz/dynconf-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dynconf',
+    author='Jorge E. Cardona',
+    description="Configuration file proxy.",
+    author_email='jorgeecardona@gmail.com',
+    version='0.2',
+    packages=['dynconf'],
+    entry_points = {
+        'console_scripts': [
+            'dynconf = dynconf.cli:main'
+            ]}
+    )
