d63fddbb683bbcf5a86d49b13a214d341bb2105e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package sysuwang/1.0.0/sysuwang-1.0.0.tar.gz

---
+++ packages/sysuwang/sysuwang-1.0.0.tar.gz/sysuwang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'sysuwang',
+        version = '1.0.0',
+        py_modules = ['sysuwang'],
+        author = 'wangrui',
+        author_email = 'sysuwangrui@gmail.com',
+        url = 'www.google.com.tw',
+        description = 'A simple test',
+     )
