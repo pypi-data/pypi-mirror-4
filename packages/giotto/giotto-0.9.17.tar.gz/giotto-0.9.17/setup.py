85ea997b418a5acceebd1f2d5d25eaf07e827588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:42 +0000

    Add package giotto/0.9.17/giotto-0.9.17.tar.gz

---
+++ packages/giotto/giotto-0.9.17.tar.gz/giotto-0.9.17/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='giotto',
+    version='0.9.17',
+    description='Functional Web Framework',
+    long_description=open('README.rst').read(),
+    author='Chris Priest',
+    author_email='cp368202@ohiou.edu',
+    url='https://github.com/priestc/giotto',
+    packages=[
+        'giotto',
+        'giotto.controllers',
+        'giotto.programs',
+        'giotto.contrib',
+        'giotto.contrib.auth',
+        'giotto.contrib.static',
+        'giotto.views'
+    ],
+    scripts=['bin/giotto'],
+    include_package_data=True,
+    license='LICENSE',
+    install_requires=[
+        'werkzeug==0.8.3',
+        'irc==5.0.1',
+        'jinja2==2.6',
+        'py-bcrypt==0.2',
+        'mimeparse==0.1.3',
+        'python-magic==0.4.3',
+        'sqlalchemy==0.7.9',
+    ],
+)
