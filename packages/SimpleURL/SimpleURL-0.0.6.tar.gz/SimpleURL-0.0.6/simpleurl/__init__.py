b13b87af8411610b91986c6b79ce6deeaf61c5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:17 +0000

    Add package SimpleURL/0.0.6/SimpleURL-0.0.6.tar.gz

---
+++ packages/SimpleURL/SimpleURL-0.0.6.tar.gz/SimpleURL-0.0.6/simpleurl/__init__.py
@@ -0,0 +1,260 @@
+#! -*- coding: utf-8 -*-
+
+# Most of the code here is inspired from
+# https://github.com/mitsuhiko/flask/blob/master/flask/app.py
+
+version = "0.0.6"
+version_info = (0, 0, 6)
+
+import inspect
+
+from brubeck.request_handling import Brubeck
+from werkzeug.routing import Rule, Map, HTTPException, RequestRedirect, BuildError
+from tldextract import extract
+
+
+# constants
+HTTP_METHODS = frozenset(['GET', 'HEAD', 'POST', 'PUT',
+    'DELETE', 'TRACE', 'OPTIONS', 'PATCH'])
+
+
+# Helpers
+def _return_func_name(view_func):
+    # helper to return func name for the url
+    assert view_func is not None, 'expected view func if endpoint '
+    return view_func.__name__
+
+
+class SimpleURL(Brubeck):
+    """SimpleURL is alternate to Brubeck Regex based routing.
+    SimpleURL uses werkzeug routing to make routing simpler.
+    """
+    def __init__(self, brubeck_object, *args, **kwargs):
+        super(Brubeck, self).__init__()
+        # copy all brubeck object attributes to simpleurl.
+        for key, val in brubeck_object.__dict__.iteritems():
+            setattr(self, key, val)
+        #: Werkzeug Rule class
+        self.url_rule_class = Rule
+        #: Map object which stores all the Rules
+        self.url_map = Map()
+        #: A dictionary of all view functions registered.  The keys will
+        #: be function names which are also used to generate URLs and
+        #: the values are the function objects themselves.
+        #: To register a view function, use the :meth:`route` decorator.
+        self.view_functions = {}
+        # if handler_tuples add routes
+        if brubeck_object.handler_tuples is not None:
+            self.handler_tuples = brubeck_object.handler_tuples
+        # Keep state for handler_tuples so that we needn't add routes in route_message
+        # every time request is received
+        self.is_handler_tuples_added = False
+        #Set kwargs key, value since other extensions values may be passed
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def init_routes(self, handler_tuples):
+        """Loops over a list of (rule, handler) tuples and adds them
+        to the routing table.
+        """
+        for ht in handler_tuples:
+            (rule, kallable) = ht
+            # create a instance of callable and then map respective http method to
+            # class method
+            obj = kallable(self, self.brubeck_object.message)
+            self.class_handlers.append({'rule': rule, 'methods': pair[0], 'kallable': kallable} for pair in inspect.getmembers(obj, predicate=inspect.ismethod)
+                if pair[0].upper() in HTTP_METHODS)
+
+    def add_route_url(self, rule, endpoint=None, view_func=None, **options):
+        """Registration point for all URL rules.
+        Basically this example::
+
+            @app.add_route('/')
+            def index(application, message):
+                pass
+
+        Is equivalent to the following::
+
+            def index():
+                pass
+            app.add_route_rule('/', 'index', index)
+
+        If the view_func is not provided you will need to connect the endpoint
+        to a view function like so::
+
+            app.view_functions['index'] = index
+
+        :param rule: the URL rule as string
+        :param endpoint: the endpoint for the registered URL rule.  Flask
+                         itself assumes the name of the view function as
+                         endpoint
+        :param view_func: the function to call when serving a request to the
+                          provided endpoint
+        :param options: the options to be forwarded to the underlying
+                        :class:`~werkzeug.routing.Rule` object.  A change
+                        to Werkzeug is handling of method options.  methods
+                        is a list of methods this rule should be limited
+                        to (`GET`, `POST` etc.).  By default a rule
+                        just listens for `GET` (and implicitly `HEAD`).
+        """
+        if endpoint is None:
+            endpoint = _return_func_name(view_func)
+        options['endpoint'] = endpoint
+        method = options.pop('method', None)
+
+        if method is None:
+            method = getattr(view_func, 'method', None) or ('GET',)
+        method = set(method)
+
+        # Methods that should always be added
+        required_method = set(getattr(view_func, 'required_methods', ()))
+
+        # Add the required methods now.
+        method |= required_method
+
+        # due to a werkzeug bug we need to make sure that the defaults are
+        # None if they are an empty dictionary.  This should not be necessary
+        # with Werkzeug 0.7
+        options['defaults'] = options.get('defaults') or None
+        rule = self.url_rule_class(rule, methods=method, **options)
+        #rule.provide_automatic_options = provide_automatic_options
+
+        self.url_map.add(rule)
+        if view_func is not None:
+            old_func = self.view_functions.get(endpoint)
+            if old_func is not None and old_func is not view_func:
+                raise AssertionError('View function mapping is overwriting an '
+                                     'existing endpoint function: %s' % endpoint)
+            self.view_functions[endpoint] = view_func
+
+    def add_route(self, rule, method=['GET'], **options):
+        """A decorator that is used to register a view function for a
+        given URL rule.  This does the same thing as :meth:`add_url_rule`
+        but is intended for decorator usage::
+
+            @app.add_route('/', method=['GET', 'POST'])
+            def index(application, message):
+                if message.method == 'GET':
+                    # import render from brubeck.request_handling
+                    return render("Foo Bar", 200, 'OK', {})
+                    return render('Hello World'
+                elif message.method == 'POST':
+                    # process the arguments
+                    # return render
+
+            # Passing default values
+            @app.add_route('/all/', defaults={'ids': 1}, method=['GET', 'POST'])
+            @app.add_route('/all/<ids>', method=['GET', 'POST'])
+            def process(application, message, ids):
+                body = 'you passed id as %s' % (str(ids))
+                return render(body, 200, 'OK', {})
+
+
+        :param rule: the URL rule as string
+        :param endpoint: the endpoint for the registered URL rule.  Flask
+                         itself assumes the name of the view function as
+                         endpoint
+        :param view_func: the function to call when serving a request to the
+                          provided endpoint
+        :param options: the options to be forwarded to the underlying
+                        :class:`~werkzeug.routing.Rule` object.  A change
+                        to Werkzeug is handling of method options.  methods
+                        is a list of methods this rule should be limited
+                        to (`GET`, `POST` etc.).
+        """
+        def decorator(f):
+            endpoint = options.pop('endpoint', None)
+            self.add_route_url(rule, endpoint, f, **options)
+            return f
+        return decorator
+
+    def check_handler_tuples(self):
+        if self.handler_tuples is not None and not self.is_handler_tuples_added:
+            for ht in self.handler_tuples:
+                (rule, kallable) = ht
+                # create a instance of callable and then map respective http method to
+                # class method
+                #obj = kallable(self, message, )
+                if not inspect.isclass(kallable):
+                    # currently tiple hadnlers don't mentin method make GET, HEAD as default
+                    self.add_route_url(rule=rule, method=['GET', 'HEAD'], view_func=kallable)
+                else:
+                    for pair in inspect.getmembers(kallable, predicate=inspect.ismethod):
+                        method = pair[0].upper()
+                        if method in HTTP_METHODS:
+                            self.add_route_url(rule=rule, endpoint='_'.join([kallable.__name__, method]), method=[method],
+                            view_func=kallable)
+            # set handler_tuples state to true
+            self.is_handler_tuples_added = True
+        
+    def route_message(self, message):
+        self.check_handler_tuples()
+        self.url_rule_class.add = self.url_map
+        #print self.url_map, self.url_rule_class
+        # FIX ME: Figure out different values of url_scheme
+
+        # check whether mongrel2 is serving or WSGI Server
+        # https://github.com/j2labs/brubeck/blob/master/brubeck/request.py
+        handler = None
+        if message.is_wsgi:
+            server_name = message.headers['HTTP_HOST']
+            url_scheme = message.headers['wsgi.url_scheme']
+            default_method = message.headers['METHOD']
+        else:
+            server_name = message.headers[u'host']
+            url_scheme = message.headers[u'VERSION'].split('/')[0]
+            default_method = message.method
+        subdomain = extract(server_name).subdomain
+        arguments = message.arguments or None
+        path_info = message.path
+        self.urls = self.url_map.bind(server_name=server_name, url_scheme=url_scheme, subdomain=subdomain,
+             default_method=default_method, path_info=path_info, query_args=arguments)
+        try:
+            endpoint = self.urls.match(message.path)
+            kallable = self.view_functions[endpoint[0]]
+            if inspect.isclass(kallable):
+                handler = kallable(self, message)
+                handler._url_args = endpoint[-1]
+                return handler
+            else:
+                handler = lambda: kallable(self, message, **endpoint[1])
+                return handler
+        except RequestRedirect, e:
+            handler = self.base_handler(self, message)
+            handler.set_status(e.name)
+        except HTTPException, e:
+            handler = self.base_handler(self, message)
+            handler.set_status(e.name)
+
+        return handler
+
+    def url_for(self, endpoint, **values):
+        """
+        @app.route("/index")
+        def index(application, message):
+            return render("index", 200, 'OK', {})
+
+        @app.route("/<int:a>")
+        def handle_int(application, message, a):
+            return render("You passed int value:%s" % str(value), 200, 'OK', {})
+
+        @app.route('/test_endpoint', endpoint='test_endpoint')
+        def test(application, message):
+            return render("Test Endpoint", 200, 'OK', {})
+
+        @app.route('/test')
+        def test(application, message):
+            return render("url_for output: index: %s, handle_int: %s, test_endpoint: %s" %(app.url_for('index'),
+                app.url_for('handle_int', a=23),
+                app.url_for('test_endpoint', external=True)), 200, 'OK', {})
+        """
+        try:
+            external = False or values.pop('external', None)
+            method = 'GET' or values.pop('method', None)
+            defaults = self.urls.match(return_rule=True, path_info="")
+            return self.urls.build(endpoint=endpoint, force_external=external, method=method, values=values)
+        except BuildError, error:
+            #FIX ME: handle exception and give better message
+            raise Exception("url rule doesn't exist")
+        except AttributeError:
+            raise Exception("url_for must be called inside function or class method")
