0c1567335e3cfd1dda5e87de4c0c1d05b156c9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package busmon_consumers/0.4.3/busmon_consumers-0.4.3.tar.gz

---
+++ packages/busmon_consumers/busmon_consumers-0.4.3.tar.gz/busmon_consumers-0.4.3/busmon_consumers/consumers.py
@@ -0,0 +1,70 @@
+import pygments.lexers
+import pygments.formatters
+from fedmsg.consumers import FedmsgConsumer
+import time
+import math
+import memcache
+
+import fedmsg
+import fedmsg.encoding
+
+
+class MessageColorizer(FedmsgConsumer):
+    topic = "*"
+    config_key = 'busmon.consumers.colorizer.enabled'
+    jsonify = False
+    destination_topic = "colorized-messages"
+
+    def consume(self, message):
+
+        if self.destination_topic in message.topic:
+            return
+
+        html_args = {'full': False}
+        code = pygments.highlight(
+            fedmsg.encoding.pretty_dumps(fedmsg.encoding.loads(message.body)),
+            pygments.lexers.JavascriptLexer(),
+            pygments.formatters.HtmlFormatter(**html_args)
+        )
+
+        code = code.strip()
+
+        fedmsg.publish(
+            topic=self.destination_topic,
+            msg=code,
+            modname="busmon",
+        )
+
+
+class MemcachedStuffer(FedmsgConsumer):
+    topic = "*"
+    config_key = 'busmon.consumers.memcached.enabled'
+    jsonify = False
+
+    def __init__(self, *args, **kwargs):
+        super(MemcachedStuffer, self).__init__(*args, **kwargs)
+
+        if not getattr(self, '_initialized', False):
+            return
+
+        servers = self.hub.config['busmon.memcached.servers'].split(',')
+        self.mc = memcache.Client(servers)
+
+    def consume(self, message):
+        if MessageColorizer.destination_topic in message.topic:
+            return
+
+        c = self.hub.config
+        duration = c['busmon.memcached.duration']
+        n = c['busmon.memcached.n']
+
+        head = int(time.time() * 1000 / duration) % n
+        key = "busmon_count_%i" % head
+
+        # This try-except block is here to support both old and new versions of
+        # python-memcached.
+        try:
+            if not self.mc.incr(key):
+                raise ValueError("Not Found")
+        except ValueError:
+            self.mc.set(key, 1, time=duration / 1000.0 * n)
---
+++ packages/busmon_consumers/busmon_consumers-0.4.3.tar.gz/busmon_consumers-0.4.3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+testpkgs=[
+]
+
+install_requires=[
+    "fedmsg>=0.5.0",
+    "pygments",
+    "python-memcached",
+]
+
+
+setup(
+    name='busmon_consumers',
+    version='0.4.3',
+    description='fedmsg-hub consumers for the busmon webapp',
+    author='Ralph Bean',
+    author_email='rbean@redhat.com',
+    url='http://github.com/ralphbean/busmon',
+    license='GPLv2+',
+    setup_requires=["PasteScript >= 1.7"],
+    paster_plugins=['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=install_requires,
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=testpkgs,
+    entry_points={
+        'moksha.consumer': (
+            'colorizer = busmon_consumers.consumers:MessageColorizer',
+            'memcached = busmon_consumers.consumers:MemcachedStuffer',
+        ),
+    },
+    zip_safe=False
+)
