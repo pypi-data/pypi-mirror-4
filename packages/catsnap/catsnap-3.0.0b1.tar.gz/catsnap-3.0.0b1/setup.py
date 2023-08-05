1a7b2e71662a980fda324b4232cd598923695f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package catsnap/3.0.0b1/catsnap-3.0.0b1.tar.gz

---
+++ packages/catsnap/catsnap-3.0.0b1.tar.gz/catsnap-3.0.0b1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="catsnap",
+      version="3.0.0b1",
+      description="catalog and store funny pictures",
+      author="Andrew Lorente",
+      author_email="andrew.lorente@gmail.com",
+      url="github.com/andrewlorente/catsnap",
+      packages=['catsnap',
+                'catsnap.document',
+                'catsnap.config',
+                'catsnap.batch'],
+      scripts=['scripts/catsnap'],
+      install_requires=[
+          "Flask==0.9",
+          "gunicorn==0.14.6",
+          "boto==2.5.2",
+          "requests==0.13.2",
+          "argparse==1.2.1",
+          "flask-openid==1.1.1",
+          "psycopg2==2.4.6",
+          "sqlalchemy==0.8.0b2",
+          "yoyo-migrations==4.1.6",
+
+          "mock==0.8",
+          "nose==1.1.2",
+          ],
+      )
