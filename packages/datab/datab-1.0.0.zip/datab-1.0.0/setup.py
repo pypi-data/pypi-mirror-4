3a0f754d768fc6c46663b875409198e1f199cb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:43 +0000

    Add package datab/1.0.0/datab-1.0.0.zip

---
+++ packages/datab/datab-1.0.0.zip/datab-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='datab',
+      version='1.0.0',
+      description='a database frame for store,search and help',
+      author='guzhen',
+      author_email='604608916@qq.com'
+      )
