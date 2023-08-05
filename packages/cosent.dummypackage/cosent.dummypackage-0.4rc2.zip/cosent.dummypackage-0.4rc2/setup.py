6dffdfaf2f02a4b87db5eccfab0362c157ab72ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:17 +0000

    Add package cosent.dummypackage/0.4rc2/cosent.dummypackage-0.4rc2.zip

---
+++ packages/cosent.dummypackage/cosent.dummypackage-0.4rc2.zip/cosent.dummypackage-0.4rc2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.4rc2'
+
+setup(name='cosent.dummypackage',
+      version=version,
+      description="A dummy package for buildtools testing",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Guido Stevens',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['cosent'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
