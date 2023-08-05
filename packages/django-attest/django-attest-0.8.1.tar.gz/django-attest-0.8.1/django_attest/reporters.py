306c2eb432d51e493698241db7c437c7926fe3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package django-attest/0.8.1/django-attest-0.8.1.tar.gz

---
+++ packages/django-attest/django-attest-0.8.1.tar.gz/django-attest-0.8.1/django_attest/reporters.py
@@ -0,0 +1,39 @@
+from .environment import testing_environment
+from attest import reporters, Tests
+from django.utils.functional import wraps
+
+
+__all__ = ("AbstractReporter", "PlainReporter", "FancyReporter",
+           "auto_reporter", "XmlReporter", "QuickFixReporter")
+
+
+def _test_loader_factory(reporter):
+    class Loader(object):
+        def loadTestsFromNames(self, names, module=None):
+            with testing_environment():
+                Tests(names).run(reporter)
+            raise SystemExit
+    return Loader()
+
+
+class ReporterMixin(object):
+    """
+    Patch :meth:`test_loader` to perform Django environment configuration.
+    """
+    @classmethod
+    def test_loader(cls):
+        return _test_loader_factory(cls)
+
+
+AbstractReporter = type("AbstractReporter", (ReporterMixin, reporters.AbstractReporter), {})
+PlainReporter    = type("PlainReporter",    (ReporterMixin, reporters.PlainReporter), {})
+FancyReporter    = type("FancyRepoter",     (ReporterMixin, reporters.FancyReporter), {})
+XmlReporter      = type("XmlReporter",      (ReporterMixin, reporters.XmlReporter), {})
+QuickFixReporter = type("QuickFixReporter", (ReporterMixin, reporters.QuickFixReporter), {})
+
+
+@wraps(reporters.auto_reporter)
+def auto_reporter(**opts):
+    return reporters.auto_reporter(**opts)
+
+auto_reporter.test_loader = lambda: _test_loader_factory(auto_reporter)
