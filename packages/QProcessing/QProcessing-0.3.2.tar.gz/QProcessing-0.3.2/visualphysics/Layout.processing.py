34814d9ed5e6596683b175a54eb172e2820ca002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package QProcessing/0.3.2/QProcessing-0.3.2.tar.gz

---
+++ packages/QProcessing/QProcessing-0.3.2.tar.gz/QProcessing-0.3.2/visualphysics/Layout.processing.py
@@ -0,0 +1,6 @@
+def setup():
+    fill(256, 256, 256)
+    size(500, 500)
+    rect(0, 0, 500, 500)
+def draw():
+    noLoop()
---
+++ packages/QProcessing/QProcessing-0.3.2.tar.gz/QProcessing-0.3.2/visualphysics/layout/ButtonTable.processing.py
@@ -0,0 +1,9 @@
+def setup():
+    fill(256, 256, 256)
+    size(720, 960)
+    rect(0, 0, 720, 960)
+    rectMode(CORNERS)
+    fill(204, 102, 0)
+    rect(0,600.0,720,960)
+def draw():
+    noLoop()
