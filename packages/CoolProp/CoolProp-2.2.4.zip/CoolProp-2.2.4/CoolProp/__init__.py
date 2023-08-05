698d3b07a3c1feea9edb8e275dd9b82a3dabe647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:44 +0000

    Add package CoolProp/2.2.4/CoolProp-2.2.4.zip

---
+++ packages/CoolProp/CoolProp-2.2.4.zip/CoolProp-2.2.4/CoolProp/__init__.py
@@ -0,0 +1,7 @@
+__version__='2.2.4'
+__svnrevision__ =176
+import CoolProp
+import HumidAirProp
+import FloodProp
+import State
+__fluids__ = CoolProp.FluidsList()
---
+++ packages/CoolProp/CoolProp-2.2.4.zip/CoolProp-2.2.4/CoolProp/purefluids/Hydrogen.h
@@ -0,0 +1,45 @@
+#ifndef Hydrogen_H
+#define Hydrogen_H
+
+	class HydrogenClass : public Fluid{
+
+	public:
+		HydrogenClass();
+		~HydrogenClass(){};
+		virtual double conductivity_Trho(double, double);
+		virtual double viscosity_Trho(double, double);
+		double psat(double);
+		double rhosatL(double);
+		double rhosatV(double);
+		double surface_tension_T(double);
+	};
+
+	class ParaHydrogenClass : public Fluid{
+
+	public:
+		ParaHydrogenClass();
+		~ParaHydrogenClass(){};
+		virtual double conductivity_Trho(double, double);
+		virtual double viscosity_Trho(double, double);
+		double psat(double);
+		double rhosatL(double);
+		double rhosatV(double);
+		double surface_tension_T(double);
+	};
+
+
+	class OrthoHydrogenClass : public Fluid{
+
+	public:
+		OrthoHydrogenClass();
+		~OrthoHydrogenClass(){};
+		virtual double conductivity_Trho(double, double);
+		virtual double viscosity_Trho(double, double);
+		double psat(double);
+		double rhosatL(double);
+		double rhosatV(double);
+		double surface_tension_T(double);
+	};
+
+
+#endif
