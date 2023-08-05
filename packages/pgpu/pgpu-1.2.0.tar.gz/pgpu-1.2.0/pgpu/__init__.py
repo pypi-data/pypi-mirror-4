f73918327ecc855b4ee97670135497ac6e11539d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package pgpu/1.2.0/pgpu-1.2.0.tar.gz

---
+++ packages/pgpu/pgpu-1.2.0.tar.gz/pgpu-1.2.0/pgpu/__init__.py
@@ -0,0 +1,27 @@
+"""
+Pydsigner's Generic Python Utilities (PGPU) -- a collection of handy modules 
+and packages for Python.
+
+__init__        --  version information
+compatibility   --  module to make it easier to write modules compatible with 
+                    both 2.x and 3.x
+iter_utils      --  generic iterable utilities
+file_utils      --  file utility module
+math_utils      --  utilities to work with bases plus some trig utilities
+encoding        --  unified interface to many codecs and hashers
+time_widgets    --  tkinter time widgets
+tk_utils        --  tkinter widgets and helper functions
+wrappers        --  utility value wrappers for places requiring functions
+
+tkinter2x       --  2.x and 3.x compatibility layer for tkinter
+
+
+This file provides version information.
+
+AUTHORS:
+v0.2.0+             --> pydsigner
+v1.0.0+             --> pydsigner
+v1.0.3+             --> pydsigner
+"""
+
+__version__ = '1.2.0'
---
+++ packages/pgpu/pgpu-1.2.0.tar.gz/pgpu-1.2.0/setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import sys
+
+### Main setup stuff
+
+import pgpu
+
+NL = '\n'
+# Get the first section, and convert all whitespace to spaces
+description = ' '.join(pgpu.__doc__.strip().split(NL * 2, 1)[0].split())
+# Get everything but the version info.
+long_description = pgpu.__doc__.strip().rsplit(NL * 3, 1)[0]
+
+def main():
+    
+    # perform the setup action
+    setup_args = {
+        'script_args': (sys.argv[1:] if len(sys.argv) > 1 
+                        else ['install']),
+        'name': 'pgpu',
+        'version': pgpu.__version__,
+        'description': description,
+        'long_description': long_description,
+        'author': 'Daniel Foerster/pydsigner',
+        'author_email': 'pydsigner@gmail.com',
+        'packages': ['pgpu','pgpu.tkinter2x'],
+        'license': 'LGPLv3',
+        'url': 'http://github.com/pydsigner/PGPU',
+        'classifiers': [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+        ]}
+    setup(**setup_args)
+
+if __name__ == '__main__':
+    main()
