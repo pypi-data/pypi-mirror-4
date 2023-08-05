9278228b43be995dbb94e9ab9cd51864cced451b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package you-get/0.2.15/you-get-0.2.15.tar.gz

---
+++ packages/you-get/you-get-0.2.15.tar.gz/you-get-0.2.15/you_get/downloader/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from .acfun import *
+from .bilibili import *
+from .cntv import *
+from .dailymotion import *
+from .googleplus import *
+from .ifeng import *
+from .iqiyi import *
+from .ku6 import *
+from .pptv import *
+from .sina import *
+from .sohu import *
+from .tudou import *
+from .vimeo import *
+from .w56 import *
+from .yinyuetai import *
+from .youku import *
+from .youtube import *
