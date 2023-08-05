c6a3258af48e45bb22284933f30b3fe6937a7708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package tl/0.4.3/tl-0.4.3.tar.gz

---
+++ packages/tl/tl-0.4.3.tar.gz/tl-0.4.3/tl/version.py
@@ -0,0 +1,42 @@
+# tl/version.py
+#
+#
+
+""" version related stuff. """
+
+## tl imports
+
+
+## basic imports
+
+import os
+import binascii
+
+## defines
+
+version = "0.4.3"
+__version__ = version
+
+## getversion function
+
+def getversion(txt=""):
+    """ return a version string. """
+    if txt: return "VERSION %s %s" % (version, txt)
+    else: return "VERSION %s" % version
+
+def getfullversion(txt="", repo=False):
+    v = str(version)
+    from tl.lib.config import getmainconfig
+    cfg = getmainconfig()
+    if cfg.dbenable: v += " -=- " + cfg.dbtype.upper()
+    tip = None
+    if repo:
+        try:
+            from mercurial import context, hg, node, repo, ui
+            repository = hg.repository(ui.ui(), '.')
+            ctx = context.changectx(repository)
+            tip = str(ctx.rev())
+        except: tip = None
+    if tip: version2 = v + " -=- HG " + tip
+    else: version2 = v
+    return "T I M E L I N E -=- %s -=- %s" % (txt, version2)
