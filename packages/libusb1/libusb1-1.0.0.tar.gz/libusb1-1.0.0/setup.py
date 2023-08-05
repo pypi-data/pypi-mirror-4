11a53b48cfd788cc27efec6c329c0e50114ea591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package libusb1/1.0.0/libusb1-1.0.0.tar.gz

---
+++ packages/libusb1/libusb1-1.0.0.tar.gz/libusb1-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='libusb1',
+    description='Python wrapper around libusb-1.0',
+    keywords='usb libusb',
+    version='1.0.0',
+    author='Vincent Pelletier',
+    author_email='plr.vincent@gmail.com',
+    url='http://github.com/vpelletier/python-libusb1',
+    license='GPL',
+    platforms=['any'],
+    py_modules=['libusb1','usb1'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+    ],
+)
