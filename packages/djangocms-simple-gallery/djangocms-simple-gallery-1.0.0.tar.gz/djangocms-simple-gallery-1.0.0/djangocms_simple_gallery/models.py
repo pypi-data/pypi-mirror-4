ea12b052a3e43c607e0678b4e6d69a455e63ff3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package djangocms-simple-gallery/1.0.0/djangocms-simple-gallery-1.0.0.tar.gz

---
+++ packages/djangocms-simple-gallery/djangocms-simple-gallery-1.0.0.tar.gz/djangocms-simple-gallery-1.0.0/djangocms_simple_gallery/models.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from cms.models.pluginmodel import CMSPlugin
+from cms.models.fields import PageField
+
+from filer.fields.image import FilerImageField
+from djangocms_simple_gallery import settings
+
+import math
+
+
+class Gallery(CMSPlugin):
+    title = models.CharField(_('title'), max_length=100, blank=True, null=True)
+    template = models.CharField(_('template'), max_length=255, default=settings.GALLERY_TEMPLATES[0][0],
+        choices=settings.GALLERY_TEMPLATES)
+    autoplay = models.PositiveIntegerField(_('autoplay'), default=0, blank=True,
+        help_text=_('Time in seconds each slide is displayed. Zero means no autoplay.'))
+
+    # Advanced
+    duration = models.PositiveIntegerField(_('duration'), default=0, blank=True,
+        help_text=_('Duration of the animation in seconds. Zero means no animation.'))
+
+    def __unicode__(self):
+        if self.title:
+            return self.title
+        return super(Gallery, self).__unicode__()
+
+    def copy_relations(self, old_instance):
+        for slide in old_instance.slides.all():
+            slide.pk = None
+            slide.gallery = self.pk
+            slide.save()
+
+
+class Slide(models.Model):
+    gallery = models.ForeignKey(Gallery, related_name='slides')
+    ordering = models.IntegerField(_('ordering'), default=100)
+    image = FilerImageField(verbose_name=_('image'))
+
+    title = models.CharField(_('title'), max_length=100, blank=True, null=True)
+    page = PageField(verbose_name=_('page'), blank=True, null=True)
+    url = models.URLField(_('url'), blank=True, null=True)
+
+    class Meta:
+        ordering = ['ordering', 'pk',]
+
+    def __unicode__(self):
+        return self.get_title
+
+    @property
+    def get_title(self):
+        if self.title:
+            return self.title
+        return self.image.label
+
+    @property
+    def link(self):
+        if self.page:
+            return self.page.get_absolute_url()
+        if self.url:
+            return self.url
+        return '#'
+
+    @property
+    def has_link(self):
+        if self.page or self.url:
+            return True
+        return False
---
+++ packages/djangocms-simple-gallery/djangocms-simple-gallery-1.0.0.tar.gz/djangocms-simple-gallery-1.0.0/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import djangocms_simple_gallery
+
+import os
+
+
+VERSION = djangocms_simple_gallery.__version__
+AUTHOR = djangocms_simple_gallery.__author__
+NAME = 'djangocms-simple-gallery'
+URL = 'https://github.com/divio/djangocms-simple-gallery'
+AUTHOR_MAIL = 'pascal.mouret@divio.ch'
+DESCRIPTION = 'Simple gallery plugin for the django-cms.'
+README = 'README.md'
+
+REQUIREMENTS = [
+    'django-filer',
+]
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+# make it work
+if not 'a' in VERSION and not 'b' in VERSION: CLASSIFIERS.append('Development Status :: 5 - Production/Stable')
+elif 'a' in VERSION: CLASSIFIERS.append('Development Status :: 3 - Alpha')
+elif 'b' in VERSION: CLASSIFIERS.append('Development Status :: 4 - Beta')
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_MAIL,
+    description=DESCRIPTION,
+    long_description=read(README),
+    url=URL,
+    classifiers=CLASSIFIERS,
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    packages=['djangocms_simple_gallery', 'djangocms_simple_gallery.migrations'],
+	include_package_data=True
+)
