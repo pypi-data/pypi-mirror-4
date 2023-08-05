ee46f7ffeeef60374e6c49fb9081d6f1cb17f59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:04 +0000

    Add package usgs-api/0.1.2a/usgs-api-0.1.2a.zip

---
+++ packages/usgs-api/usgs-api-0.1.2a.zip/usgs-api-0.1.2a/setup.py
@@ -0,0 +1,25 @@
+__author__ = 'nicksantos'
+
+from setuptools import setup
+
+setup(name='usgs-api',
+	version='0.1.2a',
+	long_description='''Please note that the module name is usgs, not usgs-api for importing.
+
+	A wrapper around the USGS Water Information System JSON API that provides native Python access. Documentation is available at Read the Docs (https://usgs-api.readthedocs.org/en/latest). The code and documentation are licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+	''',
+    description='A Python wrapper for the USGS National Water Information System JSON API',
+	url='https://bitbucket.org/UltraAyla/usgs-api',
+	author='Nick Santos',
+	author_email='python@nicksantos.com',
+    keywords=('usgs','api','nwis','water','data','wrapper'),
+	license='Creative Commons Attribution Sharealike 3.0 Unported',
+	packages=['usgs','docs'],
+	install_requires=[
+		'urllib2',
+		'urllib',
+		'json',
+		'pandas',
+		],
+	zip_safe=False)
+
