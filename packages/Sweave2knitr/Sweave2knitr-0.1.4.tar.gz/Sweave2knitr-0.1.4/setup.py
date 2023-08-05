6fd6a822107bb9686a354eae18cd50733509b519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:23 +0000

    Add package Sweave2knitr/0.1.4/Sweave2knitr-0.1.4.tar.gz

---
+++ packages/Sweave2knitr/Sweave2knitr-0.1.4.tar.gz/Sweave2knitr-0.1.4/setup.py
@@ -0,0 +1,13 @@
+import os
+from distutils.core import setup
+
+setup(name="Sweave2knitr",
+      author="David Robinson",
+      author_email="dgrtwo@princeton.edu",
+      url="http://github.com/dgrtwo/Sweave2knitr",
+      description="Convert an Sweave document to work in knitr",
+      packages=["Sweave2knitr"],
+      package_dir={"Sweave2knitr": os.path.join("src", "Sweave2knitr")},
+      version="0.1.4",
+      scripts=[os.path.join("scripts", "Sweave2knitr")],
+      )
