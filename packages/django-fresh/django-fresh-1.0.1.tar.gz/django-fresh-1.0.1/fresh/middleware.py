3ecf63ad2ff0a3daa43cc86da7bdfcb7a2fbfdd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package django-fresh/1.0.1/django-fresh-1.0.1.tar.gz

---
+++ packages/django-fresh/django-fresh-1.0.1.tar.gz/django-fresh-1.0.1/fresh/middleware.py
@@ -0,0 +1,58 @@
+import sys
+import time
+import logging
+import json
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from bs4 import BeautifulSoup
+
+from django.conf import settings
+
+
+fresh = False
+
+
+class RefreshEventHandler(FileSystemEventHandler):
+
+    def on_any_event(self, event):
+        global fresh
+        fresh = True
+
+
+class FreshMiddleware(object):
+
+    def process_response(self, request, response):
+        if not settings.DEBUG:
+            return response
+
+        mimetype = response._headers['content-type'][1]
+        global fresh
+
+        if mimetype == 'application/json':
+            items = json.loads(response.content)
+            if fresh and items.get('fresh') != None:
+                fresh = False
+                items['fresh'] = True
+                response.content = json.dumps(items)
+        elif mimetype == 'text/html; charset=utf-8':
+            soup = BeautifulSoup(response.content)
+            script_fresh = soup.new_tag('script', src='/static/fresh/js/refresher.js')
+            soup.head.append(script_fresh)
+            response.content = soup.prettify()
+
+        return response
+
+    def watcher(self):
+        observer = Observer()
+
+        path = settings.SITE_ROOT
+        event_handler = RefreshEventHandler()
+        observer.schedule(event_handler, path, recursive=True)
+
+        observer.start()
+
+    def __init__(self):
+        if settings.DEBUG:
+            self.watcher()
+
---
+++ packages/django-fresh/django-fresh-1.0.1.tar.gz/django-fresh-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='django-fresh',
+    version='1.0.1',
+    license='Simplified BSD',
+
+    install_requires = [
+        'Django',
+        'watchdog',
+        'beautifulsoup4',
+    ],
+
+    description='Auto-refresh your browser when files in your project change.',
+    long_description=open('README.md').read(),
+
+    author='Isaac Bythewood',
+    author_email='isaac@bythewood.me',
+
+    url='http://github.com/overshard/django-fresh',
+    download_url='http://github.com/overshard/django-fresh/downloads',
+
+    include_package_data=True,
+
+    packages=['fresh'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
