e649ef24a644de1296ae885370d49fffa262395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:40 +0000

    Add package django-mapistration/0.5/django-mapistration-0.5.tar.gz

---
+++ packages/django-mapistration/django-mapistration-0.5.tar.gz/django-mapistration-0.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='django-mapistration',
+    version='0.5',
+    description='Map integration with registration form',
+    author='iamkhush',
+    author_email='ankush.chadda@gmail.com',
+    url='https://github.com/iamkhush/django-mapistration',
+    long_description=open('README.rst', 'r').read(),
+    packages=find_packages(),
+    install_requires=['django'], 
+    zip_safe=False,
+    include_package_data = True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
