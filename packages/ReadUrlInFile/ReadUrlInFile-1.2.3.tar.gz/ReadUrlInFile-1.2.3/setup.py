f470985b9f303f0c2a3971f1cba134c3c1377bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package ReadUrlInFile/1.2.3/ReadUrlInFile-1.2.3.tar.gz

---
+++ packages/ReadUrlInFile/ReadUrlInFile-1.2.3.tar.gz/ReadUrlInFile-1.2.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		= 'ReadUrlInFile',
+	version		= '1.2.3',
+	py_modules	= ['ReadUrlInFile'],
+	author		= 'Chen',
+	author_email	= 'chen.yang.fr@gmail.com',
+	url		= 'http://www.yangchen.fr',
+	description	= 'Sometimes you may want to read the URLs in a file(line by line), and use it directly in a method such like http.client.HTTPConnection(url). In order to avoid a mistake, you need to delete the last char "\\n" in each URL. So this is a simple example, the programe ReadUrlInFile.py reads the URLs in a file named "list", and sends HTTP requesets to these URLs using the module http.client. (You need to manually creat a file including the URLs line by line)',
+)
