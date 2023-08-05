0144929806d961b068d148611709d36d64e6eb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package seao/0.1/seao-0.1.tar.gz

---
+++ packages/seao/seao-0.1.tar.gz/seao-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='seao',
+      version='0.1',
+      description='Seao.ca wrapper.',
+      author='Alain Gilbert',
+      author_email='alain.gilbert.15@gmail.com',
+      packages=['seao'],
+      url='https://github.com/alaingilbert/seao-python',
+      install_requires=['requests', 'beautifulsoup4'],
+     )
