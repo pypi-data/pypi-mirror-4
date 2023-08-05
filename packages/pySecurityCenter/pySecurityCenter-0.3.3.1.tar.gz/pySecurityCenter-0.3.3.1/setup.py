759f19d081027f41b48be26b8ad85f03430d096f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:34 +0000

    Add package pySecurityCenter/0.3.3.1/pySecurityCenter-0.3.3.1.tar.gz

---
+++ packages/pySecurityCenter/pySecurityCenter-0.3.3.1.tar.gz/pySecurityCenter-0.3.3.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import sys
+
+# These are the requirements for pySecurityCenter
+requirements = ['poster',]
+
+if sys.version_info < (2, 6, 0):
+    requirements.append('simplejson==2.1.0')
+if sys.version_info > (3, 0, 0):
+    requirements.append('simplejson')
+    
+
+setup(
+    name='pySecurityCenter',
+    version='0.3.3.1',
+    description='Security Center 4 API Module',
+    author='Steven McGrath',
+    author_email='smcgrath@tenable.com',
+    url='https://github.com/SteveMcGrath/pySecurityCenter',
+    py_modules=['securitycenter'],
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ]
+)
