386350b7fb81a15dedf177909825834698d22abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:49 +0000

    Add package nose-quickunit/0.5.3/nose-quickunit-0.5.3.tar.gz

---
+++ packages/nose-quickunit/nose-quickunit-0.5.3.tar.gz/nose-quickunit-0.5.3/quickunit/utils.py
@@ -0,0 +1,26 @@
+"""
+quickunit.utils
+~~~~~~~~~~~~~~~
+
+:copyright: 2012 DISQUS.
+:license: Apache 2.0
+"""
+import os
+import os.path
+
+
+def is_py_script(filename):
+    "Returns True if a file is a python executable."
+    if not os.path.exists(filename) and os.path.isfile(filename):
+        return False
+    elif filename.endswith(".py"):
+        return True
+    elif not os.access(filename, os.X_OK):
+        return False
+    else:
+        try:
+            with open(filename, "r") as fp:
+                first_line = fp.readline().strip()
+            return "#!" in first_line and "python" in first_line
+        except StopIteration:
+            return False
