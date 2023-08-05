a3990502753b623e7823701da478eb438211c0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package infi.wmpio/0.1.12/infi.wmpio-0.1.12-py2.7.egg

---
+++ packages/infi.wmpio/infi.wmpio-0.1.12-py2.7.egg/infi/wmpio/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.12"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a5cf4f8d09114f85f55e68e1f5df771d470bcf65'
+__git_head_subject__ = 'empty commit after version v0.1.11'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
