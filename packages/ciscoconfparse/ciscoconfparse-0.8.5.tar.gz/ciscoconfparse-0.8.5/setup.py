e1a7c3f2c8714927506503bcfb1462483bec4c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:52 +0000

    Add package ciscoconfparse/0.8.5/ciscoconfparse-0.8.5.tar.gz

---
+++ packages/ciscoconfparse/ciscoconfparse-0.8.5.tar.gz/ciscoconfparse-0.8.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ciscoconfparse',
+      version='0.8.5',
+      description='Parse through Cisco IOS-style configurations and retrieve portions of the config using a variety of query methods',
+      author='David Michael Pennington',
+      author_email='mike /|at|\ pennington.net',
+      license='GPL',
+      keywords='Parse query Cisco IOS configuration',
+      url='http://www.pennington.net/py/ciscoconfparse/',
+      entry_points = "",
+      packages= ['ciscoconfparse'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications',
+          'Topic :: Internet',
+          'Topic :: System :: Networking',
+          'Topic :: System :: Networking :: Monitoring',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+     )
