1dfa57df7037c25f82dbb556ad73d36ecdba0cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:02 +0000

    Add package netlog/0.1/netlog-0.1.tar.gz

---
+++ packages/netlog/netlog-0.1.tar.gz/netlog-0.1/setup.py
@@ -0,0 +1,16 @@
+# codinf: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='netlog',
+    version='0.1',
+    py_modules=['netlog'],
+    url='https://github.com/lemanyk/netlog',
+    download_url='https://github.com/lemanyk/netlog/archive/master.zip',
+    description='logging stream server written on gevent',
+    author='Gennady Leman',
+    author_email='lemanyk@gmail.com',
+    install_requires=['gevent'],
+)
