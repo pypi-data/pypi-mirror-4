2de0c1b46e6de0efc5a68ddd19f45142ba180fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package brotherprint/0.1.1/brotherprint-0.1.1.tar.gz

---
+++ packages/brotherprint/brotherprint-0.1.1.tar.gz/brotherprint-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='brotherprint',
+    version='0.1.1',
+    author='Kyle Petrovich',
+    author_email='kylepetrovich@gmail.com',
+    packages=['brotherprint', 'brotherprint.test'],
+    url='http://github.com/fozzle/python-brotherprint',
+    license='LICENSE.txt',
+    description='Wrapper for Brother networked label printing commands.',
+    long_description=open('README.txt').read(),
+)
