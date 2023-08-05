d29c25a57e374ef4845f46d3d591211187ca5cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:30 +0000

    Add package plonetheme.arnolfini/0.21/plonetheme.arnolfini-0.21.tar.gz

---
+++ packages/plonetheme.arnolfini/plonetheme.arnolfini-0.21.tar.gz/plonetheme.arnolfini-0.21/plonetheme/arnolfini/browser/interfaces.py
@@ -0,0 +1,10 @@
+from plonetheme.intkBase.browser.interfaces import IIntkBaseLayer
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+       If you need to register a viewlet only for the
+       "Arnolfini Theme" theme, this interface must be its layer
+       (in arnolfini/viewlets/configure.zcml).
+    """
