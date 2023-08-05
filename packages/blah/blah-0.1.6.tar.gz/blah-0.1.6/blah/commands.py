2c378d1b8fbfec9b75756ef810643c5214ab2ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:31 +0000

    Add package blah/0.1.6/blah-0.1.6.tar.gz

---
+++ packages/blah/blah-0.1.6.tar.gz/blah-0.1.6/blah/commands.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+import blah.repositories
+import blah.fetcher
+import blah.errors
+
+class WhatIsThisCommand(object):
+    def create_parser(self, subparser):
+        subparser.add_argument("directory", nargs="?")
+    
+    def execute(self, args):
+        directory = args.directory if args.directory is not None else os.getcwd()
+        repository = blah.repositories.find_repository(directory)
+        if repository is None:
+            print "Could not find source control repository"
+        else:
+            print "{0}+file://{1}".format(repository.type, repository.working_directory)
+
+what_is_this_command = WhatIsThisCommand()
+
+class FetchCommand(object):
+    def create_parser(self, subparser):
+        subparser.add_argument("repository_uri", metavar="repository-uri")
+        subparser.add_argument("local_path", metavar="local-path")
+        subparser.add_argument("--use-cache", default=False, help=argparse.SUPPRESS, action="store_true")
+    
+    def execute(self, args):
+        try:
+            blah.fetcher.fetch(args.repository_uri, args.local_path, args.use_cache)
+        except (blah.errors.BlahUserError, blah.util.NoSuchCommandError) as error:
+            print "fetch failed: {0}".format(error.message)
+            exit(-1)
+
+commands = {
+    "whatisthis": what_is_this_command,
+    "what-is-this": what_is_this_command,
+    "fetch": FetchCommand()
+}
