dcfab60dcda2a7ee9737f53e06b916b21169a4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:11 +0000

    Add package requests-foauth/0.1.1/requests-foauth-0.1.1.tar.gz

---
+++ packages/requests-foauth/requests-foauth-0.1.1.tar.gz/requests-foauth-0.1.1/requests_foauth.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+requests_foauth
+~~~~~~~~~~~~~~~
+
+This module provides an foauth.org TransportAdapter for Requests.
+
+In short, foauth.org allows you to easily send requests to OAUth'd
+services with HTTP Basic authentication. It's fantastic.
+
+Normally, you have to send all requests to foauth.org manually, after
+rewriting the URLs. With this adapter, all requests are sent through the
+service transparently.
+
+Example usage::
+
+    import requests
+    from requests_foauth import Foauth
+
+    s = requests.Session()
+    s.mount('http', Foauth('username', 'password))
+
+    >>> s.get('https://api.500px.com/v1/users/')
+    <Response [200]>
+"""
+
+
+from requests.adapters import BaseAdapter, HTTPAdapter
+from requests.compat import urlparse
+
+FOAUTH_TEMPLATE = 'https://foauth.org/{domain}{path}'
+
+class Foauth(BaseAdapter):
+    """The foauth.org transport adapter."""
+    def __init__(self, username, password):
+        self.auth = (username, password)
+        self.http = HTTPAdapter()
+
+    def prepare_request(self, request):
+        p = urlparse(request.url)
+
+        # Rewrite the url to use foauth.org
+        path = p.path
+        if p.query:
+            path += '?' + p.query
+
+        request.url = FOAUTH_TEMPLATE.format(domain=p.netloc, path=path)
+        # Authenticate appropriately.
+        request.prepare_auth(self.auth)
+
+        return request
+
+    def send(self, request, **kwargs):
+        request = self.prepare_request(request)
+        return self.http.send(request, **kwargs)
+
---
+++ packages/requests-foauth/requests-foauth-0.1.1.tar.gz/requests-foauth-0.1.1/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+requests-foauth
+~~~~~~~~~~~~~~~
+
+This module provides an foauth.org TransportAdapter for Requests.
+
+In short, foauth.org allows you to easily send requests to OAUth'd
+services with HTTP Basic authentication. It's fantastic.
+
+Normally, you have to send all requests to foauth.org manually, after
+rewriting the URLs. With this adapter, all requests are sent through the
+service transparently.
+
+Example usage::
+
+    import requests
+    from requests_foauth import Foauth
+
+    s = requests.Session()
+    s.mount('http', Foauth('username', 'password))
+
+    >>> s.get('https://api.500px.com/v1/users/')
+    <Response [200]>
+"""
+
+from setuptools import setup
+
+setup(
+    name='requests-foauth',
+    version='0.1.1',
+    url='https://github.com/kennethreitz/requests-foauth',
+    license='BSD',
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Requests TransportAdapter for foauth.org!',
+    long_description=__doc__,
+    py_modules=['requests_foauth'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
