cba22994e130857f484412b03cb548a5485c816e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package django-analytics-client/0.1.5/django-analytics-client-0.1.5.tar.gz

---
+++ packages/django-analytics-client/django-analytics-client-0.1.5.tar.gz/django-analytics-client-0.1.5/analytics_client/__init__.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from analytics_client.client import Metric
+
+version_info = (0, 1, 5)
+__version__ = '.'.join(map(str, version_info))
+
+
+#####################
+# Utility functions #
+#####################
+
+def save_metric(key, value, timestamp=None):
+    """
+    Wrapper used to save a metric using a celery task.
+    """
+
+    from analytics_client.settings import _ANALYTICS_ENABLED
+
+    if not _ANALYTICS_ENABLED:
+        return None
+
+    from analytics_client.tasks import store_metric
+
+    # Set a timestamp if it is undefined
+    _timestamp = timestamp
+    if _timestamp is None:
+        _timestamp = datetime.now()
+
+    store_metric.delay(Metric(key=key, value=value, timestamp=_timestamp))
