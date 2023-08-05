d37eef74806bfd9583b5631303281373696a2332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:53 +0000

    Add package Moments/2.0/Moments-2.0.tar.gz

---
+++ packages/Moments/Moments-2.0.tar.gz/Moments-2.0/README.txt
@@ -0,0 +1,37 @@
+*2011
+Deep Breath... Inhale.... Exhale.... 
+
+Greetings!
+Welcome to Moments!
+
+Moments is available at bitbucket.org/cbrandt/moments. 
+Moments is free and open source software. 
+Moments is released under the MIT license.  (See: LICENSE.txt)
+
+This software was made possible by the love of TCB, my family, my teachers and my friends.  
+Thank you!
+
+Have fun. Be happy. Be free. Namaste.
+
+DOCS:
+---------
+Complete documentation can be found in:
+docs/
+
+To generate the documentation, you will need Sphinx:
+sudo easy_install sphinx
+
+cd docs/
+sphinx-build -b html . ./_build
+
+or
+make html
+
+INSTALL:
+----------
+
+see docs/installation.txt
+
+and
+
+docs/_build/html/installation.html
---
+++ packages/Moments/Moments-2.0.tar.gz/Moments-2.0/setup.py
@@ -0,0 +1,44 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import sys, os
+
+version = '2.0'
+
+setup(
+    name='Moments',
+    version=version,
+    description="Python package to process moments log files",
+    long_description="""Moment log files are simple text documents with the \
+        format:
+        ::
+        
+        *YYYY.MM.DD hh:mm [tags]
+        [entry]
+        \n
+
+        This simple format allows your notes to:
+	  -stay accessible (no proprietary formats)
+	  -be sorted by time or tag
+	  -easily synchronize across many machines using a distributed version control system
+    """,
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='moments journal log self_tracking logging blog entry time quantified_self evernote emacs time time_management history',
+    author='Charles Brandt',
+    author_email='code@charlesbrandt.com',
+    url='http://bitbucket.org/cbrandt/moments',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'setuptools',
+    ],
+    entry_points="""
+    """,
+)
