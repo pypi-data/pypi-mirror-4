20f9b5e1a3497197121203fa3e65d0a357905773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package kabuki/0.4RC4/kabuki-0.4RC4.tar.gz

---
+++ packages/kabuki/kabuki-0.4RC4.tar.gz/kabuki-0.4RC4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="kabuki",
+    version="0.4RC4",
+    author="Thomas V. Wiecki, Imri Sofer",
+    author_email="thomas.wiecki@gmail.com",
+    url="http://github.com/hddm-devs/kabuki",
+    packages=["kabuki"],
+    description="kabuki is a python toolbox that allows easy creation of hierarchical bayesian models for the cognitive sciences.",
+    install_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'ordereddict >= 1.1', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0'],
+    setup_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'ordereddict >= 1.1', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0']
+)
