5430d8af7e5480ede9a3ca983ef524a2b39afd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:18 +0000

    Add package django-pyfixtures/0.1.1/django-pyfixtures-0.1.1.tar.gz

---
+++ packages/django-pyfixtures/django-pyfixtures-0.1.1.tar.gz/django-pyfixtures-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django-pyfixtures',
+    version='0.1.1',
+    author='Chase Seibert',
+    author_email='chase.seibert@gmail.com',
+    packages=['pyfixtures'],
+    url='https://github.com/chase-seibert/django-pyfixtures',
+    download_url='https://github.com/chase-seibert/django-pyfixtures/tarball/master',
+    license='LICENSE.txt',
+    description='dumpdata generates python source code files as the fixtures',
+)
