5afc553c6756394c2e34cdfb6e4fa9c32c9481af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:10 +0000

    Add package hermes/0.2.6/hermes-0.2.6.tar.gz

---
+++ packages/hermes/hermes-0.2.6.tar.gz/hermes-0.2.6/hermes/__init__.py
@@ -0,0 +1,23 @@
+"""
+hermes Chatroom server
+~~~~~~~~~~~~~~~~~~~~~~
+
+Hermes is an extensible XMPP-based chatroom server written in Python.
+
+Project homepage: https://github.com/mattlong/hermes
+
+:copyright: (c) 2012 by Matt Long.
+:license: ISC, see LICENSE for more details.
+"""
+
+from .server import run_server
+from .chatroom import Chatroom
+#from .version import VERSION_STRING
+
+__author__ = "Matt Long"
+__copyright__ = "Copyright 2012, Matt Long"
+__license__ = "ISC"
+__version__ = '0.2.6'
+__maintainer__ = "Matt Long"
+__status__ = "Development"
+
