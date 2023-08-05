2863143b04fd39e3003e6abd61080e7aafc179ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:44 +0000

    Add package infi.recipe.console_scripts/0.3.1/infi.recipe.console_scripts-0.3.1.tar.gz

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.3.1.tar.gz/infi.recipe.console_scripts-0.3.1/src/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0d0a2577897a5621726907b159ad91cedb2a79bf'
+__git_head_subject__ = 'Finished release v0.3.1.'
+__git_head_message__ = '# By Guy Rozendorn (4) and Arnon Yaari (1)\n* release/v0.3.1:\n  buildout.cfg: adding zc.recipe.egg==1.3.2 to requirements\n  buildout.cfg: remove zc.recipe.egg from requirements\n  buildout.cfg: removing gui-script-test from gui_scripts\n  buildout.cfg: removing console-script-test from console_scripts\n  empty commit after version v0.3'
+__git_dirty_diff__ = ''
