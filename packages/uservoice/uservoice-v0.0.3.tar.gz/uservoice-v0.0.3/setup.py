475a9fc052dc1f426cc0245b011016605497fb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package uservoice/v0.0.3/uservoice-v0.0.3.tar.gz

---
+++ packages/uservoice/uservoice-v0.0.3.tar.gz/uservoice-v0.0.3/setup.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='uservoice',
+      version='v0.0.3',
+      description='UserVoice Python library',
+      long_description=re.sub(r'```[^\s]*', '', open('README.md').read()),
+      author='Raimo Tuisku',
+      author_email='dev@uservoice.com',
+      packages=['uservoice'],
+      install_requires=[
+          'simplejson',
+          'pycrypto',
+          'pytz',
+          'PyYAML',
+          'requests-oauth'],
+      test_suite='test',
+)
