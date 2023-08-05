d861277444328ddd5ad4fc3c4814ac46eaf3388b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package djpg/0.1.1/djpg-0.1.1.tar.gz

---
+++ packages/djpg/djpg-0.1.1.tar.gz/djpg-0.1.1/djpg/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'djpg'
+__version__ = '0.1.1'
+__author__ = 'Rafael Canovas'
+__contact__ = 'rafaelcanovas@me.com'
+
+from .codes import codes
+from .models import Cart, Item
+from . import signals
