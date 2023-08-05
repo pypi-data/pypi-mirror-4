3539df5dd6def0ab6b444cfa85fdcc8e05eba920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package djangopypi2/0.5.1/djangopypi2-0.5.1.tar.gz

---
+++ packages/djangopypi2/djangopypi2-0.5.1.tar.gz/djangopypi2-0.5.1/djangopypi2/apps/pypi_frontend/admin.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Package)
+admin.site.register(models.Release)
+admin.site.register(models.Distribution)
+admin.site.register(models.Review)
