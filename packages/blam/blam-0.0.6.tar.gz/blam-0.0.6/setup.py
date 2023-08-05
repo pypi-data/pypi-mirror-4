67d702660f0f69c8943b04a03eea6a72d6897a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package blam/0.0.6/blam-0.0.6.tar.gz

---
+++ packages/blam/blam-0.0.6.tar.gz/blam-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+with open('README.txt') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='blam',
+    version='0.0.6',
+    author='Phil Adams',
+    author_email='phil@philadams.net',
+    license=license,
+    long_description=readme,
+    packages=['blam'],
+    scripts=['bin/blam'],
+    url='http://github.com/philadams/blam',
+    description='manage text snippets on the command line.',
+    install_requires=[],
+)
