4aeb43709248c1328f7e756f6a2cc4f2dc52e829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:19 +0000

    Add package django-blogger/0.2.0.8/django-blogger-0.2.0.8.tar.gz

---
+++ packages/django-blogger/django-blogger-0.2.0.8.tar.gz/django-blogger-0.2.0.8/setup.py
@@ -0,0 +1,16 @@
+import os
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-blogger",
+    version = "0.2.0.8",
+    author = "Jake Gaylor",
+    author_email = "jake@codegur.us",
+    description = "A package to administer a simple blog via Django.",
+    url = "https://github.com/jhgaylor/django-simple-blog/",
+    install_requires = [ln for ln in open('requirements.txt')],
+    packages = find_packages(), 
+    #package_data=package_data,
+    include_package_data = True,
+)
