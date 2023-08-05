3c6090a8f3357ac4dbf637d122859f0a7231c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:25 +0000

    Add package twistar/1.1/twistar-1.1.tar.gz

---
+++ packages/twistar/twistar-1.1.tar.gz/twistar-1.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="twistar",
+    version="1.1",
+    description="An implementation of the Active Record pattern for Twisted",
+    author="Brian Muller",
+    author_email="bamuller@gmail.com",
+    license="GPLv3",
+    url="http://findingscience.com/twistar",
+    packages=find_packages(),
+    requires=["twisted.enterprise.adbapi"],
+    install_requires=['twisted>=12.0']
+)
