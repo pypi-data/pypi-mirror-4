9e317da36a4da903748a810b0d0ddff3505a4519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:58 +0000

    Add package infi.projector/0.5.2/infi.projector-0.5.2.tar.gz

---
+++ packages/infi.projector/infi.projector-0.5.2.tar.gz/infi.projector-0.5.2/src/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f53e4b3f731440637d8d0c25f0c15b015c3cd823'
+__git_head_subject__ = 'Finished release v0.5.2.'
+__git_head_message__ = '* release/v0.5.2:\n  Freezing dependencies\n  TRIVIAL fix typo\n  Unfreezing dependencies\n  empty commit after version v0.5.1'
+__git_dirty_diff__ = ''
