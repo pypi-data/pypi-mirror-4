6304c5d751338dc26fe60c687727044b8e851809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:04 +0000

    Add package slc.outdated/1.3/slc.outdated-1.3.zip

---
+++ packages/slc.outdated/slc.outdated-1.3.zip/slc.outdated-1.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.3'
+shortdesc = "Mark and treat plone content as outdated"
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(name='slc.outdated',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Framework :: Plone',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+      ], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Florian Friesdorf',
+      author_email='friesdorf@syslab.com',
+      url='https://github.com/collective/slc.outdated',
+      license='General Public Licence',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['slc'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'mock',
+          ],
+      },
+      entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+      """,
+      )
