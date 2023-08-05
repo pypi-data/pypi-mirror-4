8108263154e2b2fda7c83093476cb50e498d6635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:17 +0000

    Add package pytool/1.1.1/pytool-1.1.1.tar.gz

---
+++ packages/pytool/pytool-1.1.1.tar.gz/pytool-1.1.1/setup.py
@@ -0,0 +1,28 @@
+import multiprocessing, logging # Fix atexit bug
+from setuptools import setup, find_packages
+
+import pytool
+
+def readme():
+    try:
+        return open('README.rst').read()
+    except:
+        pass
+    return ''
+
+setup(
+        name='pytool',
+        version=pytool.__version__,
+        author="Jacob Alheid",
+        author_email="jake@about.me",
+        description="A Collection of Python Tools",
+        long_description=readme(),
+        url='http://github.com/shakefu/pytool',
+        packages=find_packages(exclude=['test']),
+        test_suite='nose.collector',
+        tests_require=[
+            'nose',
+            'mock',
+            ],
+        )
+
