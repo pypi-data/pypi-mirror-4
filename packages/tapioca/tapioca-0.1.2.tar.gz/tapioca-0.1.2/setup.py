2dd4674781befa869ca898557e4fb3c7efc39a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package tapioca/0.1.2/tapioca-0.1.2.tar.gz

---
+++ packages/tapioca/tapioca-0.1.2.tar.gz/tapioca-0.1.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__version__ = '0.1.2'
+
+
+setup(
+    name = 'tapioca',
+    version = __version__,
+    description = "Tapioca is a small and flexible micro-framework on top of "\
+            "Tornado. It provides a simpler way to create RESTful API's.",
+    long_description = """
+        Tapioca is a small and flexible micro-framework on top of Tornado.
+        It provides a simpler way to create RESTful API's.
+    """,
+    keywords = 'restful rest api tornado',
+    author = 'globo.com',
+    author_email = 'lambda@corp.globo.com',
+    url = 'https://github.com/globocom/tapioca',
+    license = 'MIT',
+    classifiers = ['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 2.6',
+    ],
+    packages = ['tapioca'],
+    package_dir = {"tapioca": "tapioca"},
+    include_package_data = True,
+    install_requires=[
+      "tornado>=2.4",
+      "mimeparse>=0.1.3"
+    ]
+)
