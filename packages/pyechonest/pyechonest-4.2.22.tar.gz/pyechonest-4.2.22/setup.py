174b2db43b5f5c4439828cf927e17fa0aa0374c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:57 +0000

    Add package pyechonest/4.2.22/pyechonest-4.2.22.tar.gz

---
+++ packages/pyechonest/pyechonest-4.2.22.tar.gz/pyechonest-4.2.22/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__version__ = "4.2.22"
+
+# $Source$
+from sys import version
+import os
+from setuptools import setup
+
+if version < '2.6':
+    requires=['urllib', 'urllib2', 'simplejson']
+elif version >= '2.6':
+    requires=['urllib', 'urllib2', 'json']
+else:
+    #unknown version?
+    requires=['urllib', 'urllib2']
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pyechonest',
+    version=__version__,
+    description='Python interface to The Echo Nest APIs.',
+    long_description=read('README.md'),
+    author='Tyler Williams',
+    author_email='tyler@echonest.com',
+    maintainer='Tyler Williams',
+    maintainer_email='tyler@echonest.com',
+    url='https://github.com/echonest/pyechonest',
+    download_url='https://github.com/echonest/pyechonest',
+    package_dir={'pyechonest':'pyechonest'},
+    packages=['pyechonest'],
+    requires=requires,
+    setup_requires = ['github-distutils >= 0.1.0'],
+)
