206c040e197ed254f7796a343daab87ed0b409a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package dagdo/0.1/dagdo-0.1.tar.gz

---
+++ packages/dagdo/dagdo-0.1.tar.gz/dagdo-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dagdo',
+      description='Dependency and deadline-aware task tracking',
+      version='0.1',
+      author='Nathan Wilcox',
+      author_email='nejucomo@gmail.com',
+      license='GPLv2',
+      url='https://bitbucket.org/nejucomo/dagdo',
+      scripts=['dagdo.py'],
+     )
