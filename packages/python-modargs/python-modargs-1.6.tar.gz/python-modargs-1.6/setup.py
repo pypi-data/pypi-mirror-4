5003b41df27c21f8d9a1dbad3646c9878929fb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:30 +0000

    Add package python-modargs/1.6/python-modargs-1.6.tar.gz

---
+++ packages/python-modargs/python-modargs-1.6.tar.gz/python-modargs-1.6/setup.py
@@ -0,0 +1,21 @@
+## this file is generated from settings in build.vel
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# from options["setup"] in build.vel
+config = {
+ 'description': 'modargs is a simple command line argument parsing library that infers arguments from functions in a module',
+ 'author': 'Zed A. Shaw',
+ 'url': 'http://pypi.python.org/pypi/python-modargs',
+ 'download_url': 'http://pypi.python.org/pypi/python-modargs',
+ 'author_email': 'zedshaw@zedshaw.com',
+ 'version': '1.6',
+ 'install_requires': ['mock', 'nose'],
+ 'packages': ['modargs'],
+ 'name': 'python-modargs'
+}
+setup(**config)
+
