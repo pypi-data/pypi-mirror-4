bf820007573e64692e2a7b40ba549de5d2887b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package indeed/0.0.2/indeed-0.0.2.tar.gz

---
+++ packages/indeed/indeed-0.0.2.tar.gz/indeed-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'indeed',
+    version = open("./VERSION").read(),
+    description = 'Indeed Job Search Python Api Client',
+    author = 'Indeed Labs',
+    author_email = 'labs-team@indeed.com',
+    packages = ['indeed'],
+    install_requires=['requests==0.14.1'],
+    url = "https://github.com/indeedlabs/indeed-python",
+)
