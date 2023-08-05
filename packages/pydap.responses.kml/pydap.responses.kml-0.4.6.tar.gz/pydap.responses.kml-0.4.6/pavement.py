a74c3c5aabdab9e99f9f5dfd5658af99a0ed381f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package pydap.responses.kml/0.4.6/pydap.responses.kml-0.4.6.tar.gz

---
+++ packages/pydap.responses.kml/pydap.responses.kml-0.4.6.tar.gz/pydap.responses.kml-0.4.6/pavement.py
@@ -0,0 +1,73 @@
+import string
+
+from paver.easy import *
+from paver.setuputils import *
+import paver.doctools
+from paver.release import setup_meta
+
+__version__ = (0,4,6)
+
+options = environment.options
+setup(**setup_meta)
+
+options(
+    setup=Bunch(
+        name='pydap.responses.kml',
+        version='.'.join(str(d) for d in __version__),
+        description='KML response for Pydap',
+        long_description='''
+Pydap is an implementation of the Opendap/DODS protocol, written from
+scratch. This response enables the visualization of the data using
+Google Earth.
+        ''',
+        keywords='kml opendap dods dap data science climate oceanography meteorology',
+        classifiers=filter(None, map(string.strip, '''
+            Development Status :: 5 - Production/Stable
+            Environment :: Console
+            Environment :: Web Environment
+            Framework :: Paste
+            Intended Audience :: Developers
+            Intended Audience :: Science/Research
+            License :: OSI Approved :: MIT License
+            Operating System :: OS Independent
+            Programming Language :: Python
+            Topic :: Internet
+            Topic :: Internet :: WWW/HTTP :: WSGI
+            Topic :: Scientific/Engineering
+            Topic :: Software Development :: Libraries :: Python Modules
+        '''.split('\n'))),
+        author='Roberto De Almeida',
+        author_email='rob@pydap.org',
+        url='http://pydap.org/responses.html#kml',
+        license='MIT',
+
+        packages=find_packages(),
+        package_data=find_package_data("pydap", package="pydap",
+                only_in_packages=False),
+        include_package_data=True,
+        zip_safe=False,
+        namespace_packages=['pydap', 'pydap.responses'],
+
+        test_suite='nose.collector',
+
+        dependency_links=[],
+        install_requires=[
+            'Pydap',
+            'pydap.responses.kml',
+        ],
+        entry_points="""
+            [pydap.response]
+            kml = pydap.responses.kml:KMLResponse
+        """,
+    ),
+    minilib=Bunch(
+        extra_files=['doctools', 'virtual']
+    ),
+)
+
+
+@task
+@needs(['generate_setup', 'minilib', 'setuptools.command.sdist'])
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
