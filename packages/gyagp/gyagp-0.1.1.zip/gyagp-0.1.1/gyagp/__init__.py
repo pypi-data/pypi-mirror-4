4c4c7454da8ad71ab5a3436d888052d5d4b4776c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package gyagp/0.1.1/gyagp-0.1.1.zip

---
+++ packages/gyagp/gyagp-0.1.1.zip/gyagp-0.1.1/gyagp/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__version__ = "0.1.1"
+
+def check():
+    print('I am gyagp')
+        
+__all__ = [
+    # exceptions
+
+    # constants
+    
+    # classes
+    
+    # functions
+    'check'
+    ]        
---
+++ packages/gyagp/gyagp-0.1.1.zip/gyagp-0.1.1/test/test_gyagp.py
@@ -0,0 +1,26 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_win32api(self):
+        try:
+            import win32api
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False) 
+        
+    def test_gyagp(self):
+        try:
+            import gyagp
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False)   
+
+    def test_gyagp_module(self):
+        try:
+            from gyagp import module
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False)            
+
+if __name__ == "__main__":
+    unittest.main()
