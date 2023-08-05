c427c08c7f8cad228fd24356daba7459d103095d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package Word-game/0.0.1/Word%20game-0.0.1.zip

---
+++ packages/Word-game/Word%20game-0.0.1.zip/Word game-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Word game',
+    version = '0.0.1',
+    py_module = ['wordgame'],
+    author = 'Nehil',
+    author_email = 'jain.nehil@gmail.com',
+    url = 'nehil.e-caffeine.net',
+    description = 'A simple word game'
+    )
