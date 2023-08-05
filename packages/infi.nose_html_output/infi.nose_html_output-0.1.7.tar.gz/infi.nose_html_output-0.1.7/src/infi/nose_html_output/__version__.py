30147db748af931994bb182cd88279a0d2b1ad7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:33 +0000

    Add package infi.nose_html_output/0.1.7/infi.nose_html_output-0.1.7.tar.gz

---
+++ packages/infi.nose_html_output/infi.nose_html_output-0.1.7.tar.gz/infi.nose_html_output-0.1.7/src/infi/nose_html_output/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '03f331c6d11b8836c5616ea5a6332a7923bcff15'
+__git_head_subject__ = 'empty commit after version v0.1.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
