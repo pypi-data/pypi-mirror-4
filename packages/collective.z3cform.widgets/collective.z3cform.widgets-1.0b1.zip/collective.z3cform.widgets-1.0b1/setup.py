38a72905ccadb635faab41069b75d38ef71b8179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package collective.z3cform.widgets/1.0b1/collective.z3cform.widgets-1.0b1.zip

---
+++ packages/collective.z3cform.widgets/collective.z3cform.widgets-1.0b1.zip/collective.z3cform.widgets-1.0b1/setup.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+version = '1.0b1'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
+
+setup(name='collective.z3cform.widgets',
+      version=version,
+      description="A widget package for Dexterity projects.",
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces",
+        ],
+      keywords='plone z3c widgets dexterity',
+      author='Gonzalo Almeida',
+      author_email='flecox@ravvit.net',
+      url='https://github.com/collective/collective.z3cform.widgets',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective', 'collective.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'Products.CMFPlone>=4.1',
+        'plone.app.dexterity>=1.2.1',
+        'plone.formwidget.autocomplete>=1.2.0',
+        'plone.z3cform>=0.7.4',
+        'z3c.formwidget.query',
+        'collective.js.jqueryui',
+        ],
+      extras_require={
+        'test': ['plone.app.testing', 'plone.formwidget.contenttree'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ packages/collective.z3cform.widgets/collective.z3cform.widgets-1.0b1.zip/collective.z3cform.widgets-1.0b1/src/collective/z3cform/widgets/interfaces.py
@@ -0,0 +1,10 @@
+
+from z3c.form import interfaces
+
+class ITokenInputWidget(interfaces.ITextLinesWidget):
+    """Text lines widget."""
+
+from z3c.form import interfaces
+
+class IEnhancedTextLinesWidget(interfaces.ITextLinesWidget):
+    """Text lines widget."""
