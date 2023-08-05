e7bd9156c1bb2f09c83f6066847147145a1cb510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package moa/0.11.6/moa-0.11.6.tar.gz

---
+++ packages/moa/moa-0.11.6.tar.gz/moa-0.11.6/moa/plugin/system/configure.py
@@ -0,0 +1,330 @@
+# Copyright 2009-2011 Mark Fiers
+# The New Zealand Institute for Plant & Food Research
+#
+# This file is part of Moa - http://github.com/mfiers/Moa
+#
+# Licensed under the GPL license (see 'COPYING')
+#
+"""
+**configure** - Configure jobs
+------------------------------
+
+Control job configuration
+"""
+
+import moa.ui
+import moa.utils
+import moa.args
+import textwrap
+
+from moa.sysConf import sysConf
+
+
+##
+## Show command
+##
+@moa.args.addFlag('-a', dest='showAll', help='show all parameters')
+@moa.args.addFlag('-p', dest='showPrivate', help='show private parameters')
+@moa.args.addFlag('-R', dest='showRecursive', help='show recursively defined '
+                  + 'parameters not specified by the local template')
+@moa.args.addFlag('-u', dest='showUnrendered', help='show unrendered values ' +
+                  '(when using inline parameters)')
+@moa.args.needsJob
+@moa.args.doNotLog
+@moa.args.command
+def show(job, args):
+    """
+    Show all parameters know to this job.
+
+    Parameters in **bold** are specifically configured for this job
+    (as opposed to those parameters that are set to their default
+    value). Parameters in red are not configured, but need to be for
+    the template to operate. Parameters in blue are not configured
+    either, but are optional.
+    """
+    moa.utils.moaDirOrExit(job)
+
+    keys = job.conf.keys()
+    keys.sort()
+
+    outkeys = []
+    outvals = []
+    outflags = []
+
+    rendered = job.conf.render()
+
+    for p in keys:
+
+        isPrivate = False
+
+        if p[:4] == 'moa_':
+            isPrivate = True
+
+        if p[0] == '_':
+            isPrivate = True
+
+        isPrivate = job.conf.isPrivate(p)
+
+        if p[0] == '_':
+            isPrivate = True
+        if p[:4] == 'moa_':
+            isPrivate = True
+
+        isOptional = job.template.parameters[p].get('optional', True)
+        isLocal = job.conf.is_local(p)
+        isDefined = len(str(job.conf[p])) > 0
+
+        # args.showPrivate:
+        # args.showAll
+        # args.showRecursive
+
+        if isPrivate and not args.showPrivate:
+            continue
+
+        if not args.showAll:
+            if not isLocal and isOptional:
+                continue
+
+        outkeys.append(p)
+
+        #print '%s "%s" %s' % (p, job.conf[p], isDefined)
+
+        key = ''
+        if not isDefined:
+            key += '{{gray}}.{{reset}}'
+            outvals.append('')
+        elif isLocal:
+            outvals.append(str(job.conf[p]))
+            key += '{{bold}}{{green}}l{{reset}}'
+        else:
+            outvals.append(str(job.conf[p]))
+            key += '{{magenta}}r{{reset}}'
+
+        if isPrivate:
+            key += 'p'
+        else:
+            key += '{{gray}}.{{reset}}'
+
+        if isOptional:
+            key += '{{gray}}o{{reset}}'
+        else:
+            key += '{{green}}{{bold}}M{{reset}}'
+
+        outflags.append(key)
+
+        #     #yes: locally?
+        #     if job.conf.is_local(p):
+        #         outflags.append('{{green}}L{{reset}}')
+        #     else:
+        #         outflags.append('{{magenta}}R{{reset}}')
+        #     outvals.append(job.conf[p])
+        # else:
+        #     #not defined - does it need to be??
+        #     if job.template.parameters[p].optional:
+        #         val = job.conf[p]
+        #         if val != None:
+        #             outvals.append(
+        #                 moa.ui.fformat('{{gray}}%s{{reset}}' % val, f='j'))
+        #         else:
+        #             outvals.append(
+        #                 moa.ui.fformat(
+        #                     '{{gray}}(undefined){{reset}}', f='j'))
+        #     else:
+        #         #not optional
+        #         outflags.append('{{bold}}{{red}}E{{reset}}')
+        #         outvals.append(
+        #             moa.ui.fformat(
+        #                 '{{red}}{{bold}}(undefined){{reset}}', f='j'))
+
+    maxKeylen = max([len(x) for x in outkeys]) + 1
+
+    termx, termy = moa.utils.getTerminalSize()
+
+    wrapInit = termx - (maxKeylen + 5)
+    spacer = ' ' * (maxKeylen + 5)
+    spacerR = ' ' * (maxKeylen + 1) + \
+              moa.ui.fformat('{{gray}}r ', newline=False, f='j')
+    closeR = moa.ui.fformat('{{reset}}', newline=False, f='j')
+
+    #print outkeys
+    zipped = zip(outkeys, outvals, outflags)
+
+    zipped.sort(lambda x, y: cmp(x[0].lstrip('_'), y[0].lstrip('_')))
+    #print outkeys
+
+    for i, zippy in enumerate(zipped):
+        key, val, flag = zippy
+        if not args.showAll:
+            if not val:
+                continue
+
+        moa.ui.fprint(("%%-%ds" % maxKeylen) % key, f='jinja', newline=False)
+        moa.ui.fprint(" " + flag + " ", f='jinja', newline=False)
+        if len(str(val)) == 0:
+            print
+
+        #print str( val)
+        #print textwrap.wrap(str(val), wrapInit)
+
+        if args.showUnrendered:
+            mainval = val
+        else:
+            renval = rendered[key]
+            if str(renval):
+                mainval = renval
+            else:
+                mainval = val
+
+        for j, ll in enumerate(textwrap.wrap(str(mainval), wrapInit)):
+            if j == 0:
+                moa.ui.fprint(ll, f=None)
+            else:
+                moa.ui.fprint(spacer + ll, f=None)
+
+        if args.showUnrendered and rendered[key] and rendered[key] != val:
+            for j, ll in enumerate(textwrap.wrap(str(rendered[key]),
+                                                 wrapInit)):
+                moa.ui.fprint(spacerR + ll + closeR)
+
+
+@moa.args.argument('parameter', nargs='+', help='parameter to unset')
+@moa.args.needsJob
+@moa.args.command
+def unset(job, args):
+    """
+    Remove a parameter from the configuration
+
+    Remove a configured parameter from this job. In the parameter was
+    defined by the job template, it reverts back to the default
+    value. If it was an ad-hoc parameter, it is lost from the
+    configuration.
+    """
+
+    for a in args.parameter:
+        if '=' in a:
+            moa.ui.exitError("Invalid argument to unset %s" % a)
+        try:
+            del job.conf[a]
+            moa.ui.message('unset %s' % a)
+        except KeyError:
+            #probably a non existsing key - ignor
+            moa.ui.warn('failed to unset %s' % a)
+            pass
+    job.conf.save()
+
+
+@moa.args.argument('parameter', nargs='+',
+                   help='arguments for this job, specify' +
+                   'as KEY=VALUE without spaces')
+@moa.args.addFlag('-s', '--system', help='store this a ' +
+                  'system configuration variable')
+@moa.args.forceable
+@moa.args.command
+def set(job, args):
+    """Set one or more variables
+
+    This command can be used in two ways. In its first form both
+    parameter key and value are defined on the command line: `moa set
+    KEY=VALUE`. Note that the command line will be processed by bash,
+    which can either create complications or prove very useful. Take
+    care to escape variables that you do not want to be expandend and
+    use single quotes where necessary. For example, to include a space
+    in a variable: `moa set KEY='VALUE WITH SPACES'`.
+
+    Alternative use of the set command is by just specifying the key:
+    'moa set PARAMETER_NAME', in which case Moa will prompt the user
+    enter a value - circumventing problems with bash interpretation.
+
+    Note: without -s, moa needs to be executed from within a Moa job
+
+    System configuration
+    ####################
+
+    By specifying `-s` or `--system`, the variable is stored as a
+    system configuration variable in the YAML formatted
+    `~/.config/moa/config`. Please, use this with care!
+
+    The dots in the key name are interpreted as nested levels, so,
+    running::
+
+        moa set -s plugins.job.completion.enabled=false
+
+    will result in the following section added on top of the YAML::
+
+        plugins:
+            job:
+                completion:
+                    enabled: false
+
+    Adding keys like this mixes safely with configuration information
+    that is already present. So, setting::
+
+        moa set -s plugins.job.completion.something=else
+
+    will not remove the `enabled: false` heading under `completion:`,
+    resulting in::
+
+
+        plugins:
+            job:
+                completion:
+                    enabled: false
+                    someting: else
+
+    """
+
+    #see if we need to query the user for input somehwere
+    if not args.system:
+        moa.utils.moaDirOrExit(job)
+
+    new_pars = []
+    for a in args.parameter:
+
+        if not '=' in a:
+            old = job.conf[a]
+            val = moa.ui.askUser("%s:\n> " % a, old)
+        else:
+            key, val = a.split('=', 1)
+
+        if not args.system:
+            job.conf[key] = val
+
+        new_pars.append((key, val))
+        moa.ui.message('setting "%s" to "%s"' % (key, " ".join(val.split())))
+
+    if args.system:
+
+        sys_pars = dict(new_pars)
+
+        valCheck = {
+            'true': True,
+            'false': False}
+
+        def _dictify(d):
+            for k in list(d.keys()):
+                if '.' in k:
+
+                    na, nb = k.split('.', 1)
+                    d[na] = _dictify({nb: d[k]})
+                    del d[k]
+                else:
+                    val = d[k]
+                    if val.lower() in valCheck:
+                        val = valCheck[val.lower()]
+                        d[k] = val
+            return d
+
+        userConf = sysConf.getUser()
+        userConf.update(_dictify(sys_pars))
+        sysConf.saveUser(userConf)
+    else:
+        job.conf.save()
+
+
+def hook_git_finish_set():
+    """
+    Execute just after setting a parameter
+    """
+    job = sysConf.job
+    sysConf.git.commitJob(job, 'moa set %s in %s' % (
+        " ".join(sysConf['newargs']), job.wd))
---
+++ packages/moa/moa-0.11.6.tar.gz/moa-0.11.6/moa/plugin/system/doc/__init__.py
@@ -0,0 +1,406 @@
+# Copyright 2009-2011 Mark Fiers
+# The New Zealand Institute for Plant & Food Research
+#
+# This file is part of Moa - http://github.com/mfiers/Moa
+#
+# Licensed under the GPL license (see 'COPYING')
+#
+"""
+**doc** - Manage job documentation
+----------------------------------
+
+Manage project / title / description for jobs
+
+"""
+import datetime
+import getpass
+import os
+import pwd
+import socket
+import subprocess as sp
+import sys
+import time
+
+import jinja2
+import moa.args
+import moa.logger
+import moa.ui
+from moa.plugin.system.doc import pelican_util
+from moa.sysConf import sysConf
+
+
+l = moa.logger.getLogger(__name__)
+
+
+
+def hook_finish():
+
+    if not sysConf.commands[sysConf.args.command]['logJob']:
+        return
+
+    if sysConf.args.command == 'change':
+        #this is already taken care of!
+        return
+
+    message = moa.ui._textFormattedMessage([
+        'changelog', sysConf.args.changeMessage,
+        sysConf.doc.changeMessage
+    ])
+
+    if message:
+        _writeMessage(
+            category='change',
+            txt=message)
+
+
+def hook_defineOptions():
+    sysConf.argParser.add_argument(
+        '-m', action='store',
+        dest='changeMessage', help='Change message for this operation')
+
+
+def _writeMessage(category, txt, title=None):
+    """
+    Append a markdown formatted message to either CHANGELOG or BLOG
+
+    :param category: the category of message
+    :param title: an optional title for the message
+    :type title: string
+    :param txt: message to save
+    :type txt: array of strings
+    """
+
+    dirname = os.path.join('.moa', 'doc', category)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    now = datetime.datetime.now()
+
+    filename = os.path.join(dirname, '%s_%d%d%d_%d%d%d.md' % (
+        category, now.year, now.month, now.day,
+        now.hour, now.minute, now.second))
+
+    if title is None:
+        title = txt[0]
+        txt = txt[1:]
+
+    txt = "\n".join(txt).rstrip() + "\n"
+
+    #:date: 2010-10-03 10:20
+    with open(filename, "w") as F:
+        F.write("Title: %s\n" % title)
+        F.write("Author: %s\n" % getpass.getuser())
+        F.write("Date: %s\n\n" % (
+            now.strftime("%Y-%m-%d %H:%M")))
+        F.write(txt)
+
+
+def _readFromuser(job, ):
+    """
+    get a message from the user
+    """
+
+    oldstdin = sys.stdin
+    sys.stdin = open('/dev/tty')
+    txt = []
+    print "Enter your message. First line is used as title."
+    print "Ctrl-d on an empty line to finish..."
+    while True:
+        try:
+            line = raw_input("")
+            txt.append(line)
+        except (EOFError, KeyboardInterrupt):
+            break
+
+    sys.stdin = oldstdin
+    return txt
+
+
+@moa.args.needsJob
+@moa.args.argument('message', nargs='*')
+@moa.args.command
+def blog(job, args):
+    """
+    Add an entry to the job blog (in .moa/doc/blog/)
+
+    Allows a user to maintain a blog for this job. Use as
+    follows::
+
+        $ moa blog
+        Enter your blog message (ctrl-d on an empty line to finish)
+
+        ... enter your message here ..
+
+        [ctrl-d]
+
+    Note: the ctrl-d needs to be given on an empty line. The text is
+    appended to moa.desciption. In the web interface this is converted
+    to Markdown_.
+
+    .. _Markdown: http://daringfireball.net/projects/markdown/ markdown.
+    """
+
+    sin = _getFromStdin()
+
+    if args.message:
+        message = [" ".join(args.message)]
+    else:
+        message = _readFromuser(
+            job)
+
+    if sin:
+        message.append("\nStdin:\n")
+        message.extend(["    " + x for x in sin.split("\n")])
+
+    _writeMessage('blog', message)
+
+    moa.ui.message("Created a blog entry", store=False)
+    sysConf.doc.blog = "\n".join(message)
+
+
+def _getFromStdin():
+    import re
+
+    if not sys.stdin.isatty():
+        m = sys.stdin.read()
+        m = re.sub(r'\x1b\[[^m]*m', '', m)
+        return m
+    return ""
+
+
+def _readArticle(filename):
+    article = {}
+    body = []
+    with open(filename) as F:
+        #read header
+        for line in F:
+            if not ':' in line:
+                break
+
+            key, val = line.split(':', 1)
+            if not key in ['Title', 'Author', 'Date']:
+                break
+            article[key] = val.strip()
+
+        body.append(line)
+        for line in F:
+            body.append(line)
+
+    while body and not(body[0].strip()):
+        body = body[1:]
+    while body and not(body[-1].strip()):
+        body = body[:-1]
+
+    article['Body'] = body
+
+    if 'Date' in article:
+        article['Date'] = datetime.datetime.strptime(
+            article['Date'], "%Y-%m-%d %H:%M")
+    else:
+        article['Date'] = time.ctime(os.path.getctime(file))
+
+    if not 'Author' in article:
+        stat_info = os.stat(filename)
+        uid = stat_info.st_uid
+        article['Author'] = pwd.getpwuid(uid)[0]
+
+    return article
+
+
+@moa.args.needsJob
+@moa.args.doNotLog
+@moa.args.argument('no_entries', type=int, default=10, nargs='?',
+                   help="No of changelog entries to show (default 10)")
+@moa.args.command
+def changelog(job, args):
+    """
+    Print a changelog to stdout
+    """
+    _show_stuff('change', args.no_entries)
+
+
+@moa.args.needsJob
+@moa.args.doNotLog
+@moa.args.argument('no_entries', type=int, default=10, nargs='?',
+                   help="No of blog entries to show (default 10)")
+@moa.args.command
+def showblog(job, args):
+    """
+    Print a changelog to stdout
+    """
+    _show_stuff('blog', args.no_entries)
+
+
+def _show_stuff(category, no_messages):
+    stuff_dir = os.path.join('.moa', 'doc', category)
+    #use shell shortcut to find the latest messages
+    cl = 'ls %s -t | head -%d' % (stuff_dir, no_messages)
+    P = sp.Popen(cl, shell=True, stdout=sp.PIPE)
+    out, _ = P.communicate()
+    for entry in out.split("\n"):
+        entry = entry.strip()
+        if not entry:
+            continue
+        fname = os.path.join(stuff_dir, entry)
+        article = _readArticle(fname)
+        print "%s  %s" % (article['Date'], article['Author'])
+        print "    Title: %s\n" % article['Title']
+        for line in article['Body']:
+            print "    %s" % line.rstrip()
+        print "\n"
+
+
+@moa.args.needsJob
+@moa.args.argument('message', nargs='*')
+@moa.args.command
+def change(job, args):
+    """
+    Add entry to CHANGELOG.md
+
+    This function allows the user to add an entry to CHANGELOG.md
+    (including a timestamp). Use it as follows::
+
+        $ moa change
+        Enter your changelog message (ctrl-d on an empty line to finish)
+
+        ... enter your message here ..
+
+        [ctrl-d]
+
+    Note: the ctrl-d needs to be given on an empty line. The text is
+    appended to moa.desciption. In the web interface this is converted
+    to Markdown_.
+
+    .. _Markdown: http://daringfireball.net/projects/markdown/ markdown.
+
+    Note the same can be achieved by specifying the -m parameter
+    (before the command - for example:
+
+    `moa -m 'intelligent remark' set ...`
+
+    Note. It is also possible to cat some text into moa change:
+
+    wc -l | moa change
+
+    Moa will still query you for a message and append the data from
+    stdin to the message
+
+
+    """
+
+    sin = _getFromStdin()
+
+    if args.message:
+        message = [" ".join(args.message)]
+    else:
+        message = _readFromuser(
+            job)
+
+    if sin:
+        message.append("\nStdin:\n")
+        message.extend(["    " + x for x in sin.split("\n")])
+
+    _writeMessage('change', message)
+
+    moa.ui.message("Created a changelog entry", store=False)
+    sysConf.doc.changeMessage = "\n".join(message)
+
+
+@moa.args.doNotLog
+@moa.args.needsJob
+@moa.args.command
+def pelican(job, args):
+    """
+    Run pelican :)
+    """
+
+    jenv = jinja2.Environment(
+        loader=jinja2.PackageLoader('moa.plugin.system.doc'))
+    sysConf.plugins.pelican.jenv = jenv
+
+    themedir = os.path.join(os.path.dirname(__file__), 'theme')
+
+    sysConf.doc.server = socket.gethostname()
+    peliconf = '.moa/pelican.conf.py'
+    renderdir = '.moa/doc/pelican'
+
+    if not os.path.exists('.moa/doc/pages'):
+        os.makedirs('.moa/doc/pages')
+
+    #call a plugin hook to let other plugins generate pelican pages
+    # (if they want to)
+
+    job.pluginHandler.run('pelican', job=job)
+    sysConf.pluginHandler.run('pelican')
+
+    pelican_util.generate_parameter_page(job)
+    pelican_util.generate_file_page(job)
+    pelican_util.generate_readme_page(job)
+    pelican_util.generate_template_page(job)
+
+    jtemplate = jenv.select_template(['pelican.conf.jinja2'])
+
+    txt = jtemplate.render(sysConf)
+    with open(peliconf, 'w') as F:
+        F.write(txt)
+
+    if not os.path.exists(renderdir):
+        os.makedirs(renderdir)
+
+    cl = ('pelican -q -t %s -m md -s .moa/pelican.conf.py ' +
+          '-o .moa/doc/pelican/ .moa/doc/') % (themedir)
+
+    l.debug("Executing pelican:")
+    l.debug("   %s" % cl)
+    sp.Popen(cl, shell=True)
+
+    #create a redirect page to the proper index.thml
+    pelican_util.generate_redirect(job)
+
+
+@moa.args.needsJob
+@moa.args.command
+def readme(job, args):
+    """
+    Edit the README.md file for this job
+
+    You could, obviously, also edit the file yourself - this is a mere
+    shortcut - maybe it will stimulate you to maintain a README file
+    """
+    sp.call(
+        os.environ.get('EDITOR', 'nano').split() + ['README.md'])
+
+
+def _update_git(filename):
+    """
+    Check if a file is under version control & commit changes
+    """
+    job = sysConf.job
+    if not sysConf.git.repo:
+        #repo is not initalized..(not in a repository?)
+        return
+
+    if os.path.exists(filename):
+        sysConf.git.repo.index.add([filename])
+        sysConf.git.commitJob(job, 'Worked on %s in  %s' % (filename, job.wd))
+
+
+def hook_git_finish_readme():
+    """
+    Execute just after setting running moa readme
+    """
+    _update_git('README.md')
+
+
+def hook_git_finish_blog():
+    """
+    Execute just after setting running moa blog
+    """
+    _update_git('BLOG.md')
+
+
+def hook_git_finish_change():
+    """
+    Execute just after setting running moa blog
+    """
+    _update_git('CHANGELOG.md')
