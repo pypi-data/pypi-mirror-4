11ff76025afae793a716e099183cc1ad38105fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:33 +0000

    Add package infi.diskmanagement/0.1.21/infi.diskmanagement-0.1.21-py2.7.egg

---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.1.21-py2.7.egg/infi/diskmanagement/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.21"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c101d1bba02bbba34b9d4cf68bacdd576f2774a4'
+__git_head_subject__ = 'Finished release v0.1.21.'
+__git_head_message__ = '* release/v0.1.21:\n  TRIVIAL fix from_large_integer\n  TRIVIAL fix from_large_integer\n  TRIVIAL fix from_large_integer\n  TRIVIAL more debug information\n  TRIVIAL fixing filter\n  TRIVIAL added logging\n  empty commit after version v0.1.20'
+__git_dirty_diff__ = ''
