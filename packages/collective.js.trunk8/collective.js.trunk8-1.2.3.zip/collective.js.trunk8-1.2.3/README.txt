833ec94eb59e05127f47bbadbc94b70c3758d643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:31 +0000

    Add package collective.js.trunk8/1.2.3/collective.js.trunk8-1.2.3.zip

---
+++ packages/collective.js.trunk8/collective.js.trunk8-1.2.3.zip/collective.js.trunk8-1.2.3/README.txt
@@ -0,0 +1,8 @@
+.. contents::
+
+Introduction
+============
+
+This package integrates jQuery trunk8 plugin into Plone
+
+https://github.com/rviscomi/trunk8
---
+++ packages/collective.js.trunk8/collective.js.trunk8-1.2.3.zip/collective.js.trunk8-1.2.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2.3'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.js.trunk8',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://github.com/collective/',
+      license='MIT',
+      packages=find_packages(),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+  	  [z3c.autoinclude.plugin]
+  	  target = plone
+      """,
+      )
