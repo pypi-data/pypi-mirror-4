86fdb05f55f149e15d8f5c1fdc0b499178abff8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:01 +0000

    Add package django_exceptional_middleware/1.2/django_exceptional_middleware-1.2.tar.gz

---
+++ packages/django_exceptional_middleware/django_exceptional_middleware-1.2.tar.gz/django_exceptional_middleware-1.2/setup.py
@@ -0,0 +1,22 @@
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+PACKAGE = 'django_exceptional_middleware'
+VERSION = '1.2'
+
+package_data = {
+        'exceptional_middleware': [ 'templates/http_responses/*.html' ],
+}
+
+setup(
+    name=PACKAGE, version=VERSION,
+    description="Django middleware to allow generating arbitrary HTTP status codes via exceptions.",
+    packages=[ 'exceptional_middleware' ],
+    package_data=package_data,
+    license='MIT',
+    author='James Aylett',
+    url = 'http://tartarus.org/james/computers/django/',
+)
