9008cb29ca2d2357f56a17c5fc842624b1de9645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package django-mailbox/1.7/django-mailbox-1.7.tar.gz

---
+++ packages/django-mailbox/django-mailbox-1.7.tar.gz/django-mailbox-1.7/django_mailbox/admin.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.contrib import admin
+
+from django_mailbox.models import MessageAttachment, Message, Mailbox
+
+def get_new_mail(mailbox_admin, request, queryset):
+    for mailbox in queryset.all():
+        mailbox.get_new_mail()
+get_new_mail.short_description = 'Get new mail'
+
+class MailboxAdmin(admin.ModelAdmin):
+    list_display = (
+                'name',
+                'uri',
+                'from_email',
+                'active',
+            )
+    actions = [get_new_mail]
+
+class MessageAttachmentAdmin(admin.ModelAdmin):
+    list_display = ('document',)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+                'subject',
+                'processed',
+                'read',
+                'mailbox',
+                'outgoing',
+            )
+    ordering = ['-processed']
+    list_filter = (
+            'mailbox',
+            'outgoing',
+            'processed',
+            'read',
+            )
+    raw_id_fields = (
+            'in_reply_to',
+            )
+
+if getattr(settings, 'DJANGO_MAILBOX_ADMIN_ENABLED', True):
+    admin.site.register(Message, MessageAdmin)
+    admin.site.register(MessageAttachment, MessageAttachmentAdmin)
+    admin.site.register(Mailbox, MailboxAdmin)
