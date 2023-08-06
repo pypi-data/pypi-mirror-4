2e0041763d22cf7f3ceefb1c588e411b6dbf7248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package django-pin/1.0.1/django-pin-1.0.1.tar.gz

---
+++ packages/django-pin/django-pin-1.0.1.tar.gz/django-pin-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+
+from distutils.core import setup
+
+
+version = '1.0.1'
+
+setup(
+    name = "django-pin",
+    version = "1.0.1",
+    author = "vahid chakoshy",
+    author_email = "vchakoshy@gmail.com",
+    description = "pinterest apllication like in Django",
+    url = "http://www.wisgoon.com/",
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe=False,
+)
