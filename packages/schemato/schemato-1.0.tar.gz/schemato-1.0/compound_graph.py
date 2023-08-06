762702b808b1c06ca638664998212494b3138d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:26 +0000

    Add package schemato/1.0/schemato-1.0.tar.gz

---
+++ packages/schemato/schemato-1.0.tar.gz/schemato-1.0/compound_graph.py
@@ -0,0 +1,17 @@
+from pyMicrodata import pyMicrodata
+from pyRdfa import pyRdfa
+import rdflib
+
+
+class CompoundGraph(object):
+    def __init__(self, source):
+        super(CompoundGraph, self).__init__()
+        try:
+            self.microdata_graph = pyMicrodata().graph_from_source(source)
+        except:
+            self.microdata_graph = None
+
+        try:
+            self.rdfa_graph = pyRdfa().graph_from_source(source)
+        except:
+            self.rdfa_graph = None
