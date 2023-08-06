c6b9bfefe977d5588528e401e64bf69a23b84f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:04 +0000

    Add package ragstoriches/0.3.1dev/ragstoriches-0.3.1dev.tar.gz

---
+++ packages/ragstoriches/ragstoriches-0.3.1dev.tar.gz/ragstoriches-0.3.1dev/ragstoriches/receiver.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logbook
+
+log = logbook.Logger('ragstoriches.receiver')
+
+class Receiver(object):
+    def __init__(self, name='Unnamed Receiver'):
+        self.name = name
+        self.receivers = {}
+
+    def __call__(self, f):
+        self.receivers[f.__name__] = f
+        return f
+
+    def process(self, record, scope):
+        receiver_name, rargs, rkwargs = record
+        log.debug('receiver %s processing record on %s' % (
+            receiver_name, self.name))
+
+        if not receiver_name in self.receivers:
+            return scope.inject_and_call(
+                self.receivers['any'], receiver_name, *rargs, **rkwargs
+            )
+
+        return scope.inject_and_call(
+                self.receivers[receiver_name], *rargs, **rkwargs
+            )
