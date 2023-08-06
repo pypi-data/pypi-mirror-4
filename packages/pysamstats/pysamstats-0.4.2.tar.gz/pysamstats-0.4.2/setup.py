ed3e6d043a78f327209e732e751d4e0e9085665a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:46 +0000

    Add package pysamstats/0.4.2/pysamstats-0.4.2.tar.gz

---
+++ packages/pysamstats/pysamstats-0.4.2.tar.gz/pysamstats-0.4.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name='pysamstats',
+    version='0.4.2',
+    author='Alistair Miles',
+    author_email='alimanfoo@googlemail.com',
+    url='https://github.com/alimanfoo/pysamstats',
+    license='MIT Licenses',
+    description='A small Python utility for calculating statistics per genome position based on pileups from a SAM or BAM file.',
+    scripts=['pysamstats'],
+    ext_modules = [Extension("pysamstats", ["pysamstats.c"], include_dirs=["pysam"])],
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules'
+                 ],
+    requires=['pysam (>=0.7)'],
+)
