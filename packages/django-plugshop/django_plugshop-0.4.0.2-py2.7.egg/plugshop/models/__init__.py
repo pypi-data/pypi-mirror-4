5f64c499bd0124acc72379fab9ab25e66cb74e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package django-plugshop/0.4.0.2/django_plugshop-0.4.0.2-py2.7.egg

---
+++ packages/django-plugshop/django_plugshop-0.4.0.2-py2.7.egg/plugshop/models/__init__.py
@@ -0,0 +1,4 @@
+from plugshop.models.product import *
+from plugshop.models.category import *
+from plugshop.models.order import *
+from plugshop.models.order_products import *
---
+++ packages/django-plugshop/django_plugshop-0.4.0.2-py2.7.egg/plugshop/models/order_products.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from plugshop import settings
+from plugshop.utils import is_default_model
+
+class OrderProductsAbstract(models.Model):
+    class Meta:
+        abstract = True
+        verbose_name = _('order product')
+        verbose_name_plural = _('order product')
+
+    quantity = models.PositiveIntegerField(_('quantity'), blank=False, 
+                                           null=False, default=1)
+    order = models.ForeignKey(settings.ORDER_MODEL, verbose_name=_('order'),
+                              related_name='ordered_items')
+    product = models.ForeignKey(settings.PRODUCT_MODEL, 
+                                verbose_name=_('product'))
+
+    def price(self):
+        return self.product.price * self.quantity
+    price.short_description = _('Total price')
+
+if is_default_model('ORDER_PRODUCTS'):
+    class OrderProducts(OrderProductsAbstract):
+        class Meta:
+            app_label = 'plugshop'
+            verbose_name = _('order product')
+            verbose_name_plural = _('order product')
