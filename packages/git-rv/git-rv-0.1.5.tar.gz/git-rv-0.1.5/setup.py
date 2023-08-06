08afea848a205dd03780e3d4376eee60c9f8e942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:29 +0000

    Add package git-rv/0.1.5/git-rv-0.1.5.tar.gz

---
+++ packages/git-rv/git-rv-0.1.5.tar.gz/git-rv-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='git-rv',
+    version='0.1.5',
+    author='Ian Wetherbee',
+    author_email='ian.wetherbee@gmail.com',
+    packages=["upload"],
+    scripts=['bin/git-rv'],
+    url='https://github.com/wetherbeei/git-rv',
+    license='LICENSE.txt',
+    description='Code review tool for git and Rietveld',
+    long_description=open('README').read(),
+    install_requires=[],
+)
