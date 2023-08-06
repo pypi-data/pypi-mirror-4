d837cf504e0b4ff855d6de66835c0c4e15e92b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package Briareus/0.4.9/Briareus-0.4.9.tar.gz

---
+++ packages/Briareus/Briareus-0.4.9.tar.gz/Briareus-0.4.9/Briareus/Cloud/utils.py
@@ -0,0 +1,53 @@
+from gevent import monkey; monkey.patch_all()
+from Corellia.client import Client
+from Corellia.kvstore import KVStore
+from ..config import config
+from uuid import uuid1
+import Husky
+
+class Cloud(object):
+    def __init__(self, f):
+        self.f = f
+        self.client = Client(config.host, "Briareus", pickler=Husky, serialize=True, interval=0.01)
+
+    def __call__(self, *args):
+        return self.client.eval(self.f, args)
+
+    def __getstate__(self):
+        return Husky.dumps(self.f)
+
+    def __setstate__(self, state):
+        self.f = Husky.loads(state) 
+
+class CachedData(object):
+
+    name = "CachedData"
+
+    def __init__(self, var):
+        self.value = var
+        self.put(self.value)
+
+    def __getstate__(self):
+        return self.id
+
+    def __setstate__(self, state):
+        self.id = state
+
+    def __getattr__(self, name):
+        if name == "value":
+            self.__dict__["value"] = self.get(self.id)
+            return self.__dict__["value"]
+        else:
+            return getattr(self.value, name)
+
+    def put(self, value):
+        self.id = uuid1().hex
+        value = Husky.dumps(value)
+        KVStore(config.host, self.name).set(self.id, value, serialize=False)
+
+    def get(self, key):
+        res = KVStore(config.host, self.name).get(key, serialize=False)
+        return Husky.loads(res)
+
+    def __del__(self):
+        pass
