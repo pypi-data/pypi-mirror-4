e90b96426c067c82b1d9fe3a86db0c33c32cdf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:32 +0000

    Add package carlfg/0.2.2/carlfg-0.2.2.tar.gz

---
+++ packages/carlfg/carlfg-0.2.2.tar.gz/carlfg-0.2.2/cg/german.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def sum(n):
+    "find the sum from 1 to n by Carl Friedrich Guass method"
+    try:
+        n = int(n)
+        return (n*(1+n))/2
+    except:
+        n = int(n)
+        return (n*(1+n))/2
+
+
---
+++ packages/carlfg/carlfg-0.2.2.tar.gz/carlfg-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='carlfg',
+      version='0.2.2',
+      description='find CG coefficients, find the sum of all natural numbers from 1 to n',
+      url='https://github.com/psachin/carlfg',
+      author='sachin',
+      author_email='isachin@iitb.ac.in',
+      license=open('LICENSE.rst').read(),
+      packages=['cg'],
+      scripts=['bin/cg','bin/clebg'],
+      long_description=open('README.rst').read(),
+      keywords=['cg','clebg','pypi','coefficients','clebsch gordan'],
+      install_requires=["wxPython",
+                        "wx",
+                        ],
+      zip_safe=False)
