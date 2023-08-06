c58cc305b9a38f5b129ca2e839cd5b4a113507e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:59 +0000

    Add package clime/0.1.9/clime-0.1.9.tar.gz

---
+++ packages/clime/clime-0.1.9.tar.gz/clime-0.1.9/examples/reverse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def reverse(string):
+    '''reverse string
+    '''
+
+    print string.decode('UTF-8')[::-1]
+
+if __name__ == '__main__':
+
+    # v0.1.6
+    import clime
+    clime.customize(ignore_return=True)
+
+    # It also works, because the ``reverse`` returns noting.
+    #import clime.now
