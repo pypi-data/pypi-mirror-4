75b0c919e7a4300a2cfb0e250f20d41a5e6fb736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:05 +0000

    Add package django-any-urlfield/1.0.5/django-any-urlfield-1.0.5.tar.gz

---
+++ packages/django-any-urlfield/django-any-urlfield-1.0.5.tar.gz/django-any-urlfield-1.0.5/any_urlfield/static/any_urlfield/any_urlfield.js
@@ -0,0 +1,28 @@
+(function($)
+{
+  $(document).ready(function(){
+    var widgets = $("ul.any_urlfield-url_type");
+    widgets.find("input").live('change', onUrlTypeChange);
+
+    // Apply by default
+    widgets.each(function(){ updatePanels($(this)); });
+  });
+
+
+  function onUrlTypeChange(event)
+  {
+    var widget = $(this).parent().closest('.any_urlfield-url_type');
+    updatePanels(widget);
+  }
+
+  function updatePanels(widget)
+  {
+    var inputs = widget.find('input');
+    inputs.each(function(){
+      var slugvalue = this.value.replace(/[^a-z0-9-_]/, '');
+      var pane = widget.siblings(".any_urlfield-url-" + slugvalue);
+      pane[ this.checked ? "show" : "hide" ]();
+    });
+  }
+
+})(window.jQuery || window.django.jQuery);
