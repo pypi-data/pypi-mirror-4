71790d5242d618cd66863fabe4c0162fa6a22c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package invoke/0.2.0/invoke-0.2.0.tar.gz

---
+++ packages/invoke/invoke-0.2.0.tar.gz/invoke-0.2.0/invoke/cli.py
@@ -0,0 +1,165 @@
+from functools import partial
+import sys
+import textwrap
+
+from .vendor import six
+
+from .loader import Loader
+from .parser import Parser, Context, Argument
+from .executor import Executor
+from .exceptions import Failure, CollectionNotFound, ParseError
+from .util import debug, pty_size
+from ._version import __version__
+
+
+def task_name_to_key(x):
+    return (x.count('.'), x)
+
+sort_names = partial(sorted, key=task_name_to_key)
+
+
+def parse_gracefully(parser, argv):
+    """
+    Run ``parser.parse_argv(argv)`` & gracefully handle ``ParseError``.
+
+    'Gracefully' meaning to print a useful human-facing error message instead
+    of a traceback; the program will still exit if an error is raised.
+
+    If no error is raised, returns the result of the ``parse_argv`` call.
+    """
+    try:
+        return parser.parse_argv(argv)
+    except ParseError as e:
+        sys.exit(str(e))
+
+
+def parse(argv, collection=None):
+    # Initial/core parsing (core options can affect the rest of the parsing)
+    initial_context = Context(args=(
+        # TODO: make '--collection' a list-building arg, not a string
+        Argument(
+            names=('collection', 'c'),
+            help="Specify collection name to load. May be given >1 time."
+        ),
+        Argument(
+            names=('root', 'r'),
+            help="Change root directory used for finding task modules."
+        ),
+        Argument(
+            names=('help', 'h'),
+            kind=bool,
+            default=False,
+            help="Show this help message and exit."
+        ),
+        Argument(
+            names=('version', 'V'),
+            kind=bool,
+            default=False,
+            help="Show version and exit"
+        ),
+        Argument(
+            names=('list', 'l'),
+            kind=bool,
+            default=False,
+            help="List available tasks."
+        ),
+        Argument(
+            names=('no-dedupe',),
+            kind=bool,
+            default=False,
+            help="Disable task deduplication"
+        )
+    ))
+    # 'core' will result an .unparsed attribute with what was left over.
+    debug("Parsing initial context (core args)")
+    parser = Parser(initial=initial_context, ignore_unknown=True)
+    core = parse_gracefully(parser, argv)
+    debug("After core-args pass, leftover argv: %r" % (core.unparsed,))
+    args = core[0].args
+
+    # Print version & exit if necessary
+    if args.version.value:
+        six.print_("Invoke %s" % __version__)
+        sys.exit(0)
+
+    # Core --help output
+    # TODO: if this wants to display context sensitive help (e.g. a combo help
+    # and available tasks listing; or core flags modified by plugins/task
+    # modules) it will have to move farther down.
+    if args.help.value:
+        six.print_("Usage: inv[oke] [--core-opts] task1 [--task1-opts] ... taskN [--taskN-opts]")
+        six.print_("")
+        six.print_("Core options:")
+        indent = 2
+        padding = 3
+        # Calculate column sizes: don't wrap flag specs, give what's left over
+        # to the descriptions.
+        tuples = initial_context.help_tuples()
+        flag_width = max(len(x[0]) for x in tuples)
+        desc_width = pty_size()[0] - flag_width - indent - padding - 1
+        wrapper = textwrap.TextWrapper(width=desc_width)
+        for flag_spec, help_str in tuples:
+            # Wrap descriptions/help text
+            help_chunks = wrapper.wrap(help_str)
+            # Print flag spec + padding
+            flag_padding = flag_width - len(flag_spec)
+            spec = ''.join((
+                indent * ' ',
+                flag_spec,
+                flag_padding * ' ',
+                padding * ' '
+            ))
+            # Print help text as needed
+            if help_chunks:
+                six.print_(spec + help_chunks[0])
+                for chunk in help_chunks[1:]:
+                    six.print_((' ' * len(spec)) + chunk)
+            else:
+                six.print_(spec)
+        six.print_('')
+        sys.exit(0)
+
+    # Load collection (default or specified) and parse leftovers
+    # (Skip loading if somebody gave us an explicit task collection.)
+    if not collection:
+        debug("No collection given, loading from %r" % args.root.value)
+        loader = Loader(root=args.root.value)
+        collection = loader.load_collection(args.collection.value)
+    parser = Parser(contexts=collection.to_contexts())
+    debug("Parsing actual tasks against collection %r" % collection)
+    tasks = parse_gracefully(parser, core.unparsed)
+
+    # Print discovered tasks if necessary
+    if args.list.value:
+        six.print_("Available tasks:\n")
+        # Sort in depth, then alpha, order
+        task_names = collection.task_names
+        names = sort_names(task_names.keys())
+        for primary in names:
+            aliases = sort_names(task_names[primary])
+            out = primary
+            if aliases:
+                out += " (%s)" % ', '.join(aliases)
+            six.print_("    %s" % out)
+        six.print_("")
+        sys.exit(0)
+    return args, collection, tasks
+
+
+def main():
+    # Parse command line
+    argv = sys.argv[1:]
+    debug("Base argv from sys: %r" % (argv,))
+    args, collection, tasks = parse(argv)
+    # Take action based on 'core' options and the 'tasks' found
+    for context in tasks:
+        kwargs = {}
+        for name, arg in six.iteritems(context.args):
+            kwargs[name] = arg.value
+        try:
+            # TODO: allow swapping out of Executor subclasses based on core
+            # config options
+            # TODO: friggin dashes vs underscores
+            Executor(collection).execute(name=context.name, kwargs=kwargs, dedupe=not args['no-dedupe'])
+        except Failure as f:
+            sys.exit(f.result.exited)
---
+++ packages/invoke/invoke-0.2.0.tar.gz/invoke-0.2.0/tests/executor.py
@@ -0,0 +1,42 @@
+from spec import Spec, eq_
+from mock import Mock
+
+from invoke.executor import Executor
+from invoke.collection import Collection
+from invoke.tasks import Task
+
+
+class Executor_(Spec):
+    def setup(self):
+        self.task1 = Task(Mock())
+        self.task2 = Task(Mock(), pre=['task1'])
+        self.task3 = Task(Mock(), pre=['task1'])
+        coll = Collection()
+        coll.add_task(self.task1, name='task1')
+        coll.add_task(self.task2, name='task2')
+        coll.add_task(self.task3, name='task3')
+        self.executor = Executor(coll)
+        self.coll = coll
+
+    def base_case(self):
+        self.executor.execute('task1')
+        assert self.task1.body.called
+
+    def kwargs(self):
+        k = {'foo': 'bar'}
+        self.executor.execute(name='task1', kwargs=k)
+        self.task1.body.assert_called_once_with(**k)
+
+    def pre_tasks(self):
+        self.executor.execute(name='task2')
+        eq_(self.task1.body.call_count, 1)
+
+    def enabled_deduping(self):
+        self.executor.execute(name='task2')
+        self.executor.execute(name='task3')
+        eq_(self.task1.body.call_count, 1)
+
+    def disabled_deduping(self):
+        self.executor.execute(name='task2', dedupe=False)
+        self.executor.execute(name='task3', dedupe=False)
+        eq_(self.task1.body.call_count, 2)
