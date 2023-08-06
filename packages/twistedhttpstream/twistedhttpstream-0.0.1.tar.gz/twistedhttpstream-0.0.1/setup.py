a075dc8918b899c14c419638f965563c29abe36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:13 +0000

    Add package twistedhttpstream/0.0.1/twistedhttpstream-0.0.1.tar.gz

---
+++ packages/twistedhttpstream/twistedhttpstream-0.0.1.tar.gz/twistedhttpstream-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="twistedhttpstream",
+    version="0.0.1",
+    description="Twisted client library for the HTTP Stream",
+    author="Thomas Rabaix, Alexandre Fiori",
+    author_email="thomas.rabaix@gmail.com",
+    url="https://github.com/rande/twisted-http-stream",
+    py_modules=["twistedhttpstream"],
+    packages = ['twistedhttpstream'],
+    #install_requires=["twisted"],
+)
