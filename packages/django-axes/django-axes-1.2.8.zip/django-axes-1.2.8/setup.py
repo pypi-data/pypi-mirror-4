c71299d8898bf0ad665e72ddc05a78ad0c32b987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:07 +0000

    Add package django-axes/1.2.8/django-axes-1.2.8.zip

---
+++ packages/django-axes/django-axes-1.2.8.zip/django-axes-1.2.8/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+#import axes
+
+setup(
+    name='django-axes',
+    version='1.2.8',
+#    version=axes.get_version(),
+    description="Keep track of failed login attempts in Django-powered sites.",
+    long_description=(open('README.rst', 'r').read() + '\n' +
+        open('CHANGES.txt', 'r').read()),
+    keywords='django, security, authentication',
+    author='Josh VanderLinden, Philip Neustrom, Michael Blume',
+    author_email='codekoala@gmail.com',
+    maintainer='Alex Clark',
+    maintainer_email='aclark@aclark.net',
+    url='https://github.com/codekoala/django-axes',
+    license='MIT',
+    package_dir={'axes': 'axes'},
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Security',
+        'Topic :: System :: Logging',
+    ],
+    zip_safe=False,
+)
