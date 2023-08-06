55dc1faf2873c29fba4933827330252345e03ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package dropboxfs/0.1-9/dropboxfs-0.1-9.tar.gz

---
+++ packages/dropboxfs/dropboxfs-0.1-9.tar.gz/dropboxfs-0.1-9/setup.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+import os
+from distutils.core import setup
+
+name = 'dropboxfs'
+version = '0.1'
+release = '9'
+versrel = version + '-' + release
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+download_url = 'https://github.com/downloads/btimby/fs-dropbox' \
+                           '/' + name + '-' + versrel + '.tar.gz'
+long_description = file(readme).read()
+
+setup(
+    name = name,
+    version = versrel,
+    description = 'A pyFilesystem backend for the Dropbox API.',
+    long_description = long_description,
+    requires = [
+        'fs',
+        'dropbox',
+    ],
+    author = 'Ben Timby',
+    author_email = 'btimby@gmail.com',
+    maintainer = 'Ben Timby',
+    maintainer_email = 'btimby@gmail.com',
+    url = 'http://github.com/btimby/fs-dropbox/',
+    download_url = download_url,
+    license = 'GPLv3',
+    py_modules=['dropboxfs'],
+    package_data={'': ['README.rst']},
+    classifiers = (
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
