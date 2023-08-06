04870a143eb4c3b99d38741ee1b39e89ed0e2e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:01 +0000

    Add package cmsplugin-fbalbumlight/1.0.2/cmsplugin-fbalbumlight-1.0.2.tar.gz

---
+++ packages/cmsplugin-fbalbumlight/cmsplugin-fbalbumlight-1.0.2.tar.gz/cmsplugin-fbalbumlight-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = '1.0.2'
+
+setup(
+    name='cmsplugin-fbalbumlight',
+    version=version,
+    description='facebook album plugin for django-cms with lightbox',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com/vinitcool76/cmsplugin-fbalbum.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
