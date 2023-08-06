172a211f21cffcd41a69b6b6c54bcc6d90bf5d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package django-timepiece/0.8.1/django-timepiece-0.8.1.tar.gz

---
+++ packages/django-timepiece/django-timepiece-0.8.1.tar.gz/django-timepiece-0.8.1/timepiece/defaults.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+
+class TimepieceDefaults(object):
+
+    TIMEPIECE_EXTRA_NAV = {}
+
+    TIMEPIECE_DEFAULT_LOCATION_SLUG = None
+
+    TIMEPIECE_PAID_LEAVE_PROJECTS = {}
+
+    TIMEPIECE_ACCOUNTING_EMAILS = []
+
+    TIMEPIECE_EMAILS_USE_HTTPS = True
