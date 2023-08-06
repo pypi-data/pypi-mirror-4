0c683421c18af0eae0585e0e1cdd4e8f48ecf84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package sketch222/4.0.2/sketch222-4.0.2.zip

---
+++ packages/sketch222/sketch222-4.0.2.zip/sketch222-4.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='sketch222',
+    version     ='4.0.2',
+    py_modules  =['sketch222'],
+    author      ='hfpython',
+    author_email='hfpython@asdf',
+    url         ='www.linian.com',
+    description ="asdfasfdasdf",
+    )
