1784d92026f5ce9bfef3bf201862281ff3d578a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:32 +0000

    Add package newer/1.0.0/newer-1.0.0.zip

---
+++ packages/newer/newer-1.0.0.zip/newer-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='newer',
+    version='1.0.0',
+    py_modules=['new'],
+    author='Gautam',
+    author_email= 'gautam.jain0911@gmail.com',
+    description='function to print each item of a nested list'
+    )
