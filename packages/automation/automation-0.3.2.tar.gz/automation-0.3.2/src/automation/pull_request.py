2703974f72262115fca2b3c91feb89b94f8b4cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package automation/0.3.2/automation-0.3.2.tar.gz

---
+++ packages/automation/automation-0.3.2.tar.gz/automation-0.3.2/src/automation/pull_request.py
@@ -0,0 +1,29 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+
+@task(default=True)
+def new():
+  current_branch = local("git branch | grep '*'", capture=True)[2:]
+  restricted_branches = ['master', 'production']
+  for branch in restricted_branches:
+    if branch == current_branch:
+      abort("You cannot create pull requests from the %s branch." % branch)
+
+  local("git push origin %s" % current_branch)
+  if confirm(
+    "Is this merge associated with a GitHub issue?",
+    default=True):
+    issue = prompt(
+      "What is the issue number?",
+      validate=int)
+    local("hub pull-request -i %d -b production" % issue)
+  else:
+    title = prompt("Title:")
+    local("hub pull-request \"%s\" -b production" % title)
+
+  if confirm(
+    "Pull-request complete.  Delete your branch '%s'?" % current_branch,
+    default=False):
+    local("git checkout master")
+    local("git branch -d %s" % current_branch)
