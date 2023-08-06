d25616e3be00b07729cbd33c80247e09acd1c1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package configs/1.8/configs-1.8.tar.gz

---
+++ packages/configs/configs-1.8.tar.gz/configs-1.8/configs/api.py
@@ -0,0 +1,29 @@
+"""
+***********
+configs.api
+***********
+
+This module implements the configs API.
+"""
+
+from .config import Config
+
+def load(config_file, fallback_file=None):
+    """Constructs are returns a :class:`Config <Config>` instance.
+    
+    :param config_file: configuration file to be parsed
+    :param fallback_file: (optional) fallback configuration file with default values to be used if missing in the ``config_file``
+    
+    Usage::
+    
+        >>> import configs
+        >>> configs.load('my.conf', fallback_file='default.conf')
+        >>> print(fc['general']['some'])
+        thing
+    """
+    
+    if fallback_file:
+        return Config(config_file, Config(fallback_file))
+    else:
+        return Config(config_file)
+    
