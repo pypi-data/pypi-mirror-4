1ca13c0e7431a389280d8b67f3966e08ccff310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package django-carrots/1.5/django-carrots-1.5.tar.gz

---
+++ packages/django-carrots/django-carrots-1.5.tar.gz/django-carrots-1.5/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="django-carrots",
+    version="1.5",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['django-social-auth3'],
+
+    #package_data={
+    #    '': ['*.html'],
+    #},
+    package_data = {
+        'account': ['templates/account/*.html', 'templates/*.html'],
+    },
+
+    # metadata for upload to PyPI
+    author=u"Krzysztof Płocharz",
+    author_email="krzysztof@plocharz.info",
+    description="Basic account application for Django Workshops by Geek Girl Carrots",
+    license="PSF",
+    keywords="django facebook account",
+    url="http://geekgirlscarrots.pl/",
+    include_package_data = True,
+)
