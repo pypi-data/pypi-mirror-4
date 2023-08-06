89c50392945f6afafb91da4d82dbbcbfaa2142ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:35 +0000

    Add package pixsaw/0.0.1/pixsaw-0.0.1.tar.gz

---
+++ packages/pixsaw/pixsaw-0.0.1.tar.gz/pixsaw-0.0.1/src/pixsaw/script.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import logging
+from optparse import OptionParser
+
+from pixsaw.base import Handler
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    parser = OptionParser(
+            usage="%prog --dir path/to/dir --lines lines.png [options] path/to/image",
+            description="Cut a picture into pieces by cutting along pixel lines")
+
+    parser.add_option("--dir", "-d",
+            action="store",
+            type="string",
+            help="Set the directory to store the clip files in.",)
+    parser.add_option("--lines", "-l",
+            action="store",
+            type="string",
+            help="Set the lines file.",)
+
+    (options, args) = parser.parse_args()
+
+    if not args and not (options.dir and options.lines):
+        parser.error("Must set a directory and lines file with an image")
+
+    handler = Handler(options.dir, options.lines)
+    for image in args:
+        handler.process(image)
+
+if __name__ == '__main__':
+    main()
