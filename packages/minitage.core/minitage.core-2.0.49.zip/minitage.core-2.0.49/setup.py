f7e6d40fd93d918abf06d4150bd5c3849bd61550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:35 +0000

    Add package minitage.core/2.0.49/minitage.core-2.0.49.zip

---
+++ packages/minitage.core/minitage.core-2.0.49.zip/minitage.core-2.0.49/setup.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2009, Mathieu PASQUET <kiorky@cryptelium.net>
+# All rights reserved.
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+name = 'minitage.core'
+version = '2.0.49'
+def read(rnames):
+    setupdir =  os.path.dirname( os.path.abspath(__file__))
+    return open(
+        os.path.join(setupdir, rnames)
+    ).read()
+
+setup(
+    name = name,
+    version = version,
+    description="A meta package-manager to deploy projects on UNIX Systemes sponsored by Makina Corpus.",
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        read('INSTALL.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n'
+    ),
+    classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    #keywords = 'development buildout',
+    author = 'Mathieu Le Marec - Pasquet',
+    author_email = 'kiorky@cryptelium.net',
+    url = 'http://cheeseshop.python.org/pypi/%s' % name,
+    license = 'BSD',
+    namespace_packages = [ 'minitage', name, ],
+    install_requires = ['iniparse', 'minitage.paste >= 1.3.1850', 'ordereddict', 'setuptools'],
+    zip_safe = False,
+    include_package_data = True,
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    extras_require={'test': ['IPython', 'zope.testing', 'mocker', 'httplib2']},
+    data_files = [
+        ('etc', ['src/etc/minimerge.cfg']),
+        ('minilays', []),
+        ('share/minitage', ['README.txt', 'INSTALL.txt', 'CHANGES.txt']),
+    ],
+    entry_points = {
+        'console_scripts': [
+            'minimerge = minitage.core.launchers.minimerge:launch',
+            'minitagify = minitage.core.minitagize:main',
+        ],
+    }
+
+)
+
+
