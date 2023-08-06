ae835731e05ad3aa82bef5d2474dac29061714f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:42 +0000

    Add package python-postmark-inbound/0.1.1/python-postmark-inbound-0.1.1.tar.gz

---
+++ packages/python-postmark-inbound/python-postmark-inbound-0.1.1.tar.gz/python-postmark-inbound-0.1.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*
+
+import os
+from setuptools import setup, find_packages
+
+readme = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(readme).read()
+
+setup(
+    name='python-postmark-inbound',
+    version='0.1.1',
+    packages=find_packages(),
+    author='José Padilla',
+    author_email='jpadilla@webapplicate.com',
+    description='Python wrapper for Postmark Inbound',
+    long_description=long_description,
+    license='MIT License',
+    url='https://github.com/jpadilla/postmark-inbound-python',
+    download_url='https://github.com/jpadilla/postmark-inbound-python/tarball/master',
+)
