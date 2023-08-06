da279e824c6526fee96e2983cb6d15d67259ddab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:34 +0000

    Add package QooxdooCherrypyJsonRpc/0.3.0/QooxdooCherrypyJsonRpc-0.3.0.zip

---
+++ packages/QooxdooCherrypyJsonRpc/QooxdooCherrypyJsonRpc-0.3.0.zip/QooxdooCherrypyJsonRpc-0.3.0/qxcpjsonrpc/__init__.py
@@ -0,0 +1,18 @@
+#############################################################################
+#
+#  jsonrpc
+#
+#  Based on:
+#    http://qooxdoo.org/contrib/project#rpcpython
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#
+#  Authors:
+#    * saaj (mail@saaj.me)
+#
+#############################################################################
+
+
+from _import import *
