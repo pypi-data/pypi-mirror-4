3fc5649170833acb12007a98c2de846ee11b3da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:53 +0000

    Add package raptor/2.13/raptor-2.13.tar.gz

---
+++ packages/raptor/raptor-2.13.tar.gz/raptor-2.13/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='raptor',
+    version='2.13',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Fabric==1.4.0',
+        'paramiko>=1.8.0,<2.0',
+        'envoy==0.0.2',
+        'suds==0.4',
+        'supervisor==3.0b2-dev-vr1',
+        'redis>=2.6.2',
+        'isodate>=0.4.4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'proc_publisher = raptor.publisher:main',
+        ]
+    },
+    # Custom version of supervisor that supports proc add/delete events
+    dependency_links = ['https://bitbucket.org/yougov/velociraptor/downloads/supervisor-3.0b2-dev-vr1.tar.gz#egg=supervisor-3.0b2-dev-vr1'],
+    description=('Libraries and command line tools for deploying with '
+                 'Velociraptor'),
+)
