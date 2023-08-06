1c2ec780be20fc5b83d3265e9efa96c6f006247b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:28 +0000

    Add package appomaticcore/0.0.15/appomaticcore-0.0.15.tar.gz

---
+++ packages/appomaticcore/appomaticcore-0.0.15.tar.gz/appomaticcore-0.0.15/appomatic/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+import os.path
+
+urlpatterns = patterns('',
+    *(url(r'^', include('%s.__urls__' % (app['NAME'],)))
+      for app in settings.APPOMATIC_APP_PARTS
+      if os.path.exists(os.path.join(app['PATH'], '__urls__.py')))
+)
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + patterns(
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    )
+
