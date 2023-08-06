c0ed71fdf749bea498bd72dd376e8ae20767eb79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:05 +0000

    Add package zdfm/0.6.1/zdfm-0.6.1.tar.gz

---
+++ packages/zdfm/zdfm-0.6.1.tar.gz/zdfm-0.6.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name="zdfm",
+      version="0.6.1",
+      description="Downloader for the ZDF Mediathek",
+      url="http://github.com/enkore/zdfm/",
+      author="Marian Beermann",
+      author_email="public@enkore.de",
+      license="MIT",
+
+      py_modules=["zdfm"],
+      entry_points={
+          "console_scripts": ["zdfm = zdfm:main"],
+      },
+      install_requires=[
+        "lxml",
+      ]
+     )
