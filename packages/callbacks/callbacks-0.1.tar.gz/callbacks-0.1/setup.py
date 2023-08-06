c0f16f7ea8879c81fd9b361e430ec95c837fb53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package callbacks/0.1/callbacks-0.1.tar.gz

---
+++ packages/callbacks/callbacks-0.1.tar.gz/callbacks-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name = 'callbacks',
+        author = 'David Morton',
+        author_email = 'davidlmorton@gmail.com',
+        version = '0.1',
+        license = 'LICENSE.txt',
+        description = 'Simple callbacks using decorators',
+        long_description = open('README.md').read(),
+        url = 'https://github.com/davidlmorton/callbacks',
+        packages = find_packages(exclude=[
+            'tests',
+        ]),
+        install_requires = [
+        ],
+        setup_requires = [
+            'nose',
+        ],
+        tests_require = [
+            'nose',
+            'coverage',
+        ],
+        test_suite = 'tests',
+)
