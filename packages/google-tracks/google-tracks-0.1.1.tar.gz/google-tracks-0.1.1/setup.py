24fa14fb2561d609d38ed116fe84906ec7c7087b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:38 +0000

    Add package google-tracks/0.1.1/google-tracks-0.1.1.tar.gz

---
+++ packages/google-tracks/google-tracks-0.1.1.tar.gz/google-tracks-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import sys
+
+# Downloads setuptools if not find it before try to import
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+install_requires = ['httplib2','oauth2client','pyopenssl']
+if sys.version_info[0] <= 2 and sys.version_info[1] <= 5:
+    install_requires.append('simplejson')
+
+setup(
+    name='google-tracks',
+    version='0.1.1',
+    url='https://github.com/TDispatch/google-tracks-api',
+    author="Marinho Brandao",
+    license="BSD License",
+    install_requires=install_requires,
+    py_modules=['googletracks'],
+    )
+
