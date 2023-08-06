c9bee060ada458a0a9ef6f045ba39d8e8ce4acfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:58 +0000

    Add package douban-client/0.0.4/douban-client-0.0.4.tar.gz

---
+++ packages/douban-client/douban-client-0.0.4.tar.gz/douban-client-0.0.4/douban_client/api/bubbler.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .base import DoubanApiBase, DEFAULT_START, DEFAULT_COUNT
+
+class Bubbler(DoubanApiBase):
+
+    def __repr__(self):
+        return '<DoubanAPI Bubbler>'
