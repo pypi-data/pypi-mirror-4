440fc60d99aaeb73187f9025fa94f74d9b610e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package django_dynproxy/0.1.2/django_dynproxy-0.1.2.zip

---
+++ packages/django_dynproxy/django_dynproxy-0.1.2.zip/django_dynproxy-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+# Upload to PyPi with this command :
+# >python setup.py sdist upload
+
+setup(
+    name = 'django_dynproxy',
+    packages = ['dynproxy'],
+    version = '0.1.2',
+    author = 'Thomas Petillon',
+    author_email = 'petillon@topic.fr',
+    url = "https://github.com/tomjerry/django_dynproxy",
+    description = 'Dynamic proxy models for Django',
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ]
+)
