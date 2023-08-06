6b06acb4e2eb047961602d2dc96aab3274e7bcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package shareboard/0.1.2/shareboard-0.1.2.tar.gz

---
+++ packages/shareboard/shareboard-0.1.2.tar.gz/shareboard-0.1.2/src/shareboard/parser.py
@@ -0,0 +1,16 @@
+# vim: set fileencoding=utf-8 :
+import subprocess
+
+
+class ShellParser(object):
+    def __init__(self, command):
+        self.command = command
+
+    def parse(self, text, encoding='utf-8'):
+        p = subprocess.Popen(self.command, shell=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        # the text should be encoded to use in Shell
+        stdout, stderr = p.communicate(data.encode(encoding))
+        # the text should be decoded to use in Python
+        return unicode(stdout, encoding)
