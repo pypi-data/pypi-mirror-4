80ca45ef1d47d90bf02c04ea2aa79ec186acdc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:37 +0000

    Add package python-lust/1.0.2/python-lust-1.0.2.tar.gz

---
+++ packages/python-lust/python-lust-1.0.2.tar.gz/python-lust-1.0.2/lust/log.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import time
+
+DEBUG=False
+
+# need a simple thread lock on this, or just fuck it
+
+def setup(log_path):
+    # test we can open for writing
+    with open(log_path, 'a+') as f:
+        f.write("[%s] INFO: Log opened.\n" % time.ctime())
+
+    os.closerange(0, 1024)
+
+    fd = os.open(log_path, os.O_RDWR | os.O_CREAT)
+
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'a+', 0)
+    sys.stderr = sys.stdout
+
+def warn(msg):
+    print "[%s] WARN: %s" % (time.ctime(), msg)
+
+
+def error(msg):
+    print "[%s] ERROR: %s" % (time.ctime(), msg)
+
+
+def info(msg):
+    print "[%s] INFO: %s" % (time.ctime(), msg)
+
+
+def debug(msg):
+    if not DEBUG:
+        print "[%s] DEBUG: %s" % (time.ctime(), msg)
+
+
+def set_debug_level(on):
+    global DEBUG
+    DEBUG=on
+
