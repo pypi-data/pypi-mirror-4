12eeda6d65282fb53b1ca069953cacdacee59595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:54 +0000

    Add package mast_test/0.1/mast_test-0.1.tar.gz

---
+++ packages/mast_test/mast_test-0.1.tar.gz/mast_test-0.1/mast_test/__init__.py
@@ -0,0 +1,3 @@
+def members():
+    print "Dane, Tam, Glen, Kumaresh, Parker, Hyunwoo, and Tom. \n\
+If I missed any of you, forgive me and please let me know."
---
+++ packages/mast_test/mast_test-0.1.tar.gz/mast_test-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mast_test',
+      version='0.1',
+      description='Package upload test',
+      url='https://github.com/MLman/pypitest.git',
+      author='Hyunwoo Kim',
+      author_email='noemailplz@gmail.com',
+      license='MIT',
+      packages=['mast_test'],
+      zip_safe=False)
