7435d1014d987519e0afb6d61c9dbb34fa538128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:11 +0000

    Add package tiddlywebplugins.simplewiki/1.0.3/tiddlywebplugins.simplewiki-1.0.3.tar.gz

---
+++ packages/tiddlywebplugins.simplewiki/tiddlywebplugins.simplewiki-1.0.3.tar.gz/tiddlywebplugins.simplewiki-1.0.3/tiddlywebplugins/simplewiki/instance.py
@@ -0,0 +1,38 @@
+"""
+Data structures used for creating a new instance of
+simplewiki.
+"""
+
+from tiddlywebplugins.instancer.util import get_tiddler_locations
+
+store_contents = {
+        'wiki': [
+            'file:basecontent/FrontPage.tid',
+        ]
+}
+
+store_structure = {
+        "bags": {
+                "wiki": {
+                        "desc": "simplewiki contents",
+                        # Use default open policy. This is old skool
+                        # wiki.
+                },
+        },
+        "recipes": {
+                "wiki": {
+                        "desc": "simplewiki",
+                        "recipe": [
+                                ("wiki", "")
+                        ],
+                }
+        }
+}
+
+
+instance_config = {
+        "system_plugins": ["tiddlywebplugins.simplewiki"],
+}
+
+instance_tiddlers = get_tiddler_locations(store_contents,
+        'tiddlywebplugins.simplewiki')
