31ba92fa5cf25f0001de1dea0875a2a01f06d98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:16 +0000

    Add package virtstrap-npm/0.0.1/virtstrap-npm-0.0.1.tar.gz

---
+++ packages/virtstrap-npm/virtstrap-npm-0.0.1.tar.gz/virtstrap-npm-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(
+    name='virtstrap-npm',
+    version=version,
+    description="A virtstrap plugin that installs node dependencies",
+    long_description="""A virtstrap plugin that installs node packages""",
+    classifiers=[],
+    keywords='virtstrap ruby bundler virtualenv pip',
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url='http://github.com/ravenac95/virtstrap-npm',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'virtstrap-local',
+    ],
+    entry_points={
+        'virtstrap_local.plugins': [
+            'npm = virtstrap_npm.plugin',
+        ]
+    },
+)
