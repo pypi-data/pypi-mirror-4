57af07529b9ccc1ccbbfaec1bbde8917b1be42d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package django-flatblocks/0.7.0/django-flatblocks-0.7.0.tar.gz

---
+++ packages/django-flatblocks/django-flatblocks-0.7.0.tar.gz/django-flatblocks-0.7.0/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='django-flatblocks',
+    version='0.7.0',
+    description='django-flatblocks acts like django.contrib.flatpages but '
+                'for parts of a page; like an editable help box you want '
+                'show alongside the main content.',
+    long_description=open('README.rst').read(),
+    keywords='django apps',
+    license='New BSD License',
+    author='Horst Gutmann',
+    author_email='zerok@zerokspot.com',
+    url='http://github.com/zerok/django-flatblocks/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['flatblocks', 'flatblocks.management',
+              'flatblocks.management.commands', 'flatblocks.migrations',
+              'flatblocks.templatetags'],
+    data_files=['MANIFEST.in']
+)
