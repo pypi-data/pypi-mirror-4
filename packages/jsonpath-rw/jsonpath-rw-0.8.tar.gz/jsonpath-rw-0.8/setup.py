2a18d2db85a5a12cb93e8c39cb5db26a3dec13e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:51 +0000

    Add package jsonpath-rw/0.8/jsonpath-rw-0.8.tar.gz

---
+++ packages/jsonpath-rw/jsonpath-rw-0.8.tar.gz/jsonpath-rw-0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools 
+import sys
+import os.path
+import subprocess
+
+
+# Build README.txt from README.md if not present, and if we are actually building for distribution to pypi
+if not os.path.exists('README.txt') and 'sdist' in sys.argv:
+    subprocess.call(['pandoc', '--to=rst', '--smart', '--output=README.txt', 'README.md'])
+
+# But use the best README around
+readme = 'README.txt' if os.path.exists('README.txt') else 'README.md'
+
+setuptools.setup(
+    name='jsonpath-rw',
+    version='0.8',
+    description='A robust and significantly extended implementation of JSONPath for Python, with a clear AST for metaprogramming.',
+    author='Kenneth Knowles',
+    author_email='kenn.knowles@gmail.com',
+    url='https://github.com/kennknowles/python-jsonpath-rw',
+    license='Apache 2.0',
+    long_description=open(readme).read(),
+    packages = ['jsonpath_rw'],
+    test_suite = 'tests',
+    install_requires = [ 'ply', 'decorator', 'six' ],
+)
