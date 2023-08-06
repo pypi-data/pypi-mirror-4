c68f1386963d1a2f0dafb9e4ecc6fd3dee480fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:02 +0000

    Add package smoff/0.0.8/smoff-0.0.8.tar.gz

---
+++ packages/smoff/smoff-0.0.8.tar.gz/smoff-0.0.8/smoff/smdjango/smoffview.py
@@ -0,0 +1,27 @@
+from django.views.generic.base import View
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+
+from lxml import etree
+
+def renderSmoff(request, page_class, *args, **kwargs):
+    context = csrf(request)
+    page_class.request = request
+    ret = HttpResponse(etree.tostring(page_class(*args, **kwargs).render(context), method='html'))
+    #FIXME: handle multiple tabs etc.
+    if 'post_handler' in context:
+        request.session['post_handler'] = context['post_handler']
+    return ret
+
+   
+
+class SmoffView(View):
+    """
+    Adapts a Page as a django class-based view.
+    """
+    page_class = None
+    def get(self, *args, **kwargs):
+        #FIXME: we sort-of want to pass *args through to the page as well, but there's some other crap in there
+        return renderSmoff(self.request, self.page_class, **kwargs)
+    def post(self, *args, **kwargs):
+        return self.request.session.pop('post_handler').handlePost(self.request, **kwargs)
