879396525a0c869b55fe97ca40cca506df3c2fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package IDLSave/1.0.0/IDLSave-1.0.0.tar.gz

---
+++ packages/IDLSave/IDLSave-1.0.0.tar.gz/IDLSave-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
+setup(name='IDLSave',
+      version='1.0.0',
+      description='IDLSave - a module to read IDL save files into Python',
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      url='https://github.com/astrofrog/idlsave',
+      packages=['idlsave'],
+      provides=['idlsave'],
+      cmdclass={'build_py': build_py},
+      keywords=['Scientific/Engineering'],
+      classifiers=[
+                   "Development Status :: 5 - Production/Stable",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                  ],
+     )
