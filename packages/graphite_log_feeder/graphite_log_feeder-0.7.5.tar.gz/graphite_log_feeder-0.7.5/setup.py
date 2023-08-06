b32120c67235c43f5725f3da735eba89afea81f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:51 +0000

    Add package graphite_log_feeder/0.7.5/graphite_log_feeder-0.7.5.tar.gz

---
+++ packages/graphite_log_feeder/graphite_log_feeder-0.7.5.tar.gz/graphite_log_feeder-0.7.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='graphite_log_feeder',
+    version='0.7.5',
+    author='Travis Bear',
+    author_email='travis_bear@yahoo.com',
+    keywords = "graphite grinder",
+    packages=['glf',
+              'glf.feeder',
+              'glf.logtype',
+              'glf.logtype.grinder'],
+    scripts=['bin/graphite_log_feeder.py'],
+    url='http://pypi.python.org/pypi/graphite_log_feeder/',
+    license='LICENSE.txt',
+    description='Ingests log files into Graphite where they can be visualized.',
+    long_description=open('README.txt').read(),
+    requires=["mtFileUtil"],
+    install_requires=["mtFileUtil"]
+)
