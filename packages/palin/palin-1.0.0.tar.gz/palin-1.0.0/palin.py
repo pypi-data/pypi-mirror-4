efaab2642faeef9a80cd114798c98658f7b4ba7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package palin/1.0.0/palin-1.0.0.tar.gz

---
+++ packages/palin/palin-1.0.0.tar.gz/palin-1.0.0/palin.py
@@ -0,0 +1,2 @@
+a = raw_input("Enter the string : ")
+print (a == a[::-1]) and "String is palindrome" or "String is not palindrome"
---
+++ packages/palin/palin-1.0.0.tar.gz/palin-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		='palin',
+	version		='1.0.0',
+	py_modules	=['palin'],
+	author		='xpack',
+	author_email	='swami11sep@hotmail.com',
+	description	='Two line program which will check whether an input string is palindrome or not',
+     )
