54b1721f9beabc1a66ea7af618eeecb27b1240a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package sourcemap/0.1.1/sourcemap-0.1.1.tar.gz

---
+++ packages/sourcemap/sourcemap-0.1.1.tar.gz/sourcemap-0.1.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+SourceMap
+~~~~~~~~~
+
+Parse JavaScript source maps.
+
+  >>> import sourcemap
+  >>> sourcemap.discover('...')
+  'jquery.min.map'
+  >>> index = sourcemap.loads('...')
+  >>> index.lookup(line=10, column=10)
+  <Token: dst_line=10 dst_column=10 src='jquery.js' src_line=50 src_col=200 name='lol'>
+
+:copyright: (c) 2013 by Matt Robenolt
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+install_requires = []
+tests_require = []
+
+setup(
+    name='sourcemap',
+    version='0.1.1',
+    author='Matt Robenolt',
+    author_email='matt@ydekproductions.com',
+    url='https://github.com/mattrobenolt/python-sourcemap',
+    description='Parse JavaScript source maps.',
+    long_description=__doc__,
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
