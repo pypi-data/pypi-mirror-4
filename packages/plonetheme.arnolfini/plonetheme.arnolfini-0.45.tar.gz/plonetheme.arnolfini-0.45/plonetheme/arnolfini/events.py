36df592f94d079c90e24cb956830efa744ad5a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package plonetheme.arnolfini/0.45/plonetheme.arnolfini-0.45.tar.gz

---
+++ packages/plonetheme.arnolfini/plonetheme.arnolfini-0.45.tar.gz/plonetheme.arnolfini-0.45/plonetheme/arnolfini/events.py
@@ -0,0 +1,16 @@
+
+
+def mediaObjectAdded(ob, event):
+    print "Media Object was added!"
+    
+    if not ob.checkCreationFlag():
+        print "Creating the slideshow folder."
+        ob.invokeFactory(type_name="Folder", id='slideshow', title='slideshow')
+        
+        folder = ob['slideshow']
+        
+        folder.reindexObject()
+        try:
+            folder.portal_workflow.doActionFor(folder, "publish", comment="Slideshow content automatically published")
+        except:
+            pass
