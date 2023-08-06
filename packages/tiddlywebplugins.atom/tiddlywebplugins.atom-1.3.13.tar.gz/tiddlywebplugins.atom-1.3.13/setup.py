8bc9ac456596c6d43f9d5341caa945e8c4a8e924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:26 +0000

    Add package tiddlywebplugins.atom/1.3.13/tiddlywebplugins.atom-1.3.13.tar.gz

---
+++ packages/tiddlywebplugins.atom/tiddlywebplugins.atom-1.3.13.tar.gz/tiddlywebplugins.atom-1.3.13/setup.py
@@ -0,0 +1,24 @@
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.3.13'
+
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.atom',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin that provides an Atom feed of tiddler collections.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.atom',
+        packages = find_packages(exclude=['test']),
+        author_email = 'cdent@peermore.com',
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools',
+            'tiddlyweb>=1.2.10',
+            'feedgenerator<1.4'],
+        zip_safe = False,
+        license = 'BSD',
+        )
