2779a0ed5a9658bc5a276f29a4b61311c0ccbd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:57 +0000

    Add package nowfm/0.0.10/nowfm-0.0.10.tar.gz

---
+++ packages/nowfm/nowfm-0.0.10.tar.gz/nowfm-0.0.10/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+version = '0.0.10'
+name = 'nowfm'
+short_description = '`nowfm` is a package for get track information from Last.fm.'
+long_description = """\
+`nowfm` is a package for get track information from Last.fm.
+
+Requirements
+------------
+* Python 2.7 or later (not support 3.x)
+
+Features
+--------
+* Additional information about when user played track
+
+Setup
+-----
+::
+
+   $ pip install nowfm
+
+History
+-------
+0.0.10 (2013-03-24)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+"""
+
+classifiers = [
+    "Development Status :: 2 - Pre-Alpha",
+    "License :: OSI Approved :: Python Software Foundation License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+]
+
+setup(
+    name=name,
+    version=version,
+    description=short_description,
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['Last.fm library',],
+    author='cloverrose',
+    author_email='example@example.com',
+    url='https://github.com/cloverrose/nowfm',
+    license='PSL',
+)
