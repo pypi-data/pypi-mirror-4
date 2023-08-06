510b5bf618ded19d932153c621d3659035890a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:18 +0000

    Add package passphrase/0.0.7/passphrase-0.0.7.tar.gz

---
+++ packages/passphrase/passphrase-0.0.7.tar.gz/passphrase-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='passphrase',
+    version='0.0.7',
+    author='Rob Martin @version2beta',
+    author_email='rob@version2beta.com',
+    packages=['passphrase'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/passphrase/',
+    license='LICENSE.txt',
+    description='List a series of words for selection of a secure and rememberable passphrase. Choose from many different languages.',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+    package_data={
+        '': ['*.dist'],
+      },
+    entry_points={
+        'console_scripts': [
+          'passphrase = passphrase:main',
+        ],
+      },
+)
