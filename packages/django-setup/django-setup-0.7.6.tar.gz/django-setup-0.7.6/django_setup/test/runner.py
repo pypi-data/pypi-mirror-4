a117d5a26c47ed9717cf732b40dea4b50b90acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:38 +0000

    Add package django-setup/0.7.6/django-setup-0.7.6.tar.gz

---
+++ packages/django-setup/django-setup-0.7.6.tar.gz/django-setup-0.7.6/django_setup/test/runner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Dec 9, 2012
+
+@author: dudu
+'''
+from django_nose.runner import NoseTestSuiteRunner
+from djcelery.contrib.test_runner import CeleryTestSuiteRunnerStoringResult
+
+class DjangoSetupTestSuiteRunner(NoseTestSuiteRunner, CeleryTestSuiteRunnerStoringResult):
+    pass
+    
+
+    
