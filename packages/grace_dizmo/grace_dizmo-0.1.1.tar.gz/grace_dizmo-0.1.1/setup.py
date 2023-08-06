5117b2167aba4ac1850c0c22cfd799f5765b72a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:28 +0000

    Add package grace_dizmo/0.1.1/grace_dizmo-0.1.1.tar.gz

---
+++ packages/grace_dizmo/grace_dizmo-0.1.1.tar.gz/grace_dizmo-0.1.1/setup.py
@@ -0,0 +1,41 @@
+from glob import glob
+import os
+from setuptools import setup
+
+data_files = [
+    ('', ['LICENSE.txt']),
+    ('', ['README.txt'])
+]
+
+previous = ''
+for root, dirs, files in os.walk('skeleton'):
+    for filename in files:
+        if previous != root:
+            data_files.append((root, glob(root + '/*.*')))
+            previous = root
+
+setup(
+    name='grace_dizmo',
+    description='A plugin for grace',
+    author='Michael Diener',
+    author_email='dm.menthos@gmail.com',
+    url='https://github.com/mdiener/grace-dizmo',
+    version='0.1.1',
+    license='LICENSE.txt',
+    py_modules=['grace_dizmo'],
+    install_requires=['grace'],
+    data_files=data_files,
+    keywords='toolchain javascript dizmo js buildtool',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: JavaScript',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Environment :: Console'
+    ]
+)
