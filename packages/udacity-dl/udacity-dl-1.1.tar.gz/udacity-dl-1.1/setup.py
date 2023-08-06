baf47a8c22d3aa65bb71e4bc56b4941ecefd62a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:14 +0000

    Add package udacity-dl/1.1/udacity-dl-1.1.tar.gz

---
+++ packages/udacity-dl/udacity-dl-1.1.tar.gz/udacity-dl-1.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="udacity-dl",
+            version="1.1",
+            description="Download udacity class videos and resources",
+            long_description=open("README.md").read(),
+            author="Ritesh Pradhan",
+            author_email="ritesxz@gmail.com",
+            url="https://github.com/ritespradhan/udacity-dl",
+            packages=["udacitydl"],
+            entry_points = { "console_scripts" : [ "udacity-dl = udacitydl.udacitydl:main"]},
+            install_requires=["mechanize","beautifulsoup4","argparse"],
+           )
