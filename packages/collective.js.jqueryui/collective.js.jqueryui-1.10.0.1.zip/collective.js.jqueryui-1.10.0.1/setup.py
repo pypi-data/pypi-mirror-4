f4fdccf758cab2d5bc146e8241bb7faf3f2f9416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:30 +0000

    Add package collective.js.jqueryui/1.10.0.1/collective.js.jqueryui-1.10.0.1.zip

---
+++ packages/collective.js.jqueryui/collective.js.jqueryui-1.10.0.1.zip/collective.js.jqueryui-1.10.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.10.0.1'
+
+setup(name='collective.js.jqueryui',
+      version=version,
+      description="JQueryUI ready to be included in portal_javascript for Plone 4",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "UPGRADE.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        ],
+      keywords='plone jqueryui',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.js.jqueryui',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.jquery'
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
