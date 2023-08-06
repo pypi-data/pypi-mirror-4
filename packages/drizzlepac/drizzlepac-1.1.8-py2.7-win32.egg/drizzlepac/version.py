de9fcb67dcbc04be5a88dd1c7a45446b6c095f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:15 +0000

    Add package drizzlepac/1.1.8/drizzlepac-1.1.8-py2.7-win32.egg

---
+++ packages/drizzlepac/drizzlepac-1.1.8-py2.7-win32.egg/drizzlepac/version.py
@@ -0,0 +1,70 @@
+"""This is an automatically generated file created by stsci.distutils.hooks.version_setup_hook.
+Do not modify this file by hand.
+"""
+
+__all__ = ['__version__', '__vdate__', '__svn_revision__', '__svn_full_info__',
+           '__setup_datetime__']
+
+import datetime
+
+
+__version__ = '1.1.8'
+__vdate__ = '06-Feb-2013'
+__svn_revision__ = '24442'
+__svn_full_info__ = 'Path: drizzlepac\nURL: https://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/tags/release_1.1.8\nRepository Root: https://svn.stsci.edu/svn/ssb/stsci_python\nRepository UUID: fe389314-cf27-0410-b35b-8c050e845b92\nRevision: 24442\nNode Kind: directory\nSchedule: normal\nLast Changed Author: sienkiew\nLast Changed Rev: 24442\nLast Changed Date: 2013-04-19 17:14:03 -0400 (Fri, 19 Apr 2013)'
+__setup_datetime__ = datetime.datetime(2013, 4, 22, 12, 10, 43, 208000)
+
+
+def update_svn_info():
+    """Update the SVN info if running out of an SVN working copy."""
+
+    import os
+    import string
+    import subprocess
+
+    global __svn_revision__
+    global __svn_full_info__
+
+    # Wind up the module path until we find the root of the project
+    # containing setup.py
+    path = os.path.abspath(os.path.dirname(__file__))
+    dirname = os.path.dirname(path)
+    setup_py = os.path.join(path, 'setup.py')
+    while path != dirname and not os.path.exists(setup_py):
+        path = os.path.dirname(path)
+        dirname = os.path.dirname(path)
+        setup_py = os.path.join(path, 'setup.py')
+
+    try:
+        pipe = subprocess.Popen(['svnversion', path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if pipe.wait() == 0:
+            stdout = pipe.stdout.read().decode('latin1').strip()
+            if stdout and stdout[0] in string.digits:
+                __svn_revision__ = stdout
+    except OSError:
+        pass
+
+    try:
+        pipe = subprocess.Popen(['svn', 'info', path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if pipe.wait() == 0:
+            lines = []
+            for line in pipe.stdout.readlines():
+                line = line.decode('latin1').strip()
+                if not line:
+                    continue
+                lines.append(line)
+
+            if not lines:
+                __svn_full_info__ = 'unknown'
+            else:
+                __svn_full_info__ = '\n'.join(lines)
+    except OSError:
+        pass
+
+
+update_svn_info()
+del update_svn_info
