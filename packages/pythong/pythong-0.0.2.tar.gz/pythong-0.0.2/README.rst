5cce0225ef1ee8d2a56a173bbae26ff8ba1f449d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:03 +0000

    Add package pythong/0.0.2/pythong-0.0.2.tar.gz

---
+++ packages/pythong/pythong-0.0.2.tar.gz/pythong-0.0.2/README.rst
@@ -0,0 +1,53 @@
+pythong
+=======
+
+:Author: David Gay <oddshocks@gmail.com>
+
+Set up a minimal, yet comfortable structure for a Python project.
+
+Example Usage
+-------------
+
+Create a new project like so::
+
+    $ pythong mynewproject
+
+Or, for a quicker setup... ::
+
+    $ pythong --snap
+
+Including the project name in the command is optional.
+
+Since version 0.0.2, you can wash your pythong of messy build files::
+
+    $ pythong --wash
+
+A full list of options can be seen with::
+
+    $ pythong --help
+
+Get Pythong
+-----------
+
+You can install the latest release of Pythong from `PyPI
+<https://pypi.python.org/pypi/pythong>`_ with pip::
+
+    $ pip install pythong
+
+You can also get the source from PyPI or `GitHub
+<https://github.com/oddshocks/pythong>`_.
+Contributions are welcome! Yay, software freedom!
+
+License
+-------
+
+pythong is released under the GNU GPLv3+.
+
+Contributors
+------------
+
+Feel free to add your name.
+
+-   David Gay <oddshocks@gmail.com>
+-   Ryan Scott Brown <ryansb@csh.rit.edu>
+-   Ralph Bean  <rbean@redhat.com>
---
+++ packages/pythong/pythong-0.0.2.tar.gz/pythong-0.0.2/pythong/project.py
@@ -0,0 +1,103 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+import os
+import jinja2
+import readline
+from os.path import join
+from pythong.util import ask_yes_no, prompt_input, determine_directories
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.PackageLoader('pythong', 'templates'))
+setup_template = jinja_env.get_template('setup.py.jinja')
+distribute_template = jinja_env.get_template('distribute_setup.py.jinja')
+
+
+def prompt_new_project(name=None, snap=False):
+    """
+    sampleproject = dict(
+        encoding="utf8",
+        version="0.0.1",
+        shortname="pythong",
+        description="This is a description that has words",
+        classifiers=["thong", "wearing", "pythonistas"],
+        keywords=["more", "thong", "keywords"],
+        author="ryansb",
+        email="ryansb@csh.rit.edu",
+        url="github.com",
+        license="MIT",
+        requires=["nose", "pyramid"]
+    )
+    """
+    project = dict()
+
+    if not name:
+        name = prompt_input("Project name: ")
+
+    if os.path.isdir(name):
+        print "A project with that name already exists here."
+        exit(1)
+
+    project.update(determine_directories(name, os.getcwd(), snap))
+
+    # Files
+    project['setup_file'] = join(project['project_dir'], "setup.py")
+    project['init_file'] = join(project['source_dir'], "__init__.py")
+    if project.get('tests_dir'):
+        project['test_init_file'] = join(project['tests_dir'], "__init__.py")
+        project['test_file'] = join(project['tests_dir'], name + "_tests.py")
+
+    project['files'] = [project['setup_file'], project['init_file']]
+    if project.get('tests_dir'):
+        project['files'].extend([project['test_init_file'],
+                                 project['test_file']])
+
+    # Create project skeleton
+    print "Creating structure for new Python project {}.".format(
+        project.get("name"))
+    for dirname in project.get("directories", []):
+        os.mkdir(dirname)
+    for f in project.get("files", []):
+        project['init_file'] = open(f, 'w').close()
+
+    # Create setup.py file
+    # first, set sane defaults
+    project.update(dict(
+        encoding="utf8",
+        version="0.1.0",
+        shortname=project.get("name", "horsewithnoname"),
+        description="A new project",
+        classifiers=[],
+        keywords=[],
+        author="",
+        email="",
+        url="",
+        license="",
+        requires=[]))
+
+    if not snap and ask_yes_no("Would you like help creating a setup.py file?"):
+        project.update(dict(
+            encoding=prompt_input("Encoding [utf8]: ", default='utf8'),
+            version=prompt_input("Version [0.1.0]: ", default='0.1.0'),
+            shortname=prompt_input("Short name [%s]: " % project.get("name"),
+                                   default=project.get("name")),
+            description=prompt_input("Description [A new project]: ",
+                                     default='A new project'),
+            classifiers=prompt_input("Classifiers (comma delimited): "
+                                     ).split(','),
+            keywords=prompt_input("Keywords (comma delimited): ").split(','),
+            author=prompt_input("Author: "),
+            email=prompt_input("Author email: "),
+            url=prompt_input("Project URL: "),
+            license=prompt_input("License: "),
+            requires=prompt_input("Requirements (comma delimited): "
+                                  ).split(',')))
+    else:
+        print "Generating skeletal setup.py file."
+
+    with open(join(project['project_dir'], "distribute_setup.py"), 'w') as f:
+        f.write(distribute_template.render(project=project))
+
+    with open(project['setup_file'], 'w') as f:
+        f.write(setup_template.render(project=project))
+        exit(0)
+    exit(1)
