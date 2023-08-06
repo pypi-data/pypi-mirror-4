3e19392045961df7b9aaca989d2be2e16be4c341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:17 +0000

    Add package mlocs/1.0.4/mlocs-1.0.4.tar.gz

---
+++ packages/mlocs/mlocs-1.0.4.tar.gz/mlocs-1.0.4/mlocs/ml.py
@@ -0,0 +1,20 @@
+mloc = "J3A5"
+
+# bring to value :
+
+
+# l to r is letter, cijfer, letter cijfer etc
+
+# start with letter
+type = "letter"
+
+# endval
+val = 0
+vals = []
+
+for i in range(len(mloc)):
+	if type = 
+	print i
+
+
+
