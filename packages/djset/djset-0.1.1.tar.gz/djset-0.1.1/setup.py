dc48feb70e183c3bf5bf1d7eca84f6f8755f865c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:24 +0000

    Add package djset/0.1.1/djset-0.1.1.tar.gz

---
+++ packages/djset/djset-0.1.1.tar.gz/djset-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup
+
+
+setup(name='djset',
+    version='0.1.1',
+    description="Utilities for Django settings secrets",
+    author='Brett Haydon',
+    author_email='brett@haydon.id.au',
+    url='https://github.com/bretth/djset',
+    packages=['djset'],
+    license='LICENSE.txt',
+    entry_points={"console_scripts":
+    ["djs_locate_settings = djset.djset:locate_settings",
+     "djset = djset.command:main",
+     ]},
+    scripts=['dexportunset.sh',],
+    install_requires = ["keyring"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development',
+    ]
+)
