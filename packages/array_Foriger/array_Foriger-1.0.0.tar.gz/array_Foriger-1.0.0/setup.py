9bed82150f2bf6443ff3c006db31680b26882ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:40 +0000

    Add package array_Foriger/1.0.0/array_Foriger-1.0.0.tar.gz

---
+++ packages/array_Foriger/array_Foriger-1.0.0.tar.gz/array_Foriger-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name = 'array_Foriger',
+      version = '1.0.0',
+      py_modules = ['array_Foriger'],
+      author = 'Foriger',
+      author_email = 'foriger@abv.bg',
+      url = 'www.abv.bg',
+      description = 'Simple printer'
+) 
