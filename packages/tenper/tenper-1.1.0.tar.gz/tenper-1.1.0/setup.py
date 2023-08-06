ab380610c362e912d5a03314a1828e04e2d4535f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package tenper/1.1.0/tenper-1.1.0.tar.gz

---
+++ packages/tenper/tenper-1.1.0.tar.gz/tenper-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='tenper',
+    version='1.1.0',
+    description='A tmux session manager with optional virtualenv support.',
+    long_description=(
+        'Tenper is a tmux wrapper. It provides project-based tmux window/pane '
+        'layouts.  It has optional support for Python\'s virtualenv and the '
+        'conventions it uses permits concurrent usage of virtualenvwrapper.'),
+    keywords='tmux, virtualenv, virtualenvwrapper',
+    author='Mason Staugler',
+    author_email='mason@staugler.net',
+    url='https://github.com/mqsoh/tenper',
+    py_modules=['tenper'],
+    scripts=['tenper', 'tenper-completion.sh'],
+    license='MIT license',
+)
