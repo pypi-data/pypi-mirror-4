ea67e307d15f0810ddc6e2f066d8b528d4250b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:04 +0000

    Add package path.py/2.5/path.py-2.5.zip

---
+++ packages/path.py/path.py-2.5.zip/path.py-2.5/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import distutils.core
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+# Setup script for path
+
+kw = dict(
+    name = "path.py",
+    version = "2.5",
+    description = "A module wrapper for os.path",
+    author = "Jason Orendorff",
+    author_email = "jason.orendorff@gmail.com",
+    maintainer = "Jason R. Coombs",
+    maintainer_email = "jaraco@jaraco.com",
+    url = "http://github.com/jaraco/path.py",
+    license = "MIT License",
+    py_modules = ['path', 'test_path'],
+    cmdclass = dict(build_py=build_py),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
+
+
+if __name__ == '__main__':
+    distutils.core.setup(**kw)
