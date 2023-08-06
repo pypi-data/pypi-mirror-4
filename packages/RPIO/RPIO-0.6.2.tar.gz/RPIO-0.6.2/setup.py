3dccd525bd0b2087a25a8e3dad44c817cbd53df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:02 +0000

    Add package RPIO/0.6.2/RPIO-0.6.2.tar.gz

---
+++ packages/RPIO/RPIO-0.6.2.tar.gz/RPIO-0.6.2/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, Extension
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="RPIO",
+    version="0.6.2",
+    package_dir={"": "source"},
+    py_modules=["RPIO"],
+    ext_modules=[Extension('GPIO', ['source/c_gpio/py_gpio.c', \
+            'source/c_gpio/c_gpio.c', 'source/c_gpio/cpuinfo.c'])],
+    scripts=["source/rpio"],
+
+    description=(("Raspberry Pi GPIO tools: an extension of RPi.GPIO "
+            "to easily handle interrupts, and a command line multitool.")),
+    long_description=read('README.txt'),
+    url="https://github.com/metachris/RPIO",
+
+    author="Chris Hager",
+    author_email="chris@linuxuser.at",
+
+    license="MIT",
+    keywords=["raspberry", "raspberry pi", "interrupts", "gpio", "rpio"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Utilities",
+        "Topic :: Software Development",
+        "Topic :: Home Automation",
+        "Topic :: System :: Hardware",
+        "Intended Audience :: Developers",
+        ("License :: OSI Approved :: "
+                "GNU General Public License v3 or later (GPLv3+)"),
+        "License :: Other/Proprietary License",
+    ],
+)
