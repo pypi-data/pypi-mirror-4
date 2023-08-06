3a22b9b8389cb45182ec7c55acc36836eebdedd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:44 +0000

    Add package debug/0.2.1/debug-0.2.1.tar.gz

---
+++ packages/debug/debug-0.2.1.tar.gz/debug-0.2.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='debug',
+    version='0.2.1',
+    description='Start fancy debugger in a single statement.',
+    author='Maciej Konieczny',
+    author_email='hello@narf.pl',
+    url='https://github.com/narfdotpl/debug',
+    py_modules=['debug'],
+    install_requires=['ipdb >= 0.6.1', 'see >= 1.0']
+)
