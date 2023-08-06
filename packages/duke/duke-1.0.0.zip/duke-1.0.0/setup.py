2d4b8c156211ded3fb773adbff03f2a9c6a7f82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package duke/1.0.0/duke-1.0.0.zip

---
+++ packages/duke/duke-1.0.0.zip/duke-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+              name                     = 'duke',
+              version                  = '1.0.0',
+              py_module         = ['punk'],
+              author                   = 'sid',
+              author_email    = 'sidharthchandrasekar@gmail.com',
+              url                           = 'http://gmail.com',
+              description         = 'disintegrates list with optional identation'
+           )
