5075ca4d5e04754b9218af615243adedeecff39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:23 +0000

    Add package pyroute2/0.1.2/pyroute2-0.1.2.tar.gz

---
+++ packages/pyroute2/pyroute2-0.1.2.tar.gz/pyroute2-0.1.2/pyroute2/netlink/__init__.py
@@ -0,0 +1,236 @@
+import threading
+import select
+import struct
+import socket
+import Queue
+import os
+import io
+
+from pyroute2.netlink.generic import cmdmsg
+from pyroute2.netlink.generic import nlmsg
+from pyroute2.netlink.generic import NETLINK_GENERIC
+
+
+## Netlink message flags values (nlmsghdr.flags)
+#
+NLM_F_REQUEST = 1    # It is request message.
+NLM_F_MULTI = 2    # Multipart message, terminated by NLMSG_DONE
+NLM_F_ACK = 4    # Reply with ack, with zero or error code
+NLM_F_ECHO = 8    # Echo this request
+# Modifiers to GET request
+NLM_F_ROOT = 0x100    # specify tree    root
+NLM_F_MATCH = 0x200    # return all matching
+NLM_F_ATOMIC = 0x400    # atomic GET
+NLM_F_DUMP = (NLM_F_ROOT | NLM_F_MATCH)
+# Modifiers to NEW request
+NLM_F_REPLACE = 0x100    # Override existing
+NLM_F_EXCL = 0x200    # Do not touch, if it exists
+NLM_F_CREATE = 0x400    # Create, if it does not exist
+NLM_F_APPEND = 0x800    # Add to end of list
+
+NLMSG_NOOP = 0x1    # Nothing
+NLMSG_ERROR = 0x2    # Error
+NLMSG_DONE = 0x3    # End of a dump
+NLMSG_OVERRUN = 0x4    # Data lost
+NLMSG_MIN_TYPE = 0x10    # < 0x10: reserved control messages
+NLMSG_MAX_LEN = 0xffff  # Max message length
+
+IPRCMD_NOOP = 1
+IPRCMD_REGISTER = 2
+IPRCMD_UNREGISTER = 3
+IPRCMD_STOP = 4
+
+
+class netlink_error(socket.error):
+    def __init__(self, code, msg=None):
+        msg = msg or os.strerror(code)
+        super(netlink_error, self).__init__(code, msg)
+        self.code = code
+
+
+class marshal(object):
+    '''
+    Generic marshalling class
+    '''
+
+    msg_map = {}
+
+    def __init__(self, sock=None):
+        self.sock = sock
+        self.lock = threading.Lock()
+        # one marshal instance can be used to parse one
+        # message at once
+        self.buf = None
+        self.msg_map = self.msg_map or {}
+
+    def set_buffer(self, init=b''):
+        self.buf = io.BytesIO()
+        self.buf.write(init)
+        self.buf.seek(0)
+        return len(init)
+
+    def recv(self):
+        with self.lock:
+            total = self.set_buffer(self.sock.recv(16384))
+            offset = 0
+            result = []
+
+            while offset < total:
+                # pick type and length
+                (length, msg_type) = struct.unpack('IH', self.buf.read(6))
+                error = None
+                if msg_type == NLMSG_ERROR:
+                    self.buf.seek(16)
+                    code = abs(struct.unpack('i', self.buf.read(4))[0])
+                    if code > 0:
+                        error = netlink_error(code)
+
+                self.buf.seek(offset)
+                msg_class = self.msg_map.get(msg_type, nlmsg)
+                msg = msg_class(self.buf)
+                msg.decode()
+                msg['header']['error'] = error
+                self.fix_message(msg)
+                offset += msg.length
+                result.append(msg)
+
+            return result
+
+    def fix_message(self, msg):
+        pass
+
+
+class netlink_socket(socket.socket):
+    '''
+    Generic netlink socket
+    '''
+
+    def __init__(self, family=NETLINK_GENERIC):
+        socket.socket.__init__(self, socket.AF_NETLINK,
+                               socket.SOCK_DGRAM, family)
+        self.pid = os.getpid()
+        self.groups = None
+
+    def bind(self, groups=0):
+        self.groups = groups
+        socket.socket.bind(self, (self.pid, self.groups))
+
+
+class netlink_io(threading.Thread):
+    def __init__(self, marshal, family, groups):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.socket = netlink_socket(family)
+        self.socket.bind(groups)
+        self.marshal = marshal(self.socket)
+        self.listeners = {}
+        self.poll = select.poll()
+        (self.ctlr, self.control) = os.pipe()
+        self.register(self.ctlr)
+        self.register(self.socket)
+        self.__stop = False
+
+    def register(self, fd, mask=select.POLLIN):
+        self.poll.register(fd, mask)
+
+    def unregister(self, fd):
+        self.poll.unregister(fd)
+
+    def run(self):
+        while not self.__stop:
+            fds = self.poll.poll()
+            for fd in fds:
+                if fd[0] == self.ctlr:
+                    buf = io.BytesIO()
+                    buf.write(os.read(self.ctlr, 6))
+                    buf.seek(0)
+                    cmd = cmdmsg(buf)
+                    if cmd['command'] == IPRCMD_REGISTER:
+                        args = [cmd['v1'], ]
+                        if cmd['v2'] > 0:
+                            args.append(cmd['v2'])
+                        self.register(args)
+                    elif cmd['command'] == IPRCMD_UNREGISTER:
+                        self.unregister(cmd['v1'])
+                    elif cmd['command'] == IPRCMD_STOP:
+                        self.__stop = True
+                        break
+
+                elif fd[0] == self.socket.fileno():
+                    for msg in self.marshal.recv():
+                        key = msg['header']['sequence_number']
+                        if key not in self.listeners:
+                            key = 0
+                        if key in self.listeners:
+                            self.listeners[key].put(msg)
+
+
+class netlink(object):
+
+    family = NETLINK_GENERIC
+    groups = 0
+    marshal = marshal
+
+    def __init__(self, debug=False):
+        self.io_thread = netlink_io(self.marshal, self.family, self.groups)
+        self.io_thread.start()
+        self.listeners = self.io_thread.listeners
+        self.socket = self.io_thread.socket
+        self.debug = debug
+        self.__nonce = 1
+
+    def nonce(self):
+        '''
+        Increment netlink protocol nonce (there is no need to call it directly)
+        '''
+        if self.__nonce == 0xffffffff:
+            self.__nonce = 1
+        else:
+            self.__nonce += 1
+        return self.__nonce
+
+    def monitor(self, operate=True):
+        if operate:
+            self.listeners[0] = Queue.Queue()
+        else:
+            del self.listeners[0]
+
+    def stop(self):
+        msg = cmdmsg(io.BytesIO())
+        msg['command'] = IPRCMD_STOP
+        msg.encode()
+        os.write(self.io_thread.control, msg.buf.getvalue())
+
+    def get(self, key=0, blocking=True):
+        '''
+        Get a message from a queue
+        '''
+        assert key in self.listeners
+
+        result = []
+        while True:
+            msg = self.listeners[key].get()
+            if msg['header']['error'] is not None:
+                raise msg['header']['error']
+            if msg['header']['type'] != NLMSG_DONE:
+                result.append(msg)
+            if (msg['header']['type'] == NLMSG_DONE) or \
+               (not msg['header']['flags'] & NLM_F_MULTI):
+                break
+        return result
+
+    def nlm_request(self, msg, msg_type,
+                    msg_flags=NLM_F_DUMP | NLM_F_REQUEST):
+        nonce = self.nonce()
+        self.listeners[nonce] = Queue.Queue()
+        msg['header']['sequence_number'] = nonce
+        msg['header']['pid'] = os.getpid()
+        msg['header']['type'] = msg_type
+        msg['header']['flags'] = msg_flags
+        msg.encode()
+        self.socket.sendto(msg.buf.getvalue(), (0, 0))
+        result = self.get(nonce)
+        if not self.debug:
+            for i in result:
+                del i['header']
+        return result
---
+++ packages/pyroute2/pyroute2-0.1.2.tar.gz/pyroute2-0.1.2/pyroute2/netlink/ipdb.py
@@ -0,0 +1,254 @@
+'''
+Experimental IP database module. Please, do not use it
+unless you understand what are you doing.
+
+Quick start:
+    from pyroute2.netlink.ipdb import ipdb
+    ip = ipdb()
+    ip['eth0'].down()
+    ip['eth0']['address'] = '00:11:22:33:44:55'
+    ip['eth0']['ifname'] = 'bala'
+    ip['eth0']['txqlen'] = 2000
+    ip['eth0'].commit()
+    ip['bala'].up()
+'''
+import threading
+from socket import AF_INET
+from socket import AF_INET6
+from pyroute2.netlink.iproute import iproute
+from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
+
+nla_fields = [i[0] for i in ifinfmsg.nla_map]
+nla_fields.append('flags')
+nla_fields.append('mask')
+nla_fields.append('change')
+nla_fields.append('state')
+
+
+class upset(set):
+
+    def __init__(self):
+        set.__init__(self)
+        self.commit()
+
+    def commit(self):
+        self.added = set()
+        self.removed = set()
+
+    def add(self, item, track=True):
+        if track:
+            self.added.add(item)
+        set.add(self, item)
+
+    def remove(self, item, track=True):
+        if track:
+            self.removed.add(item)
+        set.remove(self, item)
+
+
+class dotkeys(dict):
+
+    def __getattribute__(self, key):
+        if key in self:
+            return self[key]
+        else:
+            return dict.__getattribute__(self, key)
+
+    def __setattr__(self, key, value):
+        if key in self:
+            self[key] = value
+        else:
+            dict.__setattr__(self, key, value)
+
+    def __delattr__(self, key):
+        if key in self:
+            del self[key]
+
+
+class interface(dotkeys):
+    def __init__(self, dev, ipr=None, parent=None):
+        self.ip = ipr
+        self.cleanup = ('af_spec',
+                        'attrs',
+                        'event',
+                        'map',
+                        'stats',
+                        'stats64')
+        dev.prefix = 'IFLA_'
+        self.parent = parent
+        self.fields = [dev.nla2name(i) for i in nla_fields]
+        self.__updated = {}
+        self.load(dev)
+
+    def load(self, dev):
+        dev.prefix = 'IFLA_'
+        # invalidate old info
+        for key in tuple(self.keys()):
+            del self[key]
+        # load new info
+        self.update(dev)
+        self.__attrs = set()
+        for (name, value) in dev['attrs']:
+            norm = dev.nla2name(name)
+            self.__attrs.add(norm)
+            self[norm] = value
+        for item in self.cleanup:
+            if item in self:
+                del self[item]
+        self.old_name = self['ifname']
+        self.__updated = {}
+        if self.parent is not None:
+            self['ipaddr'] = self.parent.ipaddr[self['index']]
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.__updated[key] = self[key]
+        dotkeys.__setitem__(self, key, value)
+
+    def rollback(self):
+        for i in self['ipaddr'].added:
+            self['ipaddr'].remove(i, track=False)
+        for i in self['ipaddr'].removed:
+            self['ipaddr'].add(i, track=False)
+        for i in tuple(self.__updated.keys()):
+            self[i] = self.__updated[i]
+            del self.__updated[i]
+
+    def commit(self):
+        e = None
+        try:
+            # commit IP address changes
+            for i in self['ipaddr'].added:
+                self.ip.addr_add(self['index'], i[0], i[1])
+            for i in self['ipaddr'].removed:
+                self.ip.addr_del(self['index'], i[0], i[1])
+            # commit interface changes
+            request = {}
+            for key in self.__updated:
+                if key in self.fields:
+                    request[key] = self[key]
+            self.ip.link('set', self['index'], **request)
+        except Exception as e:
+            # something went wrong: roll the transaction back
+            try:
+                # remove all added and add all removed
+                for i in self['ipaddr'].added:
+                    self.ip.addr_del(self['index'], i[0], i[1])
+                    self['ipaddr'].remove(i, track=False)
+                for i in self['ipaddr'].removed:
+                    self.ip.addr_add(self['index'], i[0], i[1])
+                    self['ipaddr'].add(i, track=False)
+                # apply old attributes
+                request = {}
+                for key in tuple(self.__updated.keys()):
+                    if key in self.fields:
+                        request[key] = self.__updated[key]
+                        del self.__updated[key]
+                self.ip.link('set', self['index'], **request)
+            except:
+                pass
+        # flush IP address changes
+        self['ipaddr'].commit()
+        # re-load interface information
+        self.load(self.ip.get_links(self['index'])[0])
+        if e is not None:
+            raise e
+
+    def up(self):
+        self.ip.link('set', self['index'], state='up')
+
+    def down(self):
+        self.saved_flags = self['flags']
+        self.ip.link('set', self['index'], state='down')
+
+    def restore(self):
+        self.ip.link('set', self['index'],
+                     flags=self.saved_flags, mask=0xffff)
+
+    def rename(self, name):
+        self.down()
+        self.ip.link('set', self['index'], ifname=name)
+        self.restore()
+
+    def remove(self):
+        self.ip.link('delete', self['index'])
+
+
+class ipdb(dotkeys):
+    '''
+    The class that maintains information about network setup
+    of the host. Monitoring netlink events allows it to react
+    immediately. It uses no polling.
+    '''
+
+    def __init__(self, ipr=None):
+        self.ip = ipr or iproute()
+
+        # start monitoring thread
+        self.ip.monitor()
+        self.mthread = threading.Thread(target=self.monitor)
+        self.mthread.setDaemon(True)
+        self.mthread.start()
+
+        # caches
+        self.ipaddr = {}
+        self.routes = {}
+        self.neighbors = {}
+
+        # load information on startup
+        self.update_links(self.ip.get_links())
+        self.update_addr(self.ip.get_addr())
+
+    def update_links(self, links):
+        for dev in links:
+            if dev['index'] not in self.ipaddr:
+                self.ipaddr[dev['index']] = upset()
+            i = self[dev['index']] = interface(dev, self.ip, self)
+            self[i['ifname']] = i
+
+    def get_addr_nla(self, msg):
+        nla = None
+        if msg['family'] == AF_INET:
+            nla = [i[1] for i in msg['attrs']
+                   if i[0] == 'IFA_LOCAL']
+        elif msg['family'] == AF_INET6:
+            nla = [i[1] for i in msg['attrs']
+                   if i[0] == 'IFA_ADDRESS']
+        return nla
+
+    def update_addr(self, addrs, action='add'):
+        for addr in addrs:
+            nla = self.get_addr_nla(addr)
+            if nla is not None:
+                nla.append(addr['prefixlen'])
+                method = getattr(self.ipaddr[addr['index']], action)
+                try:
+                    method(tuple(nla), track=False)
+                except:
+                    import traceback
+                    traceback.print_exc()
+
+    def monitor(self):
+        while True:
+            messages = self.ip.get()
+            for msg in messages:
+                if msg['event'] == 'RTM_NEWLINK':
+                    index = msg['index']
+                    if index in self:
+                        # get old name
+                        old_name = self[index].old_name
+                        # load message
+                        self[index].load(msg)
+                        # check for new name
+                        if self[index]['ifname'] != old_name:
+                            del self[old_name]
+                            self[self[index]['ifname']] = self[index]
+                    else:
+                        self.update_links([msg])
+                elif msg['event'] == 'RTM_DELLINK':
+                    del self[self[msg['index']]['ifname']]
+                    del self[msg['index']]
+                elif msg['event'] == 'RTM_NEWADDR':
+                    self.update_addr([msg], 'add')
+                elif msg['event'] == 'RTM_DELADDR':
+                    self.update_addr([msg], 'remove')
