e35b48cb53827021a2cc26f8767b28992364241c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:24 +0000

    Add package thegiant/1.2.1/thegiant-1.2.1.tar.gz

---
+++ packages/thegiant/thegiant-1.2.1.tar.gz/thegiant-1.2.1/thegiant/common.c
@@ -0,0 +1,124 @@
+#include "common.h"
+
+#define UNHEX(c) ((c >= '0' && c <= '9') ? (c - '0') : \
+                  (c >= 'a' && c <= 'f') ? (c - 'a' + 10) : \
+                  (c >= 'A' && c <= 'F') ? (c - 'A' + 10) : NOHEX)
+#define NOHEX -1
+
+size_t unquote_url_inplace(char* url, size_t len)
+{
+  for(char *p=url, *end=url+len; url != end; ++url, ++p) {
+    if(*url == '%') {
+      if(url >= end-2) {
+        /* Less than two characters left after the '%' */
+        return 0;
+      }
+      char a = UNHEX(url[1]);
+      char b = UNHEX(url[2]);
+      if(a == NOHEX || b == NOHEX) return 0;
+      *p = a*16 + b;
+      url += 2;
+      len -= 2;
+    } else {
+      *p = *url;
+    }
+  }
+  return len;
+}
+
+void _init_common(void)
+{
+  #define _(name) _##name = PyString_FromString(#name)
+  _(REMOTE_ADDR); _(PATH_INFO); _(QUERY_STRING); _(close);
+  _(REQUEST_METHOD); _(SERVER_PROTOCOL); _(GET);
+  _(HTTP_CONTENT_LENGTH); _(CONTENT_LENGTH); _(HTTP_CONTENT_TYPE); _(CONTENT_TYPE);
+  _HTTP_1_1 = PyString_FromString("HTTP/1.1");
+  _HTTP_1_0 = PyString_FromString("HTTP/1.0");
+  _SERVER_PROTOCOL = PyString_FromString("REDIS");
+  _wsgi_input = PyString_FromString("wsgi.input");
+  _empty_string = PyString_FromString("");
+  #undef _
+}
+
+
+/* Convert a string into a long long. Returns 1 if the string could be parsed
+ * into a (non-overflowing) long long, 0 otherwise. The value will be set to
+ * the parsed value when appropriate. */
+int string2ll(const char *s, size_t slen, long long *value) {
+    const char *p = s;
+    size_t plen = 0;
+    int negative = 0;
+    unsigned long long v;
+
+    if (plen == slen)
+        return 0;
+
+    /* Special case: first and only digit is 0. */
+    if (slen == 1 && p[0] == '0') {
+        if (value != NULL) *value = 0;
+        return 1;
+    }
+
+    if (p[0] == '-') {
+        negative = 1;
+        p++; plen++;
+
+        /* Abort on only a negative sign. */
+        if (plen == slen)
+            return 0;
+    }
+
+    /* First digit should be 1-9, otherwise the string should just be 0. */
+    if (p[0] >= '1' && p[0] <= '9') {
+        v = p[0]-'0';
+        p++; plen++;
+    } else if (p[0] == '0' && slen == 1) {
+        *value = 0;
+        return 1;
+    } else {
+        return 0;
+    }
+
+    while (plen < slen && p[0] >= '0' && p[0] <= '9') {
+        if (v > (ULLONG_MAX / 10)) /* Overflow. */
+            return 0;
+        v *= 10;
+
+        if (v > (ULLONG_MAX - (p[0]-'0'))) /* Overflow. */
+            return 0;
+        v += p[0]-'0';
+
+        p++; plen++;
+    }
+
+    /* Return if not all bytes were used. */
+    if (plen < slen)
+        return 0;
+
+    if (negative) {
+        if (v > ((unsigned long long)(-(LLONG_MIN+1))+1)) /* Overflow. */
+            return 0;
+        if (value != NULL) *value = -v;
+    } else {
+        if (v > LLONG_MAX) /* Overflow. */
+            return 0;
+        if (value != NULL) *value = v;
+    }
+    return 1;
+}
+
+/* Convert a string into a long. Returns 1 if the string could be parsed into a
+ * (non-overflowing) long, 0 otherwise. The value will be set to the parsed
+ * value when appropriate. */
+int string2l(const char *s, size_t slen, long *lval) {
+    long long llval;
+
+    if (!string2ll(s,slen,&llval))
+        return 0;
+
+    if (llval < LONG_MIN || llval > LONG_MAX)
+        return 0;
+
+    *lval = (long)llval;
+    return 1;
+}
---
+++ packages/thegiant/thegiant-1.2.1.tar.gz/thegiant-1.2.1/thegiant/request.h
@@ -0,0 +1,74 @@
+#ifndef __request_h__
+#define __request_h__
+
+#include <ev.h>
+#include "common.h"
+
+void _initialize_request_module(const char* host, const int port);
+
+typedef struct {
+  unsigned error_code : 2;
+  unsigned parse_finished : 1;
+  unsigned start_response_called : 1;
+  unsigned wsgi_call_done : 1;
+  unsigned keep_alive : 1;
+  unsigned response_length_unknown : 1;
+  unsigned chunked_response : 1;
+  unsigned use_sendfile : 1;
+} request_state;
+
+typedef struct {  
+  string field;
+  string value;
+  string body;
+} bj_parser;
+
+
+#define RDS_PHASE_START    1
+#define RDS_PHASE_DATA     2
+#define RDS_PHASE_CONNECT  3
+
+typedef struct {
+#ifdef DEBUG
+  unsigned long id;
+#endif
+  bj_parser parser;
+  ev_io ev_watcher;
+
+  int client_fd;
+  PyObject* client_addr;
+
+  request_state state;
+
+  int parse_phase;
+  int multibulklen;
+  int lastpos;
+  int bulklen;
+  
+  PyObject* status;
+  PyObject* headers;
+  PyObject* cmd_list; // redis command as a list...
+  int arg_cnt; // argument counter for redis commands
+
+  
+  char *requestbuffer;
+  int requestbufferlen;
+  
+  int partialread;
+
+  PyObject* current_chunk;
+  Py_ssize_t current_chunk_p;
+  PyObject* iterable;
+  PyObject* iterator;
+} Request;
+
+#define REQUEST_FROM_WATCHER(watcher) \
+  (Request*)((size_t)watcher - (size_t)(&(((Request*)NULL)->ev_watcher)));
+
+Request* Request_new(int client_fd, const char* client_addr);
+void Request_parse(Request*, const char*, const size_t);
+void Request_reset(Request*);
+void Request_clean(Request*);
+void Request_free(Request*);
+int on_line_complete(Request* request);
+#endif
