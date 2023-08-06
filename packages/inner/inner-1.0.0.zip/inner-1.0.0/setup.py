1561dbc376b3d9c0905c5d1429995f7fe7523436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:46 +0000

    Add package inner/1.0.0/inner-1.0.0.zip

---
+++ packages/inner/inner-1.0.0.zip/inner-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from  distutils.core  import setup
+
+setup(
+      name= 'inner' ,
+
+      version = '1.0.0' ,
+
+      py_module = ['python_sample'],
+      
+     author = 'ashish',
+     url = 'https://mail.google.com/mail/u/0/?shva=1#inbox' ,
+    
+    description = ' a simple test' ,
+
+) 
