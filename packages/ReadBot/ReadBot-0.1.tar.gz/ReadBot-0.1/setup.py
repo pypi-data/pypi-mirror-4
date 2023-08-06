af160a37c76ec7621cc5876c6e4fc50a01669610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:45 +0000

    Add package ReadBot/0.1/ReadBot-0.1.tar.gz

---
+++ packages/ReadBot/ReadBot-0.1.tar.gz/ReadBot-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ReadBot',
+    author='Matthew Meyer',
+    author_email='matthewwilliammeyer@gmail.com',
+    url='https://github.com/mwmnj/readbot',
+    version='0.1',
+    packages=['readbot',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
