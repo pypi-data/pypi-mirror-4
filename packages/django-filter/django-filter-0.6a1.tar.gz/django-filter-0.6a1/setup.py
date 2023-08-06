9c6c2bf632242e338a2369f25d9408e99f82b90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:55 +0000

    Add package django-filter/0.6a1/django-filter-0.6a1.tar.gz

---
+++ packages/django-filter/django-filter-0.6a1.tar.gz/django-filter-0.6a1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+f = open('README.rst')
+readme = f.read()
+f.close()
+
+setup(
+    name='django-filter',
+    version='0.6a1',
+    description=('Django-filter is a reusable Django application for allowing'
+                 ' users to filter querysets dynamically.'),
+    long_description=readme,
+    author='Alex Gaynor',
+    author_email='alex.gaynor@gmail.com',
+    url='http://github.com/alex/django-filter/tree/master',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
