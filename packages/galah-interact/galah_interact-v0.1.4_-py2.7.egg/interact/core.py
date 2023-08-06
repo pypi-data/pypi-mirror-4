75e6ccf67f695d86e4e7735eee2cbb20c9b69127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package galah-interact/v0.1.4-/galah_interact-v0.1.4_-py2.7.egg

---
+++ packages/galah-interact/galah_interact-v0.1.4_-py2.7.egg/interact/core.py
@@ -0,0 +1,341 @@
+# Copyright (c) 2013 Galah Group LLC
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of galah-interact-python.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Functions and classes that are essential when using this library. Is imported
+by core such that `interact.core.x` and `interact.x` is equivalent.
+
+"""
+
+import _utils
+import os.path
+import sys
+
+class TestResult:
+    """
+    Represents the result of one unit of testing. The goal is to generate a
+    number of these and then pass them all out of the test harness with a final
+    score.
+
+    """
+
+    class Message:
+        """
+        A message to the user. This is the primary mode of giving feedback to
+        users.
+
+        """
+
+        def __init__(self, text, *args, **kwargs):
+            # We have some special keyword arguments that we want to snatch if
+            # present.
+            self.dscore = kwargs.get("dscore", None)
+            self.type = kwargs.get("type", None)
+
+            self.args = args
+            self.kwargs = kwargs
+
+            self.text = text
+
+        def __str__(self):
+            return self.text.format(*self.args, **self.kwargs)
+
+        def __repr__(self):
+            return _utils.default_repr(self)
+
+    def __init__(
+            self, brief = None, score = None, max_score = None, messages = None,
+            default_message = None, bulleted_messages = True):
+        if messages is None:
+            messages = []
+
+        self.brief = brief
+        self.score = score
+        self.max_score = max_score
+        self.messages = messages
+        self.default_message = default_message
+        self.bulleted_messages = bulleted_messages
+
+    def add_message(self, *args, **kwargs):
+        """
+        Adds a message object to the TestResult. If a Message object that is
+        used, otherwise a new Message object is constructed and its constructor
+        is passed all the arguments.
+
+        """
+
+        if len(args) == 1 and isinstance(args[0], TestResult.Message):
+            self.messages.append(args[0])
+        else:
+            self.messages.append(TestResult.Message(*args, **kwargs))
+
+    def calculate_score(self, starting_score = None, max_score = None):
+        """
+        Automatically calculates the score by adding up the dscore fields of
+        every message.
+
+        If max_score is None, self.max_score is used.
+
+        If starting_score is None, max_score is used.
+
+        """
+        if max_score is None:
+            max_score = self.max_score
+
+        if starting_score is None:
+            starting_score = max_score
+
+        self.score = starting_score
+        self.max_score = max_score
+
+        for i in self.messages:
+            if i.dscore is not None:
+                self.score += i.dscore
+
+    def set_passing(self, passing):
+        self.score = 1 if passing else 0
+        self.max_score = 1
+
+    def is_passing(self):
+        return self.score != 0
+
+    def is_failing(self):
+        return self.score == 0
+
+    def to_galah_dict(self, name):
+        return {
+            "name": name,
+            "score": 0 if self.score is None else self.score,
+            "max_score": 0 if self.max_score is None else self.max_score,
+            "message": self.to_str(show_score = False)
+        }
+
+    def to_str(self, show_score = True):
+        result = []
+
+        quick_status = ""
+        if show_score and self.score is not None:
+            quick_status += "Score: %d" % self.score
+
+            if self.max_score is not None and self.max_score != 0:
+                quick_status += " out of %d" % self.max_score
+
+            result += [quick_status, ""]
+
+        if self.brief:
+            result += [str(self.brief), ""]
+
+        if self.messages:
+            for i in self.messages:
+                if self.bulleted_messages:
+                    result.append(" * " + str(i))
+                else:
+                    result.append(str(i))
+            result.append("")
+        elif self.default_message:
+            result += [self.default_message, ""]
+
+        return "\n".join(result[:-1])
+
+    def __str__(self):
+        return self.to_str()
+
+    def __repr__(self):
+        return _utils.default_repr(self)
+
+class FailedDependencies(TestResult):
+    def __init__(self, max_score = 10):
+        TestResult.__init__(
+            self,
+            brief = "This test will only be run if all of the other tests it "
+                    "depends on pass first. Fix those tests *before* worrying "
+                    "about this one.",
+            score = 0,
+            max_score = 10
+        )
+
+class Harness:
+    class Test:
+        def __init__(self, name, depends, func, result = None):
+            self.name = name
+            self.depends = depends
+            self.func = func
+            self.result = result
+
+    def __init__(self):
+        self.sheep_data = {}
+        self.tests = {}
+
+    def start(self):
+        """
+        Marks the start of the test harness. When no command line arguments are
+        present, this command will block and read JSON in from stdin. If
+        command line arguments are present information will be gathered from
+        them.
+
+        """
+
+        if len(sys.argv) == 4 and sys.argv[1] == "--test":
+            self.sheep_data["harness_directory"] = \
+                _utils.resolve_path(sys.argv[2])
+
+            self.sheep_data["testables_directory"] = \
+                _utils.resolve_path(sys.argv[3])
+        else:
+            import json
+            self.sheep_data = json.load(sys.stdin)
+
+    def finish(self, score = None, max_score = None):
+        """
+        Marks the end of the test harness. When start was not initialized via
+        command line arguments, this command will print out the test results in
+        a human readable fashion. Otherwise it will print out JSON appropriate
+        for Galah to read.
+
+        """
+
+        if score is None or max_score is None:
+            new_score = 0
+            new_max_score = 0
+            for i in self.tests.values():
+                if i is not None:
+                    if i.result.score:
+                        new_score += i.result.score
+
+                    if i.result.max_score:
+                        new_max_score += i.result.max_score
+
+            if score is None:
+                score = new_score
+
+            if max_score is None:
+                max_score = new_max_score
+
+        if len(sys.argv) == 4 and sys.argv[1] == "--test":
+            for i in self.tests.values():
+                if i.result:
+                    print i.result
+                    print "-------"
+            print "Final result: %d out of %d" % (score, max_score)
+        else:
+            import json
+            results = {
+                "score": score,
+                "max_score": max_score,
+                "tests": []
+            }
+
+            for i in self.tests.values():
+                if i is not None:
+                    results["tests"].append(i.result.to_galah_dict(i.name))
+
+            json.dump(results, sys.stdout)
+
+    def test(self, name, depends = None):
+        """
+        A decorator that takes in a test name and some dependencies and makes
+        the harness aware of it all.
+
+        """
+
+        def test_decorator(func):
+            def inner(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            self.tests[inner] = Harness.Test(name, depends, inner)
+
+            return inner
+
+        return test_decorator
+
+    def run_tests(self):
+        """
+        Runs all of the tests the user has registered.
+
+        If there is a cyclic dependency a RuntimeError will be raised.
+
+        """
+
+        remaining_tests = list(self.tests.keys())
+
+        # Naively execute each test in proper order based on their
+        # dependencies.
+        while remaining_tests:
+            # Figure out the next test to run
+            to_test = None
+            for i in remaining_tests:
+                current_test = self.tests[i]
+
+                # If the current test doesn't have any dependencies, let's run
+                # it now.
+                if not current_test.depends:
+                    to_test = current_test
+                    break
+                else:
+                    # Check all the dependencies of the current test, if they
+                    # are all passing, we're done.
+                    for j in current_test.depends:
+                        # If one of the dependencies hasn't been run or it's
+                        # failing
+                        if self.tests[j].result is None:
+                            break
+                        elif self.tests[j].result.is_failing():
+                            if not isinstance(current_test.result,
+                                    FailedDependencies):
+                                current_test.result = FailedDependencies()
+
+                            current_test.result.add_message(
+                                "Dependency *{dependency_name}* failed.",
+                                dependency_name = self.tests[j].name
+                            )
+                    else:
+                        if isinstance(current_test.result, FailedDependencies):
+                            remaining_tests.remove(current_test.func)
+                        else:
+                            to_test = current_test
+
+            if to_test is not None:
+                self.tests[to_test.func].result = to_test.func()
+                remaining_tests.remove(to_test.func)
+            elif remaining_tests:
+                raise RuntimeError("Cyclic dependencies!")
+            else:
+                break
+
+    def student_file(self, filename):
+        """
+        Given a path to a student's file relative to the root of the student's
+        submission, returns an absolute path to that file.
+
+        """
+
+        testables_directory = \
+            self.sheep_data.get("testables_directory", "../submission/")
+
+        path = os.path.join(testables_directory, filename)
+
+        # Ensure that we return an absolute path.
+        return _utils.resolve_path(path)
+
+    def student_files(self, *args):
+        """
+        Very similar to student_file. Given many files as arguments, will return
+        a list of absolute paths to those files.
+
+        """
+
+        return [self.student_file(i) for i in args]
---
+++ packages/galah-interact/galah_interact-v0.1.4_-py2.7.egg/interact/standardtests.py
@@ -0,0 +1,193 @@
+# Copyright (c) 2013 Galah Group LLC
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of galah-interact-python.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test suites that perform full testing on input, returning TestResult objects.
+These are typical tests that many harnesses need to perform. Such as checking
+indentation or checking to see if the correct files were submitted.
+
+"""
+
+import os
+import os.path
+import re
+import tempfile
+import subprocess
+import atexit
+import shutil
+
+import _utils
+import core
+import pretty
+import parse
+import execute
+
+def check_files_exist(*files):
+    """
+    Checks to see if the given files provided as arguments exist. They must be
+    files as defined by os.path.isfile(). Returns a TestResult.
+
+    The TestResult will be passing if and only if all of the files exist.
+
+    """
+
+    missing_files = [i for i in files if not os.path.isfile(i)]
+
+    result = core.TestResult(
+        brief = "This test ensures that all of the necessary files are "
+                "present.",
+        default_message = "Great job! All the necessary files are present."
+    )
+
+    if missing_files:
+        result.add_message(core.TestResult.Message(
+            "You are missing {missing_files_}.",
+            missing_files_ = pretty.pretty_list(missing_files),
+            missing_files = missing_files,
+            dscore = -1,
+            type = "interact/filesexist/basic_files_exist"
+        ))
+        result.set_passing(False)
+    else:
+        result.set_passing(True)
+
+    return result
+
+def indentation(code, file_name, max_score = 10, allow_negative = False):
+    """
+    Given a chunk of code as a string, this function will determine if the
+    student indented properly. Returns a TestResult object.
+
+    Tests:
+        * A student's code is correctly indented if every line of each sub-block
+          is indented strictly more than the least indented line on the parent
+          block.
+
+          If any problems are found, a single TestMessage will be added to the
+          returned TestResult object with type
+          `interact/indentation/basic_indent_level`. The lines property of this
+          message is a list of every Line that had a problem.
+
+    Test Result:
+        The test result object that is returned
+
+    """
+
+    # Transform the code into a list of Line objects
+    lines = parse.Line.make_lines(code.splitlines())
+
+    # Break the code up into blocks.
+    blocks = parse.grab_blocks(lines)
+
+    # Recursive helper function.
+    def check(block, minimum = None):
+        problems = []
+
+        # Check that each line in the current block has an indnetation level
+        # strictly greater than the minimum.
+        for i in block.lines:
+            indent_level = i.indent_level()
+            if minimum is not None and indent_level is not None and \
+                    indent_level <= minimum:
+                problems.append(i)
+
+        # Find the indent level of the least indented line in the current block
+        levels = []
+        for i in block.lines:
+            level = i.indent_level()
+            if level is not None:
+                levels.append(level)
+        new_minimum = min(levels)
+
+        # Recurse into every sub block
+        for i in block.sub_blocks:
+            problems += check(i, new_minimum)
+
+        return problems
+
+    result = core.TestResult(
+        brief = "This test checks to ensure you are indenting properly. Make "
+                "sure that every time you start a new block (curly braces "
+                "delimit blocks) you indent more.",
+        default_message = "Great job! You have perfect indentation!",
+        max_score = max_score
+    )
+
+    problems = check(blocks)
+    if problems:
+        result.add_message(core.TestResult.Message(
+            "{lines_} {line_numbers_} in {file_name} {are_} not indented more "
+                "than the outer block.",
+            line_numbers_ = pretty.pretty_list(i.line_number for i in problems),
+            are_ = "are" if len(problems) > 1 else "is",
+            lines_ = pretty.plural_if("Line", problems),
+            lines = problems,
+            file_name = file_name,
+            dscore = -len(problems),
+            type = "interact/indentation/basic_indent_level"
+        ))
+
+    # The score is just the sum of the dscores for each message plus the max
+    # score (since dscores are negative this makes sense).
+    score = max_score + sum(i.dscore for i in result.messages)
+    if not allow_negative:
+        score = max(0, score)
+    result.score = score
+
+    return result
+
+def check_compiles(files, flags = [], ignore_cache = False):
+    # Try to compile the program
+    compiler_output, executable_path = execute.compile_program(
+        files, flags = flags, ignore_cache = ignore_cache
+    )
+
+    command = pretty.craft_shell_command(
+        execute.create_compile_command(files, flags)
+    )
+
+    result = core.TestResult(
+        brief = "This test ensures that your code compiles without errors. "
+                "Your program was compiled with %s." % (command, ),
+        default_message = "**Great job!** Your code compiled cleanly without "
+                          "any problems.",
+        max_score = 10,
+        bulleted_messages = False
+    )
+
+    # If the compilation failed
+    if executable_path is None:
+        if compiler_output:
+            compiler_output = pretty.truncate_string(compiler_output)
+
+            result.add_message(
+                "Your code did not compile. The compiler outputted the following "
+                "errors:\n\n```\n{compiler_output}\n```\n",
+                compiler_output = compiler_output,
+                dscore = -10
+            )
+        else:
+            result.add_message(
+                "Your code did not compile but the compiler did not output "
+                "any errors or warnings."
+            )
+
+        result.score = 0
+    else:
+        result.score = 10
+
+    return result;
