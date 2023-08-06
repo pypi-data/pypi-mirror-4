3c7f6d4bb073a4fad3be870c719506b3bbde2682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:03 +0000

    Add package pywizard-extra-modules/0.1.2/pywizard-extra-modules-0.1.2.tar.gz

---
+++ packages/pywizard-extra-modules/pywizard-extra-modules-0.1.2.tar.gz/pywizard-extra-modules-0.1.2/pywizard_extra_modules/stunnel.py
@@ -0,0 +1,28 @@
+from pywizard.resources.file import replace_in_file, file_
+from pywizard.resources.package import package
+from pywizard.resources.service import service
+from pywizard.templating import tpl
+
+
+def stunnel(ssl_cert, ssl_key, source, dest):
+    package('stunnel4')
+
+    replace_in_file('/etc/default/stunnel4', 'ENABLED=0', 'ENABLED=1')
+
+    stunnel_service = service('stunnel4', start=False)
+
+    file_(
+        '/etc/stunnel/main.conf',
+        content=tpl('stunnel.conf', context=locals()),
+        on_create=(
+            stunnel_service.start,
+        ),
+        on_update=(
+            stunnel_service.reload,
+        ),
+        if_exist=(
+            stunnel_service.start,
+        ),
+    )
+
+
