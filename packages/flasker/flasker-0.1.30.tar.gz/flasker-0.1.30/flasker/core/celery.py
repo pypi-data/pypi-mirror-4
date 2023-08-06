379d79a945a9424c494839f37dfadddcfc9849e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package flasker/0.1.30/flasker-0.1.30.tar.gz

---
+++ packages/flasker/flasker-0.1.30.tar.gz/flasker-0.1.30/flasker/core/celery.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Creating the Celery application."""
+
+from __future__ import absolute_import
+
+from celery import Celery
+from celery.signals import worker_process_init
+from celery.task import periodic_task
+
+from ..project import current_project
+
+pj = current_project
+
+celery = Celery()
+celery.conf.update(pj.config['CELERY'])
+celery.periodic_task = periodic_task
+
+@worker_process_init.connect
+def create_worker_connection(*args, **kwargs):
+  """Initialize database connection.
+
+  This has to be done after the worker processes have been started otherwise
+  the connection will fail.
+
+  """
+  pj._setup_database_connection(app=False, celery=True)
+
+pj.celery = celery
