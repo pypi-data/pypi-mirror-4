1085b41b39447ce5b1a83dc7566f54e3f121cce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:05 +0000

    Add package richard_nester/1.0.1/richard_nester-1.0.1.zip

---
+++ packages/richard_nester/richard_nester-1.0.1.zip/richard_nester-1.0.1/nester.py
@@ -0,0 +1,9 @@
+def print_lot(the_list,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lot(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t",end="")
+			print(each_item)
+
