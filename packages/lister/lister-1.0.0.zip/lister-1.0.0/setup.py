7417b8629a62681ec7f20dd28ffe056a8aa068ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package lister/1.0.0/lister-1.0.0.zip

---
+++ packages/lister/lister-1.0.0.zip/lister-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='lister',
+	version='1.0.0',
+	py_modules=['lister'],
+	author='Abhishek',
+	author_email=('abhishekde@hotmail.com'),
+	description='A simple list printing program',
+	)
