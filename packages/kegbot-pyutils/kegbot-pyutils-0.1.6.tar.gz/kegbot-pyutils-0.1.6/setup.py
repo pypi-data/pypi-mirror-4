97c97e158b8d8d33e4d7b2900e9a4bf63240c1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:15 +0000

    Add package kegbot-pyutils/0.1.6/kegbot-pyutils-0.1.6.tar.gz

---
+++ packages/kegbot-pyutils/kegbot-pyutils-0.1.6.tar.gz/kegbot-pyutils-0.1.6/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Library of miscellaneous utilities used within Kegbot.
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1.6'
+DOCLINES = __doc__.split('\n')
+SHORT_DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+
+def setup_package():
+
+  setup(
+      name = 'kegbot-pyutils',
+      version = VERSION,
+      description = SHORT_DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      author = 'mike wakerly',
+      author_email = 'opensource@hoho.com',
+      url = 'http://kegbot.org/',
+      packages = find_packages(exclude=['testdata']),
+      namespace_packages = ['kegbot'],
+      install_requires = [
+        'isodate == 0.4.9',
+        'python-gflags >= 1.8',
+      ],
+      include_package_data = True,
+  )
+
+if __name__ == '__main__':
+  setup_package()
