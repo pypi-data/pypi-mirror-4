0ebbcc0fa97823928fae5ec0b6346e011d6985bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package airbrake-flask/0.0.2/airbrake-flask-0.0.2.tar.gz

---
+++ packages/airbrake-flask/airbrake-flask-0.0.2.tar.gz/airbrake-flask-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airbrake-flask',
+    version='0.0.2',
+    author='Kien Pham',
+    author_email='kien@sendgrid.com',
+    py_modules=['airbrake'],
+    url='https://github.com/kienpham2000/airbrake-flask',
+    license='LICENSE.txt',
+    description='Airbrake client for Python Flask',
+    long_description='Airbrake client for Python Flask',
+    )
