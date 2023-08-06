793aef5961aa10d09359d3a177c318d906866e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package thingstore/0.0.1/thingstore-0.0.1.tar.gz

---
+++ packages/thingstore/thingstore-0.0.1.tar.gz/thingstore-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='thingstore',
+    version='0.0.1',
+    packages=['thingstore'],
+    license='GNU Public License v3',
+    long_description=open('README.md').read(),
+)
