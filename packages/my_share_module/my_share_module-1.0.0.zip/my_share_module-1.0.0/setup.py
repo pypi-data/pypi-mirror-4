ac753cde0b0d7ee136ceb4ff21528e97857c3340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:00 +0000

    Add package my_share_module/1.0.0/my_share_module-1.0.0.zip

---
+++ packages/my_share_module/my_share_module-1.0.0.zip/my_share_module-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name='my_share_module',
+      version='1.0.0',
+      py_module=['my_share_module'],
+      author='duzhimin',
+      author_email='dzm1988@yeah.net',
+      description='a sample of PYPI',
+      )
