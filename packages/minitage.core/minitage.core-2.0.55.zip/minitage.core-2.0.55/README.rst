5b885f414e57cb54b92f18b8d9219ebf7a8e3621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:50 +0000

    Add package minitage.core/2.0.55/minitage.core-2.0.55.zip

---
+++ packages/minitage.core/minitage.core-2.0.55.zip/minitage.core-2.0.55/README.rst
@@ -0,0 +1,8 @@
+==============================
+Introduction
+==============================
+
+.. contents::
+
+Moved to `minitage <https://pypi.python.org/pypi/minitage>`_
+
---
+++ packages/minitage.core/minitage.core-2.0.55.zip/minitage.core-2.0.55/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+name = 'minitage.core'
+version = '2.0.55'
+def read(rnames):
+    setupdir =  os.path.dirname( os.path.abspath(__file__))
+    return open(
+        os.path.join(setupdir, rnames)
+    ).read()
+
+setup(
+    name = name,
+    version = version,
+    description="moved to minitage",
+    long_description = (
+        read('README.rst')
+        + '\n' +
+        read('CHANGES.rst')
+        + '\n'
+    ),
+    classifiers=[ ],
+    author = 'Mathieu Le Marec - Pasquet',
+    author_email = 'kiorky@cryptelium.net',
+    url = 'http://cheeseshop.python.org/pypi/%s' % name,
+    license = 'BSD',
+    namespace_packages = [],
+    install_requires = ['minitage'],
+    zip_safe = False,
+    include_package_data = True,
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+)
+
