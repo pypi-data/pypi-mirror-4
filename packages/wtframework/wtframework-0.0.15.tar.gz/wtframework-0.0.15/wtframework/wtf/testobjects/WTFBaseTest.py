c4fe901f0e2c7cec93c09b5cce47d866143182b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:24 +0000

    Add package wtframework/0.0.15/wtframework-0.0.15.tar.gz

---
+++ packages/wtframework/wtframework-0.0.15.tar.gz/wtframework-0.0.15/wtframework/wtf/testobjects/WTFBaseTest.py
@@ -0,0 +1,21 @@
+'''
+Created on Dec 24, 2012
+
+@author: "David Lai"
+'''
+from wtframework.wtf.testobjects.WatchedTestCase import WatchedTestCase
+from wtframework.wtf.web.CaptureScreenShotOnErrorTestWatcher import \
+    CaptureScreenShotOnErrorTestWatcher
+
+
+class WTFBaseTest(WatchedTestCase):
+    '''
+    Test Cases can extend this test to additional unit test functionality such as 
+    take screenshot on failure.
+    '''
+
+
+    def __init__(self, methodName='runTest', webdriver_provider=None, screenshot_util=None):
+        super(WTFBaseTest, self).__init__(self, methodName)
+        self.register_watcher(CaptureScreenShotOnErrorTestWatcher(webdriver_provider, screenshot_util))
+    
