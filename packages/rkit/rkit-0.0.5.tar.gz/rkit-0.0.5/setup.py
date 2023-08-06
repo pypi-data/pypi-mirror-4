7a8dcc2cb5ecb1825a951dc2a7bef94ea351d24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package rkit/0.0.5/rkit-0.0.5.tar.gz

---
+++ packages/rkit/rkit-0.0.5.tar.gz/rkit-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rkit',
+    version='0.0.5',
+    packages=['rkit', 'rkit.models'],
+    url='https://bitbucket.org/morgul/rkit',
+    license='MIT',
+    author='Christopher S. Case',
+    author_email='chris.case@g33xnexus.com',
+    description='Simple, Lightweight ORM for Riak'
+)
