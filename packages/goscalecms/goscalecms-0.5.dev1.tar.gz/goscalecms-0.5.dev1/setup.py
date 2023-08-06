9a61ece96930fcea2e439a74bda5637592ee0626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:24 +0000

    Add package goscalecms/0.5.dev1/goscalecms-0.5.dev1.tar.gz

---
+++ packages/goscalecms/goscalecms-0.5.dev1.tar.gz/goscalecms-0.5.dev1/setup.py
@@ -0,0 +1,42 @@
+import ez_setup
+import os
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name = "goscalecms",
+    version = "0.5.dev1",
+    packages = find_packages(),
+    author = "Evgeny Demchenko",
+    author_email = "little_pea@list.ru",
+    description = "GoScale CMS",
+    long_description = README,
+    license = "BSD",
+    keywords = "goscale cms django themes content management system mashup google ajax",
+    url = "https://github.com/sternoru/goscalecms",
+    include_package_data = True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Natural Language :: French",
+        "Natural Language :: Russian",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires = [
+        "pytz",
+        "unidecode",
+        "BeautifulSoup",
+        "feedparser",
+        "gdata",
+        "Django >= 1.1.1",
+        "django-cms >= 2.2",
+    ]
+)
