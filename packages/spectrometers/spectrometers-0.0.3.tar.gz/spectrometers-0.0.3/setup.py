f0be7784341101abb399bf71ce7ba1851453f700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:54 +0000

    Add package spectrometers/0.0.3/spectrometers-0.0.3.tar.gz

---
+++ packages/spectrometers/spectrometers-0.0.3.tar.gz/spectrometers-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="spectrometers",
+    version="0.0.3",
+    url="https://github.com/kanzure/python-spectrometers",
+    license="BSD",
+    author="Bryan Bishop",
+    author_email="kanzure@gmail.com",
+    description="API for spectrophotometers (pre-alpha)",
+    long_description=open("README.rst", "r").read(),
+    packages=["spectrometers"],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+    platforms="any",
+    classifiers=[
+        'Natural Language :: English',
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        #"Programming Language :: Python :: 2.6",
+        #"Programming Language :: Python :: 2.7",
+        #"Programming Language :: Python :: 3",
+        #"Programming Language :: Python :: 3.1",
+        #"Programming Language :: Python :: 3.2",
+        #"Programming Language :: Python :: 3.3",
+    ]
+)
---
+++ packages/spectrometers/spectrometers-0.0.3.tar.gz/spectrometers-0.0.3/spectrometers/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .spectrometer import Spectrometer
