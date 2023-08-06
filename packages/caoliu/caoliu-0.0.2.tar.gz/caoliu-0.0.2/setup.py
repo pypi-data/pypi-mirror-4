1dbbc729e6811d8d7173e27d1dc84f179c48108b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:50 +0000

    Add package caoliu/0.0.2/caoliu-0.0.2.tar.gz

---
+++ packages/caoliu/caoliu-0.0.2.tar.gz/caoliu-0.0.2/setup.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+from distutils.core import setup  
+setup(name='caoliu',  
+      author='TY',  
+      author_email='tianyu0915@gmail.com',  
+      version='0.0.2',  
+      description='用于发布草榴最新地址,不知道会不会被墙',  
+      keywords ='草榴,地址',
+      url='http://github.com/tianyu0915/caoliu',  
+      packages=['caoliu'],  
+      package_data={'caoliu':['*.*','caoliu/*.*']},
+
+)  
