873a4f231dab0d4ba813ecef8eea60f0b6f07261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:07 +0000

    Add package nanolp/1.0h/nanolp-1.0h.zip

---
+++ packages/nanolp/nanolp-1.0h.zip/nanolp-1.0h/bin/nlp.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+# Main module (to run from command line)
+# Author: Balkansoft.BlogSpot.com
+# GNU GPL licensed
+
+import getopt
+import sys
+import os
+from nanolp import lp
+
+################################################################################
+
+class AppError(Exception): pass
+
+class App:
+    input_file = None # input file name
+    cfgfile = '' # path to config. file
+    tb = False # show traceback on exceptions
+    refs = False # flush references file
+
+    def print_usage(self):
+        def parser_info(cls):
+            ext = '%s' % ', '.join(cls.ext)
+            return '   %s - %s: %s'%(cls.__name__, cls.descr or 'Unknown', ext)
+
+        formats = [parser_info(p) for p in lp.Parser.parsers]
+        formats = '\n'.join(formats)
+
+        if self.cfgfile:
+            cfginfo = "Setup from: '%s'" % self.cfgfile
+        else:
+            cfginfo = ''
+
+        USAGE = '''\
+Syntax: -i FILE [-x] [-r] [-h]
+   -i FILE      Input file
+   -x           Detailed stack-trace on errors
+   -r           Flush references file
+   -h           This help
+Supported formats:
+%s
+%s
+'''%(formats, cfginfo)
+        sys.stdout.write(USAGE)
+
+    def parse_cmdline(self):
+        """Returns True, if app may continue execution, False otherwise
+        (help printing is only need)
+        """
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], 'rxhi:', [])
+        except getopt.GetoptError as x:
+            sys.stderr.write('Syntax error! See help (-h)\n')
+            sys.exit(1)
+
+        self.input_file = None
+        for o, v in opts:
+            if o == '-h':
+                return False
+            elif o == '-i':
+                self.input_file = v
+            elif o == '-x':
+                self.tb = True
+            elif o == '-r':
+                self.refs = True
+        if not self.input_file:
+            sys.stderr.write('Input file is mandatory. See help (-h)\n')
+            sys.exit(1)
+        return True
+
+    # XXX first call parse_cmdline() to determine input dir (as possible place
+    # of cfg. file)
+    def findcfgfile(self):
+        """Return path to cfg file or raise exception, if not found. Priority
+        of search:
+            - folder of input file
+            - current working directory
+            - script directory
+        """
+        dirs = [os.getcwd(), os.path.dirname(os.path.realpath(__file__))]
+        if self.input_file:
+            # input dir has higher priority for search of cfgfile
+            absp = os.path.abspath(self.input_file)
+            dirs.insert(0, os.path.dirname(absp))
+        for indir in dirs:
+            cfgfile = os.path.join(indir, 'lprc')
+            if os.path.exists(cfgfile):
+                return cfgfile
+        raise AppError('Can not found configuration file')
+
+    def main(self):
+        sys.stderr.write(lp.__ABOUT__+'\n')
+        onlyhelp = not self.parse_cmdline()
+
+        def _do():
+            """real action"""
+            self.cfgfile = self.findcfgfile()
+            if onlyhelp:
+                self.print_usage()
+                sys.exit(0)
+            engine = lp.Lp(cfgfile=self.cfgfile)
+            parserclass = lp.Parser.fileparser(self.input_file, engine=engine)
+            parser = parserclass.parsefile(self.input_file)
+            if self.refs:
+                # if need to flush references, do it
+                fn = os.path.split(self.input_file)[1]
+                fn = fn.upper()
+                refsfile = lp.RefsFile(parser, "%s: references"%fn)
+                refsfile.save()
+
+        if self.tb:
+            _do()
+        else:
+            try:
+                _do()
+            except Exception as x:
+                sys.stderr.write("ERROR '%s': %s\n"%(x.__class__.__name__,str(x)))
+
+################################################################################
+
+if __name__ == "__main__":
+    app = App()
+    app.main()
---
+++ packages/nanolp/nanolp-1.0h.zip/nanolp-1.0h/test/test.py
@@ -0,0 +1,90 @@
+from nanolp import lp
+import os
+import sys
+import glob
+import difflib
+import string
+#import unittest
+import doctest
+
+def intsuffix(s):
+    i = len(s)-1
+    while s[i] in string.digits:
+        i -= 1
+    return int(s[i+1:])
+
+def cmpfiles(example, out, errors, master='', text=''):
+    if text:
+        master_buf = text
+    else:
+        with open(master, "rt") as masterfile:
+            master_buf = masterfile.read().rstrip('\n')
+
+    with open(out, "rt") as outfile:
+        out_buf = outfile.read().rstrip('\n')
+        out_buf = out_buf.replace('$example', example)
+
+    res = (out_buf == master_buf)
+    sys.stderr.write('\nExample %s %s\n'%(out, 'OK.' if res else 'FAILED:'))
+    if not res:
+        errors.append((example, out))
+        master_lines = ['%s\n'%l for l in master_buf.splitlines()]
+        out_lines = ['%s\n'%l for l in out_buf.splitlines()]
+        for diffln in difflib.context_diff(master_lines, out_lines):
+            if not diffln.endswith('\n'):
+                diffln += '\n'
+            sys.stderr.write(diffln)
+
+def argv1():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return ''
+
+if argv1() != '-d':
+    sys.stderr.write('==================== DO DOCTESTS ====================\n')
+    testres = doctest.testmod(lp, verbose='-q' not in sys.argv, optionflags=doctest.IGNORE_EXCEPTION_DETAIL)
+
+if argv1() != '-f':
+    sys.stderr.write('==================== DO FILE TESTS ====================\n')
+
+    failed = []
+    TESTDIR = os.path.dirname(os.path.realpath(__file__))
+    testdirs = glob.glob(os.path.join(TESTDIR, 'tests', 'test*'))
+    testdirs.sort(key=intsuffix)
+    for path in testdirs:
+
+        outs = glob.glob(os.path.join(path, '*.out'))
+        for out in outs:
+            os.remove(out)
+
+        os.chdir(path)
+        examples = glob.glob(os.path.join(path, 'example*'))
+        for example in examples:
+            if example.endswith('.out'): continue
+            if example.endswith('.err'): continue
+            sys.stderr.write('\nProcess %s...\n'%example)
+            try:
+                engine = lp.Lp()
+                parser = lp.Parser.fileparser(example, engine=engine)
+                parser.parsefile(example)
+            except Exception as x:
+                msg = str(x)
+                err = os.path.splitext(example)[0] + '.err'
+                if os.path.exists(err):
+                    cmpfiles(example=example, text=msg, out=err, errors=failed)
+                    continue
+                else:
+                    raise
+
+        masters = glob.glob(os.path.join(path, '*.master'))
+        for master in masters:
+            out = master.replace('.master', '.out')
+            cmpfiles(example=example, master=master, out=out, errors=failed)
+    if failed:
+        sys.stderr.write('\n====================\n')
+        sys.stderr.write('%d FAILED:\n'%len(failed))
+        for example,out in failed:
+            out = out.split(os.sep)[-1]
+            example = os.sep.join(example.split(os.sep)[-2:])
+            sys.stderr.write("  '%s' in '%s'\n" % (out, example))
