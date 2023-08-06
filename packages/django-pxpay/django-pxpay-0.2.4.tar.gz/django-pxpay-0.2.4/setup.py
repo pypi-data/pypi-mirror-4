9721d3535a33f4f287cb7cdb520e1ec28d3fbfba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package django-pxpay/0.2.4/django-pxpay-0.2.4.tar.gz

---
+++ packages/django-pxpay/django-pxpay-0.2.4.tar.gz/django-pxpay-0.2.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-pxpay',
+    version='0.2.4',
+    url='http://github.com/pavel-shpilev/django-pxpay',
+    author="Pavel Shpilev",
+    author_email="p.shpilev@gmail.com",
+    description="PaymentExpress PxPay Gateway for Django",
+    long_description=read('README.rst'),
+    keywords="PxPay, Payment, PaymentExpress, DPS, Django, Python",
+    license='BSD',
+    packages=find_packages(exclude=['test']),
+    install_requires=['requests>=0.13.5', 'django>=1.4.3'],
+    include_package_data=True,
+    classifiers=['Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python'])
