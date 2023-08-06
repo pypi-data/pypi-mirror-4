5c2a4fc6c0d5f9573508c35aecce26c5965bf565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:50 +0000

    Add package sydep/0.1.1/sydep-0.1.1.tar.gz

---
+++ packages/sydep/sydep-0.1.1.tar.gz/sydep-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='sydep',
+    version='0.1.1',
+    author='Juda Kaleta',
+    author_email='juda.kaleta@gmail.com',
+    url='https://github.com/yetty/sydep',
+    license=open('LICENSE.txt').read(),
+    description='tool for easy deployment over rsync',
+    long_description=open('README.txt').read(),
+    packages=['sydep', 'sydep.test'],
+    scripts=['bin/sydep'],
+    requires=['docopt (>= 0.6.1)', ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
