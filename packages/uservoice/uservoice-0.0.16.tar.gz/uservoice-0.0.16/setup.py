215fe9d2fe70b266182f9b432121d3a500bf9399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package uservoice/0.0.16/uservoice-0.0.16.tar.gz

---
+++ packages/uservoice/uservoice-0.0.16.tar.gz/uservoice-0.0.16/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='uservoice',
+      version='0.0.16',
+      description='UserVoice Python library',
+      url = 'http://pypi.python.org/pypi/uservoice/',
+      long_description=re.sub(r'```[^\s]*', '', open('README.md').read()),
+      author='Raimo Tuisku',
+      author_email='dev@uservoice.com',
+      packages=['uservoice'],
+      install_requires=[
+          'simplejson',
+          'pycrypto',
+          'pytz',
+          'PyYAML',
+          'requests==0.14.2',
+          'requests-oauth'],
+      test_suite='test',
+)
