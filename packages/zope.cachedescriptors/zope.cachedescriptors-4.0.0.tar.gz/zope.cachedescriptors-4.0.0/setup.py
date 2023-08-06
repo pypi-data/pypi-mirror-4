5883c2a48c04d7326cfb7853867794826b986a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:44 +0000

    Add package zope.cachedescriptors/4.0.0/zope.cachedescriptors-4.0.0.tar.gz

---
+++ packages/zope.cachedescriptors/zope.cachedescriptors-4.0.0.tar.gz/zope.cachedescriptors-4.0.0/setup.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.cachedescriptors package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.cachedescriptors',
+    version='4.0.0',
+    url='http://pypi.python.org/pypi/zope.cachedescriptors',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    license='ZPL 2.1',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        ],
+    description='Method and property caching decorators',
+    long_description=\
+        '.. contents::\n\n' +
+        read('src', 'zope', 'cachedescriptors', 'README.txt')
+        + '\n' +
+        read('src', 'zope', 'cachedescriptors', 'property.txt')
+        + '\n' +
+        read('src', 'zope', 'cachedescriptors', 'method.txt')
+        + '\n' +
+        read('CHANGES.txt'),
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    test_suite='zope.cachedescriptors.tests.test_suite',
+    namespace_packages=['zope',],
+    include_package_data=True,
+    install_requires=['setuptools'],
+    zip_safe=False,
+    )
