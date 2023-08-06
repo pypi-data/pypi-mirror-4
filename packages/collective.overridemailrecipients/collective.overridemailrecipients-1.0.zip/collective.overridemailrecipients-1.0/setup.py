491b6f639a05a6756b110d5feb94cb3fa6437fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:32 +0000

    Add package collective.overridemailrecipients/1.0/collective.overridemailrecipients-1.0.zip

---
+++ packages/collective.overridemailrecipients/collective.overridemailrecipients-1.0.zip/collective.overridemailrecipients-1.0/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.overridemailrecipients',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n",
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+        ],
+      keywords='',
+      author='Goldmund, Wyldebeast & Wunderliebe',
+      author_email='info@gw20e.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'collective.monkeypatcher'
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'mock',
+           ],
+      },      
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
