81102878719d91dd3dff98447c6ac0d1911e9dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:44 +0000

    Add package django-rss-plugin/0.0.2/django-rss-plugin-0.0.2.zip

---
+++ packages/django-rss-plugin/django-rss-plugin-0.0.2.zip/django-rss-plugin-0.0.2/setup.py
@@ -0,0 +1,27 @@
+__author__ = 'Zhou Guangwen'
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="django-rss-plugin",
+    version="0.0.2",
+    author="Guangwen Zhou",
+    author_email="zgwmike@hotmail.com",
+    description=("A Django CMS plugin to show django-rss-plugin."),
+    license="BSD",
+    keywords="django cms plugin django-rss-plugin",
+    url="http://github.com/zgwmike/django-rss-plugin",
+    packages=["rssplugin"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: BSD License"
+    ],
+    install_requires=['feedparser', 'django-cms'],
+    download_url='https://github.com/zgwmike/django-rss-plugin/archive/master.zip',
+    long_description=read("README.rst")
+)
