8c867a2d931fbffc2e56fa763f021d3b3599542d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package Solution/2.0.1/Solution-2.0.1.tar.gz

---
+++ packages/Solution/Solution-2.0.1.tar.gz/Solution-2.0.1/solution/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    Solution
+
+    An amazing form solution.
+
+"""
+from .form import Form, FormSet
+from .fields import *
+from .validators import *
+
+__version__ = '2.0.1'
+
