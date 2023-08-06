d81ad2b8dec811fd2ce0a62addc6211208e157bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:07 +0000

    Add package 3-1/1.0.0/3-1-1.0.0.zip

---
+++ packages/3-1/3-1-1.0.0.zip/3-1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = '3-1',
+	version = '1.0.0',
+	py_modules=['3-1'],
+	author='DuYong',
+	author_email='duy1102002@gmail.com',
+	url='www.baidu.com',
+	description='test',
+)
