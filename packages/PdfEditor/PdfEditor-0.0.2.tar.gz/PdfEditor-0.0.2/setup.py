b41a10746dcbdf314001e9b86e0de46fd31ace67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:05 +0000

    Add package PdfEditor/0.0.2/PdfEditor-0.0.2.tar.gz

---
+++ packages/PdfEditor/PdfEditor-0.0.2.tar.gz/PdfEditor-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PdfEditor',
+      version='0.0.2',
+      description='Modify previously created PDF\'s',
+      author='Yamil Asusta (elbuo8)',
+      author_email='yamil.asusta@upr.edu',
+      license='MIT',
+      url='https://github.com/elbuo8/PdfEditor',
+      )
