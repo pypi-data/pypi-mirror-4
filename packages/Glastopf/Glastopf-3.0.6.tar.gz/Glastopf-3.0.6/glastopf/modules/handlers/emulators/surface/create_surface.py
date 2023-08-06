f78ed5ebfea02dc0b19fe050cf530f6e8a68f48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:13 +0000

    Add package Glastopf/3.0.6/Glastopf-3.0.6.tar.gz

---
+++ packages/Glastopf/Glastopf-3.0.6.tar.gz/Glastopf-3.0.6/glastopf/modules/handlers/emulators/surface/create_surface.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2012  Lukas Rist <glaslos@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+
+package_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+class SurfaceCreator(object):
+
+    def __init__(self):
+        self.template_env = Environment(loader=FileSystemLoader(os.path.join(package_directory, "templates")))
+
+    def get_index(self, title="Title Title", target="/index", body="Some Body", footer="Footer Text"):
+        template = self.template_env.get_template('index.html')
+        surface_page = template.render(title=title, target=target, body=body, footer=footer)
+        return surface_page
+
+
+if __name__ == "__main__":
+    sc = SurfaceCreator()
+    sc.get_index()
---
+++ packages/Glastopf/Glastopf-3.0.6.tar.gz/Glastopf-3.0.6/setup.py
@@ -0,0 +1,21 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Glastopf',
+    version='3.0.6',
+    packages=find_packages(exclude=['bin', 'testing', 'docs']),
+    scripts=['bin/glastopf-runner'],
+    url='http://glastopf.org',
+    license='GPL 3',
+    author='Glastopf Project',
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    package_data={'glastopf': ['sandbox/Makefile']},
+    author_email='',
+    description='Web application honeypot',
+    test_suite='nose.collector',
+    install_requires=open('requirements.txt').read().splitlines(),
+)
