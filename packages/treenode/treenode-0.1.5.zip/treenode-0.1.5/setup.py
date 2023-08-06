1a658393fdacdd2798eaff3c5f01d0de98c0d50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package treenode/0.1.5/treenode-0.1.5.zip

---
+++ packages/treenode/treenode-0.1.5.zip/treenode-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'treenode',
+    version = '0.1.5',
+    url = 'http://github.com/kobejohn/simpletree',
+    py_modules = ['treenode'],
+    author_email = 'niericentral@gmail.com',
+    description = 'Class with unified tree and node behavior. Each node has custom data as attributes.',
+    license = 'MIT',
+    author = 'KobeJohn',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    ]
+)
