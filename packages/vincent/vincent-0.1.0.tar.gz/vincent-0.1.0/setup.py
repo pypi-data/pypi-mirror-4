ee608899790c553b27f66c637126eea61cde5282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:58 +0000

    Add package vincent/0.1.0/vincent-0.1.0.tar.gz

---
+++ packages/vincent/vincent-0.1.0.tar.gz/vincent-0.1.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+from os.path import abspath, dirname, join
+
+path = abspath(dirname(__file__))
+
+classifiers = (
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2.7',
+    'License :: OSI Approved :: MIT License',
+)
+
+required = (
+    'pandas'
+)
+
+kw = {
+    'name': 'vincent',
+    'version': '0.1.0',
+    'description': 'A Python to Vega translator',
+    'long_description': open(join(path, 'README.md')).read(),
+    'author': 'Rob Story',
+    'author_email': 'wrobstory@gmail.com',
+    'license': 'MIT License',
+    'url': 'https://github.com/wrobstory/vincent',
+    'keywords': 'data visualization',
+    'classifiers': classifiers,
+    'packages': ['vincent'],
+    'package_data': {'vincent': ['*.html']},
+    'install_requires': required,
+    'zip_safe': True,
+}
+
+setup(**kw)
