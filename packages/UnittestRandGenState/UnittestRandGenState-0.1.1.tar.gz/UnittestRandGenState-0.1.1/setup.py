0369eb91b64f3400c068e8ebed090cf8fe76bdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:28 +0000

    Add package UnittestRandGenState/0.1.1/UnittestRandGenState-0.1.1.tar.gz

---
+++ packages/UnittestRandGenState/UnittestRandGenState-0.1.1.tar.gz/UnittestRandGenState-0.1.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+
+from __future__ import print_function
+from distutils.core import setup, Extension, Command
+import os
+import os.path
+import glob
+import sys
+import shutil
+
+
+class _TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            os.chdir('tests')
+            run_str = '%s __init__.py' % ('python3' if sys.version_info.major >= 3 else 'python')
+            os.system(run_str)
+        finally:       
+            os.chdir('..')
+
+
+setup(
+    name = 'UnittestRandGenState',
+    version = '0.1.1',
+    author = 'Ami Tavory',
+    author_email = 'atavory at gmail.com',
+    packages = ['unittest_rand_gen_state'],
+    url = 'http://pypi.python.org/pypi/UnittestRandState',
+    license = 'BSD',
+    description = 'Smart random-generation state persistence for unittest',
+    long_description = open('README.txt').read(),
+    cmdclass = {
+        'test': _TestCommand},
+    classifiers = [   
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',  
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing '])
