428ebb53542f44452305fb1cd5575e6f3ce324f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:29 +0000

    Add package django-daddy-avatar/1.0.2/django-daddy-avatar-1.0.2.tar.gz

---
+++ packages/django-daddy-avatar/django-daddy-avatar-1.0.2.tar.gz/django-daddy-avatar-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+
+from distutils.core import setup
+
+
+version = '1.0.2'
+
+setup(
+    name = "django-daddy-avatar",
+    version = version,
+    author = "vahid chakoshy",
+    author_email = "vchakoshy@gmail.com",
+    description = "very simple avatar from gravatar",
+    url = "https://github.com/karoon/django-daddy-avatar",
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe=False,
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Natural Language :: Persian',
+        
+    ]
+)
