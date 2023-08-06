2ba36ec5286ceed210c73d342fad04e047e880de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:22 +0000

    Add package django-fluent-dashboard/0.3.3/django-fluent-dashboard-0.3.3.tar.gz

---
+++ packages/django-fluent-dashboard/django-fluent-dashboard-0.3.3.tar.gz/django-fluent-dashboard-0.3.3/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os.path import dirname, join
+import sys, os
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv:
+    try:
+        os.chdir('fluent_dashboard')
+        from django.core.management.commands.compilemessages import compile_messages
+        compile_messages(sys.stderr)
+    finally:
+        os.chdir('..')
+
+
+setup(
+    name='django-fluent-dashboard',
+    version='0.3.3',
+    license='Apache License, Version 2.0',
+
+    install_requires=[
+        'django-admin-tools>=0.5.1',  # 0.5.1 has proper Django 1.4 support.
+    ],
+    extras_require = {
+        'cachestatus': ['dashboardmods>=0.2.2'],
+    },
+
+    description='An improved django-admin-tools dashboard for Django projects',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-fluent-dashboard',
+    download_url='https://github.com/edoburu/django-fluent-dashboard/zipball/master',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
