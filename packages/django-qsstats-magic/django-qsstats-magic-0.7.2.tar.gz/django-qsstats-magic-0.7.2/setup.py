fc3177a8444dd53ab98003df1a9584c5c42d647c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:40 +0000

    Add package django-qsstats-magic/0.7.2/django-qsstats-magic-0.7.2.tar.gz

---
+++ packages/django-qsstats-magic/django-qsstats-magic-0.7.2.tar.gz/django-qsstats-magic-0.7.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+for cmd in ('egg_info', 'develop'):
+    import sys
+    if cmd in sys.argv:
+        from setuptools import setup
+
+setup(
+    name='django-qsstats-magic',
+    version='0.7.2',
+    description='A django microframework that eases the generation of aggregate data for querysets.',
+    long_description = open('README.rst').read(),
+    author='Matt Croydon, Mikhail Korobov',
+    author_email='mcroydon@gmail.com, kmike84@gmail.com',
+    url='http://bitbucket.org/kmike/django-qsstats-magic/',
+    packages=['qsstats'],
+    requires=['dateutil(>=1.4.1, < 2.0)'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
