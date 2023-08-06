fef75b62d1fa62d081e7222efdbbfd4f39072daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:10 +0000

    Add package pybeam/0.2/pybeam-0.2.tar.gz

---
+++ packages/pybeam/pybeam-0.2.tar.gz/pybeam-0.2/pybeam/beam_construct.py
@@ -0,0 +1,103 @@
+#
+# Copyright (c) 2013 Matwey V. Kornilov <matwey.kornilov@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from construct import *
+from eetf_construct import term
+
+chunk_atom = Struct("chunk_atom",
+	UBInt32("len"),
+	Array(lambda ctx: ctx.len, PascalString("atom"))
+	)
+
+chunk_expt = Struct("chunk_expt",
+	UBInt32("len"),
+	Array(lambda ctx: ctx.len, Struct("entry",
+			UBInt32("function"),
+			UBInt32("arity"),
+			UBInt32("label"),
+		)
+	)
+	)
+
+chunk_impt = Struct("chunk_impt",
+	UBInt32("len"),
+	Array(lambda ctx: ctx.len, Struct("entry",
+			UBInt32("module"),
+			UBInt32("function"),
+			UBInt32("arity"),
+		)
+	)
+	)
+
+chunk_loct = Struct("chunk_loct",
+	UBInt32("len"),
+	Array(lambda ctx: ctx.len, Struct("entry",
+			UBInt32("function"),
+			UBInt32("arity"),
+			UBInt32("label"),
+		)
+	)
+	)
+
+chunk_attr = Struct("chunk_attr",
+	Const(UBInt8("131"),131),
+	term
+	)
+
+chunk_cinf = Struct("chunk_cinf",
+	Const(UBInt8("131"),131),
+	term
+	)
+
+chunk = Struct("chunk",
+	String("chunk_name",4),
+	UBInt32("size"),
+	SeqOfOne("payload",
+		Switch("payload", lambda ctx: ctx.chunk_name,
+			{
+			"Atom" : chunk_atom,
+			"ExpT" : chunk_expt,
+			"ImpT" : chunk_impt,
+#			"Code" : chunk_code,
+#			"StrT" : chunk_strt,
+			"Attr" : chunk_attr,
+			"CInf" : chunk_cinf,
+			"LocT" : chunk_loct,
+#			"Trac" : chunk_trac,
+			},
+			default = Bytes("skip", lambda ctx: ctx.size)
+		),
+		Padding(lambda ctx: (4 - ctx.size % 4) % 4, pattern = "\00"),
+		nested = False,
+	)
+	)
+
+beam = Struct("beam",
+	Const(String('for1',4),'FOR1'),
+	UBInt32("size"),
+	Const(String('beam',4),'BEAM'),
+	GreedyRange(chunk),
+	Terminator,
+	)
+
+__all__ = ["beam"]
+
---
+++ packages/pybeam/pybeam-0.2.tar.gz/pybeam-0.2/pybeam/beam_file.py
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2013 Matwey V. Kornilov <matwey.kornilov@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from beam_construct import beam
+
+class BeamFile(object):
+	def __init__(self, filename):
+		self._tree = beam.parse(file(filename,"r").read())
+
+	def selectChunkByName(self, name):
+		for c in self._tree.chunk:
+			if c.chunk_name == name:
+				return c
+		raise KeyError(name)
+
+	@property
+	def atoms(self):
+		return self.selectChunkByName("Atom").payload.atom
+
+	@property
+	def modulename(self):
+		return self.atoms[0]
+
+	@property
+	def exports(self):
+		expt = self.selectChunkByName("ExpT")
+		atoms = self.atoms
+		return [(atoms[e.function-1], e.arity, e.label) for e in expt.payload.entry]
+
+	@property
+	def imports(self):
+		impt = self.selectChunkByName("ImpT")
+		atoms = self.atoms
+		return [(atoms[e.module-1], atoms[e.function-1], e.arity) for e in impt.payload.entry]
+
+	@property
+	def attributes(self):
+		attr = self.selectChunkByName("Attr")
+		# convert from proplist to dict
+		return dict(attr.payload.term)
+	
+	@property
+	def compileinfo(self):
+		cinf = self.selectChunkByName("CInf")
+		return dict(cinf.payload.term)
