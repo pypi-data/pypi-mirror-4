5e7bb315a1419ee553eb38817b72164bf7cbe9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:30 +0000

    Add package python_tuto/1.0.1/python_tuto-1.0.1.tar.gz

---
+++ packages/python_tuto/python_tuto-1.0.1.tar.gz/python_tuto-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'python_tuto',
+    version = '1.0.1',
+    py_modules = ['python_tuto'],
+    author = 'ls25370',
+    author_email = 'ls25370@gmail.com',
+    url = 'http://www.muzrang.com',
+    description = 'A simple printer of nested list',
+    )
