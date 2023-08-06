fb7390203108c1c62bdd42d23ce71ce76b08185d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package lino/1.6.1/lino-1.6.1.tar.gz

---
+++ packages/lino/lino-1.6.1.tar.gz/lino-1.6.1/fabfile.py
@@ -0,0 +1,38 @@
+from djangosite.utils.fablib import *
+setup_from_project()  
+
+env.django_doctests.append('tutorials.auto_create.settings')
+env.django_doctests.append('tutorials.human.settings')
+
+#~ env.simple_doctests.append('lino/utils/html2odf.py')
+#~ env.simple_doctests.append('lino/utils/memo.py')
+#~ env.simple_doctests.append('lino/modlib/contacts/utils.py')
+#~ env.simple_doctests.append('lino/utils/html2xhtml.py')
+#~ env.simple_doctests.append('lino/utils/demonames.py')
+#~ env.simple_doctests.append('lino/utils/odsreader.py')
+#~ env.simple_doctests.append('lino/utils/ssin.py')
+#~ env.simple_doctests.append('lino/core/choicelists.py')
+#~ env.simple_doctests.append('lino/utils/jsgen.py')
+#~ env.simple_doctests.append('lino/utils/ranges.py')
+#~ env.simple_doctests.append('lino/modlib/ledger/utils.py')
+env.simple_doctests.append('lino/modlib/accounts/utils.py')
+  
+env.django_admin_tests += [
+      "lino.projects.cosi.settings",
+      "lino.projects.events.settings",
+      "lino.test_apps.nomti.settings",
+      "lino.test_apps.20100212.settings",
+      "lino.test_apps.quantityfield.settings",
+      #~ "lino.test_apps.human.settings",
+      "lino.projects.cosi.settings",
+      #~ "lino.projects.presto.settings", 
+      "lino.projects.babel_tutorial.settings",
+      "lino.projects.polls_tutorial.settings",
+      "lino.projects.belref.settings",
+      "lino.projects.events.settings",
+      "lino.projects.homeworkschool.settings",
+      "lino.projects.min1.settings",
+      "lino.projects.min2.settings",
+]
+
+
