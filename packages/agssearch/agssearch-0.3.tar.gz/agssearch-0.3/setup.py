158c906895338258ac3b09ffc2f5c6af46362203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package agssearch/0.3/agssearch-0.3.tar.gz

---
+++ packages/agssearch/agssearch-0.3.tar.gz/agssearch-0.3/setup.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='agssearch',
+      version='0.3',
+      description='Python client for the German Destatis Gemeindeverzeichnis',
+      author='Marian Steinbach',
+      author_email='marian@sendung.de',
+      url='https://github.com/marians/agssearch',
+      packages=['agssearch'],
+      requires=[
+          'lxml',
+          'mechanize'
+      ])
