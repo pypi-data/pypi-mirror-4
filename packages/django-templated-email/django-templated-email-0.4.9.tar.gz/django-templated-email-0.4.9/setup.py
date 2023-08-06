d4eacb878bdedbb7743cd3173282bc0797b08e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package django-templated-email/0.4.9/django-templated-email-0.4.9.tar.gz

---
+++ packages/django-templated-email/django-templated-email-0.4.9.tar.gz/django-templated-email-0.4.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+DESCRIPTION = "A Django oriented templated / transaction email abstraction"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+setup(
+    name='django-templated-email',
+    version='0.4.9',
+    packages=['templated_email', 'templated_email.backends'],
+    author='Bradley Whittington',
+    author_email='radbrad182@gmail.com',
+    url='http://github.com/bradwhittington/django-templated-email/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+)
