3824c1a175e4185e9ca05b9874bb29b276901f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:35 +0000

    Add package nesterimyoyo/1.0.0/nesterimyoyo-1.0.0.zip

---
+++ packages/nesterimyoyo/nesterimyoyo-1.0.0.zip/nesterimyoyo-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nesterimyoyo',
+    version='1.0.0',
+    py_modules=['nesterimyoyo'],
+    author='imyoyo',
+    author_email='imyoyo@naver.com',
+    url='imyoyo.net',
+    description='GG!',
+)
+    
