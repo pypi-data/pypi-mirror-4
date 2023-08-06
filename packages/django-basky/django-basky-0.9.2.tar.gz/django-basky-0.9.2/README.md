0d2ad208412843e4d5ddee5d356b1e45e4ed0eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:29 +0000

    Add package django-basky/0.9.2/django-basky-0.9.2.tar.gz

---
+++ packages/django-basky/django-basky-0.9.2.tar.gz/django-basky-0.9.2/README.md
@@ -0,0 +1,9 @@
+# basky
+
+A lightweight, robust basket app for your Django project.
+
+Based on [django-ccbasket][0] but has been updated to work with Django 1.5.
+
+**Tests and documentation are being actively developed**
+
+[0]: https://github.com/designcc/django-ccbasket
---
+++ packages/django-basky/django-basky-0.9.2.tar.gz/django-basky-0.9.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+from basky import get_version
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+setup(
+    name='django-basky',
+    version=get_version(),
+    license='BSD 3 Clause',
+    description='A extensible basket for your django project',
+    long_description=open('README.md').read(),
+    author='Jamie Curle',
+    author_email='me@jamiecurle.co.uk',
+    url='http://django-basky.readthedocs.org',
+    package_data={
+        'basky' : [
+            'templates/*.html',
+            'templates/basky/*.html',
+        ],
+    },
+    packages=[
+        'basky',
+        'basky.templatetags',
+    ],
+    install_requires=[
+        'django >= 1.5'
+    ],
+)
