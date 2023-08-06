f730a5798b36b60b9bd3d40340760aa3b81f1469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package sc.contentrules.metadata/1.0b2/sc.contentrules.metadata-1.0b2.zip

---
+++ packages/sc.contentrules.metadata/sc.contentrules.metadata-1.0b2.zip/sc.contentrules.metadata-1.0b2/setup.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+version = '1.0b2'
+long_description = (open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read())
+
+
+setup(name='sc.contentrules.metadata',
+      version=version,
+      description="Content Rules Action: Set layout for a content item",
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='plone contentrules action metadata',
+      author='Simples Consultoria',
+      author_email='products@simplesconsultoria.com.br',
+      url='http://www.simplesconsultoria.com.br/',
+      license='GPLv2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['sc', 'sc.contentrules'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools'],
+      extras_require={
+          'develop': [
+              'Sphinx',
+              'manuel',
+              'pep8',
+              'setuptools-flakes',
+          ],
+          'test': [
+              'interlude',
+              'plone.app.testing'
+          ]
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
