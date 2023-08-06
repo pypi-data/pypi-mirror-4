99eb7f4ecd76ff0227eec8bc72cdd0a17bdc2a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:25 +0000

    Add package ll-xist/4.10/ll-xist-4.10.zip

---
+++ packages/ll-xist/ll-xist-4.10.zip/ll-xist-4.10/demos/python-quotes/rdfs_xmlns.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+from ll.xist import xsc
+
+
+xmlns = "http://www.w3.org/2000/01/rdf-schema#"
---
+++ packages/ll-xist/ll-xist-4.10.zip/ll-xist-4.10/scripts/xml2xsc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.xist.scripts import xml2xsc
+	sys.exit(xml2xsc.main())
