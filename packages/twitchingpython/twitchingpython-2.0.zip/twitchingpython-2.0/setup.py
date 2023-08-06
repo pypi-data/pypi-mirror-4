d73b8600bb5ecd9ae8276d38713833d4e28c39ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:16 +0000

    Add package twitchingpython/2.0/twitchingpython-2.0.zip

---
+++ packages/twitchingpython/twitchingpython-2.0.zip/twitchingpython-2.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+      name='twitchingpython',
+      version='2.0',
+      description = "A Twitch.tv API wrapper for Python",
+      author = "Martin Charles",
+      author_email = "martincharles07@gmail.com",
+      url = 'https://github.com/caffinatedmonkey/twitchingpython',
+      packages = ['twitchingpython'],
+      classifiers = [
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+       ],
+)
