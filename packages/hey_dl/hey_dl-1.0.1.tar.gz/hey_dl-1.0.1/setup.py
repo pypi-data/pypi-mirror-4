a48ae0b05ff9fc8da9906ef2743bf07272c34597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package hey_dl/1.0.1/hey_dl-1.0.1.tar.gz

---
+++ packages/hey_dl/hey_dl-1.0.1.tar.gz/hey_dl-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='hey_dl',
+    version='1.0.1',
+    author='Paul Heymann',
+    author_email='hey_dl@heymann.be',
+    url='http://www.pybrid.com/',
+    packages=['hey_dl', ],
+    license='LICENSE.txt',
+    description='Hey! Directory Localizer',
+    long_description=open('README.rst').read()
+)
