56512d61ef23bd93e3b40b1b8c151e116fbf67b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:12 +0000

    Add package thebops/0.1.2/thebops-0.1.2.win-amd64.zip

---
+++ packages/thebops/thebops-0.1.2.win-amd64.zip/Interpreter/Python/Python27/Scripts/rexxbi_demo-script.py
@@ -0,0 +1,9 @@
+#!C:\Interpreter\Python\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'thebops==0.1.2','console_scripts','rexxbi_demo'
+__requires__ = 'thebops==0.1.2'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('thebops==0.1.2', 'console_scripts', 'rexxbi_demo')()
+)
