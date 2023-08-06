297f0c5fd56e0702bfa877cbbf668118e1c9a02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:15 +0000

    Add package SqlHBase/0.2/SqlHBase-0.2.tar.gz

---
+++ packages/SqlHBase/SqlHBase-0.2.tar.gz/SqlHBase-0.2/setup.py
@@ -0,0 +1,21 @@
+__author__ = 'zeph'
+
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='SqlHBase',
+    version='0.2',
+    author='Guido Serra aka Zeph',
+    author_email='zeph@fsfe.org',
+    url='http://www.rocket-internet.de',
+    packages=['sqlhbase',],
+    scripts=['bin/sqlhbase-mysqlimport','bin/sqlhbase-populate'],
+    license='GPL 3',
+    description='MySQLDump to HBase, ETL scripts',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "happybase >= 0.4",
+        "hive-thrift-py == 0.0.1",
+    ]
+)
