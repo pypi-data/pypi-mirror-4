78e06daa8a72d6ce7871b0a8ee706710015cda0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:28 +0000

    Add package q/2.2/q-2.2.tar.gz

---
+++ packages/q/q-2.2.tar.gz/q-2.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='q', version='2.2', py_modules=['q'],
+      description='Quick-and-dirty debugging output',
+      author='Ka-Ping Yee', author_email='ping@zesty.ca',
+      url='http://github.com/zestyping/q', classifiers=[
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License'
+      ])
