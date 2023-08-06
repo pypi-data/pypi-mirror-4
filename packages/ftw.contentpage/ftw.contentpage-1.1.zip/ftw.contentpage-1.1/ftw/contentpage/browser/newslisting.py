d583ef4eb6d95867c7819fd42232e9101ae87c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:32 +0000

    Add package ftw.contentpage/1.1/ftw.contentpage-1.1.zip

---
+++ packages/ftw.contentpage/ftw.contentpage-1.1.zip/ftw.contentpage-1.1/ftw/contentpage/browser/newslisting.py
@@ -0,0 +1,22 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ftw.contentpage.browser.baselisting import BaseListing
+
+
+class NewsListing(BaseListing):
+
+    template = ViewPageTemplateFile('newslisting.pt')
+
+    def __call__(self):
+        if self.__name__ == 'news_rss_listing':
+            self.template = ViewPageTemplateFile('newslisting_rss.pt')
+        return super(NewsListing, self).__call__()
+
+    def get_items(self):
+        """Get all news items"""
+        query = {}
+        query['portal_type'] = 'News'
+        query['sort_on'] = 'effective'
+        query['sort_order'] = 'reverse'
+
+        # Implement archive functionality - used by the archive portlet
+        return self.search_results(query, 'effective')
---
+++ packages/ftw.contentpage/ftw.contentpage-1.1.zip/ftw.contentpage-1.1/ftw/contentpage/portlets/news_archive_portlet.py
@@ -0,0 +1,45 @@
+from plone.app.portlets.portlets import base
+from plone.memoize.view import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements
+from ftw.contentpage.portlets.base_archive_portlet import archive_summary
+
+
+class INewsArchivePortlet(IPortletDataProvider):
+    """Archive portlet interface.
+"""
+
+
+class Assignment(base.Assignment):
+    implements(INewsArchivePortlet)
+
+    @property
+    def title(self):
+        return "News Archive Portlet"
+
+
+class Renderer(base.Renderer):
+    def __init__(self, context, request, view, manager, data):
+        self.context = context
+        self.data = data
+        self.request = request
+
+    @property
+    def available(self):
+        """Only show the portlet, if there are News
+        """
+        return bool(self.archive_summary())
+
+    @memoize
+    def archive_summary(self):
+        """Returns an ordered list of summary infos per month."""
+        return archive_summary(self.context, self.request, 'News', 'effective')
+
+    render = ViewPageTemplateFile('news_archive_portlet.pt')
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
