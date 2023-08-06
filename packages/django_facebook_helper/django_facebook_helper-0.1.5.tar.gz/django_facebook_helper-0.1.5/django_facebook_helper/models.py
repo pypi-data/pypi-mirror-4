ddd2a7a18af73e76349e7759b7129e907431e838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package django_facebook_helper/0.1.5/django_facebook_helper-0.1.5.tar.gz

---
+++ packages/django_facebook_helper/django_facebook_helper-0.1.5.tar.gz/django_facebook_helper-0.1.5/django_facebook_helper/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    access_token = models.CharField(max_length=103, unique=True)
+    expires = models.IntegerField(null=True)
+    uid = models.BigIntegerField(unique=True, null=True)
+        
+    class Meta:
+        unique_together = ('user', 'uid')
+        
+    def __unicode__(self):
+        return u'Session for %s (%s) ' % (self.user.username, self.user.email)
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
