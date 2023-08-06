2636a578d5169afb7e352cc69fe26efd7ba7108c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package leon/0.3.6/leon-0.3.6-py3.2.egg

---
+++ packages/leon/leon-0.3.6-py3.2.egg/leon/__init__.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import cherrypy
+from leon.web_handler import WebHandler as _WebHandler
+from leon.arg_conversions import ListOf
+
+
+def create(config=None):
+    """
+    :param config: Optional configuration dict
+    :type config: dict
+    :return: The Leon application
+    :rtype: WebHandler
+    """
+    return _WebHandler(config)
+
+
+def start_server(app):
+    """
+    :param app: The Leon application to start
+    :type app: WebHandler
+    """
+    cherrypy.quickstart(app)
