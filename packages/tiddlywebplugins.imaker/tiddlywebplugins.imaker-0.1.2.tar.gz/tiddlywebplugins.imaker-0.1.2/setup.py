891d31592e21ad25e107632d33a8a7443ba35f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:46 +0000

    Add package tiddlywebplugins.imaker/0.1.2/tiddlywebplugins.imaker-0.1.2.tar.gz

---
+++ packages/tiddlywebplugins.imaker/tiddlywebplugins.imaker-0.1.2.tar.gz/tiddlywebplugins.imaker-0.1.2/setup.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.imaker'
+DESCRIPTION = 'Make TiddlyWeb instances'
+VERSION = '0.1.2'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    platforms = 'Posix; MacOS X; Windows',
+    packages = find_packages(exclude=['test']),
+    install_requires = ['tiddlyweb',
+        'tiddlywebplugins.utils',
+        'tiddlywebplugins.pkgstore',
+        ],
+    zip_safe = False
+)
