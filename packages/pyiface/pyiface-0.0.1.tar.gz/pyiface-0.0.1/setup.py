6e1f64604be1cdd4d0092dacacf552d8c210dd03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:19 +0000

    Add package pyiface/0.0.1/pyiface-0.0.1.tar.gz

---
+++ packages/pyiface/pyiface-0.0.1.tar.gz/pyiface-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pyiface',
+    version='0.0.1',
+    author='Sergey Kostov',
+    author_email='bat.serjo@gmail.com',
+    packages=['pyiface'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/pyiface/',
+    license='LICENSE.txt',
+    description='View and control network interfaces',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
