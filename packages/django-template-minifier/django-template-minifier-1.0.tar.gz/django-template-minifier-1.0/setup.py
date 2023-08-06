28c03c4558b3f41e81bd7a53e390811c87ca2ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package django-template-minifier/1.0/django-template-minifier-1.0.tar.gz

---
+++ packages/django-template-minifier/django-template-minifier-1.0.tar.gz/django-template-minifier-1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-template-minifier',
+    version='1.0',
+    description='Python package, providing simple django template loader. It reduces HTML output in templates by stripping out whitespace characters between HTML and django template tags.',
+    author='Leszek Piatek',
+    author_email='lpiatek@gmail.com',
+    url='https://github.com/iRynek/django-template-minifier',
+    license='GPL',
+    packages=[
+        'template_minifier',
+        'template_minifier.template',
+        'template_minifier.template.loaders',
+    ],
+    requires=[
+        'Django',
+    ],
+)
