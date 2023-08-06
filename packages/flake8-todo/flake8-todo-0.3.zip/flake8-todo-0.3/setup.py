161256c246c1aec997861573469d70ece3a4e4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:16 +0000

    Add package flake8-todo/0.3/flake8-todo-0.3.zip

---
+++ packages/flake8-todo/flake8-todo-0.3.zip/flake8-todo-0.3/setup.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from __future__ import with_statement
+from setuptools import setup
+
+
+def get_version(fname='flake8_todo.py'):
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    descr = []
+    for fname in ('README.rst',):
+        with open(fname) as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
+
+setup(
+    name='flake8-todo',
+    version=get_version(),
+    description="TODO notes checker, plugin for flake8",  # noqa
+    long_description=get_long_description(),
+    keywords='flake8 todo',
+    author='Marc Schlaich',
+    author_email='marc.schlaich@gmail.com',
+    url='https://github.com/schlamar/flake8-todo',
+    license='MIT',
+    py_modules=['flake8_todo'],
+    zip_safe=False,
+    entry_points={
+        'flake8.extension': [
+            'flake8_todo = flake8_todo:check_todo_notes',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
