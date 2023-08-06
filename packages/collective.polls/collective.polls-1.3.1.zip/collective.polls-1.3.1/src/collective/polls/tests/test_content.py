9732df901a54f014380b53bd8df630f665e07199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:14 +0000

    Add package collective.polls/1.3.1/collective.polls-1.3.1.zip

---
+++ packages/collective.polls/collective.polls-1.3.1.zip/collective.polls-1.3.1/src/collective/polls/tests/test_content.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from AccessControl import Unauthorized
+
+from zope.component import createObject
+from zope.component import queryUtility
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from plone.app.testing import logout
+
+from plone.app.referenceablebehavior.referenceable import IReferenceable
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.uuid.interfaces import IAttributeUUID
+
+from collective.polls.config import PERMISSION_VOTE
+from collective.polls.content.poll import InsuficientOptions
+from collective.polls.content.poll import IPoll
+from collective.polls.testing import INTEGRATION_TESTING
+
+
+class MockPoll(object):
+    options = []
+
+
+class IntegrationTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+
+    def test_adding(self):
+        self.folder.invokeFactory('collective.polls.poll', 'p1')
+        p1 = self.folder['p1']
+        self.assertTrue(IPoll.providedBy(p1))
+
+    def test_default_values_for_checkboxes(self):
+        self.folder.invokeFactory('collective.polls.poll', 'p1')
+        p1 = self.folder['p1']
+        self.assertTrue(p1.allow_anonymous)
+        self.assertTrue(p1.show_results)
+
+    def test_fti(self):
+        fti = queryUtility(IDexterityFTI, name='collective.polls.poll')
+        self.assertNotEquals(None, fti)
+
+    def test_schema(self):
+        fti = queryUtility(IDexterityFTI, name='collective.polls.poll')
+        schema = fti.lookupSchema()
+        self.assertEquals(IPoll, schema)
+
+    def test_factory(self):
+        fti = queryUtility(IDexterityFTI, name='collective.polls.poll')
+        factory = fti.factory
+        new_object = createObject(factory)
+        self.assertTrue(IPoll.providedBy(new_object))
+
+    def test_is_referenceable(self):
+        self.folder.invokeFactory('collective.polls.poll', 'p1')
+        p1 = self.folder['p1']
+        self.assertTrue(IReferenceable.providedBy(p1))
+        self.assertTrue(IAttributeUUID.providedBy(p1))
+
+    def test_validate_no_options(self):
+        data = MockPoll()
+        self.assertRaises(InsuficientOptions, IPoll.validateInvariants, data)
+
+    def test_validate_one_option(self):
+        data = MockPoll()
+        data.options = [{'option_id': 0, 'description': 'Foo'}]
+        self.assertRaises(InsuficientOptions, IPoll.validateInvariants, data)
+
+    def test_validate_two_options(self):
+        data = MockPoll()
+        data.options = [
+            {'option_id': 0, 'description': 'Foo'},
+            {'option_id': 1, 'description': 'Bar'},
+        ]
+        try:
+            IPoll.validateInvariants(data)
+        except InsuficientOptions:
+            self.fail()
+
+
+class VotingTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def _set_request_cookies(self, request):
+        ''' For each cookie in response, we set a cookie in request '''
+        response_cookies = request.response.cookies
+        for key, cookie in response_cookies.items():
+            request.cookies[key] = cookie['value']
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        self.folder = self.portal['test-folder']
+        self.wt = self.portal.portal_workflow
+        self.wt.doActionFor(self.folder, 'publish')
+        self.setUpPolls()
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+
+    def setUpPolls(self):
+        wt = self.wt
+        # Create 3 polls
+        self.folder.invokeFactory('collective.polls.poll', 'p1')
+        self.folder.invokeFactory('collective.polls.poll', 'p2')
+        self.folder.invokeFactory('collective.polls.poll', 'p3')
+        # Set options
+        options = [
+            {'option_id': 0, 'description': 'Option 1'},
+            {'option_id': 1, 'description': 'Option 2'},
+            {'option_id': 2, 'description': 'Option 3'},
+        ]
+        p1 = self.folder['p1']
+        p1.options = options
+        p2 = self.folder['p2']
+        p2.options = options
+        p2.allow_anonymous = False
+        p3 = self.folder['p3']
+        p3.options = options
+        # Open p2 and p3
+        wt.doActionFor(self.folder['p2'], 'open')
+        wt.doActionFor(self.folder['p3'], 'open')
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    def _active_roles(self, roles):
+        return [r['name'] for r in roles if r['selected']]
+
+    def test_permission_to_vote_private_poll(self):
+        poll = self.p1
+        roles = poll.rolesOfPermission(PERMISSION_VOTE)
+        self.assertEqual(self._active_roles(roles), [])
+
+    def test_permission_to_vote_pending_poll(self):
+        wt = self.wt
+        poll = self.p1
+        wt.doActionFor(poll, 'submit')
+        roles = poll.rolesOfPermission(PERMISSION_VOTE)
+        self.assertEqual(self._active_roles(roles), [])
+
+    def test_permission_to_vote_open_poll(self):
+        # Poll without allow_anonymous set
+        poll = self.p2
+        roles = poll.rolesOfPermission(PERMISSION_VOTE)
+        self.assertEqual(self._active_roles(roles), [
+            'Contributor',
+            'Editor',
+            'Manager',
+            'Member',
+            'Reader',
+            'Reviewer',
+            'Site Administrator',
+        ])
+
+    def test_permission_to_vote_open_poll_anon(self):
+        # Poll with allow_anonymous set
+        poll = self.p3
+        roles = poll.rolesOfPermission(PERMISSION_VOTE)
+        self.assertEqual(self._active_roles(roles), [
+            'Anonymous',
+            'Contributor',
+            'Editor',
+            'Manager',
+            'Member',
+            'Reader',
+            'Reviewer',
+            'Site Administrator',
+        ])
+
+    def test_permission_to_vote_closed_poll(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        wt = self.wt
+        poll = self.p3
+        wt.doActionFor(poll, 'close')
+        roles = poll.rolesOfPermission(PERMISSION_VOTE)
+        self.assertEqual(self._active_roles(roles), [])
+
+    def test_vote_closed_poll(self):
+        options = 2
+        self.assertRaises(Unauthorized, self.p1.setVote, options)
+
+        results = self.p1.getResults()
+        self.assertEqual(results[options][1], 0)
+
+        total = self.p1.total_votes
+        self.assertEqual(total, 0)
+
+    def test_vote_open_poll(self):
+        options = 2
+        voted = self.p2.setVote(options)
+        self.assertTrue(voted)
+        results = self.p2.getResults()
+        self.assertEqual(results[options][1], 1)
+        total = self.p2.total_votes
+        self.assertEqual(total, 1)
+
+    def test_vote_open_poll_reject_vote_again(self):
+        options = 2
+        wt = self.wt
+        setRoles(self.portal, TEST_USER_ID, ['Member', 'Reviewer'])
+        self.p2.setVote(options)
+        self.assertEqual(self.p2.total_votes, 1)
+        # Send poll back, must erase votes
+        wt.doActionFor(self.p2, 'reject')
+        self.assertEqual(self.p2.total_votes, 0)
+        # Open again
+        wt.doActionFor(self.p2, 'open')
+        # User must be allowed to vote again
+        self.p2.setVote(options)
+        self.assertEqual(self.p2.total_votes, 1)
+
+    def test_vote_same_user_twice(self):
+        options = 2
+        voted = self.p2.setVote(options)
+        self.assertTrue(voted)
+
+        self.assertRaises(Unauthorized, self.p1.setVote, options)
+
+        results = self.p2.getResults()
+        self.assertEqual(results[options][1], 1)
+        total = self.p2.total_votes
+        self.assertEqual(total, 1)
+
+    def test_invalid_option(self):
+        options = 5
+        voted = self.p2.setVote(options)
+        self.assertFalse(voted)
+
+        total = self.p2.total_votes
+        self.assertEqual(total, 0)
+
+    def test_anonymous_closed_poll(self):
+        logout()
+        options = 1
+
+        self.assertRaises(Unauthorized, self.p1.setVote, options)
+
+        results = self.p1.getResults()
+        self.assertEqual(results[options][1], 0)
+        total = self.p1.total_votes
+        self.assertEqual(total, 0)
+
+    def test_anonymous_open_restricted_poll(self):
+        logout()
+        options = 1
+
+        self.assertRaises(Unauthorized, self.p1.setVote, options)
+
+        results = self.p2.getResults()
+        self.assertEqual(results[options][1], 0)
+        total = self.p2.total_votes
+        self.assertEqual(total, 0)
+
+    def test_anonymous_open_poll(self):
+        logout()
+        options = 1
+        voted = self.p3.setVote(options, self.request)
+        self.assertTrue(voted)
+        results = self.p3.getResults()
+        self.assertEqual(results[options][1], 1)
+        total = self.p3.total_votes
+        self.assertEqual(total, 1)
+
+    def test_anonymous_twice_open_poll(self):
+        logout()
+        options = 1
+        voted = self.p3.setVote(options, self.request)
+        self.assertTrue(voted)
+        self._set_request_cookies(self.request)
+
+        self.assertRaises(Unauthorized, self.p1.setVote, options)
+
+        results = self.p3.getResults()
+        self.assertEqual(results[options][1], 1)
+        total = self.p3.total_votes
+        self.assertEqual(total, 1)
+
+    def test_percentage_vote_report(self):
+        poll = self.p3
+        # Vote as logged user
+        options = 2
+        voted = poll.setVote(options, self.request)
+        self.assertTrue(voted)
+
+        total = poll.total_votes
+        self.assertEqual(total, 1)
+
+        results = poll.getResults()
+        # One vote
+        self.assertEqual(results[options][1], 1)
+        # 100%
+        self.assertEqual(results[options][2], 1.0)
+
+        logout()
+        options = 1
+        voted = poll.setVote(options, self.request)
+        self.assertTrue(voted)
+
+        total = poll.total_votes
+        self.assertEqual(total, 2)
+
+        results = poll.getResults()
+        # One vote
+        self.assertEqual(results[options][1], 1)
+        # 50%
+        self.assertEqual(results[options][2], 0.5)
---
+++ packages/collective.polls/collective.polls-1.3.1.zip/collective.polls-1.3.1/src/collective/polls/tests/test_portlet.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
+
+from zope.i18n import translate
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from plone.app.testing import logout
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from plone.uuid.interfaces import IUUID
+
+from Products.GenericSetup.utils import _getDottedName
+
+from collective.polls.portlet import voteportlet
+
+from collective.polls.testing import INTEGRATION_TESTING
+
+
+class BasePortlet(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def _set_request_cookies(self, request):
+        ''' For each cookie in response, we set a cookie in request '''
+        response_cookies = request.response.cookies
+        for key, cookie in response_cookies.items():
+            request.cookies[key] = cookie['value']
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.wt = self.portal.portal_workflow
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        self.folder = self.portal['test-folder']
+        self.wt.doActionFor(self.folder, 'publish')
+        self.setUpPolls()
+
+    def setUpPolls(self):
+        wt = self.wt
+        # Create 3 polls
+        self.folder.invokeFactory('collective.polls.poll', 'p1')
+        self.folder.invokeFactory('collective.polls.poll', 'p2')
+        self.folder.invokeFactory('collective.polls.poll', 'p3')
+        # Set options
+        options = [
+            {'option_id': 0, 'description': 'Option 1'},
+            {'option_id': 1, 'description': 'Option 2'},
+            {'option_id': 2, 'description': 'Option 3'},
+        ]
+        p1 = self.folder['p1']
+        p1.options = options
+        p2 = self.folder['p2']
+        p2.options = options
+        p2.title = 'Umlaut Ü'
+        p3 = self.folder['p3']
+        p3.options = options
+        p3.allow_anonymous = True
+        # Open p2 and p3
+        wt.doActionFor(self.folder['p2'], 'open')
+        wt.doActionFor(self.folder['p3'], 'open')
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+
+class PortletRegistrationTest(BasePortlet):
+
+    def test_portlet_registered(self):
+        portlet = queryUtility(IPortletType,
+                               name='collective.polls.VotePortlet')
+        self.assertEqual(portlet.addview, 'collective.polls.VotePortlet')
+
+    def test_registered_interfaces(self):
+        portlet = queryUtility(IPortletType,
+                               name='collective.polls.VotePortlet')
+        registered_interfaces = [_getDottedName(i) for i in portlet.for_]
+        registered_interfaces.sort()
+        self.assertEqual(registered_interfaces, [
+            'plone.app.portlets.interfaces.IColumn',
+            'plone.app.portlets.interfaces.IDashboard',
+        ])
+
+    def test_interfaces(self):
+        portlet = voteportlet.Assignment(header='Polls', poll='latest')
+        self.assertTrue(IPortletAssignment.providedBy(portlet))
+        self.assertTrue(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_addview(self):
+        portal = self.portal
+        traversal = '++contextportlets++plone.leftcolumn'
+        portlet = queryUtility(IPortletType,
+                               name='collective.polls.VotePortlet')
+        mapping = portal.restrictedTraverse(traversal)
+        for m in mapping.keys():
+            # Remove all assignments
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        addview.createAndAdd(data={'header': 'Polls', 'poll': 'latest'})
+
+        self.assertEqual(len(mapping), 1)
+        self.assertTrue(isinstance(mapping.values()[0], voteportlet.Assignment))
+
+    def test_invoke_editview(self):
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        mapping['foo'] = voteportlet.Assignment(header='Polls', poll='latest')
+        editview = getMultiAdapter(
+            (mapping['foo'], request), name='edit')
+        self.assertTrue(isinstance(editview, voteportlet.EditForm))
+
+    def test_vocab_possible_polls(self):
+        ''' test vocabulary source for possible polls '''
+        portal = self.portal
+        vocab = voteportlet.PossiblePolls(portal)
+        # We should list here 2 open polls + latest as options
+        self.assertEqual(len(vocab), 3)
+
+    def test_vocab_possible_polls_encoding(self):
+        ''' test encoding of vocabulary '''
+
+        portal = self.portal
+        vocab = voteportlet.PossiblePolls(portal)
+
+        # The title should be stored in unicode
+        for term in vocab:
+            title = translate(term.title)
+            self.assertTrue(type(title) == unicode)
+
+    def test_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager,
+                             name='plone.leftcolumn',
+                             context=self.portal)
+        assignment = voteportlet.Assignment(header='Polls', poll='latest')
+
+        renderer = getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
+        self.assertTrue(isinstance(renderer, voteportlet.Renderer))
+
+    def test_renderer_anonymous(self):
+        NotImplemented
+
+
+class PortletRendererTest(BasePortlet):
+
+    def renderer(self, context=None, request=None, view=None,
+                 manager=None, assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(IPortletManager,
+                                        name='plone.leftcolumn',
+                                        context=self.portal)
+
+        return getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
+
+    def test_available(self):
+        # Use a opened poll
+        poll_uid = IUUID(self.p3)
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll=poll_uid))
+        self.assertEqual(True, r.available)
+
+    def test_available_anonymous(self):
+        # Use a opened poll
+        poll_uid = IUUID(self.p3)
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll=poll_uid))
+        logout()
+        self.assertEqual(True, r.available)
+
+    def test_not_available(self):
+        # Use a non-opened poll
+        poll_uid = IUUID(self.p1)
+        logout()
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll=poll_uid))
+        self.assertEqual(False, r.available)
+
+    def test_options(self):
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll='latest'))
+        self.assertEqual(3, len(r.poll().options))
+
+    def test_all_polls_closed_dont_show_closed(self):
+        wt = self.wt
+        setRoles(self.portal, TEST_USER_ID, ['Reviewer'])
+        wt.doActionFor(self.p2, 'close')
+        wt.doActionFor(self.p3, 'close')
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll='latest',
+                                                            show_closed=False))
+        self.assertEqual(False, r.available)
+
+    def test_all_polls_closed_do_show_closed(self):
+        wt = self.wt
+        setRoles(self.portal, TEST_USER_ID, ['Reviewer'])
+        wt.doActionFor(self.p2, 'close')
+        wt.doActionFor(self.p3, 'close')
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        r = self.renderer(assignment=voteportlet.Assignment(header='Polls',
+                                                            poll='latest',
+                                                            show_closed=True))
+        self.assertEqual(True, r.available)
+        self.assertEqual(3, len(r.poll().options))
