ae8757244f55e25f5e76f28d0630cdd5dc0bfdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package adrest/2.1.3/adrest-2.1.3.tar.gz

---
+++ packages/adrest/adrest-2.1.3.tar.gz/adrest-2.1.3/adrest/utils/paginator.py
@@ -0,0 +1,57 @@
+from urllib import urlencode
+
+from django.core.paginator import InvalidPage, Paginator as DjangoPaginator
+
+from .exceptions import HttpError
+from .status import HTTP_400_BAD_REQUEST
+
+
+class Paginator(object):
+    """ Paginate collections.
+    """
+    def __init__(self, request, qs, max_res):
+        self.query_dict = dict(request.GET.items())
+        self.paginator = None
+        self._page = None
+        self.path = request.path
+
+        max_items = int(self.query_dict.get('max') or max_res)
+        if max_items:
+            self.paginator = DjangoPaginator(qs, max_items)
+
+    @property
+    def page(self):
+        if not self._page:
+            try:
+                self._page = self.paginator.page(
+                    self.query_dict.get('page', 1))
+            except InvalidPage:
+                raise HttpError("Invalid page", status=HTTP_400_BAD_REQUEST)
+        return self._page
+
+    @property
+    def count(self):
+        return self.paginator.count
+
+    @property
+    def resources(self):
+        return self.page.object_list
+
+    @property
+    def next_page(self):
+        if self.page.has_next():
+            self.query_dict['page'] = self.page.next_page_number()
+            return "%s?%s" % (self.path, urlencode(self.query_dict))
+        return ""
+
+    @property
+    def previous_page(self):
+        if self.page.has_previous():
+            previous = self.page.previous_page_number()
+            if previous == 1:
+                if 'page' in self.query_dict:
+                    del self.query_dict['page']
+            else:
+                self.query_dict['page'] = previous
+            return "%s?%s" % (self.path, urlencode(self.query_dict))
+        return ""
