67579e491498ae9a9367e2423bd236ed35501e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:08 +0000

    Add package pydna/0.1.3/pydna-0.1.3.zip

---
+++ packages/pydna/pydna-0.1.3.zip/pydna-0.1.3/scripts/pydna_helper/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__       = "Björn Johansson"
+__date__         = "2012-10-25"
+__copyright__    = "Copyright 2012, Björn Johansson"
+__credits__      = ["Björn Johansson"]
+__license__      = "BSD"
+__version__      = "0.0.1"
+__maintainer__   = "Björn Johansson"
+__email__        = "bjorn_johansson@bio.uminho.pt"
+__status__       = "Development" # "Production" #"Prototype" # "Production"
+
+
+from editor import ape
+from genbank import gb
+#import load_my_primers
+#from load_my_primers import primer, new_primer, old_primer
