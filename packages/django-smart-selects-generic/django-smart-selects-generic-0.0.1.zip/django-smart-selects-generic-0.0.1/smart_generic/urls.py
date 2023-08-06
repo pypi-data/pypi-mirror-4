1bff34377370576cf8d61968efe97639cfa9fa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:02 +0000

    Add package django-smart-selects-generic/0.0.1/django-smart-selects-generic-0.0.1.zip

---
+++ packages/django-smart-selects-generic/django-smart-selects-generic-0.0.1.zip/django-smart-selects-generic-0.0.1/smart_generic/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('smart_generic.views',
+    url(r'^filter/(?P<field>[\w\-]+)/(?P<value>[\w\-]+)/$', 'generic_filterchain', name='generic_chained_filter'),
+    )
