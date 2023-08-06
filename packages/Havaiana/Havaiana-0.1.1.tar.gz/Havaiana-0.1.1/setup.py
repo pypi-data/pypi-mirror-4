8fc1fc54d08a36621d7c9591c7f38b228feaf08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package Havaiana/0.1.1/Havaiana-0.1.1.tar.gz

---
+++ packages/Havaiana/Havaiana-0.1.1.tar.gz/Havaiana-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='Havaiana',
+    version='0.1.1',
+    author='Felipe Lerena',
+    author_email='felipelerena@gmail.com',
+    packages=['havaiana'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Havaiana/',
+    license='LICENSE.txt',
+    description='A GUI for Ojota',
+    long_description=open('README.txt').read(),
+    install_requires=["ojota", "flask"],
+)
