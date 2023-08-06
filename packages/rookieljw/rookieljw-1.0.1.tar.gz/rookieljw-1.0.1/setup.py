8adbafdc95986959d64851ecfe93d080d1d9ce9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:48 +0000

    Add package rookieljw/1.0.1/rookieljw-1.0.1.tar.gz

---
+++ packages/rookieljw/rookieljw-1.0.1.tar.gz/rookieljw-1.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding=utf-8
+from distutils.core import setup
+setup(
+        name='rookieljw',
+        version='1.0.1',
+        py_modules=['rookieljw'],
+        author='rookieljw',
+        author_email='rookieljw@gmail.com',
+        url='http://www.headfirstlabs.com',
+        descriptions='A simple printer of nested lists',
+        )
