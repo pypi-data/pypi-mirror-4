05a11fa115e253e2bef74ba0bf202328fff8a10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:09 +0000

    Add package ground-soil/0.2.3/ground-soil-0.2.3.tar.gz

---
+++ packages/ground-soil/ground-soil-0.2.3.tar.gz/ground-soil-0.2.3/ground_soil/__init__.py
@@ -0,0 +1,12 @@
+#
+# Ground Soil
+# __init__.py
+#
+# Copyright (c) 2013. Dian-Je Tsai. All rights reserved.
+# Author: sodas tsai
+#
+
+from __future__ import unicode_literals
+
+__version__ = '0.2.3'
+VERSION = tuple(map(lambda x: int(x), __version__.split('.')))
