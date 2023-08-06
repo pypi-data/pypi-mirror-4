a0234f12def459ec89e447f52621b22d30980aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:32 +0000

    Add package fence/0.0.3/fence-0.0.3.tar.gz

---
+++ packages/fence/fence-0.0.3.tar.gz/fence-0.0.3/README.md
@@ -0,0 +1,19 @@
+Fence
+======
+
+Python module for BitCoin trading and trending
+
+DONE
+-----
+
+* weighted averages of buy and sell "book"
+* API authentication tested
+
+TODO
+-----
+
+* implement liquidity contributor bot 
+
+    + Buy and sell limits above and below current price
+    + No stop-loss or take-gain limits
+    + Make money when trader panic/greed cause prices to oscillate chaotically
---
+++ packages/fence/fence-0.0.3.tar.gz/fence-0.0.3/setup.py
@@ -0,0 +1,31 @@
+# setup.py for Fence
+from distutils.core import setup
+import os
+from fence import __version__
+
+setup(
+    name = "fence",
+    # packages = ["fence"],
+    version = __version__,
+    description = "BitCoin trending and trading bot",
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    author = "Hobson Lane",
+    author_email = "hobson@totalgood.com",
+    url = "http://github/hobsonlane.com/fence/",
+    # download_url = "https://github.com/hobsonlane.com/fence/archive/v%s.tar.gz" % VERSION,
+    keywords = ["bitcoin", "agent", "bot", "ai", "finance", "trend", "trade"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        # "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Office/Business :: Financial :: Investment",
+        ],
+)
