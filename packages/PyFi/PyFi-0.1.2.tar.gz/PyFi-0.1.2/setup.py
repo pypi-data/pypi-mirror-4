223314ea987b8da47c5e5721fc39d1b0c5c19827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package PyFi/0.1.2/PyFi-0.1.2.tar.gz

---
+++ packages/PyFi/PyFi-0.1.2.tar.gz/PyFi-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='PyFi',
+    version='0.1.2',
+    author='Heinz N. Gies',
+    author_email='heinz@licenser.net',
+    packages=['fifo', 'fifo.api'],
+    scripts=['bin/fifo'],
+    url='http://project-fifo.net',
+    license='CDDL',
+    description='Project FiFo API implementation and console client.',
+    long_description=open('README.md').read(),
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved',
+          'Operating System :: OS Independent',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Internet',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Utilities',
+          ],
+)
