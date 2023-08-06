9f38729b6d31cdb1d7377c83077df33a97a72bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:40 +0000

    Add package sentry-quotas/0.1.6/sentry-quotas-0.1.6.tar.gz

---
+++ packages/sentry-quotas/sentry-quotas-0.1.6.tar.gz/sentry-quotas-0.1.6/src/sentry_quotas/models.py
@@ -0,0 +1,7 @@
+"""
+sentry_quotas.models
+~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2012 by the Sentry Team, see AUTHORS for more details.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
