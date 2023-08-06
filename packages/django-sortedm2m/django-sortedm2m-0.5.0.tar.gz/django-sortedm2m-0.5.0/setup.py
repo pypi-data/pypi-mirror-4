4e42a7c0c2fc29172639d73447d6b8e29fa93e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:14 +0000

    Add package django-sortedm2m/0.5.0/django-sortedm2m-0.5.0.tar.gz

---
+++ packages/django-sortedm2m/django-sortedm2m-0.5.0.tar.gz/django-sortedm2m-0.5.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+import re
+import sys
+from setuptools import find_packages, setup
+
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+class UltraMagicString(object):
+    '''
+    Taken from
+    http://stackoverflow.com/questions/1162338/whats-the-right-way-to-use-unicode-metadata-in-setup-py
+    '''
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __unicode__(self):
+        return self.value.decode('UTF-8')
+
+    def __add__(self, other):
+        return UltraMagicString(self.value + str(other))
+
+    def split(self, *args, **kw):
+        return self.value.split(*args, **kw)
+
+
+long_description = UltraMagicString('\n\n'.join((
+    read('README.rst'),
+    read('CHANGES.rst'),
+)))
+
+
+setup(
+    name = 'django-sortedm2m',
+    version = find_version('sortedm2m', '__init__.py'),
+    url = 'http://github.com/gregmuellegger/django-sortedm2m',
+    license = 'BSD',
+    description =
+        'Drop-in replacement for django\'s many to many field with '
+        'sorted relations.',
+    long_description = long_description,
+    author = UltraMagicString('Gregor Müllegger'),
+    author_email = 'gregor@muellegger.de',
+    packages = ['sortedm2m'],
+    include_package_data = True,
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [],
+    test_suite='sortedm2m_tests.SetupTestSuite',
+)
