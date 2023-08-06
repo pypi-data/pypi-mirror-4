6edc815667d62d8754d082d1f3b771dfae9a4997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:09 +0000

    Add package rafaelbpa/1.0.0/rafaelbpa-1.0.0.zip

---
+++ packages/rafaelbpa/rafaelbpa-1.0.0.zip/rafaelbpa-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name             = 'rafaelbpa',
+        version          = '1.0.0',
+        py_modules       = ['rafaelbpa'],
+        author           = 'Rafael Araújo',
+        url              = 'http://www.google.com.br/',
+        description      = 'Aprendendo a usar o PyPI',
+    )
