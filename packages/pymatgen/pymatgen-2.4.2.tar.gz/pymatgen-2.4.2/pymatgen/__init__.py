954607c5320bcd9e28284428dd185596d2c2d77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:44 +0000

    Add package pymatgen/2.4.2/pymatgen-2.4.2.tar.gz

---
+++ packages/pymatgen/pymatgen-2.4.2.tar.gz/pymatgen-2.4.2/pymatgen/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "Shyue Ping Ong, Anubhav Jain, Michael Kocher, " + \
+             "Geoffroy Hautier, William Davidson Richard, Dan Gunter, " + \
+             "Shreyas Cholia, Vincent L Chevrier, Rickard Armiento"
+__date__ = "Jan 14 2013"
+__version__ = "2.4.2"
+
+#Useful aliases for commonly used objects and modules.
+
+from pymatgen.core.periodic_table import Element, Specie
+from pymatgen.core.composition import Composition
+from pymatgen.core.structure import Structure, Molecule
+from pymatgen.core.lattice import Lattice
+from pymatgen.serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
+from pymatgen.electronic_structure.core import Spin, Orbital
+from pymatgen.util.io_utils import zopen
+from pymatgen.io.smartio import read_structure, write_structure
