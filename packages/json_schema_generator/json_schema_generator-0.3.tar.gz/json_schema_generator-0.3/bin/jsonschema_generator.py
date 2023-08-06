7b1a30dbdbb3d16f5f2ad0dca91b5ac590bd9e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:19 +0000

    Add package json_schema_generator/0.3/json_schema_generator-0.3.tar.gz

---
+++ packages/json_schema_generator/json_schema_generator-0.3.tar.gz/json_schema_generator-0.3/bin/jsonschema_generator.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import re
+import os
+import argparse
+import string
+
+import json_schema_generator
+from json_schema_generator import Recorder, Validator
+
+
+def record(args):
+    rec = Recorder.from_url(args.json_source)
+    rec.save_json_schema(args.json_schema_file_path, indent=4)
+
+
+def validate(args):
+    from urllib2 import urlopen
+
+    json_data = urlopen(args.json_source).read()
+    validator = Validator.from_path(args.json_schema_file_path)
+    is_valid = validator.assert_json(json_data)
+
+    if is_valid:
+        print " * JSON is valid"
+    else:
+        print " ! JSON is broken "
+        print validator.error_message
+
+
+def homologate(args):
+    template_file_path = os.path.join(os.path.dirname(json_schema_generator.__file__), 'test_template.py.tmpl')
+    json_schemas_dir = os.path.join(args.path, 'json_schemas')
+    json_schema_file_name = '%s.json_schema' % args.homologation_name
+    json_schema_file_path = os.path.join(json_schemas_dir, json_schema_file_name)
+    test_file_path = os.path.join(args.path, 'test_%s_json_schema.py' % args.homologation_name)
+
+    with open(template_file_path) as template_file:
+        tmpl = string.Template(template_file.read())
+
+    if not os.path.exists(json_schemas_dir):
+        os.mkdir(json_schemas_dir)
+
+    if not os.path.exists(json_schema_file_path):
+        rec = Recorder.from_url(args.json_source)
+        rec.save_json_schema(json_schema_file_path, indent=4)
+
+    rendered = tmpl.substitute(
+        homologation_name=args.homologation_name,
+        service_url=args.json_source,
+        json_schema_file_name=json_schema_file_name,
+        json_schemas_dir=json_schemas_dir
+    )
+
+    with open(test_file_path, 'w') as test_file:
+        test_file.write(rendered)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    default_parser = argparse.ArgumentParser(add_help=False)
+    default_parser.add_argument('json_source', type=str, help='url or file')
+    default_parser.add_argument('--path', dest='path', default='', help='set path')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_record = subparsers.add_parser('record', parents=[default_parser])
+    parser_record.add_argument('json_schema_file_path', type=str, help='json schema file path')
+    parser_record.set_defaults(func=record)
+
+    parser_validate = subparsers.add_parser('validate', parents=[default_parser])
+    parser_validate.add_argument('json_schema_file_path', type=str, help='json schema file path')
+    parser_validate.set_defaults(func=validate)
+
+    parser_homologate = subparsers.add_parser('homologate', parents=[default_parser])
+    parser_homologate.add_argument('homologation_name', type=str, help='json schema file path')
+    parser_homologate.set_defaults(func=homologate)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ packages/json_schema_generator/json_schema_generator-0.3.tar.gz/json_schema_generator-0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='json_schema_generator',
+    version='0.3',
+    description='A simple json schema generator based on json resource with auto validation tools',
+    author='Felipe Ramos Ferreira',
+    author_email='perenecabuto@gmail.com',
+    maintainer='Felipe Ramos Ferreira',
+    maintainer_email='perenecabuto@gmail.com',
+    url='https://pypi.python.org/pypi/json_schema_generator/',
+
+    scripts=['bin/jsonschema_generator.py'],
+    include_dirs=('json_schema_generator/',),
+    packages=['json_schema_generator'],
+    #package_data={'jsonschema_generator': ['test_template.py.tmpl']},
+    include_package_data=True,
+    install_requires=['jsonschema==0.8.0'],
+    zip_safe=False,
+
+    keywords='json_schema, jsonschema, json, generator, api, validator',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+    ],
+)
