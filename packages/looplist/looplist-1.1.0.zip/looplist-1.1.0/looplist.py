974bb615428180e75f3a0e627469a3680ffb3454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package looplist/1.1.0/looplist-1.1.0.zip

---
+++ packages/looplist/looplist-1.1.0.zip/looplist-1.1.0/looplist.py
@@ -0,0 +1,14 @@
+"""This first part tells, when you use print_looplist(list), it keeps checking
+for nested lists and printing them out."""
+def print_looplist(the_list):
+        for items in the_list:
+                if isinstance(items, list):
+                        print_looplist(items)
+                else:
+                        print(items)
+"""This second part gives a double-nested module. Try using print_looplist(movies)
+   on it!"""
+movies = [
+    "The Holy Grail", 1975, 'Terry Jones & Terry Gilliam', 91, 
+            ["Graham Chapman",
+                    ["Michael Palin", 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
