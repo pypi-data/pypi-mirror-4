0b8f5530c49652e3bd9097ba4b762a2a42a08e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:12 +0000

    Add package fysom/1.0.6/fysom-1.0.6.tar.gz

---
+++ packages/fysom/fysom-1.0.6.tar.gz/fysom-1.0.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'fysom',
+          version = '1.0.6',
+          description = 'pYthOn Finite State Machine',
+          long_description = '''Please visit https://github.com/mriehl/fysom for more information.''',
+          author = "Mansour Behabadi, Jake Gordon, Maximilien Riehl",
+          author_email = "mansour@oxplot.com, jake@codeincomplete.com, maximilien.riehl@gmail.com",
+          license = 'MIT',
+          url = 'https://github.com/mriehl/fysom',
+          scripts = [],
+          packages = ['fysom'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+          
+          
+          
+          
+          zip_safe=True
+    )
