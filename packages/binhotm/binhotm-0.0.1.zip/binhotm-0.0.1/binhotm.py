7a20da0b7cb3d8dc74e227a8bb167aa816cecda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:40 +0000

    Add package binhotm/0.0.1/binhotm-0.0.1.zip

---
+++ packages/binhotm/binhotm-0.0.1.zip/binhotm-0.0.1/binhotm.py
@@ -0,0 +1,9 @@
+"""Modulo da funcao print lista"""
+"""By: Fabricio Junior da Silva"""
+
+def print_list(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_list(each_item)
+		else:
+			print(each_item)
---
+++ packages/binhotm/binhotm-0.0.1.zip/binhotm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+        name       =  "binhotm",
+        version    =  '0.0.1',
+        py_modules = ['binhotm'],
+        author = 'Fabricio Jr Silva',
+        author_emil = 'fabriciojuniorsilva@gmail.com',
+        url = 'no-url',
+        description = 'Simples modulo v.0.1',)
