736477e7ac50c7b3d880479ac6f5db815aad5862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package z3c.datagenerator/2.0.0/z3c.datagenerator-2.0.0.zip

---
+++ packages/z3c.datagenerator/z3c.datagenerator-2.0.0.zip/z3c.datagenerator-2.0.0/src/z3c/datagenerator/tests/test_doc.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Test Setup"""
+import doctest
+import sys
+import unittest
+from zope.testing.doctestunit import DocFileSuite
+
+def test_suite():
+    filename = 'README3.txt' if sys.version_info[0] == 3 else 'README.txt'
+    return unittest.TestSuite((
+        DocFileSuite('../'+filename,
+                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     ),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
