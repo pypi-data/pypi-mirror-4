3a695849745778789fa3895d36b951dc9269e95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:08 +0000

    Add package infi.nose_logbook/1.0/infi.nose_logbook-1.0-py2.7.egg

---
+++ packages/infi.nose_logbook/infi.nose_logbook-1.0-py2.7.egg/infi/nose_logbook/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c40164406bec6f4814db7995c1abdba27faa5f80'
+__git_head_subject__ = 'Finished release v1.0.'
+__git_head_message__ = '* release/v1.0:\n  TRIVIAL code\n  added all project files\n  empty commit after version v0'
+__git_dirty_diff__ = ''
