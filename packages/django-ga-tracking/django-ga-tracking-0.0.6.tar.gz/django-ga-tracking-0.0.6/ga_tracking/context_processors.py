792c1651315138688d8321629652cf9a40294a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:07 +0000

    Add package django-ga-tracking/0.0.6/django-ga-tracking-0.0.6.tar.gz

---
+++ packages/django-ga-tracking/django-ga-tracking-0.0.6.tar.gz/django-ga-tracking-0.0.6/ga_tracking/context_processors.py
@@ -0,0 +1,9 @@
+from . import settings
+
+def ga_tracking_id(request):
+    is_super = request.user.is_authenticated() and request.user.is_superuser
+    return dict(
+        GA_TRACKING_ID=settings.GA_TRACKING_ID,
+        GA_DOMAIN=settings.GA_DOMAIN,
+        GA_USER_UNTRACKABLE=(not settings.GA_TRACK_SUPERUSER) and is_super
+    )
