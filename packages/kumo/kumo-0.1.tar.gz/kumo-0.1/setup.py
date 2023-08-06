66312e37ce9fa52ef150804b33585b4b759131bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:15 +0000

    Add package kumo/0.1/kumo-0.1.tar.gz

---
+++ packages/kumo/kumo-0.1.tar.gz/kumo-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kumo',
+    version='0.1',
+    packages=['kumo'],
+    url='https://github.com/johnnykv/kumo',
+    license='',
+    author='jkv',
+    author_email='jkv@unixcluster.dk',
+    description='Middleware which submits logs to cloud services like loggly and loggr.'
+)
