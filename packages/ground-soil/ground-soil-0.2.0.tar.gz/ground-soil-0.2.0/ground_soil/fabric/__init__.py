50e3073d8c497929d0a0cba1db5254cabd9b82d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:09 +0000

    Add package ground-soil/0.2.0/ground-soil-0.2.0.tar.gz

---
+++ packages/ground-soil/ground-soil-0.2.0.tar.gz/ground-soil-0.2.0/ground_soil/fabric/__init__.py
@@ -0,0 +1,45 @@
+#
+# Ground Soil
+# fabric/__init__.py
+#
+# Copyright (c) 2013. Dian-Je Tsai. All rights reserved.
+# Author: sodas tsai
+#
+
+from __future__ import unicode_literals
+
+import ast
+import json
+from fabric.api import local, prefix
+from fabric.main import _escape_split
+
+escape_split = _escape_split
+
+
+def lsudo(command, capture=False):
+    return local('sudo {command}'.format(command=command), capture=capture)
+
+
+def virtual_env(venv_path):
+    return prefix('source {path}/bin/activate'.format(path=venv_path))
+
+
+def eval_arg(raw_value, is_json=False):
+    if is_json:
+        try:
+            return json.loads(raw_value)
+        except ValueError:
+            return raw_value
+    else:
+        try:
+            return ast.literal_eval(raw_value)
+        except (SyntaxError, ValueError):
+            return raw_value
+
+
+def eval_args(args):
+    return map(eval_arg, args)
+
+
+def eval_kwargs(kwargs):
+    return dict(map(lambda x: (x[0], eval_arg(x[1])), kwargs.items()))
