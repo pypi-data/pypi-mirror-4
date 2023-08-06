a8541f53f03346d6c3752f31aceedcec4e4ebd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:46 +0000

    Add package hy/0.9.0/hy-0.9.0.tar.gz

---
+++ packages/hy/hy-0.9.0.tar.gz/hy-0.9.0/hy/core/bootstrap.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2012 Paul Tagliamonte <paultag@debian.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+
+from hy.macros import macro
+from hy.models.expression import HyExpression
+from hy.models.symbol import HySymbol
+
+
+@macro("defn")
+def defn_macro(tree):
+    return HyExpression([HySymbol("def"),
+                         tree[1], HyExpression([HySymbol("fn")] + tree[2:])])
+
+
+@macro("cond")
+def cond_macro(tree):
+    tree.pop(0)  # cond flag
+    it = iter(tree)
+    conds = iter(zip(it, it))
+    test, branch = next(conds)
+
+    root = HyExpression([HySymbol("if"), test, branch])
+    ret = root
+    for (test, branch) in conds:
+        n = HyExpression([HySymbol("if"), test, branch])
+        ret.append(n)
+        ret = n
+
+    return root
---
+++ packages/hy/hy-0.9.0.tar.gz/hy-0.9.0/hy/importer.py
@@ -0,0 +1,108 @@
+#
+
+from hy.compiler import hy_compile
+from hy.lex import tokenize
+from hy.core import process
+
+
+import imp
+import sys
+import os
+
+
+if sys.version_info[0] >= 3:
+    from io import StringIO
+else:
+    from StringIO import StringIO
+
+
+def import_buffer_to_hst(fd):
+    tree = tokenize(fd.read())
+    tree = process(tree)
+    return tree
+
+
+def import_file_to_hst(fpath):
+    return import_buffer_to_hst(open(fpath, 'r'))
+
+
+def import_file_to_ast(fpath):
+    tree = import_file_to_hst(fpath)
+    ast = hy_compile(tree)
+    return ast
+
+
+def import_string_to_ast(buff):
+    tree = import_buffer_to_hst(StringIO(buff))
+    ast = hy_compile(tree)
+    return ast
+
+
+def import_file_to_module(name, fpath):
+    ast = import_file_to_ast(fpath)
+    mod = imp.new_module(name)
+    mod.__file__ = fpath
+    eval(compile(ast, fpath, "exec"), mod.__dict__)
+    return mod
+
+
+class HyFinder(object):
+    def is_package(self, fullname):
+        dirpath = "/".join(fullname.split("."))
+        for pth in sys.path:
+            pth = os.path.abspath(pth)
+            composed_path = "%s/%s/__init__.hy" % (pth, dirpath)
+            if os.path.exists(composed_path):
+                return True
+        return False
+
+    def find_on_path(self, fullname):
+        fls = ["%s/__init__.hy", "%s.hy"]
+        dirpath = "/".join(fullname.split("."))
+
+        for pth in sys.path:
+            pth = os.path.abspath(pth)
+            for fp in fls:
+                composed_path = fp % ("%s/%s" % (pth, dirpath))
+                if os.path.exists(composed_path):
+                    return composed_path
+
+
+class MetaLoader(HyFinder):
+    def load_module(self, fullname):
+        if fullname in sys.modules:
+            return sys.modules[fullname]
+
+        pth = self.find_on_path(fullname)
+        if pth is None:
+            return
+
+        sys.modules[fullname] = None
+        mod = import_file_to_module(fullname, pth)
+
+        ispkg = self.is_package(fullname)
+
+        mod.__file__ = pth
+        mod.__loader__ = self
+        mod.__name__ = fullname
+
+        if ispkg:
+            mod.__path__ = []
+            mod.__package__ = fullname
+        else:
+            mod.__package__ = fullname.rpartition('.')[0]
+
+        sys.modules[fullname] = mod
+        return mod
+
+
+class MetaImporter(HyFinder):
+    def find_module(self, fullname, path=None):
+        pth = self.find_on_path(fullname)
+        if pth is None:
+            return
+        return MetaLoader()
+
+
+sys.meta_path.append(MetaImporter())
+sys.path.insert(0, "")
