e824906f1a5de18da335b906f122c2b4a5aa96bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:51 +0000

    Add package django-model-utils/1.2.0/django-model-utils-1.2.0.tar.gz

---
+++ packages/django-model-utils/django-model-utils-1.2.0.tar.gz/django-model-utils-1.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+long_description = (open('README.rst').read() +
+                    open('CHANGES.rst').read() +
+                    open('TODO.rst').read())
+
+
+setup(
+    name='django-model-utils',
+    version='1.2.0',
+    description='Django model mixins and utilities',
+    long_description=long_description,
+    author='Carl Meyer',
+    author_email='carl@dirtcircle.com',
+    url='http://bitbucket.org/carljm/django-model-utils/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    tests_require=["Django>=1.1"],
+    test_suite='runtests.runtests'
+)
