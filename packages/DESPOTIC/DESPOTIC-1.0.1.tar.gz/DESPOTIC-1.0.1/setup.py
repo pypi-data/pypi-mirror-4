e1c1a665a8d770b93a1bdd66f15e02593a92e79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:21 +0000

    Add package DESPOTIC/1.0.1/DESPOTIC-1.0.1.tar.gz

---
+++ packages/DESPOTIC/DESPOTIC-1.0.1.tar.gz/DESPOTIC-1.0.1/setup.py
@@ -0,0 +1,14 @@
+"""
+This is a setup script for the DESPOTIC package.
+"""
+
+from distutils.core import setup
+
+setup(name='DESPOTIC', \
+          version='1.0.1', \
+          description='A library to Derive the Energetics and SPectra of Optically Thick Intserstellar Clouds', \
+          author='Mark Krumholz', \
+          author_email='mkrumhol@ucsc.edu', \
+          url='https://code.google.com/p/despotic/', \
+          packages=['despotic'], \
+          license='GPL v3')
