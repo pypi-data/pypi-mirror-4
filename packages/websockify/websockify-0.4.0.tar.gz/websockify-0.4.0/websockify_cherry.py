5aa0b9caf15247d247b77712212760b6e77dc934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:54 +0000

    Add package websockify/0.4.0/websockify-0.4.0.tar.gz

---
+++ packages/websockify/websockify-0.4.0.tar.gz/websockify-0.4.0/websockify_cherry.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import argparse
+import random
+import os
+
+import cherrypy
+
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+from ws4py.messaging import TextMessage
+
+class WebSocketProxy(WebSocket):
+    def opened(self):
+
+    def received_message(self, m):
+        cherrypy.engine.publish('websocket-broadcast', m)
+
+    def closed(self, code, reason="A client left the room without a proper explanation."):
+        cherrypy.engine.publish('websocket-broadcast', TextMessage(reason))
+
+class Root(object):
+    def __init__(self, host, port, ssl=False):
+        self.host = host
+        self.port = port
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Echo CherryPy Server')
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default=9000, type=int)
+    parser.add_argument('--ssl', action='store_true')
+    args = parser.parse_args()
+
+    cherrypy.config.update({'server.socket_host': args.host,
+                            'server.socket_port': args.port,
+                            'tools.staticdir.root': os.path.abspath(os.path.dirname(__file__))})
+
+    if args.ssl:
+        cherrypy.config.update({'server.ssl_certificate': './server.crt',
+                                'server.ssl_private_key': './server.key'})
+                            
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+    cherrypy.tools.protocols = ['binary'];
+
+    cherrypy.quickstart(Root(args.host, args.port, args.ssl), '', config={
+        '/ws': {
+            'tools.websocket.on': True,
+            'tools.websocket.handler_cls': WebSocketProxy
+            },
+        '/': {
+              'tools.staticdir.on': True,
+              'tools.staticdir.dir': '.'
+            }
+        }
+    )
