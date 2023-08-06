2f0e357bbd48870e94aad0e7ba057cd1c1e1fdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:09 +0000

    Add package collective.behavior.stock/0.3/collective.behavior.stock-0.3.zip

---
+++ packages/collective.behavior.stock/collective.behavior.stock-0.3.zip/collective.behavior.stock-0.3/src/collective/behavior/stock/docs/HISTORY.rst
@@ -0,0 +1,23 @@
+Change log
+----------
+
+0.3 (2013-02-03)
+================
+
+- Defined initial_stock. [taito]
+- Integrated Travis CI. [taito]
+
+0.2.1 (2012-09-24)
+==================
+
+- Finnish translations updated. [taito]
+
+0.2 (2012-09-17)
+================
+
+- Finnish translations added. [taito]
+
+0.1 (2012-09-01)
+================
+
+- Initial release. [taito]
---
+++ packages/collective.behavior.stock/collective.behavior.stock-0.3.zip/collective.behavior.stock-0.3/src/collective/behavior/stock/interfaces.py
@@ -0,0 +1,23 @@
+from collective.behavior.stock import _
+from zope.interface import Attribute
+from zope.interface import Interface
+from zope.schema import Int
+
+
+class IStock(Interface):
+    """Interface for Stock behavior."""
+
+    reducible_quantity = Int(
+        title=_(u'Maximum Reducible Quantity'),
+        description=_(u'The maximum quantity to be reduced at once.'),
+        default=100,
+        min=1)
+
+    initial_stock = Attribute('Sum of initial stocks')
+    stock = Attribute('Sum of stocks')
+
+    def sub_stock(value):  # pragma: no cover
+        """Decrease stock by value and return the result."""
+
+    def add_stock(value):  # pragma: no cover
+        """Add stock by value and return the result."""
