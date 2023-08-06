514a9d3d21a1cf65b68a60193a4a0330b987c031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:24 +0000

    Add package cmsplugin-blog-categories/0.5.2/cmsplugin-blog-categories-0.5.2.tar.gz

---
+++ packages/cmsplugin-blog-categories/cmsplugin-blog-categories-0.5.2.tar.gz/cmsplugin-blog-categories-0.5.2/cmsplugin_blog_categories/cms_plugins.py
@@ -0,0 +1,22 @@
+"""CMS Plugins for the ``cmsplugin_blog_categories`` app."""
+from django.utils.translation import ugettext as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from cmsplugin_blog_categories.models import CategoryPlugin
+
+
+class CMSCategoryPlugin(CMSPluginBase):
+    model = CategoryPlugin
+    name = _('Blog Category')
+    render_template = 'cmsplugin_blog_categories/category_plugin.html'
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'categories': instance.categories.all(),
+            'placeholder': placeholder,
+        })
+        return context
+
+plugin_pool.register_plugin(CMSCategoryPlugin)
