8a82a5f0e3ff61fb3f4017b06aff357daf40f113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:49 +0000

    Add package cmpGen/0.1.2/cmpGen-0.1.2.tar.gz

---
+++ packages/cmpGen/cmpGen-0.1.2.tar.gz/cmpGen-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='cmpGen',
+    version='0.1.2', # final version, now deprecated
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['cmpGen'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/cmpGen/',
+    license='LICENSE.txt',
+    description='cmpGen is deprecated; it is maintained here for legacy purposes',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
