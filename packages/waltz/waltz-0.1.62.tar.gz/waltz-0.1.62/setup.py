44093fc8c852308eb8cc1fe49d31c06b5c793c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:35 +0000

    Add package waltz/0.1.62/waltz-0.1.62.tar.gz

---
+++ packages/waltz/waltz-0.1.62.tar.gz/waltz-0.1.62/setup.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+
+"""
+    waltz
+    ~~~~~
+
+    Setup
+    `````
+
+    $ pip install waltz    
+"""
+
+from distutils.core import setup
+
+setup(
+    name='waltz',
+    version='0.1.62',
+    url='http://github.com/mekarpeles/waltz',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=[
+        'waltz',
+        ],
+    platforms='any',
+    scripts=[],
+    license='LICENSE',
+    install_requires=[
+        'lazydb >= 0.1.6',
+        'web.py >= 0.36',
+        'lepl >= 5.1.3',
+    ],
+    description="Waltz is a web.py framework for designing web apps in 3/4 time.",
+    long_description=open('README.txt').read(),
+)
