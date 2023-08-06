0a3da2b3b858734bc2e046d84fb164e8ec3fb4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:22 +0000

    Add package bottle-websocket/0.2.1/bottle-websocket-0.2.1.tar.gz

---
+++ packages/bottle-websocket/bottle-websocket-0.2.1.tar.gz/bottle-websocket-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from os import path
+
+readme = open(path.join(path.abspath(path.dirname(__file__)), 'README.md')).read()
+
+setup(
+    name='bottle-websocket',
+    version='0.2.1',
+    author='Zach Kelling',
+    author_email='zeekayy@gmail.com',
+    packages=['bottle_websocket',],
+    description='WebSockets for bottle',
+    long_description=readme,
+    install_requires=['bottle', 'gevent-websocket'],
+)
