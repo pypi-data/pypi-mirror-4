96aacc82f1858f41bb1d8e1561c9f988c17f8e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:10 +0000

    Add package custodian/0.2.0a/custodian-0.2.0a.tar.gz

---
+++ packages/custodian/custodian-0.2.0a.tar.gz/custodian-0.2.0a/README.rst
@@ -0,0 +1,78 @@
+.. image:: https://travis-ci.org/materialsproject/custodian.png
+
+Custodian is a simple, robust and flexible just-in-time job management
+framework written in Python. Using custodian, you can create wrappers that
+perform error checking, job management and error recovery. It has a simple
+plugin framework that allows you to develop specific job management workflows
+for different applications.
+
+Custodian is now in an very early alpha. Use with care.
+
+Getting custodian
+=================
+
+Stable version
+--------------
+
+The version at the Python Package Index (PyPI) is always the latest stable
+release that will be hopefully, be relatively bug-free. The easiest way to
+install custodian on any system is to use easy_install or pip, as follows::
+
+    easy_install custodian
+
+or::
+
+    pip install custodian
+
+Some plugins (e.g., vasp management) require additional setup (please see
+`custodian's documentation <http://packages.python.org/custodian>`_).
+
+Developmental version
+---------------------
+
+The bleeding edge developmental version is at the custodian's `Github repo
+<https://github.com/materialsproject/custodian>`_. The developmental
+version is likely to be more buggy, but may contain new features. The
+Github version include test files as well for complete unit testing. After
+cloning the source, you can type::
+
+    python setup.py install
+
+or to install the package in developmental mode::
+
+    python setup.py develop
+
+Requirements
+============
+
+Custodian requires Python 2.7+. There are no other required dependencies.
+
+Optional dependencies
+---------------------
+
+Optional libraries that are required if you need certain features:
+
+1. pymatgen 2.5+: To use the plugin for VASP.
+2. nose - For complete unittesting.
+
+Basic usage
+===========
+
+The main class in the workflow is known as Custodian, which manages a series
+of jobs with a list of error handlers. To use custodian, you need to implement
+concrete implementation of the abstract base classes custodian.custodian.Job
+and custodian.custodian.ErrorHandler. Specific examples of this for
+electronic structure calculations based on the Vienna Ab Initio Simulation
+Package (VASP) are implemented in the custodian.vasp package.
+
+A simple example of a script using Custodian to run a two-relaxation VASP job
+is as follows::
+
+    from custodian.custodian import Custodian
+    from custodian.vasp.handlers import VaspErrorHandler, UnconvergedErrorHandler
+    from custodian.vasp.jobs import BasicVaspJob, SecondRelaxationVaspJob
+
+    handlers = [VaspErrorHandler(), UnconvergedErrorHandler(), PoscarErrorHandler()]
+    jobs = [BasicVaspJob(), SecondRelaxationVaspJob()]
+    c = Custodian(handlers, jobs, max_errors=10)
+    c.run()
---
+++ packages/custodian/custodian-0.2.0a.tar.gz/custodian-0.2.0a/custodian/vasp/handlers.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+"""
+This module implements specific error handlers for VASP runs. These handlers
+tries to detect common errors in vasp runs and attempt to fix them on the fly
+by modifying the input files.
+"""
+
+from __future__ import division
+
+__author__ = "Shyue Ping Ong"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyuep@gmail.com"
+__status__ = "Beta"
+__date__ = "2/4/13"
+
+import os
+import shutil
+import json
+
+from custodian.custodian import ErrorHandler
+from pymatgen.io.vaspio.vasp_input import Incar, Poscar, VaspInput
+from pymatgen.io.vaspio.vasp_output import Vasprun
+from custodian.ansible.intepreter import Modder
+
+
+class VaspErrorHandler(ErrorHandler):
+
+    error_msgs = {
+        "tet": ["Tetrahedron method fails for NKPT<4",
+                "Fatal error detecting k-mesh",
+                "Fatal error: unable to match k-point",
+                "Routine TETIRR needs special values"],
+        "inv_rot_mat": ["inverse of rotation matrix was not found (increase SYMPREC)"],
+        "brmix": ["BRMIX: very serious problems"],
+        "subspacematrix": ["WARNING: Sub-Space-Matrix is not hermitian in DAV"],
+        "tetirr": ["Routine TETIRR needs special values"],
+        "incorrect_shift": ["Could not get correct shifts"],
+        "mesh_symmetry": ["Reciprocal lattice and k-lattice belong to "
+                          "different class of lattices."]
+    }
+
+    def __init__(self, output_filename="vasp.out"):
+        self.output_filename = output_filename
+
+    def check(self):
+        self.errors = set()
+        with open(self.output_filename, "r") as f:
+            for line in f:
+                l = line.strip()
+                for err, msgs in VaspErrorHandler.error_msgs.items():
+                    for msg in msgs:
+                        if l.startswith(msg):
+                            self.errors.add(err)
+        return len(self.errors) > 0
+
+    def correct(self):
+        actions = []
+        vi = VaspInput.from_directory(".")
+        history = []
+
+        if "tet" in self.errors:
+            actions.append({'dict': 'INCAR',
+                            'action': {'_set': {'ISMEAR': 0}}})
+        if "inv_rot_mat" in self.errors:
+            actions.append({'dict': 'INCAR',
+                            'action': {'_set': {'SYMPREC': 1e-8}}})
+        if "brmix" in self.errors:
+            actions.append({'dict': 'INCAR',
+                            'action': {'_set': {'IMIX': 1}}})
+        if "subspacematrix" in self.errors:
+            actions.append({'dict': 'INCAR',
+                            'action': {'_set': {'INCAR->LREAL': False}}})
+        if "tetirr" in self.errors or "incorrect_shift" in self.errors:
+            actions.append({'dict': 'KPOINTS',
+                            'action': {'_set': {'style': "Gamma"}}})
+        if "mesh_symmetry" in self.errors:
+            m = max(vi["KPOINTS"].kpts[0])
+            actions.append({'dict': 'KPOINTS',
+                            'action': {'_set': {'kpoints': [[m] * 3]}}})
+        m = Modder()
+        for a in actions:
+            vi[a["dict"]] = m.modify_object(a["action"], vi[a["dict"]])
+        self.actions = actions
+        if os.path.exists("corrections.json"):
+            with open("corrections.json", "r") as f:
+                history = json.load(f)
+        history.append({'errors': list(self.errors), 'actions': actions})
+        with open("corrections.json", "w") as f:
+            json.dump(history, f, indent=4)
+        vi.write_input(".")
+
+    def __str__(self):
+        return "Vasp error"
+
+
+class UnconvergedErrorHandler(ErrorHandler):
+    #todo: Make this work using ansible.
+
+    def check(self):
+        v = Vasprun('vasprun.xml')
+        if not v.converged:
+            return True
+        return False
+
+    def correct(self):
+        shutil.copy("CONTCAR", "POSCAR")
+        incar = Incar.from_file("INCAR")
+        incar['ISTART'] = 1
+        incar.write_file("INCAR")
+
+    def __str__(self):
+        return "Run unconverged."
+
+
+class PoscarErrorHandler(ErrorHandler):
+
+    def check(self):
+        with open("vasp.out", "r") as f:
+            output = f.read()
+            for line in output.split("\n"):
+                l = line.strip()
+                if l.startswith("Found some non-integer element in rotation matrix"):
+                    return True
+        return False
+
+    def correct(self):
+        #TODO: Add transformation applied to transformation.json if exists.
+        from pymatgen.transformations.standard_transformations import PerturbStructureTransformation
+        shutil.copy("POSCAR", "POSCAR.orig")
+        p = Poscar.from_file("POSCAR")
+        s = p.struct
+        trans = PerturbStructureTransformation(0.05)
+        new_s = trans.apply_transformation(s)
+        p = Poscar(new_s)
+        p.write_file("POSCAR")
+
+
+"""
+Aflow Error 9 (davidson)
+
+grep : "WARNING: Sub-Space-Matrix is not hermitian in DAV"
+fix : INCAR -> remove ALGO, IMIX, set ALGO=VERY_FAST
+
+Aflow Error 10 (nbands)
+
+grep : "BRMIX: very serious problems"
+fix :
+notes:
+aflow tries to get NBANDS from the OUTCAR
+-> nbands=atoi(word.substr(word.find("NBANDS")+7).c_str());
+aflow then tries to get NBANDS from INCAR
+Then it increases NBANDS via
+nbands=nbands+5+nbands/5
+
+"""
