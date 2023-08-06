5adbbe25079c5d1c561aeae474f9a9c613462f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:49 +0000

    Add package metrica-client/0.1.5/metrica-client-0.1.5.tar.gz

---
+++ packages/metrica-client/metrica-client-0.1.5.tar.gz/metrica-client-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='metrica-client',
+      version='0.1.5',
+      scripts=['metrica_setup', 'metrica_connect'],
+      install_requires=['pycrypto==2.6', 'requests==0.14.2'],
+      description='Client to connect your database to the Metrica MongoDB Analytics service',
+      url='http://getmetrica.com',
+      author='David Crawford',
+      author_email='david@getmetrica.com',
+      license='MIT'
+      )
