9fdcf12d3e043e9ec7ce0faa9801226ae503258e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package tgext.pluggable/0.1.3/tgext.pluggable-0.1.3.tar.gz

---
+++ packages/tgext.pluggable/tgext.pluggable-0.1.3.tar.gz/tgext.pluggable-0.1.3/tgext/pluggable/adapt_websetup.py
@@ -0,0 +1,47 @@
+try:
+    import transaction
+except ImportError:
+    transaction = None
+
+class PluggedBootstrap(object):
+    def __init__(self, module_name, previous_bootstrap, plugin_bootstrap):
+        self.module_name = module_name
+        self.previous_bootstrap = previous_bootstrap
+        self.plugin_bootstrap = plugin_bootstrap
+
+    def __call__(self, command, conf, vars):
+        if not transaction:
+            print 'Transaction module not available, this might lead to issues'
+
+        #Call previous bootstrap
+        self.previous_bootstrap(command, conf, vars)
+
+        #Call this pluggable app bootstrap
+        from sqlalchemy.exc import IntegrityError
+        try:
+            self.plugin_bootstrap(command, conf, vars)
+            transaction and transaction.commit()
+        except IntegrityError:
+            print 'Warning, there was a problem running %s bootstrap, might have already been already performed' % self.module_name
+            import traceback
+            print traceback.format_exc()
+            transaction and transaction.abort()
+            print 'Continuing with bootstrapping...'
+
+
+class WebSetupAdapter(object):
+    def __init__(self, config, module, options):
+        self.config = config
+        self.models = module.model
+        self.options = options
+        self.plugin_bootstrap = module.bootstrap.bootstrap
+        self.module_name = module.__name__
+
+    def adapt_bootstrap(self):
+        #Import application websetup if not already available
+        __import__(self.config['package'].__name__, globals(), locals(), ['websetup'])
+
+        current_bootstrap = self.config['package'].websetup.bootstrap
+        current_bootstrap.bootstrap = PluggedBootstrap(self.module_name,
+                                                       current_bootstrap.bootstrap,
+                                                       self.plugin_bootstrap)
