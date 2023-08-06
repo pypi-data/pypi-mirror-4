84c186627874bbe83439c9bdb59d92632a3defb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package HorizonJPL/0.1.5/HorizonJPL-0.1.5.tar.gz

---
+++ packages/HorizonJPL/HorizonJPL-0.1.5.tar.gz/HorizonJPL-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='HorizonJPL',
+    version='0.1.5',
+    author='Matthew Mihok',
+    author_email='matthew.mihok@niewma.com',
+    packages=['horizon', 'horizon.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/HorizonJPL/',
+    license='LICENSE.txt',
+    description='Access to NASA\'s JPL HORIZON Ephemeris System',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'pytest-timeout >= 0.3'
+    ]
+)
