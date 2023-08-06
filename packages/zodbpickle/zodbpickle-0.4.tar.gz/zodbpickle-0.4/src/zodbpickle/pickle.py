24468b3af9d1e42f45f03da70db9b42bf41f4cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package zodbpickle/0.4/zodbpickle-0.4.tar.gz

---
+++ packages/zodbpickle/zodbpickle-0.4.tar.gz/zodbpickle-0.4/src/zodbpickle/pickle.py
@@ -0,0 +1,34 @@
+import sys
+
+__all__ = ["PickleError", "PicklingError", "UnpicklingError", "Pickler",
+           "Unpickler", "dump", "dumps", "load", "loads"]
+
+if sys.version_info[0] >= 3:
+    from .pickle_3 import (
+        PickleError,
+        PicklingError,
+        UnpicklingError,
+        Pickler,
+        Unpickler,
+        dump,
+        dumps,
+        load,
+        loads,
+        # needed by pickletester
+        bytes_types
+    )
+
+if sys.version_info[0] >= 3:
+    from .pickle_2 import (
+        PickleError,
+        PicklingError,
+        UnpicklingError,
+        Pickler,
+        Unpickler,
+        dump,
+        dumps,
+        load,
+        loads,
+        #
+        bytes_types
+    )
