1b0b5343f71c3c3321fe6cbd44621e6e33535581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:05 +0000

    Add package csvt/1.0.0/csvt-1.0.0.tar.gz

---
+++ packages/csvt/csvt-1.0.0.tar.gz/csvt-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'csvt',
+    version = '1.0.0',
+    py_modules = ['csvt'],
+    author = 'csvt.net',
+    author_email = 'admin@csvt.net',
+    url = 'http://www.csvt.net',
+    description = 'A sample test',
+)
