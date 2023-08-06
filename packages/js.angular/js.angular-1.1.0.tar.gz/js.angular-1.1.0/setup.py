3a685fbab62865456dfe0253a7b207a40ce06e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package js.angular/1.1.0/js.angular-1.1.0.tar.gz

---
+++ packages/js.angular/js.angular-1.1.0.tar.gz/js.angular-1.1.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '1.1.0'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('js', 'angular', 'test_angularjs.txt')
+    )
+
+setup(
+    name='js.angular',
+    version=version,
+    description="Fanstatic packaging of AngularJS",
+    long_description=long_description,
+    classifiers=[
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3.2",
+      "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords='',
+    license='MIT',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'angularjs = js.angular:library',
+            ],
+        },
+    )
