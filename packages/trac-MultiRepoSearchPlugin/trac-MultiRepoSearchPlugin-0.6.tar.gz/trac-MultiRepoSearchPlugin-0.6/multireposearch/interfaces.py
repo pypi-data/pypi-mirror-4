6306b185b2c855534be9a88a43ce3cb897265a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package trac-MultiRepoSearchPlugin/0.6/trac-MultiRepoSearchPlugin-0.6.tar.gz

---
+++ packages/trac-MultiRepoSearchPlugin/trac-MultiRepoSearchPlugin-0.6.tar.gz/trac-MultiRepoSearchPlugin-0.6/multireposearch/interfaces.py
@@ -0,0 +1,21 @@
+from trac.core import Interface
+
+class IMultiRepoSearchBackend(Interface):
+    """
+    A mechanism that can be queries for full-text search on a single Trac source repository.
+
+    A backend can also optionally provide a indexing facilities.
+    """
+
+    def reindex_repository(reponame):
+        """
+        Reindex a single repository if the backend deems it necessary
+        """
+
+    def find_words(query):
+        """
+        Yield a series of filenames which match the given query.
+
+        A backend is expected to match the query against both file names and file contents.
+        """
+
