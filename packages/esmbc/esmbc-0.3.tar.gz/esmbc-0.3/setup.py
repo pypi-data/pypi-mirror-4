420cf61008b6c3de30bc327d8a80bab263cdfb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:44 +0000

    Add package esmbc/0.3/esmbc-0.3.tar.gz

---
+++ packages/esmbc/esmbc-0.3.tar.gz/esmbc-0.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='esmbc',
+      version='0.3',
+      description='EVE Online Ship Maintenance Bay Calculator',
+      long_description=long_description,
+      license='GPL3',
+      author='Stuart Baker',
+      author_email='sdb@stuartdb.com',
+      url='https://github.com/stuartdb/esmbc',
+      platforms='any',
+      packages=['esmbc'],
+      package_data={'esmbc': ['ships.json']},
+      entry_points={
+        'console_scripts': [
+            'esmbc = esmbc.__main__:main',
+            ],
+        },
+      install_requires=['distribute'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Games/Entertainment'
+        ]
+      )
