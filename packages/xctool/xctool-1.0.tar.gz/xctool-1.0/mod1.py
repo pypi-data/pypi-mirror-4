9c91ac3f6152b6c75258b37b50363763d74054e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package xctool/1.0/xctool-1.0.tar.gz

---
+++ packages/xctool/xctool-1.0.tar.gz/xctool-1.0/mod1.py
@@ -0,0 +1,14 @@
+#! /bin/local/env python
+
+
+def main():
+    """ this is mod1.py module
+    
+    what you want,and what you have?
+    make it with you handle!!!    
+    """
+    print 'mod1.main() is called!'
+
+
+if __name__ == "__main__":
+    main()
