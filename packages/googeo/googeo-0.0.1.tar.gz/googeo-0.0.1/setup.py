d946f0ba20036d5b8a20cb02c6b412253f6569a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:10 +0000

    Add package googeo/0.0.1/googeo-0.0.1.tar.gz

---
+++ packages/googeo/googeo-0.0.1.tar.gz/googeo-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name='googeo',
+
+    version='0.0.1',
+
+    modules=['googeo'],
+
+    # packages=find_packages('src'),
+
+    # include_package_data=True,
+
+    # package_dir={'': 'src'},
+
+    install_requires=[
+    ],
+
+    scripts=[]
+)
