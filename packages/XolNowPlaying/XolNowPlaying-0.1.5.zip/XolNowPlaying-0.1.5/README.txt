51bff93891775de812a4d52e4368431df62d62b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package XolNowPlaying/0.1.5/XolNowPlaying-0.1.5.zip

---
+++ packages/XolNowPlaying/XolNowPlaying-0.1.5.zip/XolNowPlaying-0.1.5/README.txt
@@ -0,0 +1,8 @@
+=============
+XolNowPlaying
+=============
+
+XolNowPlaying provides a Scrobbler class which you can use to get the artist and 
+title of a song the provided user is currently listening to via 
+AudioScrobbler/Last.fm. See the NowPlaying class for an example of writing the 
+data to a file, which can be read by OpenBroadcaster etc.
---
+++ packages/XolNowPlaying/XolNowPlaying-0.1.5.zip/XolNowPlaying-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='XolNowPlaying',
+    version='0.1.5',
+    author='Jonas Svarvaa',
+    author_email='jonassvarvaa@gmail.com',
+    packages=['xolnowplaying'],
+    scripts=['bin/np.py'],
+    url='http://pypi.python.org/pypi/XolNowPlaying',
+    license='LICENSE.txt',
+    description='Read from AudioScrobbler and write to file.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "docopt == 0.6.1",
+    ],
+)
