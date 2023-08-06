915ca595f09efe6a62deba4cbe889c6613789e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:27 +0000

    Add package django-dynamicresponse/0.5.0/django-dynamicresponse-0.5.0.tar.gz

---
+++ packages/django-dynamicresponse/django-dynamicresponse-0.5.0.tar.gz/django-dynamicresponse-0.5.0/dynamicresponse/json_response.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.http import HttpResponse
+
+from dynamicresponse.emitters import JSONEmitter
+
+class JsonResponse(HttpResponse):
+    """
+    Provides a JSON response to a client, performing automatic serialization.
+    """
+    
+    def __init__(self, object=None, **kwargs):
+
+        # Perform JSON serialization
+        if object is not None:
+            emitter = JSONEmitter(object, {}, None)
+            content = emitter.render()
+        else:
+            content = ''
+
+        # Status code for the response
+        status_code = kwargs.get('status', 200)
+
+        # Return response with correct payload/type
+        super(JsonResponse, self).__init__(
+            content,
+            content_type='application/json; charset=%s' % settings.DEFAULT_CHARSET,
+            status=status_code
+        )
---
+++ packages/django-dynamicresponse/django-dynamicresponse-0.5.0.tar.gz/django-dynamicresponse-0.5.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from dynamicresponse import __version__
+from setuptools import setup
+
+def publish():
+    """Publish to Pypi"""
+    os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+    publish()
+    sys.exit()
+
+setup(name='django-dynamicresponse',
+      version=__version__,
+      description='Lightweight framework for easily providing REST APIs for web apps built with Django.',
+      long_description=open('README.md').read(),
+      author='Funkbit AS',
+      author_email='post@funkbit.no',
+      url='https://github.com/funkbit/django-dynamicresponse',
+      packages=['dynamicresponse', 'dynamicresponse.middleware'],
+      tests_require=['django>=1.1,<1.4'],
+      test_suite='examples.myblog.blog.runtests.runtests',
+      license='BSD',
+      classifiers=(
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        )
+     )
