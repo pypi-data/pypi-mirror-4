a6ce9a3cb38bd887df98b28b7fa737f62954e97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:01 +0000

    Add package FireWorks/0.1dev0.5/FireWorks-0.1dev0.5.tar.gz

---
+++ packages/FireWorks/FireWorks-0.1dev0.5.tar.gz/FireWorks-0.1dev0.5/fireworks/tests/master_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+'''
+TODO: add docs!
+'''
+
+
+__author__ = "Anubhav Jain"
+__copyright__ = "Copyright 2013, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Anubhav Jain"
+__email__ = "ajain@lbl.gov"
+__date__ = "Jan 9, 2013"
+
+from unittest import TestCase
+
+#TODO: add test of FireworkSerializers
+
+class TestUnitTest(TestCase):
+    def test_unittest(self):
+        self.assertTrue(True)
