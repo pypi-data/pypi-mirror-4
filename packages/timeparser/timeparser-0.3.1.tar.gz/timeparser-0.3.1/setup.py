dd4a6c6c43fc3c566e28706abba9b748d8995b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:07 +0000

    Add package timeparser/0.3.1/timeparser-0.3.1.tar.gz

---
+++ packages/timeparser/timeparser-0.3.1.tar.gz/timeparser-0.3.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+VERSION = "0.3.1"
+
+setup(
+    name = "timeparser", 
+    version = VERSION, 
+    author = "Thomas Leichtfuss", 
+    author_email = "thomaslfuss@gmx.de",
+    url = "https://github.com/thomst/timeparser",
+    download_url = "https://pypi.python.org/packages/source/t/timeparser/timeparser-{version}.tar.gz".format(version=VERSION),
+    description = 'A python-module to parse strings to time-, date-, datetime- or timedelta-objects. Which formats are accepted is configurable. The module also provides classes to use with the argparse-module for parsing command-line arguments.',
+    long_description = open('README.rst').read() if os.path.isfile('README.rst') else str(),
+    py_modules = ["timeparser"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+    ],
+    license='GPL',
+    keywords='parser parse datetime time strings',
+)
