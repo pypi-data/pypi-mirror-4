1302393d5aea3a0f6c2c9496632e74d58f86ee97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:32 +0000

    Add package emcee/1.2.0/emcee-1.2.0.tar.gz

---
+++ packages/emcee/emcee-1.2.0.tar.gz/emcee-1.2.0/emcee/__init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+from emcee.sampler import *
+from emcee.mh import *
+from emcee.ensemble import *
+from emcee.ptsampler import *
+from emcee import utils
+
+
+__version__ = "1.2.0"
+
+
+def test():
+    from .tests import Tests
+
+    test_names = [
+                  ("Parallel Sampler",    "test_parallel"),
+                  ("Ensemble Sampler",    "test_ensemble"),
+                  ("Metropolis-Hastings", "test_mh"),
+                  ("Parallel Tempering", "test_pt_sampler")
+                 ]
+
+    print("Starting tests...")
+
+    failures = 0
+    tests = Tests()
+    for t in test_names:
+        tests.setUp()
+        try:
+            getattr(tests, t[1])()
+        except Exception as e:
+            print("Test: {0} failed with error:".format(t[0]))
+            print("\t{0}: {1}".format(e.__class__.__name__, e))
+            failures += 1
+        else:
+            print("Test: {0} passed.".format(t[0]))
+
+    print("{0} tests passed".format(len(test_names) - failures))
+    print("{0} tests failed".format(failures))
---
+++ packages/emcee/emcee-1.2.0.tar.gz/emcee-1.2.0/setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+vre = re.compile("__version__ = \"(.*?)\"")
+m = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                      "emcee", "__init__.py")).read()
+version = vre.findall(m)[0]
+
+
+setup(
+    name="emcee",
+    version=version,
+    author="Daniel Foreman-Mackey",
+    author_email="danfm@nyu.edu",
+    packages=["emcee"],
+    url="http://danfm.ca/emcee/",
+    license="GPLv2",
+    description="Kick ass affine-invariant ensemble MCMC sampling",
+    long_description=open("README.rst").read() + "\n\n"
+                    + "Changelog\n"
+                    + "---------\n\n"
+                    + open("HISTORY.rst").read(),
+    package_data={"": ["LICENSE", "AUTHORS.rst"]},
+    include_package_data=True,
+    install_requires=["numpy"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
