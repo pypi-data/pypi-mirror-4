8aaefa328094c6c9a4f1ac1a36b1ae11d3e30cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:36 +0000

    Add package mwlib/0.15.4/mwlib-0.15.4.zip

---
+++ packages/mwlib/mwlib-0.15.4.zip/mwlib-0.15.4/mwlib/_version.py
@@ -0,0 +1,40 @@
+__version_info__ = (0, 15, 4)
+version = __version__ = "0.15.4"
+
+try:
+    from mwlib._gitversion import gitid, gitversion
+except ImportError:
+    gitid = gitversion = ""
+
+display_version = gitversion or version
+
+
+
+def main():
+    msg = "mwlib: %s" % (version or gitversion,)
+    if gitid:
+        msg += "(%s)" % gitid
+    print msg
+
+    try:
+        from mwlib.rl import _gitversion
+        print "mwlib.rl", _gitversion.gitversion
+    except ImportError:
+        try:
+            from mwlib.rl._version import version as rlversion
+            print "mwlib.rl:", rlversion
+        except ImportError:
+            pass
+
+    try:
+        from mwlib._extversion import version as extversion
+        print "mwlib.ext:", extversion
+    except ImportError:
+        pass
+
+    try:
+        from mwlib.hiq import _gitversion
+    except ImportError:
+        pass
+    else:
+        print "mwlib.hiq", _gitversion.gitversion
