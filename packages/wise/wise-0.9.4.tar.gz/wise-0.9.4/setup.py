70950486c9919455e84996af743231dd6478ec3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:58 +0000

    Add package wise/0.9.4/wise-0.9.4.tar.gz

---
+++ packages/wise/wise-0.9.4.tar.gz/wise-0.9.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='wise',
+    version='0.9.4',
+    author='Henrik Brink',
+    author_email='henrik@wise.io',
+    packages=['wise'],
+    url='https://wise.io',
+    license='LICENSE.txt',
+    description='Client library for the wise.io machine-learning service.',
+    install_requires=["requests", "pandas"],
+    scripts = [
+        'scripts/wise.py'
+    ]
+)
