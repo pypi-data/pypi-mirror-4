a211ddc5d34e032ea1fc54be0bdd278ab2d25e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package pyroma/1.0/pyroma-1.0.tar.gz

---
+++ packages/pyroma/pyroma-1.0.tar.gz/pyroma-1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='pyroma',
+      version=version,
+      description="Test your project's packaging friendliness",
+      long_description=open("README.txt", 'rt').read() + "\n" +
+                       open("HISTORY.txt", 'rt').read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        ],
+      keywords='pypi quality testing',
+      author='Lennart Regebro',
+      author_email='regebro@gmail.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      scripts=['scripts/pyroma',],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'docutils',
+          # -*- Extra requirements: -*-
+      ],
+      test_suite='pyroma',
+      use_2to3=True,
+      )
