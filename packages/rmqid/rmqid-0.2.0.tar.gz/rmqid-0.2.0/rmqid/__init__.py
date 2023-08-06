a4dd29c579e4bb4c58363a155a4559a6825cd471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:59 +0000

    Add package rmqid/0.2.0/rmqid-0.2.0.tar.gz

---
+++ packages/rmqid/rmqid-0.2.0.tar.gz/rmqid-0.2.0/rmqid/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.0'
+
+from rmqid.connection import Connection
+from rmqid.exchange import Exchange
+from rmqid.message import Message
+from rmqid.queue import Queue
+from rmqid.tx import TX
