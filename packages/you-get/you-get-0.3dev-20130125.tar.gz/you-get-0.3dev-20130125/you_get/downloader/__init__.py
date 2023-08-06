3b2117c8d8ea4d6319c96ac7198e9dc01513577e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:46 +0000

    Add package you-get/0.3dev-20130125/you-get-0.3dev-20130125.tar.gz

---
+++ packages/you-get/you-get-0.3dev-20130125.tar.gz/you-get-0.3dev-20130125/you_get/downloader/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from .acfun import *
+from .bilibili import *
+from .cntv import *
+from .dailymotion import *
+from .googleplus import *
+from .ifeng import *
+from .iqiyi import *
+from .ku6 import *
+from .netease import *
+from .pptv import *
+from .qq import *
+from .sina import *
+from .sohu import *
+from .soundcloud import *
+from .tudou import *
+from .tumblr import *
+from .vimeo import *
+from .w56 import *
+from .xiami import *
+from .yinyuetai import *
+from .youku import *
+from .youtube import *
