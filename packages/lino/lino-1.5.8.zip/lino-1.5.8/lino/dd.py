3189904279358ee9a03b86dd941104b71dc8101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:13 +0000

    Add package lino/1.5.8/lino-1.5.8.zip

---
+++ packages/lino/lino-1.5.8.zip/lino-1.5.8/lino/dd.py
@@ -0,0 +1,259 @@
+## Copyright 2011-2013 Luc Saffre
+## This file is part of the Lino project.
+## Lino is free software; you can redistribute it and/or modify 
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## Lino is distributed in the hope that it will be useful, 
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+## GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License
+## along with Lino; if not, see <http://www.gnu.org/licenses/>.
+
+"""
+A shortcut to classes and methods needed 
+when defining your database structure in a `models` module.
+
+The name "dd" stands for "Data Definition".
+
+A small wrapper around Django's `Model` class 
+which adds some Lino specific features:
+
+- :class:`Model <lino.core.model.Model>`
+
+Tables:
+
+- :class:`Table <lino.core.dbtables.Table>`
+- :class:`VirtualTable <lino.core.tables.VirtualTable>`
+- :class:`Frame <lino.core.frames.Frame>`
+
+Extended Fields:
+
+- :class:`EnableChild <lino.utils.mti.EnableChild>`
+- :class:`NullCharField <lino.core.fields.NullCharField>`
+- :class:`IncompleteDateField <lino.core.fields.IncompleteDateField>`
+- :class:`PasswordField <lino.core.fields.PasswordField>`
+- :class:`MonthField <lino.core.fields.MonthField>`
+- :class:`PercentageField <lino.core.fields.PercentageField>`
+- :class:`QuantityField <lino.core.fields.QuantityField>`
+- :class:`PriceField<lino.core.fields.PriceField>`
+- :class:`GenericForeignKey <lino.core.fields.GenericForeignKey>`
+- :class:`GenericForeignKeyIdField <lino.core.fields.GenericForeignKeyIdField>`
+- :class:`RecurrenceField <lino.core.fields.RecurrenceField>`
+- :class:`DummyField <lino.core.fields.DummyField>`
+- :func:`ForeignKey <lino.core.fields.ForeignKey>`
+
+Babel fields:
+
+- :class:`BabelNamed <lino.utils.babel.BabelNamed>`
+- :class:`BabelCharField <lino.utils.babel.BabelCharField>`
+- :class:`BabelTextField <lino.utils.babel.BabelTextField>`
+- :class:`LanguageField <lino.utils.babel.LanguageField>`
+
+Virtual Fields:
+
+- :class:`Constant <lino.core.fields.Constant>` and 
+  :class:`@constant <lino.core.fields.constant>`
+- :class:`DisplayField <lino.core.fields.DisplayField>` and 
+  :class:`@displayfield <lino.core.fields.displayfield>`
+- :class:`VirtualField <lino.core.fields.VirtualField>` and
+  :class:`@virtualfield <lino.core.fields.virtualfield>`
+- :class:`HtmlBox <lino.core.fields.HtmlBox>`
+
+Layouts:
+
+- :class:`FormLayout <lino.core.layouts.FormLayout>`
+- :class:`Panel <lino.core.layouts.Panel>`
+  
+Utilities:
+
+- :func:`fields_list <lino.core.fields.fields_list>`
+- :func:`resolve_field <lino.core.modeltools.resolve_field>`
+- :func:`resolve_model <lino.core.modeltools.resolve_model>`
+- :func:`resolve_app <lino.core.modeltools.resolve_app>` 
+- :func:`chooser <lino.utils.choosers.chooser>` 
+- :func:`add_user_group` 
+- :func:`update_field <lino.core.inject.update_field>` 
+- :func:`inject_field <lino.core.inject.inject_field>` 
+- :func:`inject_quick_add_buttons <lino.core.inject.inject_quick_add_buttons>` 
+
+Signals:
+
+- :attr:`pre_startup <lino.core.signals.pre_startup>`
+- :attr:`pre_merge <lino.core.signals.pre_merge>`
+- :attr:`pre_ui_create <lino.core.signals.pre_ui_create>`
+- :attr:`ChangeWatcher <lino.core.signals.ChangeWatcher>`
+- :attr:`pre_ui_update <lino.core.signals.pre_ui_update>`
+- :attr:`pre_ui_delete <lino.core.signals.pre_ui_delete>`
+- :attr:`pre_analyze <lino.core.signals.pre_analyze>`
+- :attr:`receiver <django.dispatch.receiver>` : the standard Django receiver decorator
+
+Actions:
+
+- :class:`AuthorRowAction <lino.mixins.AuthorRowAction>`
+- :class:`RowAction <lino.core.actions.RowAction>`
+- :class:`AjaxAction <lino.core.actions.RowAction>`
+- :class:`ChangeStateAction <lino.core.changes.ChangeStateAction>`
+- :class:`NotifyingAction <lino.core.actions.NotifyingAction>`
+- :class:`MergeAction <lino.core.merge.MergeAction>`
+
+Miscellaneous:
+
+- :class:`ChoiceList <lino.core.choicelists.ChoiceList>`
+- :class:`Workflow <lino.core.workflows.Workflow>`
+
+
+- :class:`UserProfiles <lino.utils.auth.UserProfiles>`
+- :class:`UserGroups <lino.utils.auth.UserGroups>`
+- :class:`UserLevels <lino.utils.auth.UserLevels>`
+
+
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+from lino.core.tables import VirtualTable
+
+from lino.core.modeltools import resolve_model, resolve_app, resolve_field, get_field, UnresolvedModel
+from lino.core.modeltools import full_model_name
+from lino.core.modeltools import models_by_base
+
+from lino.core.model import Model
+from lino.core.merge import MergeAction
+
+#~ from lino.core.table import fields_list, inject_field
+from lino.core.dbtables import has_fk
+from lino.core.dbtables import Table
+from django.db.models.fields import FieldDoesNotExist
+from django.db import models
+from django.conf import settings
+#~ Model = models.Model
+#~ from lino.core import table
+#~ Table = table.Table
+
+from lino.core.dbtables import summary, summary_row
+
+from lino.core.frames import Frame
+from lino.core.dialogs import Dialog
+
+from lino.core.actions import action
+#~ from lino.core.actions import Action
+from lino.core.actions import RowAction
+AjaxAction = RowAction
+
+from lino.mixins import AuthorRowAction
+#~ from lino.core.actions import ListAction
+from lino.core.actions import GridEdit, ShowDetailAction
+from lino.core.actions import InsertRow, DeleteSelected
+from lino.core.actions import SubmitDetail, SubmitInsert
+#~ from lino.core.actions import Calendar
+
+from lino.core.choicelists import ChoiceList, Choice
+from lino.core.workflows import Workflow, ChangeStateAction
+#~ from lino.core.changes import ChangeStateAction
+from lino.core.actions import NotifyingAction
+
+
+from lino.core.fields import fields_list
+from lino.core.fields import DummyField
+from lino.core.fields import RecurrenceField
+from lino.core.fields import GenericForeignKey
+from lino.core.fields import GenericForeignKeyIdField
+from lino.core.fields import IncompleteDateField
+from lino.core.fields import DatePickerField
+from lino.core.fields import NullCharField
+from lino.core.fields import PasswordField
+from lino.core.fields import MonthField
+from lino.core.fields import PercentageField
+#~ from lino.core.fields import LinkedForeignKey
+from lino.core.fields import QuantityField
+from lino.core.fields import HtmlBox, PriceField, RichTextField
+
+from lino.core.fields import DisplayField, displayfield
+from lino.core.fields import VirtualField, virtualfield
+from lino.core.fields import RequestField, requestfield
+from lino.core.fields import Constant, constant
+from lino.core.fields import ForeignKey
+
+from lino.utils.babel import BabelCharField, BabelTextField, BabelNamed, LanguageField
+#~ from lino.core.fields import MethodField
+
+from lino.utils.choosers import chooser
+from lino.utils.mti import EnableChild
+
+from lino.utils.auth import UserLevels, UserProfiles, UserGroups
+
+#~ from lino.base.utils import UserLevels, UserGroups, UserProfiles
+
+from lino.core.layouts import FormLayout, Panel
+from lino.core.layouts import ParamsLayout
+#~ from lino.core.layouts import ActionParamsLayout
+#~ DetailLayout = InsertLayout = FormLayout
+
+#~ from lino.core.layouts import DetailLayout, InsertLayout
+
+
+from lino.core.signals import pre_ui_create, pre_ui_delete, pre_ui_update, ChangeWatcher
+from lino.core.signals import pre_analyze
+from lino.core.signals import post_analyze
+
+from lino.core.signals import auto_create
+from lino.core.signals import pre_merge
+from lino.core.signals import pre_add_child
+from lino.core.signals import pre_remove_child
+
+from django.db.models.signals import class_prepared
+from django.db.models.fields import NOT_PROVIDED
+
+from django.dispatch import receiver
+#~ from lino.core import signals
+
+
+#~ class Module(object):
+    #~ pass
+
+from lino.core.inject import inject_field
+from lino.core.inject import update_field
+from lino.core.inject import inject_quick_add_buttons
+from lino.core.inject import do_when_prepared, when_prepared
+ 
+  
+    
+def add_user_group(name,label):
+    """
+    Add a user group to the :class:`UserGroups <lino.core.perms.UserGroups>` 
+    choicelist. If a group with that name already exists, add `label` to the 
+    existing group.
+    """
+    #~ logging.info("add_user_group(%s,%s)",name,label)
+    #~ print "20120705 add_user_group(%s,%s)" % (name,unicode(label))
+    g = UserGroups.items_dict.get(name)
+    if g is None:
+        UserGroups.add_item(name,label)
+    else:
+        if g.text != label:
+            g.text += " & " + unicode(label)
+    
+def required(**kw):
+    #~ if not kw.has_key('auth'):
+        #~ kw.update(auth=True)
+    kw.setdefault('auth',True)
+    return kw
+    
+    
+class PseudoRequest:
+    def __init__(self,username):
+        self.username = username
+        self._user = None
+        
+    def get_user(self):
+        if self._user is None:
+            if settings.LINO.user_model is not None:
+                self._user = settings.LINO.user_model.objects.get(username=self.username)
+        return self._user
+    user = property(get_user)
+    
+
---
+++ packages/lino/lino-1.5.8.zip/lino-1.5.8/lino/modlib/accounts/utils.py
@@ -0,0 +1,221 @@
+# -*- coding: UTF-8 -*-
+## Copyright 2012 Luc Saffre
+## This file is part of the Lino project.
+## Lino is free software; you can redistribute it and/or modify 
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## Lino is distributed in the hope that it will be useful, 
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+## GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License
+## along with Lino; if not, see <http://www.gnu.org/licenses/>.
+
+ur"""
+Lino has a hard-coded list of the five 
+basic "account types" or "top-level accounts".
+
+An accounting transaction is either Debit or Credit.
+We represent this internally as a boolean, but define two names DEBIT and CREDIT:
+
+>>> DEBIT
+True
+>>> CREDIT
+False
+
+
+
+Accounting Equation:
+
+  Assets = Liabilities + Capital
+ 
+Expanded accounting equation: 
+
+    Assets + Expenses = Liabilities + Equity + Revenue
+    
+Accounts on the left side of the equation (Assets and Expenses) 
+are normally DEBITed and have DEBIT balances. 
+
+That's what the :attr:`dc <AccountType.dc>` attribute means.
+
+
+>>> print unicode(DC[AccountTypes.assets.dc])
+Debit
+>>> print unicode(DC[AccountTypes.expenses.dc])
+Debit
+
+>>> print isinstance(AccountTypes.bank_accounts,Assets)
+True
+
+
+  
+`Wikipedia <http://en.wikipedia.org/wiki/Debits_and_credits>`_ gives a 
+Summary table of standard increasing and decreasing attributes for the five 
+accounting elements:
+
+  ============= ===== ======
+  ACCOUNT TYPE	DEBIT	CREDIT
+  ============= ===== ======
+  Asset	        +	    −
+  Liability	    −	    +
+  Income	      −	    +
+  Expense	      +	    −
+  Equity	      −	    +      
+  ============= ===== ======
+  
+The equivalent in Lino code is:
+
+>>> for t in AccountTypes.filter(top_level=True): #doctest: +NORMALIZE_WHITESPACE
+...     print "%-12s|%-15s|%-6s" % (t.name, unicode(t), DC[t.dc])
+assets      |Assets         |Debit
+liabilities |Liabilities    |Credit
+incomes     |Incomes        |Credit
+expenses    |Expenses       |Debit
+capital     |Capital        |Credit
+
+Yes.
+
+
+TODO
+----
+
+- The Belgian and French PCMN has 7+1 top-level accounts:
+
+    | CLASSE 0 : Droits & engagements hors bilan
+    | CLASSE 1 : Fonds propres, provisions pour risques & charges et Dettes à plus d'un an
+    | CLASSE 2 : Frais d'établissement, actifs immobilisés et créances à plus d'un an
+    | CLASSE 3 : Stock & commandes en cours d'exécution
+    | CLASSE 4 : Créances et dettes à un an au plus
+    | CLASSE 5 : Placements de trésorerie et valeurs disponibles
+    | CLASSE 6 : Charges
+    | CLASSE 7 : Produits
+    
+
+  explain the differences and how to solve this.
+  See also 
+
+  - http://code.gnucash.org/docs/help/acct-types.html
+  - http://www.futureaccountant.com/accounting-process/study-notes/financial-accounting-account-types.php
+  
+
+- A Liability is Capital acquired from others. 
+  Both together is what French accountants call *passif*.
+  
+  The accounting equation "Assets = Liabilities + Capital" 
+  in French is simply:
+
+      Actif = Passif
+      
+  I found an excellent definition of these two terms at 
+  `plancomptable.com <http://www.plancomptable.com/titre-II/titre-II.htm>`_:
+
+  - Un actif est un élément identifiable du patrimoine ayant une valeur économique positive pour l’entité, c’est-à-dire un élément générant une ressource que l’entité contrôle du fait d’événements passés et dont elle attend des avantages économiques futurs.
+  
+  - Un passif est un élément du patrimoine ayant une valeur économique négative pour l'entité, c'est-à-dire une obligation de l'entité à l'égard d'un tiers dont il est probable ou certain qu'elle provoquera une sortie des ressources au bénéfice de ce tiers, sans contrepartie au moins équivalente attendue de celui-ci. 
+  
+
+Some vocabulary
+
+- Provisions pour risques et charges : Gesetzliche Rücklagen.
+- Créances et dettes : Kredite, Anleihen, Schulden.
+
+
+
+"""
+
+from django.utils.translation import ugettext_lazy as _
+#~ from lino.utils.choicelists import Choice,ChoiceList
+from lino import dd
+
+DEBIT = True
+CREDIT = False
+
+DC = { 
+  DEBIT: _("Debit"), 
+  CREDIT: _("Credit") 
+}
+
+
+class AccountType(dd.Choice):
+    top_level = True
+    #~ def __init__(self,value,text,name,dc=True,**kw):
+        #~ self.dc = dc
+        #~ super(AccountType,self).__init__(value,text,name)
+    def __init__(self):
+        pass
+        #~ self.dc = dc
+        #~ super(AccountType,self).__init__(value,text,name)
+        
+class Assets(AccountType):
+    value = 'A'
+    text = _("Assets")   # Aktiva, Anleihe, Vermögen, Anlage
+    name  = "assets"
+    dc = DEBIT
+
+class Liabilities(AccountType):
+    value = 'L'
+    text = _("Liabilities") # Guthaben, Schulden, Verbindlichkeit  
+    name  = "liabilities"
+    dc = CREDIT
+
+class Income(AccountType):
+    value = 'I'
+    text = _("Incomes") # Gain/Revenue     Einnahmen  Produits
+    name  = "incomes" 
+    dc = CREDIT
+
+class Expenses(AccountType):
+    value = 'E' 
+    text = _("Expenses") # Loss/Cost       Ausgaben   Charges
+    name = "expenses"
+    dc = DEBIT
+
+class Capital(AccountType):
+    value = 'C' 
+    text = _("Capital") # Kapital owner's Equities
+    name = "capital"
+    dc = CREDIT
+
+class BankAccounts(Assets):
+    top_level = False
+    value = 'B'
+    text = _("Bank accounts")
+    name = 'bank_accounts'
+    #~ dc = CREDIT    
+    
+    
+    
+    
+class AccountTypes(dd.ChoiceList):
+    verbose_name = _("Account Type")
+    item_class = AccountType
+    
+    
+add = AccountTypes.add_item
+#~ def add(*args):
+    #~ AccountTypes.add_item_instance(AccountType(*args))
+add = AccountTypes.add_item_instance
+add(Assets())
+add(Liabilities())
+add(Income())
+add(Expenses())
+add(Capital())
+add(BankAccounts())
+#~ add('A', _("Assets"),"asset",DEBIT)   # Aktiva, Anleihe, Vermögen, Anlage
+#~ add('L', _("Liabilities"),"liability",CREDIT) # Guthaben, Schulden, Verbindlichkeit
+#~ add('I', _("Incomes"),"income",CREDIT) # Gain/Revenue     Einnahmen  Produits
+#~ add('E', _("Expenses"),"expense",DEBIT) # Loss/Cost       Ausgaben   Charges
+#~ add('C', _("Capital"),"capital",CREDIT)  # Kapital owner's Equities
+
+
+#~ AccountTypes.add_item_instance(BankAccounts())
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
+
