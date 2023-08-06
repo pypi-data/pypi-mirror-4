289dcc6fa7648a5038d674a28f34ca09a83e8314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:51 +0000

    Add package pql/0.0.1/pql-0.0.1.tar.gz

---
+++ packages/pql/pql-0.0.1.tar.gz/pql-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='pql',
+      version='0.0.1',
+      description='A python expression to MongoDB query translator',
+      author='Alon Horev',
+      author_email='alon@horev.net',
+      url='https://github.com/alonho/pql',
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Programming Language :: Python :: 2.6",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3.3",
+                     "Operating System :: POSIX :: Linux",
+                     "Operating System :: MacOS :: MacOS X"],
+      license='BSD',
+      py_modules=['pql'])
