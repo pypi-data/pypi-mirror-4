fdbe7b6b08286219b6b09a3154eb90c871f20c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package pythonpackages.sendpickedversions/0.0.1/pythonpackages.sendpickedversions-0.0.1.zip

---
+++ packages/pythonpackages.sendpickedversions/pythonpackages.sendpickedversions-0.0.1.zip/pythonpackages.sendpickedversions-0.0.1/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+version = "0.0.1"
+
+entry_point = 'pythonpackages.sendpickedversions:install'
+entry_points = {"zc.buildout.extension": ["default = %s" % entry_point]}
+requires = ['setuptools', 'zc.buildout']
+
+if sys.version[:3] < '2.6':
+    requires.append('simplejson')
+
+tests_require=['zc.buildout', 'zope.testing', 'zc.recipe.egg']
+
+setup(name='pythonpackages.sendpickedversions',
+      version=version,
+      description="Sends picked packages and versions to a whiskers server.",
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      license='GPL',
+      keywords='buildout extension send picked versions',
+      author='Alex Clark (fork author)',
+      author_email='aclark@aclark.net',
+#      author='Jukka Ojaniemi',
+#      author_email='jukka.ojaniemi@jyu.fi',
+      url='http://github.com/pythonpackages/pythonpackages.sendpickedversions',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages=['pythonpackages'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'pythonpackages.sendpickedversions.tests.test_suite',
+      entry_points=entry_points,
+      )
