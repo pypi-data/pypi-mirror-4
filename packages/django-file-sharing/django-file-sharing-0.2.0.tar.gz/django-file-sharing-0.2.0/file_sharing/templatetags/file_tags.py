7e6ebdfa1e41d80cede5ea27eb018af507b609c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:43 +0000

    Add package django-file-sharing/0.2.0/django-file-sharing-0.2.0.tar.gz

---
+++ packages/django-file-sharing/django-file-sharing-0.2.0.tar.gz/django-file-sharing-0.2.0/file_sharing/templatetags/file_tags.py
@@ -0,0 +1,13 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def get_icon(mime):
+    if mime.startswith('image'):
+        return 'icon-picture'
+    elif mime.startswith('text'):
+        return 'icon-file'
+    elif mime.startswith('video'):
+        return 'icon-film'
+    
