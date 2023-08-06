e95432cd0833a354db10c8d5cb752cf6a5c6eef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:12 +0000

    Add package shell-pype/0.1/shell-pype-0.1.1.tar.gz

---
+++ packages/shell-pype/shell-pype-0.1.1.tar.gz/shell-pype-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+setup(name='shell-pype',
+      version='0.1.1',
+      description='Tiny helper for command-line python',
+      author='jisaacstone',
+      author_email='jisaacstone+python@gmail.com',
+      license='MIT',
+      url='https://github.com/jisaacstone/pype',
+      scripts=['pype'],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Topic :: Utilities",
+                   "License :: OSI Approved :: MIT License"],
+     )
