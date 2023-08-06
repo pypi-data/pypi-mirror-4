263b86038082540be49935719b6e07b782d7e822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package pyenest/0.5/pyenest-0.5.zip

---
+++ packages/pyenest/pyenest-0.5.zip/pyenest-0.5/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+readme = open('README.txt').read()
+setup(name='pyenest',
+      version='0.5',
+      description='Python API for Nest Thermostat - Edited',
+      author='Eugene Efremov',
+      author_email='eaefremov@gmail.com',
+      url='http://www.github.com/eae',
+      packages=find_packages())
