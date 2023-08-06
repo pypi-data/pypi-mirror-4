6c3895969a8897337a4a1c793afae8559f765be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:37 +0000

    Add package shaape/1.0.0/shaape-1.0.0.tar.gz

---
+++ packages/shaape/shaape-1.0.0.tar.gz/shaape-1.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='shaape',
+      version='1.0.0',
+      description='Shaape - ascii art to image converter',
+      author='Christian Goltz',
+      author_email='goltzchristian@googlemail.com',
+      package_data = {'shaape':['data']},
+      packages=['shaape'],
+      scripts=['bin/shaape'],
+      license='LICENSE',
+      url='http://github.com/christiangoltz/shaape',
+      requires=[
+          'networkx',
+          'cairo',
+          'scipy',
+          'numpy',
+          'PIL',
+          'yaml']
+     )
