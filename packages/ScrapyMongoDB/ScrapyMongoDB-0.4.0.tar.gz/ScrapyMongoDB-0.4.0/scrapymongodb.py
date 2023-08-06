56af3f05695f3f5f60fa45eff41ecfca0bfbda4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package ScrapyMongoDB/0.4.0/ScrapyMongoDB-0.4.0.tar.gz

---
+++ packages/ScrapyMongoDB/ScrapyMongoDB-0.4.0.tar.gz/ScrapyMongoDB-0.4.0/scrapymongodb.py
@@ -0,0 +1,57 @@
+# Copyright 2011 Julien Duponchelle <julien@duponchelle.info>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MongoDB Pipeline for scrapy"""
+
+import pymongo
+from scrapy.conf import settings
+from scrapy import log
+
+MONGODB_SAFE = False
+MONGODB_ITEM_ID_FIELD = "_id"
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        self.db = connection[settings['MONGODB_DB']]
+        self.collection = self.db[settings['MONGODB_COLLECTION']]
+        self.uniq_key = settings.get('MONGODB_UNIQ_KEY', None)
+        self.itemid = settings.get('MONGODB_ITEM_ID_FIELD', 
+            MONGODB_ITEM_ID_FIELD)
+        self.safe = settings.get('MONGODB_SAFE', MONGODB_SAFE)
+
+        if isinstance(self.uniq_key, basestring) and self.uniq_key == "":
+            self.uniq_key = None
+            
+        if self.uniq_key:
+            self.collection.ensure_index(self.uniq_key, unique=True)
+
+    def process_item(self, item, spider):
+        if self.uniq_key is None:
+            result = self.collection.insert(dict(item), safe=self.safe)
+        else:
+            result = self.collection.update(
+                            {self.uniq_key: item[self.uniq_key]},
+                            { '$set': dict(item) },
+                            upsert=True, safe=self.safe)
+
+        # If item has _id field and is None
+        if self.itemid in item.fields and not item.get(self.itemid, None):
+            item[self.itemid] = result
+
+        log.msg("Item %s wrote to MongoDB database %s/%s" %
+                    (result, settings['MONGODB_DB'], 
+                    settings['MONGODB_COLLECTION']),
+                    level=log.DEBUG, spider=spider)  
+        return item
---
+++ packages/ScrapyMongoDB/ScrapyMongoDB-0.4.0.tar.gz/ScrapyMongoDB-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(name='ScrapyMongoDB',
+      version='0.4.0',
+      license='Apache License, Version 2.0',
+      description='Scrapy pipeline which allow you to store scrapy items in MongoDB database.',
+      author='Julien Duponchelle',
+      author_email='julien@duponchelle.info',
+      url='http://github.com/noplay/scrapy-mongodb',
+      keywords="scrapy mongodb",
+      py_modules=['scrapymongodb'],
+      platforms = ['Any'],
+      install_requires = ['scrapy', 'pymongo'],
+      classifiers = [ 'Development Status :: 4 - Beta',
+                      'Environment :: No Input/Output (Daemon)',
+                      'License :: OSI Approved :: Apache Software License',
+                      'Operating System :: OS Independent',
+                      'Programming Language :: Python']
+)
