9365228d369d20f833add67c33a95b64da77acfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:33 +0000

    Add package pyramid_scss2/0.2.2/pyramid_scss2-0.2.2.tar.gz

---
+++ packages/pyramid_scss2/pyramid_scss2-0.2.2.tar.gz/pyramid_scss2-0.2.2/CHANGES.rst
@@ -0,0 +1,28 @@
+============
+Change Log
+============
+
+0.1
+============
+*1 Feb 2012*
+
+- Initial public release.
+
+0.1.1
+------------
+*3 Feb 2012*
+
+- Fixed `issue #1 <https://github.com/jessedhillon/pyramid_scss/issues/1>`_: @import could not resolve paths relative to ``asset_path``.
+
+0.2
+------------
+*19 Feb 2012*
+
+- Fixed `issue #3 <https://github.com/jessedhillon/pyramid_scss/issues/3>`_: caching implementation was incomplete/incorrect.
+- Added ``static_path`` configuration for indicating location of static assets.
+
+0.2.2
+-----
+*30 Jan 2013*
+
+- Complete upload of code that actually fixes `issue #3 <https://github.com/jessedhillon/pyramid_scss/issues/3>`_.
---
+++ packages/pyramid_scss2/pyramid_scss2-0.2.2.tar.gz/pyramid_scss2-0.2.2/pyramid_scss/__init__.py
@@ -0,0 +1,90 @@
+import os
+import logging
+
+from zope.interface import implements
+from zope.interface import Interface
+
+from pyramid.interfaces import ITemplateRenderer
+from pyramid.exceptions import ConfigurationError
+from pyramid.resource import abspath_from_asset_spec
+
+import scss
+from scss import Scss
+
+Logger = logging.getLogger('pyramid_scss')
+
+def as_bool(s, default=False):
+    if isinstance(s, bool):
+        return s
+
+    if isinstance(s, basestring):
+        if s.lower() in ['true', 'yes', 'on', '1']:
+            return True
+
+        if s.lower() in ['false', 'no', 'off', '0']:
+            return False
+
+    return default
+
+def prefixed_keys(d, prefix):
+    return dict([(k.replace(prefix, ''), v) for k, v in d.items() if k.startswith(prefix)])
+
+def _get_import_paths(settings):
+    if 'scss.asset_path' not in settings:
+        raise ConfigurationError('SCSS renderer requires ``scss.asset_path`` setting')
+
+    load_paths = []
+    for s in settings.get('scss.asset_path').split('\n'):
+        if s:
+            p = abspath_from_asset_spec(s.strip())
+            load_paths.append(p)
+            Logger.info('adding asset path %s', p)
+
+    static_path = settings.get('scss.static_path', '')
+    if static_path:
+        static_path = abspath_from_asset_spec(static_path.strip())
+        Logger.info('setting static path %s', static_path)
+
+    return (load_paths, static_path)
+
+def renderer_factory(info):
+    settings = prefixed_keys(info.settings, 'scss.')
+
+    options = {
+        'compress': settings.get('compress', False),
+        'cache': settings.get('cache', False),
+    }
+
+    options = dict((k, as_bool(v)) for k, v in options.items())
+    return ScssRenderer(info, options)
+
+class ScssRenderer(object):
+    implements(ITemplateRenderer)
+    cache = None
+
+    def __init__(self, info, options):
+        self.cache = {}
+        self.info = info
+        self.options = options
+
+    def __call__(self, scss, system):
+        parser = Scss(scss_opts=self.options)
+
+        if 'request' in system:
+            request = system.get('request')
+            request.response_content_type = 'text/css'
+            key = request.matchdict.get('css_path')
+
+            if not self.options.get('cache', False) or key not in self.cache:
+                Logger.info('generating %s', key)
+                self.cache[key] = parser.compile(scss)
+
+            return self.cache.get(key)
+
+        return parser.compile(scss)
+
+def includeme(config):
+    load_paths, static_path = _get_import_paths(config.registry.settings)
+    scss.LOAD_PATHS = ','.join([scss.LOAD_PATHS, ','.join(load_paths)])
+    scss.STATIC_ROOT = static_path
+    config.add_renderer('scss', renderer_factory)
