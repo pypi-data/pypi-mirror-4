d0dcf7d97830edff6981a7a118b2c28e376e9d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:32 +0000

    Add package wtframework/0.0.10/wtframework-0.0.10.tar.gz

---
+++ packages/wtframework/wtframework-0.0.10.tar.gz/wtframework-0.0.10/wtframework/wtf/_devtools_/filetemplates/_runtests_py_.py
@@ -0,0 +1,37 @@
+content = \
+'''#!/usr/bin/env python
+"""
+Created on Feb 5, 2013
+
+@author: "David Lai"
+"""
+
+import os
+from optparse import OptionParser
+from wtframework.wtf.utils.ProjectUtils import ProjectUtils
+from wtframework.wtf.config.ConfigReader import ConfigReader
+
+if __name__ == '__main__':
+
+    usage = "usage: %prog [-options args]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-c", "--config", dest="config",
+                  help="Config to use (without the .yaml suffix)", metavar="FILE")
+    (options, args) = parser.parse_args()
+
+
+    if options.config:
+        # check if config exists.
+        if os.path.exists(ProjectUtils.get_project_root() + \
+                          ConfigReader.CONFIG_LOCATION + options.config +\
+                          ConfigReader.CONFIG_EXT):
+            print "Setting config WTF_ENV to:", options.config
+            os.putenv(ConfigReader.ENV_VARS, options.config)
+        else:
+            print "Cannot find config: ", ProjectUtils.get_project_root() + \
+                          ConfigReader.CONFIG_LOCATION + options.config +\
+                          ConfigReader.CONFIG_EXT
+                          
+    os.system("nosetests tests/tests/ --with-nosexunit --core-target=reports")
+
+'''
