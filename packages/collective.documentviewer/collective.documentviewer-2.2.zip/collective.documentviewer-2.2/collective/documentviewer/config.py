2ee7029b02de922fd392c956b3f0c53a8c67861a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package collective.documentviewer/2.2/collective.documentviewer-2.2.zip

---
+++ packages/collective.documentviewer/collective.documentviewer-2.2.zip/collective.documentviewer-2.2/collective/documentviewer/config.py
@@ -0,0 +1,38 @@
+
+
+class DocType(object):
+
+    def __init__(self, name, extensions, requires_conversion=True):
+        self.name = name
+        self.extensions = extensions
+        self.requires_conversion = requires_conversion
+
+
+CONVERTABLE_TYPES = {
+    'pdf': DocType(u'PDF', ('pdf',), False),
+    'word': DocType(u'Word Document', ('doc', 'docx', 'odt', 'sxw',
+                                       'wks', 'wpd', 'vor', 'sdw')),
+    'excel': DocType(u'Excel File', ('xls', 'xlsx', 'xlt', 'ods')),
+    'ppt': DocType(u'Powerpoint', ('ppt', 'pptx', 'pps', 'ppa', 'pwz',
+                                  'odp', 'sxi')),
+    'html': DocType(u'HTML File', ('htm', 'html', 'xhtml')),
+    'rft': DocType(u'RTF', ('rtf',)),
+    'ps': DocType(u'PS Document', ('ps', 'eps', 'ai')),
+    'photoshop': DocType(u'Photoshop', ('psd',)),
+    'visio': DocType(u'Visio', ('vss', 'vst', 'vsw', 'vsd')),
+    'palm': DocType(u'Aportis Doc Palm', ('pdb',)),
+}
+
+EXTENSION_TO_ID_MAPPING = {}
+
+for id, doc in CONVERTABLE_TYPES.items():
+    for ext in doc.extensions:
+        EXTENSION_TO_ID_MAPPING[ext] = id
+
+
+GROUP_VIEW_DISPLAY_TYPES = (
+    'Folder',
+    'Large Plone Folder',
+    'Plone Site',
+    'Topic'
+)
