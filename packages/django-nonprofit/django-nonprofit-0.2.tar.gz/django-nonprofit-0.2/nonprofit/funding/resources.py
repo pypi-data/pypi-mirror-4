c3cad63d66a0f899995c920efe64f7c0e8ddcd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:23 +0000

    Add package django-nonprofit/0.2/django-nonprofit-0.2.tar.gz

---
+++ packages/django-nonprofit/django-nonprofit-0.2.tar.gz/django-nonprofit-0.2/nonprofit/funding/resources.py
@@ -0,0 +1,14 @@
+from tastypie.resources import ModelResource
+from nonprofit.funding.models import Contribution, Grant
+
+
+class ContributionResource(ModelResource):
+    class Meta:
+        queryset = Contribution.objects.all()
+        resource_name = 'contribution'
+
+
+class GrantResource(ModelResource):
+    class Meta:
+        queryset = Grant.objects.all()
+        resource_name = 'grant'
