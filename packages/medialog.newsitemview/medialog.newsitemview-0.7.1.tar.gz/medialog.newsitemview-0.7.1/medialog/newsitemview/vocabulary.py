f0dfa0ee1eb639f9cb394fc5c65c992721b8daee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:04 +0000

    Add package medialog.newsitemview/0.7.1/medialog.newsitemview-0.7.1.tar.gz

---
+++ packages/medialog.newsitemview/medialog.newsitemview-0.7.1.tar.gz/medialog.newsitemview-0.7.1/medialog/newsitemview/vocabulary.py
@@ -0,0 +1,37 @@
+#from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+#from Products.CMFCore.utils import getToolByName
+#from zope.component import getUtility 
+#from zope.schema.interfaces import IVocabularyFactory 
+from zope.app.component.hooks import getSite
+#from zope.interface import alsoProvides 
+
+from five import grok
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+from Products.CMFCore.utils import getToolByName
+
+
+def format_size(size):
+    return size.split(' ')[0]
+ 
+
+
+@grok.provider(IContextSourceBinder)
+def ImageSizeVocabulary(self, context):
+    site = self.getSite()
+    sizes = ['thumb', 'thumb', 'mini', 'preview', 'large', 'none'] 
+    portal_properties = getToolByName(site, 'portal_properties', None)
+    if 'imaging_properties' in portal_properties.objectIds():
+        sizes = portal_properties.imaging_properties.getProperty(
+        'allowed_sizes')
+    terms = [SimpleTerm(value=format_size(pair),
+            token=format_size(pair),
+            title=pair) for pair in sizes]
+            
+    return SimpleVocabulary(terms)
+
+
+
+
+
+ 
