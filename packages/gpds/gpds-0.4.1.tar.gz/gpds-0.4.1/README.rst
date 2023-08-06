8cc4fd1fd43369c2390bc4f0143e62cd7b4139a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:04 +0000

    Add package gpds/0.4.1/gpds-0.4.1.tar.gz

---
+++ packages/gpds/gpds-0.4.1.tar.gz/gpds-0.4.1/README.rst
@@ -0,0 +1,90 @@
+====================
+ GPDS version 0.4.1
+====================
+
+Simple GET-PUT-DELETE service (*GPDS*) to (temporary) store your files as an alternative to NFS shared folders.
+
+It uses Gunicorn_ WSGI server as base and pass through all it's options. So you can simply start it using next command ``gpds WORKING-DIRECTORY``, where *working directory* is path to store and read files (by default, it'll be started on 8000 port).
+
+.. _Gunicorn: http://gunicorn.org/
+
+This server can process only three request types, so try them with ``curl`` from your console::
+
+    curl -i -X PUT -T somefile.jpg http://localhost:8000/
+
+    HTTP/1.1 201 Created
+    Server: gpds/0.3.1
+    Date: Mon, 04 Feb 2013 16:06:27 GMT
+    Connection: close
+    Location: /55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg
+    Content-Length: 0
+
+So, what can we see here. GPDS created new file on yours file system (201 Created HTTP response) and this file is accessible on next address ``http://localhost:8000/55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg``. This big string *551...551* is SHA1 hash from uploaded file content. So we can do next trick::
+
+    curl -i -X PUT -T somefile.txt http://localhost:8000/
+
+    HTTP/1.1 200 OK
+    Server: gpds/0.3.1
+    Date: Mon, 04 Feb 2013 16:17:12 GMT
+    Connection: close
+    Location: /55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg
+    Content-Length: 0
+
+As you can see, ``Location`` is the same as in previous request. GPDS won't rewrite uploaded file.
+
+OK, let's try to access this file::
+
+    curl -i http://localhost:8000/55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg
+
+    HTTP/1.1 200 OK
+    Server: gpds/0.3.1
+    Date: Mon, 04 Feb 2013 16:38:26 GMT
+    Connection: close
+    Content-Length: 56737
+    Content-Type: image/jpeg
+
+    ...file content...
+
+Cool, our service sent our image for us properly. Now we'll delete this file and try to access it one more time::
+
+    curl -i -X DELETE http://localhost:8000/55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg
+
+    HTTP/1.1 204 No Content
+    Server: gpds/0.3.1
+    Date: Mon, 04 Feb 2013 16:40:49 GMT
+    Connection: close
+    Content-Length: 0
+
+    curl -i http://localhost:8001/55/1c/551ced9e9d54658155ae8b4197afd32087a1c551.jpg
+
+    HTTP/1.1 404 Not Found
+    Server: gpds/0.3.1
+    Date: Mon, 04 Feb 2013 16:41:09 GMT
+    Connection: close
+    Content-Length: 0
+
+As you can see GPDS successfully erased your file from disk and next time sent not found response for you.
+
+Ok, now we'll try to do previous work with Python:
+
+.. code-block:: python
+
+    from requests import put, get, delete
+
+    filename = 'somefile.jpg'
+    server = 'http://localhost:8000'
+    r = put('%s/%s' % (server, filename), data=open(filename, 'rb'))
+    result = ''.join([server, r.headers['Location']])
+    print result # http://localhost:8000/2c/f3/2cf3af58d2416d40e18ca5e38f5baabc8a092765.jpg
+
+    r = get(result)
+    print r.headers['Content-Type'] # image/jpeg (our image sent for us)
+
+    for _ in range(2):
+        r = delete(result)
+        print r.status_code # 1st time - 204 (No Content = deleted), 
+                            # 2nd time - 404 (Not Found = already deleted)
+
+As you can see, we used requests_ library, best way to do HTTP request in Python.
+
+.. _requests: http://docs.python-requests.org/en/latest/
---
+++ packages/gpds/gpds-0.4.1.tar.gz/gpds-0.4.1/gpds.py
@@ -0,0 +1,144 @@
+import sys
+import gunicorn
+
+from os.path import abspath, join, splitext, isfile, getsize, isdir
+from os import makedirs, unlink, getcwd
+
+from tempfile import NamedTemporaryFile
+from string import hexdigits
+from errno import EEXIST
+from hashlib import sha1
+from shutil import move
+
+from gunicorn.app.wsgiapp import WSGIApplication
+
+from sh import file
+
+version_info = (0, 4, 1)
+__version__ = '.'.join(map(str, version_info))
+__server__ = '%s/%s' % (__name__, __version__)
+
+gunicorn.SERVER_SOFTWARE = __server__
+
+class GPDS(object):
+    basepath = '.'
+
+    def __init__(self, environ):
+        self.environ = environ
+
+    def process(self, start_response):
+        http_method = self.environ['REQUEST_METHOD']
+        method = 'method_%s' % http_method
+
+        if hasattr(self, method):
+            return getattr(self, method)(start_response)
+
+    def _check_path(self, input):
+        parts = input.split('/')
+        hash, ext = splitext(parts[-1])
+        output = join(self.basepath, input[1:])
+
+        return len(parts) == 4 and hash.startswith(''.join(parts[1:2])) and \
+           all(c in hexdigits for c in hash) and len(hash) == 40 and isfile(output)
+
+    def _respond_error(self, start_response, error='404 Not Found'):
+        start_response(error, [
+            ('Content-Length', 0)
+        ])
+
+    def method_PUT(self, start_response):
+        input = self.environ['PATH_INFO']
+        ext = splitext(input)[1]
+        hash = sha1()
+
+        temporary = NamedTemporaryFile('wb', suffix=ext, dir=self.basepath, delete=False)
+        for chunk in iter(lambda: self.environ['wsgi.input'].read(32768), b''):
+            hash.update(chunk)
+            temporary.write(chunk)
+        temporary.close()
+
+        if not getsize(temporary.name):
+            unlink(temporary.name)
+            return self._respond_error(start_response, error='400 Bad Request')
+
+        hash = hash.hexdigest()
+        filename = ''.join([hash, ext])
+        directory = join(self.basepath, hash[0:2], hash[2:4])
+        if not isdir(directory):
+            try:
+                makedirs(directory, 0755)
+            except OSError as e:
+                if not isdir(directory) or e.errno != EEXIST: # Some other thread already created this directory
+                    raise
+        output = join(directory, filename)
+
+        response = '201 Created'
+        if not isfile(output):
+            move(temporary.name, output)
+        else:
+            unlink(temporary.name)
+            response = '200 OK'
+
+        start_response(response, [
+            ('Location', output.replace(self.basepath, '')),
+            ('Content-Length', 0)
+        ])
+
+    def method_GET(self, start_response):
+        input = self.environ['PATH_INFO']
+        output = join(self.basepath, input[1:])
+
+        if not self._check_path(input):
+            return self._respond_error(start_response)
+
+        mime = file("-b", "--mime-type", output)
+        start_response('200 OK', [
+            ('Content-Length', getsize(output)),
+            ('Content-Type', mime)
+        ])
+
+        return open(output, 'rb')
+
+    def method_DELETE(self, start_response):
+        input = self.environ['PATH_INFO']
+        output = join(self.basepath, input[1:])
+
+        response = '404 Not Found'
+        if self._check_path(input):
+            unlink(output)
+            response = '204 No Content'
+
+        start_response(response, [
+            ('Content-Length', 0)
+        ])
+
+def main(environ, start_response):
+    return GPDS(environ).process(start_response) or iter([''])
+
+class GpdsApplication(WSGIApplication):
+    def init(self, parser, opts, args):
+        if len(args) != 1:
+            parser.error("No working directory specified.")
+
+        working_dir = args[0]
+        if not isdir(working_dir):
+            try:
+                makedirs(working_dir, 0755)
+            except OSError:
+                parser.error('Can create working directory "%s"' % working_dir)
+        else:
+            try:
+                NamedTemporaryFile(dir=working_dir).close()
+            except OSError:
+                parser.error('Can\'t write any data to the working directory "%s"' % working_dir)
+
+        GPDS.basepath = abspath(working_dir)
+
+        proc = "%s:main" % __name__
+        self.cfg.set("default_proc_name", proc)
+        self.app_uri = proc
+
+        sys.path.insert(0, getcwd())
+
+def run():
+    GpdsApplication("%(prog)s [OPTIONS] DIRECTORY").run()
