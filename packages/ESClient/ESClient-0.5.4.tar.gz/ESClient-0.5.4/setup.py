f44b121eba1f1bfacd56d8d107ceca49d00964b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:30 +0000

    Add package ESClient/0.5.4/ESClient-0.5.4.tar.gz

---
+++ packages/ESClient/ESClient-0.5.4.tar.gz/ESClient-0.5.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='ESClient',
+        version="0.5.4",
+        description='A lightweight Python client for ElasticSearch',
+        author='Erik-Jan van Baaren',
+        author_email='erikjan@gmail.com',
+        url='https://github.com/eriky/ESClient',
+        py_modules=['esclient'],
+        license='New BSD license',
+        keywords = ["elasticsearch", "REST", "JSON"],
+        install_requires = ['requests >= 0.10.0'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+            ],
+        long_description = long_description
+        )
