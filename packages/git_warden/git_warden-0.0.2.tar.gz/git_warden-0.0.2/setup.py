2ae9c2c7f3ba9777d42aadde927ec5ed65a140ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:24 +0000

    Add package git_warden/0.0.2/git_warden-0.0.2.tar.gz

---
+++ packages/git_warden/git_warden-0.0.2.tar.gz/git_warden-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+import git_warden as module
+
+setup(
+    name="git_warden",
+    version=module.__version__,
+    description=module.__doc__,
+    author="Ceasar Bautista",
+    author_email="cbautista2010@gmail.com",
+    url="http://www.github.com/Ceasar/git_warden",
+    py_modules=[module.__name__],
+    install_requires=["fabric"]
+)
