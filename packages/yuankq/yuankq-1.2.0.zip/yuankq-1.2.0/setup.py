f20f9d5a84986f54874ec55ced03e90210a05cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:50 +0000

    Add package yuankq/1.2.0/yuankq-1.2.0.zip

---
+++ packages/yuankq/yuankq-1.2.0.zip/yuankq-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='yuankq',
+    version='1.2.0',
+    py_modules=['tangdy'],
+    author='tangdk',
+    author_email='18951369810@189.cn',
+    url='http://www.headfirstlabs.com',
+    description='A 一个列表打印模块',
+    )
