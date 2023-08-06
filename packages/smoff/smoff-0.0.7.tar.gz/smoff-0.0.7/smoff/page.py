8e9354fe283c5b3046e9f05ba8b4f18c03c06c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:51 +0000

    Add package smoff/0.0.7/smoff-0.0.7.tar.gz

---
+++ packages/smoff/smoff-0.0.7.tar.gz/smoff-0.0.7/smoff/page.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from webmarkupcontainer import WebMarkupContainer
+from markupfromfile import MarkupFromFile
+
+class Page(WebMarkupContainer, MarkupFromFile):
+    """
+    A top-level page. Knows how to render() its component hierarchy into an lxml.etree.Element tree
+    """
+    def render(self, context):
+        tree = self.markupFromFile()
+        self.renderInto(tree, context)
+        return tree
---
+++ packages/smoff/smoff-0.0.7.tar.gz/smoff-0.0.7/smoff/smdjango/smoffview.py
@@ -0,0 +1,21 @@
+from django.views.generic.base import View
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+
+from lxml import etree
+
+class SmoffView(View):
+    """
+    Adapts a Page as a django class-based view.
+    """
+    page_class = None
+    def get(self, *args, **kwargs):
+        context = csrf(self.request)
+        self.page_class.request = self.request
+        ret = HttpResponse(etree.tostring(self.page_class().render(context)))
+        #FIXME: handle multiple tabs etc.
+        if 'post_handler' in context:
+            self.request.session['post_handler'] = context['post_handler']
+        return ret
+    def post(self, *args, **kwargs):
+        return self.request.session.pop('post_handler').handlePost(self.request)
