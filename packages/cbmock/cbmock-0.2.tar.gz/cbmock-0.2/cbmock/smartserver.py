ed3a9181343640411bfdf7425a3465336e8cb8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:47 +0000

    Add package cbmock/0.2/cbmock-0.2.tar.gz

---
+++ packages/cbmock/cbmock-0.2.tar.gz/cbmock-0.2/cbmock/smartserver.py
@@ -0,0 +1,46 @@
+import logging
+import logging.config
+
+from twisted.web.resource import Resource
+
+from httpmock import HttpMockServer
+
+
+logging.config.fileConfig('logging.conf')
+log = logging.getLogger()
+
+
+class SmartServer(Resource):
+    isLeaf = True
+
+    def __init__(self):
+        log.info('Started smart server on port 8080')
+
+    def __del__(self):
+        log.info('Stopped smart server on port 8080')
+
+    def render_POST(self, request):
+        response = None
+        try:
+            method = request.args['method'][0]
+        except KeyError:
+            log.warn('Missing "method" parameter, will use GET by default')
+            method = 'GET'
+        try:
+            code = int(request.args['response_code'][0])
+        except KeyError:
+            log.warn('Missing "response_code" parameter, will use 200 by default')
+            code = 200
+        try:
+            path = request.args['path'][0]
+            body = request.args['response_body'][0]
+        except KeyError, key:
+            response = 'Missing key: {0}'.format(key)
+            log.error(response)
+        else:
+            HttpMockServer.dispatcher[method][path] = {
+                'response_code': code, 'response_body': body
+            }
+            response = 'Success'
+        finally:
+            return response
