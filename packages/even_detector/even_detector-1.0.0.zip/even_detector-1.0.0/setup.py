43c41bc51d3401b3408bb411583fd43bf5cb7f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:26 +0000

    Add package even_detector/1.0.0/even_detector-1.0.0.zip

---
+++ packages/even_detector/even_detector-1.0.0.zip/even_detector-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'even_detector',
+        version         = '1.0.0',
+        py_modules      = ['even_detector'],
+        author          = 'Ridhwanul',
+        author_email    = 'ridhwanul_h_emon@yahoo.com',
+        url             = 'pypi.python.org',
+        description     = 'A simple function which will help to detect that whether the number is an even or not',
+    )
