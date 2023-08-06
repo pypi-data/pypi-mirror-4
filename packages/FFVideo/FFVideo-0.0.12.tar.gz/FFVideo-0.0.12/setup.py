20f954b36216bcb5e5da8b6556f5d2961b3a9a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:15 +0000

    Add package FFVideo/0.0.12/FFVideo-0.0.12.tar.gz

---
+++ packages/FFVideo/FFVideo-0.0.12.tar.gz/FFVideo-0.0.12/setup.py
@@ -0,0 +1,32 @@
+import os
+from distutils.core import setup, Extension
+
+try:
+    from Cython.Distutils import build_ext
+    sources = ["ffvideo/ffvideo.pyx"]
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    sources = ["ffvideo/ffvideo.c"]
+    cmdclass = {}
+
+def read(fn):
+    return open(os.path.join(os.path.dirname(__file__), fn)).read()
+
+VERSION = "0.0.12"
+
+setup(
+    name="FFVideo",
+    version=VERSION,
+    description="FFVideo is a python extension makes possible to access to decoded frames at two format: PIL.Image or numpy.ndarray.",
+    long_description=read("README.txt"),
+    ext_modules=[
+        Extension("ffvideo", sources,
+                  include_dirs=["/usr/include/ffmpeg"],
+                  libraries=["avformat", "avcodec", "swscale"])
+    ],
+    cmdclass=cmdclass,
+    author="Zakhar Zibarov",
+    author_email="zakhar.zibarov@gmail.com",
+    url="http://bitbucket.org/zakhar/ffvideo/",
+)
+
