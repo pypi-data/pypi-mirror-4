42235245cbb0c0f7a1f78dbae2376f1f2a54f4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:57 +0000

    Add package djero/0.0a9/djero-0.0a9.tar.gz

---
+++ packages/djero/djero-0.0a9.tar.gz/djero-0.0a9/djero/sitemap.py
@@ -0,0 +1,15 @@
+from djero.models import Topic
+
+# TODO
+# This sucks, really.  But I'm not sure if I want to 
+# generate the sitemap based on popularity, or if I
+# want to limit these results, or what.
+class DjeroSitemap(sitemaps.Sitemap):
+    changefreq = "daily"
+    priority   = 0.5
+
+    def items(self):
+        return Topic.objects.all()    
+
+    def lastmod(self, obj):
+        return obj.created
