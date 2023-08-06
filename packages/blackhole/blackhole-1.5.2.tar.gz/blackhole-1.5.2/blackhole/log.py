bdc0432c0ce7c676b208a39409a663339e7c9eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:48 +0000

    Add package blackhole/1.5.2/blackhole-1.5.2.tar.gz

---
+++ packages/blackhole/blackhole-1.5.2.tar.gz/blackhole-1.5.2/blackhole/log.py
@@ -0,0 +1,15 @@
+"""blackhole.log - Logging configuration for the blackhole
+server."""
+
+import logging
+
+from tornado.options import options
+
+
+logging.basicConfig(format="%(message)s")
+log = logging.getLogger(__name__)
+file_handler = logging.FileHandler(options.log)
+file_handler.setLevel(logging.INFO)
+file_formatter = logging.Formatter("%(asctime)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+log.addHandler(file_handler)
