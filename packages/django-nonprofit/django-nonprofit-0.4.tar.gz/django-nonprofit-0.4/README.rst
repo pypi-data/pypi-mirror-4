f44e0c32b5fa7e807a59b9b76723830ff506ce1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:47 +0000

    Add package django-nonprofit/0.4/django-nonprofit-0.4.tar.gz

---
+++ packages/django-nonprofit/django-nonprofit-0.4.tar.gz/django-nonprofit-0.4/README.rst
@@ -0,0 +1,18 @@
+django-nonprofit
+================
+
+
+Settings
+--------
+
+NONPROFIT_NAME
+    'Sunlight Foundation'
+
+NONPROFIT_URL_DISPLAY
+    'SunlightFoundation.com'
+
+NONPROFIT_EMAIL
+    'contact@sunlightfoundation.com'
+
+NONPROFIT_FUNDING_DOWNLOAD
+    False
---
+++ packages/django-nonprofit/django-nonprofit-0.4.tar.gz/django-nonprofit-0.4/nonprofit/funding/views.py
@@ -0,0 +1,67 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django_tablib import ModelDataset
+from nonprofit.funding.models import Contribution, Grant
+
+FUNDING_DOWNLOAD = getattr(settings, 'NONPROFIT_FUNDING_DOWNLOAD', False)
+
+MIMETYPES = {
+    'csv': 'text/csv',
+    'xls': 'application/vnd.ms-excel',
+}
+
+# tablib stuff
+
+
+class ContributionDataset(ModelDataset):
+    class Meta:
+        queryset = Contribution.objects.all()
+
+
+def dyn_amount(row):
+    print row
+    if row[5] == 'True':
+        return '0.00'
+    return row[3]
+
+# the views
+
+
+def funding(request):
+    data = {
+        'contributions': Contribution.objects.filter(is_inkind=False),
+        'inkind': Contribution.objects.filter(is_inkind=True),
+        'grants': Grant.objects.filter(is_minigrant=False),
+        'minigrants': Grant.objects.filter(is_minigrant=True),
+    }
+    return render_to_response('about/funding.html', data, context_instance=RequestContext(request))
+
+
+def funding_download(request, ext):
+
+    if not FUNDING_DOWNLOAD:
+        return HttpResponseRedirect(reverse('funding'))
+
+    ext = ext.lower()
+    if ext not in MIMETYPES:
+        raise Http404()
+
+    data = ContributionDataset()
+    data.headers = None
+    data.append(col=[dyn_amount])
+    data.headers = ['ID', 'Year', 'Contributor', 'Original Amount', 'Note', 'Is In-kind Contribution', 'Amount']
+    del data['ID']                  # remove data column
+    del data['Original Amount']     # remove original amount column
+
+    return HttpResponse(getattr(data, ext), mimetype=MIMETYPES[ext])
+
+
+def grants(request):
+    data = {
+        'grants': Grant.objects.filter(is_minigrant=False),
+        'minigrants': Grant.objects.filter(is_minigrant=True),
+    }
+    return render_to_response('about/grants.html', data, context_instance=RequestContext(request))
