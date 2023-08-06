c491d105524576df62f64a42718157e9815e02ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:52 +0000

    Add package paymill/0.1.1/paymill-0.1.1.tar.gz

---
+++ packages/paymill/paymill-0.1.1.tar.gz/paymill-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='paymill',
+        version='0.1.1',
+        description='Minimal python API client library for Paymill API v2',
+        url='https://github.com/rutherford/Paymill',
+        author='Matt Rutherford',
+        author_email='rutherford@clientsideweb.net',
+        license='MIT',
+        packages=find_packages(),
+        zip_safe=False)
