46f74573f938aa23d7f6f572e61279bfec29e430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package BlinkStick/0.3.1/BlinkStick-0.3.1.tar.gz

---
+++ packages/BlinkStick/BlinkStick-0.3.1.tar.gz/BlinkStick-0.3.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='BlinkStick',
+    version='0.3.1',
+    author='Arvydas Juskevicius',
+    author_email='arvydas@arvydas.co.uk',
+    packages=find_packages(),
+    scripts=["bin/blinkstick"],
+    url='http://pypi.python.org/pypi/BlinkStick/',
+    license='LICENSE.txt',
+    description='Python package to control BlinkStick USB devices.',
+    long_description=read('README.rst'),
+    install_requires=[
+        "grapefruit",
+        "webcolors",
+        "pyusb",
+        "websocket-client",
+        "psutil"
+    ],
+)
