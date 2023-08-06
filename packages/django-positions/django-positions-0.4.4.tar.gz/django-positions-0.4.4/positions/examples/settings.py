d1d8cff5a19d499ba0c3ffcfbdc6fb5120f36dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:20 +0000

    Add package django-positions/0.4.4/django-positions-0.4.4.tar.gz

---
+++ packages/django-positions/django-positions-0.4.4.tar.gz/django-positions-0.4.4/positions/examples/settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'positions.examples.lists',
+    'positions.examples.nodes',
+    'positions.examples.generic',
+    'positions.examples.todo',
+    'positions.examples.store',
+    'positions.examples.photos',
+)
