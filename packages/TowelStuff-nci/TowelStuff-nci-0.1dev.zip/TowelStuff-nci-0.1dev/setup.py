b0d7cde4d5205e4a3120a15b088007953c07735d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:01 +0000

    Add package TowelStuff-nci/0.1dev/TowelStuff-nci-0.1dev.zip

---
+++ packages/TowelStuff-nci/TowelStuff-nci-0.1dev.zip/TowelStuff-nci-0.1dev/setup.py
@@ -0,0 +1,9 @@
+﻿from distutils.core import setup
+
+setup(
+    name='TowelStuff-nci',
+	version='0.1dev',
+	packages=['towelstuff',],
+	license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	long_descripton=open('README.txt').read(),
+)
