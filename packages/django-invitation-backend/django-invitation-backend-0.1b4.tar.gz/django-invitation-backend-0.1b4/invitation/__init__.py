940051e400cec5e394b6951bb815da241048a50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:06 +0000

    Add package django-invitation-backend/0.1b4/django-invitation-backend-0.1b4.tar.gz

---
+++ packages/django-invitation-backend/django-invitation-backend-0.1b4.tar.gz/django-invitation-backend-0.1b4/invitation/__init__.py
@@ -0,0 +1,17 @@
+VERSION = (0, 1, 0, 'beta', 4)
+
+__author__ = u'Daniel Barreto'
+__credits__ = [u'Atamert \xd6l\xe7gen']
+
+
+__license__ = 'BSD'
+__maintainer__ = u'Daniel Barreto'
+__email__ = 'daniel.barreto.n@gmail.com'
+__status__ = 'Beta'
+
+def get_version():
+    try:
+        from django.utils.version import get_version as django_get_version
+    except ImportError:
+        from django import get_version as django_get_version
+    return django_get_version(VERSION) # pragma: no cover
