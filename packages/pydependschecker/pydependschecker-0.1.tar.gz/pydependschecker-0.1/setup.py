2be814d183585f087d42372b5e98d7a7bf50fa84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:41 +0000

    Add package pydependschecker/0.1/pydependschecker-0.1.tar.gz

---
+++ packages/pydependschecker/pydependschecker-0.1.tar.gz/pydependschecker-0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = "pydependschecker",
+    version = "0.1",
+    url = 'https://github.com/sys-git/pydependschecker',
+    packages = find_packages(),
+    package_dir = {'pydependschecker': 'pydependschecker'},
+    include_package_data = True,
+    author = "Francis Horsman",
+    author_email = "francis.horsman@gmail.com",
+    description = "A pure-python dependency graph generator and checker",
+    license = "GNU General Public License",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications',
+    ]
+)
