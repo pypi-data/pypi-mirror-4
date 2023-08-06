d672602bb692f49b3b328ab7617c30765a29842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:00 +0000

    Add package preggy/0.3.7/preggy-0.3.7.tar.gz

---
+++ packages/preggy/preggy-0.3.7.tar.gz/preggy-0.3.7/preggy/types/nullable.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''preggy "null" (None) assertions.  For use with `expect()` (see `preggy.core`).
+'''
+
+# preggy assertions
+# https://github.com/heynemann/preggy
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
+
+from preggy import assertion
+
+
+@assertion
+def to_be_null(topic):
+    '''Asserts that `topic` is `None`.'''
+    if topic is not None:
+        raise AssertionError('Expected topic({0}) to be None', topic)
+
+
+@assertion
+def not_to_be_null(topic):
+    '''Asserts that `topic` is NOT `None`.'''
+    if topic is None:
+        raise AssertionError('Expected topic({0}) not to be None', topic)
