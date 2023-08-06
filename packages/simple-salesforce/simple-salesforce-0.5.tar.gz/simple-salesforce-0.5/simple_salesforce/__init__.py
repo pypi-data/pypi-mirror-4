10db8b222c43286032aa6259e6c8a6f00498b0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:33 +0000

    Add package simple-salesforce/0.5/simple-salesforce-0.5.tar.gz

---
+++ packages/simple-salesforce/simple-salesforce-0.5.tar.gz/simple-salesforce-0.5/simple_salesforce/__init__.py
@@ -0,0 +1,2 @@
+from simple_salesforce.api import Salesforce, SalesforceAPI, SFType, SalesforceMoreThanOneRecord, SalesforceExpiredSession, SalesforceRefusedRequest, SalesforceResourceNotFound, SalesforceGeneralError
+from simple_salesforce.login import SalesforceLogin, SalesforceAuthenticationFailed
