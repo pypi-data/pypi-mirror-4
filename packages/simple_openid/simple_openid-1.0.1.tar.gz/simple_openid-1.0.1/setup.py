e3979a54cc3f00e55889b6c91e09fffcf48caec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:25 +0000

    Add package simple_openid/1.0.1/simple_openid-1.0.1.tar.gz

---
+++ packages/simple_openid/simple_openid-1.0.1.tar.gz/simple_openid-1.0.1/setup.py
@@ -0,0 +1,17 @@
+"""Simple OpenID. One-line setup for OpenID login for Flask.
+"""
+
+from distutils.core import setup
+
+doclines = __doc__.split("\n")
+
+setup(name="simple_openid",
+      version="1.0.1",
+      maintainer="Max Goedjen",
+      maintainer_email="max.goedjen@gmail.com",
+      url = "http://github.com/maxgoedjen/simple_openid",
+      platforms = ["any"],
+      description = doclines[0],
+      install_requires = ['flask_openid'],
+      long_description = "\n".join(doclines[2:])
+      )
