ebd9bddedceb0673fe9356b514672fc3ddd95499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package itunes-iap/0.1/itunes-iap-0.1.tar.gz

---
+++ packages/itunes-iap/itunes-iap-0.1.tar.gz/itunes-iap-0.1/itunesiap/exceptions.py
@@ -0,0 +1,14 @@
+
+from prettyexc import PrettyException
+
+class ModeNotAvailable(PrettyException):
+    pass
+
+class RequestError(PrettyException):
+    pass
+
+class ItunesServerNotAvailable(RequestError):
+    pass
+
+class InvalidReceipt(RequestError):
+    pass
