fc37ef53916751fc51f0a4f9f3042185793c78a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package django-feedme/0.3.2/django-feedme-0.3.2.tar.gz

---
+++ packages/django-feedme/django-feedme-0.3.2.tar.gz/django-feedme-0.3.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='django-feedme',
+    version='0.3.2',
+    packages=['feedme', 'feedme.migrations'],
+    zip_safe = False,
+    include_package_data=True,
+    url='http://github.com/dstegelman/django-feedme',
+    license='MIT',
+    author='Derek Stegelman',
+    author_email='email@stegelman.com',
+    description='Django Google Reader Replacement',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        "Framework :: Django",
+        ],
+    install_requires = ['feedparser>=5.1.3,<6.0', 'django-bootstrap-static>=2.0', 'hadrian>=1.1']
+    )
