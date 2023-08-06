b67ca830a77f2257d6329c6ff671a84954e26d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:53 +0000

    Add package plone.app.imagecropping/0.1rc1/plone.app.imagecropping-0.1rc1.zip

---
+++ packages/plone.app.imagecropping/plone.app.imagecropping-0.1rc1.zip/plone.app.imagecropping-0.1rc1/src/plone/app/imagecropping/browser/static/cropping.js
@@ -0,0 +1,79 @@
+var imagecropping = {};
+
+if (jQuery) {
+
+    (function($) {
+        imagecropping.clearCoords = function() {
+            $('#coords input[hidden]').val('');
+            $('#h').css({color:'red'});
+            window.setTimeout(function(){
+                $('#h').css({color:'inherit'});
+            },500);
+        };
+
+        imagecropping.doChange = function(c) {
+            // show coords
+            $('#x1').val(c.x);
+            $('#y1').val(c.y);
+            $('#x2').val(c.x2);
+            $('#y2').val(c.y2);
+
+            // render thumbnail
+            /*
+            rx = c.w / 100;
+            ry = c.h / 100;
+            prev_node = $('#preview-' + $("#scalename").val());
+            console.log(prev_node);
+            cropbox_img = $('img.cropbox');
+            thumb_img = $('<img />');
+            thumb_img.attr('src', cropbox_img.attr('src'));
+            prev_node.html(thumb_img);
+            $("img", prev_node).css({
+                width: Math.round(rx * cropbox_img.attr('width')) + 'px',
+                height: Math.round(ry * cropbox_img.attr('height')) + 'px',
+                marginLeft: '-' + Math.round(rx * c.x) + 'px',
+                marginTop: '-' + Math.round(ry * c.y) + 'px'
+            });
+            */
+        };
+
+        imagecropping.option_change = function(option) {
+
+            var config = jQuery.parseJSON(option.attr('data-jcrop_config')),
+                scale_name = option.attr('data-scale_name'),
+                jcrop_config = {
+                    onChange: imagecropping.doChange,
+                    onSelect: imagecropping.doChange,
+                    onRelease: imagecropping.clearCoords
+                };
+            jQuery.extend(jcrop_config, config);
+
+            $('#coords img.cropbox').attr('src', config["data-imageURL"]);
+            $('#coords img.cropbox').width(config["origWidth"]);
+            $('#coords img.cropbox').height(config["origHeight"]);
+            $('#scalename').val(scale_name);
+
+            var jcrop_api = $('#coords img.cropbox').data('Jcrop');
+            if (jcrop_api != undefined) {
+                jcrop_api.destroy();
+            }
+
+            $('#coords img.cropbox').Jcrop(jcrop_config);
+        }
+
+        imagecropping.init_editor = function() {
+            var scales = $('ul.scales li');
+
+            if(scales.length) {
+                scales.click(function(e) {
+                    imagecropping.option_change($(this));
+                    $(this).addClass('selected').siblings().removeClass('selected');
+                });
+                // TODO: preview of actual scale
+                //$('ul.scales a').prepOverlay({subtype: 'image'});
+                imagecropping.option_change($('ul.scales li.selected'));
+                scales.scrollTop($('ul.scales li.selected').scrollTop());
+            }
+        }
+    })(jQuery);
+}
