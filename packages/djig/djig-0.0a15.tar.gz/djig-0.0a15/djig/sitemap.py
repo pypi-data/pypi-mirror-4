5b3db59ec1781d7613cc1f5932dd79d719630bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:57 +0000

    Add package djig/0.0a15/djig-0.0a15.tar.gz

---
+++ packages/djig/djig-0.0a15.tar.gz/djig-0.0a15/djig/sitemap.py
@@ -0,0 +1,13 @@
+from django.contrib import sitemaps
+import datetime
+from djig.models import Articleclass 
+
+DjigSitemap(sitemaps.Sitemap):    
+    changefreq = "daily"    
+    priority   = 0.5    
+    
+    def items(self):        
+        return Article.objects.all()    
+    
+    def lastmod(self, obj):
+        return obj.created
