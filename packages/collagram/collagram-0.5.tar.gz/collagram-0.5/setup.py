0b128a2509840377d2c2db38a84d2270b5fe872c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:48 +0000

    Add package collagram/0.5/collagram-0.5.tar.gz

---
+++ packages/collagram/collagram-0.5.tar.gz/collagram-0.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='collagram',
+    version='0.5',
+    author='Adam Patterson',
+    author_email='adam@adamrt.com',
+    url='http://github.com/vurbmedia/collagram/',
+    license='ISC',
+    description='Generate collages of Instagram photographs.',
+    packages=['collagram', 'collagram.test'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'PIL',
+        'python-instagram'
+    ],
+)
