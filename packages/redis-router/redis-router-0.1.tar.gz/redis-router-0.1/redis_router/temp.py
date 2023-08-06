ee5e49dd43c555e209437066ac536a4ce8072df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:24 +0000

    Add package redis-router/0.1/redis-router-0.1.tar.gz

---
+++ packages/redis-router/redis-router-0.1.tar.gz/redis-router-0.1/redis_router/temp.py
@@ -0,0 +1,9 @@
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+"""
+router = Router("serverlist")
+
+router.set("forge", 13)
+router.set("spawning_pool", 6)
+print router.get("forge"), router.get("spawning_pool")
+"""
+
