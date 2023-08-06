dc3f53a56af51f79b3b02fc75c0714f5061a16c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package ATpy/0.9.4/ATpy-0.9.4.tar.gz

---
+++ packages/ATpy/ATpy-0.9.4.tar.gz/ATpy-0.9.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try: # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError: # Python 2.x
+    from distutils.command.build_py import build_py
+
+
+setup(name='ATpy',
+      version='0.9.4',
+      description='Astronomical Tables in Python',
+      author='Eli Bressert and Thomas Robitaille',
+      author_email='elibre@users.sourceforge.net, \
+        robitaille@users.sourceforge.net',
+      url='http://atpy.sourceforge.net/',
+      packages=['atpy'],
+      cmdclass = {'build_py':build_py},
+     )
