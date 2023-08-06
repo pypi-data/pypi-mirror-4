b32b2b9becba8a8de2d0ca004eb46e7f9d2123d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package llvmpy/0.11.0/llvmpy-0.11.0.tar.gz

---
+++ packages/llvmpy/llvmpy-0.11.0.tar.gz/llvmpy-0.11.0/docs/source/doc/llvm.core.ArrayType.rst
@@ -0,0 +1,37 @@
++--------------------------------+
+| layout: page                   |
++--------------------------------+
+| title: ArrayType (llvm.core)   |
++--------------------------------+
+
+llvm.core.ArrayType
+===================
+
+Base Class
+----------
+
+-  `llvm.core.Type <llvm.core.Type.html>`_
+
+Properties
+----------
+
+``element``
+~~~~~~~~~~~
+
+[read-only]
+
+A ``Type`` object representing the type of the element of the array.
+
+``count``
+~~~~~~~~~
+
+[read-only]
+
+The number of elements in the array.
+
+
+Automatically Generated Documentation
+-------------------------------------
+.. autoclass:: llvm.core.ArrayType
+   :members:
+   :undoc-members:
---
+++ packages/llvmpy/llvmpy-0.11.0.tar.gz/llvmpy-0.11.0/llpython/tests/llfunctys.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# ______________________________________________________________________
+
+import llvm.core as lc
+
+from llpython import bytetype
+
+# ______________________________________________________________________
+
+doslice = lc.Type.function(bytetype.li8_ptr, (
+        bytetype.li8_ptr, bytetype.lc_size_t, bytetype.lc_size_t))
+
+ipow = lc.Type.function(bytetype.li32, (bytetype.li32,
+                                        bytetype.li32))
+
+pymod = lc.Type.function(bytetype.li32, (bytetype.li32,
+                                         bytetype.li32))
+
+# ______________________________________________________________________
+# End of llfunctys.py
