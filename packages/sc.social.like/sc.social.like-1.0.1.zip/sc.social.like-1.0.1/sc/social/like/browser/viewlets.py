7852f3d1c756feb80279e264d120e4d5132d395c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:44 +0000

    Add package sc.social.like/1.0.1/sc.social.like-1.0.1.zip

---
+++ packages/sc.social.like/sc.social.like-1.0.1.zip/sc.social.like-1.0.1/sc/social/like/browser/viewlets.py
@@ -0,0 +1,138 @@
+from zope.component import getMultiAdapter
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from plone.app.layout.viewlets import ViewletBase
+
+from sc.social.like.config import FB_LOCALES
+
+
+def fix_iso(code):
+    #TODO: We should be dealing also with *simple*
+    #      language codes like pt or en or es
+    if code.find('-') > -1:
+        # we have a iso code like pt-br and FB_LOCALES uses pt_BR
+        code = code.split('-')
+        code = '%s_%s' % (code[0], code[1].upper())
+    elif code.find('_') == -1:
+        # XXX: Hack follows!
+        # Try to find the best combination...
+        available = [fb for fb in FB_LOCALES if fb.startswith(code)]
+        if len(available) == 1:
+            code = available[0]
+
+        elif len(available) > 1:
+            # We have several choices... try to find a xx_XX combination if possible.
+            # if not, return the first one..
+            if '%s_%s' % (code.lower(), code.upper()) in FB_LOCALES:
+                code = '%s_%s' % (code.lower(), code.upper())
+            else:
+                code = available[0]
+
+    return code
+
+
+def facebook_language(languages, default):
+    """Given the prefered language on request we return the right
+    language_code option to the template
+    """
+    if not languages:
+        # do not change anything
+        return default
+    languages = [fix_iso(l) for l in languages]
+    prefered = [l for l in languages if l in FB_LOCALES]
+    return prefered and prefered[0] or default
+
+
+class BaseLikeViewlet(ViewletBase):
+
+    enabled_portal_types = []
+    typebutton = ''
+    twitter_enabled = False
+    twittvia = ''
+    fb_enabled = False
+    fbaction = ''
+    fbadmins = ''
+    gp_enabled = False
+    language = 'en_US'
+
+    def __init__(self, context, request, view, manager):
+        super(BaseLikeViewlet, self).__init__(context, request, view, manager)
+        pp = getToolByName(context, 'portal_properties')
+
+        self.context = context
+        self.request = request
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+
+        languages = self.request.get('HTTP_ACCEPT_LANGUAGE', '').split(';')[0].split(',')
+        self.language = facebook_language(languages, self.language)
+        self.site_url = self.portal_state.portal_url()
+        self.sheet = getattr(pp, 'sc_social_likes_properties', None)
+        if self.sheet:
+            self.enabled_portal_types = self.sheet.getProperty("enabled_portal_types", [])
+            self.typebutton = self.sheet.getProperty("typebutton", "")
+            self.twitter_enabled = self.sheet.getProperty("twitter_enabled", True)
+            self.twittvia = self.sheet.getProperty("twittvia", "")
+            self.fb_enabled = self.sheet.getProperty("fb_enabled", True)
+            self.fbaction = self.sheet.getProperty("fbaction", "")
+            self.fbadmins = self.sheet.getProperty("fbadmins", "")
+            self.gp_enabled = self.sheet.getProperty("gp_enabled", True)
+
+    def enabled(self):
+        """Validates if the viewlet should be enabled for this context
+        """
+        context = self.context
+        enabled_portal_types = self.enabled_portal_types
+        return context.portal_type in enabled_portal_types
+
+    # HACK: fixes https://bitbucket.org/takaki/sc.social.like/issue/1
+    def update(self):
+        """Overriding ViewletBase because we may be called for
+        UnauthorizedBinding objects
+        """
+        return
+
+
+class SocialMetadataViewlet(BaseLikeViewlet):
+    """Viewlet used to insert metadata into page header
+    """
+    render = ViewPageTemplateFile("templates/metadata.pt")
+
+    def hasImage(self):
+        """Return object image
+        """
+        context = self.context
+        try:
+            image = context.getField('image').get(context)
+        except:
+            image = ''
+
+        try:
+            return image.getSize() > 0
+        except:
+            return bool(image)
+
+    def portaltitle(self):
+        """Return the portal title
+        """
+        portaltitle = self.portal_state.portal_title()
+
+        return portaltitle
+
+    def logoname(self):
+        """Return portal logo name
+        """
+        portal = self.portal_state.portal()
+        bprops = portal.restrictedTraverse('base_properties', None)
+        if bprops is not None:
+            logoName = bprops.logoName
+        else:
+            logoName = 'logo.png'
+
+        return logoName
+
+
+class SocialLikesViewlet(BaseLikeViewlet):
+    """Viewlet used to display the buttons
+    """
+    render = ViewPageTemplateFile("templates/sociallikes.pt")
---
+++ packages/sc.social.like/sc.social.like-1.0.1.zip/sc.social.like-1.0.1/sc/social/like/controlpanel/likes.py
@@ -0,0 +1,197 @@
+# -*- coding:utf-8 -*-
+
+from zope.schema import Bool
+from zope.schema import TextLine
+from zope.schema import Tuple
+from zope.schema import Choice
+from zope.component import adapts
+from zope.interface import Interface
+from zope.interface import implements
+
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.CMFPlone.utils import getToolByName
+from Products.CMFDefault.formlib.schema import ProxyFieldProperty
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+
+from plone.app.controlpanel.form import ControlPanelForm
+
+from plone.fieldsets.fieldsets import FormFieldsets
+
+from zope.app.form.browser.itemswidgets import MultiSelectWidget as BaseMultiSelectWidget
+
+from sc.social.like import LikeMessageFactory as _
+
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+verbs = SimpleVocabulary([
+    SimpleTerm(value=u'like', title=_(u'Like')),
+    SimpleTerm(value=u'recommend', title=_(u'Recommend')),
+])
+
+
+class MultiSelectWidget(BaseMultiSelectWidget):
+    """ """
+    def __init__(self, field, request):
+        """Initialize the widget."""
+        super(MultiSelectWidget, self).__init__(field, field.value_type.vocabulary, request)
+
+
+class IProvidersSchema(Interface):
+    """ General Configurations """
+
+    enabled_portal_types = Tuple(
+        title=_(u'Content types'),
+        description=_(
+            u'help_portal_types',
+            default=u"Please select content types in which the "
+                    u"viewlet will be applied.",
+        ),
+        required=True,
+        value_type=Choice(vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
+    )
+
+    typebutton = Choice(
+        title=_(u'Button style'),
+        description=_(
+            u'help_selected_buttons',
+            default=u"Choose your button style.",
+        ),
+        required=True,
+        default=_(u'horizontal'),
+        values=(_(u'horizontal'), _(u'vertical')),
+    )
+
+
+class ITwitterSchema(Interface):
+    """ Twitter configurations """
+
+    twitter_enabled = Bool(
+        title=_(u"Enable Twitter button"),
+        default=True,
+        required=False,
+    )
+
+    twittvia = TextLine(
+        title=_(u'Twitter nick'),
+        description=_(
+            u'help_your_twitter_nick',
+            default=u"Enter your twitter nick. eg. simplesconsultoria",
+        ),
+        required=False,
+    )
+
+
+class IFbSchema(Interface):
+    """ Facebook configurations """
+
+    fb_enabled = Bool(
+        title=_(u"Enable Facebook button"),
+        default=True,
+        required=False,
+    )
+
+    fbaction = Choice(
+        title=_(u'Verb to display'),
+        description=_(
+            u'help_verb_display',
+            default=u"The verb to display in the facebook button. "
+                    u"Currently only 'like' and 'recommend' are "
+                    u"supported.",
+        ),
+        required=True,
+        default=u'like',
+        vocabulary=verbs,
+    )
+
+    fbadmins = TextLine(
+        title=_(u'Admins'),
+        description=_(
+            u'help_admins',
+            default=u"A comma-separated list of either the "
+                    u"Facebook IDs of page administrators or a "
+                    u"Facebook Platform application ID.",
+        ),
+        required=False,
+    )
+
+
+class IGpSchema(Interface):
+    """ Google+ configurations """
+
+    gp_enabled = Bool(
+        title=_(u"Enable Google+ action"),
+        default=True,
+        required=False,
+    )
+
+
+class BaseControlPanelAdapter(SchemaAdapterBase):
+    """ Base control panel adapter """
+
+    def __init__(self, context):
+        super(BaseControlPanelAdapter, self).__init__(context)
+        portal_properties = getToolByName(context, 'portal_properties')
+        self.context = portal_properties.sc_social_likes_properties
+
+
+class LikeControlPanelAdapter(BaseControlPanelAdapter):
+    """ Like control panel adapter """
+    adapts(IPloneSiteRoot)
+    implements(IProvidersSchema)
+
+    enabled_portal_types = ProxyFieldProperty(IProvidersSchema['enabled_portal_types'])
+    typebutton = ProxyFieldProperty(IProvidersSchema['typebutton'])
+
+
+class TwitterControlPanelAdapter(BaseControlPanelAdapter):
+    """ Twitter control panel adapter """
+    adapts(IPloneSiteRoot)
+    implements(ITwitterSchema)
+
+    twitter_enabled = ProxyFieldProperty(ITwitterSchema['twitter_enabled'])
+    twittvia = ProxyFieldProperty(ITwitterSchema['twittvia'])
+
+
+class FbControlPanelAdapter(BaseControlPanelAdapter):
+    """ Facebook control panel adapter """
+    adapts(IPloneSiteRoot)
+    implements(IFbSchema)
+
+    fb_enabled = ProxyFieldProperty(IFbSchema['fb_enabled'])
+    fbaction = ProxyFieldProperty(IFbSchema['fbaction'])
+    fbadmins = ProxyFieldProperty(IFbSchema['fbadmins'])
+
+
+class GpControlPanelAdapter(BaseControlPanelAdapter):
+    """ Google+ control panel adapter """
+    adapts(IPloneSiteRoot)
+    implements(IGpSchema)
+
+    gp_enabled = ProxyFieldProperty(IGpSchema['gp_enabled'])
+
+baseset = FormFieldsets(IProvidersSchema)
+baseset.id = 'baseset'
+baseset.label = _(u'Base Plugin Configuration')
+
+twitterset = FormFieldsets(ITwitterSchema)
+twitterset.id = 'twitterset'
+twitterset.label = _(u'Twitter settings')
+
+fbset = FormFieldsets(IFbSchema)
+fbset.id = 'fbset'
+fbset.label = _(u'Facebook settings')
+
+gpset = FormFieldsets(IGpSchema)
+gpset.id = 'gpset'
+gpset.label = _(u'Google+ settings')
+
+
+class ProvidersControlPanel(ControlPanelForm):
+    """ """
+    form_fields = FormFieldsets(baseset, twitterset, fbset, gpset)
+
+    form_fields['enabled_portal_types'].custom_widget = MultiSelectWidget
+
+    label = _('Social: Like Actions settings')
+    description = _('Configure settings for social like actions.')
+    form_name = _('Social: Like Actions')
