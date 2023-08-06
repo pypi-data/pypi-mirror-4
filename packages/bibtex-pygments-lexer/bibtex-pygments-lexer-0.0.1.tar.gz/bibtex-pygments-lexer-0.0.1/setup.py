cb1f86bd716d94855c37e82918c54ba611e048b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:51 +0000

    Add package bibtex-pygments-lexer/0.0.1/bibtex-pygments-lexer-0.0.1.tar.gz

---
+++ packages/bibtex-pygments-lexer/bibtex-pygments-lexer-0.0.1.tar.gz/bibtex-pygments-lexer-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='bibtex-pygments-lexer',
+    version='0.0.1',
+    description='Pygments Lexer for BibTeX',
+    author='Marco D. Adelfio',
+    packages=['bibtex_lexer'],
+    entry_points='''[pygments.lexers]
+bibtexlexer = bibtex_lexer:BibtexLexer
+'''
+)
