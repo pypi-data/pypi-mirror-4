facc0ef5deeb205b5c43924c7f3cb3f886b1d7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:28 +0000

    Add package blackhole/1.4.5/blackhole-1.4.5.tar.gz

---
+++ packages/blackhole/blackhole-1.4.5.tar.gz/blackhole-1.4.5/blackhole/__init__.py
@@ -0,0 +1,13 @@
+__author__ = "Kura"
+__copyright__ = "None"
+__credits__ = ["Kura", ]
+__license__ = "MIT"
+__version__ = "1.4.5"
+__maintainer__ = "Kura"
+__email__ = "kura@kura.io"
+__status__ = "Stable"
+
+__pname__ = "blackhole"
+__desc__ = "blackhole.io MTA"
+
+__fullname__ = "%s %s (%s)" % (__pname__, __version__, __status__)
