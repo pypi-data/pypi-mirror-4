2769467ea75e0b483bcb41b2def094df94e48139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:04 +0000

    Add package novabeacon/0.1/novabeacon-0.1.tar.gz

---
+++ packages/novabeacon/novabeacon-0.1.tar.gz/novabeacon-0.1/beacon/__init__.py
@@ -0,0 +1,20 @@
+import json
+
+class Beacon(object):
+
+    def __init__(self, redisclient):
+        self.client = redisclient
+
+    def publish(self, channel, dict_msg):
+        if self.client is None:
+            return
+        self.client.publish(channel, json.dumps(dict_msg))
+
+    def blue(self, **dict_msg):
+        self.publish('blue_beacon', dict_msg)
+
+    def red(self, **dict_msg):
+        self.publish('red_beacon', dict_msg)
+
+    def yellow(self, **dict_msg):
+        self.publish('yellow_beacon', dict_msg)
