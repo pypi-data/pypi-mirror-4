b05c8bfd4e3f052761cb2a9a3412b794b07185ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package starboard/0.1.1/starboard-0.1.1.tar.gz

---
+++ packages/starboard/starboard-0.1.1.tar.gz/starboard-0.1.1/starboard/__init__.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def find_local_free_tcp_port():
+	return find_local_free_tcp_ports(number=1)[0]
+
+
+def find_local_free_tcp_ports(number):
+    sockets = []
+    try:
+        for i in range(0, number):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sockets.append(s)
+            s.bind(("", 0))
+        return [s.getsockname()[1] for s in sockets]
+    finally:
+        for s in sockets:
+            s.close()
+        
+
+def find_local_hostname():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    finally:
+        s.close()
