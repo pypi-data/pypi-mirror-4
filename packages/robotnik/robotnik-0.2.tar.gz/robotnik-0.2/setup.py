7bc30a8912ba9abf6d881c32c16235435e45c9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:47 +0000

    Add package robotnik/0.2/robotnik-0.2.tar.gz

---
+++ packages/robotnik/robotnik-0.2.tar.gz/robotnik-0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+
+setup(
+    name='robotnik',
+    version='0.2',
+    description="Robotnik, python gemnasium",
+    long_description="Robotnik, python gemnasium",
+    keywords='gemnasium robotnik',
+    author='Guilherme Souza',
+    author_email='guivideojob@gmail.com',
+    url='http://github.com/guilhermef/robotnik',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    packages=['robotnik'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'tornado>=3.0.1',
+        'derpconf==0.4.7',
+        'pycurl==7.19.0'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'robotnik=robotnik.server:main'
+        ],
+    },
+
+)
