899ed19ead2ac0e040dc067af897e7edfd7eb52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package q/0.00/q-0.00.tar.gz

---
+++ packages/q/q-0.00.tar.gz/q-0.00/q.py
@@ -0,0 +1,18 @@
+# Copyright 2012 Google Inc.  All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at: http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distrib-
+# uted under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
+# specific language governing permissions and limitations under the License.
+
+print """
+Sorry!  I haven't gotten the okay to release q.py yet, but this
+formality will be taken care of soon.  Please try installing it
+again in a day or two.
+
+-- @zestyping
+"""
