c7ad688d58b1d016c264f10e07a12a1f8ac42970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package django-plugshop/0.4.0.2/django-plugshop-0.4.0.2.tar.gz

---
+++ packages/django-plugshop/django-plugshop-0.4.0.2.tar.gz/django-plugshop-0.4.0.2/plugshop/middleware.py
@@ -0,0 +1,16 @@
+import time
+from plugshop import settings
+from plugshop.cart import Cart
+
+class CartMiddleware(object):
+
+    def process_request(self, request):
+        setattr(request, 
+                settings.REQUEST_NAMESPACE, 
+                Cart(request, settings.SESSION_NAMESPACE))
+
+    def process_response(self, request, response):
+        if hasattr(request, settings.REQUEST_NAMESPACE):
+            cart = getattr(request, settings.REQUEST_NAMESPACE)
+            cart.save()
+        return response
