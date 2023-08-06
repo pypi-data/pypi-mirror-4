2b23c6b20b698e17dbad4a870f9c045c5b248bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package sierra-django-json-mixin-form/1.0/sierra-django-json-mixin-form-1.0.tar.gz

---
+++ packages/sierra-django-json-mixin-form/sierra-django-json-mixin-form-1.0.tar.gz/sierra-django-json-mixin-form-1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='sierra-django-json-mixin-form',
+      version='1.0',
+      description='Utility to render Form responses in a JSON usable format',
+      long_description=long_description,
+      author='Rafael Sierra',
+      author_email='sierra@rafaelsdm.com	',
+      url='http://github.com/rafaelsierra/django-json-mixin-form',
+      packages=[
+                'sierra',
+                'sierra.dj',
+                'sierra.dj.mixins',
+                ],
+      package_dir = {'':'src'},
+      requires = ['django (>=1.4)'],
+      license = 'MIT',
+)
