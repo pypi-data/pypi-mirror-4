f095ad329cdeaa6689f489fc3e32a0ac103bf45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:23 +0000

    Add package txwinrm/0.3/txwinrm-0.3.tar.gz

---
+++ packages/txwinrm/txwinrm-0.3.tar.gz/txwinrm-0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+from distutils.core import setup
+
+setup(name='txwinrm',
+      version='0.3',
+      description='Asynchronous Python WinRM client',
+      author='Zenoss',
+      author_email='bedwards@zenoss.com',
+      url='https://github.com/zenoss/txwinrm',
+      packages=['txwinrm', 'txwinrm.request'],
+      package_data={'txwinrm.request': ['*.xml']},
+      scripts=['scripts/winrm']
+      )
