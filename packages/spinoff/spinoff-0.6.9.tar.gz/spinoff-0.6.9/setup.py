44eac15a263b3714d89974bc6186e5b5601fd49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:48 +0000

    Add package spinoff/0.6.9/spinoff-0.6.9.tar.gz

---
+++ packages/spinoff/spinoff-0.6.9.tar.gz/spinoff-0.6.9/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup, find_packages
+
+if os.path.exists('README.rst'):
+    with open('README.rst') as file:
+        long_description = file.read()
+else:
+    long_description = ''
+
+setup(
+    name="spinoff",
+    description="Framework for writing distributed, fault tolerant and scalable applications",
+    long_description=long_description,
+    version="0.6.9",
+    packages=find_packages(),
+
+    install_requires=[
+        'twisted>=12.0',
+        'txcoroutine',
+        'txzmq==0.5.1',
+    ],
+
+    data_files=[('twisted/plugins', ['twisted/plugins/startnode.py'])],
+
+    dependency_links=[
+        'https://github.com/eallik/txZMQ/tarball/newapi#egg=txzmq-0.5.1'
+    ],
+
+    author="Erik Allik",
+    author_email="erik.allik@skype.net",
+    license="BSD",
+    url="http://github.com/eallik/spinoff/"
+)
