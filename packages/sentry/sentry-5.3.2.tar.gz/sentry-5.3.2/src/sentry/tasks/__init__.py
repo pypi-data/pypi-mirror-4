d1fb109438f3e551bf61235f66f3fd53d9f273af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:22 +0000

    Add package sentry/5.3.2/sentry-5.3.2.tar.gz

---
+++ packages/sentry/sentry-5.3.2.tar.gz/sentry-5.3.2/src/sentry/tasks/__init__.py
@@ -0,0 +1,12 @@
+"""
+sentry.tasks
+~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+import sentry.tasks.cleanup  # NOQA
+import sentry.tasks.fetch_source  # NOQA
+import sentry.tasks.index  # NOQA
+import sentry.tasks.store  # NOQA
