9e0cf84d4ed6fc79edb1509d5b9c483f17f3f589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:40 +0000

    Add package pastebin_python/1.1/pastebin_python-1.1.tar.gz

---
+++ packages/pastebin_python/pastebin_python-1.1.tar.gz/pastebin_python-1.1/pastebin_python/pastebin_exceptions.py
@@ -0,0 +1,15 @@
+"""
+.. module:: pastebin_exceptions
+	:synopsis: This module contains all of the exceptions
+
+.. moduleauthor:: Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
+
+"""
+class PastebinBadRequestException(Exception):
+	pass
+
+class PastebinNoPastesException(Exception):
+	pass
+
+class PastebinFileException(Exception):
+	pass
