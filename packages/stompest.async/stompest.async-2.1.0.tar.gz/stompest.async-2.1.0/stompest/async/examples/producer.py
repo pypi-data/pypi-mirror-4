9eea639437b53c0a53b6083e1fb66bd19baa28fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:29 +0000

    Add package stompest.async/2.1.0/stompest.async-2.1.0.tar.gz

---
+++ packages/stompest.async/stompest.async-2.1.0.tar.gz/stompest.async-2.1.0/stompest/async/examples/producer.py
@@ -0,0 +1,28 @@
+import json
+import logging
+
+from twisted.internet import defer, reactor
+
+from stompest.async import Stomp
+from stompest.config import StompConfig
+
+class Producer(object):
+    QUEUE = '/queue/testIn'
+
+    def __init__(self, config=None):
+        if config is None:
+            config = StompConfig('tcp://localhost:61613')
+        self.config = config
+
+    @defer.inlineCallbacks
+    def run(self):
+        stomp = yield Stomp(self.config).connect()
+        for j in range(10):
+            yield stomp.send(self.QUEUE, json.dumps({'count': j}), receipt='message-%d' % j)
+        yield stomp.disconnect() # graceful disconnect: waits until all receipts have arrived
+        reactor.stop()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    Producer().run()
+    reactor.run()
---
+++ packages/stompest.async/stompest.async-2.1.0.tar.gz/stompest.async-2.1.0/stompest/async/protocol.py
@@ -0,0 +1,99 @@
+import logging
+
+from twisted.internet import defer, reactor, task
+from twisted.internet.protocol import Factory, Protocol
+
+from stompest.protocol import StompFailoverTransport, StompParser
+
+from .util import endpointFactory
+
+LOG_CATEGORY = __name__
+
+class StompProtocol(Protocol):
+    #
+    # twisted.internet.Protocol interface overrides
+    #
+    def connectionLost(self, reason):
+        try:
+            self._onConnectionLost(reason)
+        finally:
+            Protocol.connectionLost(self, reason)
+
+    def dataReceived(self, data):
+        # self.log.debug('Received data: %s' % repr(data))
+        self._parser.add(data)
+        while True:
+            frame = self._parser.get()
+            if frame is None:
+                break
+            if self.log.isEnabledFor(logging.DEBUG):
+                self.log.debug('Received %s' % frame.info())
+            try:
+                self._onFrame(frame)
+            except Exception as e:
+                self.log.error('Unhandled error in frame handler: %s' % e)
+
+    def __init__(self, onFrame, onConnectionLost):
+        self._onFrame = onFrame
+        self._onConnectionLost = onConnectionLost
+        self._parser = StompParser()
+
+        # leave the logger public in case the user wants to override it
+        self.log = logging.getLogger(LOG_CATEGORY)
+
+    #
+    # user interface
+    #
+    def loseConnection(self):
+        self.transport.loseConnection()
+
+    def send(self, frame):
+        if self.log.isEnabledFor(logging.DEBUG):
+            self.log.debug('Sending %s' % frame.info())
+        self.transport.write(str(frame))
+
+    def setVersion(self, version):
+        self._parser.version = version
+
+class StompFactory(Factory):
+    protocol = StompProtocol
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def buildProtocol(self, _):
+        protocol = self.protocol(*self.args, **self.kwargs)
+        protocol.factory = self
+        return protocol
+
+class StompProtocolCreator(object):
+    protocolFactory = StompFactory
+    failoverFactory = StompFailoverTransport
+
+    @classmethod
+    def endpointFactory(cls, broker, timeout=None):
+        return endpointFactory(broker, timeout)
+
+    def __init__(self, uri):
+        self._failover = self.failoverFactory(uri)
+        self.log = logging.getLogger(LOG_CATEGORY)
+
+    @defer.inlineCallbacks
+    def connect(self, timeout, *args, **kwargs):
+        for (broker, delay) in self._failover:
+            yield self._sleep(delay)
+            endpoint = self.endpointFactory(broker, timeout)
+            self.log.info('Connecting to %(host)s:%(port)s ...' % broker)
+            try:
+                protocol = yield endpoint.connect(self.protocolFactory(*args, **kwargs))
+            except Exception as e:
+                self.log.warning('%s [%s]' % ('Could not connect to %(host)s:%(port)d' % broker, e))
+            else:
+                defer.returnValue(protocol)
+
+    def _sleep(self, delay):
+        if not delay:
+            return
+        self.log.info('Delaying connect attempt for %d ms' % int(delay * 1000))
+        return task.deferLater(reactor, delay, lambda: None)
