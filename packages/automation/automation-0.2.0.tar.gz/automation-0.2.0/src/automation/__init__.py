2bd549cda4ef675c2cae98832d2b146f596f0416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package automation/0.2.0/automation-0.2.0.tar.gz

---
+++ packages/automation/automation-0.2.0.tar.gz/automation-0.2.0/src/automation/__init__.py
@@ -0,0 +1,27 @@
+from fabric.api import env
+from fabric.contrib import django
+
+import deploy
+import merge
+import pull_request
+import refresh
+import sync
+import topic
+
+repr(deploy)
+repr(merge)
+repr(pull_request)
+repr(refresh)
+repr(sync)
+repr(topic)
+
+
+required_settings = [
+  'project_name',
+  'django_settings_module',
+  'django_test_settings_module']
+for required_setting in required_settings:
+  if not hasattr(env, required_setting):
+    raise RuntimeError('You must set %s in fabfile.py' % required_setting)
+
+django.settings_module(env.django_settings_module)
