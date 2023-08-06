fb76016aa9c2b5ec6d9d328650e7f9210ef35e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:27 +0000

    Add package grace/0.1/grace-0.1.tar.gz

---
+++ packages/grace/grace-0.1.tar.gz/grace-0.1/setup.py
@@ -0,0 +1,40 @@
+from glob import glob
+import os
+from setuptools import setup
+
+data_files = [
+    ('', ['LICENSE.txt'])
+]
+
+previous = ''
+for root, dirs, files in os.walk(os.path.join('grace_package', 'skeleton')):
+    for filename in files:
+        if previous != root:
+            data_files.append((root, glob(root + '/*.*')))
+            previous = root
+
+setup(
+    name='grace',
+    description='A tool to simplify JavaScript development.',
+    author='Michael Diener',
+    author_email='dm.menthos@gmail.com',
+    url='https://github.com/mdiener/grace',
+    version='0.1',
+    license='LGPL3',
+    packages=['grace_package'],
+    py_modules=['grace'],
+    install_requires=['libsass'],
+    data_files=data_files,
+    keywords='toolchain javascript dizmo js buildtool',
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: JavaScript',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Environment :: Console'
+    ]
+)
