2372504f532cbc147ca94af9ff26badaf5c9f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package vint/0.1.4/vint-0.1.4-py2.7.egg

---
+++ packages/vint/vint-0.1.4-py2.7.egg/vint/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'vint'
+__version__ = '0.1.4'
+__author__ = 'Tyr Chen'
+__email__ = 'tyr.chen@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Tyr Chen'
