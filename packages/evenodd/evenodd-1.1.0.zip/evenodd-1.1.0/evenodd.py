a56db9d555b04a8ac1b81458456f0c10ca18f320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:03 +0000

    Add package evenodd/1.1.0/evenodd-1.1.0.zip

---
+++ packages/evenodd/evenodd-1.1.0.zip/evenodd-1.1.0/evenodd.py
@@ -0,0 +1,22 @@
+'''
+This module is used for detecting even numbers and odd numbers using the function
+called "even_odd" which will tell that whether the number is a even or odd.
+'''
+def even_odd(num):
+    '''
+    This function will tell whether the given number is an even or an odd
+
+    >>> even_odd(44)
+    Even
+    >>> even_odd(47)
+    Odd
+    '''
+    if num % 2 == 0:
+        print ("Even")
+    elif num % 2 == 0:
+        print ("Odd")
+        
+    else:
+        result = num % 2
+        print (result,"It is probably a float for which it is not able decide whether the number is an Odd or an Even")
+
