abfe0039c9ef2328707ca22e5db5ebcd02713533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package lcinvestor/1.0/lcinvestor-1.0.tar.gz

---
+++ packages/lcinvestor/lcinvestor-1.0.tar.gz/lcinvestor-1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='lcinvestor',
+    version='1.0',
+    author='Jeremy Gillick',
+    author_email='j_gillick@yahoo.com',
+    packages=['LendingClubInvestor', 'LendingClubInvestor.tests'],
+    scripts=['bin/lcinvestor'],
+    url='https://github.com/jgillick/LendingClubAutoInvestor',
+    license='LICENSE.txt',
+    description='A simple tool that will watch your LendingClub account and automatically invest cash as it becomes available.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "python-daemon >= 1.6",
+        "requests >= 1.2.0",
+        "beautifulsoup4 >= 4.1.3",
+        "html5lib >= 0.95",
+        "argparse >= 1.2.1"
+    ],
+)
