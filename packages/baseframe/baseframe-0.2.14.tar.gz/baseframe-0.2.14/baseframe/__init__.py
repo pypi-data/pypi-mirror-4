632ad95da217a5c3a2d8e3d61833873e8182ac5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package baseframe/0.2.14/baseframe-0.2.14.tar.gz

---
+++ packages/baseframe/baseframe-0.2.14.tar.gz/baseframe-0.2.14/baseframe/__init__.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import os
+from datetime import datetime, timedelta
+from flask import Blueprint, send_from_directory, render_template, current_app
+from coaster.assets import split_namespec
+from flask.ext.assets import Environment
+from .assets import assets, Version
+
+__all__ = ['baseframe', 'baseframe_js', 'baseframe_css', 'assets']
+
+
+class BaseframeBlueprint(Blueprint):
+    def init_app(self, app, requires=[]):
+        assets_js = []
+        assets_css = []
+        for item in requires:
+            name, spec = split_namespec(item)
+            for alist, ext in [(assets_js, '.js'), (assets_css, '.css')]:
+                if name + ext in assets:
+                    alist.append(name + ext + unicode(spec))
+        js_all = assets.require('!jquery.js', *assets_js)
+        css_all = assets.require(*assets_css)
+        app.assets = Environment(app)
+        app.assets.register('js_all', js_all,
+            filters='closure_js', output='js/baseframe-packed.js')
+        app.assets.register('css_all', css_all,
+            filters=['cssrewrite', 'cssmin'], output='js/baseframe-packed.css')
+        app.register_blueprint(self)
+
+
+baseframe = BaseframeBlueprint('baseframe', __name__,
+    static_folder='static',
+    static_url_path='/_baseframe',
+    template_folder='templates')
+
+
+@baseframe.route('/favicon.ico')
+def favicon():
+    app_icon_path = current_app.static_folder
+    # Does the app have a favicon.ico in /static?
+    if not os.path.exists(os.path.join(app_icon_path, 'favicon.ico')):
+        # Nope? Is it in /static/img?
+        app_icon_path = os.path.join(current_app.static_folder, 'img')
+        if not os.path.exists(os.path.join(app_icon_path, 'favicon.ico')):
+            # Still nope? Serve default favicon from baseframe
+            app_icon_path = os.path.join(baseframe.static_folder, 'img')
+    return send_from_directory(app_icon_path,
+      'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@baseframe.route('/humans.txt')
+def humans():
+    return send_from_directory(
+        current_app.static_folder if os.path.exists(
+            os.path.join(current_app.static_folder, 'humans.txt')) else baseframe.static_folder,
+       'humans.txt', mimetype='text/plain')
+
+
+@baseframe.route('/robots.txt')
+def robots():
+    return send_from_directory(
+        current_app.static_folder if os.path.exists(
+            os.path.join(current_app.static_folder, 'robots.txt')) else baseframe.static_folder,
+       'robots.txt', mimetype='text/plain')
+
+
+@baseframe.route('/_toastr_messages.js')
+def toastr_messages_js():
+    return current_app.response_class(render_template('toastr_messages.js'), mimetype='application/javascript')
+
+
+@baseframe.route('/_editor.css')
+def editorcss():
+    response = current_app.response_class(render_template('editor.css'),
+        mimetype='text/css',
+        headers={'Expires': (datetime.utcnow() + timedelta(minutes=60)).strftime('%a, %d %b %Y %H:%M:%S GMT')})
+    return response
+
+
+@baseframe.app_errorhandler(404)
+def error404(e):
+    return render_template('404.html'), 404
+
+
+@baseframe.app_errorhandler(403)
+def error403(e):
+    return render_template('403.html'), 403
+
+
+@baseframe.app_errorhandler(500)
+def error500(e):
+    return render_template('500.html'), 500
+
+
+@baseframe.after_app_request
+def process_response(response):
+    # Prevent pages from being placed in an iframe. If the response already
+    # set has a value for this option, let it pass through
+    if 'X-Frame-Options' in response.headers:
+        frameoptions = response.headers.get('X-Frame-Options')
+        # FIXME: There has to be a better way to signal this.
+        if not frameoptions or frameoptions == 'ALLOW':
+            response.headers.pop('X-Frame-Options')
+    else:
+        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
+
+
+# Deprecated imports
+from .deprecated import *
