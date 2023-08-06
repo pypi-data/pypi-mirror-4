e8035d170751b2b9fc36384a720de54a4d2f2d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:57 +0000

    Add package kamaki/0.6.3.3/kamaki-0.6.3.3.tar.gz

---
+++ packages/kamaki/kamaki-0.6.3.3.tar.gz/kamaki-0.6.3.3/kamaki/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.7rc2_795_21d2194"
+__version_info__ = ['0', '7rc2_795_21d2194']
+__version_vcs_info__ = {'branch': 'release-0.7',
+ 'revid': '21d2194',
+ 'revno': 795,
+ 'toplevel': '/root/kamaki'}
+__version_user_info__ = "root@snf-9436"
