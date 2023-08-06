c964ec50e881458b7a6637086b22679b140b12e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:29 +0000

    Add package pynt/0.7.1/pynt-0.7.1.tar.gz

---
+++ packages/pynt/pynt-0.7.1.tar.gz/pynt-0.7.1/pynt/__init__.py
@@ -0,0 +1,10 @@
+"""
+Lightweight Python Build Tool
+"""
+
+__version__ = "0.7.1"
+__license__ = "MIT License"
+__contact__ = "http://rags.github.com/pynt/"
+from ._pynt import task, main
+
+__all__ = ["task",  "main"]
