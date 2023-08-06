f34246bf37379dacb73e371d91b662ec8ac1a05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:04 +0000

    Add package django-lazysignup/0.12.0/django-lazysignup-0.12.0.tar.gz

---
+++ packages/django-lazysignup/django-lazysignup-0.12.0.tar.gz/django-lazysignup-0.12.0/testproject/urls.py
@@ -0,0 +1,32 @@
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError:
+    from django.conf.urls.defaults import patterns, url, include
+from django.contrib.auth.forms import UserCreationForm
+
+from lazysignup.tests import GoodUserCreationForm
+
+# URL test patterns for lazysignup. Use this file to ensure a consistent
+# set of URL patterns are used when running unit tests.
+
+urlpatterns = patterns('',
+    (r'^convert/', include('lazysignup.urls')),
+    (r'^custom_convert/', 'lazysignup.views.convert', {
+        'template_name': 'lazysignup/done.html'
+        }),
+    (r'^custom_convert_ajax/', 'lazysignup.views.convert', {
+        'ajax_template_name': 'lazysignup/done.html'
+        }),
+)
+
+urlpatterns += patterns('lazysignup.tests',
+    url(r'^nolazy/$', 'view', name='test_view'),
+    url(r'^lazy/$', 'lazy_view', name='test_lazy_view'),
+)
+
+urlpatterns += patterns('lazysignup.views',
+    url(r'^bad-custom-convert/$', 'convert', {
+        'form_class': UserCreationForm}, name='test_bad_convert'),
+    url(r'^good-custom-convert/$', 'convert', {
+        'form_class': GoodUserCreationForm}, name='test_good_convert'),
+)
