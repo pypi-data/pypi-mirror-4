7f1fa18a0bbfbdc31585df9fcf17addb23afbf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:14 +0000

    Add package osdcquery/0.0.4dev/osdcquery-0.0.4dev.tar.gz

---
+++ packages/osdcquery/osdcquery-0.0.4dev.tar.gz/osdcquery-0.0.4dev/osdcquery/dir_build.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# Apache 2 license 
+
+import os
+
+class TcgaDirBuild(object):
+    ''' Returns a dictionary of filepath and names with the keys being
+    symlinks to create and the values being the original files to be the
+    targets of the links '''
+
+    def __init__(self, old_dir, new_dir):
+	self.old_dir = old_dir
+	self.new_dir = new_dir
+
+
+    def associate(self, metadata):
+	''' Create files using the metadata dictionary.'''
+	
+	return {
+	    os.path.join(self.new_dir,  entry['files'][0]['filename']):
+	    os.path.join(self.old_dir, 
+		entry['disease_abbr'],
+		entry['analysis_id'], 
+		entry ['files'][0]['filename']
+	    )
+	    for entry in metadata
+	}
---
+++ packages/osdcquery/osdcquery-0.0.4dev.tar.gz/osdcquery-0.0.4dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='osdcquery',
+    version='0.0.4dev',
+    packages=['osdcquery',],
+    license='Apache License 2.0"',
+    long_description=open('README.txt').read(),
+)
