10c3d573ddf71a841926212f71d9d799b216af59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:56 +0000

    Add package pysofia/0.1/pysofia-0.1.tar.gz

---
+++ packages/pysofia/pysofia-0.1.tar.gz/pysofia-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+import glob
+import numpy as np
+
+sources =['pysofia/_sofia_ml.pyx'] + glob.glob('pysofia/src/*.cc')
+import pysofia
+
+setup(name='pysofia',
+    version=pysofia.__version__,
+    description='Python bindings for sofia-ml',
+    author='Fabian Pedregosa',
+    author_email='fabian@fseoane.net',
+    url='',
+    packages=['pysofia'],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension('pysofia._sofia_ml',
+        sources=sources,
+        language='c++', include_dirs=[np.get_include()])],
+)
