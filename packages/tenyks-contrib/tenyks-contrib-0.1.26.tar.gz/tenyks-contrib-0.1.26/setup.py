207b3dec63a07540985725ef77560d0e6a71b986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:03 +0000

    Add package tenyks-contrib/0.1.26/tenyks-contrib-0.1.26.tar.gz

---
+++ packages/tenyks-contrib/tenyks-contrib-0.1.26.tar.gz/tenyks-contrib-0.1.26/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.26'
+
+setup(name='tenyks-contrib',
+      version=version,
+      description="Contributed clients for the tenyks IRC bot",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='clients tenyks ircbot services tenyksclient',
+      author='Kyle Terry',
+      author_email='kyle@kyleterry.com',
+      url='https://github.com/kyleterry/tenyks-contrib',
+      license='LICENSE',
+      packages=find_packages('src', exclude=['ez_setup', 'examples', 'tests']),
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'tenyksclient==0.1.24',
+          'feedparser',
+      ],
+      entry_points={
+          'console_scripts': [
+              'tenyksfeeds = tenyksfeeds.main:main',
+              'tenyksleetpoints = tenyksleetpoints.main:main',
+              'tenyksmusic = tenyksmusic.main:main',
+              'tenykssearch = tenykssearch.core:main',
+              'tenykshi = tenykshi.main:main',
+          ]
+      },
+      )
