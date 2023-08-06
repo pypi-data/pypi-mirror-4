51c8dc1ba9135b1d6630022c6c7fd20484b54b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:02 +0000

    Add package confetti/2.0.0.dev2/confetti-2.0.0.dev2.tar.gz

---
+++ packages/confetti/confetti-2.0.0.dev2.tar.gz/confetti-2.0.0.dev2/confetti/exceptions.py
@@ -0,0 +1,17 @@
+class ConfigException(Exception):
+    pass
+
+class CannotSetValue(ConfigException):
+    pass
+
+class InvalidPath(ConfigException):
+    pass
+
+class CannotDeduceType(ConfigException):
+    pass
+
+class CannotResolveError(ConfigException):
+    pass
+
+class NoBackup(ConfigException):
+    pass
