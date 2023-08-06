f46e24375fe713fe0bd729b23f43b32ab24e49b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:34 +0000

    Add package eea.tags/5.2/eea.tags-5.2.zip

---
+++ packages/eea.tags/eea.tags-5.2.zip/eea.tags-5.2/eea/tags/widgets/subtypes.py
@@ -0,0 +1,28 @@
+""" Overrides default keywords widget
+"""
+from zope.interface import implements
+from archetypes.schemaextender.interfaces import ISchemaModifier, \
+    IBrowserLayerAwareExtender
+from eea.tags.browser.interfaces import IEEATagsLayer
+
+
+class SchemaModifier(object):
+    """ EEA Tags keywords widget
+    """
+    implements(ISchemaModifier, IBrowserLayerAwareExtender)
+    layer = IEEATagsLayer
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        """ Modify schema
+        """
+        if 'subject' not in schema:
+            return
+
+        xfield = schema['subject'].copy()
+        xfield.widget.macro = 'eea.tags'
+        xfield.widget.helper_js = ('++resource++eea.tags.js',)
+        xfield.widget.helper_css = ('++resource++eea.tags.css',)
+        schema['subject'] = xfield
