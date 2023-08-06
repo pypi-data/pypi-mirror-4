c336ea5d4a320832b5df9723ea09d4282b034033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package pkginfo/1.0/pkginfo-1.0.tar.gz

---
+++ packages/pkginfo/pkginfo-1.0.tar.gz/pkginfo-1.0/pkginfo/tests/test_sdist.py
@@ -0,0 +1,88 @@
+import unittest
+
+class SDistTests(unittest.TestCase):
+
+    def _getTargetClass(self):
+        from pkginfo.sdist import SDist
+        return SDist
+
+    def _makeOne(self, filename=None, metadata_version=None):
+        if metadata_version is not None:
+            return self._getTargetClass()(filename, metadata_version)
+        return self._getTargetClass()(filename)
+
+    def _checkSample(self, sdist, filename):
+        self.assertEqual(sdist.filename, filename)
+        self.assertEqual(sdist.name, 'mypackage')
+        self.assertEqual(sdist.version, '0.1')
+        self.assertEqual(sdist.keywords, None)
+        self.assertEqual(list(sdist.supported_platforms), [])
+
+    def _checkClassifiers(self, sdist):
+        self.assertEqual(list(sdist.classifiers),
+                         ['Development Status :: 4 - Beta',
+                          'Environment :: Console (Text Based)',
+                         ])
+
+    def test_ctor_w_invalid_filename(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/nonesuch-0.1.tar.gz' % d
+        self.assertRaises(ValueError, self._makeOne, filename)
+
+    def test_ctor_wo_PKG_INFO(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/nopkginfo-0.1.zip' % d
+        self.assertRaises(ValueError, self._makeOne, filename)
+
+    def test_ctor_w_gztar(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.tar.gz' % d
+        sdist = self._makeOne(filename)
+        self.assertEqual(sdist.metadata_version, '1.0')
+        self._checkSample(sdist, filename)
+
+    def test_ctor_w_gztar_and_metadata_version(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.tar.gz' % d
+        sdist = self._makeOne(filename, metadata_version='1.1')
+        self._checkSample(sdist, filename)
+        self.assertEqual(sdist.metadata_version, '1.1')
+        self._checkClassifiers(sdist)
+
+    def test_ctor_w_bztar(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.tar.bz2' % d
+        sdist = self._makeOne(filename)
+        self.assertEqual(sdist.metadata_version, '1.0')
+        self._checkSample(sdist, filename)
+
+    def test_ctor_w_bztar_and_metadata_version(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.tar.bz2' % d
+        sdist = self._makeOne(filename, metadata_version='1.1')
+        self.assertEqual(sdist.metadata_version, '1.1')
+        self._checkSample(sdist, filename)
+        self._checkClassifiers(sdist)
+
+    def test_ctor_w_zip(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.zip' % d
+        sdist = self._makeOne(filename)
+        self.assertEqual(sdist.metadata_version, '1.0')
+        self._checkSample(sdist, filename)
+
+    def test_ctor_w_zip_and_metadata_version(self):
+        import os
+        d, _ = os.path.split(__file__)
+        filename = '%s/../../docs/examples/mypackage-0.1.zip' % d
+        sdist = self._makeOne(filename, metadata_version='1.1')
+        self.assertEqual(sdist.metadata_version, '1.1')
+        self._checkSample(sdist, filename)
+        self._checkClassifiers(sdist)
