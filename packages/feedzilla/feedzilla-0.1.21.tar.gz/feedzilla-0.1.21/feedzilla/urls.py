838a08766968e207710e758f1f4d177a77532fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package feedzilla/0.1.21/feedzilla-0.1.21.tar.gz

---
+++ packages/feedzilla/feedzilla-0.1.21.tar.gz/feedzilla-0.1.21/feedzilla/urls.py
@@ -0,0 +1,18 @@
+# Copyright: 2011, Grigoriy Petukhov
+# Author: Grigoriy Petukhov (http://lorien.name)
+# License: BSD
+from django.conf.urls.defaults import *
+
+from feedzilla.syndication import PostFeed
+
+urlpatterns = patterns('feedzilla.views',
+    url(r'^$', 'index', name='feedzilla_index'),
+    url('^tag/(?P<tag_value>.+)$', 'tag', name='feedzilla_tag'),
+    url('^sources$', 'source_list', name='feedzilla_source_list'),
+    url('^search$', 'search', name='feedzilla_search'),
+    url('^submit$', 'submit_blog', name='feedzilla_submit_blog'),
+)
+
+urlpatterns += patterns('django.contrib.syndication.views',
+    url(r'^ru/projects/feed$', PostFeed(), name='feedzilla_feed'),
+)
---
+++ packages/feedzilla/feedzilla-0.1.21.tar.gz/feedzilla-0.1.21/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'feedzilla',
+    version = '0.1.21',
+    description = 'Django application for atom/rss feeds aggregation i.e. planet engine',
+    url = 'http://bitbucket.org/lorien/feedzilla',
+    author = 'Grigoriy Petukhov',
+    author_email = 'lorien@lorien.name',
+
+    packages = find_packages(),
+    include_package_data = True,
+
+    license = "BSD",
+    keywords = "django application feeds syndication aggregation atom rss planet",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
