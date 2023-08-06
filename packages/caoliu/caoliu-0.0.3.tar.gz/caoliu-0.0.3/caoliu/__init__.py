8cadd3de072f7f4f473bf911720b55da49093828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:51 +0000

    Add package caoliu/0.0.3/caoliu-0.0.3.tar.gz

---
+++ packages/caoliu/caoliu-0.0.3.tar.gz/caoliu-0.0.3/caoliu/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#encoding:utf-8
+
+url = 'http://baidu.com'
+
---
+++ packages/caoliu/caoliu-0.0.3.tar.gz/caoliu-0.0.3/setup.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+from distutils.core import setup  
+setup(name='caoliu',  
+      author='TY',  
+      author_email='tianyu0915@gmail.com',  
+      version='0.0.3',  
+      description='你懂',  
+      keywords ='你懂',
+      url='http://github.com/tianyu0915/caoliu',  
+      packages=['caoliu'],  
+      package_data={'caoliu':['*.*','caoliu/*.*']},
+
+)  
