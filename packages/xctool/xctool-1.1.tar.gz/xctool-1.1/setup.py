768c47dca7760e0a60b41fe823709d39550c71b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package xctool/1.1/xctool-1.1.tar.gz

---
+++ packages/xctool/xctool-1.1.tar.gz/xctool-1.1/setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup( name="xctool",
+       version='1.1',
+       description='this is a xctool,hehe!',
+       author='xing chao',
+       author_email='pbxingchao@yahoo.com.cn',
+       url='http://www.sysxboy.com/',
+       py_modules=['mod1'],
+       )
