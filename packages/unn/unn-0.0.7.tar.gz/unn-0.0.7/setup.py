471f69948baf83630a0a0535129881a12082aa85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:00 +0000

    Add package unn/0.0.7/unn-0.0.7.tar.gz

---
+++ packages/unn/unn-0.0.7.tar.gz/unn-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+requires = open('requirements.txt').read().split('\n')
+requires = [' (=='.join(x.split('==')) + ')' for x in requires if x]
+
+setup(
+    name = 'unn',
+    version = '0.0.7',
+    packages = ['unn',],
+    scripts = ['unn/unn'],
+    license = 'MIT',
+    requires = requires,
+    long_description = open('README.md').read(),
+)
