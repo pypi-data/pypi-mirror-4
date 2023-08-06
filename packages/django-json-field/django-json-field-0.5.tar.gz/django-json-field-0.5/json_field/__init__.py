5862b1754e5107eb0c79703b5deb65f61b999128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:43 +0000

    Add package django-json-field/0.5/django-json-field-0.5.tar.gz

---
+++ packages/django-json-field/django-json-field-0.5.tar.gz/django-json-field-0.5/json_field/__init__.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ImproperlyConfigured
+try:
+    from json_field.fields import JSONField
+except (ImportError, ImproperlyConfigured):
+    pass # fails when imported by setup.py, no worries
+
+__version__ = '0.5'
