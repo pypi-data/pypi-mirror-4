132093099d1861e489d4cf1ec0d07037eb6065eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:42 +0000

    Add package PyAvaTax/1.0rc/PyAvaTax-1.0rc.tar.gz

---
+++ packages/PyAvaTax/PyAvaTax-1.0rc.tar.gz/PyAvaTax-1.0rc/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+# python setup.py sdist
+# python setup.py sdist bdist_wininst upload
+
+setup(
+    name='PyAvaTax',
+    url = 'http://github.com/activefrequency/pyavatax/',
+    author = 'John Obelenus',
+    author_email = 'jobelenus@activefrequency.com',
+    version='1.0rc',
+    install_requires = ['requests==1.1', 'decorator==3.4.0', 'suds==0.4'],
+    package_data = {
+        '': ['*.txt', '*.rst', '*.md']
+    },
+    packages=['pyavatax',],
+    license='BSD',
+    long_description="PyAvaTax is a Python library for easily integrating Avalara's RESTful AvaTax API Service",
+)
+
