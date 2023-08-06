b45517a29bf2ec288ac6d63642fe0e45680915cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:29 +0000

    Add package GraphGit/0.2.0/GraphGit-0.2.0.tar.gz

---
+++ packages/GraphGit/GraphGit-0.2.0.tar.gz/GraphGit-0.2.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+setup(name='GraphGit',
+      description='Git repository graph generator',
+      author='Ashish Prasad (codemaniac)',
+      author_email='ashish.ap.rao@gmail.com',
+      url = "https://github.com/codemaniac/graphgit/tree/v0.2.0",
+      version='0.2.0',
+      packages=['graphgit'],
+      scripts=['bin/graphgit'],
+      install_requires=['argparse', 'networkx', 'GitPython'],
+      license = "BSD",
+      keywords = "git repository graph visualization network analytics"
+      )
