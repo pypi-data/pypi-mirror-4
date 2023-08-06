f4f5cc945e637b27c1e72fa43aab9a21a5d58005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:41 +0000

    Add package Erik/0.0.2/Erik-0.0.2.tar.gz

---
+++ packages/Erik/Erik-0.0.2.tar.gz/Erik-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+import sys
+from subprocess import Popen,PIPE
+
+README = open('README.rst').read()
+
+def all_files(path,num_root_dir_to_skip=1):
+    all= map(lambda x: x.strip(),Popen(['find',path],stdout=PIPE).stdout.readlines())
+    return map(lambda x: '/'.join(x.split('/')[num_root_dir_to_skip:]), filter(os.path.isfile,all))
+
+setup(name='Erik',
+    version='0.0.2',
+    description = "Erik Misc",
+    author='Erik Gafni',
+    license='Non-commercial',
+    package_data={'erik':['README.rst','LICENSE']},
+    long_description=README,
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
