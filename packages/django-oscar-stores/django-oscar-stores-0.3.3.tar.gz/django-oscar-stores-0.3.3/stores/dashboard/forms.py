4598adc83c8087f75e6735c48a0e5f4db21a4bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:23 +0000

    Add package django-oscar-stores/0.3.3/django-oscar-stores-0.3.3.tar.gz

---
+++ packages/django-oscar-stores/django-oscar-stores-0.3.3.tar.gz/django-oscar-stores-0.3.3/stores/dashboard/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.db.models import get_model
+from django.contrib.gis.forms import fields
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.gis.geoip import HAS_GEOIP
+
+
+class StoreAddressForm(forms.ModelForm):
+
+    class Meta:
+        model = get_model('stores', 'StoreAddress')
+        exclude = ('title', 'first_name', 'last_name', 'search_text')
+
+
+class StoreForm(forms.ModelForm):
+    location = fields.GeometryField(widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        current_ip = kwargs.pop('current_ip', None)
+        super(StoreForm, self).__init__(*args, **kwargs)
+
+        # Make sure that we store the initial data as GeoJSON so that
+        # it is easier for us to use it in Javascript.
+        instance = kwargs.get('instance', None)
+        if instance:
+            self.initial['location'] = instance.location.geojson
+        elif HAS_GEOIP:
+            from django.contrib.gis.geoip import GeoIP
+            point = GeoIP().geos(current_ip)
+            if point:
+                self.initial['location'] = point.geojson
+
+    def clean_reference(self):
+        ref = self.cleaned_data['reference']
+        if ref == "":
+            return None
+        return ref
+
+    class Meta:
+        model = get_model('stores', 'Store')
+        exclude = ('slug',)
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 40, 'rows': 10}),
+        }
+
+
+class OpeningPeriodForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(OpeningPeriodForm, self).__init__(*args, **kwargs)
+        time_input = ['%H:%M', '%H', '%I:%M%p', '%I%p', '%I:%M %p', '%I %p']
+        self.fields['start'].input_formats = time_input
+        self.fields['end'].input_formats = time_input
+
+    class Meta:
+        model = get_model('stores', 'OpeningPeriod')
+        widgets = {
+            'name': forms.TextInput(
+                attrs={'placeholder': _("e.g. Christmas")}
+            ),
+            'start': forms.TimeInput(
+                format='%H:%M',
+                attrs={'placeholder': _("e.g. 9 AM, 11:30, etc.")}
+            ),
+            'end': forms.TimeInput(
+                format='%H:%M',
+                attrs={'placeholder': _("e.g. 5 PM, 18:30, etc.")}
+            ),
+        }
