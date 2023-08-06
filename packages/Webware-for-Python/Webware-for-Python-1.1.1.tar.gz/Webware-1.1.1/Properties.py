f793b3ea94505cea88327ac4ce1ae7a1d6d75980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package Webware-for-Python/1.1.1/Webware-for-Python-1.1.1.tar.gz

---
+++ packages/Webware-for-Python/Webware-for-Python-1.1.1.tar.gz/Webware-1.1.1/Properties.py
@@ -0,0 +1,11 @@
+name = 'Webware for Python'
+
+version = (1, 1, 1)
+
+status = 'stable'
+
+releaseDate = '01/18/13'
+
+requiredPyVersion = (2, 4, 0)
+
+synopsis = """Webware provides a suite of Python components for developing Web applications. Included are an app server, servlets, Python Server Pages (PSP), Kid template plug-in (KidKit), object-relational mapping and task scheduling. The architecture is very modular, and you can provide your own extensions."""
