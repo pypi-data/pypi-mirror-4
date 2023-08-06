6f07a2f7e18a42138db143a8625dad2b2659b9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:21 +0000

    Add package nester222/1.0.0/nester222-1.0.0.zip

---
+++ packages/nester222/nester222-1.0.0.zip/nester222-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='nester222',
+    version     ='1.0.0',
+    py_modules  =['nester222'],
+    author      ='hfpython',
+    author_email='hfpython@asdf',
+    url         ='www.linian.com',
+    description ="asdfasfdasdf",
+    )
