53aeabe050d6199e70e5589958b3454f906964c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:38 +0000

    Add package uasparser2/0.1/uasparser2-0.1.tar.gz

---
+++ packages/uasparser2/uasparser2-0.1.tar.gz/uasparser2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='uasparser2',
+      version='0.1',
+      author='Jure Ham',
+      license='BSD',
+      author_email='jure.ham@zemanta.com',
+      description="Fast and reliable User-Agent parser for Python.",
+      url='https://github.com/hamaxx/uasparser2',
+      packages=['uasparser2',],
+      zip_safe=True)
