bb21cb52e362f833c52639aef7dd7855c9bf75a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:50 +0000

    Add package configs/1.3/configs-1.3.tar.gz

---
+++ packages/configs/configs-1.3.tar.gz/configs-1.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='configs',
+    version='1.3',
+    description='Configuration for humans',
+    author='Konstantin Molchanov',
+    author_email='moigagoo@myopera.com',
+    url='https://bitbucket.org/moigagoo/configs',
+    py_modules=['configs'],
+    data_files=[
+        ('test', ['test.conf']),
+    ],
+    license='MIT'
+)
