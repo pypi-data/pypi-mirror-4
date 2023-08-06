4d2718ca4c6c03e85fa117b21de63a888bd977f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:37 +0000

    Add package raid-guard/0.1/raid-guard-0.1.tar.gz

---
+++ packages/raid-guard/raid-guard-0.1.tar.gz/raid-guard-0.1/README.rst
@@ -0,0 +1,71 @@
+===========
+Description
+===========
+
+Monitoring hard discs in a raid system on linux servers. Raid Guard runs as a 
+daemon and will send mails on specific events.
+
+**License**
+
+    MIT License
+
+**Notes**
+
+    * Only tested on a linux server
+
+========
+Features
+========
+
+    * runs as a daemon.
+    * send a mail if a hard drive loss.
+    * setup via configuration file.
+
+============
+Installation
+============
+
+**Dependences**
+
+    * None
+
+**Installation / Deinstallation**
+
+    *with pip*
+        
+        ::
+        
+        pip install raid-guard
+
+        pip uninstall raid-guard
+
+
+    *or setup.py*
+
+        1. Unpack your package.
+        2. Open a terminal and change to the folder which contains the setup.py and type
+
+        ::
+
+            python setup.py install
+   
+=====
+Setup
+=====
+    
+    * edit the /usr/share/raid_guard/raid_guard.ini an copy it to /root/.raid_guard.ini
+      or start the prog the first time and the ini file will copied automaticly to root.
+    * Add an entry into the /etc/rc.local file: /usr/share/raid_guard/raid_guard start
+
+=====
+Usage
+=====
+
+    raid_guard option
+    options are: start, stop, status, help, test, logfile
+    
+=====
+Hints
+=====
+
+    * The logfile is stored at /var/log/raid_guard.log
---
+++ packages/raid-guard/raid-guard-0.1.tar.gz/raid-guard-0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# This Python file uses the following encoding: utf-8
+
+
+from setuptools import setup, find_packages
+from glob import glob
+import os
+
+# strings
+lib_path = '/usr/share/raid_guard'
+
+# Get description from Readme file
+readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(readme_file).read()
+
+setup(  
+    name='raid-guard',
+    version=0.1,
+    description='raid hard disc monitoring daemon for linux servers',
+    long_description=long_description,
+    author='Andreas Fritz',
+    author_email='raid_guard@bitzeit.eu',
+    url='http://www.sources.e-blue.eu/',
+    download_url='https://pypi.python.org/pypi',
+    packages = find_packages(),
+    entry_points={'console_scripts':['raid_guard=raid_guard:main'],},
+    include_package_data=True,
+    data_files=[
+                (lib_path, glob('raid_guard.ini')),
+                (lib_path, glob('README.rst')),
+                (lib_path, glob('LICENSE')),
+                ],
+    keywords="raid hard-disk server monitoring daemon",
+    classifiers=[
+                'Development Status :: 3 - Alpha',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: Unix',
+                'Programming Language :: Python :: 2.7',
+                'Environment :: Console',
+                'Natural Language :: English',
+                'Intended Audience :: System Administrators',
+                'Topic :: System :: Networking :: Monitoring',
+                'Topic :: System :: Networking :: Monitoring :: Hardware Watchdog',
+                'Topic :: System :: Systems Administration',
+                ],
+    install_requires = ['deiman','iniparse'],
+    )
