d6f0218d99f68fe58ef097a795ead395c51776ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:26 +0000

    Add package customevent/0.3/customevent-0.3.tar.gz

---
+++ packages/customevent/customevent-0.3.tar.gz/customevent-0.3/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="customevent",
+      version="0.3",
+      description="Lightweight Python Custom Event Module",
+      author="Minacle",
+      author_email="mayuki.minano@gmail.com",
+      url="https://github.com/minacle/python-customevent",
+      packages=["customevent"],
+     )
