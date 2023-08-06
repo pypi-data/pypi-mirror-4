744de125f25ff76f4a72a38a71cc686b8766e5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package collective.editskinswitcher/2.5/collective.editskinswitcher-2.5.zip

---
+++ packages/collective.editskinswitcher/collective.editskinswitcher-2.5.zip/collective.editskinswitcher-2.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+readme = open('README.txt').read().strip()
+history = open('CHANGES.rst').read().strip()
+long_description = "%s\n\n\n%s" % (readme, history)
+
+setup(name='collective.editskinswitcher',
+      version='2.5',
+      description="Switch to the edit skin for certain domains.""",
+      long_description=long_description,
+      classifiers=[
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Zope2",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url="https://github.com/collective/collective.editskinswitcher",
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.app.publication',
+          'zope.app.publisher',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
