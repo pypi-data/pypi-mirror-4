9ca42293d45f46e8848b335cc42da8f9d24de1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:21 +0000

    Add package z3c.template/2.0.0a1/z3c.template-2.0.0a1.zip

---
+++ packages/z3c.template/z3c.template-2.0.0a1.zip/z3c.template-2.0.0a1/setup.py
@@ -0,0 +1,86 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='z3c.template',
+    version='2.0.0a1',
+    author = "Roger Ineichen and the Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "A package implementing advanced Page Template patterns.",
+    long_description=(
+        read('README.txt')
+        + '\n\n.. contents::\n\n'+
+        read('src', 'z3c', 'template', 'README.txt')
+        + '\n' +
+        read('src', 'z3c', 'template', 'zcml.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 template layout zpt pagetemplate",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.template',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'zope.testing',
+            'zope.traversing',
+            ],
+        chameleon = [
+            'z3c.pt >= 2.1',
+            'z3c.ptcompat>=1.0',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.browserpage',
+        'zope.component',
+        'zope.configuration',
+        'zope.interface',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.schema',
+        ],
+    tests_require = [
+        'zope.testing',
+        'zope.traversing',
+        ],
+    test_suite = 'z3c.template.tests.test_suite',
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/z3c.template/z3c.template-2.0.0a1.zip/z3c.template-2.0.0a1/src/z3c/template/tests.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests
+"""
+import doctest
+import itertools
+import re
+import unittest
+import zope.component
+from zope.configuration import xmlconfig
+from zope.component import testing
+from zope.testing import renormalizing
+
+import z3c.template.template
+
+checker = renormalizing.RENormalizing([
+    # Python 3 unicode removed the "u".
+    #(re.compile("u('.*?')"),
+    # r"\1"),
+    #(re.compile('u(".*?")'),
+    # r"\1"),
+    # Python 3 adds module name to exceptions.
+    (re.compile("zope.interface.interfaces.ComponentLookupError"),
+     r"ComponentLookupError"),
+    ])
+
+try:
+    import z3c.ptcompat
+except ImportError:
+    Z3CPT_AVAILABLE = False
+else:
+    Z3CPT_AVAILABLE = True
+
+
+def setUp(test):
+    testing.setUp(test)
+    # Traversal Setup
+    from zope.traversing.testing import setUp
+    setUp()
+    test.globs['root'] = object()
+
+def tearDown(test):
+    testing.tearDown(test)
+    #setup.placefulTearDown()
+
+def setUpZPT(suite):
+    setUp(suite)
+
+def setUpZ3CPT(suite):
+    setUp(suite)
+    import z3c.pt
+    import z3c.ptcompat
+    xmlconfig.XMLConfig('configure.zcml', z3c.pt)()
+    xmlconfig.XMLConfig('configure.zcml', z3c.ptcompat)()
+
+    # We have to cook this template explicitly, because it's a module
+    # global.
+    from z3c.template.template import Macro
+    Macro.wrapper._cook()
+
+
+def test_suite():
+    setups = (setUpZPT,)
+    if Z3CPT_AVAILABLE:
+        setups += (setUpZ3CPT,)
+    tests = ((
+        doctest.DocFileSuite('README.txt',
+            setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            checker=checker,
+            ),
+        doctest.DocFileSuite('zcml.txt', setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            checker=checker,
+            ),
+        ) for setUp in setups)
+
+    return unittest.TestSuite(itertools.chain(*tests))
