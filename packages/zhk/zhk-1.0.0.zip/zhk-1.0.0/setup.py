90cbd1b5e4832de580d302bab5488721478c75c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:57 +0000

    Add package zhk/1.0.0/zhk-1.0.0.zip

---
+++ packages/zhk/zhk-1.0.0.zip/zhk-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zhk',
+    version='1.0.0',
+    py_modules=['zhw'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A 汤司令佳作时间simple printer of dylist lists',
+    )
+   
