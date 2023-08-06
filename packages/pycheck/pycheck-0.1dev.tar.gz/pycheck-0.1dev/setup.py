277905eeec1a93b2f625f9ac961cd6d79f2ac0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package pycheck/0.1dev/pycheck-0.1dev.tar.gz

---
+++ packages/pycheck/pycheck-0.1dev.tar.gz/pycheck-0.1dev/setup.py
@@ -0,0 +1,25 @@
+'''
+Created on Jan 13, 2013
+
+@author: Scott Pigman
+'''
+from distutils.core import setup
+
+setup(
+    name='pycheck',
+    version='0.1dev',
+    description='Type checking tools for debugging and developing Python',
+    packages=['pycheck', 'pycheck.test'],
+    classifiers=[
+                 'Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Intended Audience :: Developers'
+                ],
+    long_description=open('README.TXT').read(),
+    author='Scott Pigman',
+    author_email='scott.pigman@gmail.com',
+    
+)
