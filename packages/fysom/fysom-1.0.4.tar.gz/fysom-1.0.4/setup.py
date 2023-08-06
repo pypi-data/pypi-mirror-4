7ebebecd6f91b06bf75e9a707411c98bdc5f84e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:11 +0000

    Add package fysom/1.0.4/fysom-1.0.4.tar.gz

---
+++ packages/fysom/fysom-1.0.4.tar.gz/fysom-1.0.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'fysom',
+          version = '1.0.4',
+          description = '',
+          long_description = '''''',
+          author = "Mansour Behabadi, Jake Gordon, Maximilien Riehl",
+          author_email = "mansour@oxplot.com, jake@codeincomplete.com, maximilien.riehl@gmail.com",
+          license = 'MIT',
+          url = 'https://github.com/mriehl/fysom',
+          scripts = [],
+          packages = ['fysom'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+          
+          
+          
+          
+          zip_safe=True
+    )
