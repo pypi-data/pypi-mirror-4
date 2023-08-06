deb77bf86f0e394f8971850e1f1fc05f126d8cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:20 +0000

    Add package as.recipe.frozenpkg/0.2.21/as.recipe.frozenpkg-0.2.21.tar.gz

---
+++ packages/as.recipe.frozenpkg/as.recipe.frozenpkg-0.2.21.tar.gz/as.recipe.frozenpkg-0.2.21/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2.6
+
+from setuptools import setup, find_packages
+import os
+
+
+__VERSION__ = '0.2.21'
+
+
+
+
+setup(
+    name                = "as.recipe.frozenpkg",
+    description         = "ZC Buildout recipe for freezing buildouts in RPM's, tar.gz's, etc",
+    long_description    = open('README.md', 'rt').read(),
+    version             = __VERSION__,
+
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+
+    keywords        = 'buildout recipe',
+    author          = 'Alvaro Saurin',
+    author_email    = 'name dot surname at gmail.com',
+
+    packages        = find_packages(exclude = ['ez_setup']),
+
+    namespace_packages = ['as',
+                          'as.recipe'],
+
+    include_package_data = True,
+    install_requires = [
+        'distribute',
+        'virtualenv',
+    ],
+
+    license             = 'GPL',
+    zip_safe            = False,
+
+    entry_points        = {
+        'zc.buildout': [
+            'default = as.recipe.frozenpkg.frozenrpm:FrozenRPM',
+            'rpm     = as.recipe.frozenpkg.frozenrpm:FrozenRPM',
+            'tgz     = as.recipe.frozenpkg.frozentgz:FrozenTgz'
+        ]
+    },
+)
+
