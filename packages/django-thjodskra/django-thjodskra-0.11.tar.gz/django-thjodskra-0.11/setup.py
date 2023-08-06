1b14cb6e5dfe94b0e7fe51a77b8e71479132d786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:19 +0000

    Add package django-thjodskra/0.11/django-thjodskra-0.11.tar.gz

---
+++ packages/django-thjodskra/django-thjodskra-0.11.tar.gz/django-thjodskra-0.11/setup.py
@@ -0,0 +1,16 @@
+#encoding: utf-8
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-thjodskra",
+    version = "0.11",
+    packages = find_packages(),
+    author = "Úlfur Kristjánsson",
+    author_email = "ulfur@theawesometastic.com",
+    description = "A package for integrating the icelandic national registry into your Django project.",
+    url = "https://github.com/ulfur/django-thjodskra",
+    include_package_data = True
+)
