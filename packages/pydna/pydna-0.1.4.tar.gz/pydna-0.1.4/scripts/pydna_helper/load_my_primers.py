5d5d20df9102bea4d8d8b908abd2747de41ce94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package pydna/0.1.4/pydna-0.1.4.tar.gz

---
+++ packages/pydna/pydna-0.1.4.tar.gz/pydna-0.1.4/scripts/pydna_helper/load_my_primers.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from Bio import SeqIO
+from Bio.Alphabet.IUPAC import IUPACAmbiguousDNA
+
+from pydna import parse
+
+global new_primer
+global primer
+global old_primer
+global new_primer_dict
+global primer_dict
+global old_primer_dict
+
+new_primer = parse("/home/bjorn/Dropbox/wikidata/PrimersToBuy.wiki", ds=False)
+primer     = parse("/home/bjorn/Dropbox/wikidata/Primers.wiki", ds=False)
+
+primer     = primer[::-1]
+old_primer = primer[:37]
+primer     = primer[37:]
+
+new_primer_dict         = dict((p.id, p) for p in new_primer)
+primer_dict             = dict((p.id, p) for p in primer)
+old_primer_dict         = dict((p.id, p) for p in old_primer)
+
+assert str(primer_dict["509_mycGFPr"].seq) == "CTACTTGTACAGCTCGTCCA"
+assert primer[0].id == "0_S1"
+assert primer[580].id == "580_GXF1_YPK_fwd"
+
+print "{:3d} old primers    -> old_primer [list]".format(len(old_primer))
+print "{:3d} primers        -> primer [list]".format(len(primer))
+print "{:3d} primers to buy -> new_primer [list]".format(len(new_primer))
