7a5dc1e01311c75a62a3cd775805d86845ecde27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:44 +0000

    Add package flasker/0.1.15/flasker-0.1.15.tar.gz

---
+++ packages/flasker/flasker-0.1.15.tar.gz/flasker-0.1.15/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='flasker',
+    version='0.1.15',
+    description='Flasker',
+    long_description=open('README.rst').read(),
+    author='Matthieu Monsch',
+    author_email='monsch@mit.edu',
+    url='https://github.com/mtth/flasker',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: System :: Distributed Computing',
+      'Programming Language :: Python',
+    ],
+    install_requires=[
+      'celery',
+      'flask',
+      'flask-login',
+      'sqlalchemy',
+      'redis',
+      'flower'
+    ],
+    package_data={'flasker': [
+      'components/templates/*',
+      'data/**/**/*',
+      'configs/*',
+    ]},
+    include_package_data=True,
+    entry_points={'console_scripts': ['flasker = flasker.__main__:main']},
+)
