46a85f34d52e5f9cf7590c139196efdac841c040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package redturtle.monkey/1.0/redturtle.monkey-1.0.zip

---
+++ packages/redturtle.monkey/redturtle.monkey-1.0.zip/redturtle.monkey-1.0/redturtle/monkey/tests/test_vocabularies.py
@@ -0,0 +1,33 @@
+import unittest2 as unittest
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+
+from redturtle.monkey.testing import \
+    REDTURTLE_MONKEY_INTEGRATION_TESTING
+
+
+class TestMonkeyVocabularies(unittest.TestCase):
+
+    layer = REDTURTLE_MONKEY_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def test_template_vocabs(self):
+        vfactory = getUtility(IVocabularyFactory,
+                      name='redturtle.monkey.vocabularies.AvailableTemplates')
+        vocab = vfactory(self.portal)
+        self.assertTrue(u'My template' in [a.title for a in vocab])
+
+    def test_list_vocabs(self):
+        vfactory = getUtility(IVocabularyFactory,
+                      name='redturtle.monkey.vocabularies.AvailableLists')
+        vocab = vfactory(self.portal)
+        self.assertTrue(u'ACME Newsletter' in [a.title for a in vocab])
+
+    def test_slots_vocabs(self):
+        vfactory = getUtility(IVocabularyFactory,
+                      name='redturtle.monkey.vocabularies.AvailableSlots')
+        vocab = vfactory(self.portal)
+        self.assertTrue(u'main_primopiano' in [a.title for a in vocab])
---
+++ packages/redturtle.monkey/redturtle.monkey-1.0.zip/redturtle.monkey-1.0/redturtle/monkey/vocabularies.py
@@ -0,0 +1,54 @@
+from zope.component import getUtility, queryMultiAdapter
+from zope.component import subscribers
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+
+from redturtle.monkey.interfaces import IMonkeyLocator, IMailchimpSlot, IMailchimpSlotRenderer
+
+
+def available_lists(context):
+    mailchimp = getUtility(IMonkeyLocator)
+    lists = mailchimp.lists(campaign=context)
+    if not lists:
+        return SimpleVocabulary([])
+    return SimpleVocabulary([
+        SimpleTerm(value=li['id'], title=li['name']) for li in lists]
+    )
+
+
+def available_templates(context):
+    mailchimp = getUtility(IMonkeyLocator)
+    templates = mailchimp.templates(campaign=context)
+    if not templates:
+        return SimpleVocabulary([])
+    return SimpleVocabulary([
+        SimpleTerm(value=str(li['id']), title=li['name']) for li in templates]
+    )
+
+
+def available_slots(context):
+    slots = subscribers([context], IMailchimpSlot)
+    return SimpleVocabulary([
+        SimpleTerm(value=str(li.name), title=li.name) for li in slots \
+                if queryMultiAdapter((context, context.REQUEST),
+                                     IMailchimpSlotRenderer, name=li.name)]
+    )
+
+
+def email_type(context):
+    terms = []
+    terms.append(
+        SimpleTerm(
+            value='text',
+            token='text',
+            title='Plain text',
+        )
+    )
+    terms.append(
+        SimpleTerm(
+            value='html',
+            token='html',
+            title='HTML',
+        )
+    )
+    return SimpleVocabulary(terms)
