3d58fb82fec2f6c6949d4619c3301b990ed8fc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:28 +0000

    Add package printFunction/1.0.0/printFunction-1.0.0.zip

---
+++ packages/printFunction/printFunction-1.0.0.zip/printFunction-1.0.0/printFunction.py
@@ -0,0 +1,12 @@
+''' fsdafasefsd
+fawefds
+feawfasdf
+afefefffffffffffffffff
+'''
+def printList(_list):
+        """        1156456        """
+        for var in _list:
+                if isinstance(var,list):
+                        printList(var)
+                else:
+                        print(var)
