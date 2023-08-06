f63fdaa705b0823adfd3ce6872357dced9b26e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:30 +0000

    Add package Products.TinyMCE/1.3.1/Products.TinyMCE-1.3.1.zip

---
+++ packages/Products.TinyMCE/Products.TinyMCE-1.3.1.zip/Products.TinyMCE-1.3.1/Products/TinyMCE/browser/tiny_mce_gzip.js
@@ -0,0 +1,56 @@
+(function($, Patterns, undefined) {
+
+  window.initTinyMCE = function(context) {
+
+    $('textarea.mce_editable', context).each(function() {
+      var $el = $(this),
+          $field = $el.parents('.field'),
+          tinymceActive = false,
+          config = $.parseJSON($el.attr('data-mce-config'));
+
+      $('.suppressVisualEditor', $field).hide();
+      $('.fieldTextFormat > select', $field).bind('change', function(e) {
+        e.stopPropagation();
+
+        if ($(e.target).val() === 'text/html') {
+          // only activate if it unactive
+          if (!tinymceActive) {
+            $el.tinymce(config);
+            tinymceActive = true;
+          }
+
+        // only deactivate if active
+        } else if (tinymceActive) {
+          tinyMCE.execCommand('mceRemoveControl', false, $el.attr('id'));
+          tinymceActive = false;
+        }
+
+      // set Text Format dropdown untabbable for better UX
+      }).attr('tabindex', '-1');
+
+      if ($('.fieldTextFormat > select', $field).val() === 'text/html') {
+        $el.tinymce(config);
+        tinymceActive = true;
+      }
+
+    });
+  };
+
+  $(document).ready(function() {
+    if (Patterns) {
+      console.log('tinymce!!!');
+      var PloneTinyMCE = Patterns.extend({
+        name: 'plone-tinymce',
+        jqueryPlugin: 'ploneTinymce',
+        init: function() {
+          initTinyMCE(this.$el.parent());
+        }
+      });
+    } else {
+      $(document).ready(function() {
+        window.initTinyMCE(document);
+      });
+    }
+  });
+
+}(window.jQuery, window.Patterns));
