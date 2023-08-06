7ef7c3d3057558b15887ebd985437e57dd1ff497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:48 +0000

    Add package reqhash/0.1/reqhash-0.1.tar.gz

---
+++ packages/reqhash/reqhash-0.1.tar.gz/reqhash-0.1/reqhash/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import hashlib
+
+from reqhash.parser import parse_requirements
+
+
+def hash_requirements(filename):
+    requirements = parse_requirements(filename)
+    digest = hashlib.md5("\n".join(requirements)).hexdigest()
+    return digest
+
+
+def main():
+    import argparse
+    argparser = argparse.ArgumentParser(description='Calculate a digest for a requirements file.')
+    argparser.add_argument('filename', help='path to requirements file')
+    args = argparser.parse_args()
+    print hash_requirements(args.filename)
+
+
+if __name__ == '__main__':
+    main()
