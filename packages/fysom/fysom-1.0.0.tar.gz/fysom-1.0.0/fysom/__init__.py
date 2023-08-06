b882c3201ffc74dd872f671a5fd0e1b6789db2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:52 +0000

    Add package fysom/1.0.0/fysom-1.0.0.tar.gz

---
+++ packages/fysom/fysom-1.0.0.tar.gz/fysom-1.0.0/fysom/__init__.py
@@ -0,0 +1,9 @@
+from finite_state_machine import Fysom, FysomError
+
+__author__ = 'Mansour Behabadi'
+__copyright__ = 'Copyright 2011, Mansour Behabadi and Jake Gordon'
+__credits__ = ['Mansour Behabadi', 'Jake Gordon']
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__maintainer__ = 'Mansour Behabadi'
+__email__ = 'mansour@oxplot.com'
