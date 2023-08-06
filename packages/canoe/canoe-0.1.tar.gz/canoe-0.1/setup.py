93ff89305d99dcf465bf765e017b13ea8caa4091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:13 +0000

    Add package canoe/0.1/canoe-0.1.tar.gz

---
+++ packages/canoe/canoe-0.1.tar.gz/canoe-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "canoe",
+    version = "0.1",
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'canoe = canoe.main:main',
+        ],
+        },
+    author = "Andrew Gwozdziewycz",
+    author_email = "web@apgwoz.com",
+    description = "Carve out log files and paddle",
+    install_requires = ['ply'],
+    license = "GPL",
+    keywords = "log analyze alert monitor",
+    url = "https://github.com/apgwoz/canoe",
+)
