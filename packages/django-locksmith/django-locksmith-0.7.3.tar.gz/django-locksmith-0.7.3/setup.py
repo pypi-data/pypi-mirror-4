a217e45e08c95700dc7c677b14a3868a11fc45b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:01 +0000

    Add package django-locksmith/0.7.3/django-locksmith-0.7.3.tar.gz

---
+++ packages/django-locksmith/django-locksmith-0.7.3.tar.gz/django-locksmith-0.7.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django-locksmith',
+      version='0.7.3',
+      description='Django apps for API authentication and centralized authorization',
+      author='James Turk',
+      author_email='jturk@sunlightfoundation.com',
+      license='BSD',
+      url='http://github.com/sunlightlabs/django-locksmith/',
+      packages=find_packages(),
+      package_data={'locksmith': ['static/scripts/*.js', 'static/styles/*.css',
+                                  'static/bootstrap/js/*.js', 'static/bootstrap/css/*.css',
+                                  'static/bootstrap/img/*.png'],
+                    'locksmith.hub': ['templates/locksmith/*.html']},
+)
