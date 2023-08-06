eb21421d63bd6764ff7e6c22a4fe2525c8a91551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:58 +0000

    Add package Products.PlonePopoll/2.8b1/Products.PlonePopoll-2.8b1.tar.gz

---
+++ packages/Products.PlonePopoll/Products.PlonePopoll-2.8b1.tar.gz/Products.PlonePopoll-2.8b1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.8b1'
+
+setup(name='Products.PlonePopoll',
+      version=version,
+      description="A Poll tool for Plone based on archetypes.",
+      long_description=open(os.path.join("README.rst")).read() +
+      open(os.path.join("docs", "HISTORY.rst")).read() +
+      "\n\n",
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Zope Plone Poll',
+      author='Ingeniweb',
+      author_email='support@ingeniweb.com',
+      url='https://github.com/collective/Products.PlonePopoll',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
