dd160f411f35a8b1613c83140ffa673083131129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package XlsxWriter/0.0.4/XlsxWriter-0.0.4.tar.gz

---
+++ packages/XlsxWriter/XlsxWriter-0.0.4.tar.gz/XlsxWriter-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='XlsxWriter',
+    version='0.0.4',
+    author='John McNamara',
+    author_email='jmcnamara@cpan.org',
+    url='http://pypi.python.org/pypi/XlsxWriter/',
+    packages=['xlsxwriter'],
+    license='BSD',
+    description='A Python module to create Excel XLSX files.',
+    long_description=open('README.txt').read(),
+)
