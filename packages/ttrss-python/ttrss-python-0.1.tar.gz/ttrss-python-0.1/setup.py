e9624322c594bb33259df0af083132d8cda7a5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:55 +0000

    Add package ttrss-python/0.1/ttrss-python-0.1.tar.gz

---
+++ packages/ttrss-python/ttrss-python-0.1.tar.gz/ttrss-python-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_desc = f.read()
+
+setup(
+        name='ttrss-python',
+        version='0.1',
+        description='A client library for the Tiny Tiny RSS web API',
+        long_description=long_desc,
+        url='https://github.com/Vassius/ttrss-python',
+        author='Markus Wiik',
+        author_email='markus.wiik@gmail.com',
+        packages=['ttrss'],
+        data_files=[('', ['README.rst'])],
+        requires=['requests (>=1.1.0)'],
+        provides=['ttrss'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Internet :: WWW/HTTP',
+            ],
+        )
