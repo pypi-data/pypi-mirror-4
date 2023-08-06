b1e3174fdd27832265776a1cbf189737a3bf3532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:47 +0000

    Add package FlaskEx/0.0.2/FlaskEx-0.0.2.tar.gz

---
+++ packages/FlaskEx/FlaskEx-0.0.2.tar.gz/FlaskEx-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='FlaskEx',
+    version='0.0.2',
+    packages=['flaskex'],
+    install_requires=[
+        'Flask', 'pyjade', 'pytz', 'iso8601'
+    ]
+)
