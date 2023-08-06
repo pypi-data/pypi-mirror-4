46a2cebed1c1f790cd6a4d58fd6d293680037596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:56 +0000

    Add package test_pr/0.0.1/test_pr-0.0.1.zip

---
+++ packages/test_pr/test_pr-0.0.1.zip/test_pr-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='test_pr',
+    version='0.0.1',
+    py_modules=['test_pr'],
+    author='Xzeroo Chan',
+    description='It is just a test,do not use it!',
+    )
