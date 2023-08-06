110850351c0cd9171f4dd61a3b0227d368b50856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:44 +0000

    Add package googeo/0.0.2/googeo-0.0.2.tar.gz

---
+++ packages/googeo/googeo-0.0.2.tar.gz/googeo-0.0.2/setup.py
@@ -0,0 +1,18 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+from setuptools import find_packages, setup
+
+setup(
+
+name='googeo',
+
+version='0.0.2',
+
+py_modules=['googeo'],
+
+install_requires=[
+],
+
+scripts=[]
+)
+
