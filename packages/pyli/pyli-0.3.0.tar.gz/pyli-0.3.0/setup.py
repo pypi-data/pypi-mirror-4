e18d2d3254acacc5d375f781c03f71153babbde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package pyli/0.3.0/pyli-0.3.0.tar.gz

---
+++ packages/pyli/pyli-0.3.0.tar.gz/pyli-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pyli',
+    version='0.3.0',
+    author='thenoviceoof',
+    author_email='thenoviceoof@gmail.com',
+    packages=[],
+    scripts=['bin/pyli'],
+    url='https://github.com/thenoviceoof/pyli',
+    license='LICENSE',
+    description='Better python CLI integration',
+    long_description=open('README.rst').read(),
+    install_requires=[
+    ],
+)
