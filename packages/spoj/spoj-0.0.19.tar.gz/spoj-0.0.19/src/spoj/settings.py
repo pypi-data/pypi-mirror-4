2846fbaf0817160786acbba4e3d6fcdec6f0cb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package spoj/0.0.19/spoj-0.0.19.tar.gz

---
+++ packages/spoj/spoj-0.0.19.tar.gz/spoj-0.0.19/src/spoj/settings.py
@@ -0,0 +1,17 @@
+import os
+
+SPOJ_URL = 'http://www.spoj.com/'
+LOGIN_URL = SPOJ_URL
+spoj_dirname = 'ABRAMOV'
+compiler_id = None
+
+ROOM_URL = lambda : SPOJ_URL + spoj_dirname +'/'
+
+CONFIG_FILE_NAME = os.path.expanduser('~/.spojrc')
+COOKIE_FILE_NAME = os.path.expanduser('~/.spoj_cookie')
+
+user_name = None
+#cookie jar
+cj = None
+
+_url = lambda path: ROOM_URL() + path + '/'
