1505b673a19f07513466e4b6b868fbfe88ae25eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:28 +0000

    Add package smoff/0.0.2/smoff-0.0.2.tar.gz

---
+++ packages/smoff/smoff-0.0.2.tar.gz/smoff-0.0.2/setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='smoff', version='0.0.2', description='Wicket-inspired template engine for python/django', author='Michael Donaghy', author_email='michael@optimalsocial.com', url='https://bitbucket.org/lmm/smoff', packages=['smoff'], package_dir = {'': 'src'})
