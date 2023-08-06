760cabb6853a69fc46d0d66a4138ac43e7b37373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:03 +0000

    Add package django-messages-ui/0.2.1/django-messages-ui-0.2.1.tar.gz

---
+++ packages/django-messages-ui/django-messages-ui-0.2.1.tar.gz/django-messages-ui-0.2.1/CHANGES.rst
@@ -0,0 +1,63 @@
+CHANGES
+=======
+
+0.2.1 (2013.01.14)
+------------------
+
+* Rewrite using method plugin architecture; simpler 'add' method to add msg.
+* Add option to display unescaped HTML in message content.
+
+0.2.0 (2013.01.11)
+------------------
+
+* Add option to use Handlebars.js (new default) instead of ICanHaz.js.
+
+0.1.8 (2013.01.03)
+------------------
+
+* Make close-link selector specific to within a message; use preventDefault.
+
+0.1.7 (2012.11.06)
+------------------
+
+* JS stop transient-message fade on close-link click.
+
+0.1.6 (2012.10.05)
+------------------
+
+* JS don't parse non-json.
+
+0.1.5 (2012.07.23)
+------------------
+
+* Don't touch non-200 responses.
+
+0.1.4 (2011.07.14)
+------------------
+
+* JS cleanup; added JSLint options.
+
+0.1.3 (2011.06.28)
+------------------
+
+* Added ``closeLink: false`` plugin option.
+* Subsequent plugin calls on the same element default to previous options
+  unless explicitly overridden.
+
+0.1.2 (2011.06.27)
+------------------
+
+* Added ``AjaxMessagesMiddleware`` and ``handleAjax`` plugin option.
+
+
+0.1.1 (2011.06.27)
+------------------
+
+* Updated HTML template (removed ``<aside>`` and moved ``#messages`` to
+  ``<ul>``).
+
+
+0.1.0 (2011.06.25)
+------------------
+
+* Initial release.
---
+++ packages/django-messages-ui/django-messages-ui-0.2.1.tar.gz/django-messages-ui-0.2.1/messages_ui/static/messages_ui/jquery.messages-ui.js
@@ -0,0 +1,119 @@
+/**
+ * jQuery Messages UI 0.2.1
+ *
+ * Copyright (c) 2013, Jonny Gerig Meyer
+ * All rights reserved.
+ *
+ * Licensed under the New BSD License
+ * See: http://www.opensource.org/licenses/bsd-license.php
+ */
+
+/*jslint    browser:    true,
+            indent:     4,
+            confusion:  true */
+/*global    jQuery, ich, Handlebars */
+
+(function ($) {
+
+    'use strict';
+
+    var methods = {
+        init: function (opts) {
+            var messageList = $(this);
+            var options = $.extend({}, $.fn.messages.defaults, messageList.data('messages-ui-opts'), opts);
+            messageList.data('messages-ui-opts', options);
+            if (options.closeLink) {
+                messageList.on('click', options.message + ' ' + options.closeLink, function (e) {
+                    e.preventDefault();
+                    var thisMessage = $(this).closest(options.message);
+                    methods['remove'](thisMessage);
+                });
+            }
+            if (options.handleAjax) {
+                $.ajaxSetup({
+                    dataType: 'json',
+                    dataFilter: function (data, type) {
+                        if (data && type === 'json') {
+                            var json;
+                            try {
+                                json = $.parseJSON(data);
+                            } catch (e) {
+                                json = false;
+                            }
+                            if (json && json.messages) {
+                                var messages = $(json.messages);
+                                messages.each(function () {
+                                    methods['add'](this, opts, messageList);
+                                });
+                            }
+                        }
+                        return data;
+                    }
+                });
+            }
+            methods.bindHandlers(messageList);
+        },
+
+        add: function (msg_data, opts, messageList) {
+            var msgList = messageList || $(this);
+            var options = $.extend({}, $.fn.messages.defaults, msgList.data('messages-ui-opts'), opts);
+            var msg;
+            msg_data.escapeHTML = options.escapeHTML;
+            if (options.templating === 'handlebars') {
+                msg = Handlebars.templates['message.html'](msg_data);
+            } else if (options.templating === 'ich') {
+                msg = ich.message(msg_data);
+            }
+            $(msg).appendTo(msgList);
+            methods.bindHandlers(msgList);
+        },
+
+        remove: function (msg) {
+            msg.stop().fadeOut('fast', function () {
+                msg.detach();
+            });
+        },
+
+        bindHandlers: function (messageList) {
+            var options = $.extend({}, $.fn.messages.defaults, messageList.data('messages-ui-opts'));
+            var messages = messageList.find(options.message);
+            var transientMessages = messages.filter(options.transientMessage);
+            if (transientMessages.length) {
+                transientMessages.each(function () {
+                    var msg = $(this);
+                    $(document).bind('mousedown keydown', function (event) {
+                        $.doTimeout(options.transientDelay, function () {
+                            msg.fadeOut(options.transientFadeSpeed, function () {
+                                msg.detach();
+                            });
+                            $(this).unbind(event);
+                        });
+                    });
+                });
+            }
+        }
+    };
+
+    $.fn.messages = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.messages');
+        }
+    };
+
+    /* Setup plugin defaults */
+    $.fn.messages.defaults = {
+        message: '.message',            // Selector for individual messages
+        transientMessage: '.success',   // Selector for messages that will disappear on mousedown, keydown
+        closeLink: '.close',            // Selector for link that closes message (set to ``false`` to disable close-link handlers)
+        transientDelay: 500,            // Delay before mousedown or keydown events trigger transient message fade (ms)
+        transientFadeSpeed: 3000,       // Fade speed for transient messages (ms)
+        handleAjax: false,              // Enable automatic handling of messages in "messages" key of JSON AJAX response
+        templating: 'handlebars',       // Set to ``ich`` to use ICanHaz.js instead of Handlebars.js for templating
+                                        //      ...only used if ``handleAjax: true``
+        escapeHTML: true                // Set to ``false`` to not HTML-escape message content (allowing for in-line HTML in message)
+    };
+}(jQuery));
