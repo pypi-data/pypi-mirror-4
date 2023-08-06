9b44a1a2c3353c4627effa6f0c98aa2a102e6008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:27 +0000

    Add package europeana-search/0.2.1/europeana-search-0.2.1.tar.gz

---
+++ packages/europeana-search/europeana-search-0.2.1.tar.gz/europeana-search-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='europeana-search',
+    version='0.2.1',
+    author='Martin Keegan',
+    author_email='martin@no.ucant.org',
+    packages=['europeana'],
+    scripts=[],
+    url='',
+    license='LICENSE.txt',
+    description='Basic Python wrapper for Europeana Search API.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 1.1.0"
+    ],
+)
