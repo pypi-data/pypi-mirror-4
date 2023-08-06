2431868c1d7f1df1e1891aab6892a16baa1c8064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:07 +0000

    Add package pelican-flickrtag/0.2.0/pelican-flickrtag-0.2.0.tar.gz

---
+++ packages/pelican-flickrtag/pelican-flickrtag-0.2.0.tar.gz/pelican-flickrtag-0.2.0/pelican_flickrtag/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'pelican-flickrtag'
+__version__ = '0.2.0'
+__author__ = 'Chris Streeter'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013'
+
+
+from pelican_flickrtag.plugin import register
