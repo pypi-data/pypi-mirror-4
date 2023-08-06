b84a053025f8e5525ca252af88d7791b597413c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:50 +0000

    Add package mockfs/1.0.1/mockfs-1.0.1.tar.gz

---
+++ packages/mockfs/mockfs-1.0.1.tar.gz/mockfs-1.0.1/mockfs/util.py
@@ -0,0 +1,96 @@
+import os
+
+def is_file(entry):
+    return isinstance(entry, basestring)
+
+
+def is_dir(entry):
+    return type(entry) is dict
+
+
+def sanitize(path):
+    """
+    Clean up path arguments for use with MockFS
+
+    MockFS isn't happy with trailing slashes since it uses a dict
+    to simulate the file system.
+
+    """
+    while '//' in path:
+        path = path.replace('//', '/')
+    while len(path) > 1 and path.endswith('/'):
+        path = path[:-1]
+    return path
+
+
+def merge_dicts(src, dst):
+    """
+    Merge entries from 'src' into 'dst'.
+
+    :param src: source dictionary
+    :param dst: destination dictionary
+
+    """
+    for k, v in src.items():
+        if k not in dst:
+            dst[k] = v
+            continue
+        if type(dst[k]) is dict:
+            dst[k] = merge_dicts(v, dst[k])
+            continue
+        if type(dst[k]) is list and type(v) is list:
+            dst[k].extend(v)
+            continue
+        dst[k] = v
+    return dst
+
+
+def build_nested_dict(entries):
+    """
+    Convert a flat dict of paths to a nested dict
+
+    :param entries: Path to entry dictionary
+    :type entries: :py:data:`dict`
+
+    e.g. `{'/unix/path': 'content', '/unix/dir': {},}`
+
+    """
+    result = {}
+    if not entries:
+        return {}
+    # Each entry key is either a file path or an empty directory.
+    # Directories are represented by dictionaries, the empty dictionary
+    # can be passed as a value to indicate an empty directory.
+    for raw_path, value in entries.items():
+        path = sanitize(raw_path)
+        basename = os.path.basename(path)
+        subpaths = path.split('/')[1:]
+        subentry = result
+        current = subentry
+        for subpath in subpaths:
+            current = subentry
+            subentry = subentry.setdefault(subpath, {})
+        current[basename] = value
+    return result
+
+
+def build_nested_dir_dict(dirpath):
+    """Build a nested dict of dicts from a directory path
+
+    :param dirpath: Directory path
+    :type dirpath: :py:data:`str`
+
+    """
+    result = {}
+    path = sanitize(dirpath)
+    basename = os.path.basename(path)
+    subpaths = path.split('/')[1:]
+
+    subentry = result
+    current = subentry
+    for subpath in subpaths:
+        current = subentry
+        subentry = subentry.setdefault(subpath, {})
+
+    current[basename] = {}
+    return result
---
+++ packages/mockfs/mockfs-1.0.1.tar.gz/mockfs-1.0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2009, 2010 David Aguilar (davvid -at- gmail.com)
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+
+import sys
+import mockfs
+import mockfs.mfs
+
+from distutils.core import setup
+
+
+SETUP_ARGS = dict(
+    name='mockfs',
+    version=mockfs.__version__,
+    description='Mock filesystem implementation for unit tests',
+    long_description=mockfs.mfs.__doc__,
+    author='David Aguilar',
+    author_email='davvid@gmail.com',
+    url='http://github.com/mockfs/mockfs',
+    license='BSD',
+    platforms=['POSIX', 'Windows'],
+    keywords=['unittest', 'mockfs', 'filesystem'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python'
+    ],
+    options={'clean': {'all': 1}},
+    packages=['mockfs'],
+)
+
+
+def main():
+    setup(**SETUP_ARGS)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
