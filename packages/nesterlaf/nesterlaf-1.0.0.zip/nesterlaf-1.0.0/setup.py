a4c514b6f5f7611ea4c50bd98fc5fa9454e9f2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:27 +0000

    Add package nesterlaf/1.0.0/nesterlaf-1.0.0.zip

---
+++ packages/nesterlaf/nesterlaf-1.0.0.zip/nesterlaf-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+            name = 'nesterlaf' ,
+            version = '1.0.0' ,
+            py_modules = ['nesterlaf'] ,
+            author = 'laf' ,
+            author_email = 'leslyfranc@gmail.com' ,
+            description = 'Nesting Lists for func ' 
+        )
+
