869cf88a5af31e61ece3331ef0efaea8de378580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package pycps/0.2.1/pycps-0.2.1.tar.gz

---
+++ packages/pycps/pycps-0.2.1.tar.gz/pycps-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pycps',
+    version='0.2.1',
+    author='Viesturs Silins',
+    author_email='example@example.com',
+    packages=['pycps'],
+    url='https://github.com/clusterpoint/pycps',
+    license='GNU Affero General Public License',
+    description='Python API for Clusterpoint Server',
+    long_description=open('README').read(),
+)
