5934015bfa6022fa655cf70ec1b63fe3caf6d711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package collective.js.fullcalendar/1.6.1/collective.js.fullcalendar-1.6.1.zip

---
+++ packages/collective.js.fullcalendar/collective.js.fullcalendar-1.6.1.zip/collective.js.fullcalendar-1.6.1/collective/js/fullcalendar/upgrades.py
@@ -0,0 +1,20 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def v2(context):
+    context.runImportStepFromProfile('profile-collective.js.fullcalendar:default',
+                                     'jsregistry', run_dependencies=False,
+                                     purge_old=False)
+    getToolByName(context, 'portal_javascripts').cookResources()
+
+
+def v3(context):
+    context.runImportStepFromProfile('profile-collective.js.fullcalendar:default',
+                                     'cssregistry', run_dependencies=False,
+                                     purge_old=False)
+    getToolByName(context, 'portal_css').cookResources()
+
+
+def recook_js_resources(context):
+    getToolByName(context, 'portal_javascripts').cookResources()
+
