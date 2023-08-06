ae852b3927643a80087ea60e11ee615737ac84c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:28 +0000

    Add package fishc_test/1.5.0/fishc_test-1.5.0.zip

---
+++ packages/fishc_test/fishc_test-1.5.0.zip/fishc_test-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'fishc_test',
+	version	= '1.5.0',
+	py_modules	= ['fishc_test'], 
+	author	= '小甲鱼',
+	author_email	= 'goodboy5201314@163.com', 
+	url	= 'http://www.fishc.com', 
+	description	= '教学视频演示例子', 
+	)
