d302bdc625da887cd8a34ec112b59569834c1aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:15 +0000

    Add package whack/0.4.1/whack-0.4.1.tar.gz

---
+++ packages/whack/whack-0.4.1.tar.gz/whack-0.4.1/whack/cli.py
@@ -0,0 +1,66 @@
+import argparse
+
+import whack.config
+import whack.args
+
+env_default = whack.args.env_default(prefix="WHACK")
+
+
+def main(argv, operations):
+    commands = [
+        InstallCommand("install"),
+        InstallCommand("build"),
+    ]
+    
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    
+    for command in commands:
+        subparser = subparsers.add_parser(command.name)
+        subparser.set_defaults(func=command.execute)
+        command.create_parser(subparser)
+    
+    args = parser.parse_args(argv[1:])
+    args.func(operations, args)
+    
+    
+class InstallCommand(object):
+    def __init__(self, name):
+        self.name = name
+    
+    def create_parser(self, subparser):
+        subparser.add_argument('package')
+        subparser.add_argument('target_dir', metavar="target-dir")
+        subparser.add_argument("--no-cache", action="store_true")
+        subparser.add_argument("--http-cache-url", action=env_default)
+        subparser.add_argument("--http-cache-key", action=env_default)
+        subparser.add_argument("--add-parameter", "-p", action="append", default=[])
+    
+    def execute(self, operations, args):
+        params = {}
+        for parameter_arg in args.add_parameter:
+            if "=" in parameter_arg:
+                key, value = parameter_arg.split("=", 1)
+                params[key] = value
+            else:
+                params[parameter_arg] = ""
+        
+        caching = whack.config.caching_config(
+            enabled=not args.no_cache,
+            http_cache_url=args.http_cache_url,
+            http_cache_key=args.http_cache_key
+        )
+        
+        if self.name == "install":
+            operation = operations.install
+        elif self.name == "build":
+            operation = operations.build
+        else:
+            raise Exception("Unrecognised operation")
+            
+        operation(
+            package=args.package,
+            install_dir=args.target_dir,
+            caching=caching,
+            params=params
+        )
---
+++ packages/whack/whack-0.4.1.tar.gz/whack-0.4.1/whack/providers.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import shutil
+
+from . import downloads
+from .tempdir import create_temporary_dir
+from .naming import name_package
+from .common import WHACK_ROOT
+from .files import mkdir_p
+
+
+class BuildingPackageProvider(object):
+    def provide_package(self, package_src, params, package_dir):
+        with create_temporary_dir() as temp_dir:
+            build_dir = os.path.join(temp_dir, "build")
+            
+            self._build(package_src, build_dir, package_dir, params)
+    
+    def _build(self, package_src, build_dir, package_dir, params):
+        ignore = shutil.ignore_patterns(".svn", ".hg", ".hgignore", ".git", ".gitignore")
+        shutil.copytree(package_src.path, build_dir, ignore=ignore)
+        build_env = params_to_build_env(params)
+        self._fetch_downloads(build_dir, build_env)
+            
+        build_script = os.path.join(build_dir, "whack/build")
+        mkdir_p(package_dir)
+        build_command = [
+            "whack-run-with-whack-root",
+            package_dir, # package_dir is mounted at WHACK_ROOT
+            build_script, # build_script is executed
+            WHACK_ROOT # WHACK_ROOT is passed as the first argument to build_script
+        ]
+        subprocess.check_call(build_command, cwd=build_dir, env=build_env)
+
+    def _fetch_downloads(self, build_dir, build_env):
+        downloads_file_path = os.path.join(build_dir, "whack/downloads")
+        downloads.fetch_downloads(downloads_file_path, build_env, build_dir)
+
+
+class CachingPackageProvider(object):
+    def __init__(self, cacher):
+        self._cacher = cacher
+        self._underlying_provider = BuildingPackageProvider()
+    
+    def provide_package(self, package_source, params, package_dir):
+        package_name = name_package(package_source, params)
+        result = self._cacher.fetch(package_name, package_dir)
+        
+        if not result.cache_hit:
+            self._underlying_provider.provide_package(package_source, params, package_dir)
+            self._cacher.put(package_name, package_dir)
+
+
+def params_to_build_env(params):
+    build_env = os.environ.copy()
+    for name, value in (params or {}).iteritems():
+        build_env[name.upper()] = str(value)
+    return build_env
