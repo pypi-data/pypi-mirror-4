95744c6853b630789ec320876edae9fa488879a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package textminer/1.0.0/textminer-1.0.0.tar.gz

---
+++ packages/textminer/textminer-1.0.0.tar.gz/textminer-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='textminer',
+    version='1.0.0',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    description='Utility for extracting values, lists and dicts from text.',
+    packages=['textminer'],
+    url='https://github.com/CooledCoffee/textminer/',
+)
