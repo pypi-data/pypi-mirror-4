28f61fa7ef17a48b25ca959cbbfc508062b0f062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:36 +0000

    Add package simplelayout.types.news/1.1/simplelayout.types.news-1.1.tar.gz

---
+++ packages/simplelayout.types.news/simplelayout.types.news-1.1.tar.gz/simplelayout.types.news-1.1/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+
+tests_require = [
+    'plone.app.testing',
+    'plone.mocktestcase',
+    'ftw.testing',
+    ]
+
+setup(name='simplelayout.types.news',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/simplelayout.types.news',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['simplelayout', 'simplelayout.types'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'simplelayout.types.common',
+        'plone.formwidget.contenttree',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
