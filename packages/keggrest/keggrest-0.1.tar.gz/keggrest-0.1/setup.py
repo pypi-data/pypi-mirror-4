6d3897f25e7d9b03aece5819e7777ba47cd98461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package keggrest/0.1/keggrest-0.1.tar.gz

---
+++ packages/keggrest/keggrest-0.1.tar.gz/keggrest-0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='keggrest',
+      version='0.1',
+      description='Basic frontend for the kegg database rest API',
+      author='Enrico Giampieri',
+      author_email='enrico.giampieri@unibo.it',
+      py_modules=['keggrest'],
+      download_url='https://github.com/EnricoGiampieri/keggrest.git',
+      url='https://github.com/EnricoGiampieri/keggrest.git',
+      license = "BSD",
+      long_description=read('README.txt'),
+      keywords = "rest API bioinformatics",
+      classifiers=[
+          "License :: OSI Approved :: BSD License",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Science/Research",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Scientific/Engineering :: Bio-Informatics",
+          ],
+      )
