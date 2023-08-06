141cc923311b15b73fccd5e373db9b7853164e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:03 +0000

    Add package Loslassa/0.1-dev-2/Loslassa-0.1-dev-2.tar.gz

---
+++ packages/Loslassa/Loslassa-0.1-dev-2.tar.gz/Loslassa-0.1-dev-2/loslassa/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+
+# noinspection PyUnresolvedReferences
+from plumbum.cmd import sphinx_build
+
+# quick hack to make this work without adding something to PYTHONPATH
+path = os.path.dirname(os.path.abspath(__file__)).rpartition('loslassa')[0]
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+from loslassa.constants import *
+from loslassa.devserver import serve_with_reloader
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    htmlBuilder = sphinx_build[
+        "-b", "dirhtml", "-d", DOCTREES_PATH, SOURCE_PATH, OUTPUT_PATH]
+    serve_with_reloader(OUTPUT_PATH, 8080, htmlBuilder,pathToWatch=SOURCE_PATH)
+
+
+if __name__ == "__main__":
+    main()
