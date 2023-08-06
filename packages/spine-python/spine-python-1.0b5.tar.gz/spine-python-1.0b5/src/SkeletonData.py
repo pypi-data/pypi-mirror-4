9eddc7bc7341e01affcfd981a11757acce3c5c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package spine-python/1.0b5/spine-python-1.0b5.tar.gz

---
+++ packages/spine-python/spine-python-1.0b5.tar.gz/spine-python-1.0b5/src/SkeletonData.py
@@ -0,0 +1,43 @@
+class SkeletonData(object):
+    def __init__(self):
+        super(SkeletonData, self).__init__()
+        self.bones = []
+        self.slots = []
+        self.skins = []
+        self.defaultSkin = None
+
+        
+    def findBone(self, boneName):
+        for i, bone in enumerate(self.bones):
+            if bone.name == boneName:
+                return bone
+        return None
+
+
+    def findBoneIndex(self, boneName):
+        for i, bone in enumerate(self.bones):
+            if bone.name == boneName:
+                return i
+        return -1
+
+
+    def findSlot(self, slotName):
+        for i, slot in enumerate(self.slots):
+            if slot.name == slotName:
+                return slot
+        return None
+
+    
+    def findSlotIndex(self, slotName):
+        for i, slot in enumerate(self.slots):
+            if slot.name == slotName:
+                return i
+        return -1
+
+
+    def findSkin(self, skinName):
+        for i, skin in enumerate(self.skins):
+            if skin.name == skinName:
+                return skin
+        return None
+    
