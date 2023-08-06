1ef86e45ae85a27f0b56de6e32610eab0972f47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:45 +0000

    Add package todoq/1.0.3/todoq-1.0.3.tar.gz

---
+++ packages/todoq/todoq-1.0.3.tar.gz/todoq-1.0.3/todoqlib/file_access_helper.py
@@ -0,0 +1,141 @@
+import os
+from xml.dom.minidom import parse, Document, getDOMImplementation
+import time
+import heapq
+from task import Task
+
+
+class FileAccessHelper:
+  """ The helper class for data file saving, modifying and sync """
+  def __init__(self, todoq_dir, debug = False):
+    self.todoq_dir = todoq_dir
+    if not os.path.exists(todoq_dir):
+      os.makedirs(todoq_dir)
+    self.queue_info_file = os.path.join(todoq_dir, 'queue.info')
+    self.debug = debug
+    self.queue_name = None
+    self.queue_path = None
+    self.queue_dom = None
+    self.queue_node = None
+  
+  def set_debug(self, debug):
+    self.debug = debug
+    return
+
+  def get_queue_name(self):
+    """ Get the current queue """
+    self.queue_name = 'default'
+    return self.queue_name
+
+  def get_queue_path(self, queue_name = None):
+    queue_name = queue_name or self.queue_name or self.get_queue_name()
+    self.queue_path = self.queue_path or \
+        os.path.join(self.todoq_dir, '{0}.task'.format(queue_name))
+    return self.queue_path
+
+  def get_queue_dom(self, queue_path = None):
+    """ Get the current queue dom """
+    queue_path = queue_path or self.get_queue_path()
+    if self.queue_dom:
+      return self.queue_dom
+    try:
+      self.queue_dom = parse(self.queue_path)
+    except IOError:
+      self.queue_dom = getDOMImplementation().createDocument(None, 'Queue', None)
+    return self.queue_dom
+
+  def get_queue_node(self, queue_dom = None):
+    queue_dom = queue_dom or self.get_queue_dom()
+    self.queue_node = self.queue_node or queue_dom.documentElement or \
+        queue_dom.appendChild(queue_dom.createElement('Queue'))
+    return self.queue_node
+
+  def save_file(self, queue_dom = None):
+    """ The xml dom is stored to a temporary first, and then overwrite the initial file """
+    queue_dom = queue_dom or self.get_queue_dom()
+    tmp_queue_path = self.get_queue_path() + '.swap'
+    tmp_queue_file = open(tmp_queue_path, 'w')
+    tmp_queue_file.write(queue_dom.toxml())
+    tmp_queue_file.close()
+    os.rename(tmp_queue_path, self.get_queue_path())
+    return
+
+  def add_task(self, task_name, priority):
+    """ Add the new task to the curr queue """
+    if self.debug:
+      print 'add new task {0} with priority {1} to queue {2}'.format(\
+          task_name, priority, self.get_queue_name())
+    queue_dom = self.get_queue_dom()
+    queue_node = self.get_queue_node()
+    task = Task(task_name, priority, 'Unfinished')
+    queue_node.appendChild(task.to_xml_node(queue_dom))
+    self.save_file()
+    return
+
+  def get_task_heap(self, task_nodes):
+    """ The key is the negation of priority """
+    heap = []
+    for task_node in task_nodes:
+      key = -int(task_node.getElementsByTagName('Priority')[0].firstChild.data)
+      heapq.heappush(heap, (key, task_node))
+    return heap
+
+  def get_node_data(self, node, tag):
+    return node.getElementsByTagName(tag)[0].childNodes[0].data
+
+  def get_top_node(self):
+    queue_dom = self.get_queue_dom()
+    unfinished_tasks = queue_dom.getElementsByTagName('Unfinished')
+    heap = self.get_task_heap(unfinished_tasks)
+    top_node = heap[0][1]
+    return top_node
+
+  def get_top_task(self):
+    """ Return the the top task """
+    top_node = self.get_top_node()
+    return Task.parse_task(top_node)
+  
+  def change_top_task_to_tag(self, tag):
+    queue_dom = self.get_queue_dom()
+    top_node = self.get_top_node()
+    if self.debug:
+      print 'Mark top task "{0}" as {1}'.format(self.get_node_data(top_node, 'Name'), tag)
+    top_node.tagName = tag
+    return
+    
+  def mark_top_task_as_finished(self):
+    self.change_top_task_to_tag('Finished')
+    self.save_file()
+    return
+  
+  def mark_top_task_as_dropped(self):
+    self.change_top_task_to_tag('Dropped')
+    self.save_file()
+    return
+
+  def postpone_top_task(self, priority):
+    dom = self.get_queue_dom()
+    top_node = self.get_top_node()
+    if self.debug:
+      print 'Reset top task with the priority of {0}'.format(priority)
+    top_node.getElementsByTagName('Priority')[0].firstChild.data = str(priority)
+    self.save_file()
+    return
+
+  def get_tasks(self, filter_tuple, count):
+    """ filter_tuple = (all, unfinished, finished, dropped) """
+    queue_node = self.get_queue_node()
+    task_nodes = []
+    if filter_tuple[0] or filter_tuple[1] \
+      or not filter_tuple[2] and not filter_tuple[3]:
+      task_nodes = task_nodes + queue_node.getElementsByTagName('Unfinished')
+    if filter_tuple[0] or filter_tuple[2]:
+      task_nodes = task_nodes + queue_node.getElementsByTagName('Finished')
+    if filter_tuple[0] or filter_tuple[3]:
+      task_nodes = task_nodes + queue_node.getElementsByTagName('Dropped')
+    heap = self.get_task_heap(task_nodes)
+    tasks = []
+    for element in reversed(heap):
+      task_node = element[1]
+      tasks.append(Task.parse_task(task_node))
+    return tasks
---
+++ packages/todoq/todoq-1.0.3.tar.gz/todoq-1.0.3/todoqlib/todoq.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import os
+from file_access_helper import FileAccessHelper
+from task import Task
+
+helper = FileAccessHelper(os.path.join(os.path.expanduser('~'), '.todoq'))
+
+class CommandLineApplication:
+  """ The command line application class to deal with sub-command dispatching """
+  def __init__(self, sub_command_list, debug = False):
+    """ sub_command_list is the list from sub-command to handler class """
+    self.sub_command_list = sub_command_list
+    helper.set_debug(debug)
+
+  def run(self):
+    """ The function to be called to run the whole program """
+    parser = argparse.ArgumentParser(prog='todoq')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    for element in self.sub_command_list:
+      handler = element[1]()
+      parser_add = subparsers.add_parser(element[0], help=handler.get_help_str())
+      handler.add_arguments(parser_add)
+      parser_add.set_defaults(func=handler.execute)
+    args = parser.parse_args()
+    args.func(args)
+    return
+
+
+class SubCommandHandler:
+  """ The base class for all the sub-command handlers """
+  def get_help_str(self):
+    """ Returns default help str """
+    return 'sub-command'
+  
+  def add_arguments(self, subparser):
+    """ Add no arguments for default """
+    return
+
+  def execute(self, args):
+    """ The default function to be called when sub-command is executed """
+    return
+
+  def print_no_task_error(self):
+    print 'There is no task in the queue.'
+    return
+
+class SubCommandAddHandler(SubCommandHandler):
+  """ The handler to deal with sub-command 'add' """
+
+  def get_help_str(self):
+    """ Returns the help str for sub-command 'add' """
+    return 'add a new task'
+
+  def add_arguments(self, subparser):
+    """ Add optional and positional arguments to subparser """
+    subparser.add_argument('task_name', nargs=1)
+    subparser.add_argument('priority', type=int, nargs=1)
+    return
+
+  def execute(self, args):
+    """ The function to be called when sub-command 'add' is executed """
+    helper.add_task(args.task_name[0], args.priority[0])
+    return
+
+
+class SubCommandTopHandler(SubCommandHandler):
+  """ The handler to deal with sub-command 'top' """
+  def get_help_str(self):
+    """ Returns the help str for sub-command 'top' """
+    return 'show the top task'
+
+  def execute(self, args):
+    """ The function to be called when sub-command 'top' is executed """
+    try:
+      task = helper.get_top_task()
+      print task.to_str()
+    except IndexError:
+      self.print_no_task_error()
+    return
+
+
+class SubCommandFinishHandler(SubCommandHandler):
+  """ The handler to deal with 'finish' """
+  def get_help_str(self):
+    """ Returns the help str for 'finish' """
+    return 'mark the top task as finished and archive it'
+
+  def execute(self, args):
+    try:
+      helper.mark_top_task_as_finished()
+    except IndexError:
+      self.print_no_task_error()
+    return
+
+
+class SubCommandPostponeHandler(SubCommandHandler):
+  """ The handler to deal with 'postpone' """
+  def get_help_str(self):
+    """ Returns the help str for 'postpone' """
+    return 'postpone the top task, and advance the second task'
+
+  def add_arguments(self, subparser):
+    """ Add the priority argument for 'postpone """
+    subparser.add_argument('priority', type=int, nargs=1)
+    return
+
+  def execute(self, args):
+    """ Change the priority value of the top task """
+    try:
+      helper.postpone_top_task(args.priority[0])
+    except IndexError:
+      self.print_no_task_error()
+    return
+
+
+class SubCommandDropHandler(SubCommandHandler):
+  """ The handler to deal with 'drop' """
+  def get_help_str(self):
+    """ Returns the help str for 'drop' """
+    return 'mark the top task as dropped and archive it'
+
+  def execute(self, args):
+    try:
+      helper.mark_top_task_as_dropped()
+    except IndexError:
+      self.print_no_task_error()
+    return
+
+
+class SubCommandListHandler(SubCommandHandler):
+  """ The handler to deal with 'list' """
+
+  def get_help_str(self):
+    """ Returns the help str for 'list' """
+    return 'list all the tasks in the current task queue'
+
+  def add_arguments(self, subparser):
+    """ Add the priority argument for 'postpone """
+    subparser.add_argument('-u', '--unfinished', 
+                           help='display all the unfinished tasks',
+                           action='store_true')
+    subparser.add_argument('-a', '--all', 
+                           help='display all the tasks',
+                           action='store_true')
+    subparser.add_argument('-f', '--finished', 
+                           help='display all the finished tasks',
+                           action='store_true')
+    subparser.add_argument('-d', '--dropped', 
+                           help='display all the dropped tasks',
+                           action='store_true')
+    subparser.add_argument('-n', '--count', 
+                           help='specify the number of tasks to be displayed',
+                           type=int)
+    return
+
+  def execute(self, args):
+    tasks = helper.get_tasks((args.all, args.unfinished, args.finished,
+                              args.dropped), args.count)
+    if not tasks:
+      self.print_no_task_error()
+    for task in tasks:
+      print task.to_str()
+    return
+
+
+class SubCommandShowqHandler(SubCommandHandler):
+  """ The handler to deal with 'showq' """
+  def get_help_str(self):
+    """ Returns the help str for 'showq' """
+    return 'list all the queues'
+
+class SubCommandSelectqHandler(SubCommandHandler):
+  """ The handler to deal with 'selectq' """
+  def get_help_str(self):
+    """ Returns the help str for 'selectq' """
+    return 'select the queue as the current queue'
+
+class SubCommandCreateqHandler(SubCommandHandler):
+  """ The handler to deal with 'createq' """
+  def get_help_str(self):
+    """ Returns the help str for 'createq' """
+    return 'create a new queue'
+
+class SubCommandSyncHandler(SubCommandHandler):
+  """ The handler to deal with 'sync' """
+  def get_help_str(self):
+    """ Returns the help str for 'sync' """
+    return 'sync all the task queues with Dropbox'
+
+def main():
+  app = CommandLineApplication([
+      ('add', SubCommandAddHandler),
+      ('top', SubCommandTopHandler),
+      ('postpone', SubCommandPostponeHandler),
+      ('finish', SubCommandFinishHandler),
+      ('drop', SubCommandDropHandler),
+      ('list', SubCommandListHandler),
+      ('showq', SubCommandShowqHandler),
+      ('selectq', SubCommandSelectqHandler),
+      ('createq', SubCommandCreateqHandler),
+      ('sync', SubCommandSyncHandler),
+    ], debug=True);
+  app.run()
+
+if __name__ == '__main__':
+  main()
