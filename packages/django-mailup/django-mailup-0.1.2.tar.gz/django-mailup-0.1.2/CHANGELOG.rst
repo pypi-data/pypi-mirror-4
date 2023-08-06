faa7ab20fc73d5a85732c2170600b32edeae8fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:00 +0000

    Add package django-mailup/0.1.2/django-mailup-0.1.2.tar.gz

---
+++ packages/django-mailup/django-mailup-0.1.2.tar.gz/django-mailup-0.1.2/CHANGELOG.rst
@@ -0,0 +1,18 @@
+=========
+Changelog
+=========
+
+0.1 - 31 January 2013
+~~~~~~~~~~~~~~~~~~~~~
+
+* First integration with mailup service.
+
+0.1.1 - 31 January 2013
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Add dist and egg-info directory in .hgignore
+
+0.1.2 - 31 January 2013
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Add doc in checkSubscriber function
---
+++ packages/django-mailup/django-mailup-0.1.2.tar.gz/django-mailup-0.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+Installation script:
+
+To release a new version to PyPi:
+- Ensure the version is correctly set in oscar.__init__.py
+- Run: python setup.py sdist upload
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='django-mailup',
+      version="0.1.2",
+      url='https://bitbucket.org/marcominutoli/django-mailup.git',
+      author="Marco Minutoli",
+      author_email="info@marcominutoli.it",
+      description="A plugin to integrate Django and Mailup",
+      long_description=open('README.rst').read(),
+      keywords="Newsletter, Django, Mailup",
+      license='BSD',
+      packages=find_packages(),
+      install_requires=[
+          'suds==0.4',
+      ],
+      # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python']
+      )
