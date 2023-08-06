507b6c7c06014e4704928ce6fe9bd0b792af8db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:52 +0000

    Add package django-postgres-pebble/0.0.4/django-postgres-pebble-0.0.4.tar.gz

---
+++ packages/django-postgres-pebble/django-postgres-pebble-0.0.4.tar.gz/django-postgres-pebble-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = ("First-class Postgre feature support for the Django ORM."
+    "Includes custom extension for manually synching PG Views. For the "
+    "base package, see django-postgres.")
+
+setup(
+    name='django-postgres-pebble',
+    version='0.0.4',
+    description="First-class Postgres feature support for the Django ORM.",
+    long_description=LONG_DESCRIPTION,
+    author='Scott Walton',
+    author_email='scw@talktopebble.co.uk',
+    license='Public Domain',
+    packages=find_packages(),
+    install_requires=[
+        'bitstring',
+    ],
+)
