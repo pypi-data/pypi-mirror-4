9f90e8221e1b3389876092c533bdddd62b8b0405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:11 +0000

    Add package Pizza/0.1.1/Pizza-0.1.1.tar.gz

---
+++ packages/Pizza/Pizza-0.1.1.tar.gz/Pizza-0.1.1/pizza_setup/test/test_utils.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+# TODO [template]: code this stub class.
+class GetDiffPathsTestCase(unittest.TestCase):
+
+    # TODO [template]: add a test case for excluding an item in the project
+    # directory.
+    def test(self):
+        self.assertEquals(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
