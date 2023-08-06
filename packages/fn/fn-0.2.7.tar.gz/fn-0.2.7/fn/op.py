d665bdfcf28dbeb0cd0b90f41483949667cfe34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:04 +0000

    Add package fn/0.2.7/fn-0.2.7.tar.gz

---
+++ packages/fn/fn-0.2.7.tar.gz/fn-0.2.7/fn/op.py
@@ -0,0 +1,34 @@
+from sys import version_info
+
+identity = lambda arg: arg
+
+def _apply(f, args=None, kwargs=None):
+    return f(*(args or []), **(kwargs or {}))
+
+apply = apply if version_info[0] == 2 else _apply
+
+def flip(f):
+    """Return function that will apply arguments in reverse order"""
+
+    # Original function is saved in special attribute
+    # in order to optimize operation of "duble flipping",
+    # so flip(flip(a)) is a
+    flipper = getattr(f, "__flipback__", None)
+    if flipper is not None:
+        return flipper
+
+    def _flipper(a, b):
+        return f(b, a)
+
+    _flipper.__flipback__ = f
+    return _flipper
+
+def curry(f, arg, *rest):
+    return curry(f(arg), *rest) if rest else f(arg)
+
+from .func import F
+from itertools import starmap
+
+def zipwith(f): 
+    'zipwith(f)(seq1, seq2, ..) -> [f(seq1[0], seq2[0], ..), f(seq1[1], seq2[1], ..), ...]'
+    return F(starmap, f) << zip
