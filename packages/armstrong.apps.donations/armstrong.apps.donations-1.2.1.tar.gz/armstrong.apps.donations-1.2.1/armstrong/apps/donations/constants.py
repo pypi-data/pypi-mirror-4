0965ad9008a21246bacff4010304fa9cded0e509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:36 +0000

    Add package armstrong.apps.donations/1.2.1/armstrong.apps.donations-1.2.1.tar.gz

---
+++ packages/armstrong.apps.donations/armstrong.apps.donations-1.2.1.tar.gz/armstrong.apps.donations-1.2.1/armstrong/apps/donations/constants.py
@@ -0,0 +1,14 @@
+"""
+
+.. todo:: Fix issue if this were to stay in memory across a new year
+"""
+import datetime
+now = datetime.datetime.now()
+
+MONTHS = ["%02d" % (i + 1) for i in range(12)]
+MONTH_CHOICES = zip(MONTHS, MONTHS)
+
+YEARS = range(now.year, now.year + 10)
+YEAR_CHOICES = zip(YEARS, YEARS)
+
+MAILING_SAME_AS_BILLING = "mailing_same_as_billing"
