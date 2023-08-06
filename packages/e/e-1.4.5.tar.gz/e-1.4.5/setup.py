c6f26a18e64a8823f802f329d4c3af52d9a7167c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:02 +0000

    Add package e/1.4.5/e-1.4.5.tar.gz

---
+++ packages/e/e-1.4.5.tar.gz/e-1.4.5/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- encoding: utf8 -*-
+
+from distutils.core import setup
+
+import e
+
+setup(
+    name="e",
+    version='1.4.5',
+    description='''Evaluate and display command line expressions with
+        python -me expr''',
+    long_description=u'\n'.join(e.__doc__.splitlines()[1:]),
+    author="Georg Brandl, Michael Foord and Richard Jones",
+    author_email="richard@python.org",
+    py_modules=['e'],
+)
+
+# vim: set filetype=python ts=4 sw=4 et si
