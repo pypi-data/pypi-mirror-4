8007de438e86680bc318593649900b903f34caac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:09 +0000

    Add package django-international/0.0.4/django-international-0.0.4.zip

---
+++ packages/django-international/django-international-0.0.4.zip/django-international-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='django-international',
+    description='Country and currency data for Django projects',
+    long_description=open('README.rst').read(),
+    version='0.0.4',
+    packages=['international'],
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-international',
+    download_url='https://bitbucket.org/monwara/django-international/downloads',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+    include_package_data=True
+)
+
+
