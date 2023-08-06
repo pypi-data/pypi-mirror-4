ae6f2206a512cf1de0ee826444c7653a869a3d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package jig/0.1.4/jig-0.1.4.tar.gz

---
+++ packages/jig/jig-0.1.4.tar.gz/jig-0.1.4/src/jig/commands/runnow.py
@@ -0,0 +1,23 @@
+import argparse
+
+from jig.commands.base import BaseCommand
+from jig.runner import Runner
+
+_parser = argparse.ArgumentParser(
+    description='Run all plugins and show the results',
+    usage='jig runnow [-h] [PATH]')
+
+_parser.add_argument('path', nargs='?', default='.',
+    help='Path to the Git repository')
+
+
+class Command(BaseCommand):
+    parser = _parser
+
+    def process(self, argv):
+        path = argv.path
+
+        # Make the runner use our view
+        runner = Runner(view=self.view)
+
+        runner.fromhook(path, interactive=False)
