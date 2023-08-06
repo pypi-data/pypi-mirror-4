9d7469aa76f75b1a8bc1f67c8c00d60327196cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:04 +0000

    Add package declare-amqp/0.0.3/declare-amqp-0.0.3.tar.gz

---
+++ packages/declare-amqp/declare-amqp-0.0.3.tar.gz/declare-amqp-0.0.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def run_setup():
+    setup(
+        name='declare-amqp',
+        version='0.0.3',
+        description='A way to declare your AMQP exchanges, queues, and bindings outside of the application',
+        keywords = 'amqp',
+        url='http://github.com/philipcristiano/declare-amqp',
+        author='Philip Cristiano',
+        author_email='philipcristiano@gmail.com',
+        license='BSD',
+        packages=['declareamqp'],
+        install_requires=[
+            'haigha>=0.5.9',
+            'pyyaml',
+        ],
+        test_suite='tests',
+        long_description=read('README.md'),
+        zip_safe=True,
+        classifiers=[
+        ],
+        entry_points="""
+        [console_scripts]
+            declare-amqp=declareamqp.damqp:main
+        """,
+    )
+
+if __name__ == '__main__':
+    run_setup()
