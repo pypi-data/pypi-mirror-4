c9f2066d171f233f0cd60ed9ef284830bff7a294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:55 +0000

    Add package mMachine/0.0.2/mMachine-0.0.2.tar.gz

---
+++ packages/mMachine/mMachine-0.0.2.tar.gz/mMachine-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+        name = 'mMachine',
+        version = '0.0.2',
+        py_modules = ['mMachine'],
+        author  = 'Xin Meng',
+        author_email = 'mengxin008@gmail.com',
+        url  = 'http://www.picb.ac.cn/stabwiki/index.php/mMachine',
+        description = 'A rule-based modeling and simulation tool for biological system.'
+        )
