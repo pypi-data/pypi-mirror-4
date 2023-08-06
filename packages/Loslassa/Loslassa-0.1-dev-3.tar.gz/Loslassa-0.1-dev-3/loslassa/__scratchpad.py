250917dd93b47b790e15ed9d91fe9a411bd78eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:11 +0000

    Add package Loslassa/0.1-dev-3/Loslassa-0.1-dev-3.tar.gz

---
+++ packages/Loslassa/Loslassa-0.1-dev-3.tar.gz/Loslassa-0.1-dev-3/loslassa/__scratchpad.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+from ollitools.dev import obj_attr
+from plumbum.local_machine import LocalPath
+
+#lp = LocalPath(os.getcwd())
+#print str(lp)
+#print obj_attr(lp, filterMethods=False)
+#print obj_attr(lp._path)
+
+here = os.getcwd()
+scriptPath = os.path.join(here, "main.py")
+os.chdir(os.path.join(here, "example_project"))
+print os.getcwd()
+subprocess.check_output([scriptPath, "play"])
+os.chdir(scriptPath)
+print os.getcwd()
+
---
+++ packages/Loslassa/Loslassa-0.1-dev-3.tar.gz/Loslassa-0.1-dev-3/setup.py
@@ -0,0 +1,30 @@
+"""See README.rst for description"""
+from setuptools import setup
+
+setup(
+    name='Loslassa',
+    description='Web Site generator based on sphinx and git',
+    version='0.1-dev-3',
+    license='BSD',
+    url='http://github.com/obestwalter/loslassa/',
+    author='Oliver Bestwalter',
+    author_email='oliver@bestwalter.de',
+    long_description=open("README.rst").read(),
+    packages=['loslassa', 'loslassa.testsuite'],
+    package_data={'loslassa': ["example_project/source/*"]},
+    include_package_data=True,
+    zip_safe=False,
+    platforms='Unix', # todo add windows and Mac Os X
+    install_requires=['Sphinx>=1.1.3', 'plumbum', 'sphinx_bootstrap_theme'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
