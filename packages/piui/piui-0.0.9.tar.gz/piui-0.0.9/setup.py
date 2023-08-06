5dd5741cecdc7d4520fb92e1d2850adcf0e451c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:59 +0000

    Add package piui/0.0.9/piui-0.0.9.tar.gz

---
+++ packages/piui/piui-0.0.9.tar.gz/piui-0.0.9/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "piui",
+    version = "0.0.9",
+    author = "David Singleton",
+    author_email = "davidsingleton@gmail.com",
+    description = ("Add a mobile UI to your RaspberryPi"
+                   " project."),
+    license = "BSD",
+    keywords = "raspberrypi mobile ui",
+    url = "http://github.com/dps/piui",
+    packages=['piui'],
+    package_dir={'piui': 'piui'},
+    long_description=("Add a mobile UI to your RaspberryPi"
+                   " project."),
+    package_data = {'piui' : ['static/*']},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires = ['cherrypy'],
+)
