7e41f7cf28c380128a9c9ebc0caa4d8782a4ae26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:55 +0000

    Add package fysom/1.0.2/fysom-1.0.2.tar.gz

---
+++ packages/fysom/fysom-1.0.2.tar.gz/fysom-1.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'fysom',
+          version = '1.0.2',
+          description = '',
+          long_description = '''''',
+          author = "Mansour Behabadi, Jake Gordon",
+          author_email = "mansour@oxplot.com, jake@codeincomplete.com",
+          license = 'MIT',
+          url = 'https://github.com/oxplot/fysom',
+          scripts = [],
+          packages = ['fysom'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+          
+          
+          
+          
+          zip_safe=True
+    )
