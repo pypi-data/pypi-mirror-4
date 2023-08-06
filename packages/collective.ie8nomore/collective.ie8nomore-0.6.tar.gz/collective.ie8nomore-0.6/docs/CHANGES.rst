d97008e507fa22d166d396b6179cba7d79518d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:09 +0000

    Add package collective.ie8nomore/0.6/collective.ie8nomore-0.6.tar.gz

---
+++ packages/collective.ie8nomore/collective.ie8nomore-0.6.tar.gz/collective.ie8nomore-0.6/docs/CHANGES.rst
@@ -0,0 +1,69 @@
+Changelog
+=========
+
+0.6 (2013-04-15)
+----------------
+
+ * Moved Chrome icon before Safari in the browser ballot 
+   [ichim-david]
+
+ * Added romanian translation
+   [ichim-david]
+
+ * Changed condition of this viewlet to be less then IE9 since this package is
+   called ie8 no more
+   [ichim-david]
+
+
+0.5 (2013-04-09)
+----------------
+
+ * Modernized Icons, template optimizations.
+   [thet]
+
+ * Added Internet Explorer and Opera to alternative Browsers.
+   [thet]
+
+ * Added German translation.
+   [thet]
+
+ * Added Italian translation
+   [giacomos] 
+
+
+0.4 (16-Dec-2011)
+-----------------
+
+ * Fix packaging
+
+
+0.3 (16-Dec-2011)
+-----------------
+
+ * Update version to 0.3. 
+
+ * Update trove classifiers.
+
+ * Update package URL
+
+ * Use z3c.autoinclude to avoid ZCML slug in buildout. Remove unnedded ZCML declaration
+
+
+0.2 (15-Dec-2011)
+-----------------
+
+ * Add spanish translations.
+ 
+ * Update documentation.
+ 
+ * Improve CSS.
+ 
+ * Add a wrapper div.
+
+
+0.1dev (unreleased)
+-------------------
+
+ * Package created using zopeskel
+
+ * Add the viewlet.
---
+++ packages/collective.ie8nomore/collective.ie8nomore-0.6.tar.gz/collective.ie8nomore-0.6/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6'
+
+long_description = (
+    open('README.rst').read() + '\n' +
+    open(os.path.join('docs', 'CHANGES.rst')).read() + '\n')
+
+setup(name='collective.ie8nomore',
+      version=version,
+      description="Plone viewlet to prompt users to upgrade to a better web browser.",
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Natural Language :: Spanish",
+        ],
+      keywords='plone internet explorer',
+      author='Noe Nieto',
+      author_email='nnieto@noenieto.com',
+      url='https://github.com/collective/collective.ie8nomore',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.CMFPlone',
+          'plonetheme.sunburst'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
