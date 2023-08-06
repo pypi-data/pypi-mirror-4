5668921aee3ca95ee18ffa253003a182bb4c912b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:02 +0000

    Add package kissxml/0.1/kissxml-0.1.tar.gz

---
+++ packages/kissxml/kissxml-0.1.tar.gz/kissxml-0.1/kissxml/__init__.py
@@ -0,0 +1,4 @@
+from ._xml import XMLTree, XMLNode
+from .parser import parse, parsestring, ET
+
+__all__ = ('parse', 'parsestring', 'XMLTree', 'XMLNode', )
---
+++ packages/kissxml/kissxml-0.1.tar.gz/kissxml-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='kissxml',
+    version='0.1',
+    author='Joseph C. Stump',
+    author_email='joe@stu.mp',
+    description='A very simple XML manipulation.',
+    url='https://github.com/joestump/kissxml',
+    packages=['kissxml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
