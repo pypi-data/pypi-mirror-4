df63bba95a88598a3b89d058b03ce21419a6ded9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:20 +0000

    Add package django-linguo/1.3.2/django-linguo-1.3.2.tar.gz

---
+++ packages/django-linguo/django-linguo-1.3.2.tar.gz/django-linguo-1.3.2/linguo/tests/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from linguo.forms import MultilingualModelForm
+from linguo.tests.models import Bar
+
+
+class BarForm(forms.ModelForm):
+    class Meta:
+        model = Bar
+
+
+class BarFormWithFieldsSpecified(forms.ModelForm):
+    class Meta:
+        model = Bar
+        fields = ('name', 'price', 'description', 'quantity',)
+
+
+class BarFormWithFieldsExcluded(forms.ModelForm):
+    class Meta:
+        model = Bar
+        exclude = ('categories', 'name',)
+
+
+class BarFormWithCustomField(BarFormWithFieldsSpecified):
+    custom_field = forms.CharField(_('custom'))
+
+
+class MultilingualBarFormAllFields(MultilingualModelForm):
+    class Meta:
+        model = Bar
