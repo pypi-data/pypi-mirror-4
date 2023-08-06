ac570ec7a5a486d340341199e09c1283a6328801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:48 +0000

    Add package django-async-gt/0.6/django-async-gt-0.6.tar.gz

---
+++ packages/django-async-gt/django-async-gt-0.6.tar.gz/django-async-gt-0.6/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import async_image_save
+
+setup(
+    name='django-async-gt',
+    data_files=[('', ['requirements.txt', 'README.rst', 'LICENSE', 'MANIFEST', '.gitignore']),],
+    version='0.6',
+    packages=['async_image_save',],
+    description='a simple app to asynchronously upload images with Django, using Celery',
+    long_description=open('README.rst').read(),
+    author='Gregory Terzian',
+    author_email='gregory.terzian@gmail.com',
+    license='BSD License',
+    url='http://github.com/gterzian/django_async.git',
+    platforms=["any"],
+    requires=['django', 'celery', 'pil'],
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
