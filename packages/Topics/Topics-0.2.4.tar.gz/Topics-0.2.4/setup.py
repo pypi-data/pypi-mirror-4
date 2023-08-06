d820747e43e94dacd5f0e2ba8f62384129f6cc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package Topics/0.2.4/Topics-0.2.4.tar.gz

---
+++ packages/Topics/Topics-0.2.4.tar.gz/Topics-0.2.4/setup.py
@@ -0,0 +1,17 @@
+#coding:utf8
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name='Topics',
+    version="0.2.4",
+    author='Flávio C. Coelho',
+    author_email="fccoelho@gmail.com",
+    license="GPLv3",
+    url="https://github.com/NAMD/topicmodeling",
+    description="Set of tools for topic modeling and visualization",
+    ext_modules=cythonize("Topics/visualization/*.pyx"),
+    packages=['Topics', 'Topics.onlineldavb', 'Topics.onlinehdp', 'Topics.visualization'],
+    install_requires=['numpy>=1.6.2', 'PIL>=1.1.7', 'cython>=0.18', 'regex'],
+    #    package_data={'tests': ['data/*']},
+)
