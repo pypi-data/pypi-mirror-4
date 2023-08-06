0f5576934e1fcf9032fc8c59538cd8e6f29372cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:35 +0000

    Add package nester-yogi/1.1.0/nester-yogi-1.1.0.tar.gz

---
+++ packages/nester-yogi/nester-yogi-1.1.0.tar.gz/nester-yogi-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name  =   'nester-yogi',
+      version   = '1.1.0',
+      py_modules    =   ['nester'],
+      author    =   'hfpython',
+      author_email  =   'yogesh.b.d@gmail.com',
+      url   =   'http://geekyogi.com',
+      description   =   'A simple nester class'
+      )
