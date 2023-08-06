a377664e0c15a4057da3f15ca19f7fd92cfa9f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:53 +0000

    Add package rosdistro/0.2.7/rosdistro-0.2.7.tar.gz

---
+++ packages/rosdistro/rosdistro-0.2.7.tar.gz/rosdistro-0.2.7/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rosdistro',
+    version='0.2.7',
+    install_requires=['argparse', 'catkin_pkg', 'distribute', 'rospkg', 'PyYAML'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=[
+#        'scripts/rosdistro',
+        'scripts/rosdistro_build_cache',
+#        'scripts/rosdistro_convert',
+#        'scripts/rosdistro_generate_cache',
+        'scripts/rosdistro_reformat'
+    ],
+    author='Wim Meeussen, Dirk Thomas',
+    author_email='wim@hidof.com, dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='http://www.ros.org/wiki/rosdistro',
+    download_url='http://pr.willowgarage.com/downloads/rosdistro/',
+    keywords=['ROS'],
+    classifiers=['Programming Language :: Python',
+                 'License :: OSI Approved :: BSD License',
+                 'License :: OSI Approved :: MIT License'],
+    description="A tool to work with rosdistro files",
+    long_description="""A tool to work with rosdistro files""",
+    license='BSD, MIT'
+)
