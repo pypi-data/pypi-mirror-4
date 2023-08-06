c52f0dbf13d843b8a315391f767ba20a4fa96c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:28 +0000

    Add package frobozz/0.1/frobozz-0.1.tar.gz

---
+++ packages/frobozz/frobozz-0.1.tar.gz/frobozz-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='frobozz',
+    version='0.1',
+    py_modules=['frobozz'],
+    author='Distlib User',
+    author_email='distlib.user@dummy.org',
+)
