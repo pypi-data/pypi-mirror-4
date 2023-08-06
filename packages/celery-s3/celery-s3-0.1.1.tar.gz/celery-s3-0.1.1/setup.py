d2305c1ca4c66f5d2f6ad242f20f0f6604b76d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package celery-s3/0.1.1/celery-s3-0.1.1.tar.gz

---
+++ packages/celery-s3/celery-s3-0.1.1.tar.gz/celery-s3-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='celery-s3',
+    version='0.1.1',
+    description='An S3 result store backend for Celery',
+    long_description=open('README.md').read(),
+    author='Rob Golding',
+    author_email='rob@robgolding.com',
+    license='BSD',
+    url='https://github.com/robgolding63/celery-s3',
+    download_url='https://github.com/robgolding63/celery-s3/downloads',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'boto>=2.8.0,<3.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Distributed Computing',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+)
