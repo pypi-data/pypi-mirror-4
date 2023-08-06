94f7748261a7e0d0f45caeace03573b9f1f9e95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:26 +0000

    Add package psphere/0.5.2/psphere-0.5.2.tar.gz

---
+++ packages/psphere/psphere-0.5.2.tar.gz/psphere-0.5.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name="psphere",
+      version="0.5.2",
+      description="vSphere SDK for Python",
+      long_description=read("README.rst"),
+      author="Jonathan Kinred",
+      author_email="jonathan.kinred@gmail.com",
+      url="https://github.com/jkinred/psphere",
+      packages=["psphere"],
+      package_data={"psphere": ["wsdl/*"]},
+      install_requires=["suds", "PyYAML"],
+      keywords=["vsphere", "vmware"],
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: Apache Software License"],
+     )
