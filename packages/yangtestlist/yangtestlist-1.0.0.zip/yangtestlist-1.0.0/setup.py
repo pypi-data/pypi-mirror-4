62b01a1b1392f575cfbc21e7902cceeed554c9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:32 +0000

    Add package yangtestlist/1.0.0/yangtestlist-1.0.0.zip

---
+++ packages/yangtestlist/yangtestlist-1.0.0.zip/yangtestlist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = "yangtestlist",
+	version = "1.0.0",
+	py_modules = ["yangtestlist"],
+	author = "kevin.Yang",
+	author_email = "yanxingyang@gmail.com",
+	url = "http://www.yangyanxing.com",
+	description = "This is just a sample in head first python book",
+)
