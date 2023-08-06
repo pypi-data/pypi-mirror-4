62dedb9823a12fa7e9a4bc62d6d7770ada1ee3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:25 +0000

    Add package Gnotty/0.2.5/Gnotty-0.2.5.tar.gz

---
+++ packages/Gnotty/Gnotty-0.2.5.tar.gz/Gnotty-0.2.5/gnotty/example_project/build/gevent-socketio/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gevent-socketio",
+    version="0.3.5-beta",
+    description=(
+        "SocketIO server based on the Gevent pywsgi server, "
+        "a Python network library"),
+    author="Jeffrey Gelens",
+    author_email="jeffrey@noppo.pro",
+    maintainer="Alexandre Bourget",
+    maintainer_email="alex@bourget.cc",
+    license="BSD",
+    url="https://github.com/abourget/gevent-socketio",
+    download_url="https://github.com/abourget/gevent-socketio",
+    install_requires=("gevent-websocket", "anyjson"),
+    setup_requires=("versiontools >= 1.7",),
+    packages=find_packages(exclude=["examples", "tests"]),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)
---
+++ packages/Gnotty/Gnotty-0.2.5.tar.gz/Gnotty-0.2.5/gnotty/example_project/build/gevent-socketio/socketio/server.py
@@ -0,0 +1,78 @@
+import sys
+import traceback
+
+from socket import error
+
+from gevent.pywsgi import WSGIServer
+
+from socketio.handler import SocketIOHandler
+from socketio.policyserver import FlashPolicyServer
+from socketio.virtsocket import Socket
+
+__all__ = ['SocketIOServer']
+
+
+class SocketIOServer(WSGIServer):
+    """A WSGI Server with a resource that acts like an SocketIO."""
+
+    def __init__(self, *args, **kwargs):
+        """
+        This is just like the standard WSGIServer __init__, except with a
+        few additional ``kwargs``:
+
+        :param namespace: The namespace to use. Defaults to the global
+            namespace.
+        :param transports: Optional list of transports to allow. List of
+            strings, each string should be one of
+            handler.SocketIOHandler.handler_types.
+        :param policy_server: Boolean describing whether or not to use the
+            Flash policy server.  Default True.
+        """
+        self.sockets = {}
+        if 'resource' in kwargs:
+            print "DEPRECATION WARNING: use `namespace` instead of `resource`"
+        self.namespace = kwargs.pop('resource', kwargs.pop('namespace',
+                                                           'socket.io'))
+        self.transports = kwargs.pop('transports', None)
+
+        if kwargs.pop('policy_server', True):
+            self.policy_server = FlashPolicyServer()
+        else:
+            self.policy_server = None
+
+        kwargs['handler_class'] = SocketIOHandler
+        super(SocketIOServer, self).__init__(*args, **kwargs)
+
+    def start_accepting(self):
+        if self.policy_server is not None:
+            try:
+                self.policy_server.start()
+            except error, ex:
+                sys.stderr.write(
+                    'FAILED to start flash policy server: %s\n' % (ex, ))
+            except Exception:
+                traceback.print_exc()
+                sys.stderr.write('FAILED to start flash policy server.\n\n')
+        super(SocketIOServer, self).start_accepting()
+
+    def kill(self):
+        if self.policy_server is not None:
+            self.policy_server.kill()
+        super(SocketIOServer, self).kill()
+
+    def handle(self, socket, address):
+        handler = self.handler_class(socket, address, self)
+        handler.handle()
+
+    def get_socket(self, sessid=''):
+        """Return an existing or new client Socket."""
+
+        socket = self.sockets.get(sessid)
+
+        if socket is None:
+            socket = Socket(self)
+            self.sockets[socket.sessid] = socket
+        else:
+            socket.incr_hits()
+
+        return socket
