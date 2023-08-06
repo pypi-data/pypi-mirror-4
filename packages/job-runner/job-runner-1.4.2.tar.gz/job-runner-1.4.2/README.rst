b2c67dd37d1f6970367421a4212b8023692f1320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:53 +0000

    Add package job-runner/1.4.2/job-runner-1.4.2.tar.gz

---
+++ packages/job-runner/job-runner-1.4.2.tar.gz/job-runner-1.4.2/README.rst
@@ -0,0 +1,240 @@
+Job-Runner
+==========
+
+This project contains the Job-Runner dashboard and admin interface to
+manage and view the current status of scheduled jobs.
+
+For the full documentation see ``docs/``.
+
+
+Changes
+-------
+
+v1.4.2
+~~~~~~
+
+* Fix the issue where runs from other projects than the selected one, were
+  rendered on the dashboard.
+
+
+v1.4.1
+~~~~~~
+
+* Add caching of objects to improve the performance (frontend).
+
+
+v1.4.0
+~~~~~~
+
+* Complete refactor of front-end code. The front-end is now based on AngularJS.
+* Misc admin interface improvements (sorting, labels, etc...).
+* Add if the run was manual and / or killed to the error e-mail template.
+* Add compressor for JavaScript code.
+
+
+v1.3.3
+~~~~~~
+
+* Broadcast ping requests to the worker (default: every 5 minutes) and show
+  last ping response in admin. This will make it more easy to discover problems
+  with workers.
+
+
+v1.3.2
+~~~~~~
+
+* Spil specific settings removed.
+
+
+v1.3.1
+~~~~~~
+
+* Fix run status modal JavaScript code (was not working when there is no log
+  yet).
+* Fix related name of ``run_log``, to make sure it shows up correctly in the
+  template when there is an error.
+
+
+v1.3.0
+~~~~~~
+
+* Move logs to separate model and RESTful resource. Make sure that you update
+  the worker to >= v1.1.0.
+
+
+v1.2.10
+~~~~~~~
+
+* Show full path (project - template - worker ...) in object title.
+* Improve ordering of objects in the admin.
+* Added getting started section to the docs.
+* Misc documentation improvements.
+
+
+v1.2.9
+~~~~~~
+
+* Fix kill button so that it is only visible when the user has permission to
+  kill a job-run (would else result in a HTTP error).
+
+
+v1.2.8
+~~~~~~
+
+* Add option in dashboard to kill job-runs.
+* Fix time-zone in failed-run e-mail template (will now use the time-zone
+  configured in the Django config).
+
+
+v1.2.7
+~~~~~~
+
+* Disable the job when it failed more than x times (optional setting).
+
+
+v1.2.6
+~~~~~~
+
+* Make it possible to disable the enqueue of a project, worker or job-template.
+
+
+v1.2.5
+~~~~~~
+
+* Display parent - child relationships in job details.
+* Fix an other issue with the run broadcaster to make sure it doesn't broadcast
+  multiple runs for the same job.
+* Fix styling glitches by adding a ``boot.css`` which is used as long the
+  ``.less`` files aren't compiled yet.
+
+
+v1.2.4
+~~~~~~
+
+* Fix run broadcaster so that it doesn't send runs to the workers when there
+  is still an other run for the same job active (in queue or started).
+* Fix autoselect environment settings.
+* Update hostnames in configuration.
+
+
+v1.2.3
+~~~~~~
+
+* Add filters to limit the number of displayed jobs.
+* Add status icon to display if there is a connection with the WebSocket server
+* Add option for monthly re-scheduling **Note:** monthly re-scheduling works
+  by incrementing the ``dts`` with the number of days that are in the ``dts``.
+  When incrementing by multiple months, it will check the days for each month.
+
+
+v1.2.2
+~~~~~~
+
+* Add description fields to projects, workers, job-templates and jobs
+* Add ``TransactionMiddleware``
+* Fix rescheduling when two runs are active of the same job
+
+
+v1.2.1
+~~~~~~
+
+* Add MySQL to requirements.txt (since the python setup.py install is
+  creating a zipped .egg which doesn't work when the user does not have
+  a homedir (or when the homedir is not executable).
+
+
+v1.2.0
+~~~~~~
+
+* Show job details in a column instead of a modal
+* Show last 100 runs of a job incl. duration graph
+* Fix ``AUTHENTICATION_BACKENDS`` setting for staging and production
+  (without ``ModelBackend`` included, permissions are not working!)
+* A job-title must now be unique per job-template
+* Show re-schedule interval in job details
+
+
+v1.1.1
+~~~~~~
+
+* Fix dependencies in ``setup.py`` (was not using the development version
+  from GitHub).
+
+
+v1.1.0
+~~~~~~
+
+* Run and job details are made deeplinkable
+* Runs and jobs that are suspended are greyed-out
+
+
+v1.0.0
+~~~~~~
+
+* Fix size (height) of run / job headers
+* Fix order of run objects
+* Fix escaping of HTML characters in job script and log output
+* Enable timezone, all data is now presented (and expected to be) in the
+  *Europe/Amsterdam* timezone
+* Order scheduled runs ascending (first to run on top)
+* Add option to schedule children or not, when manually scheduling runs
+
+v0.7.4
+~~~~~~
+
+* Deployar related changes.
+
+
+v0.7.3
+~~~~~~
+
+* Add ``job_runner.settings.env.production_longrun`` settings module for long
+  running processes to avoid "cached" results.
+
+
+v0.7.2
+~~~~~~
+
+* Fix issue where filtering the groups would result in duplicated results.
+* Remove WebKit browser notifications, since it was breaking the front-end in
+  Firefox.
+
+
+v0.7.1
+~~~~~~
+
+* Check that runs received from the WebSocket server are within the current
+  active project.
+* Add WebKit browser notifications.
+
+
+v0.7.0
+~~~~~~
+
+* **Major refactor:** It is now possible to use Sheldon assigned groups when
+  creating projects. Since basically all models are changed / renamed, it was
+  not possible to migrate old data to the new structure. Therefore you should
+  re-create the database!
+* Add ``broadcast_queue`` management command to publish enqueueable runs to
+  the workers.
+
+
+v0.6.2
+~~~~~~
+
+* Add missing static-file and logging settings.
+
+
+v0.6.1
+~~~~~~
+
+* Add MySQL package as a requirement + update requirements in docs.
+* Make it possible to override production settings by creating
+  ``/bigdisk/docs/DATA/job_runner/settings_production.py``.
+
+
+v0.6.0
+~~~~~~
+
+* Move Job-Runner code out of engportal project.
+* Add overview of jobs + scheduling.
---
+++ packages/job-runner/job-runner-1.4.2.tar.gz/job-runner-1.4.2/job_runner/apps/job_runner/api.py
@@ -0,0 +1,389 @@
+from django.contrib.auth.models import Group
+from tastypie import fields
+from tastypie.authentication import MultiAuthentication, SessionAuthentication
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+from tastypie.resources import ModelResource
+
+from job_runner.apps.job_runner.auth import (
+    HmacAuthentication, ModelAuthorization)
+from job_runner.apps.job_runner.models import (
+    Job,
+    JobTemplate,
+    KillRequest,
+    Project,
+    Run,
+    RunLog,
+    Worker,
+)
+
+
+class NoRelatedSaveMixin(object):
+    def save_related(self, *args, **kwargs):
+        """
+        Override to NOT save related models.
+
+        In this case there is no need for it (it will update the related run
+        every time a kill-requests is updated). Case where it goes wrong:
+
+        * Kill requests has been executed -> PATCH kill_request
+        * Run returned                    -> PATCH run
+
+        Since the PATCH kill_request will do a select on the run (and getting
+        the old data), around the same time the PATCH run will update the run
+        and around the same time the PATCH kill_request will update the run
+        as well, we will end-up with a not-updated run.
+
+        An other option to work around this is to use the SERIALIZE transaction
+        level. However, this seems not to be possible with all Django DB
+        backends.
+
+        See: https://github.com/toastdriven/django-tastypie/issues/742
+
+        """
+        pass
+
+
+class GroupResource(ModelResource):
+    """
+    RESTful resource for Django groups.
+    """
+    class Meta:
+        queryset = Group.objects.all()
+        resource_name = 'group'
+        allowed_methods = ['get']
+        fields = ['name']
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='jobtemplate__worker__api_key',
+            user_groups_path='',
+        )
+
+
+class ProjectResource(ModelResource):
+    """
+    RESTful resource for projects.
+    """
+    class Meta:
+        queryset = Project.objects.all()
+        resource_name = 'project'
+        allowed_methods = ['get']
+        fields = ['title', 'id', 'description', 'enqueue_is_enabled']
+        filtering = {
+            'id': ('exact',),
+        }
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='worker__api_key',
+            user_groups_path='groups',
+        )
+
+
+class WorkerResource(ModelResource):
+    """
+    RESTful resource for workers.
+    """
+    project = fields.ToOneField(
+        'job_runner.apps.job_runner.api.ProjectResource', 'project')
+
+    class Meta:
+        queryset = Worker.objects.all()
+        resource_name = 'worker'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get', 'patch']
+        fields = [
+            'id',
+            'title',
+            'api_key',
+            'description',
+            'enqueue_is_enabled',
+            'ping_response_dts'
+        ]
+        filtering = {
+            'project': ALL_WITH_RELATIONS,
+            'api_key': ALL,
+        }
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='api_key',
+            user_groups_path='project__groups',
+        )
+
+
+class JobTemplateResource(ModelResource):
+    """
+    RESTful resource for job-templates.
+    """
+    worker = fields.ToOneField(
+        'job_runner.apps.job_runner.api.WorkerResource', 'worker')
+    auth_groups = fields.ToManyField(
+        'job_runner.apps.job_runner.api.GroupResource',
+        'auth_groups',
+        null=True
+    )
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+        orm_filters = super(JobTemplateResource, self).build_filters(filters)
+
+        if 'project_id' in filters:
+            orm_filters.update({
+                'worker__project__id': filters['project_id']
+            })
+
+        return orm_filters
+
+    class Meta:
+        queryset = JobTemplate.objects.all()
+        resource_name = 'job_template'
+        allowed_methods = ['get']
+        fields = ['id', 'title', 'description', 'enqueue_is_enabled']
+        filtering = {
+            'worker': ALL_WITH_RELATIONS,
+        }
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='worker__api_key',
+            user_groups_path='worker__project__groups',
+        )
+
+
+class JobResource(NoRelatedSaveMixin, ModelResource):
+    """
+    RESTful resource for jobs.
+    """
+    job_template = fields.ToOneField(
+        'job_runner.apps.job_runner.api.JobTemplateResource', 'job_template')
+    parent = fields.ToOneField('self', 'parent', null=True)
+    children = fields.ToManyField('self', 'children', null=True)
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+        orm_filters = super(JobResource, self).build_filters(filters)
+
+        if 'project_id' in filters:
+            orm_filters.update({
+                'job_template__worker__project__id': filters['project_id']
+            })
+
+        return orm_filters
+
+    class Meta:
+        queryset = Job.objects.all()
+        resource_name = 'job'
+        detail_allowed_methods = ['get', 'put']
+        list_allowed_methods = ['get']
+        fields = [
+            'id',
+            'title',
+            'description',
+            'script_content',
+            'enqueue_is_enabled',
+            'reschedule_interval',
+            'reschedule_interval_type',
+            'reschedule_type',
+        ]
+        filtering = {
+            'job_template': ALL_WITH_RELATIONS,
+        }
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='job_template__worker__api_key',
+            user_groups_path='job_template__worker__project__groups',
+            auth_user_groups_path='job_template__auth_groups',
+        )
+
+
+class RunResource(NoRelatedSaveMixin, ModelResource):
+    """
+    RESTful resource for job runs.
+    """
+    job = fields.ToOneField(
+        'job_runner.apps.job_runner.api.JobResource', 'job')
+
+    run_log = fields.ToOneField(
+        'job_runner.apps.job_runner.api.RunLogResource', 'run_log',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        queryset = Run.objects.filter()
+        resource_name = 'run'
+        detail_allowed_methods = ['get', 'patch']
+        list_allowed_methods = ['get', 'post']
+        filtering = {
+            'schedule_dts': ALL,
+            'job': ALL_WITH_RELATIONS,
+        }
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='job__job_template__worker__api_key',
+            user_groups_path='job__job_template__worker__project__groups',
+            auth_user_groups_path='job__job_template__auth_groups',
+        )
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+        orm_filters = super(RunResource, self).build_filters(filters)
+
+        state_filters = {
+            'scheduled': {
+                'enqueue_dts__isnull': True,
+            },
+            'in_queue': {
+                'enqueue_dts__isnull': False,
+                'start_dts__isnull': True,
+            },
+            'started': {
+                'enqueue_dts__isnull': False,
+                'start_dts__isnull': False,
+                'return_dts__isnull': True,
+            },
+            'completed': {
+                'enqueue_dts__isnull': False,
+                'start_dts__isnull': False,
+                'return_dts__isnull': False,
+            },
+            'completed_successful': {
+                'enqueue_dts__isnull': False,
+                'start_dts__isnull': False,
+                'return_dts__isnull': False,
+                'return_success': True,
+            },
+            'completed_with_error': {
+                'enqueue_dts__isnull': False,
+                'start_dts__isnull': False,
+                'return_dts__isnull': False,
+                'return_success': False,
+            }
+        }
+
+        if 'state' in filters and filters['state'] in state_filters:
+            orm_filters.update(state_filters[filters['state']])
+
+        if 'project_id' in filters:
+            orm_filters.update({
+                'job__job_template__worker__project__id': filters['project_id']
+            })
+
+        return orm_filters
+
+    def obj_update(self, bundle, request=None, *args, **kwargs):
+        """
+        Override of the default obj_update method.
+
+        This will call the ``reschedule`` method after a successful object
+        update, which will re-schedule the job if needed (incl. children).
+
+        If the object update represents the returning of a run with error,
+        it will call also ``send_error_notification`` method.
+
+        """
+        deserialized = self.deserialize(
+            request,
+            request.raw_post_data,
+            format=request.META.get('CONTENT_TYPE', 'application/json')
+        )
+
+        result = super(RunResource, self).obj_update(
+            bundle, request, *args, **kwargs)
+
+        job = bundle.obj.job
+
+        if (deserialized.get('return_dts', None) and
+                deserialized.get('return_success', None) is False):
+            # the job failed
+            bundle.obj.send_error_notification()
+            job.fail_times = job.fail_times + 1
+
+            # disable job when it failed more than x times
+            if (job.disable_enqueue_after_fails and
+                    job.fail_times > job.disable_enqueue_after_fails):
+                job.enqueue_is_enabled = False
+
+            job.save()
+
+        job.reschedule()
+
+        if (deserialized.get('return_dts', None) and
+                deserialized.get('return_success', None) is True):
+            # reset the fail count
+            job.fail_times = 0
+            job.save()
+
+        if (deserialized.get('return_dts', None) and
+                deserialized.get('return_success', None) is True and
+                bundle.obj.schedule_children):
+            # the job completed successfully and has children to
+            # schedule now
+            for child in bundle.obj.job.children.all():
+                child.schedule_now()
+
+        return result
+
+
+class KillRequestResource(NoRelatedSaveMixin, ModelResource):
+    """
+    RESTful resource for kill requests.
+    """
+    run = fields.ToOneField(
+        'job_runner.apps.job_runner.api.RunResource', 'run')
+
+    class Meta:
+        queryset = KillRequest.objects.all()
+        resource_name = 'kill_request'
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'patch']
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='run__job__job_template__worker__api_key',
+            user_groups_path='run__job__job_template__worker__project__groups',
+            auth_user_groups_path='run__job__job_template__auth_groups',
+        )
+
+
+class RunLogResource(NoRelatedSaveMixin, ModelResource):
+    """
+    RESTful resource for run log-output.
+    """
+    run = fields.ToOneField(
+        'job_runner.apps.job_runner.api.RunResource', 'run')
+
+    class Meta:
+        queryset = RunLog.objects.all()
+        resource_name = 'run_log'
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'patch']
+
+        authentication = MultiAuthentication(
+            SessionAuthentication(), HmacAuthentication())
+
+        authorization = ModelAuthorization(
+            api_key_path='run__job__job_template__worker__api_key',
+            user_groups_path='run__job__job_template__worker__project__groups',
+            auth_user_groups_path='run__job__job_template__auth_groups',
+        )
