d13c734c05bf47f102d20cddaee732d62febfb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:04 +0000

    Add package evenodd/1.3.0/evenodd-1.3.0.zip

---
+++ packages/evenodd/evenodd-1.3.0.zip/evenodd-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'evenodd',
+    version         = '1.3.0',
+    py_modules      = ['evenodd'],
+    author          = 'rhemon19',
+    author_email    = 'rhemon19@gmail.com',
+    url             = 'pypi.python.org',
+    description     = 'A simple printer which will tell you whether the number you enter is an odd number or an even number.',
+    )
