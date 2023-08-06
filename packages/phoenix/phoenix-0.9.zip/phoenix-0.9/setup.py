74b6f36db1dbd5e43b94042c2514d82b5e365373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:32 +0000

    Add package phoenix/0.9/phoenix-0.9.zip

---
+++ packages/phoenix/phoenix-0.9.zip/phoenix-0.9/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='phoenix',
+      version='0.9',
+      description='MUS Interface for Phoenix Emulator 3.x',
+      author='Bui',
+      author_email='bui@bui.pm',
+      url='https://pypi.python.org/pypi/phoenix',
+      packages=['phoenix'],
+      data_files=['LICENSE']
+     )
