96b4f2fce615a906f76846ffee1df32155a279f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:53 +0000

    Add package pandora-client/0.2.116/pandora_client-0.2.116.tar.gz

---
+++ packages/pandora-client/pandora_client-0.2.116.tar.gz/pandora_client-0.2.116/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# vi:si:et:sw=4:sts=4:ts=4
+# encoding: utf-8
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def get_bzr_version():
+    return 116 #import os
+    info = os.path.join(os.path.dirname(__file__), '.bzr/branch/last-revision')
+    if os.path.exists(info):
+        f = open(info)
+        rev = int(f.read().split()[0])
+        f.close()
+        if rev:
+            return u'%s' % rev
+    return u'unknown'
+
+setup(
+    name="pandora_client",
+    version="0.2.%s" % get_bzr_version() ,
+    description='''pandora_client is a commandline client for pan.do/ra.
+You can use it to import videos into a pan.do/ra system.
+It is currently known to work on Linux and Mac OS X.
+''',
+    author="j",
+    author_email="j@mailb.org",
+    url="http://wiki.0x2620.org/wiki/pandora_client",
+    download_url="http://code.0x2620.org/pandora_client/download",
+    license="GPLv3",
+    scripts = [
+        'bin/pandora_client',
+    ],
+    packages=[
+        'pandora_client'
+    ],
+    install_requires=['ox'],
+    keywords = [
+],
+    classifiers = [
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+)
+
