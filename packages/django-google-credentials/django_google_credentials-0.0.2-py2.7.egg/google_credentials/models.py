110cf4e110cdfea3f26e9904640d9534173b0c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package django-google-credentials/0.0.2/django_google_credentials-0.0.2-py2.7.egg

---
+++ packages/django-google-credentials/django_google_credentials-0.0.2-py2.7.egg/google_credentials/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from oauth2client.django_orm import CredentialsField
+
+
+class Credentials(models.Model):
+    client_id = models.CharField(
+        max_length=128
+    )
+    credentials = CredentialsField(
+        editable=False,
+        blank=True,
+        null=True,
+    )
