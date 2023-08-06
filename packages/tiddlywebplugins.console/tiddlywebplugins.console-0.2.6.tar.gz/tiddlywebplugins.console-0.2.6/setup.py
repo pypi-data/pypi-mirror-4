f3a9dbb22b482755e596a497db94f85e67638cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:07 +0000

    Add package tiddlywebplugins.console/0.2.6/tiddlywebplugins.console-0.2.6.tar.gz

---
+++ packages/tiddlywebplugins.console/tiddlywebplugins.console-0.2.6.tar.gz/tiddlywebplugins.console-0.2.6/setup.py
@@ -0,0 +1,34 @@
+AUTHOR = "FND"
+AUTHOR_EMAIL = "FNDo@gmx.net"
+NAME = "tiddlywebplugins.console"
+DESCRIPTION = "TiddlyWeb explorer"
+VERSION = "0.2.6" # N.B.: duplicate of tiddlywebplugins.console.__init__
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+	namespace_packages = ["tiddlywebplugins"],
+	name = NAME,
+	version = VERSION,
+	description = DESCRIPTION,
+	long_description = open(os.path.join(os.path.dirname(__file__), "README")).read(),
+	author = AUTHOR,
+	url = "http://pypi.python.org/pypi/%s" % NAME,
+	packages = find_packages(exclude="test"),
+	author_email = AUTHOR_EMAIL,
+	platforms = "Posix; MacOS X; Windows",
+	install_requires = [
+		"setuptools",
+		"tiddlyweb",
+		"tiddlywebplugins.instancer>=0.7.2",
+		"tiddlywebplugins.twimport>=0.5",
+		"tiddlywebplugins.status",
+		"httpexceptor"
+	],
+	include_package_data = True,
+	zip_safe = False
+)
