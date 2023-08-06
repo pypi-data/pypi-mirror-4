b6ae842a2811edf670116ba8ea2ab2733ffcf9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package django-pin/1.2.3/django-pin-1.2.3.tar.gz

---
+++ packages/django-pin/django-pin-1.2.3.tar.gz/django-pin-1.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+
+from distutils.core import setup
+
+
+version = '1.2.3'
+
+setup(
+    name = "django-pin",
+    version = version,
+    author = "vahid chakoshy",
+    author_email = "vchakoshy@gmail.com",
+    description = "pinterest apllication like in Django",
+    url = "https://github.com/karoon/django-pin",
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe=False,
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Natural Language :: Persian',
+        
+    ]
+)
