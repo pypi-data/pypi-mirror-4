f47e6da0637dcacd5d4d9303097913373723670d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:39 +0000

    Add package Ashser_AthleteList/1.0.0/Ashser_AthleteList-1.0.0.zip

---
+++ packages/Ashser_AthleteList/Ashser_AthleteList-1.0.0.zip/Ashser_AthleteList-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+	name				=	'Ashser_AthleteList',
+	version				=	'1.0.0',
+	py_modules			=	['Ashser_AthleteList'],
+	author				=	'Ashser',
+	author_email		=	'night_eagle21@hotmail.com',
+	description			=	'A modification of the list class',
+	)
