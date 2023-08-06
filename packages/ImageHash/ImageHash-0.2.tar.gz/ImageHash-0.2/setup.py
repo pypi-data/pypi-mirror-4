33232f33dfb93fca32be70cabf725d7b1990874a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:26 +0000

    Add package ImageHash/0.2/ImageHash-0.2.tar.gz

---
+++ packages/ImageHash/ImageHash-0.2.tar.gz/ImageHash-0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='ImageHash',
+    version='0.2',
+    author='Johannes Buchner',
+    author_email='buchner.johannes@gmx.at',
+    packages=['imagehash'],
+    scripts=['find_similar_images.py'],
+    url='http://pypi.python.org/pypi/imagehash/',
+    license='LICENSE',
+    description='Image Hashing library',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "scipy",
+        "numpy",
+        "PIL",
+    ],
+)
+
