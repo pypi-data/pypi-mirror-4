1ae75a6c746c3121c4fab19802b209199cea5e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package djset/0.2.1/djset-0.2.1.tar.gz

---
+++ packages/djset/djset-0.2.1.tar.gz/djset-0.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import distribute_setup
+import sys
+distribute_setup.use_setuptools()
+from setuptools import setup
+
+
+INSTALL_REQUIRES = ["keyring", "docopt"]
+if sys.version_info[0] < 3: # Install the backport of configparser
+    INSTALL_REQUIRES.append("configparser")
+    
+
+setup(name='djset',
+    version='0.2.1',
+    description="Utilities for Django settings secrets",
+    long_description=open('README.md').read(),
+    author='Brett Haydon',
+    author_email='brett@haydon.id.au',
+    url='https://github.com/bretth/djset',
+    packages=['djset'],
+    py_modules=['distribute_setup'],
+    license='LICENSE.txt',
+    entry_points={"console_scripts":
+    ["djs_locate_settings = djset.utils:locate_settings",
+     "djsecret = djset.commands:djsecret",
+     "djconfig = djset.commands:djconfig",
+     ]},
+    scripts=['dexportunset.sh',],
+    install_requires = INSTALL_REQUIRES,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development',
+    ]
+)
