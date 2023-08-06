ea77e234f33772aabfcd86658f8d387fd19ae972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:45 +0000

    Add package pingdom/0.2.0/pingdom-0.2.0.tar.gz

---
+++ packages/pingdom/pingdom-0.2.0.tar.gz/pingdom-0.2.0/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+
+setup(name='pingdom',
+      version="0.2.0",
+      description='Pingdom Library',
+      long_description="""3rd-party Python interface to Pingdom's REST API.""",
+      author='Mike Babineau',
+      author_email='michael.babineau@gmail.com',
+      install_requires=[ 'requests>=0.10.8', 'simplejson'],
+      url='https://github.com/mbabineau/pingdom-python',
+      packages=['pingdom'],
+      license='Apache v2.0',
+      platforms='Posix; MacOS X; Windows',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Topic :: System :: Monitoring', ],
+      )
