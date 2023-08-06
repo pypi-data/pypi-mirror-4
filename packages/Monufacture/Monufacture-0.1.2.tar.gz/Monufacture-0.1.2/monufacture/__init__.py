cd5afb824ce6ba61fae3a1f337827d0dd4f94b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:14 +0000

    Add package Monufacture/0.1.2/Monufacture-0.1.2.tar.gz

---
+++ packages/Monufacture/Monufacture-0.1.2.tar.gz/Monufacture-0.1.2/monufacture/__init__.py
@@ -0,0 +1,49 @@
+from factory import Factory
+from dynamic import dependent, sequence, subdoc, id_of, random_text, dbref_to, date, ago, from_now
+
+# Registry for all factories
+factories = {}
+
+
+def factory(factory_document_name, collection=None, **attrs):
+    """Declares a new named factory with the given attributes."""
+    factory = Factory(collection, **attrs)
+    factories[factory_document_name] = factory
+
+
+def create(name, **overrides):
+    """Creates and returns instance of the named document using the factory
+    with which it was declared, utilising any provided attribute
+    overrides, storing the instance in the database."""
+    return factories[name].create(**overrides)
+
+
+def build(name, **overrides):
+    """Builds and returns instance of the named document using the factory
+    with which it was declared, utilising any provided attribute
+    overrides, without storing the instance in the database."""
+    return factories[name].build(**overrides)
+
+
+def build_list(name, count):
+    """Builds a list of `count` instances of the named document using the 
+    associated factory."""
+    return [build(name) for x in range(count)]
+
+
+def create_list(name, count):
+    """Creates a list of `count` instances of the named document using the 
+    associated factory."""
+    return [create(name) for x in range(count)]
+
+
+def cleanup():
+    """Cleans up all factory data generated since the process was started, 
+    or since the last time this method was called."""
+    for factory in factories.itervalues():
+        factory.cleanup()
+
+def reset():
+    """Resets Monufacturer, removing all registered factories. Only really 
+    here for testing purposes."""
+    factories.clear()
