4b2061e1b78c31062d805a31ec754e381bdb00e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:16 +0000

    Add package nestly/0.4/nestly-0.4.tar.gz

---
+++ packages/nestly/nestly-0.4.tar.gz/nestly-0.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+import nestly
+
+if sys.version_info < (2, 7, 0):
+    raise Exception('Python 2.7 is required.')
+
+extra = {}
+if sys.version_info >= (3, 0, 0):
+    extra['use_2to3'] = True
+
+setup(name='nestly',
+      version=nestly.__version__,
+      description="""Nestly is a collection of functions designed to make
+      running software with combinatorial choices of parameters easier.""",
+      author='Erick Matsen',
+      author_email='matsen@fhcrc.org',
+      packages=find_packages(),
+      test_suite='nestly.test',
+      entry_points={
+          'console_scripts': [
+              'nestrun = nestly.scripts.nestrun:main',
+              'nestagg = nestly.scripts.nestagg:main',
+          ]
+      },
+      **extra
+      )
