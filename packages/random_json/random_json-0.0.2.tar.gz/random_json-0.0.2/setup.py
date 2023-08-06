87b7660f8b4fa77008709810c27d416be79e56c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package random_json/0.0.2/random_json-0.0.2.tar.gz

---
+++ packages/random_json/random_json-0.0.2.tar.gz/random_json-0.0.2/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="random_json",
+      version="0.0.2",
+      description="Generate random JSON objects for testing purposes",
+      author="Maxwell Krohn",
+      author_email="themax@gmail.com",
+      url="https://github.com/maxtaco/python-random-json",
+      packages=['random_json'])
