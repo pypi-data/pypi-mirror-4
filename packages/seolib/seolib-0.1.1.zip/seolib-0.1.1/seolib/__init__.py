0839d8d7137fe3cf852574290fca24eec66fa38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package seolib/0.1.1/seolib-0.1.1.zip

---
+++ packages/seolib/seolib-0.1.1.zip/seolib-0.1.1/seolib/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+__title__ = 'seolib'
+__version__ = '0.1.1'
+
+
+from api import get_seomoz_data
+from api import get_alexa
+from api import get_semrush
+from api import get_tweets
+from api import get_google_plus
+from api import get_facebook_likes
