a219f20d91ee88177136efb40d305d2e5b9cc906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:33 +0000

    Add package pyeda/0.10.0/pyeda-0.10.0.tar.gz

---
+++ packages/pyeda/pyeda-0.10.0.tar.gz/pyeda-0.10.0/pyeda/dimacs.py
@@ -0,0 +1,234 @@
+"""
+Dimacs
+
+For more information on the input formats,
+see "Satisfiability Suggested Format".
+
+Interface Functions:
+    parse_cnf
+    parse_sat
+"""
+
+# standard library
+import collections
+import re
+
+# pyeda
+from pyeda.expr import Not, Or, And, Xor, Equal
+from pyeda.vexpr import bitvec
+
+Token = collections.namedtuple('Token', ['typ', 'val', 'line', 'col'])
+
+# lexical states
+_TOK_RE = (
+    ('WS', r'[ \t]+'),
+    ('NL', r'\n'),
+
+    ('CNF',   r'\bcnf\b'),
+    ('SATEX', r'\bsatex\b'),
+    ('SATX',  r'\bsatx\b'),
+    ('SATE',  r'\bsate\b'),
+    ('SAT',   r'\bsat\b'),
+
+    ('c', r'c.*'),
+    ('p', r'p'),
+
+    ('ZERO',   r'0'),
+    ('POSINT', r'[1-9][0-9]*'),
+
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+
+    ('NOT',   r'\-'),
+    ('OR',    r'\+'),
+    ('AND',   r'\*'),
+    ('XOR',   r'\bxor\b'),
+    ('EQUAL', r'='),
+)
+
+_regex = re.compile('|'.join("(?P<%s>%s)" % pair for pair in _TOK_RE))
+
+def iter_tokens(s):
+    """Iterate through all tokens in a DIMACS format input string.
+
+    This method is common between cnf/sat/satx/sate/satex formats
+    """
+    line = 1
+    pos = line_start = 0
+
+    m = _regex.match(s)
+    while m is not None:
+        typ = m.lastgroup
+        if typ == 'NL':
+            line_start = pos
+            line += 1
+        elif typ != 'WS':
+            val = m.group(typ)
+            if typ in {'ZERO', 'POSINT'}:
+                val = int(val)
+            col = m.start() - line_start
+            yield Token(typ, val, line, col)
+        pos = m.end()
+        m = _regex.match(s, pos)
+    if pos != len(s):
+        fstr = "unexpected character on line {}: {}"
+        raise SyntaxError(fstr.format(line, s[pos]))
+
+# Grammar for a CNF file
+#
+# CNF := COMMENT* PREAMBLE FORMULA
+#
+# COMMENT := 'c' .*
+#
+# PREAMBLE := 'p' 'cnf' VARIABLES CLAUSES
+#
+# VARIABLES := POSINT
+#
+# CLAUSES := POSINT
+#
+# FORMULA := POSINT+ ('0' POSINT+)*
+
+_CNF_FORMULA_TOKS = {'ZERO', 'NOT', 'POSINT'}
+
+def parse_cnf(s, varname='x'):
+    """Parse an input string in DIMACS CNF format, and return an expression."""
+    gtoks = iter_tokens(s)
+    try:
+        nvars, ncls = _cnf_preamble(gtoks)
+    except StopIteration:
+        raise SyntaxError("incomplete preamble")
+
+    X = bitvec(varname, (1, nvars + 1))
+    clauses = [[]]
+
+    try:
+        while True:
+            tok = _expect_token(gtoks, _CNF_FORMULA_TOKS)
+            if tok.typ == 'ZERO':
+                clauses.append([])
+            elif tok.typ == 'NOT':
+                tok = _expect_token(gtoks, {'POSINT'})
+                assert tok.val <= len(X)
+                clauses[-1].append(-X[tok.val])
+            else:
+                assert tok.val <= len(X)
+                clauses[-1].append(X[tok.val])
+    except StopIteration:
+        pass
+
+    assert len(clauses) == ncls
+    return And(*[Or(*clause) for clause in clauses])
+
+def _cnf_preamble(gtoks):
+    tok = _expect_token(gtoks, {'c', 'p'})
+    if tok.typ == 'c':
+        return _cnf_preamble(gtoks)
+    else:
+        _expect_token(gtoks, {'CNF'})
+        nvars = _expect_token(gtoks, {'POSINT'}).val
+        ncls = _expect_token(gtoks, {'POSINT'}).val
+        return nvars, ncls
+
+# Grammar for a SAT file
+#
+# SAT := COMMENT* PREAMBLE FORMULA
+#
+# COMMENT := 'c' .*
+#
+# PREAMBLE := 'p' FORMAT VARIABLES
+#
+# FORMAT := 'sat' | 'satx' | 'sate' | 'satex'
+#
+# VARIABLES := POSINT
+#
+# FORMULA := POSINT
+#          | '-' POSINT
+#          | '(' FORMULA ')'
+#          | '-' '(' FORMULA ')'
+#          | '+' '(' FORMULA* ')'
+#          | '*' '(' FORMULA* ')'
+#          | 'xor' '(' FORMULA* ')'
+#          | '=' '(' FORMULA* ')'
+
+_SAT_FORMAT_TOKS = {'SAT', 'SATX', 'SATE', 'SATEX'}
+
+_SAT_OP_TOKS = {
+    'sat': {'NOT', 'OR', 'AND'},
+    'satx': {'NOT', 'OR', 'AND', 'XOR'},
+    'sate': {'NOT', 'OR', 'AND', 'EQUAL'},
+    'satex': {'NOT', 'OR', 'AND', 'XOR', 'EQUAL'},
+}
+
+_SAT_TOK2OP = {
+    'NOT': Not,
+    'OR': Or,
+    'AND': And,
+    'XOR': Xor,
+    'EQUAL': Equal,
+}
+
+def parse_sat(s, varname='x'):
+    """Parse an input string in DIMACS SAT format, and return an expression."""
+    gtoks = iter_tokens(s)
+    try:
+        fmt, nvars = _sat_preamble(gtoks)
+    except StopIteration:
+        raise SyntaxError("incomplete preamble")
+
+    types = {'POSINT', 'LPAREN'} | _SAT_OP_TOKS[fmt]
+    X = bitvec(varname, (1, nvars + 1))
+    try:
+        tok = _expect_token(gtoks, types)
+        return _sat_formula(gtoks, tok, fmt, X)
+    except StopIteration:
+        raise SyntaxError("incomplete formula")
+
+def _sat_preamble(gtoks):
+    tok = _expect_token(gtoks, {'c', 'p'})
+    if tok.typ == 'c':
+        return _sat_preamble(gtoks)
+    else:
+        fmt = _expect_token(gtoks, _SAT_FORMAT_TOKS).val
+        nvars = _expect_token(gtoks, {'POSINT'}).val
+        return fmt, nvars
+
+def _sat_formula(gtoks, tok, fmt, X):
+    if tok.typ == 'POSINT':
+        assert tok.val <= len(X)
+        return X[tok.val]
+    elif tok.typ == 'NOT':
+        tok = _expect_token(gtoks, {'POSINT', 'LPAREN'})
+        if tok.typ == 'POSINT':
+            assert tok.val <= len(X)
+            return -X[tok.val]
+        else:
+            return _one_formula(gtoks, fmt, X)
+    elif tok.typ == 'LPAREN':
+        return _one_formula(gtoks, fmt, X)
+    # OR/AND/XOR/EQUAL
+    else:
+        op = _SAT_TOK2OP[tok.typ]
+        tok = _expect_token(gtoks, {'LPAREN'})
+        return op(*_zom_formulas(gtoks, fmt, X))
+
+def _one_formula(gtoks, fmt, X):
+    f = _sat_formula(gtoks, next(gtoks), fmt, X)
+    _expect_token(gtoks, {'RPAREN'})
+    return f
+
+def _zom_formulas(gtoks, fmt, X):
+    fs = []
+    types = {'POSINT', 'LPAREN', 'RPAREN'} | _SAT_OP_TOKS[fmt]
+    tok = _expect_token(gtoks, types)
+    while tok.typ != 'RPAREN':
+        fs.append(_sat_formula(gtoks, tok, fmt, X))
+        tok = _expect_token(gtoks, types)
+    return fs
+
+def _expect_token(gtoks, types):
+    tok = next(gtoks)
+    if tok.typ in types:
+        return tok
+    else:
+        fstr = "unexpected token on line {0.line}, col {0.col}: {0.val}"
+        raise SyntaxError(fstr.format(tok))
---
+++ packages/pyeda/pyeda-0.10.0.tar.gz/pyeda-0.10.0/pyeda/nfexpr.py
@@ -0,0 +1,223 @@
+"""
+Normal Form Expressions
+
+Interface Functions:
+    expr2dnf
+    expr2cnf
+    dnf2expr
+    cnf2expr
+
+Interface Classes:
+    ConjNormalForm
+"""
+
+from collections import Counter
+
+from pyeda.common import boolify, cached_property
+from pyeda.boolfunc import Function
+from pyeda.expr import Expression, Or, And
+from pyeda.sat import backtrack, dpll
+
+def expr2dnf(expr):
+    """Convert an expression into a DNF."""
+    if expr.is_dnf():
+        int2lit = dict()
+        for v in expr.inputs:
+            int2lit[-v.gidx] = -v
+            int2lit[v.gidx] = v
+        clauses = {frozenset(lit.gidx for lit in clause.args)
+                   for clause in expr.args}
+        return DisjNormalForm(int2lit, clauses)
+    else:
+        raise TypeError("expression is not a DNF")
+
+def expr2cnf(expr):
+    """Convert an expression into a CNF."""
+    if expr.is_cnf():
+        int2lit = dict()
+        for v in expr.inputs:
+            int2lit[-v.gidx] = -v
+            int2lit[v.gidx] = v
+        clauses = {frozenset(lit.gidx for lit in clause.args)
+                   for clause in expr.args}
+        return ConjNormalForm(int2lit, clauses)
+    else:
+        raise TypeError("expression is not a CNF")
+
+def dnf2expr(dnf):
+    """Convert a DNF into an expression."""
+    return Or(*[
+              And(*[ dnf.int2lit[num] for num in clause ])
+                     for clause in dnf.clauses ])
+
+def cnf2expr(cnf):
+    """Convert a CNF into an expression."""
+    return And(*[
+               Or(*[ cnf.int2lit[num] for num in clause ])
+                     for clause in cnf.clauses ])
+
+
+class NormalForm(Function):
+    """
+    Normal Form Representation
+    """
+    def __new__(cls, int2lit, clauses):
+        if clauses:
+            return super(NormalForm, cls).__new__(cls)
+        else:
+            return cls.IDENTITY
+
+    def __init__(self, int2lit, clauses):
+        self.int2lit = int2lit
+        self.clauses = clauses
+
+    # From Function
+    @cached_property
+    def support(self):
+        return {self.int2lit[abs(num)]
+                for clause in self.clauses for num in clause}
+
+    @cached_property
+    def inputs(self):
+        return sorted(self.support)
+
+    def restrict(self, mapping):
+        idents = set()
+        doms = set()
+        for v, val in mapping.items():
+            low, high = (-v, v) if boolify(val) == self.IDENTITY else (v, -v)
+            idents.add(low.gidx)
+            doms.add(high.gidx)
+
+        new_clauses = set()
+        for clause in self.clauses:
+            if not any(n in clause for n in doms):
+                new_clause = frozenset(n for n in clause if n not in idents)
+                if new_clause:
+                    new_clauses.add(new_clause)
+                else:
+                    return self.DOMINATOR
+
+        return self.__class__(self.int2lit, new_clauses)
+
+
+class DisjNormalForm(NormalForm):
+    """
+    Disjunctive Normal Form
+    """
+
+    IDENTITY = 0
+    DOMINATOR = 1
+
+    def __add__(self, other):
+        return DNF_Or(self, other)
+
+
+class ConjNormalForm(NormalForm):
+    """
+    Conjunctive Normal Form
+    """
+
+    IDENTITY = 1
+    DOMINATOR = 0
+
+    def __mul__(self, other):
+        return CNF_And(self, other)
+
+    def satisfy_one(self, algorithm='dpll'):
+        if algorithm == 'backtrack':
+            return backtrack(self)
+        elif algorithm == 'dpll':
+            return dpll(self)
+        else:
+            raise ValueError("invalid algorithm")
+
+    def bcp(self):
+        """Boolean Constraint Propagation"""
+        return _bcp(self)
+
+    def ple(self):
+        """Pure Literal Elimination
+
+        A literal is *pure* if it only exists with one polarity in the CNF.
+
+        For example:
+
+        >>> from pyeda import *
+        >>> a, b, c = map(var, "abc")
+
+        In this CNF, 'a' is pure: (a + b + c) * (a + -b + -c)
+        >>> cnf = expr2cnf((a + b + c) * (a + -b + -c))
+
+        Eliminating 'a' results in a true statement.
+        >>> cnf.ple()
+        (1, {a: 1})
+
+        In this CNF, '-a' is pure: (-a + b) * (-a + c) * (-b + -c)
+        >>> cnf = expr2cnf((-a + b) * (-a + c) * (-b + -c))
+
+        Eliminating '-a' results in: -b + -c
+        >>> cnf, point = cnf.ple()
+        >>> cnf2expr(cnf), point
+        (b' + c', {a: 0})
+        """
+        cntr = Counter()
+        for clause in self.clauses:
+            cntr.update(clause)
+
+        point = dict()
+        while cntr:
+            num, cnt = cntr.popitem()
+            if -num in cntr:
+                cntr.pop(-num)
+            else:
+                point[self.int2lit[abs(num)]] = int(num > 0)
+        if point:
+            return self.restrict(point), point
+        else:
+            return self, {}
+
+
+def DNF_Or(*args):
+    args = [expr2dnf(arg) if isinstance(arg, Expression) else arg
+            for arg in args]
+
+    int2lit = dict()
+    clauses = set()
+    for arg in args:
+        int2lit.update(arg.int2lit)
+        clauses.update(arg.clauses)
+
+    return DisjNormalForm(int2lit, clauses)
+
+def CNF_And(*args):
+    args = [expr2cnf(arg) if isinstance(arg, Expression) else arg
+            for arg in args]
+
+    int2lit = dict()
+    clauses = set()
+    for arg in args:
+        int2lit.update(arg.int2lit)
+        clauses.update(arg.clauses)
+
+    return ConjNormalForm(int2lit, clauses)
+
+def _bcp(cnf):
+    """Boolean Constraint Propagation"""
+    if cnf in {0, 1}:
+        return cnf, {}
+    else:
+        point = dict()
+        for clause in cnf.clauses:
+            if len(clause) == 1:
+                num = list(clause)[0]
+                if num > 0:
+                    point[cnf.int2lit[num]] = 1
+                else:
+                    point[cnf.int2lit[-num]] = 0
+        if point:
+            _cnf, _point = _bcp(cnf.restrict(point))
+            point.update(_point)
+            return _cnf, point
+        else:
+            return cnf, point
