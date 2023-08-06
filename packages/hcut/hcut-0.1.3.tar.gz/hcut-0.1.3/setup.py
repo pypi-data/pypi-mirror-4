3018d7bb0795ee26ded4d12bd3ab04d77c428502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:04 +0000

    Add package hcut/0.1.3/hcut-0.1.3.tar.gz

---
+++ packages/hcut/hcut-0.1.3.tar.gz/hcut-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from hcut import  __version__
+from setuptools import setup
+import sys
+sys.path.append("./test")
+
+setup(
+    name='hcut',
+    py_modules=['hcut'],
+    scripts=["bin/hcut"],
+    version=__version__,
+    license='BSD',
+    platforms=['POSIX', 'Windows'],
+    description='Cutter for text files with header',
+    author='Toshiyuki Takahashi',
+    author_email='t.toshi.0412 at gmail.com',
+    url='https://github.com/tototoshi/hcut',
+    keywords=['cut', 'tsv'],
+    classifiers = [
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities ",
+        "Topic :: Software Development",
+        ],
+    long_description='Cutter for text files with header',
+    install_requires = ["argparse"],
+    test_suite = 'test_hcut.suite'
+    )
