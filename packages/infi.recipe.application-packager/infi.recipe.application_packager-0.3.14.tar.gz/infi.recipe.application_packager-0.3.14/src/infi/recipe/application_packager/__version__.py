4230ee4786d22aaead622e0e22d053e97877b94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:12 +0000

    Add package infi.recipe.application-packager/0.3.14/infi.recipe.application_packager-0.3.14.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.14.tar.gz/infi.recipe.application_packager-0.3.14/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.14"
+__git_commiter_name__ = "wiggin15"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '956949548c4870afdf227914ea613e177949f4b6'
+__git_head_subject__ = 'fix tests - lxml package version'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
