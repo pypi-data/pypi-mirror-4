6a59b72a79f407c6352e4a996dd99d3243baff5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:45 +0000

    Add package twitter-text-python/1.0.0.1/twitter-text-python-1.0.0.1.tar.gz

---
+++ packages/twitter-text-python/twitter-text-python-1.0.0.1.tar.gz/twitter-text-python-1.0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='twitter-text-python',
+    version='1.0.0.1',
+    description='Twitter Tweet parser and formatter',
+    long_description="Extract @users, #hashtags and URLs from tweets including entity locations, also generate HTML for output. Visit the github site for full instructions.",
+    #open('README.rst').read(),
+    author='Maintained by Ian Ozsvald (originally by Ivo Wetzel)',
+    author_email='ian@ianozsvald.com',
+    url='https://github.com/ianozsvald/twitter-text-python',
+    license='MIT',
+    #py_modules=['ttp', 'tests'],
+    packages=['ttp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    #data_files=[('./', ['README.rst'])],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        #'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
