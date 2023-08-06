1908e864bf58c1ec48871fd40451d4c8ca7589df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package osha.oira/1.2.37/osha.oira-1.2.37.zip

---
+++ packages/osha.oira/osha.oira-1.2.37.zip/osha.oira-1.2.37/src/osha/oira/country.py
@@ -0,0 +1,10 @@
+from five import grok
+from euphorie.client.country import View as EuphorieView
+from osha.oira.interfaces import IOSHAClientSkinLayer
+
+grok.templatedir("templates")
+
+
+class View(EuphorieView):
+    grok.layer(IOSHAClientSkinLayer)
+    grok.template("sessions")
