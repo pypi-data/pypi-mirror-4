c61d98f61a92157a90f024fbc67b284da3203c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:16 +0000

    Add package yadtshell/1.5.2/yadtshell-1.5.2.tar.gz

---
+++ packages/yadtshell/yadtshell-1.5.2.tar.gz/yadtshell-1.5.2/yadtshell/settings.py
@@ -0,0 +1,263 @@
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os.path
+import sys
+import logging
+import time
+import filecmp
+import shutil
+import yaml
+
+import hostexpand.HostExpander
+
+import yadtshell.TerminalController
+from yadtshell.helper import condense_hosts, condense_hosts2, get_user_info
+from yadtshell.loggingtools import create_next_log_file_name_with_command_arguments_as_tag
+import yadtshell.helper
+
+sys.path.append('/etc/yadtshell')
+
+USER_INFO = get_user_info()
+OUTPUT_DIR = os.path.expanduser('~%s/.yadtshell' % USER_INFO['user'])
+
+OUT_DIR = os.path.join(OUTPUT_DIR, 'tmp', os.getcwd().lstrip('/'))
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+
+message_formatter = logging.Formatter('%(levelname)-8s %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(message_formatter)
+root_logger.addHandler(console_handler)
+
+logger = logging.getLogger('settings')
+
+
+try:
+    from loggingconf import *
+except Exception, e:
+    root_logger.debug(e)
+    LOG_DIR_PREFIX = '/var/log/yadtshell'
+
+
+def initialize_broadcast_client():
+    global DummyBroadcaster, broadcasterconf_imported, broadcasterconf, e
+
+    class DummyBroadcaster(object):
+        def addOnSessionOpenHandler(self, *args, **kwargs):
+            pass
+
+        def sendServiceChange(self, data):
+            pass
+
+        def sendFullUpdate(self, data):
+            pass
+
+        def connect(self):
+            pass
+
+        def publish_cmd(self, *args, **kwargs):
+            pass
+
+    broadcasterconf_imported = False
+    try:
+        sys.path.append("/etc/yadtbroadcast-client/")
+        import broadcasterconf
+        sys.path.pop()
+        broadcasterconf_imported = True
+    except Exception, e:
+        logger.warn('no broadcaster config found')
+        logger.warn(e)
+
+
+def load_settings():
+
+    initialize_broadcast_client()
+
+    os.umask(2)
+
+    try:
+        os.makedirs(OUT_DIR)
+    except OSError, e:
+        if e.errno != 17:   # 17: file exists
+            root_logger.critical('cannot write to out dir %s' % OUT_DIR)
+            root_logger.exception(e)
+            sys.exit(1)
+        pass
+
+    global TODAY
+    TODAY = time.strftime('%Y-%m-%d')
+
+    TIME_FORMAT = '%Y-%m-%d--%H-%M-%S'
+    global STARTED_ON
+    STARTED_ON = time.strftime(TIME_FORMAT)
+
+    global term
+    term = yadtshell.TerminalController.TerminalController()
+
+    TARGET_SETTINGS_FILE = 'target'
+    try:
+        settings_file = open(TARGET_SETTINGS_FILE)
+    except IOError:
+        root_logger.critical('cannot find target definition file, aborting')
+        sys.exit(1)
+    global TARGET_SETTINGS
+    TARGET_SETTINGS = yaml.load(settings_file)
+    settings_file.close()
+
+    TARGET_SETTINGS.setdefault('name', os.path.basename(os.getcwd()))
+
+    global ybc
+    if broadcasterconf_imported:
+        ybc = broadcasterconf.create(TARGET_SETTINGS['name'])
+    else:
+        ybc = DummyBroadcaster()
+
+    LOG_DIR = os.path.join(LOG_DIR_PREFIX, TODAY)
+    try:
+        os.makedirs(LOG_DIR)
+    except OSError, e:
+        if e.errno != 17:   # 17: file exists
+            root_logger.critical('cannot write to log dir %s' % LOG_DIR)
+            root_logger.exception(e)
+            sys.exit(1)
+
+    global log_file
+    log_file = create_next_log_file_name_with_command_arguments_as_tag(
+                    log_dir=LOG_DIR,
+                    target_name=TARGET_SETTINGS['name'],
+                    command_start_timestamp=STARTED_ON,
+                    user_name=USER_INFO['user'],
+                    source_host=USER_INFO['yadt_host'].split('.')[0],
+                    command_arguments=sys.argv
+    )
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s', '%Y%m%d-%H%M%S')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(file_handler)
+
+    logger.debug(yaml.dump(USER_INFO, default_flow_style=False))
+    logger.debug('Called "{0}"'.format(' '.join(sys.argv)))
+    logger.debug('output dir is %s' % OUTPUT_DIR)
+
+    he = hostexpand.HostExpander.HostExpander(outputformat=hostexpand.HostExpander.HostExpander.FQDN)
+    TARGET_SETTINGS['original_hosts'] = TARGET_SETTINGS['hosts']
+    TARGET_SETTINGS['hosts'] = he.expand(TARGET_SETTINGS['hosts'])
+
+    OUT_TARGET_FILE = os.path.join(OUT_DIR, TARGET_SETTINGS_FILE)
+    try:
+        changed = not filecmp.cmp(TARGET_SETTINGS_FILE, OUT_TARGET_FILE)
+    except OSError:
+        changed = True
+    if changed:
+        logger.info('target settings have changed since last call, thus cleaning cached data')
+        shutil.rmtree(OUT_DIR)
+        os.makedirs(OUT_DIR)
+        shutil.copy2(TARGET_SETTINGS_FILE, OUT_TARGET_FILE)
+
+    global VIEW_SETTINGS
+    VIEW_SETTINGS = {'info-view': ['matrix', 'color', 'maxcols']}
+    VIEW_SETTINGS_FILE = 'view'
+    try:
+        view_file = open(VIEW_SETTINGS_FILE)
+        VIEW_SETTINGS = yaml.load(view_file)
+        view_file.close()
+
+    except:
+        logger.debug('"view" file not found, falling back to default values: %s' %
+                VIEW_SETTINGS)
+
+    hosts_condensed_file = open(os.path.join(OUT_DIR, 'hosts_condensed'), 'w')
+    print >> hosts_condensed_file, ', '.join(condense_hosts2(condense_hosts(TARGET_SETTINGS['hosts'])))
+    hosts_condensed_file.close()
+
+    def list_selected_hosts():
+        return 'You are working now on %s\n\nas full list: %s\n' % (
+            term.render('${BOLD}') + ', '.join(condense_hosts2(condense_hosts(TARGET_SETTINGS['hosts']))) + term.render('${NORMAL}'),
+            ', '.join(TARGET_SETTINGS['hosts']),
+        )
+
+    identity = TARGET_SETTINGS.get('identity')
+    login = TARGET_SETTINGS.get('login')
+    credentials = ''
+    if identity:
+        credentials += ' -i %(identity)s' % locals()
+    if login:
+        credentials += ' -l %(login)s' % locals()
+    CONNECTIONS_DIR = os.path.join(OUTPUT_DIR, 'connections')
+
+    global SSH_CONTROL_PATH
+    SSH_CONTROL_PATH = os.path.join(CONNECTIONS_DIR, '%h')
+    try:
+        os.makedirs(CONNECTIONS_DIR)
+    except OSError:
+        pass
+    global SSH
+    SSH = 'ssh -o ControlPath=%s -A %s -T -o ConnectTimeout=4 -o BatchMode=yes -o CheckHostIP=no -o StrictHostKeyChecking=no' % (SSH_CONTROL_PATH, credentials)
+
+
+HOST = "host"
+SERVICE = "service"
+ARTEFACT = "artefact"
+CONFIG = 'config'
+TARGET = 'target'
+REVISION = 'revision'
+
+CURRENT = 'current'
+NEXT = 'next'
+PREVIOUS = 'previous'
+
+DIFFERENT_VERSION = "version mismatch"
+OUTDATED_DEPENDENCY = "outdated dependency"
+
+
+START = 'start'
+STOP = 'stop'
+STATUS = 'status'
+INSTALL = 'install'
+PURGE = 'purge'
+UPDATE = 'update'
+BOOTSTRAP = 'bootstrap'
+
+
+INSTALLED = 'installed'
+UNKNOWN = 'unknown'
+DOWN = 'down'
+UP = 'up'
+UPTODATE = 'uptodate'
+UPDATE_NEEDED = 'update_needed'
+SUCCESS = 'success'
+FINISH = 'finish'
+
+
+MISSING = 'missing'
+EMPTY = ''
+
+STATE_DESCRIPTIONS = {
+    0: UP,
+    1: DOWN,
+    2: DOWN,
+    3: DOWN,
+    UP: UP,
+    DOWN: DOWN,
+    UNKNOWN: UNKNOWN,
+}
---
+++ packages/yadtshell/yadtshell-1.5.2.tar.gz/yadtshell-1.5.2/yadtshell/update.py
@@ -0,0 +1,128 @@
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+import yadtshell
+
+
+logger = logging.getLogger('update')
+
+def compare_versions(protocol=None, hosts=None, update_plan_post_handler=None, parallel=None, **kwargs):
+    components = yadtshell.util.restore_current_state()
+    if not update_plan_post_handler:
+        update_plan_post_handler = yadtshell.metalogic.chop_minimal_related_chunks
+
+    all_hosts = set([c for c in components.values() if isinstance(c, yadtshell.components.Host)])
+
+    if hosts:
+        handled_hosts = yadtshell.helper.expand_hosts(hosts)
+        handled_hosts = yadtshell.helper.glob_hosts(components, handled_hosts)
+        logger.debug('User requested update for ' + ', '.join(handled_hosts))
+    else:
+        handled_hosts = [h.uri for h in all_hosts]
+        logger.debug('User requested update for all hosts.')
+
+    # create the base rules for starting all services
+    all_services = set([s.uri for s in components.values() if isinstance(s, yadtshell.components.Service)])
+
+    start_plan = yadtshell.metalogic.metalogic(yadtshell.settings.START, all_services, plan_post_handler=yadtshell.metalogic.identity)
+#    for action in start_plan.actions:
+#        host_uri = components[action.uri].host_uri
+#        action.preconditions.add(yadtshell.actions.TargetState(host_uri, 'state', yadtshell.settings.UPTODATE))
+
+    hosts_with_update = [h for h in all_hosts if h.state == yadtshell.settings.UPDATE_NEEDED]
+    if hosts_with_update:
+        logger.debug('New artefacts found for %s' % ', '.join(h.uri for h in hosts_with_update))
+
+        hosts_with_update = [h for h in hosts_with_update if h.uri in handled_hosts]
+        logger.debug('Handling hosts with new artefacts: %s' % ', '.join(h.uri for h in hosts_with_update))
+    else:
+        logger.info('No hosts with pending updates.')
+
+    next_artefacts = set([artefact.uri
+        for artefact in components.values()
+            if artefact.type == yadtshell.settings.ARTEFACT
+                and artefact.revision == yadtshell.settings.NEXT
+                and artefact.host_uri in handled_hosts
+    ])
+
+    if not next_artefacts:
+        yadtshell.util.dump_action_plan('update', start_plan)
+        return 'update'
+
+    current_artefacts = [components.get(yadtshell.uri.change_version(next_artefact, 'current'))
+        for next_artefact in next_artefacts
+    ]
+    current_artefacts = set([current.uri for current in current_artefacts if current])
+
+    logger.debug('next_artefacts: ' + ', '.join(next_artefacts))
+    logger.debug('current_artefacts: ' + ', '.join(current_artefacts))
+
+    diff = next_artefacts | current_artefacts
+    logger.debug('diff: ' + ', '.join(diff))
+
+    if not diff:
+        yadtshell.util.dump_action_plan('update', start_plan)
+        return 'update'
+
+
+    stop_plan = yadtshell.metalogic.metalogic(yadtshell.settings.STOP, diff, plan_post_handler=yadtshell.metalogic.identity)
+    stopped_services = set()
+    for action in stop_plan.actions:
+        stopped_services.add(action.uri)
+        #host_uri = components[action.uri].host_uri
+        #action.preconditions.add(yadtshell.actions.TargetState(host_uri, 'state', yadtshell.settings.UPDATE_NEEDED))
+
+    for action in start_plan.actions:
+        if action.uri in stopped_services:
+            host_uri = components[action.uri].host_uri
+            action.preconditions.add(yadtshell.actions.TargetState(host_uri, 'state', yadtshell.settings.UPTODATE))
+
+    update_actions = set()
+    for host in hosts_with_update:
+        action = yadtshell.actions.Action(yadtshell.settings.UPDATE, host.uri, 'state', yadtshell.settings.UPTODATE)
+        #for needs_host in [s for s in host.needed_by if s.startswith('service://')]:
+        for needs_host in [components.get(s) for s in stopped_services]:
+            if needs_host.host_uri != host.uri:
+                continue
+            action.preconditions.add(yadtshell.actions.TargetState(needs_host, 'state', yadtshell.settings.DOWN))
+        update_actions.add(action)
+
+    all_actions = set(start_plan.actions) | set(stop_plan.actions) | update_actions
+    all_plan = yadtshell.actions.ActionPlan('all', all_actions)
+    all_plan = yadtshell.metalogic.chop_minimal_related_chunks(all_plan)
+
+    update_chunks = set()
+    for chunk in all_plan.actions:
+        all_chunk_cmds = set(a.cmd for a in chunk.actions)
+        if yadtshell.settings.UPDATE in all_chunk_cmds:
+            update_chunks.add(chunk)
+            continue
+
+    prestart_chunks = set()
+    for chunk in all_plan.actions:
+        if chunk in update_chunks:
+            continue
+        prestart_chunks.add(chunk)
+
+    plan = yadtshell.actions.ActionPlan('update', [yadtshell.actions.ActionPlan('prestart', prestart_chunks), yadtshell.actions.ActionPlan('stopupdatestart', update_chunks)], nr_workers=1)
+    plan = yadtshell.metalogic.apply_instructions(plan, parallel)
+    yadtshell.util.dump_action_plan('update', plan)
+    return 'update'
+
