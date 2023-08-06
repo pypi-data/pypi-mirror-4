2b0c635a51cf8072d48c44b62a1c5db0839c5d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:11 +0000

    Add package pyA13/0.1.1/pyA13-0.1.1.tar.gz

---
+++ packages/pyA13/pyA13-0.1.1.tar.gz/pyA13-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+classifiers = [ 'Development Status :: 1 - Planning',
+                'Operating System :: POSIX :: LINUX',
+                'Intended Audience :: Developers',
+                'Programming Landuage :: Python :: 2.6',
+                'Programming Landuage :: Python :: 2.7',
+                'Programming Landuage :: Python :: 3',
+                'Topic :: Software Development',
+                'Topic :: Home Automation',
+                'Topic :: System',
+                'Topic :: Hardware']
+                
+
+setup(
+    name            = 'pyA13',
+    version         = '0.1.1',
+    author          = 'Stefan Mavrodiev',
+    author_email    = 'support@olimex.com',
+    packages        = ['pyA13', 'pyA13.test'],
+    url             = 'https://www.olimex.com/',
+    license         = 'LICENSE.txt',
+    description     = 'Control GPIOs on OLinuXino-A13.',
+    long_description= open('README.txt').read())
