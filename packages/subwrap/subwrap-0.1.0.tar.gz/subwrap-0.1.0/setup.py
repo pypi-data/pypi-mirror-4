bc563968e1293105687e56b5372ae770296057d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:35 +0000

    Add package subwrap/0.1.0/subwrap-0.1.0.tar.gz

---
+++ packages/subwrap/subwrap-0.1.0.tar.gz/subwrap-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = '0.1.0'
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(name='subwrap',
+    version=VERSION,
+    description="subwrap - A simple wrapper for subprocess",
+    long_description=LONG_DESCRIPTION,
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url="https://github.com/ravenac95/subwrap",
+    license='MIT',
+    platforms='Unix',
+    py_modules=['subwrap'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
