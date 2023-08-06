43bafc871b99fe701fac3cee0b70a576c84c7517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:06 +0000

    Add package SRPAstro/3.10.9/SRPAstro-3.10.9.tar.gz

---
+++ packages/SRPAstro/SRPAstro-3.10.9.tar.gz/SRPAstro-3.10.9/SRP/SRPStatistics/__init__.py
@@ -0,0 +1,29 @@
+""" 
+
+Context : SRP
+Module  : Statistics
+Version : 1.4.0
+Author  : Stefano Covino
+Date    : 04/04/2013
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+
+Usage   : to be imported
+
+Remarks :
+
+History : (28/10/2010) First version.
+        : (30/04/2011) ChiSqIncrement.
+        : (19/02/2012) ScoreatPercentile.
+        : (27/08/2012) Faster sigma clipping for frames.
+        : (04/03/2013) MAD.
+        : (04/04/2013) XCorr_1D.
+"""
+
+
+
+__all__ = ['AsymmetricGaussianDistrib', 'AverIterSigmaClipp', 'AverSigmaClippFrame', 
+           'AverSigmaClippFrameFast', 'ChSqIncrement', 'CoordDistanceMinimization', 'GenFitPars',
+           'GenGaussSet', 'MAD', 'ScoreatPercentile', 'XCorr_1D', 'WeightedMean', 'WeightedMeanFrame']
+
