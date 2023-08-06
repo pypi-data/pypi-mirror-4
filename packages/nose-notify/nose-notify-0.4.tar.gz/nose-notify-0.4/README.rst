5cadffe59742ebae83bf5eef6aa3d59635aec9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:04 +0000

    Add package nose-notify/0.4/nose-notify-0.4.tar.gz

---
+++ packages/nose-notify/nose-notify-0.4.tar.gz/nose-notify-0.4/README.rst
@@ -0,0 +1,25 @@
+===========
+nose-notify
+===========
+
+Notify-OSD notifications for Ubuntu systems on testsuite start and completion.
+
+Usage
+-----
+
+Run nosetests with the ``--with-notify`` or set the environment variable
+``NOSE_WITH_NOTIFYPLUGIN`` flag. Enjoy.
+
+Options
+~~~~~~~
+
+Passing the ``--no-start-message`` flag skips the notification at the beginning
+of your test run. This is useful in case your test suite passes really quickly.
+Which it should.
+
+Thanks
+------
+
+Thanks to Victor Ng and `nose-growl`_ for the inspiration.
+
+.. _nose-growl: http://bitbucket.org/crankycoder/nosegrowl
---
+++ packages/nose-notify/nose-notify-0.4.tar.gz/nose-notify-0.4/nosenotify/adapters.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+nosenotify.adapters
+~~~~~~~~~~~~~~~~~~~
+
+Aternative pynotify implementation falling back to ``notify-send``.
+
+:copyright: 2013, Diogo Baeder
+:license: BSD, see doc/LICENSE for more details.
+"""
+
+import subprocess
+import os
+
+
+IMAGES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'images'))
+
+
+def get_icon(icon_type):
+    if icon_type == 'dialog-info':
+        dialog_file = 'dialog-ok.png'
+    else:
+        dialog_file = 'dialog-cancel.png'
+
+    return os.path.join(IMAGES_DIR, dialog_file)
+
+
+def init(message):
+    pass
+
+
+class Notification(object):
+    def __init__(self, title, body, icon_type=None):
+        self.title = title
+        self.body = body
+        self.icon_type = icon_type
+
+    def show(self):
+        command_header = self.__get_command_header()
+        subprocess.call(command_header + [self.title, self.body])
+
+    def __get_command_header(self):
+        command_header = ['notify-send']
+
+        if self.icon_type is not None:
+            dialog_file = get_icon(self.icon_type)
+            command_header.append('--icon="{0}"'.format(dialog_file))
+
+        return command_header
