2c42fc6cdd6970bc66c1f7c94607d11d89a3cb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package FSGDeploy/0.1.4/FSGDeploy-0.1.4.zip

---
+++ packages/FSGDeploy/FSGDeploy-0.1.4.zip/FSGDeploy-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='FSGDeploy',
+    version='0.1.4',
+    author='FSG',
+    author_email='dweiss@frontierstrategygroup.com',
+    packages=['FSGDeploy'],
+    url='http://pypi.python.org/pypi/FSGDeploy/',
+    license='LICENSE.txt',
+    description='Useful FSGDeploy stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+    entry_points = {
+        'console_scripts':
+            ['deploy = FSGDeploy.deploy:main'],
+    }
+)
