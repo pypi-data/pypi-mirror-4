8d83a80759b727dc7bc55642c7af64051a97293c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package humbledb/3.2.0/humbledb-3.2.0.tar.gz

---
+++ packages/humbledb/humbledb-3.2.0.tar.gz/humbledb-3.2.0/humbledb/__init__.py
@@ -0,0 +1,55 @@
+# flake8: noqa
+"""
+LICENSE
+=======
+
+Copyright 2012 Jacob Alheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+__version__ = '3.2.0'
+
+
+__all__ = [
+        # HumbleDB provides these
+        'Index',
+        'Mongo',
+        'Document',
+        'Embed',
+        'report',
+        # Pymongo provides the rest
+        'errors',
+        'ASC',
+        'DESC',
+        ]
+
+# Shortcuts to pymongo exceptions
+from pymongo import errors
+
+# Shortcuts to pymongo index directions
+import pymongo
+DESC = pymongo.DESCENDING
+ASC = pymongo.ASCENDING
+del pymongo  # Clean up the namespace
+
+# Import shortcuts to HumbleDB document basics
+from .index import Index
+from .mongo import Mongo
+from .document import Document, Embed
+# These need to be imported last or it causes with circular imports
+from . import array
+from . import report
+# Make pyflakes happy
+array = array
+report = report
