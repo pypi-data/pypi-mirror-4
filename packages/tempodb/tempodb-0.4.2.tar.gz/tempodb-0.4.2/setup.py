ede410f5731fc97368534d0c4204aceabd4d3180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:02 +0000

    Add package tempodb/0.4.2/tempodb-0.4.2.tar.gz

---
+++ packages/tempodb/tempodb-0.4.2.tar.gz/tempodb-0.4.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+tempodb/setup.py
+
+Copyright (c) 2012 TempoDB Inc. All rights reserved.
+"""
+
+from setuptools import setup
+
+
+install_requires = [
+    'python-dateutil==1.5',
+    'requests',
+    'simplejson',
+]
+
+tests_require = [
+    'mock',
+    'unittest2',
+]
+
+setup(
+    name="tempodb",
+    version="0.4.2",
+    author="TempoDB Inc",
+    author_email="dev@tempo-db.com",
+    url="http://github.com/getsentry/tempodb-python/",
+    description="A client for the TempoDB API",
+    packages=["tempodb"],
+    long_description="A client for the TempoDB API.",
+    dependency_links=[
+        'http://github.com/tempodb/requests/tarball/development#egg=requests-0.11.1ssl'
+    ],
+    setup_requires=['nose>=1.0'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+)
