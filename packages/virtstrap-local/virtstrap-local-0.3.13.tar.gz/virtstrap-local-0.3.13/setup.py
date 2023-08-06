c3af63627557fa882569cb8e2252543147fc84e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:10 +0000

    Add package virtstrap-local/0.3.13/virtstrap-local-0.3.13.tar.gz

---
+++ packages/virtstrap-local/virtstrap-local-0.3.13.tar.gz/virtstrap-local-0.3.13/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.3.13'
+
+setup(name='virtstrap-local',
+    version=VERSION,
+    description="virtstrap's project local commands",
+    long_description="virtstrap-local is not meant to be installed manually",
+    classifiers=[],
+    keywords='',
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'virtstrap-core',
+    ],
+    entry_points={
+        'console_scripts': [
+            'virtstrap-local = virtstrap_local.runner:main'
+        ]
+    },
+)
