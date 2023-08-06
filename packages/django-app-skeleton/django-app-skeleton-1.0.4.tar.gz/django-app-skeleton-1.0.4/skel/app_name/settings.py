3603f12f6a38019474d8411005eaaba17488b0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:23 +0000

    Add package django-app-skeleton/1.0.4/django-app-skeleton-1.0.4.tar.gz

---
+++ packages/django-app-skeleton/django-app-skeleton-1.0.4.tar.gz/django-app-skeleton-1.0.4/skel/app_name/settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf import settings as site_settings
+
+
+DEFAULT_SETTINGS = {
+
+}
+
+USER_SETTINGS = DEFAULT_SETTINGS.copy()
+USER_SETTINGS.update(getattr(site_settings, '{{app_name}}_SETTINGS', {}))
+
+globals().update(USER_SETTINGS)
---
+++ packages/django-app-skeleton/django-app-skeleton-1.0.4.tar.gz/django-app-skeleton-1.0.4/skel/doc_src/index.rst
@@ -0,0 +1,19 @@
+============================
+{{pkg_name}} v |version|
+============================
+
+About
+=====
+
+
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+   :glob:
+
+   installation
+   getting_started
+   reference/index
