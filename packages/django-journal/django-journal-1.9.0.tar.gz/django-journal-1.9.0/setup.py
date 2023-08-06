24a15f2c5b287fb1f3ce686ff5f0909b52a05efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:55 +0000

    Add package django-journal/1.9.0/django-journal-1.9.0.tar.gz

---
+++ packages/django-journal/django-journal-1.9.0.tar.gz/django-journal-1.9.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
+
+setup(name='django-journal',
+        version='1.9.0',
+        license='AGPLv3',
+        description='Keep a structured -- i.e. not just log strings -- journal'
+                    ' of events in your applications',
+        url='http://dev.entrouvert.org/projects/django-journal/',
+        download_url='http://repos.entrouvert.org/django-journal.git/',
+        author="Entr'ouvert",
+        author_email="info@entrouvert.com",
+        packages=find_packages(os.path.dirname(__file__) or '.'),
+        package_data={'django_journal':
+            ['locale/*/LC_MESSAGES/*.po',
+             'locale/*/LC_MESSAGES/*.mo',
+             'static/journal/css/*.css',
+             'static/journal/images/*.png']},
+        install_requires=[
+            'django >= 1.4.2, < 1.5',
+        ],
+)
