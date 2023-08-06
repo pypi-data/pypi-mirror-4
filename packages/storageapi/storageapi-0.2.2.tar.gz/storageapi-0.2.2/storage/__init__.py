67189bd3682ef1d30f11e309c9a2876971de2386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package storageapi/0.2.2/storageapi-0.2.2.tar.gz

---
+++ packages/storageapi/storageapi-0.2.2.tar.gz/storageapi-0.2.2/storage/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# This file is part of StorageAPI, by Luke Granger-Brown
+# and is licensed under the MIT license, under the terms listed within
+# LICENSE which is included with the source of this package
+
+from .v1 import ApiV1
+from .auth import *
