411389f1faa09bc6a016f7eede541b8c67bee669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:14 +0000

    Add package django-dispose/1.1/django-dispose-1.1.tar.gz

---
+++ packages/django-dispose/django-dispose-1.1.tar.gz/django-dispose-1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-dispose',
+    description='Disposes of media files that are no longer referenced in the database.',
+    version='1.1',
+    author='Daniel Hawkes',
+    author_email='dan@danhawkes.co.uk',
+    license='MIT',
+    url='https://bitbucket.org/danhawkes/django-dispose',
+    packages=[
+        'django_dispose',
+        'django_dispose.management',
+        'django_dispose.management.commands'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Installation/Setup'
+    ]
+)
