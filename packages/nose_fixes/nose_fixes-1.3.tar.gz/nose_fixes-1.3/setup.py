d7a9c6531217ee6f5180f8299e6e53c147c3d016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:53 +0000

    Add package nose_fixes/1.3/nose_fixes-1.3.tar.gz

---
+++ packages/nose_fixes/nose_fixes-1.3.tar.gz/nose_fixes-1.3/setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2011-2012 Simplistix Ltd
+# See license.txt for license details.
+
+import os
+from setuptools import setup, find_packages
+
+name = 'nose_fixes'
+
+setup(
+    name=name,
+    version='1.3',
+    author='Chris Withers',
+    author_email='chris@simplistix.co.uk',
+    license='MIT',
+    description="A plugin to make nose behave better.",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'docs','description.txt')).read(),
+    url='http://packages.python.org/'+name,
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    ],    
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'nose',
+        'setuptools', # so nose's EntryPointPluginManager gets used
+        ],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'nose_fixes = nose_fixes.plugin:Plugin'
+            ]
+        },
+    )
