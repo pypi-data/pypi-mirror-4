e9ecb7f430decd00771d87aca8d47ce671d9a012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:31 +0000

    Add package errormator-client/0.5.7/errormator_client-0.5.7.tar.gz

---
+++ packages/errormator-client/errormator_client-0.5.7.tar.gz/errormator_client-0.5.7/errormator_client/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.5.7'
+__protocol_version__ = '0.3.5'
+
+class ErrormatorException(Exception):
+
+    @property
+    def message(self):
+        return self._message
+
+    @message.setter
+    def message_set(self, message):
+        self._message = message
+
+    def __str__(self):
+        return repr(self.args)
+
+from errormator_client.client import make_errormator_middleware
