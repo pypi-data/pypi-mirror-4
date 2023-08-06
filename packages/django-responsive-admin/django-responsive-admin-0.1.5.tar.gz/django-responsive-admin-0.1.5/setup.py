decd81828f3381bb87725ece04d2021255c624f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:16 +0000

    Add package django-responsive-admin/0.1.5/django-responsive-admin-0.1.5.tar.gz

---
+++ packages/django-responsive-admin/django-responsive-admin-0.1.5.tar.gz/django-responsive-admin-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-responsive-admin',
+    version='0.1.5',
+    description='A responsive admin for Django using Kube CSS',
+    author='Pierre Drescher',
+    author_email='pierre.drescher@gmail.com',
+    url='https://github.com/pdr/django-responsive-admin',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
