a1a47a600244f1ff69ed6cb45bcedb892e251838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package jhpy/1.0.5/jhpy-1.0.5.zip

---
+++ packages/jhpy/jhpy-1.0.5.zip/jhpy-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from distutils.command.build_py import build_py as _build_py
+from distutils.core import setup
+
+class build_py(_build_py):
+	"""Command-line tool for JHPY."""
+
+setup(name = "jhpy",
+      version = "1.0.5",
+      url = "https://github.com/djwatt5/jhpy-clt",
+      description = "Command-line manager and template builder for JHPY.",
+      author = "Daniel Watson",
+      author_email = "watsondaniel6@gmail.com",
+      py_modules = ["jhpy", "jhpy_clt"],
+      scripts = ["jhpy"],
+      classifiers = [
+      	  'Environment :: Console',
+      	  'Environment :: Web Environment',
+      	  'Intended Audience :: Developers',
+      	  'Programming Language :: Python',
+      	  'Topic :: Software Development',
+      	  ]
+      )
