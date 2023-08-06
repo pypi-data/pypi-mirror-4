fe81a8844985d56c709f8d6e5a3c917cc9af0896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package wallaby-backend-couchdb/0.1.27/wallaby-backend-couchdb-0.1.27.tar.gz

---
+++ packages/wallaby-backend-couchdb/wallaby-backend-couchdb-0.1.27.tar.gz/wallaby-backend-couchdb-0.1.27/README.md
@@ -0,0 +1,5 @@
+Wallaby backend for CouchDB
+===========================
+
+This package provides an asynchronous python interface to CouchDB (using twisted).
+
---
+++ packages/wallaby-backend-couchdb/wallaby-backend-couchdb-0.1.27.tar.gz/wallaby-backend-couchdb-0.1.27/setup.py
@@ -0,0 +1,30 @@
+# Copyright (c) by it's authors. 
+# Some rights reserved. See LICENSE, AUTHORS.
+
+from setuptools import setup, find_packages
+import os
+
+setup(name='wallaby-backend-couchdb',
+      version='0.1.27',
+      url='https://github.com/FreshXOpenSource/wallaby-backend-couchdb',
+      author='FreshX GbR',
+      author_email='wallaby@freshx.de',
+      license='BSD',
+      description='Wallaby backend for CouchDB.',
+      long_description=open('README.md').read(),
+      package_data={'': ['LICENSE', 'AUTHORS', 'README.md']},
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+      ],
+      packages=find_packages('.'),
+      install_requires=['wallaby-backend-http']
+  )
