d5e5e664819c5fe20e20f914646ec8996279bc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:21 +0000

    Add package gigya-server-lib/0.1.5/gigya-server-lib-0.1.5.tar.gz

---
+++ packages/gigya-server-lib/gigya-server-lib-0.1.5.tar.gz/gigya-server-lib-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='gigya-server-lib',
+    version="0.1.5",
+    author="Miguel Pilar",
+    author_email='miguel@miguelpilar.com',
+    packages=['gslib'],
+    url='http://pypi.python.org/pypi/GigyaServerLib/',
+    license=open('LICENSE.txt').read(),
+    description='Gigya Server Library (gslib) is a python adaptation of the Gigya Server SDK',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    install_requires=[
+        "requests>=1.1.0,<2.0"
+    ],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP'
+    ),
+)
