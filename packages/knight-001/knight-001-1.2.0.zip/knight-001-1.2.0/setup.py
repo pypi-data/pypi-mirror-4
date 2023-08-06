35c08fc7ae1a31209c0f15fcc91b542974fa37be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:05 +0000

    Add package knight-001/1.2.0/knight-001-1.2.0.zip

---
+++ packages/knight-001/knight-001-1.2.0.zip/knight-001-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name          =    'knight-001',
+    version       =    '1.2.0',
+    py_modules    =    ['nester'],
+    author        =    'Knight2013',
+    author_email  =    'Knight2013@hotmail.com',
+    url           =    'http://www.knight.com',
+    discription   =    'A simple printer of nested lists',
+    )
+    
