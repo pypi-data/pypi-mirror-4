4fafc4a05ea0928758caa132fb1f4a16650800f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:03 +0000

    Add package mstranslator/0.2/mstranslator-0.2.tar.gz

---
+++ packages/mstranslator/mstranslator-0.2.tar.gz/mstranslator-0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='mstranslator',
+    version='0.2',
+    license='BSD',
+    description='Microsoft Translator Api wrapper',
+    author='Michael Elovskikh',
+    author_email='wronglink@gmail.com',
+    py_modules=['mstranslator'],
+    install_requires=[
+        'requests',
+    ],
+    tests_require=[
+        'tox'
+    ],
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ),
+)
