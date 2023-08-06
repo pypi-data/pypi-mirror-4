6eadfd55924d1cbb6ebb222f9d0df4ef6543e755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:17 +0000

    Add package errordite/0.3/errordite-0.3.zip

---
+++ packages/errordite/errordite-0.3.zip/errordite-0.3/errordite/__init__.py
@@ -0,0 +1,13 @@
+"""
+Custom log handler for posting errors to errordite (www.errordite.com).
+
+Dependencies: Requests (http://docs.python-requests.org)
+"""
+__title__ = 'errordite'
+__version__ = '0.3'
+__author__ = 'Hugo Rodger-Brown'
+__license__ = 'Simplified BSD License'
+__copyright__ = 'Copyright 2013 Hugo Rodger-Brown'
+__description__ = 'Errordite exception logging.'
+
+from handlers import ErrorditeHandler
