cf759c59498dc774b88df3f9b1ab78c3c2422e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:40 +0000

    Add package py-lorem/1.1/py-lorem-1.1.tar.gz

---
+++ packages/py-lorem/py-lorem-1.1.tar.gz/py-lorem-1.1/loremipsum/action.py
@@ -0,0 +1,43 @@
+import os
+from random import choice
+
+__author__ = 'nubela'
+__here__ = os.path.dirname(os.path.abspath(__file__))
+__lorem_file__ = os.path.join(__here__, "loremipsum.txt")
+PROSE = """
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\n
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.\n
+"""
+
+
+def sentence(max_char=None):
+    """
+    Gets a random sentence from a lorem ipsum paragraph.
+    """
+    para_text = paragraph()
+    sent = filter(lambda x: x != "", para_text.split("."))
+    s = choice(sent)
+    return __strip(s[:max_char]) + "."
+
+
+def paragraph(max_char=None):
+    """
+    Gets a random paragraph from a lorem ipsum prose.
+    """
+    para_text = choice(__paragraphs())
+    return __strip(para_text[:max_char])
+
+
+def __paragraphs():
+    """
+    Get a list of paragraphs from the lorem file
+    """
+    return filter(lambda x: x != "", PROSE.split("\n"))
+
+
+def __strip(str):
+    """
+    Removes extra whitespace
+    """
+    return ' '.join(str.split())
