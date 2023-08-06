a8eb6aa6901e9018bf76457ed805d1d709cc6e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package pimp/0.3dev/pimp-0.3dev.tar.gz

---
+++ packages/pimp/pimp-0.3dev.tar.gz/pimp-0.3dev/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pimp',
+    version='0.3dev',
+    description='pimp installs magic packages. Install rpm packages from PyPi. No refunds.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/pimp',
+    license='MIT',
+    install_requires=['shutilwhich', 'tempdir'],
+    packages=find_packages(exclude=['test']),
+    entry_points={
+        'console_scripts': [
+            'pimp = pimp:main',
+        ],
+    }
+)
