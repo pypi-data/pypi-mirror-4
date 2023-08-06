b7579ae7fff943199007297ded8ab5fa0bca3ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:18 +0000

    Add package Ashser_spinner/1.0.0/Ashser_spinner-1.0.0.zip

---
+++ packages/Ashser_spinner/Ashser_spinner-1.0.0.zip/Ashser_spinner-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name			=	'Ashser_spinner',
+	version			=	'1.0.0',
+	py_modules		=	['Ashser_spinner'],
+	author			=	'Ashser',
+	author_email	=	'night_eagle21@hotmail.com',
+	description		=	'A little program to make a spinning cursor',
+	)
