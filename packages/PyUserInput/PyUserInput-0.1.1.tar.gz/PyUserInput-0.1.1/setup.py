f8374f1585e775b4da3aba726b669ca87677df58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package PyUserInput/0.1.1/PyUserInput-0.1.1.tar.gz

---
+++ packages/PyUserInput/PyUserInput-0.1.1.tar.gz/PyUserInput-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+def long_description():
+    with open('README.md', 'r') as readme:
+        readme_text = readme.read()
+    return(readme_text)
+
+setup(name='PyUserInput',
+      version='0.1.1',
+      description='A simple, cross-platform module for mouse and keyboard control',
+      long_description=long_description(),
+      author='Paul Barton',
+      #Original author of PyMouse: Pepijn de Vos
+      author_email='pablo.barton@gmail.com',
+      url='https://github.com/SavinaRoja/PyUserInput',
+      packages = ['pykeyboard', 'pymouse'],
+      license='http://www.gnu.org/licenses/gpl-3.0.html',
+      )
