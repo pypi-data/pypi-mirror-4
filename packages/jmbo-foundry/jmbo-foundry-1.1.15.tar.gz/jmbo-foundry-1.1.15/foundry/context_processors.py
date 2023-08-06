171c152d031cfff00ad4b48ae6f76b390562b4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package jmbo-foundry/1.1.15/jmbo-foundry-1.1.15.tar.gz

---
+++ packages/jmbo-foundry/jmbo-foundry-1.1.15.tar.gz/jmbo-foundry-1.1.15/foundry/context_processors.py
@@ -0,0 +1,13 @@
+from django.contrib.sites.models import get_current_site
+from django.conf import settings
+
+from preferences import preferences
+
+def foundry(request):
+    return {
+        'FOUNDRY': settings.FOUNDRY,
+        'LAYER_PATH': settings.FOUNDRY['layers'][0] + '/',
+        'CURRENT_SITE': get_current_site(request),
+        'ANALYTICS_TAGS': preferences.GeneralPreferences.analytics_tags,
+        'SITE_DESCRIPTION': preferences.GeneralPreferences.site_description,
+    }
