af42c2fcc1a761685f0635692da59f575e1f2676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:35 +0000

    Add package django-user-profile/1.0.1/django-user-profile-1.0.1.tar.gz

---
+++ packages/django-user-profile/django-user-profile-1.0.1.tar.gz/django-user-profile-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+
+from distutils.core import setup
+
+version = '1.0.1'
+
+setup(
+    name = "django-user-profile",
+    version = version,
+    packages = ['user_profile'],
+    author = "vahid chakoshy",
+    author_email = "vchakoshy@gmail.com",
+    description = "user profile for pinterest apllication like in Django",
+    url = "http://www.wisgoon.com/",
+    py_modules = ["user_rofile"],
+    #package_dir = {},
+    package_dir = {'user_rofile': 'user_rofile'},
+    include_package_data = True,
+    zip_safe=False,
+)
