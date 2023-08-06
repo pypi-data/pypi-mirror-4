47a499680d290b8dcf0c1db75fee145f0de1c687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:37 +0000

    Add package infi.pyutils/0.0.30/infi.pyutils-0.0.30.tar.gz

---
+++ packages/infi.pyutils/infi.pyutils-0.0.30.tar.gz/infi.pyutils-0.0.30/setup.py
@@ -0,0 +1,28 @@
+import os
+import itertools
+import platform
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "infi", "pyutils", "__version__.py")) as version_file:
+    exec(version_file.read())
+
+install_requires = ["mock", "six"]
+if platform.python_version() < '2.7':
+    install_requires.append('unittest2')
+    install_requires.append('ordereddict')
+
+setup(name="infi.pyutils",
+      classifiers = [
+          "Programming Language :: Python :: 2.6",
+          ],
+      description="Misc. pure-python utilities",
+      license="BSD",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      #url="your.url.here",
+      version=__version__,
+      packages=find_packages(exclude=["tests"]),
+      namespace_packages=["infi"],
+      install_requires=install_requires,
+      scripts=[],
+      )
