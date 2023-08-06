10b6de410b07b9b68151041e0c5c122816c7df5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:36 +0000

    Add package OpenGroupware/0.1.49rc33/OpenGroupware-0.1.49rc33-py2.6.egg

---
+++ packages/OpenGroupware/OpenGroupware-0.1.49rc33-py2.6.egg/coils/logic/workflow/keymap.py
@@ -0,0 +1,15 @@
+COILS_ROUTE_KEYMAP = {
+    'objectid':             ['object_id', 'int', 0],
+    'object_id':            ['object_id', 'int', 0],
+    }
+
+COILS_PROCESS_KEYMAP = {
+    'objectid':             ['object_id', 'int', 0],
+    'object_id':            ['object_id', 'int', 0],
+    'routeid':              ['route_id', 'int'],
+    'route_id':             ['route_id', 'int'],
+    'routeobjectid':        ['route_id', 'int'],
+    'data':                 None,
+    'filename':             None,
+    'state':                None
+    }
