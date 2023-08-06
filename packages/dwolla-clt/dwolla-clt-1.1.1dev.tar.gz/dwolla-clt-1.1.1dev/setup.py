f578d796187b7b7b2b2f5f4a5951d369f8b3e42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:05 +0000

    Add package dwolla-clt/1.1.1dev/dwolla-clt-1.1.1dev.tar.gz

---
+++ packages/dwolla-clt/dwolla-clt-1.1.1dev.tar.gz/dwolla-clt-1.1.1dev/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='dwolla-clt',
+    license='MIT',
+    version='1.1.1dev',
+    install_requires=[
+        'dwolla>=1.4.8',
+        'argparse>=1.2.1'
+    ],
+    scripts=['bin/dwolla-clt'],
+    
+    description='Dwolla Command Line Tools',
+    long_description=open('README.rst').read(),
+    
+    author='Michael Schonfeld',
+    author_email='michael@dwolla.com',
+    url='https://github.com/dwolla/dwolla-clt',
+    
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
