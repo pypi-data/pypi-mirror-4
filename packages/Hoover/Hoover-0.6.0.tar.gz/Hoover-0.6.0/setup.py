aecad27fc59da0c90537cb1854d4eaddb5c7c99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:54 +0000

    Add package Hoover/0.6.0/Hoover-0.6.0.tar.gz

---
+++ packages/Hoover/Hoover-0.6.0.tar.gz/Hoover-0.6.0/setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='Hoover',
+      version='0.6.0',
+      description="Library for logging to Loggly from within Python webapps",
+      author="Mike Blume",
+      author_email="mike@loggly.com",
+      url="http://www.github.com/loggly/hoover",
+      packages=['hoover'],
+      install_requires=['requests'],
+)
