eaf07e14f0ba2ffa365564ad90f1f45d1447e26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:45 +0000

    Add package js.bootstrap_datepicker/2013.03.12-1/js.bootstrap_datepicker-2013.03.12-1.tar.gz

---
+++ packages/js.bootstrap_datepicker/js.bootstrap_datepicker-2013.03.12-1.tar.gz/js.bootstrap_datepicker-2013.03.12-1/README.txt
@@ -0,0 +1,16 @@
+js.bootstrap_datepicker
+=======================
+
+.. contents::
+
+Introduction
+------------
+
+This library packages `Datepicker for Bootstrap`_ for `fanstatic`_.
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`Datepicker for Bootstrap`: http://www.eyecon.ro/bootstrap-datepicker/
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original resources (shipped in the ``resources``
+directory in ``js.bootstrap_datepicker``) are published to some URL.
---
+++ packages/js.bootstrap_datepicker/js.bootstrap_datepicker-2013.03.12-1.tar.gz/js.bootstrap_datepicker-2013.03.12-1/setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup, find_packages
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '2013.03.12-1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = '\n'.join(
+    [read('README.txt'),
+     "\n",
+     read('CHANGES.txt'),
+     "\n",
+     read('js', 'bootstrap_datepicker', 'test_bootstrap-datepicker.txt'),
+     ])
+
+setup(
+    name='js.bootstrap_datepicker',
+    version=version,
+    description="Fanstatic packaging of Datepicker for Bootstrap (bootstrap-datepicker.js)",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Nikolai Sevostjanov',
+    author_email='nikolai.sevostjanov@gmail.com',
+    url='https://bitbucket.org/s_nikolai/js.bootstrap_datepicker',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+        'js.bootstrap',
+    ],
+    entry_points={
+        'fanstatic.libraries': [
+            'bootstrap_datepicker = js.bootstrap_datepicker:library',
+            ],
+        },
+    )
