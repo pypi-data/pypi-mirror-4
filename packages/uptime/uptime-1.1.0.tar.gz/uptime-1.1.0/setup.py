88bbac2b31e60fc8855c2704b5a0f925786b53fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package uptime/1.1.0/uptime-1.1.0.tar.gz

---
+++ packages/uptime/uptime-1.1.0.tar.gz/uptime-1.1.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='uptime',
+    version='1.1.0',
+    description='Cross-platform uptime library',
+    long_description='''\
+This module provides a cross-platform way to retrieve system uptime.
+Supported platforms are Linux, Windows, OS X, *BSD, Solaris, and Plan 9.''',
+    author='Koen Crolla',
+    author_email='cairnarvon@gmail.com',
+    url='https://github.com/Cairnarvon/uptime',
+    package_dir={'': 'src'},
+    py_modules=['uptime'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: System Administrators',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX :: BSD',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: POSIX :: Other',
+                 'Operating System :: POSIX :: SunOS/Solaris',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 3',
+                 'Topic :: System']
+)
---
+++ packages/uptime/uptime-1.1.0.tar.gz/uptime-1.1.0/src/uptime.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+import ctypes
+import struct
+import sys
+import time
+
+"""
+Contains a bunch of functions that implement platform-specific ways to figure
+out the system uptime, and one that uses all of them to figure out yours.
+In principle, none of these functions create new processes or raise any
+exceptions. They all return a float representing the number of seconds of
+uptime, or None if they couldn't figure it out.
+"""
+
+def _uptime_linux():
+    """Returns uptime in seconds or None, on Linux."""
+    # With procfs
+    try:
+        with open('/proc/uptime', 'r') as f:
+            return float(f.readline().split()[0])
+    except (IOError, ValueError):
+        pass
+
+    # Without procfs (really?)
+    try:
+        libc = ctypes.CDLL('libc.so')
+    except OSError:
+        # Debian and derivatives do the wrong thing because /usr/lib/libc.so
+        # is a GNU ld script rather than an ELF object. To get around this, we
+        # have to be more specific.
+        # We don't want to use ctypes.util.find_library because that creates a
+        # new process on Linux. We also don't want to try too hard because at
+        # this point we're already pretty sure this isn't Linux.
+        try:
+            libc = ctypes.CDLL('libc.so.6')
+        except OSError:
+            return None
+
+    if not hasattr(libc, 'sysinfo'):
+        # Not Linux
+        return None
+
+    buf = ctypes.create_string_buffer(128) # 64 suffices on 32-bit, whatever.
+    if libc.sysinfo(buf) < 0:
+        return None
+
+    up = struct.unpack('@l', buf.raw[:struct.calcsize('@l')])[0]
+    return up if up >= 0 else None
+
+def _uptime_bsd():
+    """Returns uptime in seconds or None, on BSD (including OS X)."""
+    try:
+        libc = ctypes.CDLL('libc.so')
+    except OSError:
+        # OS X; can't use ctypes.util.find_library because that creates
+        # a new process on Linux, which is undesirable.
+        try:
+            libc = ctypes.CDLL('libc.dylib')
+        except OSError:
+            return None
+    
+    if not hasattr(libc, 'sysctlbyname'):
+        # Not BSD
+        return None
+
+    # Determine how much space we need for the response
+    sz = ctypes.c_uint(0)
+    libc.sysctlbyname('kern.boottime', None, ctypes.byref(sz), None, 0)
+    if sz.value != struct.calcsize('@LL'):
+        # Unexpected, let's give up.
+        return None
+
+    # For real now
+    buf = ctypes.create_string_buffer(sz.value)
+    libc.sysctlbyname('kern.boottime', buf, ctypes.byref(sz), None, 0)
+    sec, usec = struct.unpack('@LL', buf.raw)
+
+    # OS X disagrees what that second value is
+    if usec > 1000000:
+        usec = 0.
+
+    boottime = sec + usec / 1000000.
+    up = time.time() - boottime
+    return up if up > 0 else None
+
+_uptime_osx = _uptime_bsd
+
+def _uptime_plan9():
+    """Returns uptime in seconds or None, on Plan 9."""
+    # Apparently Plan 9 only has Python 2.2, which I'm not prepared to
+    # support. Maybe some Linuxes implement /dev/time, though, someone was
+    # taling about it somewhere.
+    try:
+        with open('/dev/time', 'r') as f:
+            # The time file holds one 32-bit number representing the sec-
+            # onds since start of epoch and three 64-bit numbers, repre-
+            # senting nanoseconds since start of epoch, clock ticks, and
+            # clock frequency.
+            #  -- cons(3)
+            s, ns, ct, cf = f.read().split()
+            return float(ct) / float(cf)
+    except (IOError, ValueError):
+        return None
+
+def _uptime_solaris():
+    """Returns uptime in second or None, on Solaris."""
+    try:
+        kstat = ctypes.CDLL('libkstat.so')
+    except OSError:
+        return None
+
+    # kstat doesn't have uptime, but it does have boot time.
+    boottime = None
+
+    # Unfortunately, getting at it isn't perfectly straightforward.
+    # But we're going to do this in Python if it kills us.
+
+    # First, let's pretend to be kstat.h
+
+    # Constant
+    KSTAT_STRLEN = 31   # According to every kstat.h I could find.
+
+    # Structs
+    class kstat_t(ctypes.Structure):
+        # One of kstat_t's fields is a pointer to kstat_t, so...
+        pass
+    kstat_t._fields_ = [('ks_crtime', ctypes.c_uint64),
+                        ('ks_next', ctypes.POINTER(kstat_t)),
+                        ('ks_kid', ctypes.c_int),
+                        ('ks_module', ctypes.c_char * KSTAT_STRLEN),
+                        ('ks_resv', ctypes.c_char),
+                        ('ks_instance', ctypes.c_int),
+                        ('ks_name', ctypes.c_char * KSTAT_STRLEN),
+                        ('ks_type', ctypes.c_char),
+                        ('ks_class', ctypes.c_char * KSTAT_STRLEN),
+                        ('ks_flags', ctypes.c_char),
+                        ('ks_data', ctypes.c_voidp),
+                        ('ks_ndata', ctypes.c_uint),
+                        ('ks_data_size', ctypes.c_uint),
+                        ('ks_snaptime', ctypes.c_uint64),
+                        ('ks_update', ctypes.c_void_p),
+                        ('ks_private', ctypes.c_void_p),
+                        ('ks_snapshot', ctypes.c_void_p),
+                        ('ks_lock', ctypes.c_void_p)]
+
+    class kstat_ctl_t(ctypes.Structure):
+        _fields_ = [('kc_chain_id', ctypes.c_int),
+                    ('kc_chain', ctypes.POINTER(kstat_t)),
+                    ('kc_kd', ctypes.c_int)]
+
+    class anon_union(ctypes.Union):
+        # The ``value'' union in kstat_named_t actually has a bunch more
+        # members, but we're only using it for boot_time, so we only need
+        # the padding and the one we're actually using.
+        _fields_ = [('c', ctypes.c_char * 16),
+                    ('time', ctypes.c_int)]
+
+    class kstat_named_t(ctypes.Structure):
+        _fields_ = [('name', ctypes.c_char * KSTAT_STRLEN),
+                    ('data_type', ctypes.c_char),
+                    ('value', anon_union)]
+
+    # Function signatures
+    kstat.kstat_open.restype = ctypes.POINTER(kstat_ctl_t)
+    kstat.kstat_lookup.restype = ctypes.POINTER(kstat_t)
+    kstat.kstat_lookup.argtypes = [ctypes.POINTER(kstat_ctl_t),
+                                   ctypes.c_char_p,
+                                   ctypes.c_int,
+                                   ctypes.c_char_p]
+    kstat.kstat_read.restype = ctypes.c_int
+    kstat.kstat_read.argtypes = [ctypes.POINTER(kstat_ctl_t),
+                                 ctypes.POINTER(kstat_t),
+                                 ctypes.c_void_p]
+    kstat.kstat_data_lookup.restype = ctypes.POINTER(kstat_named_t)
+    kstat.kstat_data_lookup.argtypes = [ctypes.POINTER(kstat_t),
+                                        ctypes.c_char_p]
+
+    # Now, let's do something useful.
+
+    # Initialise kstat control structure.
+    kc = kstat.kstat_open()
+    if not kc:
+        return None
+
+    # We're looking for unix:0:system_misc:boot_time.
+    ksp = kstat.kstat_lookup(kc, 'unix', 0, 'system_misc')
+    if ksp and kstat.kstat_read(kc, ksp, None) != -1:
+        data = kstat.kstat_data_lookup(ksp, 'boot_time')
+        if data:
+            boottime = data.contents.value.time
+
+    # Clean-up.
+    # FIXME I don't know if this also frees the buffer allocated by kstat_read.
+    kstat.kstat_close(kc)
+
+    if boottime is not None:
+        return time.time() - boottime
+
+    return None
+
+def _uptime_syllable():
+    """Returns None, on Syllable."""
+    return None
+
+def _uptime_windows():
+    """
+    Returns uptime in seconds or None, on Windows. Warning: may return
+    incorrect answers after 49.7 days on versions older than Vista.
+    """
+    if not hasattr(ctypes, 'windll') or not hasattr(ctypes.windll, 'kernel32'):
+        return None
+    if hasattr(ctypes.windll.kernel32, 'GetTickCount64'):
+        # Vista/Server 2008 or later
+        return ctypes.windll.kernel32.GetTickCount64() / 1000.
+    if hasattr(ctypes.windll.kernel32, 'GetTickCount'):
+        # Win2k or later; gives wrong answers after 49.7 days
+        return ctypes.windll.kernel32.GetTickCount() / 1000.
+    return None
+
+def uptime():
+    """Returns uptime in seconds if even remotely possible, or None if not."""
+    if sys.platform == 'linux2':
+        up = _uptime_linux()
+        if up is not None:
+            return up
+
+    if sys.platform == 'win32':
+        up = _uptime_windows()
+        if up is not None:
+            return up
+
+    if sys.platform == 'darwin' or 'bsd' in sys.platform:
+        up = _uptime_bsd()
+        if up is not None:
+            return up
+
+    if sys.platform == 'sunos5':
+        up = _uptime_solaris()
+        if up is not None:
+            return up
+
+    return _uptime_bsd() or _uptime_plan9() or \
+           _uptime_linux() or _uptime_windows() or _uptime_solaris()
+
+
+if __name__ == '__main__':
+    up = uptime()
+    if up is not None:
+        h, m, s = up / 3600, up / 60 % 60, up % 60
+        sys.stdout.write('Uptime: %02d:%02d:%02d (%.2f seconds).\n' % (h, m, s, up))
+    else:
+        sys.stdout.write('Unable to determine uptime. Patches welcome.\n')
