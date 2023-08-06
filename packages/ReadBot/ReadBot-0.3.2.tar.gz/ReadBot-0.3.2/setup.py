081ed7e29bad8fca195f83b4c025237fef186f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:17 +0000

    Add package ReadBot/0.3.2/ReadBot-0.3.2.tar.gz

---
+++ packages/ReadBot/ReadBot-0.3.2.tar.gz/ReadBot-0.3.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ReadBot',
+    author='Matthew Meyer',
+    author_email='matthewwilliammeyer@gmail.com',
+    url='https://github.com/mwmnj/readbot',
+    version='0.3.2',
+    packages=['readbot',],
+    license='MIT',
+    description='A delightful tesseract OCR module',
+)
