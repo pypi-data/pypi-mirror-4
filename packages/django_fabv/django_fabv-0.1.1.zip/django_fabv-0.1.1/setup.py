3011cf36f25d8ffab5abb42201c65b50beefe4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:10 +0000

    Add package django_fabv/0.1.1/django_fabv-0.1.1.zip

---
+++ packages/django_fabv/django_fabv-0.1.1.zip/django_fabv-0.1.1/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+setup(
+    name = 'django_fabv',
+    version = '0.1.1',
+    url = 'https://pypi.python.org/pypi/django_fabv/0.1.0',
+    include_package_data=True,
+    packages=find_packages(),
+    license = '3-clause BSD',
+    author = 'Bob Jansen',
+    author_email = 'bob.jansen@veneficus.nl',
+    description = 'A/B module for Django based on ABingo',
+    long_description=open('README.txt').read(),
+)
