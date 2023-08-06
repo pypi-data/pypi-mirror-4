8a37232118ed108a090fb51200eed6993697bf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:07 +0000

    Add package ipython-sql/0.1.2.1/ipython-sql-0.1.2.1.tar.gz

---
+++ packages/ipython-sql/ipython-sql-0.1.2.1.tar.gz/ipython-sql-0.1.2.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1.2.1'
+
+install_requires = [
+    'prettytable',
+    'ipython>=0.13',
+    'sqlalchemy',
+    'sqlparse',
+]
+
+
+setup(name='ipython-sql',
+    version=version,
+    description="RDBMS access via IPython",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+    keywords='database ipython postgresql mysql',
+    author='Catherine Devlin',
+    author_email='catherine.devlin@gmail.com',
+    url='pypi.python.org/pypi/ipython-sql',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
