1859692ab92641fe32647ac115bf82e340d3a836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:31 +0000

    Add package firehose/0.1/firehose-0.1.tar.gz

---
+++ packages/firehose/firehose-0.1.tar.gz/firehose-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='firehose',
+    version='0.1',
+    description='Library for working with output from static code analyzers',
+    packages=['firehose',
+              'firehose.parsers'],
+    license='LGPL2.1 or later',
+    author='David Malcolm <dmalcolm@redhat.com>',
+    url='https://github.com/fedora-static-analysis/firehose',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    )
+)
