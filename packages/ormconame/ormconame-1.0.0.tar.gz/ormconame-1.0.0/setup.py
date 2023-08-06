da36ab8ca8d42a0d63964353828308c2bb04afa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:15 +0000

    Add package ormconame/1.0.0/ormconame-1.0.0.tar.gz

---
+++ packages/ormconame/ormconame-1.0.0.tar.gz/ormconame-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='ormconame', version='1.0.0', py_modules=['ormco'],author='fengchj', author_email='fengchangjian@gmail.com', url='http://fengchangjian.com', description='an example',)
