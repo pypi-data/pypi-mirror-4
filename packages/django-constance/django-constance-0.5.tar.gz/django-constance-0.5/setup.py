096dc499a7c333a1d4a8a72bfa76be3afddbe5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package django-constance/0.5/django-constance-0.5.tar.gz

---
+++ packages/django-constance/django-constance-0.5.tar.gz/django-constance-0.5/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+
+# work around to prevent http://bugs.python.org/issue15881 from showing up
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+try:
+    f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+    long_description = f.read().strip()
+    f.close()
+except IOError:
+    long_description = None
+
+setup(
+    name='django-constance',
+    version='0.5',
+    url="http://github.com/comoga/django-constance",
+    description='Django live settings with pluggable backends, including Redis.',
+    long_description=long_description,
+    author='Comoga Django Team',
+    author_email='dev@comoga.cz',
+    maintainer='Jannis Leidel',
+    maintainer_email='jannis@leidel.info',
+    license='BSD',
+    keywords='django libraries settings redis'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    test_suite='tests.runtests.main',
+    test_requires=['django-nose'],
+    extras_require = {
+        'database':  ['django-picklefield'],
+        'redis': ['redis'],
+    }
+)
