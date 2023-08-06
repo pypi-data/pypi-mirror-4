252a184f9530bfc32a3deb711bfd7488ee6799cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:07 +0000

    Add package confman/0.1/confman-0.1.tar.gz

---
+++ packages/confman/confman-0.1.tar.gz/confman-0.1/example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from confman import ConfigSource
+
+options = {
+    'tags': ['desktop'],
+    'hostname': 'test',
+}
+
+from os import path
+samples_path = path.join(path.dirname(__file__), 'samples')
+
+c = ConfigSource(samples_path, "/tmp/dotfiles-test", None, options)
+c.sync()
+
+print
+print repr(c)
