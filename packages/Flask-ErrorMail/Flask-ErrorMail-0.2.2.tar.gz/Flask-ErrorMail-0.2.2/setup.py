6e423f351d5e63cf3555770bd6f0bec1ff965f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:14 +0000

    Add package Flask-ErrorMail/0.2.2/Flask-ErrorMail-0.2.2.tar.gz

---
+++ packages/Flask-ErrorMail/Flask-ErrorMail-0.2.2.tar.gz/Flask-ErrorMail-0.2.2/setup.py
@@ -0,0 +1,52 @@
+"""
+Flask-ErrorMail
+----------
+
+A Flask extension to automatically send stacktrace error e-mails to site 
+administrators when 500 errors occur.
+
+Please refer to the online documentation for details.
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-ErrorMail>`_
+* `development version
+  <https://github.com/jasonwyatt/Flask-ErrorMail/tarball/master>`_
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-ErrorMail',
+    version='0.2.2',
+    url='http://github.com/jasonwyatt/Flask-ErrorMail',
+    license='MIT',
+    author='Jason Wyatt Feinstein',
+    author_email='jason.feinstein@gmail.com',
+    description='Flask extension for sending administrators e-mails with stacktraces when internal server errors occur.',
+    long_description=__doc__,
+    packages=[
+        'flask_errormail',
+    ],
+    test_suite='nose.collector',
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-Mail',
+    ],
+    tests_require=[
+        'nose',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
