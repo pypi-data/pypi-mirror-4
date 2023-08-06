d556df6ba81f9b15ae658bf0bbc54adadf9084ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:03 +0000

    Add package Loslassa/0.1-dev-1/Loslassa-0.1-dev-1.tar.gz

---
+++ packages/Loslassa/Loslassa-0.1-dev-1.tar.gz/Loslassa-0.1-dev-1/setup.py
@@ -0,0 +1,29 @@
+"""See README.rst for description"""
+from setuptools import setup
+
+setup(
+    name='Loslassa',
+    version='0.1-dev-1',
+    url='http://github.com/obestwalter/loslassa/',
+    license='BSD',
+    author='Oliver Bestwalter',
+    author_email='oliver@bestwalter.de',
+    description='Web Site generator based on sphinx and git',
+    long_description=open("README.rst").read(),
+    packages=['loslassa', 'loslassa.testsuite'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Sphinx>=1.1.3', 'plumbum', 'sphinx_bootstrap_theme'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
