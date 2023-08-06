617439bdda9bbac316fbaf977cd95c2eaea9dd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:12 +0000

    Add package corpcrawl/0.0.34/corpcrawl-0.0.34.tar.gz

---
+++ packages/corpcrawl/corpcrawl-0.0.34.tar.gz/corpcrawl-0.0.34/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(name='corpcrawl',
+      version='0.0.34',
+      author="rozap",
+      author_email="chrisd1891@gmail.com",
+      license="MIT",
+      url="https://github.com/chrisd1891/corpcrawl/",
+      description="Corpcrawl EDGAR Scraper",
+      long_description=long_description,
+      platforms=['any'],
+      packages=["corpcrawl"]
+)
