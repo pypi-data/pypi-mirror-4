52dacf8c30365a2382e469e322b346b7c471478e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:02 +0000

    Add package iron-core/1.0.1/iron-core-1.0.1.tar.gz

---
+++ packages/iron-core/iron-core-1.0.1.tar.gz/iron-core-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+        name = "iron-core",
+        py_modules = ["iron_core"],
+        install_requires=["iso8601", "requests >= 1.1.0"],
+        version = "1.0.1",
+        description = "Universal classes and methods for Iron.io API wrappers to build on.",
+        author = "Iron.io",
+        author_email = "thirdparty@iron.io",
+        url = "https://www.github.com/iron-io/iron_core_python",
+        keywords = ["Iron.io"],
+        classifiers = [
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 3",
+                "Intended Audience :: Developers",
+                "Operating System :: OS Independent",
+                "Development Status :: 2 - Pre-Alpha",
+                "License :: OSI Approved :: BSD License",
+                "Natural Language :: English",
+                "Topic :: Internet",
+                "Topic :: Internet :: WWW/HTTP",
+                "Topic :: Software Development :: Libraries :: Python Modules",
+
+        ],
+        long_description = """\
+Iron.io common library
+----------------------
+
+This package offers common functions for Iron.io APIs and services. It does not wrap 
+any APIs or contain API-specific features, but serves as a common base that wrappers
+may be built on. Users looking for API wrappers should instead look at 
+iron_worker_python and iron_worker_mq."""
+)
