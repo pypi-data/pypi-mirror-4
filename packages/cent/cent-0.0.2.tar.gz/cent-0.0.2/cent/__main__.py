b757b5bdbc5e9bb0063c1f9794b1a40b370bc4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:48 +0000

    Add package cent/0.0.2/cent-0.0.2.tar.gz

---
+++ packages/cent/cent-0.0.2.tar.gz/cent-0.0.2/cent/__main__.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import os
+import sys
+
+try:
+    import configparser as ConfigParser
+except ImportError:
+    import ConfigParser
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from .client import Client
+
+
+def run():
+
+    parser = argparse.ArgumentParser(description='Centrifuge client')
+
+    parser.add_argument(
+        'section', metavar='SECTION', type=str, help='section key from zambie configuration file'
+    )
+    parser.add_argument(
+        'category', metavar='CATEGORY', type=str, help='category name'
+    )
+    parser.add_argument(
+        'key', metavar='KEY', type=str, help='event key'
+    )
+    parser.add_argument(
+        '--data', type=str, help='event data', default='{}'
+    )
+    parser.add_argument(
+        '--tag', action='append', help='event tags'
+    )
+    parser.add_argument(
+        '--unique', action='append', help='unique event data keys'
+    )
+    parser.add_argument(
+        '--config', type=str, default="~/.zambierc", help='zambie configuration file'
+    )
+
+    options = parser.parse_args()
+
+    config_file = os.path.expanduser(options.config)
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+
+    if not options.section in config.sections():
+        print(
+            "Section {0} not found in {1} configuration file".format(
+                options.section, options.config
+            )
+        )
+        sys.exit(1)
+
+    try:
+        address = config.get(options.section, 'address')
+        project = config.get(options.section, 'project')
+        public_key = config.get(options.section, 'public_key')
+        secret_key = config.get(options.section, 'secret_key')
+        try:
+            timeout = config.getint(options.section, 'timeout')
+        except:
+            timeout = 2
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    if not sys.stdin.isatty():
+        json_data = sys.stdin.read().strip()
+    else:
+        json_data = options.data
+
+    try:
+        data = json.loads(json_data)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    if not data:
+        print("no event data, nothing to do")
+        sys.exit(1)
+
+    client = Client(
+        address,
+        project,
+        public_key,
+        secret_key,
+        timeout=timeout
+    )
+
+    if not isinstance(data, dict):
+        print("event data must be valid JSON object")
+        sys.exit(1)
+
+    event = {
+        'category': options.category,
+        'key': options.key,
+        'data': data
+    }
+    if options.unique:
+        event['unique_keys'] = options.unique
+    if options.tag:
+        event['tags'] = options.tag
+
+    to_send = [event]
+    response = client.send(to_send)
+    result, error = response
+    if error:
+        print(error)
+        sys.exit(1)
+    else:
+        print(result)
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/cent/cent-0.0.2.tar.gz/cent-0.0.2/setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from setuptools import setup
+
+
+if sys.argv[-1] == 'test':
+    status = os.system('python tests/tests.py')
+    sys.exit(1 if status > 127 else status)
+
+
+requirements = ['six>=1.3.0']
+if sys.version_info[:2] in ((2, 6),):
+    # argparse has been added in Python 3.2 / 2.7
+    requirements.append('argparse>=1.2.1')
+
+
+def long_description():
+    return "centrifuge terminal client"
+
+
+setup(
+    name='cent',
+    version='0.0.2',
+    description="centrifuge terminal client",
+    long_description=long_description(),
+    url='https://github.com/FZambia/cent',
+    download_url='https://github.com/FZambia/cent',
+    author="Alexandr Emelin",
+    author_email='frvzmb@gmail.com',
+    license='BSD',
+    packages=['cent'],
+    entry_points={
+        'console_scripts': [
+            'cent = cent.__main__:run',
+            ],
+        },
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ]
+)
