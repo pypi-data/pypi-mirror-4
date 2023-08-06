44f3cd2d6fc3aedda171e6a95095dd27e4bf6069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package proofread/0.1.0/proofread-0.1.0.tar.gz

---
+++ packages/proofread/proofread-0.1.0.tar.gz/proofread-0.1.0/proofread/contrib/django_proofread/management/__init__.py
@@ -0,0 +1,7 @@
+"""
+proofread.contrib.django_proofread.management
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2013 by Matt Robenolt
+:license: BSD, see LICENSE for more details.
+"""
---
+++ packages/proofread/proofread-0.1.0.tar.gz/proofread-0.1.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Proofread
+=========
+
+Test your web app without writing tests!
+
+Compatible with Django and Flask.
+
+:copyright: (c) 2013 by Matt Robenolt
+:license: BSD, see LICENSE for more details.
+"""
+
+from setuptools import setup, find_packages
+
+tests_require = map(lambda x: x.strip(), open('requirements.txt').readlines())
+
+setup(
+    name='proofread',
+    version='0.1.0',
+    author='Matt Robenolt',
+    author_email='matt@ydekproductions.com',
+    url='https://github.com/mattrobenolt/proofread',
+    description='Test your web app without writing tests!',
+    long_description=__doc__,
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    tests_require=tests_require,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
