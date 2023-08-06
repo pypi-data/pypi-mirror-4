0618f3c22d7ec2698c61fabe6e5c1fd7dd266c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:42 +0000

    Add package binhotm/1.5.a/binhotm-1.5.a.zip

---
+++ packages/binhotm/binhotm-1.5.a.zip/binhotm-1.5.a/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+        name       =  "binhotm",
+        version    =  '1.5.a',
+        py_modules = ['binhotm'],
+        author = 'Fabricio Jr Silva',
+        author_emil = 'fabriciojuniorsilva@gmail.com',
+        url = 'no-url',
+        description = 'Print list',)
