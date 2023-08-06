0fcd089f0976e6ddfbf2cdfdbe068c2fe9d6358c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:25 +0000

    Add package django-nopassword/0.2/django-nopassword-0.2.tar.gz

---
+++ packages/django-nopassword/django-nopassword-0.2.tar.gz/django-nopassword-0.2/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+setup(
+    name = "django-nopassword",
+    version = '0.2',
+    url='http://github.com/relekang/django-nopassword',
+    author = 'Rolf Erik Lekang',
+    author_email = 'me@rolflekang.com',
+    description = 'Authentication backend for django that uses email verification instead of passwords',
+    packages = find_packages(exclude='tests'),
+    tests_require=[
+        'django>=1.3',
+    ],
+    test_suite='runtests.runtests',
+    include_package_data=True,
+)
