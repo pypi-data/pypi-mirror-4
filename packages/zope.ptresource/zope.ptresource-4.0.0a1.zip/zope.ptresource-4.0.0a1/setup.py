772b10e95188bb519a721f1cc1d18e5542a395f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package zope.ptresource/4.0.0a1/zope.ptresource-4.0.0a1.zip

---
+++ packages/zope.ptresource/zope.ptresource-4.0.0a1.zip/zope.ptresource-4.0.0a1/setup.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""zope.ptresource setup
+"""
+from setuptools import setup, find_packages
+
+long_description = (open('README.txt').read() + '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(name='zope.ptresource',
+      version='4.0.0a1',
+      url='http://pypi.python.org/pypi/zope.ptresource/',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      classifiers = [
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+        ],
+      description='Page template resource plugin for zope.browserresource',
+      long_description=long_description,
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=[
+        'setuptools',
+        'zope.browserresource',
+        'zope.interface',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.security',
+        ],
+      extras_require={
+          'test': ['zope.testing'],
+          },
+      tests_require=['zope.testing'],
+      test_suite='zope.ptresource.tests.test_suite',
+      include_package_data=True,
+      zip_safe = False,
+      )
