128289c76f7f46d36f4c991d4141c5550010488e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package Flask-WebCache/0.7/Flask-WebCache-0.7.tar.gz

---
+++ packages/Flask-WebCache/Flask-WebCache-0.7.tar.gz/Flask-WebCache-0.7/flask_webcache/validation.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from httplib import NOT_IMPLEMENTED, NOT_MODIFIED, OK
+import hashlib
+
+from flask import request, g
+
+class Validation(object):
+    def can_set_etag(self, response):
+        return not (
+            response.is_streamed or
+            'etag' in response.headers or
+            response.status_code != OK
+        )
+    def set_etag(self, response):
+        response.set_etag(hashlib.md5(response.data).hexdigest())
+
+    def if_none_match(self, response):
+        if response.status[0] != '2' and response.status_code != NOT_MODIFIED:
+            return False
+        etag, weak = response.get_etag()
+        return etag in request.if_none_match
+    def return_not_modified_response(self, response):
+        if request.method not in {"GET", "HEAD"}:
+            # HACK: RFC says we MUST NOT have performed this method, but it was
+            #        just performed and there's nothing we can do about it.
+            #       To handle this request correctly, the application logic
+            #        of this app should have made the if-none-match check and
+            #        abort() with 412 PRECONDITION_FAILED.
+            #       Not sure what to do, I'm opting to return the 5xx status
+            #        501 NOT_IMPLEMENTED. Meh.
+            if not hasattr(g, 'webcache_ignore_if_none_match'):
+                response.status_code = NOT_IMPLEMENTED
+            return
+        response.data = ''
+        response.status_code = NOT_MODIFIED
+    def add_date_fields(self, response):
+        now = datetime.utcnow() # freeze time for identical dates
+        if 'last-modified' not in response.headers:
+            response.last_modified = now
+        if 'date' not in response.headers:
+            response.date = now
