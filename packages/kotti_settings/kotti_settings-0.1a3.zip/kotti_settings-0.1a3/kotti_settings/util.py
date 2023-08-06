77ed5150ade73c30c2cc7671b5bcbfe248bfcf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:26 +0000

    Add package kotti_settings/0.1a3/kotti_settings-0.1a3.zip

---
+++ packages/kotti_settings/kotti_settings-0.1a3.zip/kotti_settings-0.1a3/kotti_settings/util.py
@@ -0,0 +1,57 @@
+import inspect
+
+from kotti.resources import get_root
+
+from kotti_settings.config import SETTINGS
+from kotti_settings.settings import ModuleSettings
+from kotti_settings.settings import SettingObj
+
+
+def get_setting(name, not_found=None):
+    frame = inspect.stack()[1]
+    module = inspect.getmodule(frame[0])
+    modname = module.__name__
+    if '.' in modname:
+        modname = modname[:modname.find('.')]  # pragma: no cover
+    if not name.startswith(modname):
+        name = '{0}-{1}'.format(modname, name)
+    settings = get_settings()
+    if name in settings:
+        return settings[name]
+    return not_found
+
+
+def get_settings():
+    root = get_root()
+    if "kotti_settings" not in root.annotations:
+        root.annotations['kotti_settings'] = {}
+    return root.annotations['kotti_settings']
+
+
+def add_settings(module_settings):
+    """Get a dictionary and translate this into an object structure.
+    """
+    settings = get_settings()
+    module_settings = ModuleSettings(**module_settings)
+    if module_settings.module is None:
+        # If the module name is omitted: get it
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        modname = module.__name__
+        if '.' in modname:
+            modname = modname[:modname.find('.')]
+        module_settings.module = modname
+    for setting in module_settings.settings:
+        setting_obj = SettingObj(**setting)
+        # name and title of a setting is required
+        if setting_obj.name is None:
+            raise ValueError('A setting has to have a name.')
+        if setting_obj.title is None:
+            raise ValueError('A setting has to have a title.')
+        setting_obj.module = module_settings.module
+        default = None
+        if 'default' in setting:
+            default = setting['default']
+        settings[setting_obj.field_name] = default
+        module_settings.settings_objs.append(setting_obj)
+    SETTINGS.append(module_settings)
---
+++ packages/kotti_settings/kotti_settings-0.1a3.zip/kotti_settings-0.1a3/local/CHANGES.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+0.1a3(2013-03-13)
+-----------------
+
+* Only take the module name itself into account for the setting name.
+
+
+0.1a2(2013-03-11)
+-----------------
+
+* Added util method get_setting as default way to get a setting.
+
+
+0.1a1(2013-03-06)
+-----------------
+
+- Initial release.
