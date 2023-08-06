deaf203e9504cfd5ef59cd8c3c68228f5f9f0c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:42 +0000

    Add package pypi_pronounce/0.0.0.1/pypi_pronounce-0.0.0.1.tar.gz

---
+++ packages/pypi_pronounce/pypi_pronounce-0.0.0.1.tar.gz/pypi_pronounce-0.0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pypi_pronounce",
+    author="Jean-Paul Calderone",
+    author_email="exarkun@twistedmatrix.com",
+    url="http://pypi.python.org/",
+    description="A user-friendly guide to pronouncing PyPI.",
+    py_modules=["pypi_pronounce"],
+    version="0.0.0.1")
