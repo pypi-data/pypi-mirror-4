4e3cbf1edb4c5e9a398453860271a953ca4bcf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:33 +0000

    Add package fsq/0.2.1/fsq-0.2.1.tar.gz

---
+++ packages/fsq/fsq-0.2.1.tar.gz/fsq-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from glob import glob
+
+setup(
+    name='fsq',
+    version='0.2.1',
+    author='Matthew Story',
+    author_email='matt.story@axial.net',
+    packages=['fsq', 'fsq.tests'],
+    scripts=['bin/fsq'],
+    data_files=[('share/man/man1', ['man/man1/fsq.1',
+                                    'man/man1/fsq-down.1',
+                                    'man/man1/fsq-enqueue.1',
+                                    'man/man1/fsq-scan.1',
+                                    'man/man1/fsq-up.1',
+                                    'man/man1/fsq-install.1']),
+                ('share/man/man7', ['man/man7/fsq.7']),
+                ('libexec/fsq', ['libexec/fsq/down.py',
+                                 'libexec/fsq/enqueue.py',
+                                 'libexec/fsq/install.py',
+                                 'libexec/fsq/scan.py',
+                                 'libexec/fsq/up.py']),
+               ],
+    url='https://github.com/axialmarket/fsq',
+    license='3-BSD',
+    description='File System Queue',
+    long_description=open('README').read(),
+)
+
