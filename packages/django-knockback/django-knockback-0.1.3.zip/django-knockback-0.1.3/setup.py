d2a3ac4b3fcb7996a86614908a1c8e4fc539331a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:29 +0000

    Add package django-knockback/0.1.3/django-knockback-0.1.3.zip

---
+++ packages/django-knockback/django-knockback-0.1.3.zip/django-knockback-0.1.3/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+
+__doc__ = """
+================
+django-knockback
+================
+
+Django-Knockback is a utility app that just contains all the javascript dependencies that are required in order to use `knockback.js <https://github.com/kmalakoff/knockback>`_.
+
+The app depends on `django-compressor <https://github.com/jezdez/django_compressor>`_ in order to serve knockback.js in both compressed and uncompressed forms.
+
+The current versions of the javascript libraries are:
+
+- jQuery 1.9.1
+- Underscore 1.4.4
+- Backbone 1.0.0
+- Knockout 2.2.0
+- Knockback 0.17.0
+
+
+Installation:
+^^^^^^^^^^^^^^^^^^^^^
+Open the console and then type::
+
+    pip install django-knockback
+
+Usage:
+^^^^^^^^^^^^^^^^^^^^^
+Include the 'knockback' app in the ``INSTALLED_APPS`` and then type::
+
+    ./manage.py collectstatic.
+
+To include knockback.js into your page just include the knockback/knockback.html template into the <head> tag or extend the knockback/base.html template.
+The base.html has two blocks named head and body that contain the content of their respective tags.
+"""
+
+__maintainer__ = "Omer Katz"
+__email__ = "omer.drow@gmail.com"
+__license__ = "BSD"
+__version__ = "0.1.3"
+__status__ = "Stable"
+
+setup(name='django-knockback',
+    version=__version__,
+    packages=['knockback'],
+    license=__license__,
+    author=__maintainer__,
+    author_email=__email__,
+    long_description=__doc__,
+    description='A django app that contains the necessary javascript files to use knockback.js',
+    keywords=['django', 'knockout.js', 'backbone.js', 'jquery', 'knockback.js'],
+    url='https://github.com/thedrow/django-knockback/',
+    install_requires=['Django>=1.3', 'django_compress'])
