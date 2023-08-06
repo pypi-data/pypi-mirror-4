a615e1c57516780001aa4137aa1294b56868d324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:57 +0000

    Add package sshstdlib/1.1/sshstdlib-1.1.tar.gz

---
+++ packages/sshstdlib/sshstdlib-1.1.tar.gz/sshstdlib-1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+fp = open(os.path.join(ROOT,"requirements.txt"))
+REQUIREMENTS = [r.strip() for r in fp.readlines()]
+fp.close()
+
+
+if __name__ == "__main__":
+    setup(
+        name="sshstdlib",
+        version="1.1",
+        license="BSD",
+
+        description="A standard library emulation layer over SSH",
+        author="Steve Stagg",
+        author_email="stestagg@gmail.com",
+        url="http://github.com/stestagg/sshstdlib",
+
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+        ],
+
+        install_requires=REQUIREMENTS
+
+    )
