986707d61fd7a871cb5b37212f00afdb002b8a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:32 +0000

    Add package argdeclare/0.5.2/argdeclare-0.5.2.tar.gz

---
+++ packages/argdeclare/argdeclare-0.5.2.tar.gz/argdeclare-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = '0.5.2'
+
+setup(name="argdeclare",
+      version=version,
+      description="Implementation of the interface provided by the cmdln module but using argparse.",
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      author="Parnell Springmeyer",
+      author_email="ixmatus@gmail.com",
+      url="http://bitbucket.org/ixmatus/argdeclare",
+      license="PSF",
+      zip_safe=False,
+      packages=find_packages(),
+      requires=['argcomplete'],
+      include_package_data=True
+      )
