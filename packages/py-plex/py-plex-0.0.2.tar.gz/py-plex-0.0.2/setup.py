e58658a2f7bcab9bc670fdc32b06e0dff8e6ca6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:47 +0000

    Add package py-plex/0.0.2/py-plex-0.0.2.tar.gz

---
+++ packages/py-plex/py-plex-0.0.2.tar.gz/py-plex-0.0.2/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "py-plex",
+    version = "0.0.2",
+    author = "Thomas Rückstieß",
+    description = ("Python client library for Plex Media Server"),
+    url = "https://github.com/rueckstiess/py-plex",
+    packages=['plex'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
