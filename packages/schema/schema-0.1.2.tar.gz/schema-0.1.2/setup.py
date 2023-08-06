0c31e887fc4f11336e4d256e5bdf535f3afe2887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:41 +0000

    Add package schema/0.1.2/schema-0.1.2.tar.gz

---
+++ packages/schema/schema-0.1.2.tar.gz/schema-0.1.2/setup.py
@@ -0,0 +1,25 @@
+"""`schema` lives on `GitHub <http://github.com/halst/schema/>`_."""
+from setuptools import setup
+
+import schema
+
+
+setup(
+    name = schema.__name__,
+    version = schema.__version__,
+    author = "Vladimir Keleshev",
+    author_email = "vladimir@keleshev.com",
+    description = "Simple data validation library",
+    license = "MIT",
+    keywords = "schema json validation",
+    url = "http://github.com/halst/schema",
+    py_modules=['schema'],
+    long_description=__doc__,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
