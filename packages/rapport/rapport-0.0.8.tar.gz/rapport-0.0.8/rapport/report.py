74d6ef40d0a09819129659ea055d06b53b47c702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:03 +0000

    Add package rapport/0.0.8/rapport-0.0.8.tar.gz

---
+++ packages/rapport/rapport-0.0.8.tar.gz/rapport-0.0.8/rapport/report.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2013, Sascha Peilicke <saschpe@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (see the file COPYING); if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+import os
+import sys
+
+import rapport.config
+import rapport.util
+
+
+def list():
+    pass
+
+def get(report=None):
+    pass
+
+def create():
+    pass
