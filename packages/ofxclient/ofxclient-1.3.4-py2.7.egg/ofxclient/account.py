d0c0fa84e21602654ca643d2f97895e455716949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package ofxclient/1.3.4/ofxclient-1.3.4-py2.7.egg

---
+++ packages/ofxclient/ofxclient-1.3.4-py2.7.egg/ofxclient/account.py
@@ -0,0 +1,222 @@
+from ofxparse import OfxParser, AccountType
+import datetime
+import StringIO
+import time
+import hashlib
+
+class Account(object):
+    """Base class for accounts at an institution
+
+    It provides basic methods for retrieving transactions
+    that are common to all different types of accounts.
+
+    Subclasses only need to implement the download_query method
+    which is expected to take an 'as_of' parameter representing
+    an effective lower date boundary in 'YYYYMMDD' format.  The
+    return value of this function is expected to be a prepared
+    query as returned by the ofxclient.client.Client
+
+    See these subclasses for more details:
+    BankAccount
+    CreditCardAccount
+    BrokerageAccount
+    """
+    def __init__(self, number, institution, description=None ):
+        self.institution = institution
+        self.number      = number
+        self.description = description or self._default_description()
+
+    def local_id(self):
+        """Locally generated unique account identifier.
+
+        :rtype: string
+        """
+        return hashlib.sha256("%s%s" % (
+                self.institution.local_id(),
+                self.number )).hexdigest()
+
+    def number_masked(self):
+        """Masked version of the account number for privacy.
+
+        :rtype: string
+        """
+        return "***%s" % self.number[-4:]
+
+    def long_description(self):
+        """Long description of the account (includes institution description).
+
+        :rtype: string
+        """
+        return "%s: %s" % (self.institution.description,self.description)
+
+    def _default_description(self):
+        return self.number_masked()
+
+    def download(self,days=60):
+        """Downloaded OFX response for the given time range
+
+        :param days: Number of days to look back at
+        :type days: integer
+        :rtype: :py:class:`StringIO.StringIO`
+
+        """
+        days_ago  = datetime.datetime.now() - datetime.timedelta( days=days )
+        as_of     = time.strftime("%Y%m%d",days_ago.timetuple())
+        query     = self.download_query( as_of = as_of )
+        response  = self.institution.client().post(query)
+        return StringIO.StringIO(response)
+
+    def download_parsed(self,days=60):
+        """Downloaded OFX response parsed by :py:meth:`ofxparser.OfxParser.parse`
+
+        :param days: Number of days to look back at
+        :type days: integer
+        :rtype: :py:class:`ofxparser.Ofx`
+        """
+        return OfxParser.parse( self.download(days=days) )
+
+    def statement(self,days=60):
+        """Download the :py:class:`ofxparse.Statement` given the time range
+
+        :param days: Number of days to look back at
+        :type days: integer
+        :rtype: :py:class:`ofxparser.Statement`
+        """
+        parsed = self.download_parsed(days=days)
+        return parsed.account.statement
+
+    def transactions(self,days=60):
+        """Download a a list of :py:class:`ofxparse.Transaction` objects
+        
+        :param days: Number of days to look back at
+        :type days: integer
+        :rtype: list of :py:class:`ofxparser.Transaction` objects
+        """
+        return self.statement(days=days).transactions
+
+    def serialize(self):
+        """Serialize predictably for use in configuration storage.
+
+        Output look like this::
+
+          {
+            'local_id':       'string',
+            'number':         'account num',
+            'description':    'descr',
+            'broker_id':      'may be missing - type dependent',
+            'routing_number': 'may be missing - type dependent,
+            'account_type':   'may be missing - type dependent,
+            'institution': {
+                # ... see :py:meth:`ofxclient.Institution.serialize`
+            }
+          }
+
+        :rtype: nested dictionary
+        """
+        data = {
+                'local_id': self.local_id(),
+                'institution': self.institution.serialize(),
+                'number': self.number,
+                'description': self.description
+        }
+        if hasattr(self,'broker_id'):
+            data['broker_id'] = self.broker_id
+        elif hasattr(self,'routing_number'):
+            data['routing_number'] = self.routing_number
+            data['account_type']   = self.account_type
+
+        return data
+
+    @staticmethod
+    def deserialize(raw):
+        """Instantiate :py:class:`ofxclient.Account` subclass from dictionary
+
+        :param raw: serilized Account
+        :param type: dict as  given by :py:method:`~ofxclient.Account.serialize`
+        :rtype: subclass of :py:class:`ofxclient.Account`
+        """
+        from ofxclient.institution import Institution
+        institution = Institution.deserialize(raw['institution'])
+
+        del raw['institution']
+        del raw['local_id']
+
+        if   raw.has_key('broker_id'):
+            a = BrokerageAccount(institution=institution,**raw)
+        elif raw.has_key('routing_number'):
+            a = BankAccount(institution=institution,**raw)
+        else:
+            a = CreditCardAccount(institution=institution,**raw)
+        return a
+
+    @staticmethod
+    def from_ofxparse( data, institution ):
+        """Instantiate :py:class:`ofxclient.Account` subclass from ofxparse module
+
+        :param data: an ofxparse account
+        :type data: An :py:class:`ofxparse.Account` object
+        :param institution: The parent institution of the account
+        :type institution: :py:class:`ofxclient.Institution` object
+        """
+
+        description = data.desc if hasattr(data,'desc') else None
+        if data.type == AccountType.Bank:
+            return BankAccount(
+                    institution=institution,
+                    number=data.account_id,
+                    routing_number=data.routing_number,
+                    account_type=data.account_type,
+                    description=description )
+        elif data.type == AccountType.CreditCard:
+            return CreditCardAccount( 
+                    institution=institution,
+                    number=data.account_id,
+                    description=description )
+        elif data.type == AccountType.Investment:
+            return BrokerageAccount(
+                    institution=institution,
+                    number=data.account_id,
+                    broker_id=data.brokerid,
+                    description=description )
+        raise ValueError("unknown account type: %s" % data.type)
+
+
+class BrokerageAccount(Account):
+    """Implementation representing a brokerage/investment account"""
+    def __init__(self, broker_id, **kwargs):
+        super( BrokerageAccount, self ).__init__(**kwargs)
+        self.broker_id = broker_id
+
+    def download_query(self,as_of):
+        """formulate the specific query needed for download"""
+        c = self.institution.client()
+        q = c.brokerage_account_query(number=self.number,date=as_of,broker_id=self.broker_id)
+        return q
+
+class BankAccount(Account):
+    """Implementation representing a checking or savings account"""
+    def __init__(self, routing_number, account_type, **kwargs):
+        super( BankAccount, self ).__init__(**kwargs)
+        self.routing_number = routing_number
+        self.account_type   = account_type
+
+    def download_query(self,as_of):
+        """formulate the specific query needed for download"""
+        c = self.institution.client()
+        q = c.bank_account_query(
+                number=self.number,
+                date=as_of,
+                account_type=self.account_type,
+                bank_id=self.routing_number)
+        return q
+
+class CreditCardAccount(Account):
+    """Implementation representing a credit card account"""
+    def __init__(self, **kwargs):
+        super( CreditCardAccount, self ).__init__(**kwargs)
+
+    def download_query(self,as_of):
+        """formulate the specific query needed for download"""
+        c = self.institution.client()
+        q = c.credit_card_account_query(number=self.number,date=as_of)
+        return q
---
+++ packages/ofxclient/ofxclient-1.3.4-py2.7.egg/ofxclient/institution.py
@@ -0,0 +1,167 @@
+import StringIO
+import hashlib
+from ofxclient.client import Client
+from ofxparse import OfxParser
+from BeautifulSoup import BeautifulStoneSoup
+
+class Institution(object):
+    """Represents an institution or bank
+
+    This is where you specify all connection details needed
+    for the specific institution.
+
+    For help obtaining the id, org, and url; please see the
+    ofxhome python module and/or the http://ofxhome.com website.
+
+    An optional description can be passed in for, well, descriptive
+    purposes.
+
+    The last optional parameter, client_args, should be a dictionary
+    containing some or all of the parameters that ofxclient.client.Client
+    takes.  This allows you to override the settings for a particular
+    institution
+    """
+    def __init__(self, id, org, url, username, password, broker_id='', description=None, client_args={} ):
+        self.id = id
+        self.org = org
+        self.url = url
+        self.broker_id = broker_id
+        self.username = username
+        self.password = password
+        self.description = description or self._default_description()
+        self.client_args = client_args
+
+    def client(self):
+        """Build a client for talking with the bank
+
+        It implicitly passes in the ``self.client_args`` that were passed
+        when instantiating this ``Institution``.
+
+        :rtype: :py:class:`ofxclient.client.Client`
+        """
+        settings = self.client_args
+        return Client(institution=self,**settings)
+
+    def local_id(self):
+        """Locally generated unique account identifier.
+
+        :rtype: string
+        """
+        return hashlib.sha256("%s%s" % (
+                self.id,
+                self.username )).hexdigest()
+
+    def _default_description(self):
+        return self.org
+
+    def authenticate(self,username=None,password=None):
+        """Test the authentication credentials
+
+        Raises a ``ValueError`` if there is a problem authenticating
+        with the human readable reason given by the institution.
+
+        :param username: optional username (use self.username by default)
+        :type username: string or None
+        :param password: optional password (use self.password by default)
+        :type password: string or None
+        """
+
+        u = username or self.username
+        p = password or self.password
+
+        client = self.client()
+        query  = client.authenticated_query(username=u,password=p)
+        res    = client.post(query)
+        ofx    = BeautifulStoneSoup(res)
+
+        sonrs = ofx.find('sonrs')
+        code = int(sonrs.find('code').contents[0].strip())
+
+        try:
+            status = sonrs.find('message').contents[0].strip()
+        except Exception:
+            status = ''
+
+        if code == 0:
+            return 1
+
+        raise ValueError(status)
+
+    def accounts(self):
+        """Ask the bank for the known accounts.
+        
+        :rtype: list of :py:class:`ofxclient.Account` objects
+        """
+        from ofxclient.account import Account
+        client  = self.client()
+        query   = client.account_list_query()
+        resp    = client.post(query)
+        resp_handle = StringIO.StringIO(resp)
+
+        accounts = []
+
+        for a in OfxParser.parse(resp_handle).accounts:
+            accounts.append( Account.from_ofxparse(a,institution=self) )
+        return accounts
+
+    def serialize(self):
+        """Serialize predictably for use in configuration storage.
+
+        Output looks like this::
+
+          {
+            'local_id':    'unique local identifier',
+            'id':          'FI Id',
+            'org':         'FI Org',
+            'url':         'FI OFX Endpoint Url',
+            'broker_id':   'FI Broker Id',
+            'username':    'Customer username',
+            'password':    'Customer password',
+            'description': 'descr',
+            'client_args': {
+                'id':          'random client id - see Client() for default',
+                'app_id':      'app name - see Client() for default',
+                'app_version': 'app version - see Client() for default',
+                'ofx_version': 'ofx version - see Client() for default',
+            }
+          }
+
+        :rtype: nested dictionary
+        """
+        client = self.client()
+        client_args = {
+                'id': client.id,
+                'app_id': client.app_id,
+                'app_version': client.app_version,
+                'ofx_version': client.ofx_version,
+        }
+        return {
+                'id': self.id,
+                'org': self.org,
+                'url': self.url,
+                'broker_id': self.broker_id,
+                'username': self.username,
+                'password': self.password,
+                'description': self.description,
+                'client_args': client_args,
+                'local_id': self.local_id()
+        }
+
+    @staticmethod
+    def deserialize(raw):
+        """Instantiate :py:class:`ofxclient.Institution` from dictionary
+
+        :param raw: serialized ``Institution``
+        :param type: dict as given by :py:method:`~ofxclient.Institution.serialize`
+        :rtype: subclass of :py:class:`ofxclient.Institution`
+        """
+        return Institution(
+                id = raw['id'],
+                org = raw['org'],
+                url = raw['url'],
+                broker_id = raw.get('broker_id',''),
+                username = raw['username'],
+                password = raw['password'],
+                description = raw.get('description',None),
+                client_args = raw.get('client_args',{}),
+        )
