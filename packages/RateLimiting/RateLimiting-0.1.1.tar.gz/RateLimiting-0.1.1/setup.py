ea10d996328216c1e5d564e3fe67be25998a81d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package RateLimiting/0.1.1/RateLimiting-0.1.1.tar.gz

---
+++ packages/RateLimiting/RateLimiting-0.1.1.tar.gz/RateLimiting-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='RateLimiting',
+    version='0.1.1',
+    description='Simple python rate limiting object',
+
+    author='Arnaud Porterie',
+    author_email='arnaud.porterie@gmail.com',
+    url='https://github.com/icecrime/RateLimiting',
+
+    py_modules=['ratelimiting'],
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
