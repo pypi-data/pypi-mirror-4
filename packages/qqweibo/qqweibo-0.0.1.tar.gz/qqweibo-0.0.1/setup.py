6dbb3139a2051d16e34fc33dd99a360a6798cd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:38 +0000

    Add package qqweibo/0.0.1/qqweibo-0.0.1.tar.gz

---
+++ packages/qqweibo/qqweibo-0.0.1.tar.gz/qqweibo-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+from distutils.core import setup  
+setup(name='qqweibo',  
+      author='TY(pythoner.net)',  
+      author_email='tianyu0915@gmail.com',  
+      version='0.0.1',  
+      description='qq微博的python SDK',  
+      keywords ='qq weibo SDK',
+      url='http://github.com/tianyu0915/qqweibo',  
+      packages=['qqweibo'],  
+      package_data={'qqweibo':['*.*']},
+
+)  
