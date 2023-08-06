9ed886d94dec3055f90f7814d89c98ad2b8b2e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:49 +0000

    Add package moo/0.1.8/moo-0.1.8.zip

---
+++ packages/moo/moo-0.1.8.zip/moo-0.1.8/setup.py
@@ -0,0 +1,45 @@
+"""
+moo
+~~~
+
+Moo is a mini markdown render server that provides preview of markdown
+files. It can automatically reload the preview in your broswer when the
+monitored file changes, which makes it suitable to live preview markdown
+in editors that does not provide this feature. Plugins can be
+easily written to interface with it.
+
+"""
+
+from setuptools import setup
+
+version = '0.1.8'
+
+print __doc__
+
+setup(
+	name='moo',
+	version=version,
+	description="Yet another markdown preview server.",
+	long_description=__doc__,
+    author="metaphysiks",
+    author_email="i@dingstyle.me",
+    keywords=("markdown", "pygments", "preview", "flask"),
+    url = "https://github.com/metaphysiks/moo",
+	packages=['moo'],
+	include_package_data=True,
+	install_requires=['Flask', 'pygments', 'misaka'],
+	zip_safe=False,
+	entry_points={
+        'console_scripts': ['moo = moo:run_server'],
+    },
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ]
+)
+
