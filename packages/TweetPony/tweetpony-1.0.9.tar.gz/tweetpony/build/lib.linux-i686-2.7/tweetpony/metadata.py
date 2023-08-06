13a18b7963cf20b01ff5b5942717044727566c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:30 +0000

    Add package TweetPony/1.0.9/tweetpony-1.0.9.tar.gz

---
+++ packages/TweetPony/tweetpony-1.0.9.tar.gz/tweetpony/build/lib.linux-i686-2.7/tweetpony/metadata.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2013 Julian Metzler
+# See the LICENSE file for the full license.
+
+name = "TweetPony"
+version = "1.0.9"
+description = "A Twitter library for Python"
+license = "AGPLv3"
+author = "Julian Metzler"
+author_email = "contact@mezgrman.de"
+dependencies = ['requests']
+url = "https://github.com/Mezgrman/TweetPony"
+keywords = "twitter library api wrapper pony"
