6d43ea3412f93d4136c4d72fe40ab44f4df52a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package django-simple-history/1.2.0/django-simple-history-1.2.0.tar.gz

---
+++ packages/django-simple-history/django-simple-history-1.2.0.tar.gz/django-simple-history-1.2.0/CHANGES.rst
@@ -0,0 +1,19 @@
+CHANGES
+=======
+
+1.2.0 (2013-04-21)
+------------------
+
+- Added tests
+- Added history view/revert feature in admin interface
+- Various fixes and improvements
+
+Oct 22, 2010
+------------
+
+- Merged setup.py from Klaas van Schelven - Thanks!
+
+Feb 21, 2010
+------------
+
+- Initial project creation, with changes to support ForeignKey relations.
---
+++ packages/django-simple-history/django-simple-history-1.2.0.tar.gz/django-simple-history-1.2.0/simple_history/tests/tests.py
@@ -0,0 +1,331 @@
+from datetime import datetime, timedelta
+from django import VERSION
+from django.test import TestCase
+from django_webtest import WebTest
+from django.core.files.base import ContentFile
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from .models import Poll, Choice, Restaurant, FileModel, Document
+
+
+today = datetime(2021, 1, 1, 10, 0)
+tomorrow = today + timedelta(days=1)
+
+
+def get_fake_file(filename):
+    fake_file = ContentFile('file data')
+    fake_file.name = filename
+    return fake_file
+
+
+class HistoricalRecordsTest(TestCase):
+
+    def assertDatetimesEqual(self, time1, time2):
+        self.assertAlmostEqual(time1, time2, delta=timedelta(seconds=2))
+
+    def assertRecordValues(self, record, klass, values_dict):
+        for key, value in values_dict.items():
+            self.assertEqual(getattr(record, key), value)
+        self.assertEqual(record.history_object.__class__, klass)
+        for key, value in values_dict.items():
+            if key != 'history_type':
+                self.assertEqual(getattr(record.history_object, key), value)
+
+    def test_create(self):
+        p = Poll(question="what's up?", pub_date=today)
+        p.save()
+        record, = p.history.all()
+        self.assertRecordValues(record, Poll, {
+            'question': "what's up?",
+            'pub_date': today,
+            'id': p.id,
+            'history_type': "+"
+        })
+        self.assertDatetimesEqual(record.history_date, datetime.now())
+
+    def test_update(self):
+        Poll.objects.create(question="what's up?", pub_date=today)
+        p = Poll.objects.get()
+        p.pub_date = tomorrow
+        p.save()
+        update_record, create_record = p.history.all()
+        self.assertRecordValues(create_record, Poll, {
+            'question': "what's up?",
+            'pub_date': today,
+            'id': p.id,
+            'history_type': "+"
+        })
+        self.assertRecordValues(update_record, Poll, {
+            'question': "what's up?",
+            'pub_date': tomorrow,
+            'id': p.id,
+            'history_type': "~"
+        })
+
+    def test_delete(self):
+        p = Poll.objects.create(question="what's up?", pub_date=today)
+        poll_id = p.id
+        p.delete()
+        delete_record, create_record = Poll.history.all()
+        self.assertRecordValues(create_record, Poll, {
+            'question': "what's up?",
+            'pub_date': today,
+            'id': poll_id,
+            'history_type': "+"
+        })
+        self.assertRecordValues(delete_record, Poll, {
+            'question': "what's up?",
+            'pub_date': today,
+            'id': poll_id,
+            'history_type': "-"
+        })
+
+    def test_save_without_historical_record(self):
+        pizza_place = Restaurant.objects.create(name='Pizza Place', rating=3)
+        pizza_place.rating = 4
+        pizza_place.save_without_historical_record()
+        pizza_place.rating = 6
+        pizza_place.save()
+        update_record, create_record = Restaurant.updates.all()
+        self.assertRecordValues(create_record, Restaurant, {
+            'name': "Pizza Place",
+            'rating': 3,
+            'id': pizza_place.id,
+            'history_type': "+",
+        })
+        self.assertRecordValues(update_record, Restaurant, {
+            'name': "Pizza Place",
+            'rating': 6,
+            'id': pizza_place.id,
+            'history_type': "~",
+        })
+
+    def test_foreignkey_field(self):
+        why_poll = Poll.objects.create(question="why?", pub_date=today)
+        how_poll = Poll.objects.create(question="how?", pub_date=today)
+        choice = Choice.objects.create(poll=why_poll, votes=0)
+        choice.poll = how_poll
+        choice.save()
+        update_record, create_record = Choice.history.all()
+        self.assertRecordValues(create_record, Choice, {
+            'poll_id': why_poll.id,
+            'votes': 0,
+            'id': choice.id,
+            'history_type': "+",
+        })
+        self.assertRecordValues(update_record, Choice, {
+            'poll_id': how_poll.id,
+            'votes': 0,
+            'id': choice.id,
+            'history_type': "~",
+        })
+
+    def test_file_field(self):
+        model = FileModel.objects.create(file=get_fake_file('name'))
+        self.assertEqual(model.file.name, 'files/name')
+        model.file.delete()
+        update_record, create_record = model.history.all()
+        self.assertEqual(create_record.file, 'files/name')
+        self.assertEqual(update_record.file, '')
+
+    def test_inheritance(self):
+        pizza_place = Restaurant.objects.create(name='Pizza Place', rating=3)
+        pizza_place.rating = 4
+        pizza_place.save()
+        update_record, create_record = Restaurant.updates.all()
+        self.assertRecordValues(create_record, Restaurant, {
+            'name': "Pizza Place",
+            'rating': 3,
+            'id': pizza_place.id,
+            'history_type': "+",
+        })
+        self.assertRecordValues(update_record, Restaurant, {
+            'name': "Pizza Place",
+            'rating': 4,
+            'id': pizza_place.id,
+            'history_type': "~",
+        })
+
+    def test_specify_history_user(self):
+        user1 = User.objects.create_user('user1', '1@example.com')
+        user2 = User.objects.create_user('user2', '1@example.com')
+        document = Document.objects.create(changed_by=user1)
+        document.changed_by = user2
+        document.save()
+        document.changed_by = None
+        document.save()
+        self.assertEqual([d.history_user for d in document.history.all()],
+                         [None, user2, user1])
+
+    def test_raw_save(self):
+        document = Document()
+        document.save_base(raw=True)
+        self.assertEqual(document.history.count(), 0)
+        document.save()
+        self.assertRecordValues(document.history.get(), Document, {
+            'changed_by_id': None,
+            'id': document.id,
+            'history_type': "~",
+        })
+
+
+class RegisterTest(TestCase):
+    def test_register_no_args(self):
+        self.assertEqual(len(Choice.history.all()), 0)
+        poll = Poll.objects.create(pub_date=today)
+        choice = Choice.objects.create(poll=poll, votes=0)
+        self.assertEqual(len(choice.history.all()), 1)
+
+    def test_register_separate_app(self):
+        get_history = lambda model: model.history
+        self.assertRaises(AttributeError, get_history, User)
+        self.assertEqual(len(User.histories.all()), 0)
+        user = User.objects.create(username='bob', password='pass')
+        self.assertEqual(len(User.histories.all()), 1)
+        self.assertEqual(len(user.histories.all()), 1)
+
+
+class HistoryManagerTest(TestCase):
+    def test_most_recent(self):
+        poll = Poll.objects.create(question="what's up?", pub_date=today)
+        poll.question = "how's it going?"
+        poll.save()
+        poll.question = "why?"
+        poll.save()
+        poll.question = "how?"
+        most_recent = poll.history.most_recent()
+        self.assertEqual(most_recent.__class__, Poll)
+        self.assertEqual(most_recent.question, "why?")
+
+    def test_most_recent_on_model_class(self):
+        Poll.objects.create(question="what's up?", pub_date=today)
+        self.assertRaises(TypeError, Poll.history.most_recent)
+
+    def test_most_recent_nonexistant(self):
+        # Unsaved poll
+        poll = Poll(question="what's up?", pub_date=today)
+        self.assertRaises(Poll.DoesNotExist, poll.history.most_recent)
+        # Deleted poll
+        poll.save()
+        poll.delete()
+        self.assertRaises(Poll.DoesNotExist, poll.history.most_recent)
+
+    def test_as_of(self):
+        poll = Poll.objects.create(question="what's up?", pub_date=today)
+        poll.question = "how's it going?"
+        poll.save()
+        poll.question = "why?"
+        poll.save()
+        poll.question = "how?"
+        most_recent = poll.history.most_recent()
+        self.assertEqual(most_recent.question, "why?")
+        times = [r.history_date for r in poll.history.all()]
+        question_as_of = lambda time: poll.history.as_of(time).question
+        self.assertEqual(question_as_of(times[0]), "why?")
+        self.assertEqual(question_as_of(times[1]), "how's it going?")
+        self.assertEqual(question_as_of(times[2]), "what's up?")
+
+    def test_as_of_on_model_class(self):
+        Poll.objects.create(question="what's up?", pub_date=today)
+        time = Poll.history.all()[0].history_date
+        self.assertRaises(TypeError, Poll.history.as_of, time)
+
+    def test_as_of_nonexistant(self):
+        # Unsaved poll
+        poll = Poll(question="what's up?", pub_date=today)
+        time = datetime.now()
+        self.assertRaises(Poll.DoesNotExist, poll.history.as_of, time)
+        # Deleted poll
+        poll.save()
+        poll.delete()
+        self.assertRaises(Poll.DoesNotExist, poll.history.as_of, time)
+
+
+def get_history_url(model, history_index=None):
+    info = model._meta.app_label, model._meta.module_name
+    if history_index is not None:
+        history = model.history.order_by('history_id')[history_index]
+        return reverse('admin:%s_%s_simple_history' % info,
+            args=[model.pk, history.history_id])
+    else:
+        return reverse('admin:%s_%s_history' % info, args=[model.pk])
+
+
+class AdminSiteTest(WebTest):
+    def setUp(self):
+        self.user = User.objects.create_superuser('u', 'u@example.com', 'pass')
+
+    def login(self):
+        form = self.app.get(reverse('admin:index')).form
+        form['username'] = self.user.username
+        form['password'] = 'pass'
+        return form.submit()
+
+    def test_history_list(self):
+        self.login()
+        poll = Poll.objects.create(question="why?", pub_date=today)
+        response = self.app.get(get_history_url(poll))
+        self.assertIn(get_history_url(poll, 0), response.content)
+        self.assertIn("Poll object", response.content)
+        self.assertIn("Created", response.content)
+
+    def test_history_form(self):
+        self.login()
+        poll = Poll.objects.create(question="why?", pub_date=today)
+        poll.question = "how?"
+        poll.save()
+
+        # Make sure form for initial version is correct
+        response = self.app.get(get_history_url(poll, 0))
+        self.assertEqual(response.form['question'].value, "why?")
+        self.assertEqual(response.form['pub_date_0'].value, "2021-01-01")
+        self.assertEqual(response.form['pub_date_1'].value, "10:00:00")
+
+        # Create new version based on original version
+        response.form['question'] = "what?"
+        response.form['pub_date_0'] = "2021-01-02"
+        response = response.form.submit()
+        self.assertEqual(response.status_code, 302)
+        if VERSION < (1, 4, 0):
+            self.assertTrue(response.headers['location']
+                            .endswith(get_history_url(poll)))
+        else:
+            self.assertTrue(response.headers['location']
+                            .endswith(reverse('admin:tests_poll_changelist')))
+
+        # Ensure form for second version is correct
+        response = self.app.get(get_history_url(poll, 1))
+        self.assertEqual(response.form['question'].value, "how?")
+        self.assertEqual(response.form['pub_date_0'].value, "2021-01-01")
+        self.assertEqual(response.form['pub_date_1'].value, "10:00:00")
+
+        # Ensure form for new third version is correct
+        response = self.app.get(get_history_url(poll, 2))
+        self.assertEqual(response.form['question'].value, "what?")
+        self.assertEqual(response.form['pub_date_0'].value, "2021-01-02")
+        self.assertEqual(response.form['pub_date_1'].value, "10:00:00")
+
+        # Ensure current version of poll is correct
+        poll = Poll.objects.get()
+        self.assertEqual(poll.question, "what?")
+        self.assertEqual(poll.pub_date, tomorrow)
+        self.assertEqual([p.history_user for p in Poll.history.all()],
+                         [self.user, None, None])
+
+    def test_history_user_on_save_in_admin(self):
+        self.login()
+
+        # Ensure polls created via admin interface save correct user
+        add_page = self.app.get(reverse('admin:tests_poll_add'))
+        add_page.form['question'] = "new poll?"
+        add_page.form['pub_date_0'] = "2012-01-01"
+        add_page.form['pub_date_1'] = "10:00:00"
+        changelist_page = add_page.form.submit().follow()
+        self.assertEqual(Poll.history.get().history_user, self.user)
+
+        # Ensure polls saved on edit page in admin interface save correct user
+        change_page = changelist_page.click("Poll object")
+        change_page.form.submit()
+        self.assertEqual([p.history_user for p in Poll.history.all()],
+                         [self.user, self.user])
