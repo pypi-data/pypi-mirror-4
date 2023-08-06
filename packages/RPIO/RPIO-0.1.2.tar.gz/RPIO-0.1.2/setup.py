602300410e2bd05771b470c7e8ee2999a7eadbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:05 +0000

    Add package RPIO/0.1.2/RPIO-0.1.2.tar.gz

---
+++ packages/RPIO/RPIO-0.1.2.tar.gz/RPIO-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+long_desc = read('README.md')
+
+setup(
+    install_requires=['RPi.GPIO'],
+    name="RPIO",
+    packages=['RPIO'],
+    version="0.1.2",
+    description="An extension of RPi.GPIO to easily use interrupts on the Raspberry Pi",
+    long_description=long_desc,
+    url="https://github.com/metachris/raspberrypi-utils",
+
+    author="Chris Hager",
+    author_email="chris@linuxuser.at",
+
+    license="MIT",
+    keywords=["raspberry", "raspberry pi", "interrupts", "gpio", "gpio2"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Utilities",
+        "Topic :: Software Development",
+        "Topic :: Home Automation",
+        "Topic :: System :: Hardware",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
