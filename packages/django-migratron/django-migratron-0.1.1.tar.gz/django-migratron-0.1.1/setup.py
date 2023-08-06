97fb74fee53d63d489d641cd5a084ec0c1adf21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:55 +0000

    Add package django-migratron/0.1.1/django-migratron-0.1.1.tar.gz

---
+++ packages/django-migratron/django-migratron-0.1.1.tar.gz/django-migratron-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='django-migratron',
+    version='0.1.1',
+    author='Chase Seibert',
+    author_email='chase.seibert@gmail.com',
+    packages=['migratron'],
+    url='https://github.com/chase-seibert/django-migratron',
+    download_url='https://github.com/chase-seibert/django-migratron/tarball/master',
+    license='LICENSE.txt',
+    description='Create and run different buckets of unordered schema and data migrations.',
+    requires=[
+        'yaml',
+        'yamlfield',
+        'termcolor',
+    ],
+)
