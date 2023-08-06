1a24d2b9cf38573ca70a90be6bd674ab4b1a75df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:48 +0000

    Add package shrine/0.0.24/shrine-0.0.24.tar.gz

---
+++ packages/shrine/shrine-0.0.24.tar.gz/shrine-0.0.24/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+import shrine
+
+
+def get_packages():
+    packages = []
+    for root, dirnames, filenames in os.walk('shrine'):
+        if '__init__.py' in filenames:
+            packages.append(".".join(os.path.split(root)).strip("."))
+
+    return packages
+
+required_modules = [
+    'tornado',
+    'django',
+    'requests',
+    'dj_database_url',
+    'envoy',
+    'couleur',
+]
+
+setup(
+    name='shrine',
+    version=shrine.version,
+    description='Tornado + Django',
+    author=u'Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://falcao.it/shrine',
+    packages=get_packages(),
+    install_requires=required_modules,
+    entry_points={
+        'console_scripts': ['shrine = shrine.bin:main'],
+    },
+    package_data={
+        'shrine': [
+            'skel/*/*/*.*',
+            'skel/*/*.*',
+            'skel/*.*',
+            'skel/Procfile',
+            'skel/.gitignore',
+        ],
+    },
+)
---
+++ packages/shrine/shrine-0.0.24.tar.gz/shrine-0.0.24/shrine/cmds/run.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import logging
+
+from glob import glob
+from os.path import join, basename, splitext
+from .registry import Command
+
+from tornado.ioloop import IOLoop
+from shrine.loader import Module
+from tornado import autoreload
+
+
+class RunProject(Command):
+    shell = 'run'
+
+    current_dir_name = basename(os.getcwdu())
+
+    def get_settings(self):
+        os.environ['SHRINE_SETTINGS_MODULE'] = '{}.settings'.format(basename(os.getcwdu()))
+        from shrine.conf import settings
+        return settings
+
+    def run(self, args):
+        settings = self.get_settings()
+
+        for name in map(basename, glob(join('controllers', '*.py'))):
+            autoreload.watch(name)
+            Module.load(self.get_controller_import_path(name))
+
+        from shrine.routes import make_application
+
+        MESSAGE = "{} running at http://localhost:{}\n".format(
+            settings.PRODUCT_NAME,
+            settings.PORT,
+        )
+
+        application = make_application()
+        application.listen(settings.PORT)
+        self.log.bold_green(MESSAGE)
+
+        from shrine.log import logger
+        logger.setLevel(logging.WARNING)
+
+        try:
+            autoreload.start()
+            IOLoop.instance().start()
+        except KeyboardInterrupt:
+            self.log.bold_red_on_black("\rInterrupted by the User (Control-C)\n")
+
+    def get_controller_import_path(self, name):
+        return '{}.controllers.{}'.format(self.current_dir_name,
+                                          self.remove_extension(name))
+
+    def remove_extension(self, name):
+        return splitext(name)[0]
+
+
+class RunInProduction(RunProject):
+    shell = 'run:production'
+
+    def run(self, args):
+        settings = self.get_settings()
+        if not settings.DEBUG:
+            return super(RunInProduction, self).run(args)
+
+        self.log.bold_red("Cannot run in production because settings.DEBUG is True")
+        raise SystemExit(1)
