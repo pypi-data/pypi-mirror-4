54568d2ff1d85be965efbc12efa8bcef4af4847b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:29 +0000

    Add package eusful/0.2/eusful-0.2.zip

---
+++ packages/eusful/eusful-0.2.zip/eusful-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='eusful',
+      version='0.2',
+      author='Eugene Efremov',
+      author_email='eaefremov@gmail.com',
+      license='MIT',
+      description='These are things I use in places.',
+      py_modules=['data', 'errors', 'for_web'])
