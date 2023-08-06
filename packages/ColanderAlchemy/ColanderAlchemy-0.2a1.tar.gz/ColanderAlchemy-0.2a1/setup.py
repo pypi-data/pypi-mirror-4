5efdaa278b0e8e89928924b9e6f5ba9970d3139f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:06 +0000

    Add package ColanderAlchemy/0.2a1/ColanderAlchemy-0.2a1.tar.gz

---
+++ packages/ColanderAlchemy/ColanderAlchemy-0.2a1.tar.gz/ColanderAlchemy-0.2a1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.2'
+
+setup(name='ColanderAlchemy',
+      version=version,
+      description="Autogenerate Colander schemas based on SQLAlchemy models.",
+      long_description=open('README.rst').read(),
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Database'],
+      keywords='serialize deserialize validate schema validation colander sqlalchemy',
+      author='Stefano Fontanelli',
+      author_email='s.fontanelli@asidev.com',
+      url='https://github.com/stefanofontanelli/ColanderAlchemy',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            'colander >= 0.9.8',
+            'SQLAlchemy >= 0.8dev'],
+      tests_require=[],
+      test_suite='tests',
+      )
