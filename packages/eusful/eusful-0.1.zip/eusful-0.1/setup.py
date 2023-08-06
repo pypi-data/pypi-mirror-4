41b307590087fbb8ad9251910780fb39fcb4064b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:30 +0000

    Add package eusful/0.1/eusful-0.1.zip

---
+++ packages/eusful/eusful-0.1.zip/eusful-0.1/setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import setup, find_packages
+readme = open('README.txt').read()
+setup(name='eusful',
+      version='0.1',
+      author='Eugene Efremov',
+      author_email='eaefremov@gmail.com',
+      license='MIT',
+      description='This is a small package of useful functions I do not want to rewrite or copy-paste.',
+      long_description=readme,
+      packages=find_packages())
