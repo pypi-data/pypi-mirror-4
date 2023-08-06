6645b30a3e8959de96ea4336f4c4ee6b38a1bb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:26 +0000

    Add package sqlbuilder/0.7.2/sqlbuilder-0.7.2.tar.gz

---
+++ packages/sqlbuilder/sqlbuilder-0.7.2.tar.gz/sqlbuilder-0.7.2/sqlbuilder/smartsql/dialects.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, unicode_literals
+from . import (Condition, Name, sql_dialects, sqlrepr)
+
+TRANSLATION_MAP = {
+    'sqlite': {
+        'LIKE': 'GLOB',
+        'ILIKE': 'LIKE',
+    },
+    'mysql': {
+        'LIKE': 'LIKE BINARY',
+        'ILIKE': 'LIKE',
+    }
+}
+
+
+@sql_dialects.register('sqlite', Condition)
+@sql_dialects.register('mysql', Condition)
+def condition_sqlrepr(self, dialect):
+    """Translates operators for sqlbuilder.smartsql.Condition"""
+    s1 = sqlrepr(self._expr1, dialect)
+    s2 = sqlrepr(self._expr2, dialect)
+    op = TRANSLATION_MAP.get(dialect, {}).get(self._op, self._op)
+    if not s1:
+        return s2
+    if not s2:
+        return s1
+    return "{0} {1} {2}".format(s1, op, s2)
+
+
+@sql_dialects.register('sqlite', Name)
+@sql_dialects.register('mysql', Name)
+def name_sqlrepr(self, dialect):
+    return self._sqlrepr_base('`', dialect)
