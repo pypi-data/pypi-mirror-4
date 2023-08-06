6b74c241cea9588356d404014b3e8a08488d9828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package feedly/0.2.9/feedly-0.2.9.zip

---
+++ packages/feedly/feedly-0.2.9.zip/feedly-0.2.9/feedly/tasks.py
@@ -0,0 +1,46 @@
+from celery import task
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@task.task()
+def fanout_love_feedly(feedly, user, following_group, operation, *args, **kwargs):
+    '''
+    Simple task wrapper for _fanout task
+    Just making sure code is where you expect it :)
+    '''
+    logger.info(u'fanning out for user %s', user.username)
+    feeds = feedly._fanout_task(
+        user, following_group, operation, *args, **kwargs)
+
+
+@task.task()
+def follow_many(feedly, user_id, follower_user_ids):
+    '''
+    Simple task wrapper for follow_many
+    Just making sure code is where you expect it :)
+    '''
+    logger.info(u'following many for user id %s', user_id)
+    feed = feedly._follow_many_task(user_id, follower_user_ids)
+
+
+@task.task(queue_name='notification_add_love')
+def notification_add_love(love):
+    from feedly.feed_managers.notification_feedly import NotificationFeedly
+    feedly = NotificationFeedly()
+    feedly._add_love(love)
+
+
+@task.task(queue_name='notification_follow')
+def notification_follow(follow):
+    from feedly.feed_managers.notification_feedly import NotificationFeedly
+    feedly = NotificationFeedly()
+    feedly._follow(follow)
+
+
+@task.task(queue_name='notification_add_to_list')
+def notification_add_to_list(list_item):
+    from feedly.feed_managers.notification_feedly import NotificationFeedly
+    feedly = NotificationFeedly()
+    feedly._add_to_list(list_item)
