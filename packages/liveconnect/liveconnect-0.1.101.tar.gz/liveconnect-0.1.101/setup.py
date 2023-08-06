0c390e094ce60f647437182527d9c8a82b9177d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:08 +0000

    Add package liveconnect/0.1.101/liveconnect-0.1.101.tar.gz

---
+++ packages/liveconnect/liveconnect-0.1.101.tar.gz/liveconnect-0.1.101/setup.py
@@ -0,0 +1,18 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+	name = "liveconnect",
+	version = "0.1.101",
+	packages = find_packages(),
+	install_requires = ['requests'],
+	package_data = {
+		'' : ['*.md']
+	},
+	author = "Samuel Lucidi",
+	author_email = "mansam@csh.rit.edu",
+	description = "LiveConnect OAuth adapter for Python",
+	license = "MIT",
+	keywords = "liveconnect skydrive",
+	url = "http://github.com/mansam/liveconnect"
+)
