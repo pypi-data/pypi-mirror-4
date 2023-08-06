d74082ec308c4ba6a1a6a0e07d5cf51cf95b9318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:48 +0000

    Add package galleryserve/0.1.3/galleryserve-0.1.3.tar.gz

---
+++ packages/galleryserve/galleryserve-0.1.3.tar.gz/galleryserve-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='galleryserve',
+    version='0.1.3',
+    description='Adapted from imageserve to fit broader gallery needs such as video embedding and content for controls.',
+
+    author='Imaginary Landscape',
+    author_email='dbertrand@imagescape.com',
+
+    install_requires=(
+    ),
+
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(exclude=('ez_setup', 'examples', 'tests')),
+)
