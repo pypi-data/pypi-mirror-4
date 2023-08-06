db804f5e68998de224c87e83eeb15eee2853deaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:50 +0000

    Add package django-shortuuidfield/0.1.2/django-shortuuidfield-0.1.2.tar.gz

---
+++ packages/django-shortuuidfield/django-shortuuidfield-0.1.2.tar.gz/django-shortuuidfield-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-shortuuidfield',
+    version='0.1.2',
+    author='Ben Roberts',
+    author_email='roberts81@gmail.com',
+    description='Short UUIDField for Django.  Good for use in urls & file names.  (Base 57, 22 characters)',
+    url='https://github.com/nebstrebor/django-shortuuidfield',
+    zip_safe=False,
+    install_requires=[
+        'django',
+        'shortuuid'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
