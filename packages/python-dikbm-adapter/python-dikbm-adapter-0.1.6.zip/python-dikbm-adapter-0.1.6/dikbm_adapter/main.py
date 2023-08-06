8f69dbe74ad85b6ff1e016e5fa1384bf22326de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:35 +0000

    Add package python-dikbm-adapter/0.1.6/python-dikbm-adapter-0.1.6.zip

---
+++ packages/python-dikbm-adapter/python-dikbm-adapter-0.1.6.zip/python-dikbm-adapter-0.1.6/dikbm_adapter/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from lockfile import FileLock, AlreadyLocked, LockFailed
+
+from client import DiKBMClient
+from settings import settings
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logger.info('Starting DiKBM python client')
+    try:
+        client = DiKBMClient()
+    except:
+        logger.exception('Connect Error')
+    else:
+        lock = FileLock("dikbm")
+        try:
+            lock.acquire(0)
+        except AlreadyLocked:
+            logger.info('lock %s already locked' % lock.unique_name)
+        except LockFailed:
+            logger.error('lock %s cant be locked' % lock.unique_name)
+        else:
+            logger.debug('lock %s acquired' % lock.unique_name)
+            try:
+                client.proceed_in()
+                client.proceed_status()
+            except:
+                logger.exception('Proceed Error')
+            finally:
+                lock.release()
+                logger.debug('lock %s released' % lock.unique_name)
+    finally:
+        logger.info('Finished DiKBM python client')
+
+if __name__ == '__main__':
+    main()
