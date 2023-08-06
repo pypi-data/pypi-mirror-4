44206e5b4a8620127f42b6ba04108852ce4f3976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:47 +0000

    Add package mjson/0.1/mjson-0.1.tar.gz

---
+++ packages/mjson/mjson-0.1.tar.gz/mjson-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup (
+    name='mjson',
+    version='0.1',
+    description='Convenient "python -mjson.tools"',
+    author='kjirou',
+    author_email='sorenariblog@gmail.com',
+    url='http://blog.kjirou.net/',
+    packages=find_packages(),
+)
