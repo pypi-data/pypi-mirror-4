e4a70898c30ee0afbe9446dcf2b3c93f5f581ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:52 +0000

    Add package WTForms-Components/0.4.6/WTForms-Components-0.4.6.tar.gz

---
+++ packages/WTForms-Components/WTForms-Components-0.4.6.tar.gz/WTForms-Components-0.4.6/wtforms_components/__init__.py
@@ -0,0 +1,31 @@
+from wtforms import Form
+from .fields import (
+    NumberRangeField,
+    SelectField,
+    SelectMultipleField,
+    PhoneNumberField
+)
+from .validators import DateRange, Unique, If, Chain
+
+
+__all__ = (
+    Chain,
+    DateRange,
+    If,
+    NumberRangeField,
+    PhoneNumberField,
+    SelectField,
+    SelectMultipleField,
+    Unique,
+)
+
+
+class ModelForm(Form):
+    """
+    Simple ModelForm, use this if your form needs to use the Unique validator
+    """
+    def __init__(self, formdata=None, obj=None, prefix='', **kwargs):
+        Form.__init__(
+            self, formdata=formdata, obj=obj, prefix=prefix, **kwargs
+        )
+        self._obj = obj
