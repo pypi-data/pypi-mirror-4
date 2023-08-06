55bc77cabaf4e72eec37f85451b98b9f070072e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:54 +0000

    Add package django-geojson-tiles/0.1.2/django-geojson-tiles-0.1.2.tar.gz

---
+++ packages/django-geojson-tiles/django-geojson-tiles-0.1.2.tar.gz/django-geojson-tiles-0.1.2/geojson_tiles/serializers.py
@@ -0,0 +1,226 @@
+"""
+Adapted from @jeffkistler's geojson serializer at: https://gist.github.com/967274
+"""
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+import datetime
+import decimal
+import types
+import json
+
+from django.db.models.base import Model
+from django.db.models.query import QuerySet, ValuesQuerySet
+from django.core.serializers.python import Serializer as PythonSerializer
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers.base import SerializationError
+from django.utils.encoding import is_protected_type, smart_unicode
+from django.utils import datetime_safe
+from django.contrib.gis.geos.geometry import GEOSGeometry
+from django.contrib.gis.db.models.fields import GeometryField
+
+
+class DjangoGeoJSONEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, GEOSGeometry):
+            return json.loads(o.geojson)
+        else:
+            return super(DjangoGeoJSONEncoder, self).default(o)
+
+
+class GeoJSONSerializer(PythonSerializer):
+    def start_serialization(self):
+        self.feature_collection = {"type": "FeatureCollection", "features": []}
+        self.feature_collection["crs"] = self.get_crs()
+
+        bbox = self.options.pop('bbox', None)
+        if bbox:
+            self.feature_collection["bbox"] = bbox[0][1:]
+
+        self._current = None
+
+    def get_crs(self):
+        if self.crs == False:
+            return None
+        crs = {}
+        srid = self.options.get("srid", "4326")
+
+        crs["type"] = "link"
+        properties = {}
+        properties["href"] = "http://spatialreference.org/ref/epsg/%s/" % (str(srid))
+        properties["type"] = "proj4"
+        crs["properties"] = properties
+        return crs
+
+    def start_object(self, obj):
+        self._current = {"type": "Feature", "properties": {}}
+
+        # Try to determine the primary key from the obj
+        # self.primary_key can be a function (callable on obj), or a string
+        # if self.primary_key is not set, use obj.pk if obj is a Model
+        # otherwise the primary key will not be used
+        primary_key = None
+        if self.primary_key and hasattr(self.primary_key, '__call__'):
+            primary_key = self.primary_key(obj)
+        elif self.primary_key and isinstance(self.primary_key, basestring):
+            if isinstance(obj, Model):
+                primary_key = getattr(obj, self.primary_key)
+            else:
+                primary_key = obj[self.primary_key]
+        elif isinstance(obj, Model):
+            primary_key = obj.pk
+
+        if primary_key:
+            self._current['id'] = primary_key
+
+    def end_object(self, obj):
+        self.feature_collection["features"].append(self._current)
+        self._current = None
+
+    def end_serialization(self):
+        self.options.pop('stream', None)
+        self.options.pop('properties', None)
+        self.options.pop('primary_key', None)
+        self.options.pop('geometry_field', None)
+        self.options.pop('use_natural_keys', None)
+        self.options.pop('crs', None)
+        self.options.pop('srid', None)
+
+        json.dump(self.feature_collection, self.stream, cls=DjangoGeoJSONEncoder, **self.options)
+
+    def handle_field(self, obj, field_name):
+        if isinstance(obj, Model):
+            value = getattr(obj, field_name)
+        elif isinstance(obj, dict):
+            value = obj[field_name]
+        else:
+            # Only supports dicts and models, not lists (e.g. values_list)
+            return
+
+        # ignore other geometries, only one geometry per feature
+        if field_name == self.geometry_field:
+            # this will handle GEOSGeometry objects and string representations (e.g. ewkt, bwkt)
+            try:
+                self._current['geometry'] = GEOSGeometry(value)
+            # if the geometry couldn't be parsed, we can't generate valid geojson
+            except ValueError:
+                raise SerializationError('The field ["%s", "%s"] could not be parsed as a valid geometry' % (
+                    self.geometry_field, value
+                ))
+
+        elif self.properties and \
+            field_name in self.properties:
+            # set the field name to the key's value mapping in self.properties
+            if isinstance(self.properties, dict):
+                property_name = self.properties[field_name]
+                self._current['properties'][property_name] = value
+            else:
+                self._current['properties'][field_name] = value
+
+        elif not self.properties:
+            self._current['properties'][field_name] = value            
+
+    def getvalue(self):
+        if callable(getattr(self.stream, 'getvalue', None)):
+            return self.stream.getvalue()
+
+    def handle_fk_field(self, obj, field):
+        related = getattr(obj, field.name)
+        if related is not None:
+            if self.use_natural_keys and hasattr(related, 'natural_key'):
+                related = related.natural_key()
+            else:
+                if field.rel.field_name == related._meta.pk.name:
+                    # Related to remote object via primary key
+                    related = related._get_pk_val()
+                else:
+                    # Related to remote object via other field
+                    related = smart_unicode(getattr(related, field.rel.field_name), strings_only=True)
+        self._current['properties'][field.name] = related
+
+    def handle_m2m_field(self, obj, field):
+        if field.rel.through._meta.auto_created:
+            if self.use_natural_keys and hasattr(field.rel.to, 'natural_key'):
+                m2m_value = lambda value: value.natural_key()
+            else:
+                m2m_value = lambda value: smart_unicode(value._get_pk_val(), strings_only=True)
+            self._current['properties'][field.name] = [m2m_value(related)
+                               for related in getattr(obj, field.name).iterator()]
+
+    def serialize_values_queryset(self, queryset):
+        for obj in queryset:
+            self.start_object(obj)
+
+            # handle the geometry field
+            self.handle_field(obj, self.geometry_field)
+
+            for field_name in obj:
+                if not field_name in obj:
+                    continue
+                if self.properties is None or field_name in self.properties:
+                    self.handle_field(obj, field_name)
+
+            self.end_object(obj)
+
+    def serialize_queryset(self, queryset):
+        opt = queryset.model._meta
+        local_fields = queryset.model._meta.local_fields
+        many_to_many_fields = queryset.model._meta.many_to_many
+
+        # populate each queryset obj as a feature
+        for obj in queryset:
+            self.start_object(obj)
+
+            # handle the geometry field
+            self.handle_field(obj, self.geometry_field)
+
+            # handle the property fields
+            for field in local_fields:
+                # don't include the pk in the properties
+                # as it is in the id of the feature
+                if field.name == queryset.model._meta.pk.name:
+                    continue
+                # ignore other geometries
+                if isinstance(field, GeometryField):
+                    continue
+
+                if field.serialize or field.primary_key:
+                    if field.rel is None:
+                        if self.properties is None or field.attname in self.properties:
+                            self.handle_field(obj, field.name)
+                    else:
+                        if self.properties is None or field.attname[:-3] in self.properties:
+                            self.handle_fk_field(obj, field)
+            for field in many_to_many_fields:
+                if field.serialize:
+                    if self.properties is None or field.attname in self.properties:
+                        self.handle_m2m_field(obj, field)
+            self.end_object(obj)
+
+    def serialize(self, queryset, **options):
+        """
+        Serialize a queryset.
+        """
+        self.options = options
+
+        self.stream = options.get("stream", StringIO())
+        self.primary_key = options.get("primary_key", None)
+        self.properties = options.get("properties")
+        self.geometry_field = options.get("geometry_field")
+        self.use_natural_keys = options.get("use_natural_keys", False)
+        self.bbox = options.get("bbox", None)
+        self.crs = options.get("crs", True)
+
+        self.start_serialization()
+        
+        if isinstance(queryset, ValuesQuerySet):
+            self.serialize_values_queryset(queryset)
+
+        elif isinstance(queryset, QuerySet):
+            self.serialize_queryset(queryset)
+
+        self.end_serialization()
+        return self.getvalue()
---
+++ packages/django-geojson-tiles/django-geojson-tiles-0.1.2.tar.gz/django-geojson-tiles-0.1.2/geojson_tiles/views.py
@@ -0,0 +1,97 @@
+from django.contrib.gis.geos import Polygon
+from django.http import HttpResponse, HttpResponseServerError
+from django.contrib.gis.db.models import GeometryField, MultiPointField, PointField
+from serializers import GeoJSONSerializer
+import ModestMaps
+import TileStache
+
+
+class GeoJSONTile:
+    width = 256
+    height = 256
+
+    # projection settings
+    projection = TileStache.Geography.SphericalMercator()
+    provider = ModestMaps.OpenStreetMap.Provider()
+    srid = 4326
+
+    serializer = GeoJSONSerializer()
+
+    def coords_to_bbox_mmap(self, z, x, y):
+        # set up bounding box from coord
+        coord = ModestMaps.Core.Coordinate(y, x, z)
+        tl = self.projection.coordinateLocation(coord)
+        br = self.projection.coordinateLocation(coord.right().down())
+        bbox = Polygon.from_bbox((tl.lon, tl.lat, br.lon, br.lat))
+        bbox.srid = self.srid
+
+        modest_map = ModestMaps.mapByExtentZoom(self.provider, tl, br, z)
+        return bbox, modest_map
+
+    def pre_serialization(self, queryset, z, x, y, bbox):
+        """
+        Hook to modify queryset before serialization
+        """
+        return queryset
+
+    def post_serialization(self, geojson, z, x, y, bbox):
+        """
+        Hook to modify geojson after serialization
+        """
+        return geojson
+
+    def __init__(self, model, geometry_field=None, trim_to_boundary=True, properties=None, primary_key=None):
+        self.model = model
+        self.geometry_field = geometry_field
+        self.trim_to_boundary = trim_to_boundary
+        self.properties = properties
+        self.primary_key = primary_key
+
+        # if geometry field name is not specified,
+        # use the first GeometryField that is found
+        if self.geometry_field == None and self.model != None:
+            try:
+                field = [f for f in self.model._meta.fields if isinstance(f, GeometryField)][0]
+                self.geometry_field = field.name
+            except IndexError:
+                pass
+
+    def __call__(self, request, z, x, y):
+        z = int(z)
+        x = int(x)
+        y = int(y)
+
+        if self.geometry_field == None:
+            return HttpResponseServerError('No geometry was specified or the model "%s" did not have a GeometryField present' % (self.model._meta.object_name))
+
+        bbox, modest_map = self.coords_to_bbox_mmap(z, x, y)
+
+        shapes = self.model.objects.filter(**{
+            '%s__intersects' % self.geometry_field: bbox
+        })
+
+        # Can't trim point geometries to a boundary
+        self.trim_to_boundary = self.trim_to_boundary \
+            and not isinstance(shapes.model._meta.get_field(self.geometry_field), PointField) \
+            and not isinstance(shapes.model._meta.get_field(self.geometry_field), MultiPointField) \
+
+        if self.trim_to_boundary:
+            shapes = shapes.intersection(bbox)
+            geometry_field = 'intersection'
+        else:
+            geometry_field = self.geometry_field
+
+        serializer_options = {
+            'bbox': bbox,
+            'geometry_field': geometry_field,
+            'srid': self.srid,
+        }
+        if self.properties:
+            serializer_options.update(properties=self.properties)
+        if self.primary_key:
+            serializer_options.update(primary_key=self.primary_key)
+
+        shapes = self.pre_serialization(shapes, z, x, y, bbox)
+        data = self.serializer.serialize(shapes, **serializer_options)
+        data = self.post_serialization(data, z, x, y, bbox)
+        return HttpResponse(data, content_type='application/json')
