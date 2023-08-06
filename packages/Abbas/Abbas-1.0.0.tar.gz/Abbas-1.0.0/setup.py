ec691defaa9689cc9b945d4c210fb7a446b6c503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:11 +0000

    Add package Abbas/1.0.0/Abbas-1.0.0.tar.gz

---
+++ packages/Abbas/Abbas-1.0.0.tar.gz/Abbas-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name ='Abbas',
+	version ='1.0.0',
+	py_modules=['Abbas'],
+	author='abbas',
+	author_email='abbasalizaidi1990@gmail.com',
+	description='A simple printer of lists'
+     )
+
