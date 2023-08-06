71d4a49ba3e7203ed3ad062838312efaed5e7a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:24 +0000

    Add package python_tuto/1.0.0/python_tuto-1.0.0.tar.gz

---
+++ packages/python_tuto/python_tuto-1.0.0.tar.gz/python_tuto-1.0.0/python_tuto.py
@@ -0,0 +1,13 @@
+
+""" This module is named python module.py and bring you function print_lol. In this function, you can find list
+      type element and show them as data."""
+
+def print_lol(the_list, level):
+    for each_item in the_list :
+        if isinstance(each_item, list) :
+            print_lol(each_item)
+        else :
+            for tab_stop in range(level) :
+                print("\t",end='')
+            print(each_item)
+
