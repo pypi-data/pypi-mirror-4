99592aa5e9cdb5e9de5f18dda6b169b8a32b6d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package pypmserver/0.1.65/pypmserver-0.1.65.zip

---
+++ packages/pypmserver/pypmserver-0.1.65.zip/pypmserver-0.1.65/pypmserver/info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding:utf-8 vi:et:ts=2
+
+# pypmserver information.
+# Copyright 2013 Grigory Petrov
+# See LICENSE for details.
+
+import os
+import pkg_resources
+
+NAME_SHORT = "pypmserver"
+VER_MAJOR = 0
+VER_MINOR = 1
+try :
+  VER_TXT = pkg_resources.require( NAME_SHORT )[ 0 ].version
+##  Installing via 'setup.py develop'?
+except pkg_resources.DistributionNotFound :
+  VER_BUILD = 0
+  VER_TXT = ".".join( map( str, [ VER_MAJOR, VER_MINOR, VER_BUILD ] ) )
+DIR_THIS = os.path.dirname( os.path.abspath( __file__ ) )
+NAME_FULL = "Pypm standalone server."
+DESCR = """
+{s_name_short} v. {s_ver_txt}\\n\\n
+Simple standalone server for .pypm packages.
+""".replace( '\n', '' ).replace( '\\n', '\n' ).strip().format(
+  s_name_short = NAME_SHORT,
+  s_ver_txt = VER_TXT )
+
+DB_FILE_NAME = '.{0}.sqlite'.format( NAME_SHORT )
+DB_FILE_PATH = os.path.join( os.path.expanduser( '~' ), DB_FILE_NAME )
+##  'realm' for HTTP digest authentication.
+AUTH_REALM = 'pypmserver'
+
