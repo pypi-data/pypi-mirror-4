d0cec27dd1bc2d515f88e5f3a653bb697b847324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:54 +0000

    Add package yhat/0.0.2/yhat-0.0.2.tar.gz

---
+++ packages/yhat/yhat-0.0.2.tar.gz/yhat-0.0.2/yhat/pml.py
@@ -0,0 +1,33 @@
+import pickle
+import requests
+import json
+import urllib
+import pprint as pp
+import inspect
+        
+
+class BaseModel(object):
+    """
+    BaseModel is the standard Yhat model class that gives your model
+    the functionality to run it as a RESTful API. Once you create your own
+    model, you need to implement the following functions:
+         - transform
+         - predict
+    """
+    def __init__(self, **kwargs):
+        for kw, arg in kwargs.iteritems():
+            setattr(self, kw, arg)
+
+    def transform(self, rawData):
+        """
+        Transform takes the raw data that's going to be sent to your yhat API and
+        converts it into the format required to be run through your model.
+        """
+        return rawData
+
+    def predict(self, transformedData):
+        """
+        Predict executes your predictive model, formats the data into response, and
+        returns it.
+        """
+        return self.clf.predict(transformedData)
