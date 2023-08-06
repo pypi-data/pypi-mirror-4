c7986959c60fa09d5b6a3f102ef24383328702ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package szq/1.1.0/szq-1.1.0.zip

---
+++ packages/szq/szq-1.1.0.zip/szq-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'szq',
+        version = '1.1.0',
+        py_modules = ['szq'],
+        author = 'Shi',
+        author_email = 'sdsdf',
+        url = 'http://www.szq.com',
+        description = 'test',
+    )
---
+++ packages/szq/szq-1.1.0.zip/szq-1.1.0/szq.py
@@ -0,0 +1,2 @@
+def myFun(arg):
+    print(arg)
