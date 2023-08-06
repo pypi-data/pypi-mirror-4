05f607f410634ac03801e3e3238bcca9fb46d3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:43 +0000

    Add package AlienFeed/0.1.2/AlienFeed-0.1.2.tar.gz

---
+++ packages/AlienFeed/AlienFeed-0.1.2.tar.gz/AlienFeed-0.1.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='AlienFeed',
+      version='0.1.2',
+      description='AlienFeed is a command line application made for displaying and interacting with Reddit submissions.',
+      author='Jared Wright',
+      license='MIT',
+      keywords = "AlienFeed alien reddit feed rss tool cli",
+      author_email='jawerty210@gmail.com',
+      url='http://github.com/jawerty/AlienFeed',
+      scripts=['alienfeed/alien.py'],
+      install_requires=['praw'],
+      entry_points = {
+        'console_scripts': [
+            'alienfeed = alien:main'
+        ],
+	  }
+     )
