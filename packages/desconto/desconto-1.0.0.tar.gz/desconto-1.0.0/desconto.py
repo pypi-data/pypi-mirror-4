63fa887b17e413ecb7afc09ca840e16289b3797c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:15 +0000

    Add package desconto/1.0.0/desconto-1.0.0.tar.gz

---
+++ packages/desconto/desconto-1.0.0.tar.gz/desconto-1.0.0/desconto.py
@@ -0,0 +1,3 @@
+'''Método para conceder desconto em um valor por porcentagem'''
+def descontoOrcamento(valor, porcentagem):
+	return (valor - (valor * (porcentagem/100)))
---
+++ packages/desconto/desconto-1.0.0.tar.gz/desconto-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'desconto',
+	version		= '1.0.0',
+	py_modules	= ['desconto'],
+	author		= 'drissmello',
+	author_email	= 'drisspring@hotmail.com',
+	url		= 'http://br.bing.com/search?q=driss',
+	description	= 'Retorna um valor com desconto.',
+     )
