c13305f1210ca8bca7b7026268e51c3cf361244e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:50 +0000

    Add package gitinit/1.0.0/gitinit-1.0.0.tar.gz

---
+++ packages/gitinit/gitinit-1.0.0.tar.gz/gitinit-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = "gitinit",
+    version = "1.0.0",
+    author = "Bibhas C Debnath",
+    author_email = "me@bibhas.in",
+    description = ("Initiates git with gitignore for provided language"),
+    license = "LGPL",
+    keywords = "example documentation tutorial",
+    url = "https://github.com/iambibhas/gitinit",
+    packages=find_packages(),
+    long_description=read('README.txt'),
+    package_data={'gitinit': ['*.gitignore', 'gitinit/gitignores/*.gitignore', 'gitinit/gitignores/Global/*.gitignore']},
+    include_package_data=True,
+    entry_points={"console_scripts": ["gitinit=gitinit.gitinit:main"]},
+)
