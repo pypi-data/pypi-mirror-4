cf052203728a15ec3b01636b6ce687931de0b1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package collective.contentfiles2aws/1.0/collective.contentfiles2aws-1.0.tar.gz

---
+++ packages/collective.contentfiles2aws/collective.contentfiles2aws-1.0.tar.gz/collective.contentfiles2aws-1.0/collective/contentfiles2aws/client/fsclient.py
@@ -0,0 +1,25 @@
+class FileClientError(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+class FileClientRetrieveError(FileClientError):
+    """ Base Retrieve Error Exception"""
+    pass
+
+
+class FileClientStoreError(FileClientError):
+    """ Base Store Error Exception """
+    pass
+
+
+class FileClientRemoveError(FileClientError):
+    """ Base Remove Error Exception """
+    pass
+
+
+class FSFileClient(object):
+    pass
