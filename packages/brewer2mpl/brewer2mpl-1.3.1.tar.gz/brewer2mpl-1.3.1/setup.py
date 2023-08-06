f108b2761881daa5dbe3d0f6167b3da548482dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:09 +0000

    Add package brewer2mpl/1.3.1/brewer2mpl-1.3.1.tar.gz

---
+++ packages/brewer2mpl/brewer2mpl-1.3.1.tar.gz/brewer2mpl-1.3.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='brewer2mpl',
+      version='1.3.1',
+      description='Connect colorbrewer2.org color maps to Python and matplotlib',
+      author='Matt Davis',
+      author_email='jiffyclub@gmail.com',
+      url='https://github.com/jiffyclub/brewer2mpl/wiki',
+      packages=['brewer2mpl'],
+      package_data={'brewer2mpl': ['data/colorbrewer*']},
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Visualization'])
