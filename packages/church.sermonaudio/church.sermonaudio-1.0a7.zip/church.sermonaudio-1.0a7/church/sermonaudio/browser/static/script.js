50194883bfd6a90aab3c4361f57a09c897631272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package church.sermonaudio/1.0a7/church.sermonaudio-1.0a7.zip

---
+++ packages/church.sermonaudio/church.sermonaudio-1.0a7.zip/church.sermonaudio-1.0a7/church/sermonaudio/browser/static/script.js
@@ -0,0 +1,35 @@
+$(document).ready( function() {
+    var options = {
+        audioWidth: 200,
+        success: function (mediaElement, domObject) {
+
+           $('.sermon-player').bind("click", function(event){
+            var parent_container = $(this).attr('rel');
+            var target = parent_container + " .mejs-container";
+            console.log(target);
+            console.log($(target).attr('class'));
+            $(target).gtoggle();
+});
+
+        }
+    }
+$('video,audio').mediaelementplayer(options);
+
+function checkFile(event) {
+        var fileElement = document.getElementById("form-widgets-audio_file-input");
+        var fileExtension = "";
+        if (fileElement.value.lastIndexOf(".") > 0) {
+            fileExtension = fileElement.value.substring(fileElement.value.lastIndexOf(".") + 1, fileElement.value.length);
+        }
+        if (fileExtension == "mp3") {
+            return true;
+        }
+        else {
+            alert("You must select a mp3 file for upload");
+            return false;
+        }
+    }
+
+$("form#form").bind("submit",checkFile);
+
+});
---
+++ packages/church.sermonaudio/church.sermonaudio-1.0a7.zip/church.sermonaudio-1.0a7/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a7'
+
+setup(name='church.sermonaudio',
+      version=version,
+      description="Sermon Audio",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['church'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.namedfile [blobs]',
+          'collective.mediaelementjs',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      # The next two lines may be deleted after you no longer need
+      # addcontent support from paster and before you distribute
+      # your package.
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
