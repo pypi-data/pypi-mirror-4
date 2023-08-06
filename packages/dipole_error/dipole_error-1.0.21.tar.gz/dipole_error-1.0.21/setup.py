a0ee170deb43ba74a6bd6825d4fd5e0ba7fea527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:22 +0000

    Add package dipole_error/1.0.21/dipole_error-1.0.21.tar.gz

---
+++ packages/dipole_error/dipole_error-1.0.21.tar.gz/dipole_error-1.0.21/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name="dipole_error",
+      version="1.0.21",
+      description="Calculate the predicted dipole value and error for given input.",
+      py_modules=['dipole_error'],
+      author='Jonathan Whitmore',
+      author_email='jbwhit@gmail.com',
+      url='https://github.com/jbwhit/dipole_error',
+      )
+      # download_url
+      # long_description
+      # classifiers?
