ee1e2304b49bd956cc103f973fa27308762d9e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:14 +0000

    Add package acstools/1.7.2/acstools-1.7.2-py2.7-win32.egg

---
+++ packages/acstools/acstools-1.7.2-py2.7-win32.egg/acstools/__init__.py
@@ -0,0 +1,26 @@
+"""The acstools package holds Python tasks useful for analyzing ACS data.
+
+These tasks include:
+
+Utility and library functions used by these tasks are also included in this
+module.
+
+
+"""
+
+from .version import *
+
+import acs_destripe
+import PixCteCorr
+import runastrodriz
+import calacs
+import acsccd
+import acs2d
+import acsrej
+import acssum
+
+# These lines allow TEAL to print out the names of TEAL-enabled tasks
+# upon importing this package.
+import os
+from stsci.tools import teal
+teal.print_tasknames(__name__, os.path.dirname(__file__))
