7dd758c5c497f35e72e678ff8efa70b38faa2b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package lrparsing/1.0/lrparsing-1.0.tar.gz

---
+++ packages/lrparsing/lrparsing-1.0.tar.gz/lrparsing-1.0/doc/conf.py
@@ -0,0 +1,10 @@
+# (c) 2013, Russell Stuart.
+# Licensed under GPLv2, see COPYING for details.
+
+master_doc = 'lrparsing'
+project = u'lrparsing'
+copyright = u'2013, Russell Stuart'
+version = '1.0.0'
+release = '1.0.0'
+extensions = ['sphinx.ext.intersphinx']
+intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
