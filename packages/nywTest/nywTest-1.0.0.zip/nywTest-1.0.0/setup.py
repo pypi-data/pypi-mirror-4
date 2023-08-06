44ba6a6b7279428d36004fe7351cd10c3e6ab611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:20 +0000

    Add package nywTest/1.0.0/nywTest-1.0.0.zip

---
+++ packages/nywTest/nywTest-1.0.0.zip/nywTest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nywTest",
+    version = "1.0.0",
+    py_modules = ["nywTest"],
+    author = "nyw",
+    author_email = "skadyddn123@gmail.com",
+    url = "http://dislab.hufs.ac.kr",
+    description = "A simple printer of nested list",
+    )
