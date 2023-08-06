6e3ca2fabedf8ce2cd0c224d2e2e797cf2a474f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:27 +0000

    Add package pybuilder/0.9.3/pybuilder-0.9.3.tar.gz

---
+++ packages/pybuilder/pybuilder-0.9.3.tar.gz/pybuilder-0.9.3/pythonbuilder/plugins/python/flake8_plugin.py
@@ -0,0 +1,77 @@
+#   Flake8 Plugin for Python-Builder
+#
+#   Copyright 2012 The Python Builder Team
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+    Plugin for Tarek Ziade's flake8 script.
+    Flake8 is a wrapper around: PyFlakes, pep8, Ned's McCabe script.
+
+    https://bitbucket.org/tarek/flake8
+"""
+
+__author__ = 'Michael Gruber'
+
+from pythonbuilder.core import after, task, init, use_plugin
+from pythonbuilder.utils import assert_can_execute, read_file
+from pythonbuilder.plugins.python.python_plugin_helper import execute_tool_on_source_files
+
+
+use_plugin("python.core")
+
+@init
+def initialize_flake8_plugin(project):
+    if project.verbose:
+        project.set_property("flake8_verbose_output", True)
+    else:
+        project.set_property("flake8_verbose_output", False)
+
+
+@after("prepare")
+def assert_flake8_is_executable(logger):
+    """
+        Asserts that the flake8 script is executable.
+    """
+    logger.debug("Checking if flake8 is executable.")
+
+    assert_can_execute(command_and_arguments=["flake8"],
+                       prerequisite="flake8",
+                       caller="plugin python.flake8")
+
+
+@task
+def analyze(project, logger):
+    """
+        Applies the flake8 script to the sources of the given project.
+    """
+    logger.info("Executing flake8 on project sources.")
+
+    command_and_arguments = ["flake8"]
+
+    property_flake8_ignore = project.get_property("flake8_ignore")
+    if property_flake8_ignore is not None:
+        ignore_option = "--ignore={0}".format(property_flake8_ignore)
+        command_and_arguments.append(ignore_option)
+
+    execution_result = execute_tool_on_source_files(project=project,
+                                                    name="flake8",
+                                                    command_and_arguments=command_and_arguments,
+                                                    logger=logger)
+                                           
+    report_file = execution_result[1]
+    report_lines = read_file(report_file)
+    count_of_warnings = len(report_lines)
+
+    if count_of_warnings > 0:
+        logger.warn("flake8 found %d warning(s).", count_of_warnings)
