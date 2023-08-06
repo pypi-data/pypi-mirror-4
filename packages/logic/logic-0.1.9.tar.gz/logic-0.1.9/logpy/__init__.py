a9c000f71ef9bf4a0df8bad579f3cf2f4f01297d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:57 +0000

    Add package logic/0.1.9/logic-0.1.9.tar.gz

---
+++ packages/logic/logic-0.1.9.tar.gz/logic-0.1.9/logpy/__init__.py
@@ -0,0 +1,9 @@
+"""
+LogPy is a Python library for logic and relational programming.
+"""
+
+from core import run, eq, var, isvar, conde, membero
+from goals import seteq
+from facts import Relation, fact, facts
+
+__version__ = '0.1.9'
