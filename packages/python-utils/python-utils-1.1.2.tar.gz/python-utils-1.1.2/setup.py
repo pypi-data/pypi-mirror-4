72df9898fa042d82deece9c2da9cb12e7ef9f42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:51 +0000

    Add package python-utils/1.1.2/python-utils-1.1.2.tar.gz

---
+++ packages/python-utils/python-utils-1.1.2.tar.gz/python-utils-1.1.2/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+import python_utils
+
+if os.path.isfile('README.rst'):
+    long_description = open('README.rst').read()
+else:
+    long_description = 'See http://pypi.python.org/pypi/python-utils/'
+
+setup(
+    name='python-utils',
+    version=python_utils.__version__,
+    author=python_utils.__author__,
+    author_email=python_utils.__author_email__,
+    description=python_utils.__description__,
+    url='https://github.com/WoLpH/python-utils',
+    license='BSD',
+    packages=find_packages(),
+    long_description=long_description,
+    test_suite='nose.collector',
+    tests_requires=['nose'],
+    classifiers=['License :: OSI Approved :: BSD License'],
+)
+
