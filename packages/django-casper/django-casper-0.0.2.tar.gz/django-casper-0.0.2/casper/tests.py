616ba632c70681366ea1d4c4accfdb6fa0fc859b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:35 +0000

    Add package django-casper/0.0.2/django-casper-0.0.2.tar.gz

---
+++ packages/django-casper/django-casper-0.0.2.tar.gz/django-casper-0.0.2/casper/tests.py
@@ -0,0 +1,66 @@
+from django.test import LiveServerTestCase
+from subprocess import Popen, PIPE
+import os.path
+import sys
+
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.contrib.staticfiles.views import serve
+from django.utils.http import http_date
+from django.conf import settings
+
+__all__ = ['CasperTestCase']
+
+
+def staticfiles_handler_serve(self, request):
+    import time
+    resp = serve(request, self.file_path(request.path), insecure=True)
+    if resp.status_code == 200:
+        resp["Expires"] = http_date(time.time() + 24 * 3600)
+    return resp
+
+
+class CasperTestCase(LiveServerTestCase):
+    """LiveServerTestCase subclass that can invoke CasperJS tests."""
+
+    use_phantom_disk_cache = False
+
+    def __init__(self, *args, **kwargs):
+        super(CasperTestCase, self).__init__(*args, **kwargs)
+        if self.use_phantom_disk_cache:
+            StaticFilesHandler.serve = staticfiles_handler_serve
+
+    def casper(self, test_filename, **kwargs):
+        """CasperJS test invoker.
+
+        Takes a test filename (.js) and optional arguments to pass to the
+        casper test.
+
+        Returns True if the test(s) passed, and False if any test failed.
+
+        Since CasperJS startup/shutdown is quite slow, it is recommended
+        to bundle all the tests from a test case in a single casper file
+        and invoke it only once.
+        """
+
+        kwargs.update({
+            'load-images': 'no',
+            'disk-cache': 'yes' if self.use_phantom_disk_cache else 'no',
+            'ignore-ssl-errors': 'yes',
+            'url-base': self.live_server_url
+        })
+
+        cn = settings.SESSION_COOKIE_NAME
+        if cn in self.client.cookies:
+            kwargs['cookie-' + cn] = self.client.cookies[cn].value
+
+        cmd = ['casperjs', 'test', '--no-colors']
+        cmd.extend([('--%s=%s' % i) for i in kwargs.iteritems()])
+        cmd.append(test_filename)
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE,
+            cwd=os.path.dirname(test_filename))  # flake8: noqa
+        out, err = p.communicate()
+        if p.returncode != 0:
+            sys.stdout.write(out)
+            sys.stderr.write(err)
+        return p.returncode == 0
---
+++ packages/django-casper/django-casper-0.0.2.tar.gz/django-casper-0.0.2/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, Command
+import os
+import sys
+
+
+class BaseCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class TestCommand(BaseCommand):
+
+    description = "run self-tests"
+
+    def run(self):
+        os.chdir('testproject')
+        ret = os.system('python manage.py test testapp')
+        if ret != 0:
+            sys.exit(-1)
+
+
+class CoverageCommand(BaseCommand):
+    description = "run self-tests and report coverage (requires coverage.py)"
+
+    def run(self):
+        os.chdir('testproject')
+        ret = os.system('coverage run --source=casper manage.py test testapp')
+        if ret != 0:
+            sys.exit(-1)
+        os.system('coverage html')
+
+
+setup(
+    name='django-casper',
+    version='0.0.2',
+    author='Senko Rasic',
+    author_email='senko.rasic@goodcode.io',
+    description='CasperJS test integration for Django',
+    license='MIT',
+    url='http://goodcode.io/',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    cmdclass={
+        'test': TestCommand,
+        'coverage': CoverageCommand
+    }
+)
