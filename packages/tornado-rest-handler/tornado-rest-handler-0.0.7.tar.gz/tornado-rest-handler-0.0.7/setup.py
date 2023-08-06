88183e1c8465719a029c4f593c0218a135622e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:23 +0000

    Add package tornado-rest-handler/0.0.7/tornado-rest-handler-0.0.7.tar.gz

---
+++ packages/tornado-rest-handler/tornado-rest-handler-0.0.7.tar.gz/tornado-rest-handler-0.0.7/setup.py
@@ -0,0 +1,38 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+VERSION = open('VERSION', 'r').read().strip()
+PROJECT_NAME = 'tornado-rest-handler'
+
+tests_require = [
+    'nose',
+    'coverage',
+]
+
+install_requires = [
+    'tornado==3.0.1',
+    'python-rest-handler==0.0.2',
+]
+
+setup(name='%s' % PROJECT_NAME,
+      url='https://github.com/paulocheque/%s' % PROJECT_NAME,
+      author="paulocheque",
+      author_email='paulocheque@gmail.com',
+      keywords='python tornado rest handler',
+      description='A simple Python Tornado handler that manage Rest requests automatically.',
+      license='MIT',
+      classifiers=[
+          # 'Framework :: Tornado',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development'
+      ],
+
+      version='%s' % VERSION,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      # test_suite='runtests.runtests',
+      # extras_require={'test': tests_require},
+
+      packages=find_packages(),
+)
+
