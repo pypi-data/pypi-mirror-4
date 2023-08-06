e28766cff575ba7fca608c5e40e45d19d5e3a265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package dylist/1.1.0/dylist-1.1.0.zip

---
+++ packages/dylist/dylist-1.1.0.zip/dylist-1.1.0/dylist.py
@@ -0,0 +1,11 @@
+'''这是一个列表模块'''
+def dylist(bname,level):
+    '''主要内容为一个循环套'''
+    for abc in bname:
+        if isinstance(abc,list):
+                      dylist(abc,level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(abc)
+                      
