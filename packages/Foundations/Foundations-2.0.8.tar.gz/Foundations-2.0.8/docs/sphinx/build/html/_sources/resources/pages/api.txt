f997ce863e5cce2658a6c3dd914c8b655f80d5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:06 +0000

    Add package Foundations/2.0.8/Foundations-2.0.8.tar.gz

---
+++ packages/Foundations/Foundations-2.0.8.tar.gz/Foundations-2.0.8/docs/sphinx/build/html/_sources/resources/pages/api.txt
@@ -0,0 +1,29 @@
+Api
+====
+
+Modules Summary:
+
+.. toctree::
+   :maxdepth: 1
+
+   foundations.cache <api/foundations.cache>
+   foundations.common <api/foundations.common>
+   foundations.core <api/foundations.core>
+   foundations.dataStructures <api/foundations.dataStructures>
+   foundations.decorators <api/foundations.decorators>
+   foundations.environment <api/foundations.environment>
+   foundations.exceptions <api/foundations.exceptions>
+   foundations.globals.constants <api/foundations.globals.constants>
+   foundations.io <api/foundations.io>
+   foundations.library <api/foundations.library>
+   foundations.namespace <api/foundations.namespace>
+   foundations.nodes <api/foundations.nodes>
+   foundations.parsers <api/foundations.parsers>
+   foundations.pkzip <api/foundations.pkzip>
+   foundations.rotatingBackup <api/foundations.rotatingBackup>
+   foundations.strings <api/foundations.strings>
+   foundations.tcpServer <api/foundations.tcpServer>
+   foundations.trace <api/foundations.trace>
+   foundations.ui.common <api/foundations.ui.common>
+   foundations.verbose <api/foundations.verbose>
+   foundations.walkers <api/foundations.walkers>
---
+++ packages/Foundations/Foundations-2.0.8.tar.gz/Foundations-2.0.8/setup.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**setup.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	`https://pypi.python.org/pypi/Foundations <https://pypi.python.org/pypi/Foundations>`_ package setup file.
+
+**Others:**
+
+"""
+
+#**********************************************************************************************************************
+#***	Future imports.
+#**********************************************************************************************************************
+from __future__ import unicode_literals
+
+#**********************************************************************************************************************
+#***	Encoding manipulations.
+#**********************************************************************************************************************
+import sys
+
+def _setEncoding():
+	"""
+	This definition sets the Application encoding.
+	"""
+
+	reload(sys)
+	sys.setdefaultencoding("utf-8")
+
+_setEncoding()
+
+#**********************************************************************************************************************
+#***	External imports.
+#**********************************************************************************************************************
+import re
+from setuptools import setup
+from setuptools import find_packages
+
+#**********************************************************************************************************************
+#***	Internal imports.
+#**********************************************************************************************************************
+import foundations.globals.constants
+
+#**********************************************************************************************************************
+#***	Module attributes.
+#**********************************************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2013 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["getLongDescription"]
+
+#**********************************************************************************************************************
+#***	Module classes and definitions.
+#**********************************************************************************************************************
+def getLongDescription():
+	"""
+	This definition returns the Package long description.
+
+	:return: Package long description. ( String )
+	"""
+
+	description = []
+	with open("README.rst") as file:
+		for line in file:
+			if ".. code:: python" in line and len(description) >= 2:
+				blockLine = description[-2]
+				if re.search(r":$", blockLine) and not re.search(r"::$", blockLine):
+					description[-2] = "::".join(blockLine.rsplit(":", 1))
+				continue
+
+			description.append(line)
+	return "".join(description)
+
+setup(name=foundations.globals.constants.Constants.applicationName,
+	version=foundations.globals.constants.Constants.releaseVersion,
+	author=foundations.globals.constants.__author__,
+	author_email=foundations.globals.constants.__email__,
+	include_package_data=True,
+	packages=find_packages(),
+	scripts=[],
+	url="https://github.com/KelSolaar/Foundations",
+	license="GPLv3",
+	description="Foundations is the core package of Umbra, sIBL_GUI and sIBL_Reporter.",
+	long_description=getLongDescription(),
+	install_requires=["ordereddict>=1.1", "sphinx>=1.1.3", "unittest2>=0.5.1"],
+	classifiers=["Development Status :: 5 - Production/Stable",
+				"Environment :: Console",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+				"Natural Language :: English",
+				"Operating System :: OS Independent",
+				"Programming Language :: Python :: 2.7",
+				"Topic :: Utilities"])
