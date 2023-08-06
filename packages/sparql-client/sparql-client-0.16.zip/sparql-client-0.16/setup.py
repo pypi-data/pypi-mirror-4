f6362a8aaba89857fae7ec802276965026cb6a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package sparql-client/0.16/sparql-client-0.16.zip

---
+++ packages/sparql-client/sparql-client-0.16.zip/sparql-client-0.16/setup.py
@@ -0,0 +1,27 @@
+import os.path
+from distutils.core import setup
+
+
+docs = open('README.rst').read() + "\n\n"
+
+setup(name='sparql-client',
+      version='0.16',
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      description='Python API to query a SPARQL endpoint',
+      long_description=docs,
+      author='Soren Roug',
+      author_email='soren.roug@eea.europa.eu',
+      url='http://www.eionet.europa.eu/software/sparql-client',
+      py_modules =['sparql'],
+      )
