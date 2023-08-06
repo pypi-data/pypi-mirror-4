041ce281eb7c30bf47349ed1bc05a1cd06d91814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:27 +0000

    Add package lingua/1.5/lingua-1.5.tar.gz

---
+++ packages/lingua/lingua-1.5.tar.gz/lingua-1.5/src/lingua/extractors/tests/test_python.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+import unittest
+
+
+class ExtractTests(unittest.TestCase):
+    def extract(self, snippet):
+        from lingua.extractors.python import extract_python
+        from StringIO import StringIO
+        snippet = StringIO(snippet)
+        return list(extract_python(snippet, ['_'], None, None))
+
+    def test_syntax_error(self):
+        self.assertEqual(
+                self.extract("def class xya _('foo')"),
+                [(1, None, u'foo', [])])
+
+    def test_multiline_string(self):
+        self.assertEqual(
+                self.extract("_('one two '\n'three')"),
+                [(1, None, u'one two three', [])])
