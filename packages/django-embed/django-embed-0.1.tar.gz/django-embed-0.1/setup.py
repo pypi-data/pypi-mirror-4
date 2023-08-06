832e7f05485ecbad20c248f1709ad0730052e89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:33 +0000

    Add package django-embed/0.1/django-embed-0.1.tar.gz

---
+++ packages/django-embed/django-embed-0.1.tar.gz/django-embed-0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = 'django-embed',
+    version = '0.1',
+    packages = find_packages(),
+    include_package_data = True,
+    license = 'MIT License',
+    description = 'A simple Django app to generate embed code from youtube, twitter and slideshare.',
+    long_description = README,
+    url = 'http://mejorando.la/',
+    author = 'https://github.com/mejorandola',
+    author_email = 'd@mejorando.la',
+    install_requires = ['requests==0.14.2', 'requests-oauth==0.4.1'],
+    keywords='Embed youtube twitter slideshare python',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
