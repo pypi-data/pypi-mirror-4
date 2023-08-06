489fbecac555f57217026b2ca7cd4ed9bf38481f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:52 +0000

    Add package django-simple-menu/1.0.3/django-simple-menu-1.0.3.tar.gz

---
+++ packages/django-simple-menu/django-simple-menu-1.0.3.tar.gz/django-simple-menu-1.0.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django-simple-menu',
+      packages=find_packages(),
+      version='1.0.3',
+      description='Simple, yet powerful, code-based menus for Django applications',
+      long_description=open('README.rst').read(),
+      author='Evan Borgstrom',
+      author_email='evan@fatbox.ca',
+      url='https://github.com/fatbox/django-simple-menu',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      install_requires=['setuptools'])
