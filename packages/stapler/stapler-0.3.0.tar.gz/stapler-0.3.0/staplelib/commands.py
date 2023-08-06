75570cf6170985de5e3b716d4943a5f6c7bd368f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:32 +0000

    Add package stapler/0.3.0/stapler-0.3.0.tar.gz

---
+++ packages/stapler/stapler-0.3.0.tar.gz/stapler-0.3.0/staplelib/commands.py
@@ -0,0 +1,131 @@
+"""Module containing the actual commands stapler understands."""
+
+import math
+import os.path
+import os
+
+from pyPdf import PdfFileWriter, PdfFileReader
+
+from . import CommandError, iohelper
+import staplelib
+
+
+def select(args, inverse=False):
+    """
+    Concatenate files / select pages from files.
+
+    inverse=True excludes rather than includes the selected pages from
+    the file.
+    """
+
+    filesandranges = iohelper.parse_ranges(args[:-1])
+    outputfilename = args[-1]
+    verbose = staplelib.OPTIONS.verbose
+
+    if not filesandranges or not outputfilename:
+        raise CommandError("Both input and output filenames are required.")
+
+    output = PdfFileWriter()
+    try:
+        for input in filesandranges:
+            pdf = input['pdf']
+            if verbose:
+                print input['name']
+
+            # empty range means "include all pages"
+            if not inverse:
+                pagerange = input['pages'] or [
+                    (p, iohelper.ROTATION_NONE) for p in
+                    range(1, pdf.getNumPages()+1)]
+            else:
+                excluded = [p for p, r in input['pages']]
+                pagerange = [(p, iohelper.ROTATION_NONE) for p in
+                             range(1, pdf.getNumPages()+1) if
+                             p not in excluded]
+
+            for pageno, rotate in pagerange:
+                if 1 <= pageno <= pdf.getNumPages():
+                    if verbose:
+                        print "Using page: %d (rotation: %d deg.)" % (pageno, rotate)
+                    output.addPage(pdf.getPage(pageno-1).rotateClockwise(rotate))
+                else:
+                    raise CommandError(
+                        "Page %d not found in %s." % (pageno, input['name']))
+
+    except Exception, e:
+        raise CommandError(e)
+
+    if os.path.isabs(outputfilename) :
+        iohelper.write_pdf(output, outputfilename)
+    else :
+        iohelper.write_pdf(output, staplelib.OPTIONS.destdir + os.sep + outputfilename)
+
+
+def delete(args):
+    """Concatenate files and remove pages from files."""
+
+    return select(args, inverse=True)
+
+
+def split(args):
+    """Burst an input file into one file per page."""
+
+    files = args
+    verbose = staplelib.OPTIONS.verbose
+
+    if not files:
+        raise CommandError("No input files specified.")
+
+    inputs = []
+    try:
+        for f in files:
+            inputs.append(iohelper.read_pdf(f))
+    except Exception, e:
+        raise CommandError(e)
+
+    filecount = 0
+    pagecount = 0
+    for input in inputs:
+        # zero-padded output file name
+        (base, ext) = os.path.splitext(os.path.basename(files[filecount]))
+        output_template = ''.join([
+            base, '_',
+            '%0', str(math.ceil(math.log10(input.getNumPages()))), 'd',
+            ext
+        ])
+
+        for pageno in range(input.getNumPages()):
+            output = PdfFileWriter()
+            output.addPage(input.getPage(pageno))
+
+            outputname = output_template % (pageno+1)
+            if verbose:
+                print outputname
+            iohelper.write_pdf(output, staplelib.OPTIONS.destdir + os.sep + outputname)
+            
+            pagecount += 1
+        filecount += 1
+
+    if verbose:
+        print
+        print "%d page(s) in %d file(s) processed." % (pagecount, filecount)
+
+
+def info(args):
+    """Display Metadata content for all input files."""
+    files = args
+
+    if not files:
+        raise CommandError("No input files specified.")
+
+    for f in files:
+        pdf = iohelper.read_pdf(f)
+        print "*** Metadata for %s" % f
+        print
+        info = pdf.documentInfo
+        if info:
+            for name, value in info.items():
+                print "    %s:  %s" % (name, value)
+        else:
+            print "    (No metadata found.)"
+        print
---
+++ packages/stapler/stapler-0.3.0.tar.gz/stapler-0.3.0/staplelib/iohelper.py
@@ -0,0 +1,120 @@
+"""Helper functions for user-supplied arguments and file I/O."""
+
+import getpass
+import os.path
+import re
+import sys
+
+from pyPdf import PdfFileWriter, PdfFileReader
+
+from . import CommandError
+import staplelib
+
+
+ROTATION_NONE = 0
+ROTATION_RIGHT = 90
+ROTATION_TURN = 180
+ROTATION_LEFT = 270
+ROTATIONS = {'u': ROTATION_NONE,
+             'r': ROTATION_RIGHT,
+             'd': ROTATION_TURN,
+             'l': ROTATION_LEFT}
+
+
+def read_pdf(filename):
+    """Open a PDF file with pyPdf."""
+    if not os.path.exists(filename):
+        raise CommandError("%s does not exist" % filename)
+    pdf = PdfFileReader(file(filename, "rb"))
+    if pdf.isEncrypted:
+        while True:
+            pw = prompt_for_pw(filename)
+            matched = pdf.decrypt(pw)
+            if matched:
+                break
+            else:
+                print "The password did not match."
+    return pdf
+
+
+def write_pdf(pdf, filename):
+    """Write the content of a PdfFileWriter object to a file."""
+    if os.path.exists(filename):
+        raise CommandError("File already exists: %s" % filename)
+
+    opt = staplelib.OPTIONS
+    if opt:
+        if opt.ownerpw or opt.userpw:
+            pdf.encrypt(opt.userpw or '', opt.ownerpw)
+
+    outputStream = file(filename, "wb")
+    pdf.write(outputStream)
+    outputStream.close()
+
+
+def prompt_for_pw(filename):
+    """Prompt the user for the password to access an input file."""
+    print 'Please enter a password to decrypt %s.' % filename
+    print '(The password will not be shown. Press ^C to cancel).'
+
+    try:
+        return getpass.getpass('--> ')
+    except KeyboardInterrupt:
+        sys.stderr.write('Aborted by user.\n')
+        sys.exit(2)
+
+
+def check_input_files(files):
+    """Make sure all input files exist."""
+
+    for filename in files:
+        if not os.path.exists(filename):
+            raise CommandError("%s does not exist" % filename)
+
+
+def check_output_file(filename):
+    """Make sure the output file does not exist."""
+
+    if os.path.exists(filename):
+        raise CommandError("File already exists: %s" % filename)
+
+
+def parse_ranges(files_and_ranges):
+    """Parse a list of filenames followed by ranges."""
+
+    operations = []
+    for inputname in files_and_ranges:
+        if inputname.lower().endswith('.pdf'):
+            operations.append({"name": inputname,
+                               "pdf": read_pdf(inputname),
+                               "pages": []})
+        else:
+            match = re.match('([0-9]+|end)(?:-([0-9]+|end))?([LRD]?)', inputname)
+            if not match:
+                raise CommandError('Invalid range: %s' % inputname)
+
+            current = operations[-1]
+            max_page = current['pdf'].getNumPages()
+            # allow "end" as alias for the last page
+            replace_end = lambda page: (
+                max_page if page.lower() == 'end' else int(page))
+            begin = replace_end(match.group(1))
+            end = replace_end(match.group(2)) if match.group(2) else begin
+
+            rotate = ROTATIONS.get((match.group(3) or 'u').lower())
+
+            if begin > max_page or end > max_page:
+                raise CommandError(
+                    'Range %s-%s exceeds maximum page number %s of file %s' % (
+                        begin, end, max_page, current['name']))
+
+            # negative ranges sort pages backwards
+            if begin < end:
+                pagerange = range(begin, end+1)
+            else:
+                pagerange = range(end, begin+1)[::-1]
+
+            for p in pagerange:
+                current['pages'].append((p, rotate))
+
+    return operations
