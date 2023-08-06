540d968715d733984c45b7f5a16b4e37ff8bb77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:34 +0000

    Add package wsaccel/0.4/wsaccel-0.4.tar.gz

---
+++ packages/wsaccel/wsaccel-0.4.tar.gz/wsaccel-0.4/wsaccel/__init__.py
@@ -0,0 +1,26 @@
+def patch_autobahn():
+    from wsaccel.utf8validator import Utf8Validator
+    from wsaccel.xormask import XorMaskerNull, XorMaskerSimple
+    import autobahn.websocket
+
+    autobahn.websocket.Utf8Validator = Utf8Validator
+    autobahn.websocket.XorMaskerNull = XorMaskerNull
+    autobahn.websocket.XorMaskerSimple = XorMaskerSimple
+    autobahn.websocket.XorMaskerShifted1 = XorMaskerSimple
+
+
+def patch_ws4py():
+    from wsaccel.utf8validator import Utf8Validator
+    from wsaccel.xormask import XorMaskerSimple
+    from ws4py import streaming, framing
+
+    streaming.Utf8Validator = Utf8Validator
+
+    def mask(self, data):
+        if self.masking_key:
+            masker = XorMaskerSimple(self.masking_key)
+            return masker.process(data)
+        return data
+
+    framing.Frame.mask = mask
+    framing.Frame.unmask = mask
