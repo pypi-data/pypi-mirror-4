dc4e9812f3ca394098e1d3ab7bae5334664de1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:53 +0000

    Add package Topics/0.1.1/Topics-0.1.1.tar.gz

---
+++ packages/Topics/Topics-0.1.1.tar.gz/Topics-0.1.1/setup.py
@@ -0,0 +1,15 @@
+#coding:utf8
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    name='Topics',
+    version="0.1.1",
+    author= 'Flávio C. Coelho',
+    author_email= "fccoelho@gmail.com",
+    license= "GPLv3",
+    description="Set of tools for topic modeling and visualization",
+    ext_modules=cythonize("visualization/*.pyx"),
+    packages = ['onlineldavb','visualization','onlinehdp','tests'],
+    package_data={'tests': ['data/*']},
+)
