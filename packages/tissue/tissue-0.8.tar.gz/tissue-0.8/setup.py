620ef1b531aff3089a5ed92e3902c7348b628ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:18 +0000

    Add package tissue/0.8/tissue-0.8.tar.gz

---
+++ packages/tissue/tissue-0.8.tar.gz/tissue-0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+version = "0.8"
+
+setup(name="tissue",
+      version=version,
+      description="Tissue - automated pep8 checker for nose",
+      long_description=open("README.rst").read(),
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords="pep8 nose",
+      author="Jason K\xc3\xb6lker",
+      author_email="jason@koelker.net",
+      url="https://github.com/jkoelker/tissue",
+      license="GNU LGPL",
+      py_modules=["tissue"],
+      install_requires=[
+          "nose",
+          "pep8",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+[nose.plugins.0.10]
+tissue = tissue:Tissue
+      """,
+      )
