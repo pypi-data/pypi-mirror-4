8179b6c018436f499a52beaf71913c5a86e98e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:31 +0000

    Add package mezzanine-file-collections/1.0.1/mezzanine-file-collections-1.0.1.tar.gz

---
+++ packages/mezzanine-file-collections/mezzanine-file-collections-1.0.1.tar.gz/mezzanine-file-collections-1.0.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = 'mezzanine-file-collections',
+    version = '1.0.1',
+    packages = ['mezzanine_file_collections'],
+    include_package_data = True,
+    license = 'WTFPL',
+    description = 'A simple file collection page type for Mezzanine',
+    long_description = README,
+    url = 'https://github.com/thibault/mezzanine-file-collections',
+    author = 'Thibault Jouannic',
+    author_email = 'thibault@miximum.fr',
+    setup_requires=('setuptools'),
+    requires = ('mezzanine'),
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
