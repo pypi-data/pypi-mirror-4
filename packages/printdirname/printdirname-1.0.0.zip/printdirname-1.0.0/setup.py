7d32dadf29f05dade3484df2b71da9887305a0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package printdirname/1.0.0/printdirname-1.0.0.zip

---
+++ packages/printdirname/printdirname-1.0.0.zip/printdirname-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name        =       'printdirname',
+    version     =       '1.0.0',
+    py_modules  =       ['printdirname'],
+    author      =       'Av_PMlove',
+    author_email=       'lc.chao.liu@gamil.com',
+    url         =       'http://weibo.com/lclzx1988?wvr=5&lf=reg',
+    description =       'A simple printer of the files name of the Directory',
+    )
