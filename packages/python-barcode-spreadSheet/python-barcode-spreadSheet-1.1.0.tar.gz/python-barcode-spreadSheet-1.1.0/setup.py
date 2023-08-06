efafe244b7e394918b6b222b40f3816dd43d551d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:47 +0000

    Add package python-barcode-spreadSheet/1.1.0/python-barcode-spreadSheet-1.1.0.tar.gz

---
+++ packages/python-barcode-spreadSheet/python-barcode-spreadSheet-1.1.0.tar.gz/python-barcode-spreadSheet-1.1.0/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='python-barcode-spreadSheet',
+    version='1.1.0',
+    license='BSD',
+    description='A simple Python tool to generate bar-codes and programmatically insert them into Google Spreadsheets for printing.',
+    long_description=README,
+    url='http://prasenjitsingh.com/opensource/python-barcode-spreadSheet/',
+    author='Prasenjit Singh',
+    author_email='prasenjit0625@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
