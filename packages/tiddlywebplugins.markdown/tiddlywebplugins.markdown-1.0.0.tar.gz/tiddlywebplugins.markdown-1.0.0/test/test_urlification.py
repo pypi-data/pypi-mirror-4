0df7309c4a9a74ba6c9aa7c36d6a0f356c1e415f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:20 +0000

    Add package tiddlywebplugins.markdown/1.0.0/tiddlywebplugins.markdown-1.0.0.tar.gz

---
+++ packages/tiddlywebplugins.markdown/tiddlywebplugins.markdown-1.0.0.tar.gz/tiddlywebplugins.markdown-1.0.0/test/test_urlification.py
@@ -0,0 +1,30 @@
+from tiddlywebplugins.markdown import render
+from tiddlyweb.model.tiddler import Tiddler
+
+
+def test_urlification():
+    tiddler = Tiddler('blah')
+    tiddler.text = """
+lorem ipsum http://example.org dolor sit amet
+... http://www.example.com/foo/bar ... and <http://example.com>
+     """
+
+    environ = {'tiddlyweb.config': {'markdown.wiki_link_base': ''}}
+    output = render(tiddler, environ)
+
+    for url in ["http://example.org", "http://www.example.com/foo/bar",
+            'http://example.com']:
+        assert '<a href="%(url)s">%(url)s</a>' % { "url": url } in output
+
+
+def test_precedence():
+    tiddler = Tiddler('cow')
+    tiddler.text = """
+* [Pry](http://www.philaquilina.com/2012/05/17/tossing-out-irb-for-pry/)
+* [Rails console](http://37signals.com/svn/posts/3176-three-quick-rails-console-tips)
+"""
+
+    environ = {'tiddlyweb.config': {'markdown.wiki_link_base': ''}}
+    output = render(tiddler, environ)
+
+    assert "http://<a href" not in output
