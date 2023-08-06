01dde0a6dfb386945710d2df4f21a44659dba690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package django-browserid/0.8/django-browserid-0.8.tar.gz

---
+++ packages/django-browserid/django-browserid-0.8.tar.gz/django-browserid-0.8/setup.py
@@ -0,0 +1,43 @@
+import codecs
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-browserid',
+    description='Django application for adding BrowserID support.',
+    long_description=read('README.rst'),
+    version=find_version('django_browserid/__init__.py'),
+    packages=find_packages(),
+    author='Paul Osman, Michael Kelly',
+    author_email='mkelly@mozilla.com',
+    url='https://github.com/mozilla/django-browserid',
+    license='MPL v2.0',
+    install_requires=['requests>=0.9.1', 'fancy_tag==0.2.0'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+)
