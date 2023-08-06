1c38b485a860099c80ba51e89ab293c61ee8a4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:40 +0000

    Add package tiddlywebwiki/0.61.0/tiddlywebwiki-0.61.0.tar.gz

---
+++ packages/tiddlywebwiki/tiddlywebwiki-0.61.0.tar.gz/tiddlywebwiki-0.61.0/tiddlywebwiki/__init__.py
@@ -0,0 +1,42 @@
+"""
+A TiddlyWeb plugin providing a multi-user TiddlyWiki environment.
+"""
+
+__version__ = '0.61.0'
+
+
+from tiddlyweb.web.handler.recipe import get_tiddlers
+ 
+  
+def friendlywiki(environ, start_response):
+    """
+    Reframe the WSGI environment before internally redirecting
+    to a recipe.
+    """
+    environ['tiddlyweb.type'] = 'text/x-tiddlywiki'
+    return get_tiddlers(environ, start_response)
+
+
+def init(config):
+    # These imports must be in init otherwise getting
+    # the version number causes cyclic imports to happen.
+    import tiddlywebplugins.instancer
+    import tiddlywebplugins.status
+    import tiddlywebplugins.atom
+    import tiddlywebplugins.differ
+    import tiddlywebplugins.twimport
+    import tiddlywebplugins.console
+    from tiddlyweb.util import merge_config
+    from tiddlywebwiki.config import config as twwconfig
+
+    merge_config(config, twwconfig)
+    tiddlywebplugins.instancer.init(config)
+    tiddlywebplugins.status.init(config)
+    tiddlywebplugins.atom.init(config)
+    tiddlywebplugins.differ.init(config)
+    tiddlywebplugins.twimport.init(config)
+    tiddlywebplugins.console.init(config)
+
+    if 'selector' in config:
+        if config.get('tiddlywebwiki.friendlywiki', True):
+            config['selector'].add('/{recipe_name:segment}', GET=friendlywiki)
