f1d2a0083ee00652f0b490432ce391f20b028bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:26 +0000

    Add package completeme/0.1.7/completeme-0.1.7.tar.gz

---
+++ packages/completeme/completeme-0.1.7.tar.gz/completeme-0.1.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import completeme
+data_files = [("/etc", [completeme.CONFIG_FN])]
+setup(
+        name = "completeme",
+        version = open("VERSION").read().strip(),
+        description = "Automagic ctrl+t filename completion to launch in your favorite editor",
+        long_description = open("README.rst").read(),
+        author = "Matt Spitz",
+        author_email = "mattspitz@gmail.com",
+        url = "https://github.com/mattspitz/completeme",
+
+        py_modules = ["completeme"],
+        entry_points = {
+            "console_scripts": [
+                    "completeme = completeme:main"
+                ]
+            },
+        scripts = ["setup_completeme_key_binding.sh"],
+        data_files = data_files
+)
