5f96759b5d495fd4f518b4335bef0b30c447e910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:12 +0000

    Add package parse_rest/0.4.2012/parse_rest-0.4.2012.tar.gz

---
+++ packages/parse_rest/parse_rest-0.4.2012.tar.gz/parse_rest-0.4.2012/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader
+
+
+class TestCommand(Command):
+    """Run test suite using 'python setup.py test'"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run test suite in parse_rest.tests"""
+        from parse_rest import tests
+        tests = TestLoader().loadTestsFromNames(["parse_rest.tests"])
+        t = TextTestRunner(verbosity=1)
+        t.run(tests)
+
+
+setup(
+    name='parse_rest',
+    version='0.4.2012',
+    description='A client library for Parse.com\'.s REST API',
+    url='https://github.com/dgrtwo/ParsePy',
+    packages=['parse_rest'],
+    maintainer="David Robinson",
+    maintainer_email="dgrtwo@princeton.edu",
+    cmdclass={"test": TestCommand},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+        ]
+    )
