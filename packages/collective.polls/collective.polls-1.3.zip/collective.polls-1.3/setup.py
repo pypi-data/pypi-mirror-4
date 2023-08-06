eef3f376f3a5eb730864be15e44230430486f2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package collective.polls/1.3/collective.polls-1.3.zip

---
+++ packages/collective.polls/collective.polls-1.3.zip/collective.polls-1.3/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.3'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
+
+setup(name='collective.polls',
+      version=version,
+      description="A content type, workflow, and portlet for conducting "
+                  "online polls, for anonymous and logged-in users.",
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone dexterity polls',
+      author='Franco Pellegrini',
+      author_email='frapell@ravvit.net',
+      url='https://github.com/collective/collective.polls',
+      license='GPLv2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'Pillow',
+        'Products.CMFPlone>=4.1',
+        'plone.app.dexterity>=1.2.1',
+        'plone.app.referenceablebehavior',
+        'plone.directives.dexterity',
+        'collective.z3cform.widgets>=1.0b3',
+        ],
+      extras_require={
+        'test': [
+          'plone.app.testing',
+          'plone.formwidget.contenttree',  # FIXME: needed by collective.z3cform.widgets
+          'robotsuite',
+          'robotframework-selenium2library',
+          ],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
