a066f7683e2e7456a9277e4e41a540f059957188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:51 +0000

    Add package on.caroufredsel/0.1.1a/on.caroufredsel-0.1.1a.tar.gz

---
+++ packages/on.caroufredsel/on.caroufredsel-0.1.1a.tar.gz/on.caroufredsel-0.1.1a/CHANGES.rst
@@ -0,0 +1,12 @@
+
+
+=========
+Changelog
+=========
+
+
+Version 0.1 - 2013-02-24
+========================
+
+Initial release with carouFredSel 6.1.0
+
---
+++ packages/on.caroufredsel/on.caroufredsel-0.1.1a.tar.gz/on.caroufredsel-0.1.1a/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version_file = os.path.join('on', 'caroufredsel', 'version.txt')
+version = open(version_file).read().strip()
+
+setup(name='on.caroufredsel',
+      version=version,
+      description="Shim to load Caroufredsel as a resource",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+	"Framework :: Plone :: 4.2",
+	"Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        ],
+      keywords='Caroufredsel',
+      author='Toni Mueller',
+      author_email='support@oeko.net',
+      url='https://github.com/muellert/on.caroufredsel.git',
+      license='MIT + GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['on'],
+      include_package_data=True,
+      zip_safe=False,
+      setup_requires=['setuptools-git'],
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      extras_require = {
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      )
