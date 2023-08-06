e88dc5f561d4d079e31d510745d36d01a4e94d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:31 +0000

    Add package django-thjodskra/0.22/django-thjodskra-0.22.tar.gz

---
+++ packages/django-thjodskra/django-thjodskra-0.22.tar.gz/django-thjodskra-0.22/setup.py
@@ -0,0 +1,24 @@
+#encoding: utf-8
+from distutils.core import setup
+
+setup(
+	name = "django-thjodskra",
+    version = "0.22",
+    description = "django-thjodskra is a simple app for integrating the icelandic national registry into your django project",
+    author = "Úlfur Kristjánsson",
+    author_email = "ulfur@theawesometastic.com",
+    url = "https://github.com/ulfur/django-thjodskra",
+    packages = [
+				"thjodskra", 
+				"thjodskra.management", 
+				"thjodskra.management.commands", 
+				"thjodskra.templates",
+				"thjodskra.templates.thjodskra",
+				],
+    package_data = {
+					"thjodskra" : ["thjodskra/*.py"],
+					"management": ["thjodskra/management/*.py"],
+					"commands"	: ["thjodskra/management/commands/*.py"],
+					"templates" : ["thjodskra/templates/*", "thjodskra/templates/thjodskra/*"]
+					},
+)
