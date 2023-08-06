3a39e2c707b634587ef2e3de28d045b198d18605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:46 +0000

    Add package ztfy.thesaurus/0.1.1/ztfy.thesaurus-0.1.1-py2.7.egg

---
+++ packages/ztfy.thesaurus/ztfy.thesaurus-0.1.1-py2.7.egg/ztfy/thesaurus/browser/resources/js/i18n/fr.js
@@ -0,0 +1,9 @@
+(function($) {
+
+	$.ZTFY.thesaurus.I18n = {
+
+		CONFIRM_UNSELECT_WITH_CHILD: "Enlever ce terme de cet extrait enlèvera également tous ses termes spécifiques.\nÊtes-vous sûr ?"
+
+	}
+
+})(jQuery);
