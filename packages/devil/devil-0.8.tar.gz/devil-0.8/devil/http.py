d7c5dd2b950263bfdeaeeb5fe1f877e393177e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package devil/0.8/devil-0.8.tar.gz

---
+++ packages/devil/devil-0.8.tar.gz/devil-0.8/devil/http.py
@@ -0,0 +1,47 @@
+#  -*- coding: utf-8 -*-
+# http.py ---
+#
+# Created: Wed Dec 14 15:09:35 2011 (+0200)
+# Author: Janne Kuuskeri
+#
+
+
+CODES = dict(
+    ALL_OK=('OK', 200),
+    CREATED=('Created', 201),
+    DELETED=('No Content', 204),
+    BAD_REQUEST=('Bad Request', 400),
+    UNAUTHORIZED=('Unauthorized', 401),
+    FORBIDDEN=('Forbidden', 403),
+    NOT_FOUND=('Not Found', 404),
+    METHOD_NOT_ALLOWED=('Method Not Allowed', 405),
+    NOT_ACCEPTABLE=('Not Acceptable', 406),
+    CONFLICT=('Conflict/Duplicate', 409),
+    NOT_HERE=('Gone', 410),
+    INTERNAL_SERVER_ERROR=('Internal Server Error', 500),
+    NOT_IMPLEMENTED=('Not Implemented', 501),
+    THROTTLED=('Throttled', 503),
+    )
+
+
+class Codes(object):
+    pass
+codes = Codes()
+codes.__dict__.update(CODES)
+
+
+class Response(object):
+    def __init__(self, code, data, headers={}):
+        self.code = code
+        self.content = data
+        self.headers = headers
+
+    def get_code_num(self):
+        """ Return numerical status code. """
+        try:
+            return self.code[1]
+        except TypeError:
+            return self.code
+
+#
+# http.py ends here
---
+++ packages/devil/devil-0.8.tar.gz/devil-0.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='devil',
+    version='0.8',
+    author='Janne Kuuskeri (wuher)',
+    author_email='janne.kuuskeri@gmail.com',
+    url='https://github.com/wuher/devil/',
+    packages=['devil', 'devil.perm', 'devil.mappers'],
+    install_requires=['simplejson>=2.1.0', 'django>=1.3.0'],
+    license='MIT',
+    description='Simple REST framework for Django',
+    long_description=open('README.md').read(),
+)
