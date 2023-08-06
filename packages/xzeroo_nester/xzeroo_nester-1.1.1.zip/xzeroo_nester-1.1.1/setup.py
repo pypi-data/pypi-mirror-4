a5cd448127f1b60ee6d146b9a023b0fd963cf4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:45 +0000

    Add package xzeroo_nester/1.1.1/xzeroo_nester-1.1.1.zip

---
+++ packages/xzeroo_nester/xzeroo_nester-1.1.1.zip/xzeroo_nester-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name ='xzeroo_nester',
+	version='1.1.1',
+	py_modules=['xzeroo_nester'],
+	author='Xzeroo Chan',
+	author_email='chenbing90@yeah.net',
+	url='http://www.renren.com/308355074'
+      )
+
