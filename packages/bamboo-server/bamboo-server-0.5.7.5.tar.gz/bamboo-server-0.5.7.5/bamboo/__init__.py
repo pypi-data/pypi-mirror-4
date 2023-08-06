ef6160c5b867dc141be0241e329cc24f58e183ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:52 +0000

    Add package bamboo-server/0.5.7.5/bamboo-server-0.5.7.5.tar.gz

---
+++ packages/bamboo-server/bamboo-server-0.5.7.5.tar.gz/bamboo-server-0.5.7.5/bamboo/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+from bamboo.core.frame import *
+from bamboo.lib.async import *
+
+# test settings
+TESTING = 'test' in sys.argv[0].split('/')[-1]
+
+os.environ['CELERY_CONFIG_MODULE'] = 'bamboo.config.celeryconfig'
+
+if TESTING:
+    set_async(False)
+    os.environ['CELERY_CONFIG_MODULE'] += '_test'
