f6f8c28031a37ea3689d1fc09ca9ce73bcabcd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:05 +0000

    Add package histogramy/0.1.1/histogramy-0.1.1.tar.gz

---
+++ packages/histogramy/histogramy-0.1.1.tar.gz/histogramy-0.1.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = "0.1.1"
+
+def read(filename):
+    import os.path
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+setup(
+    name="histogramy",
+    version=version,
+    description = "A small program to analysis 1 dimensional data",
+    long_description=read('README.md'),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords = "1-dimensional, analysis, histogram",
+    author = "Alisue",
+    author_email = "lambdalisue@hashnote.net",
+    url=r"https://github.com/lambdalisue/hisy",
+    download_url = r"https://github.com/lambdalisue/hisy/tarball/master",
+    license = 'MIT',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe = True,
+    install_requires=[
+        'setuptools',
+        'numpy', 'matplotlib',
+        'scikit-learn',
+    ],
+    entry_points={
+        'console_scripts': [
+            'histogramy = histogramy.main:main',
+        ],
+    },
+)
---
+++ packages/histogramy/histogramy-0.1.1.tar.gz/histogramy-0.1.1/src/histogramy/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+#
+# Author:   lambdalisue (lambdalisue@hashnote.net)
+# URL:      http://hashnote.net/
+# License:  MIT license
+# Created:  2013-01-23
+#
+import sys
+from numpy import concatenate
+
+from demo import demo
+from loader import load
+from simulator import fit
+from modulator import modulate_base, to_float
+from options import parse_args
+
+
+def analyze(opts):
+    # use sys.stdin insted if no filename is specified
+    if len(opts.filenames) == 0:
+        opts.filenames.append(sys.stdin)
+
+    if opts.demo:
+        data = [demo(), demo(), demo()]
+    else:
+        # load data file
+        data = [load(f, opts) for f in opts.filenames]
+    # transform to 1-dimensional array
+    data = concatenate(data, axis=0)
+
+    # modulate the data with base
+    data = modulate_base(data, opts.base)
+
+    # remove data with threshold
+    if opts.min_threshold:
+        data = data[data>opts.min_threshold]
+    if opts.max_threshold:
+        data = data[data<opts.max_threshold]
+
+    # convert to float
+    data = to_float(data)
+
+    # fitting
+    kwargs = dict(
+        n_components=opts.classifiers,
+        covariance_type=opts.covariance_type,
+        min_covar=opts.min_covar)
+    model, criterions = fit(data, **kwargs)
+
+    # call function
+    return opts.func(data, model, criterions, opts)
+
+
+def main(args=None):
+    # parse args
+    opts = parse_args(args)
+    # analyze
+    result = analyze(opts)
+
+    if opts.func.__name__ == 'plot':
+        # result = pyplot
+        if opts.output:
+            result.savefig(opts.output)
+        else:
+            try:
+                result.show()
+            except KeyboardInterrupt:
+                exit(0)
+
+
+if __name__ == '__main__':
+    main()
