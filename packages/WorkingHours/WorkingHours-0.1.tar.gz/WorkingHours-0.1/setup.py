61868d184b0dea7f058a831dfe275706c7cf9415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:50 +0000

    Add package WorkingHours/0.1/WorkingHours-0.1.tar.gz

---
+++ packages/WorkingHours/WorkingHours-0.1.tar.gz/WorkingHours-0.1/setup.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='WorkingHours',
+    version='0.1',
+    author='César Bustíos',
+    author_email='cesarbustios@gmail.com',
+    packages=['workinghours'],
+    url='http://pypi.python.org/pypi/WorkingHours/',
+    license='Unlicense License',
+    description='Calculate working hours between two dates',
+)
