42d150eb16482d743b4b15082f1a06968bf6c5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package dudu/0.0.2/dudu-0.0.2.tar.gz

---
+++ packages/dudu/dudu-0.0.2.tar.gz/dudu-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dudu',
+    scripts=['dudu'],
+    version='0.0.2',
+    description='get things done!',
+    author='Kadir Pekel',
+    author_email='kadirpekel@gmail.com',
+    url='https://github.com/kadirpekel/dudu',
+    install_requires=[
+        'komandr>=0.1.1'
+    ],
+)
