f320bcf02988e0c0e71b31fc1ea542b8e0aa58c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package getMysqlConnPara/1.0.2/getMysqlConnPara-1.0.2.tar.gz

---
+++ packages/getMysqlConnPara/getMysqlConnPara-1.0.2.tar.gz/getMysqlConnPara-1.0.2/getMysqlConnPara.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#檔名：getMysqlConnPara.py
+#version:1.0.1
+#change log:修正return，改為輸出dict型態
+
+import os
+
+def getMysqlConnPara(pathString):
+	string ={'user':'',
+		'password':'',
+		'host':'',
+		'database':''}
+	i = 0
+	start = 0
+	stringCom = ''
+	try:#如果檔案不存在，如指定的檔案不存在，回報「指定檔案路徑錯誤或不存在」	
+		with open(pathString)as data:
+			while 1:
+				line = data.readline()
+				#line是矩陣
+				if not line :#line == EOF
+					break
+				else:
+					#conf檔案允許加入註解，一行的首字如果為 # ，表示註解，忽略不處理
+					if line[0] == "#":
+						continue
+					else:
+						#參數不足，如果不足四個參數，回報「參數不足」
+						if str(line).find("=") > 0:
+							#print str(line).find("=")
+							#格式錯誤，例如passwod='123456'，回報「格式錯誤」
+							index = str(line).find("=", start)
+							stringCom = str(line[0:index])
+							if not (stringCom == "host" or stringCom == "user" or stringCom == "password" or stringCom == "database") :
+								print "格式錯誤！"
+								print stringCom
+								break
+							if index > 0 :
+								i = i+1
+							#不是空白行時
+							if not line == "\n" :
+								line = line.split()[0].split('=')
+								string[line[0]] = line[1]
+						elif str(line).find("=") < 0 and len(line) > 1:
+							print line+"此為非註解且非參數的內容，請檢查是否正確。"
+							continue
+			if i < 4:
+				print('參數不足')
+				return ''
+			elif i > 4:
+				print('參數過多')
+				return ''
+		#因加上逗號之故，所以要去除掉最前面的逗號才開始列印字串
+		#string = string[1:len(string)]
+		return string
+	except IOError:
+		print('指定檔案路徑錯誤或不存在')
---
+++ packages/getMysqlConnPara/getMysqlConnPara-1.0.2.tar.gz/getMysqlConnPara-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		=	'getMysqlConnPara',
+	version		=	'1.0.2',
+	py_modules	=	['getMysqlConnPara'],
+	author		=	'mingtak',
+	author_email	=	'andy@mingtak.com.tw',
+	url		=	'http://www.mingtak.com.tw',
+	description	=	'Mysql connection parameters defined in a text file, reads the parameter file and return the connection parameter string',
+)
