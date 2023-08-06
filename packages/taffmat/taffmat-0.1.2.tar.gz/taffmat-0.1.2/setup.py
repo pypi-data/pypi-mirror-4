bfe43bfbcac22b0fee3790901c4a2c9a66b0fecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:54 +0000

    Add package taffmat/0.1.2/taffmat-0.1.2.tar.gz

---
+++ packages/taffmat/taffmat-0.1.2.tar.gz/taffmat-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='taffmat',
+    version='0.1.2',
+    author='Matthew Rankin',
+    author_email='matthew@questrail.com',
+    py_modules=['taffmat'],
+    url='http://github.com/questrail/taffmat',
+    license='LICENSE.txt',
+    description='Read and write Teac TAFFmat files.',
+    requires=['numpy (>=1.6.0)'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
