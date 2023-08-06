38f295ba1899f6633e914483275957f50f8df73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package infi.recipe.application-packager/0.3.13/infi.recipe.application_packager-0.3.13-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.13-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.13"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '156078c299b8062239c22828a9f3785b8475db0b'
+__git_head_subject__ = 'HOSTDEV-699 adding support for rpm-dependencies'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
