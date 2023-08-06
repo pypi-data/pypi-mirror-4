e1ac6374b8015de47ac0bba3a85b19610d148fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package bottle-haml/0.1.1/bottle-haml-0.1.1.tar.gz

---
+++ packages/bottle-haml/bottle-haml-0.1.1.tar.gz/bottle-haml-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+dependencies = [
+    'bottle',
+    'pyhaml'
+]
+
+setup(
+    name="bottle-haml",
+    packages=["bottlehaml",],
+    install_requires=dependencies,
+    version="0.1.1",
+    author="Zachary Jones",
+    author_email="me@zacharytamas.com",
+    license="BSD",
+    url="https://github.com/wireload/bottle-haml",
+    long_description=open('README.md').read(),
+    package_data = {
+        '': ['*.md', '*.txt']
+    }
+)
