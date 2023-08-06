63cfdbb6109606d182802dd3c3f1cf4a6fd1f7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:22 +0000

    Add package medialog.qrcode/0.1/medialog.qrcode-0.1.tar.gz

---
+++ packages/medialog.qrcode/medialog.qrcode-0.1.tar.gz/medialog.qrcode-0.1/README.txt
@@ -0,0 +1,13 @@
+Introduction
+============
+
+This product adds a browser view that generates a QRcode for the given page.
+
+Use it like this:
+
+http://mysite.com/mypage/@@qrimage
+To get a qrcode for http://mysite.com/mypage 
+
+Or:
+http://mysite.com/mypage/@@qrimage?view=myview
+To get a qrcode for http://mysite.com/mypage/myview
---
+++ packages/medialog.qrcode/medialog.qrcode-0.1.tar.gz/medialog.qrcode-0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='medialog.qrcode',
+      version=version,
+      description="QRcode views for plone 4 (ONLY).",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'qrcode',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
