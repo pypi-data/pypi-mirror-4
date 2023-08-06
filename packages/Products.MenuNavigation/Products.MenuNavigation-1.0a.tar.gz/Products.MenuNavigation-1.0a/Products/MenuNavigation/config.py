06fccd8cc26cbd45b2470718de8e4a5885ae2509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package Products.MenuNavigation/1.0a/Products.MenuNavigation-1.0a.tar.gz

---
+++ packages/Products.MenuNavigation/Products.MenuNavigation-1.0a.tar.gz/Products.MenuNavigation-1.0a/Products/MenuNavigation/config.py
@@ -0,0 +1,9 @@
+from Products.CMFCore.permissions import AddPortalContent
+from Products.Archetypes.public import DisplayList
+
+ADD_CONTENT_PERMISSION = AddPortalContent
+PROJECTNAME = "MenuNavigation"
+SKINS_DIR = 'skins'
+
+GLOBALS = globals()
+
