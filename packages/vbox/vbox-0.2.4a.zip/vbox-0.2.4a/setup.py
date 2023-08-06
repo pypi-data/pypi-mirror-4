95394dfb020fec3de56a1f130d965dd02e0c6af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:01 +0000

    Add package vbox/0.2.4a/vbox-0.2.4a.zip

---
+++ packages/vbox/vbox-0.2.4a.zip/vbox-0.2.4a/setup.py
@@ -0,0 +1,45 @@
+import os
+from distutils.core import setup
+
+def is_package(path):
+    return (
+        os.path.isdir(path) and
+        os.path.isfile(os.path.join(path, '__init__.py'))
+        )
+
+def find_packages(path, base="" ):
+    """ Find all packages in path """
+    packages = {}
+    for item in os.listdir(path):
+        dir = os.path.join(path, item)
+        if is_package( dir ):
+            if item.lower() == "tests":
+                # Ignore 'tests' packages
+                continue
+            if base:
+                module_name = "%(base)s.%(item)s" % vars()
+            else:
+                module_name = item
+            packages[module_name] = dir
+            packages.update(find_packages(dir, module_name))
+    return packages
+
+setup(name='vbox',
+    version='0.2.4a',
+    description="VirtualBox CLI bindings",
+    long_description=open("README.rst").read(),
+
+    author="Ilja Orlovs",
+    author_email="vywn@gryyf.fb".decode("rot13"),
+    url='https://github.com/VRGhost/vbox',
+    packages=find_packages("src"),
+    package_dir = {'': 'src'},
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ])
---
+++ packages/vbox/vbox-0.2.4a.zip/vbox-0.2.4a/src/vbox/cli/__init__.py
@@ -0,0 +1,5 @@
+# Command-line interface bindings
+
+from . import util
+from .subCmd import CmdError
+from .cliInterface import CommandLineInterface
