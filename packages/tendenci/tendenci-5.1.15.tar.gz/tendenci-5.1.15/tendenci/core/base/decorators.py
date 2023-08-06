a359d8d7f68e036e8750c2f73b8c2f77b69067de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:08 +0000

    Add package tendenci/5.1.15/tendenci-5.1.15.tar.gz

---
+++ packages/tendenci/tendenci-5.1.15.tar.gz/tendenci-5.1.15/tendenci/core/base/decorators.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.http import urlquote
+from django.shortcuts import redirect
+
+
+def ssl_required(view_func):
+    """Decorator to force url to be accessed over SSL (https).
+    """
+    def decorator(request, *args, **kwargs):
+        if not any([request.is_secure(), request.META.get("HTTP_X_FORWARDED_PROTO", "") == 'https']):
+            if getattr(settings, 'SSL_ENABLED', False):
+                request_url = request.build_absolute_uri(request.get_full_path())
+                ssl_url = request_url.replace('http://', 'https://')
+                return HttpResponseRedirect(ssl_url)
+        return view_func(request, *args, **kwargs)
+    return decorator
+
+
+def password_required(view):
+    """Decorator to force a password promt"""
+    def decorator(request, *args, **kwargs):
+        if request.session.get('password_promt', False):
+            return view(request, *args, **kwargs)
+        return redirect(("%s?next=%s") % (reverse("password_again"), urlquote(request.get_full_path())))
+    return decorator
