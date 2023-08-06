5a75c7272cbe011c32be8d7f1ab42167cf596e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:40 +0000

    Add package zope.i18n/4.0.0a4/zope.i18n-4.0.0a4.zip

---
+++ packages/zope.i18n/zope.i18n-4.0.0a4.zip/zope.i18n-4.0.0a4/setup.py
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.i18n package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return text
+
+setup(
+    name='zope.i18n',
+    version='4.0.0a4',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Zope Internationalization Support',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords=('zope3 internationalization localization i18n l10n '
+              'gettext ICU locale'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.i18n',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope',],
+    install_requires=['setuptools',
+                      'pytz',
+                      'zope.schema',
+                      'zope.i18nmessageid',
+                      'zope.component',
+                      ],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
+        test=['zope.testing'],
+        compile=['python-gettext'],
+        zcml=[
+            'zope.component [zcml]',
+            'zope.configuration',
+            'zope.security',
+            ],
+        ),
+    )
