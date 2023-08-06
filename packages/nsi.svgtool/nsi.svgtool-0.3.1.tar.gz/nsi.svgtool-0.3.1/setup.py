4c5c6f90373b2fe47f17f0f17238b92eeecea7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:26 +0000

    Add package nsi.svgtool/0.3.1/nsi.svgtool-0.3.1.tar.gz

---
+++ packages/nsi.svgtool/nsi.svgtool-0.3.1.tar.gz/nsi.svgtool-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.3.1'
+
+setup(name='nsi.svgtool',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Leandro Cruz',
+      author_email='lmvcruz@gmail.com',
+      url='http://nsi.iff.edu.br',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['nsi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
