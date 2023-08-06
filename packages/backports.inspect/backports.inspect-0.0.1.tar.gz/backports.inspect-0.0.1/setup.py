55ce7de914d6f4a84754a00fb8f19845dbce7b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:23 +0000

    Add package backports.inspect/0.0.1/backports.inspect-0.0.1.tar.gz

---
+++ packages/backports.inspect/backports.inspect-0.0.1.tar.gz/backports.inspect-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='backports.inspect',
+    version='0.0.1',
+
+    author='Tripp Lilley',
+    author_email='tripplilley@gmail.com',
+    url='https://github.com/agoraplex/backports.inspect',
+
+    license='BSD',
+    description='Backport (some) Python 3 `inspect` module features to 2.7 (extending aliles/funcsigs)',
+    long_description=open('README.rst').read(),
+
+    packages=[
+        'backports',
+        ],
+    namespace_packages=[
+        'backports',
+        ],
+
+    install_requires=[
+        'distribute',
+        'funcsigs==0.2',
+        ],
+    tests_require=[
+        ],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
