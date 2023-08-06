a1c21a3b7fb26f71af87811ba2273c13cd3fafa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:54 +0000

    Add package bearcart/0.1.0/bearcart-0.1.0.tar.gz

---
+++ packages/bearcart/bearcart-0.1.0.tar.gz/bearcart-0.1.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='bearcart',
+    version='0.1.0',
+    description='Python Pandas + Rickshaw.js = BearCart',
+    author='Rob Story',
+    author_email='wrobstory@gmail.com',
+    license='MIT License',
+    url='https://github.com/wrobstory/bearcart',
+    keywords='data visualization',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: MIT License'],
+    packages=['bearcart'],
+    package_data={'': ['*.js',
+                       '*.css',
+                       'templates/*.html',
+                       'templates/*.js']}
+)
