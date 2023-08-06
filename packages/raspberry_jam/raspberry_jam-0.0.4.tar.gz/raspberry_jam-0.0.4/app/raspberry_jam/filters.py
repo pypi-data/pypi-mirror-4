5a69b378829566f7b91762497a4992758f234fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:57 +0000

    Add package raspberry_jam/0.0.4/raspberry_jam-0.0.4.tar.gz

---
+++ packages/raspberry_jam/raspberry_jam-0.0.4.tar.gz/raspberry_jam-0.0.4/app/raspberry_jam/filters.py
@@ -0,0 +1,43 @@
+from raspberry_jam import app, models
+
+class Filter:
+    def filter_track(self, track, table):
+        if table == 'bucket':
+            return self._bucket_filter(track)
+        elif table == 'library':
+            return self._library_filter(track)
+        return False
+
+    def popularity(self, track):
+        try:
+            if track.favoritings_count > 0 and track.playback_count > 0:
+                return float(track.favoritings_count) / float(track.playback_count)
+            else:
+                return 0
+        except Exception, e:
+            app.logger.debug('err')
+            return 0
+
+    def unique(self, track_sc_id, table):
+        m = models.Storage()
+        if table == 'bucket' :
+            if not m.lookup_bucket(track_sc_id):
+                return True
+            else:
+                return False
+        elif table == 'library' :
+            if not m.lookup_library(track_sc_id):
+                return True
+            else:
+                return False
+
+    def _bucket_filter(self, track):
+        if hasattr(track, 'stream_url'):
+            return True
+        return False
+
+    def _library_filter(self, track):
+        if self.popularity(track) > 0 and self.unique(track.id, 'library'):
+            return True
+        return False
+
---
+++ packages/raspberry_jam/raspberry_jam-0.0.4.tar.gz/raspberry_jam-0.0.4/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "raspberry_jam",
+    version = "0.0.4",
+    author = "Ralph Saunders",
+    author_email = "ralph@ralphsaunders.co.uk",
+    description = ("Raspberry Jam for music"),
+    license = "Private",
+    keywords = "music",
+    url = "http://example.com",
+    packages=['app', 'app.raspberry_jam', 'app.spider'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: Other/Proprietary License",
+    ],
+    install_requires = [
+        "Flask==0.9",
+        "Jinja2==2.6",
+        "Werkzeug==0.8.3",
+        "certifi==0.0.8",
+        "chardet==2.1.1",
+        "fudge==1.0.3",
+        "nose==1.1.2",
+        "psycopg2==2.4.5",
+        "readline==6.2.4.1",
+        "requests==0.10.1",
+        "simplejson==2.6.2",
+        "soundcloud==0.3.1",
+        "tornado==2.4",
+        "wsgiref==0.1.2"
+    ]
+)
