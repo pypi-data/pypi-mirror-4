1adad50934525e4206cb3524607682c5a99744cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package adrest/2.2.1/adrest-2.2.1.tar.gz

---
+++ packages/adrest/adrest-2.2.1.tar.gz/adrest-2.2.1/tests/core/tests/__init__.py
@@ -0,0 +1,13 @@
+from ...main.tests import *
+from ...rpc.tests import *
+from ...simple.tests import *
+
+from .api import *
+from .handler import *
+from .resource import *
+from .serializer import *
+from .utils import *
+from .test import *
+
+
+# lint_ignore=W404
