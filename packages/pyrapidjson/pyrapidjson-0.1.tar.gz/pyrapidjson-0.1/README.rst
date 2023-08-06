893ddf85cbdd9788c5ade31b40e1da2aa5952bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:05 +0000

    Add package pyrapidjson/0.1/pyrapidjson-0.1.tar.gz

---
+++ packages/pyrapidjson/pyrapidjson-0.1.tar.gz/pyrapidjson-0.1/README.rst
@@ -0,0 +1,53 @@
+pyrapidjson
+===========
+.. image:: https://secure.travis-ci.org/hhatto/pyrapidjson.png?branch=master
+   :target: https://secure.travis-ci.org/hhatto/pyrapidjson
+   :alt: Build status
+
+
+About
+-----
+pyrapidjson is a wrapper for `rapidjson`_ (JSON parser/generator).
+
+.. _`rapidjson`: http://code.google.com/p/rapidjson/
+
+
+Installation
+------------
+from pip::
+
+    $ pip install pyrapidjson
+
+from easy_install::
+
+    easy_install -ZU pyrapidjson
+
+
+Requirements
+------------
+Python2.7+
+
+
+Usage
+-----
+
+basic usage::
+
+    >>> import rapidjson
+    >>> rapidjson.loads('[1, 2, {"test": "hoge"}]')
+    >>> [1, 2, {"test": "hoge"}]
+    >>> rapidjson.dumps([1, 2, {"foo": "bar"}])
+    '[1,2,{"foo":"bar"}]'
+    >>>
+
+
+Links
+-----
+* PyPI_
+* GitHub_
+* `Travis-CI`_
+
+.. _PyPI: http://pypi.python.org/pypi/pyrapidjson/
+.. _GitHub: https://github.com/hhatto/pyrapidjson
+.. _`Travis-CI`: https://secure.travis-ci.org/hhatto/pyrapidjson
+
---
+++ packages/pyrapidjson/pyrapidjson-0.1.tar.gz/pyrapidjson-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup, Extension
+
+
+setup(
+    name='pyrapidjson',
+    version='0.1',
+    description='Python Interface for rapidjson(JSON parser and generator).',
+    long_description=open('README.rst').read(),
+    license='Expat License',
+    author='Hideo Hattori',
+    author_email='hhatto.jp@gmail.com',
+    url='https://github.com/hhatto/pyrapidjson',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='json rapidjson',
+    ext_modules=[
+        Extension('rapidjson',
+                  sources=['pyrapidjson/_pyrapidjson.cpp'],
+                  include_dirs=['./pyrapidjson/include/'],
+                  #extra_compile_args=["-DDEBUG"],
+                  )]
+)
