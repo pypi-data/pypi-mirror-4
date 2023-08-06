987e5c351a87706e255f0cc8fc32177af7fd7122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package virtualenvwrapper.alex/0.1/virtualenvwrapper.alex-0.1.tar.gz

---
+++ packages/virtualenvwrapper.alex/virtualenvwrapper.alex-0.1.tar.gz/virtualenvwrapper.alex-0.1/virtualenvwrapper/alex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2013 Doug Hellmann.  All rights reserved.
+#
+"""Define aliases for common typos for virtualenvwrapper commands.
+"""
+
+import logging
+import subprocess
+
+log = logging.getLogger('virtualenvwrapper.alex')
+
+def initialize_source(args):
+    """Installs aliases
+    """
+    return """
+alias wokr=workon
+alias kmvirtualenv=mkvirtualenv
+alias mrvirtualenv=rmvirtualenv
+alias slvirtualenv=lsvirtualenv
+alias pcvirtualenv=cpvirtualenv
+"""
