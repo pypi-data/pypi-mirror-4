4da41215eb0108a0b610a874003930608e65e7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package python-gettext/2.1/python-gettext-2.1.zip

---
+++ packages/python-gettext/python-gettext-2.1.zip/python-gettext-2.1/README.rst
@@ -0,0 +1,28 @@
+Overview
+========
+
+This implementation of Gettext for Python includes a Msgfmt class which can be
+used to generate compiled mo files from Gettext po files and includes support
+for the newer msgctxt keyword.
+
+The idea for this project had been rather ambitious, but never lived up to what
+is was supposed to do. Look at Babel (http://pypi.python.org/pypi/Babel) for a
+project more worthy of this projects name.
+
+Development takes place at https://github.com/hannosch/python-gettext
+
+Current Development CI Status
+-----------------------------
+
+.. image:: https://travis-ci.org/hannosch/python-gettext.png?branch=master
+   :width: 82px
+   :height: 13px
+   :alt: Travis CI build report
+   :target: https://travis-ci.org/hannosch/python-gettext
+
+Contributors
+------------
+
+* Hanno Schlichting
+* Christian Heimes
+* Andrei Polushin
---
+++ packages/python-gettext/python-gettext-2.1.zip/python-gettext-2.1/setup.py
@@ -0,0 +1,50 @@
+import os.path
+import sys
+from setuptools import setup
+
+version = '2.1'
+
+PY3 = sys.version_info[0] == 3
+install_requires = []
+if not PY3:
+    install_requires = ['unittest2']
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+setup(
+    name='python-gettext',
+    version=version,
+    description="Python Gettext po to mo file compiler.",
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+    ],
+    keywords='Python Gettext Msgctxt',
+    author='Hanno Schlichting',
+    author_email='hanno@hannosch.eu',
+    url='http://pypi.python.org/pypi/python-gettext',
+    license='BSD',
+    packages=['pythongettext', 'pythongettext.tests'],
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="pythongettext.tests",
+)
