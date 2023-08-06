695b5dee4d31bd3c4966fa744f28297135adf569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:09 +0000

    Add package hmako/1.16/hmako-1.16.tar.gz

---
+++ packages/hmako/hmako-1.16.tar.gz/hmako-1.16/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+version = '1.16'
+
+setup(name='hmako',
+      version=version,
+      description="a hack version mako(0.74) for html , will remove empty line",
+      keywords='wsgi myghty mako',
+      author='zsp',
+      author_email='zsp007@gmail.com',
+      license='MIT',
+      packages = ["hmako","hmako.ext"],
+      zip_safe=False
+)
