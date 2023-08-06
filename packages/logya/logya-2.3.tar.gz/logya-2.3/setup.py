9ce10edc026e1363ff225e429f0339e042109192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:31 +0000

    Add package logya/2.3/logya-2.3.tar.gz

---
+++ packages/logya/logya-2.3.tar.gz/logya-2.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+def get_package_data():
+    """Return a list of files in sites dir to include in package."""
+
+    dir_base = os.path.join(os.getcwd(), 'logya')
+    dir_sites = os.path.join(dir_base, 'sites')
+    sites_files = []
+    for root, dirs, files in os.walk(dir_sites):
+        sites_files.extend([os.path.join(root, f).replace(dir_base, '').lstrip('/') for f in files])
+    return sites_files
+
+setup(
+    name='logya',
+    version='2.3',
+    description='Logya is a static Web site generator written in Python designed to be easy to use and flexible.',
+    long_description=readme,
+    url='http://yaph.github.com/logya/',
+    author='Ramiro Gómez',
+    author_email='code@ramiro.org',
+    maintainer='Ramiro Gómez',
+    maintainer_email='code@ramiro.org',
+    keywords=['Website Generator'],
+    license=license,
+    packages=find_packages(),
+    package_data={'logya': get_package_data()},
+    install_requires = required,
+    #test_suite='tests.all_tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Site Management'],
+    entry_points={
+        'console_scripts': [
+            'logya = logya.main:main'
+        ]
+    }
+)
---
+++ packages/logya/logya-2.3.tar.gz/logya-2.3/tests/test_extensions.py
@@ -0,0 +1,24 @@
+from __init__ import TestLogya
+
+from logya.ext import ExtensionLoader
+
+
+class TestExtensions(TestLogya):
+
+    def setUp(self):
+        self.extension_loader = ExtensionLoader()
+
+    def test_extension_directories(self):
+        extensions = {
+            'disqus': 'logya/ext/disqus',
+            'sitemap': 'logya/ext/sitemap',
+        }
+        # the code below sucks
+        for e in self.extension_loader.get_by_type('doc'):
+            name = e.get_module_name()
+            if 'disqus' == name:
+                self.assertTrue(e.get_directory().endswith(extensions[name]))
+        for e in self.extension_loader.get_by_type('index'):
+            name = e.get_module_name()
+            if 'sitemap' == name:
+                self.assertTrue(e.get_directory().endswith(extensions[name]))
