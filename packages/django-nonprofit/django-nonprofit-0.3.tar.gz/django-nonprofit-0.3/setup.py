2d720aa2844bf4990356194909a0b175fb729bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:39 +0000

    Add package django-nonprofit/0.3/django-nonprofit-0.3.tar.gz

---
+++ packages/django-nonprofit/django-nonprofit-0.3.tar.gz/django-nonprofit-0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-nonprofit',
+    version='0.3',
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@sunlightfoundation.com',
+    description='Django app for managing and publishing basic information related to a non-profit organization.',
+    long_description=open("README.rst").read(),
+    url='http://github.com/sunlightlabs/django-nonprofit/',
+    license='BSD',
+    packages=find_packages(),
+    package_data={
+        'nonprofit.mailroom': ['templates/mailroom/*'],
+        'nonprofit.helpwanted': ['templates/helpwanted/feeds/*'],
+    },
+    install_requires=[
+        "PIL",
+        "South == 0.7.6",
+        "tablib == 0.9.11",
+        "django-tablib == 2.4.1",
+        "django-tastypie == 0.9.14",
+        "django-humanity == 0.2",
+    ],
+)
