7fede666d43457575f962f842d316135db729192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:26 +0000

    Add package uwosh.portaltabssearchbox/0.1.10/uwosh.portaltabssearchbox-0.1.10.tar.gz

---
+++ packages/uwosh.portaltabssearchbox/uwosh.portaltabssearchbox-0.1.10.tar.gz/uwosh.portaltabssearchbox-0.1.10/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.10'
+
+setup(name='uwosh.portaltabssearchbox',
+      version=version,
+      description="moves search box into portal tabs",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='portal tabs,search box,viewlet',
+      author='T. Kim Nguyen',
+      author_email='nguyen@uwosh.edu',
+      url='http://plone.org/products/uwosh.portaltabssearchbox',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['uwosh'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+#      setup_requires=["PasteScript"],
+#      paster_plugins=["ZopeSkel"],
+      )
