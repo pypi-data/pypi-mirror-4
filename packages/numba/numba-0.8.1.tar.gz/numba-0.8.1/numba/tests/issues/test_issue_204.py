b963a66d3e7da13ae8e95257e54998babab6656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package numba/0.8.1/numba-0.8.1.tar.gz

---
+++ packages/numba/numba-0.8.1.tar.gz/numba-0.8.1/numba/tests/issues/test_issue_204.py
@@ -0,0 +1,14 @@
+from numba import autojit, jit
+
+@autojit
+def closure_modulo(a, b):
+    @jit('int32()')
+    def foo():
+        return a % b
+    return foo()
+
+def test_closure_modulo():
+    assert closure_modulo(100, 48) == 4
+
+if __name__ == '__main__':
+    test_closure_modulo()
