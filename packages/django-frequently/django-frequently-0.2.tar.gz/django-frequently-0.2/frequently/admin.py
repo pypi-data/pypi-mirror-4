fd2f87b44ed288754b34fcd84efc075361ae5462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:09 +0000

    Add package django-frequently/0.2/django-frequently-0.2.tar.gz

---
+++ packages/django-frequently/django-frequently-0.2.tar.gz/django-frequently-0.2/frequently/admin.py
@@ -0,0 +1,23 @@
+"""Admin sites for the ``django-frequently`` app."""
+from django.contrib import admin
+
+from frequently import models
+
+
+class EntryCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('entry', 'user_email', 'submission_date', 'validation')
+
+    def user_email(self, obj):
+        if obj.user and obj.user.email:
+            return "{0}".format(obj.user.email)
+        return ""
+    user_email.short_description = 'Email'
+
+
+admin.site.register(models.Entry)
+admin.site.register(models.EntryCategory, EntryCategoryAdmin)
+admin.site.register(models.Feedback, FeedbackAdmin)
