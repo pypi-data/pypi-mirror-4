3964757d0a30377fcc8e9e340e5d342a491ceab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:51 +0000

    Add package collagram/0.2/collagram-0.2.tar.gz

---
+++ packages/collagram/collagram-0.2.tar.gz/collagram-0.2/collagram/__init__.py
@@ -0,0 +1,153 @@
+import os
+import cStringIO
+from datetime import datetime
+import urllib
+
+from instagram.client import InstagramAPI
+import Image
+
+INSTAGRAM_TOKEN = os.environ.get('INSTAGRAM_TOKEN')
+PATH_USERS = os.path.join(os.path.dirname(__file__), 'users')
+PATH_TAGS = os.path.join(os.path.dirname(__file__), 'tags')
+SIZE_DICT = {
+    'thumbnail': 150,
+    'low_resolution': 306,
+    'standard_resolution': 612
+}
+
+
+class MissingTokenError(Exception):
+    pass
+
+
+class Collage(object):
+
+    def __init__(self, username=None, tag=None, token=INSTAGRAM_TOKEN,
+                 columns=10, rows=2, size='thumbnail',  path_users=PATH_USERS,
+                 path_tags=PATH_TAGS):
+
+        if username and tag:
+            raise AttributeError('You may not specify both username and tag')
+
+        if not username and not tag:
+            raise AttributeError('You must specify either username and tag')
+
+        if token is None:
+            raise MissingTokenError('INSTAGRAM_TOKEN environment variable or the `token` attribute must be set.')
+
+        self.api = InstagramAPI(access_token=token)
+
+        self.username = username
+        self.tag = tag
+        self.path_users = path_users
+        self.path_tags = path_tags
+        self.columns = columns
+        self.rows = rows
+        self.size = size
+
+    @property
+    def name(self):
+        """Return canonical name
+
+        Username or tag prefixed with @ or # respectively.
+        """
+        if self.username:
+            return '@%s' % self.username
+        elif self.tag:
+            return '#%s' % self.tag
+        else:
+            return None
+
+    @property
+    def user_id(self):
+        """Retrieve user id from username
+
+        Search for username via api and then check for matching
+        username. If found return id, otherwise throw 404.
+        """
+        if self.username:
+            rv = self.api.user_search(q=self.username)
+            for result in rv:
+                if self.username == result.username:
+                    return result.id
+        return None
+
+    @property
+    def filename(self):
+        if self.username:
+            return os.path.join(self.path_users, "%s.jpg" % self.username)
+        elif self.tag:
+            return os.path.join(self.path_tags, "%s.jpg" % self.tag)
+        else:
+            return None
+
+    def is_cached(self):
+        """Check for existing version of file"""
+
+        return True if os.path.exists(self.filename) else False
+
+    def is_stale(self, seconds=60):
+        """Check if the cached file is a certain age
+
+        Check if the file is younger than the number of seconds
+        specified. Return True for youger, False for older.
+        """
+
+        modified = datetime.fromtimestamp(os.path.getmtime(self.filename))
+        age = datetime.now() - modified
+        fresh = True if age.seconds < seconds else False
+        return fresh
+
+    def fetch(self, force=False):
+        """Return filename, generating if necessary
+
+        Check if there is a chached version. If a cached version
+        doesn't exists or `force` is True we generate one. Otherwise
+        we just return the filename.
+
+        """
+
+        if not self.is_cached() or force:
+            self.generate()
+        return self.filename
+
+    def media_json(self):
+        """Username/tag specific JSON from instagram's API."""
+
+        if self.username:
+            return self.api.user_recent_media(user_id=self.user_id)[0]
+        elif self.tag:
+            return self.api.tag_recent_media(tag_name=self.tag)[0]
+        else:
+            return None
+
+    def media_urls(self):
+        """List of instagram image URLS"""
+
+        return [m.images[self.size].url for m in self.media_json()]
+
+    def generate(self):
+        """Generate a collage
+
+        Use instagram api to get the recent media json for a username
+        or tag. Then download the images and stick them in temp files
+        and places them into a blank image. The image is saved to disk
+        after all collage is created.
+        """
+
+        dimension = self.SIZE_DICT.get(self.size, 150)
+        blank_image = Image.new("RGB",
+                                (self.columns * dimension,
+                                 self.rows * dimension))
+
+        x = y = 0
+        for idx, url in enumerate(self.media_urls()):
+            image_from_url = cStringIO.StringIO(urllib.urlopen(url).read())
+            image = Image.open(image_from_url)
+            blank_image.paste(image, (x, y))
+            x += dimension
+            if idx == (self.columns - 1):
+                x = 0
+                y += dimension
+
+        blank_image.save(self.filename)
---
+++ packages/collagram/collagram-0.2.tar.gz/collagram-0.2/collagram/test/tests.py
@@ -0,0 +1,50 @@
+import unittest
+from collagram import Collage
+
+
+USER = 'adamrt'
+USER_ID = '19226776'
+TAG = 'vurbranch'
+BAD_USER = 'jfjfjfjfjfjfjfjfij'
+PATH_USERS = '/srv/www/collagram.com/media/users/'
+PATH_TAGS = '/srv/www/collagram.com/media/tags/'
+
+
+class TestCollage(unittest.TestCase):
+
+    def setUp(self):
+        self.user = Collage(username=USER)
+        self.bad_user = Collage(username=BAD_USER)
+        self.tag = Collage(tag=TAG)
+        self.size = Collage(username=USER, columns=4, rows=4)
+        self.user_path = Collage(username=USER, path_users=PATH_USERS)
+        self.tag_path = Collage(tag=TAG, path_tags=PATH_TAGS)
+
+    def test_attributes(self):
+        self.assertRaises(AttributeError, Collage)
+        self.assertRaises(AttributeError, Collage,
+                          username=USER,
+                          tag=TAG)
+
+    def test_columns_and_rows(self):
+        self.assertEqual(self.user.columns, 10)
+        self.assertEqual(self.user.rows, 2)
+
+        self.assertEqual(self.size.columns, 4)
+        self.assertEqual(self.size.rows, 4)
+
+    def test_name(self):
+        self.assertEqual(self.user.name, '@' + USER)
+        self.assertEqual(self.tag.name, '#' + TAG)
+
+    def test_user_id(self):
+        self.assertEqual(self.user.user_id, USER_ID)
+        self.assertEqual(self.bad_user.user_id, None)
+        self.assertEqual(self.tag.user_id, None)
+
+    def test_filename(self):
+        assert self.user.filename.endswith('users/' + USER + '.jpg')
+        self.assertEqual(self.user_path.filename, PATH_USERS + USER + '.jpg')
+
+if __name__ == '__main__':
+    unittest.main()
