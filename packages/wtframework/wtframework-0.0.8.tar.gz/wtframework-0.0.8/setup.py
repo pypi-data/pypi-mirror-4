44fcd1631733efe35a02d9b6607e49dfe69a1bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package wtframework/0.0.8/wtframework-0.0.8.tar.gz

---
+++ packages/wtframework/wtframework-0.0.8.tar.gz/wtframework-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    name='wtframework',
+    version='0.0.8',
+    author='David Lai',
+    author_email='david@wiredrive.com',
+    packages=['wtframework',
+              'wtframework.wtf',
+              'wtframework.wtf.assettools',
+              'wtframework.wtf.config',
+              'wtframework.wtf.email',
+              'wtframework.wtf.utils',
+              'wtframework.wtf.testobjects',
+              'wtframework.wtf.web',
+              'wtframework.wtf._devtools_',
+              'wtframework.wtf._devtools_.filetemplates',
+              ],
+    scripts=['bin/wtf_init.py', 'bin/wtf_tools.py'],
+    url='https://github.com/wiredrive/wtframework',
+    license='LICENSE.txt',
+    description='WTF - Web Test Framework',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Mox>=0.5.3",
+        "nose>=1.2.1",
+        "NoseXUnit>=0.3.3",
+        "ddt>=0.2.0",
+        "pyyaml>=3.10",
+        "selenium>=2.29.0",
+        "python-rest-client>=0.3",
+    ],
+)
