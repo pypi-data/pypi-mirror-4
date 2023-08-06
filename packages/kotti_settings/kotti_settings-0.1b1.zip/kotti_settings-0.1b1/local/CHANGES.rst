37e7efc6a17241f0047d52400b855b75f4bbffc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:20 +0000

    Add package kotti_settings/0.1b1/kotti_settings-0.1b1.zip

---
+++ packages/kotti_settings/kotti_settings-0.1b1.zip/kotti_settings-0.1b1/local/CHANGES.rst
@@ -0,0 +1,25 @@
+Changelog
+=========
+
+0.1b1(2013-03-29)
+-----------------
+
+* Added event handling for callbacks before and after the settings are saved.
+
+
+0.1a3(2013-03-13)
+-----------------
+
+* Only take the module name itself into account for the setting name.
+
+
+0.1a2(2013-03-11)
+-----------------
+
+* Added util method get_setting as default way to get a setting.
+
+
+0.1a1(2013-03-06)
+-----------------
+
+- Initial release.
---
+++ packages/kotti_settings/kotti_settings-0.1b1.zip/kotti_settings-0.1b1/local/kotti_settings/tests/test_util.py
@@ -0,0 +1,61 @@
+import pytest
+
+
+def test_add_settings_with_missing_attributes(db_session):
+    from kotti_settings.util import add_settings
+
+    TestSettings = {
+        'description': "Some settings in a dict.",
+        'name': 'a name',
+        'success_message': u"Successfully saved test settings.",
+        'settings': [
+            {'type': 'String',
+             'description': 'a test setting',
+             'default': 'my first string', },
+        ]
+    }
+    with pytest.raises(ValueError) as excinfo:
+        add_settings(TestSettings)
+    assert str(excinfo.value) == 'A setting has to have a name.'
+
+    TestSettings['settings'][0]['name'] = 'testsetting'
+    with pytest.raises(ValueError) as excinfo:
+        add_settings(TestSettings)
+    assert str(excinfo.value) == 'A setting has to have a title.'
+
+    TestSettings['settings'][0]['title'] = 'test title'
+    assert add_settings(TestSettings) == None
+
+
+def test_get_setting(db_session, root):
+    from kotti_settings.util import add_settings
+    from kotti_settings.util import get_setting
+    test_settings = {
+        'name': 'test-get-setting',
+        'title': "Some test settings.",
+        'settings': [
+            {'type': 'String',
+             'name': 'first_test_setting',
+             'title': 'first test setting',
+             'default': 'first test string', },
+            {'type': 'Integer',
+             'name': 'second_test_setting',
+             'title': 'second test setting',
+             'default': 5, },
+        ]
+    }
+    add_settings(test_settings)
+
+    first = get_setting('first_test_setting')
+    assert first == 'first test string'
+    second = get_setting('kotti_settings-second_test_setting')
+    assert second == 5
+
+
+def test_get_setting_not_found(db_session, root):
+    from kotti_settings.util import get_setting
+
+    setting = get_setting('not_exiting_setting')
+    assert setting == None
+    setting = get_setting('not_exiting_setting', 'default')
+    assert setting == 'default'
