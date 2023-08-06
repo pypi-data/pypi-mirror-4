67724f44d7b4f3204ec5d760d913e7c3245ccaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:59 +0000

    Add package parseli/0.0.2/parseli-0.0.2.tar.gz

---
+++ packages/parseli/parseli-0.0.2.tar.gz/parseli-0.0.2/parseli/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""parseli (save, rows o' json, and time)"""
+
+__version__ = "0.0.2"
+__author__ = [
+    "Mek <michael.karpeles@gmail.com>"
+]
+__license__ = "public domain"
+__contributors__ = "see AUTHORS"
+
+from parseli import getli, crawli, parseli
