7bd6331f4967b9c48c15ce931662beef9d747548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:06 +0000

    Add package teleport/0.0.3/teleport-0.0.3.tar.gz

---
+++ packages/teleport/teleport-0.0.3.tar.gz/teleport-0.0.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+with open("README.rst") as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'teleport',
+    version = "0.0.3",
+    py_modules = ['teleport'],
+    description = 'An extandable serialization system',
+    license = "MIT",
+    author_email = "alexei.boronine@gmail.com",
+    long_description = long_description
+)
