811911d76bf9190d2ef24ed642c6cfadab76b048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:27 +0000

    Add package infi.eventlog/0.1.2/infi.eventlog-0.1.2-py2.7.egg

---
+++ packages/infi.eventlog/infi.eventlog-0.1.2-py2.7.egg/infi/eventlog/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8f1d9c67f07a0b4396b99632d4c59b36aa735faf'
+__git_head_subject__ = 'empty commit after version v0.1.1'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
