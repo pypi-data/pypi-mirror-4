a6ffd08bb92910fb715d4da193f96640feb5879b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:52 +0000

    Add package ite/1.4.1/ite-1.4.1.zip

---
+++ packages/ite/ite-1.4.1.zip/ite-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='ite',
+    version='1.4.1',
+    py_modules=[''],
+    author='ccb',
+    author_email = '445365969@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'simple',
+    )
+
