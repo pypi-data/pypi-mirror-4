75a8bad3e072204617dd06293a60b97ffc5e44e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:02 +0000

    Add package adrest/2.2.2/adrest-2.2.2.tar.gz

---
+++ packages/adrest/adrest-2.2.2.tar.gz/adrest-2.2.2/tests/core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Pirate(models.Model):
+    name = models.CharField(max_length=50)
+    character = models.CharField(max_length=10, choices=(
+        ('good', 'good'),
+        ('evil', 'evil'),
+        ('sorrow', 'sorrow'),
+    ))
+
+
+class Island(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Treasure(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    pirate = models.ForeignKey(Pirate, null=True, blank=True)
+    island = models.ForeignKey(Island)
+
+
+class Boat(models.Model):
+    title = models.CharField(max_length=50)
+    pirate = models.ForeignKey(Pirate)
---
+++ packages/adrest/adrest-2.2.2.tar.gz/adrest-2.2.2/tests/core/tests/handler.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.views.generic import View
+from milkman.dairy import milkman
+
+from adrest.mixin import HandlerMixin
+from adrest.tests import AdrestTestCase
+from ..api import api as API
+
+
+class CoreHandlerTest(AdrestTestCase):
+
+    api = API
+
+    def test_meta_model(self):
+
+        class Resource(View, HandlerMixin):
+            model = 'core.pirate'
+
+        self.assertTrue(issubclass(Resource.model, models.Model))
+
+    def test_meta_name(self):
+
+        class Resource(View, HandlerMixin):
+            model = 'core.pirate'
+
+        self.assertEqual(Resource.meta.name, 'pirate')
+
+        class IslandResource(View, HandlerMixin):
+            model = 'core.island'
+            name = 'map'
+
+        self.assertEqual(IslandResource.meta.name, 'map')
+
+        class TreasureResource(View, HandlerMixin):
+            parent = Resource
+            model = 'core.treasure'
+
+        self.assertEqual(TreasureResource.meta.name, 'treasure')
+
+    def test_methods(self):
+
+        for _ in xrange(3):
+            pirate = milkman.deliver('core.pirate', character='good')
+
+        response = self.get_resource('pirate')
+        self.assertContains(response, '"count": 3')
+
+        response = self.post_resource('pirate', data=dict(
+            name='John',
+            character='evil',
+        ))
+        self.assertContains(response, '"name": "John"')
+
+        john = response.response
+        response = self.put_resource('pirate', pirate=john, data=dict(
+            name='Billy'
+        ))
+        self.assertContains(response, '"name": "Billy"')
+        billy = response.response
+        self.assertEqual(john, billy)
+
+        response = self.delete_resource('pirate', pirate=billy)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(billy).objects.filter(name='Billy').count(), 0)
+
+        response = self.patch_resource('pirate', data=dict(
+            pirate=[1, 2],
+            name='Tom'
+        ))
+        self.assertEqual(len(response.response), 2)
+
+        for pirate in response.response:
+            self.assertEqual(pirate.name, 'Tom')
+            self.assertTrue(pirate.pk in [1, 2])
+
+
+# lint_ignore=F0401
