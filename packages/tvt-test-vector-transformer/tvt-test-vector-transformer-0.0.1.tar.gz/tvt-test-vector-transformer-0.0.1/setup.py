f859e503721dc523d2865aa3e7aebab626e07634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:18 +0000

    Add package tvt-test-vector-transformer/0.0.1/tvt-test-vector-transformer-0.0.1.tar.gz

---
+++ packages/tvt-test-vector-transformer/tvt-test-vector-transformer-0.0.1.tar.gz/tvt-test-vector-transformer-0.0.1/setup.py
@@ -0,0 +1,31 @@
+
+from glob import glob
+from distutils.core import setup
+from setuptools import find_packages
+
+URL = 'https://github.com/gitfoxi/' \
+      'tvt-test-vector-transformer'
+
+setup(
+    name='tvt-test-vector-transformer',
+    version='0.0.1',
+    description='Translate digital test vectors for automated test '
+                'equipment for semiconductors',
+    author='Michael Fox',
+    author_email='415fox@gmail.com',
+    url=URL,
+    download_url=URL + '/tarball/master',
+    use_2to3=True,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: "
+        "Electronic Design Automation (EDA)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    packages=find_packages(),
+    scripts=glob("Scripts/*.py"),
+)
