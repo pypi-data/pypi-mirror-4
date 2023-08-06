06b953497e88c12742646964557693888d2903c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:21 +0000

    Add package seolib/0.1.3/seolib-0.1.3.zip

---
+++ packages/seolib/seolib-0.1.3.zip/seolib-0.1.3/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+
+requires = ['requests', 'lxml']
+
+packages = ['seolib']
+
+
+setup(
+	name='seolib',
+	version='0.1.3',
+	description='Python lib that provides you with some usefull and simple methods to obtain website metrics',
+	long_description=open('README.rst').read(),
+	author='Andrey Bernatsky',
+	author_email='xelblch@gmail.com',
+	license=open('LICENSE').read(),
+	url='http://www.xelblch.com/seolib/',
+
+	include_package_data=True,
+	packages=packages,
+	install_requires=requires,
+
+	classifiers=(
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'License :: OSI Approved :: Apache Software License',
+		'Intended Audience :: Developers',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Operating System :: Unix',
+		'Programming Language :: Python',
+	    'Programming Language :: Python :: 2.6',
+	    'Programming Language :: Python :: 2.7',
+	),
+)
