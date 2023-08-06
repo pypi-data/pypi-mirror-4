0581d4d90b4045fd772002356c6f475754a4aabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:37 +0000

    Add package pathfix.py/0.6.1/pathfix.py-0.6.1.tar.gz

---
+++ packages/pathfix.py/pathfix.py-0.6.1.tar.gz/pathfix.py-0.6.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+install_requires = []
+
+try:
+    import argparse
+except ImportError:
+    install_requires.append('argparse')
+
+setup(
+    name='pathfix.py',
+    version='0.6.1',
+    provides=['pathfix.py'],
+    description='Translate OS X / Windows file URLs to directory paths',
+    long_description=open('README.rst').read(),
+    author='James Rutherford',
+    author_email='jim@jimr.org',
+    url='https://github.com/jimr/pathfix.py',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        ],
+    license='MIT',
+    test_suite='tests',
+    scripts=['pathfix.py'],
+    py_modules=['pathfix'],
+    install_requires=install_requires,
+)
