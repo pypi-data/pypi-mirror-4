0e23ef888ea0df4e4ca180cf853bdd3a8275e7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:22 +0000

    Add package axon/0.2.1/axon-0.2.1.tar.gz

---
+++ packages/axon/axon-0.2.1.tar.gz/axon-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+VERSION = '0.2.1'
+
+setup(
+    name = 'axon',
+    packages = ['axon'], 
+    version = VERSION,
+    description = "A library to communicate with visionmedia's axon",
+    author = 'polyrabbit',
+    author_email = 'poly@wiredcraft.com',
+    url = 'https://github.com/devo-ps/python-node-axon',
+    download_url = 'https://github.com/devo-ps/python-node-axon/archive/0.2.tar.gz',
+)
