996e46307986b713d0349dc6ccced7c2a59971c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:17 +0000

    Add package Flask-WebCache/0.9.1/Flask-WebCache-0.9.1.tar.gz

---
+++ packages/Flask-WebCache/Flask-WebCache-0.9.1.tar.gz/Flask-WebCache-0.9.1/flask_webcache/handlers.py
@@ -0,0 +1,50 @@
+from flask import g
+
+from . import storage, validation, modifiers
+
+def register_extension(app, name, obj):
+    if not hasattr(app, 'extensions'):
+        app.extensions = {}
+    app.extensions.setdefault('webcache', {})[name] = obj
+
+class RequestHandler(storage.Retrieval):
+    def __init__(self, cache, app=None, config=None):
+        super(RequestHandler, self).__init__(cache, config)
+        if app is not None:
+            self.init_app(app)
+    def init_app(self, app):
+        app.before_request(self.before_request)
+        register_extension(app, 'request', self)
+    def before_request(self):
+        modifiers.setup_for_this_request()
+        g.webcache_cached_response = False
+        try:
+            if self.should_fetch_response() and not self.is_exempt():
+                return self.fetch_response()
+        except storage.CacheMiss:
+            pass
+
+class ResponseHandler(validation.Validation, storage.Store):
+    def __init__(self, cache, app=None, config=None):
+        storage.Store.__init__(self, cache, config)
+        if app is not None:
+            self.init_app(app)
+    def init_app(self, app):
+        app.after_request(self.after_request)
+        register_extension(app, 'response', self)
+    def after_request(self, response):
+        self.add_date_fields(response)
+        for modifier in modifiers.after_request:
+            modifier(response)
+        if self.can_set_etag(response):
+            self.set_etag(response)
+        if self.if_none_match(response):
+            return self.return_not_modified_response(response) or response
+        if g.webcache_cached_response:
+            return response
+        if self.should_cache_response(response) and not self.is_exempt():
+            self.cache_response(response)
+            self.mark_cache_miss(response)
+        elif self.should_invalidate_resource(response):
+            self.invalidate_resource()
+        return response
---
+++ packages/Flask-WebCache/Flask-WebCache-0.9.1.tar.gz/Flask-WebCache-0.9.1/flask_webcache/utils.py
@@ -0,0 +1,23 @@
+from random import getrandbits
+
+def make_salt(bits=128):
+    return hex(getrandbits(bits))
+
+def effective_max_age(response):
+    if response.cache_control.s_maxage is not None:
+        return response.cache_control.s_maxage
+    if response.cache_control.max_age is not None:
+        return response.cache_control.max_age
+    return None
+
+def none_or_truthy(v):
+    if v is None:
+        return True
+    return bool(v)
+
+def werkzeug_cache_get_or_add(cache, key, new_obj, expiry_seconds):
+    stored_obj = None
+    while stored_obj is None:
+        cache.add(key, new_obj, expiry_seconds)
+        stored_obj = cache.get(key)
+    return stored_obj
