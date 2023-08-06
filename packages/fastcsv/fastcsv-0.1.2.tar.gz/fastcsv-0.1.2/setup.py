f1cc06fa9809e403ffd9ac58300b974ace96aaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:55 +0000

    Add package fastcsv/0.1.2/fastcsv-0.1.2.tar.gz

---
+++ packages/fastcsv/fastcsv-0.1.2.tar.gz/fastcsv-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension
+
+setup(
+    name='fastcsv',
+    version='0.1.2',
+    description='fastcsv',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Text Processing'],
+    author='Masaya SUZUKI',
+    author_email='draftcode@gmail.com',
+    url='https://github.com/draftcode/fastcsv',
+    ext_modules=[Extension('_fastcsv',
+                           sources=['_fastcsv.c',
+                                    '_fastcsv_reader.c',
+                                    '_fastcsv_writer.c'])],
+    py_modules=['fastcsv'],
+    test_suite='tests',
+    test_loader='tests:RegexpPrefixLoader'
+    )
+
