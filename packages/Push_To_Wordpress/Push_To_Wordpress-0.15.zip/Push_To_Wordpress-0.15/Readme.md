2e1832d9a879e7e69f242135d2d8f9545c57a6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:54 +0000

    Add package Push_To_Wordpress/0.15/Push_To_Wordpress-0.15.zip

---
+++ packages/Push_To_Wordpress/Push_To_Wordpress-0.15.zip/Push_To_Wordpress-0.15/Readme.md
@@ -0,0 +1,58 @@
+#Push (Markdown) to wordpress
+
+**currently basic functionality**
+
+A simple, python based, commandline wordpress blogging tool.  
+A commandline wrapper to publishing with [python-markdown](https://github.com/waylan/Python-Markdown)
+
+##Version
+
+[Version](Version.md)
+
+##Techonology
+Using:
+
+1. [python-wordpress-xmlrpc](https://github.com/maxcutler/python-wordpress-xmlrpc)  
+2. [python-markdown](https://github.com/waylan/Python-Markdown)  
+
+using wordpress [XML-RPC Api](http://codex.wordpress.org/XML-RPC_WordPress_API) to drive wordpress.
+
+
+##Installing
+**The short way**:
+Straight from the cheeseshop
+
+    pip install push_to_wordpress
+
+**The long way**:
+
+
+    git clone git://github.com/alonisser/PushToWordpress.git
+
+    cd PushToWordpress
+    python setup.py install #(If that doesn't work you should run the build command first)
+
+
+##Usage:
+Current and quite basic:
+
+    presser --title optionaltitle --posts inputfile.md anotherinputfile
+
+where there is a config.ini file with wordpress blog connection details in the same folder. 
+
+**[Full command line options and flags](usage.txt)**
+
+Simple isn't it? going to be simpler soon..  
+currently can't handle media files with the posts (but check the TODO section for more info about that).
+Don't forget to enter your wordpress blog config in the config.ini file (look at configdemo.ini for inspiration)
+
+##License 
+see **[License](License.md)**
+
+##TODO or "How can I help or contribute":
+
+check our [TODO](TODO.md)
+
+##Notice:
+
+for older wordpress installs you should enable [enable the XML-RPC api](http://codex.wordpress.org/XML-RPC_Support) in later versions it's enabled by default.
---
+++ packages/Push_To_Wordpress/Push_To_Wordpress-0.15.zip/Push_To_Wordpress-0.15/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+VERSION = '0.15'
+
+# Grab requirments.
+
+with open('require.txt','r') as f:
+    required = f.readlines()
+
+
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    # os.system('python setup.py bdist_egg --target-version=2.7 register upload ')
+    # os.system('python setup.py bdist_wininst --target-version=2.7 register upload')
+    try:
+        os.system('pandoc -f markdown -t rst Readms.md -o Readme_rst.rst ')
+    except:
+        print("pandoc isn't installed, please update the readme_rst by yourself")
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+if sys.argv[-1] == 'test_upload':
+    os.system('python setup.py sdist upload -r https://testpypi.python.org/pypi')
+    sys.exit()
+
+with open('Readme_rst.rst', 'r') as f:
+    long_desc = f.read()
+
+setup(
+    name = "Push_To_Wordpress",
+    version = VERSION,
+    #scripts = ['presser.py'],
+
+    install_requires = required,
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.md', '*.rst','*.txt','*.ini'],
+    },
+    packages = ['push_to_wordpress',],
+    entry_points = {
+        'console_scripts': [
+            'presser = push_to_wordpress.presser:presser'
+        ],
+    },
+    # metadata for upload to PyPI
+    author = "Alonisser",
+    author_email = "alonisser@gmail.com",
+    description = "a commandline tool to post to Markdown files to wordpress posts (based on wordpress XML-RPC)",
+    long_description = long_desc,
+    license = "OSI Approved :: MIT License",
+    keywords = "blogging wordpress commandline",
+    url = "https://github.com/alonisser/PushToWordpress",   # project home page, if any
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7', #maybe more, didn't check
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        ]
+
+    
+
+    # could also include long_description, download_url, classifiers, etc.
+)
