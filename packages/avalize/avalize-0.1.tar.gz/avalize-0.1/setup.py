ab0473996e6a7af6d11a3d3f538d9db2b8458de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:34 +0000

    Add package avalize/0.1/avalize-0.1.tar.gz

---
+++ packages/avalize/avalize-0.1.tar.gz/avalize-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='avalize',
+    version='0.1',
+    packages=['avalize',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
