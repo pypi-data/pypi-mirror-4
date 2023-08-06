be95a9d3a001e9530a5a588673f88148eaa2db0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package parse_rest/0.0.2012/parse_rest-0.0.2012.tar.gz

---
+++ packages/parse_rest/parse_rest-0.0.2012.tar.gz/parse_rest-0.0.2012/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='parse_rest',
+    version='0.0.2012',
+    description='A client library for Parse.com\'.s REST API',
+    url='https://github.com/dgrtwo/ParsePy',
+    packages=['parse_rest'],
+    maintainer="David Robinson",
+    maintainer_email="dgrtwo@princeton.edu",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+        ]
+    )
