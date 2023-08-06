c4c82c92af25e03d02f0d94c11ba86dd96ca8823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:10 +0000

    Add package looplist/1.0.0/looplist-1.0.0.zip

---
+++ packages/looplist/looplist-1.0.0.zip/looplist-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+                name                = 'looplist',
+                version             = '1.0.0',
+                py_modules   = ['looplist'] ,
+                author              = 'metributor',
+                author_email = 'iodunlap@live.com',
+                url                       = '',
+                description     = 'A nested list printer, use print_looplist(list_name) to use',
+             )
