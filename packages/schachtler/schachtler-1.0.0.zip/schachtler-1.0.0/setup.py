488d03c881d8538ab992bf94782640a2b003ba9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package schachtler/1.0.0/schachtler-1.0.0.zip

---
+++ packages/schachtler/schachtler-1.0.0.zip/schachtler-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'schachtler',
+	version = '1.0.0',
+	py_modules = 'schachtler',
+	author = 'd3rd4v1d',
+	author_email = 'd4vid81@gmail.com',
+	url = 'http://www.netzrebel.com/',
+	description = 'some weird exercise from my python book'
+	)
