1406d2f9ff685cf179091fcb2887bf10c9273c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:35 +0000

    Add package anybox.buildbot.openerp/0.8.1/anybox.buildbot.openerp-0.8.1.tar.gz

---
+++ packages/anybox.buildbot.openerp/anybox.buildbot.openerp-0.8.1.tar.gz/anybox.buildbot.openerp-0.8.1/anybox/buildbot/openerp/buildouts.py
@@ -0,0 +1,8 @@
+from ConfigParser import ConfigParser
+
+
+def parse_manifest(filepath):
+    """Return a ConfigParser for buildouts MANIFEST, ready for queries."""
+    parser = ConfigParser()
+    parser.read(filepath)
+    return parser
