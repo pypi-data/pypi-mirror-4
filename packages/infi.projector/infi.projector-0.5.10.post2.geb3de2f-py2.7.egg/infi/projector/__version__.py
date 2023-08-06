f9acb878c9e6e0c1969fbe688f07b8f1be3407c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package infi.projector/0.5.10.post2.geb3de2f/infi.projector-0.5.10.post2.geb3de2f-py2.7.egg

---
+++ packages/infi.projector/infi.projector-0.5.10.post2.geb3de2f-py2.7.egg/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.10.post2.geb3de2f"
+__git_commiter_name__ = "wiggin15"
+__git_commiter_email__ = "wiggin15@yahoo.com"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@infinigit.infinidat.com:/host/projector.git'
+__git_head_hash__ = 'eb3de2faec4186b60840ba5a8e38916c92db4eb7'
+__git_head_subject__ = "HOSTDEV-690 block 'projector version release current/latest'"
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
