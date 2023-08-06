f838679866d0e7b6edf6768e909e87759d284809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package datCrawl/0.2.0/datCrawl-0.2.0.tar.gz

---
+++ packages/datCrawl/datCrawl-0.2.0.tar.gz/datCrawl-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='datCrawl',
+    version='0.2.0',
+    author='Felipe Martin',
+    author_email='fmartingr@me.com',
+    packages=['datCrawl'],
+    url='http://github.com/fmartingr/datCrawl/',
+    license='LICENSE',
+    description='Crawl as is you were using an API.',
+)
---
+++ packages/datCrawl/datCrawl-0.2.0.tar.gz/datCrawl-0.2.0/test/test_base.py
@@ -0,0 +1,25 @@
+import unittest
+from datCrawl import *
+from test.requirements import *
+
+
+class datCrawlBaseTests(unittest.TestCase):
+
+    def test_instance_check(self):
+        core = datCrawl()
+        self.assertTrue(isinstance(core, datCrawl))
+
+    def test_register_urls(self):
+        core = datCrawl()
+        data = ('action', 'http://www.google.es/', 'AwesomeGoogleCrawler')
+        core.register_url(data[0], data[1], data[2])
+        self.assertEquals(core.urls[0], data)
+
+    def test_running_full_crawler(self):
+        core = datCrawl()
+        core.register_crawler(AwesomeWikipediaTitleCrawler)
+        result = core.run('http://en.wikipedia.org/wiki/Python')
+        self.assertEquals(result['title'], 'Python')
+
+if __name__ == '__main__':
+    unittest.main()
