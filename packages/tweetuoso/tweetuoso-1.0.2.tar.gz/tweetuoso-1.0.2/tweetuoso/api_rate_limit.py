8cc935f6ae2c557b4a1b99b4bc3189395a6bd745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package tweetuoso/1.0.2/tweetuoso-1.0.2.tar.gz

---
+++ packages/tweetuoso/tweetuoso-1.0.2.tar.gz/tweetuoso-1.0.2/tweetuoso/api_rate_limit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__appname__ = 'api_rate_limit'
+__version__ = "0.3"
+__author__ = "@c0ding <https://twitter.com/c0ding>"
+__licence__ = "WTFPL"
+
+import os
+import tweepy as tw
+from auth import keys
+from colorama import Fore, Style
+
+def rate_limit():
+	try:
+		auth = tw.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+		auth.set_access_token(keys['access_token'], keys['access_secret'])
+		api = tw.API(auth)
+		print api.rate_limit_status()
+		
+	except tw.TweepError as error:
+		os.system('clear')
+		print(Fore.RED + ">> " + Fore.RESET + "Error occured: %s" % error)
+		return
+					
+if __name__ == '__main__':
+	rate_limit()
