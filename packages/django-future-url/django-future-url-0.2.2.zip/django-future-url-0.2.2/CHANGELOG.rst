6af55f3a5084e7fa7f0ac9ed94e5d91882d71b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:08 +0000

    Add package django-future-url/0.2.2/django-future-url-0.2.2.zip

---
+++ packages/django-future-url/django-future-url-0.2.2.zip/django-future-url-0.2.2/CHANGELOG.rst
@@ -0,0 +1,29 @@
+Changelog
+=========
+
+0.2.2 (2013-01-28)
+^^^^^^^^^^^^^^^^^^
+* Clarification for README
+
+
+0.2.1 (2013-01-28)
+^^^^^^^^^^^^^^^^^^
+* Fixed setup.py to properly define runner
+* Added even more tests
+* Improved output
+* Made ``--verbose`` a default behavior
+
+
+0.2 (2013-01-27)
+^^^^^^^^^^^^^^^^
+* Renamed the tool from ``django-make-future-url.py`` to  ``future_url``
+* Removed ``--dry-run`` option, to make actual changes use ``--write``
+* Added python 3 support (Roman Gladkov)
+* Internal refactoring
+* Added tests
+
+
+0.1 (2012-04-24)
+^^^^^^^^^^^^^^^^
+* Initial release
+
---
+++ packages/django-future-url/django-future-url-0.2.2.zip/django-future-url-0.2.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+import codecs
+from setuptools import setup, find_packages
+
+import django_future_url
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+setup(
+    name="django-future-url",
+    version=django_future_url.__version__,
+    author='Vitaly Olevinsky',
+    author_email='olevinsky.v.s@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/futurecolors/django-future-url/',
+    description="Migration tool for django 1.4, fixes url template tag deprecation warnings.",
+    long_description=read(os.path.join(os.path.dirname(__file__), 'README.rst')),
+    license='MIT',
+    install_requires=['docopt'],
+    scripts=['django_future_url/bin/future_url'],
+    tests_require=['cram==0.5'],
+    test_suite='django_future_url.test',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
