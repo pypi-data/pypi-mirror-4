0eb3007dfc5bb88dbba9d8ed4ffca778a745b7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:46 +0000

    Add package swftp/1.0.3/swftp-1.0.3.tar.gz

---
+++ packages/swftp/swftp-1.0.3.tar.gz/swftp-1.0.3/swftp/__init__.py
@@ -0,0 +1,11 @@
+"""
+SwFTP is an FTP and SFTP interface for Openstack Swift
+
+See COPYING for license information.
+"""
+from swftp.utils import VERSION
+__title__ = 'swftp'
+__version__ = VERSION
+__author__ = 'SoftLayer Technologies, Inc.'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 SoftLayer Technologies, Inc.'
