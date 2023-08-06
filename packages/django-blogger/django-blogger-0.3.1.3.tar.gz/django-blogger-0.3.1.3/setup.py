9b9fdbd22fa56361622bf3ef2813f2f2922870f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package django-blogger/0.3.1.3/django-blogger-0.3.1.3.tar.gz

---
+++ packages/django-blogger/django-blogger-0.3.1.3.tar.gz/django-blogger-0.3.1.3/setup.py
@@ -0,0 +1,17 @@
+import os
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-blogger",
+    version = "0.3.1.3",
+    author = "Jake Gaylor",
+    author_email = "jake@codegur.us",
+    description = "A package to administer a simple blog via Django.",
+    url = "https://github.com/jhgaylor/django-blogger/",
+    dependancy_links = ['-e git+https://github.com/yedpodtrzitko/django-taggit.git#egg=django-taggit-0.9.4'],
+    install_requires = [ln for ln in open('requirements.txt')],
+    packages = find_packages(), 
+    #package_data=package_data,
+    include_package_data = True,
+)
