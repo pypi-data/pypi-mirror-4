4280ea77728d0c4266a69b23a529059885c08289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:03 +0000

    Add package django-mediamosa-tinymce/0.1/django-mediamosa-tinymce-0.1.tar.gz

---
+++ packages/django-mediamosa-tinymce/django-mediamosa-tinymce-0.1.tar.gz/django-mediamosa-tinymce-0.1/mediamosa_tinymce/views.py
@@ -0,0 +1,45 @@
+import json
+
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, Http404
+
+from django_mediamosa.base import api
+
+
+class TinyMceAssetSelectionDialog(ListView):
+    template_name = 'mediamosa_tinymce/tinymce_asset_dialog.html'
+    context_object_name = 'public_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        asset_list = api.asset_list(
+            mime_type='mp4',
+            mime_type_match='contains',
+            is_public_list=True)
+        return asset_list
+
+    def get_context_data(self, **kwargs):
+        context = super(TinyMceAssetSelectionDialog, self).get_context_data(**kwargs)
+        context['private_list'] = api.asset_list(
+            mime_type='mp4',
+            mime_type_match='contains',
+            owner_id=self.request.user.username)
+        return context
+
+
+class JSONTinyMceMediafileId(DetailView):
+
+    def get_object(self, queryset=None):
+        asset_id = self.request.GET.get('id')
+        try:
+            mediafile = api.asset(asset_id).get_mediafile()
+        except:
+            return None
+        return mediafile
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.object is None:
+            raise Http404
+        else:
+            return HttpResponse(
+                json.dumps({'mediafile_id': self.object.id}))
