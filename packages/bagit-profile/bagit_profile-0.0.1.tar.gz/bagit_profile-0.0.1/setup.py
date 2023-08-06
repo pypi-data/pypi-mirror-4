37279647e9bda43feb9d6285ecf39a43c76f6d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:56 +0000

    Add package bagit-profile/0.0.1/bagit_profile-0.0.1.tar.gz

---
+++ packages/bagit-profile/bagit_profile-0.0.1.tar.gz/bagit_profile-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+description = \
+    """
+    This module can be used to validate BagitProfiles.
+    """
+with open('README.rst') as file:
+  long_description = file.read()
+
+setup(
+      name = 'bagit_profile',
+      version = '0.0.1',
+      url = 'https://github.com/ruebot/bagit-profiles-validator',
+      install_requires=['bagit', 'requests'],
+      author = 'Mark Jordon, Nick Ruest',
+      author_email = 'mjordan@sfu.ca, ruestn@gmail.com',
+      py_modules = ['bagit_profile'],
+      scripts = ['bagit_profile.py'],
+      description = description,
+      long_description = long_description,
+      platforms = ['POSIX'],
+      test_suite = 'test',
+      classifiers = [
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities',
+      ],
+)
