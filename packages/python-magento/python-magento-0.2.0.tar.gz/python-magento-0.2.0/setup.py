71063063b7e76208d98d7baa82445ee34ebd0c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:47 +0000

    Add package python-magento/0.2.0/python-magento-0.2.0.tar.gz

---
+++ packages/python-magento/python-magento-0.2.0.tar.gz/python-magento-0.2.0/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='python-magento',
+    version='0.2.0',
+    author='Vikram Oberoi',
+    author_email='voberoi@gmail.com',
+    packages=['magento'],
+    entry_points={
+        'console_scripts': [
+            'magento-ipython-shell = magento.magento_ipython_shell:main'
+        ]
+    },
+    url='http://pypi.python.org/pypi/python-magento/',
+    license='LICENSE.md',
+    description='A Python wrapper to Magento\'s XML-RPC API.',
+    long_description=open('README.rst').read(),
+)
