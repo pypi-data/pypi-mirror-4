77e602fa0cad01256f93ffa0c207a981193a47b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:14 +0000

    Add package costools/1.1/costools-1.1.tar.gz

---
+++ packages/costools/costools-1.1.tar.gz/costools-1.1/lib/costools/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import division         # confidence high
+
+from .version import *
+
+import timefilter
+
+# These lines allow TEAL to print out the names of TEAL-enabled tasks
+# upon importing this package.
+import os
+from stsci.tools import teal
+teal.print_tasknames(__name__, os.path.dirname(__file__))
