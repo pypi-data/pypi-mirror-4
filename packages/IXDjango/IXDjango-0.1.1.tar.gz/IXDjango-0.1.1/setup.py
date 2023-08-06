3c68eb1d6e6c959eeb0a623693eefd7157695dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:08 +0000

    Add package IXDjango/0.1.1/IXDjango-0.1.1.tar.gz

---
+++ packages/IXDjango/IXDjango-0.1.1.tar.gz/IXDjango-0.1.1/setup.py
@@ -0,0 +1,22 @@
+"""
+Package configuration file
+"""
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='IXDjango',
+    version='0.1.1',
+    author='Infoxchanhe Australia dev team',
+    author_email='devs@infoxchange.net.au',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/IXDjango/',
+    license='MIT',
+    description='Management commands for deploying Django projects.',
+    long_description=open('README').read(),
+    install_requires=[
+        "Django >= 1.3.0",
+        "pep8 >= 1.0.1",
+        "pylint >= 0.25.1"
+    ],
+)
