6f521665c9ab622677fee1938069ae28c631d368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:20 +0000

    Add package multiplicationTable/1.0.0/multiplicationTable-1.0.0.tar.gz

---
+++ packages/multiplicationTable/multiplicationTable-1.0.0.tar.gz/multiplicationTable-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'multiplicationTable',
+	version 	= '1.0.0',
+	py_modules	= ['multiplicationTable'],
+	author		= 'navnad',
+	author_email	= 'navandn@gmail.com',
+	url		= '',
+	description	= 'Create multiplication table',
+)
