eb4e58b05caa9932a780d6c55080fc6bf235f621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:31 +0000

    Add package pyformat/0.2/pyformat-0.2.tar.gz

---
+++ packages/pyformat/pyformat-0.2.tar.gz/pyformat-0.2/example.py
@@ -0,0 +1,29 @@
+def launch_rocket   ():
+
+
+
+    """Launch
+the
+rocket. Go colonize space."""
+
+def factorial(x):
+    '''
+
+    Return x factorial.
+
+    This uses math.factorial.
+
+    '''
+    import math
+    import re
+    import os
+    return math.factorial( x );
+def print_factorial(x):
+    """Print x factorial"""
+    print( factorial(x)  )
+def main():
+    """Main
+    function"""
+    print_factorial(5)
+    if factorial(10):
+    	launch_rocket()
