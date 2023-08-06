e98621ed2506213e3db53a518b454a0bc8856b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:54 +0000

    Add package django-paging/0.2.5/django-paging-0.2.5.tar.gz

---
+++ packages/django-paging/django-paging-0.2.5.tar.gz/django-paging-0.2.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+tests_require = [
+    'unittest2',
+    'django',
+]
+
+setup(
+    name='django-paging',
+    version='0.2.5',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/dcramer/django-paging',
+    install_requires=[
+        'django-templatetag-sugar>=0.1',
+    ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='unittest2.collector',
+    description='An efficient paginator that works.',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
