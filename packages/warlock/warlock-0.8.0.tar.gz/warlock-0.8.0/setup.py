61c70934b5a02e97ce73a978d3b982950ed1fa16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:26 +0000

    Add package warlock/0.8.0/warlock-0.8.0.tar.gz

---
+++ packages/warlock/warlock-0.8.0.tar.gz/warlock-0.8.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+def parse_requirements():
+    fap = open('requirements.txt', 'r')
+    raw_req = fap.read()
+    fap.close()
+    return raw_req.split('\n')
+
+
+setuptools.setup(
+    name='warlock',
+    version='0.8.0',
+    description='Python object model built on JSON schema and JSON patch.',
+    author='Brian Waldon',
+    author_email='bcwaldon@gmail.com',
+    url='http://github.com/bcwaldon/warlock',
+    packages=['warlock'],
+    install_requires=parse_requirements(),
+)
---
+++ packages/warlock/warlock-0.8.0.tar.gz/warlock-0.8.0/warlock/core.py
@@ -0,0 +1,21 @@
+"""Core Warlock functionality"""
+
+import copy
+
+import model
+
+
+def model_factory(schema, base_class=model.Model):
+    """Generate a model class based on the provided JSON Schema
+
+    :param schema: dict representing valid JSON schema
+    """
+    schema = copy.deepcopy(schema)
+
+    class Model(base_class):
+        def __init__(self, *args, **kwargs):
+            self.__dict__['schema'] = schema
+            base_class.__init__(self, *args, **kwargs)
+
+    Model.__name__ = str(schema['name'])
+    return Model
