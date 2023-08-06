c6b887a97c04c7823c1e41b87a4e15d88c008482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package redis-collections/0.1.3/redis-collections-0.1.3.tar.gz

---
+++ packages/redis-collections/redis-collections-0.1.3.tar.gz/redis-collections-0.1.3/redis_collections/__init__.py
@@ -0,0 +1,14 @@
+
+
+__title__ = 'redis_collections'
+__version__ = '0.1.3'
+__author__ = 'Jan Javorek'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2013 Jan Javorek'
+
+
+from .base import RedisCollection
+
+from .sets import Set
+from .lists import List
+from .dicts import Dict, Counter
