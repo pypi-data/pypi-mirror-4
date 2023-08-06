0381ed7155ef084135196e6d51224674150f123e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:12 +0000

    Add package django-oscar-easyrec/0.0.2/django-oscar-easyrec-0.0.2.tar.gz

---
+++ packages/django-oscar-easyrec/django-oscar-easyrec-0.0.2.tar.gz/django-oscar-easyrec-0.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='django-oscar-easyrec',
+      version='0.0.2',
+      url='https://github.com/tangentlabs/django-oscar-easyrec',
+      author="Jonathan Moss",
+      author_email="jonathan.moss@tangentsnowball.com.au",
+      description="easyrec recommendations module for django-oscar",
+      long_description=open('README.rst').read(),
+      keywords="Recommendation, easyrec",
+      license='BSD',
+      packages=find_packages(exclude=['tests*',]),
+      install_requires=[
+            'django-oscar>=0.5-pre-alpha',
+            'requests>=1.1.0,<1.2'
+      ],
+      dependency_links=[
+            'https://github.com/tangentlabs/django-oscar/tarball/master#egg=django-oscar-0.5-pre-alpha',
+      ],
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python']
+      )
