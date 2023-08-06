4d7698e5f7d3f34518487343f7ebb751e9db6b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:08 +0000

    Add package django_price/0.1.1-1/django_price-0.1.1-1.tar.gz

---
+++ packages/django_price/django_price-0.1.1-1.tar.gz/django_price-0.1.1-1/django_price/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+# we do not add admins, this has to be done inside the project
+#admin.site.register(LinearTax, admin.ModelAdmin)
+#admin.site.register(MultiTax, admin.ModelAdmin)
+
