9df2af7e531f7947ed8a896fbf7c92555b0819f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:47 +0000

    Add package bashplotlib/0.3.0/bashplotlib-0.3.0.tar.gz

---
+++ packages/bashplotlib/bashplotlib-0.3.0.tar.gz/bashplotlib-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="bashplotlib",
+    version="0.3.0",
+    author="Greg Lamp",
+    author_email="lamp.greg@gmail.com",
+    url="https://github.com/glamp/bashplotlib",
+    license="LICENSE.txt",
+    packages=find_packages(),
+    description="plotting from the command line",
+    long_description="README.txt",
+)
+
