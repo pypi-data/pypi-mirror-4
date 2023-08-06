e7c924b03957690b638bb7327cee613905800957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package Kokusai_PP_pack/0.1.0/Kokusai_PP_pack-0.1.0.tar.gz

---
+++ packages/Kokusai_PP_pack/Kokusai_PP_pack-0.1.0.tar.gz/Kokusai_PP_pack-0.1.0/setup.py
@@ -0,0 +1,11 @@
+## kokusai biaxial testing data post-processor setup.py module.
+from distutils.core import setup
+setup(name='Kokusai_PP_pack',
+      version='0.1.0',
+      description='Python package for kokusai post-processor',
+      author='Youngung Jeong',
+      author_email='youngung.jeong@gmail.com',
+      packages=['kokusai_bpp'],
+      package_dir={'':'src'}
+      )
+
