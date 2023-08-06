164071263e05780d6c1857fcd192f469e39b906e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package collective.z3cform.widgets/1.0b3/collective.z3cform.widgets-1.0b3.zip

---
+++ packages/collective.z3cform.widgets/collective.z3cform.widgets-1.0b3.zip/collective.z3cform.widgets-1.0b3/src/collective/z3cform/widgets/interfaces.py
@@ -0,0 +1,14 @@
+
+from z3c.form import interfaces
+
+
+class ITokenInputWidget(interfaces.ITextLinesWidget):
+    """Text lines widget."""
+
+
+class IEnhancedTextLinesWidget(interfaces.ITextLinesWidget):
+    """Text lines widget."""
+
+
+class ISimpleRichTextWidget(interfaces.ITextAreaWidget):
+    """Simple RichText widget."""
