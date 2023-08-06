ce0452d8135a5d67715a8d532bf0d1e5955bdf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:50 +0000

    Add package USBLock/0.1.3/USBLock-0.1.3.tar.gz

---
+++ packages/USBLock/USBLock-0.1.3.tar.gz/USBLock-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='USBLock',
+    version='0.1.3',
+    author='Sven Steinbauer',
+    author_email='sven@unlogic.co.uk',
+    packages=['usblock'],
+    scripts=['bin/usblock'],
+    url='http://pypi.python.org/pypi/USBLock',
+    license='LICENSE.txt',
+    description='Lock and unlock your desktop using a USB stick as a key.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "daemonize==2.1.1",
+        "argparse==1.1.1",
+    ],
+)
