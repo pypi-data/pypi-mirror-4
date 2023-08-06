219efe5f72d0b6fad5091884ed576fd04a61faf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:51 +0000

    Add package durabledict/0.7.0/durabledict-0.7.0.tar.gz

---
+++ packages/durabledict/durabledict-0.7.0.tar.gz/durabledict-0.7.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+try:
+    import multiprocessing  # Seems to fix http://bugs.python.org/issue15881
+except ImportError:
+    pass
+
+setup_requires = []
+
+if 'nosetests' in sys.argv[1:]:
+    setup_requires.append('nose')
+
+setup(
+    name='durabledict',
+    version='0.7.0',
+    author='DISQUS',
+    author_email='opensource@disqus.com',
+    url='http://github.com/disqus/durabledict/',
+    description = 'Dictionary-style access to different types of models.',
+    packages=find_packages(),
+    zip_safe=False,
+    tests_require=[
+        'Django',
+        'nose',
+        'mock',
+        'redis',
+        'kazoo'
+    ],
+    test_suite = 'nose.collector',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
