8b858026179e7486a2d29ea4521881540095dfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package django-rosetta-inpage/0.0.10/django-rosetta-inpage-0.0.10.tar.gz

---
+++ packages/django-rosetta-inpage/django-rosetta-inpage-0.0.10.tar.gz/django-rosetta-inpage-0.0.10/rosetta_inpage/__init__.py
@@ -0,0 +1,27 @@
+VERSION = (0, 0, 10)
+# 0.0.9 is on pypi
+
+import hashlib
+
+
+def get_version():
+    if len(VERSION) > 3 and VERSION[3] != 'final':
+        return '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
+
+__version__ = get_version()
+
+
+def hash_text(text):
+    """
+    Create a md5 hash for for a message string
+
+    @param text:
+    @rtype: str
+    """
+    encoding = text.encode("UTF-16LE")
+    md5 = hashlib.md5()
+    md5.update(encoding)
+    return md5.hexdigest()
+
---
+++ packages/django-rosetta-inpage/django-rosetta-inpage-0.0.10.tar.gz/django-rosetta-inpage-0.0.10/rosetta_inpage/conf.py
@@ -0,0 +1,14 @@
+"""
+Some configurations & constants
+"""
+COOKIE_PARAM = 'rosetta_inpage_view_locale'
+EDIT_MODE = 'rosetta_inpage_edit_mode'
+MESSAGES = 'rosetta_inpage_messages'
+REQUEST = 'rosetta_inpage_request'
+REQUEST_LOCALE = 'rosetta_inpage_request_locale'
+
+THIRD_PARTY_APPS = True
+
+
+
+
