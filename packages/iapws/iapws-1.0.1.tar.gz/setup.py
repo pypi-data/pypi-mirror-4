d5d87608b736a5f9bab185fabe82b09257c29a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package iapws/1.0.1/iapws-1.0.1.tar.gz

---
+++ packages/iapws/iapws-1.0.1.tar.gz/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+                                                     
+setup(  
+	name = 'iapws',
+	version = '1.0.1',
+	py_modules = ['iapws'],
+	author = 'jjgomera',
+	author_email = 'jjgomera@gmail.com',
+	url = '',
+	description  = 'Python implementation of international-standard IAPWS-IF97 steam tables ',
+	license = "gpl v3")
+                                                                                
+
