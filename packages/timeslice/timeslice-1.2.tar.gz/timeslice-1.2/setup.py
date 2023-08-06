db90557c0d4a4ba41df4567fd4047f0d845947a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:00 +0000

    Add package timeslice/1.2/timeslice-1.2.tar.gz

---
+++ packages/timeslice/timeslice-1.2.tar.gz/timeslice-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='timeslice',
+    version='1.2',
+    author='Rafael Marques Martins',
+    author_email='snit.ram@gmail.com',
+    packages=['timeslice',],
+    url='http://github.com/snit-ram/timeslice'
+)
