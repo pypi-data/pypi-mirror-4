9c6f014d10534cee263ca5044b4ecad2011a01d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:43 +0000

    Add package tw2.slideymenu/2.1/tw2.slideymenu-2.1.tar.gz

---
+++ packages/tw2.slideymenu/tw2.slideymenu-2.1.tar.gz/tw2.slideymenu-2.1/tw2/slideymenu/__init__.py
@@ -0,0 +1,13 @@
+""" TW2 fancy slidey menu that I made just for you!
+
+Slideymenu is best demonstrated by its use in `narcissus
+<http://narcissus.rc.rit.edu>`_.  Definitely check it out.
+
+You can also see it in the `tw2 WidgetBrowser
+<http://tw2-demos.threebean.org/module?module=tw2.slideymenu>`_ although it
+doesn't do it justice.
+
+Get the source from http://github.com/toscawidgets/tw2.slideymenu
+"""
+
+from widgets import *
